629fa3a03ae9b10a3dc438ec64beb23754fa5328
Xavier J <xjulien@octo.com>
2015-08-11 15:07:54 +0200

    Suppression décorateur et helpeur inutile

--- app/decorators/etablissement_decorator.rb
+++
@@ -1,7 +0,0 @@
-class EtablissementDecorator < Draper::Decorator
-  delegate_all
-
-  def siege_social_true_false
-    siege_social? ? 'Cet établissement est le siège social' : 'Cet établissement n\'est pas le siège social'
-  end
-end
--- app/helpers/recapitulatif_helper.rb
+++
@@ -1,2 +0,0 @@
-module RecapitulatifHelper
-end
