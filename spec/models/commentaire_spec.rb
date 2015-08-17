db79bd9c3d8e067eedebaacae29c8f50449b76bc
Tanguy PATTE <tanguy.patte@gmail.com>
2015-08-17 11:43:01 +0200

    add test to commentaire

---
+++ spec/models/commentaire_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe Commentaire do
+  it { is_expected.to have_db_column(:email) }
+  it { is_expected.to have_db_column(:body) }
+  it { is_expected.to have_db_column(:created_at) }
+  it { is_expected.to have_db_column(:updated_at) }
+  it { is_expected.to belong_to(:dossier) }
+end
