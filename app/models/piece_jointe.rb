92e522eb3d60cb48f5f826f2d3fbf47179635f48
Tanguy PATTE <tanguy.patte@gmail.com>
2015-08-18 17:00:09 +0200

    delegate libelle do type piece jointe

--- app/models/piece_jointe.rb
+++ app/models/piece_jointe.rb
@@ -1,7 +1,7 @@
 class PieceJointe < ActiveRecord::Base
   belongs_to :dossier
   belongs_to :type_piece_jointe
-  delegate :api_entreprise, to: :type_piece_jointe
+  delegate :api_entreprise, :libelle, to: :type_piece_jointe
   mount_uploader :content, PieceJointeUploader
 
 
