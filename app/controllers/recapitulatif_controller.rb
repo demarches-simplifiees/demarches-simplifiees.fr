152e7cd31ec4f1e4c0374552f377a37648517fc8
Tanguy PATTE <tanguy.patte@gmail.com>
2015-08-20 16:39:36 +0200

    style check

--- app/controllers/recapitulatif_controller.rb
+++ app/controllers/recapitulatif_controller.rb
@@ -3,13 +3,13 @@ class RecapitulatifController < ApplicationController
     @dossier = Dossier.find(params[:dossier_id])
     @dossier = @dossier.decorate
 
-    #mettre dans le modèle
+    # mettre dans le modele
     @commentaires = @dossier.commentaires.order(created_at: :desc)
 
     @commentaires = @commentaires.all.decorate
 
     @commentaire_email = @dossier.mail_contact
   rescue
-     redirect_to url_for({controller: :start, action: :error_dossier})
+    redirect_to url_for(controller: :start, action: :error_dossier)
   end
 end
