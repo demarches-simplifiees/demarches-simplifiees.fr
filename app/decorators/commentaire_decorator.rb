3c7d291c52b032cd672bef6054ea5028156f7879
Tanguy PATTE <tanguy.patte@gmail.com>
2015-08-20 16:42:05 +0200

    style check

--- app/decorators/commentaire_decorator.rb
+++ app/decorators/commentaire_decorator.rb
@@ -2,7 +2,7 @@ class CommentaireDecorator < Draper::Decorator
   delegate_all
 
   def created_at_fr
-    created_at.to_datetime.strftime("%d/%m/%Y - %H:%M")
+    created_at.to_datetime.strftime('%d/%m/%Y - %H:%M')
   rescue
     'dd/mm/YYYY - HH:MM'
   end
