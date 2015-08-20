a1e9b938d63f6abc092726e156bf0ea668520045
Tanguy PATTE <tanguy.patte@gmail.com>
2015-08-20 16:42:32 +0200

    style check

--- app/decorators/dossier_decorator.rb
+++ app/decorators/dossier_decorator.rb
@@ -2,7 +2,7 @@ class DossierDecorator < Draper::Decorator
   delegate_all
 
   def date_fr
-    date_previsionnelle.to_date.strftime("%d/%m/%Y")
+    date_previsionnelle.to_date.strftime('%d/%m/%Y')
   rescue
     'dd/mm/YYYY'
   end
