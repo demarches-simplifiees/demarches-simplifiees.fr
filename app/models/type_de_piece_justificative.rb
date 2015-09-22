542cdbaefd8762614081a15b3f75a7f4a4b4c179
Xavier J <xjulien@octo.com>
2015-09-22 13:11:56 +0200

    validation des pièces justificative

--- app/models/type_de_piece_justificative.rb
+++ app/models/type_de_piece_justificative.rb
@@ -1,4 +1,7 @@
 class TypeDePieceJustificative < ActiveRecord::Base
   has_many :pieces_justificatives
   belongs_to :procedure
+
+  validates :libelle, presence: true, allow_blank: false, allow_nil: false
+  validates :description, presence: true, allow_blank: false, allow_nil: false
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150922085811) do
+ActiveRecord::Schema.define(version: 20150922110719) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -126,9 +126,9 @@ ActiveRecord::Schema.define(version: 20150922085811) do
   create_table "types_de_piece_justificative", force: :cascade do |t|
     t.string   "libelle"
     t.string   "description"
-    t.boolean  "api_entreprise"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.boolean  "api_entreprise", default: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
     t.integer  "procedure_id"
   end
 
