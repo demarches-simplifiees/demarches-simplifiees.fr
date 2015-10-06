f4e3ec1a724a7711ad2fd944ff48507a5cbc738c
Tanguy PATTE <tanguy.patte@gmail.com>
2015-10-06 17:22:06 +0200

    add feature test on france connect

--- spec/features/france_connect/france_connect_spec.rb
+++ spec/features/france_connect/france_connect_spec.rb
@@ -12,5 +12,16 @@ feature 'France Connect Connexion' do
       expect(page).to have_css('a#france_connect')
     end
 
+    context 'and click on france connect link' do
+      let(:code) { 'plop' }
+      before do
+        allow_any_instance_of(FranceConnectClient).to receive(:authorization_uri).and_return(france_connect_callback_path(code: code))
+        allow(FranceConnectService).to receive(:retrieve_user_informations).and_return(Hashie::Mash.new(email: 'patator@cake.com'))
+        page.find_by_id('france_connect').click
+      end
+      scenario 'he is redirected to france connect' do
+        expect(page).to have_content('Vos dossiers')
+      end
+    end
   end
 end
