b979c9df329c50f6f2023137693ae307e3ce7e1b
Xavier J <xjulien@octo.com>
2015-10-06 16:35:03 +0200

    begin controler france connect test bad code params

--- spec/controllers/france_connect_controller_spec.rb
+++ spec/controllers/france_connect_controller_spec.rb
@@ -17,14 +17,31 @@ describe FranceConnectController, type: :controller do
       end
     end
     context 'when params code is present' do
-      let(:code) { 'plop'}
+      let(:code) { 'plop' }
+      before do
+        get :callback, code: code
+      end
+
       context 'when code is correct' do
         before do
           allow(FranceConnectService).to receive(:retrieve_user_informations).and_return(Hashie::Mash.new(email: 'patator@cake.com'))
         end
-        it 'redirect to login page' do
-          get :callback, code: code
+        it 'redirect to dossiers list' do
           expect(response).to redirect_to(controller: 'users/dossiers', action: :index)
+        end
+      end
+
+      context 'wen code is not correct' do
+        before do
+          allow(FranceConnectService).to receive(:retrieve_user_informations).and_raise(Rack::OAuth2::Client::Error)
+        end
+
+        it 'redirect to login page' do
+          expect(response).to redirect_to(controller: 'users/sign_in', action: :index)
+        end
+
+        it 'flash message error connexion with France Connect' do
+
         end
       end
     end
