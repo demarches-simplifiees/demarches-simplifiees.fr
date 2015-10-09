0bd43e538af45fd36c1bc969a6b11ced40b18b01
Xavier J <xjulien@octo.com>
2015-10-09 16:26:39 +0200

    add current_user_dossier function at UserController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,9 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+
+  def current_user_dossier dossier_id=nil
+    dossier_id ||= params[:dossier_id]
+
+    current_user.dossiers.find(dossier_id)
+  end
 end
