02453e4cb1bc8a0f999dab15f15f28460469dc3a
Xavier J <xavijulien@gmail.com>
2015-11-26 16:39:37 +0100

    Pagination per page procedures for admin change 2 => 12

--- app/controllers/admin/procedures_controller.rb
+++ app/controllers/admin/procedures_controller.rb
@@ -2,7 +2,7 @@ class Admin::ProceduresController < AdminController
 
   def index
     @procedures = current_administrateur.procedures
-    @procedures = @procedures.paginate(:page => params[:page], :per_page => 2)
+    @procedures = @procedures.paginate(:page => params[:page], :per_page => 12)
   end
 
   def show
