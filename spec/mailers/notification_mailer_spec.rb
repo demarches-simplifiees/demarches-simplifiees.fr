372011e4d65cf050b7d1b24dcf3d0380c05f975c
Xavier J <xavijulien@gmail.com>
2015-12-15 11:41:50 +0100

    NotificationMailer : dossier_validated

--- spec/mailers/notification_mailer_spec.rb
+++ spec/mailers/notification_mailer_spec.rb
@@ -11,4 +11,15 @@ RSpec.describe NotificationMailer, type: :mailer do
     it { expect(subject.body).to include("Pour le consulter, merci de vous rendre sur #{users_dossiers_url(id: dossier.id)}") }
     it { expect(subject.subject).to eq("Nouveau commentaire pour votre dossier TPS N°#{dossier.id}") }
   end
+
+  describe ".dossier_validated" do
+    let(:user) { create(:user) }
+    let(:dossier) { create(:dossier, :with_procedure, user: user) }
+
+    subject(:subject) { described_class.dossier_validated(dossier) }
+
+    it { expect(subject.body).to match("Votre dossier N°#{dossier.id} a été validé par votre gestionnaire.") }
+    it { expect(subject.body).to include("Afin de finaliser son dépot, merci de vous rendre sur #{users_dossiers_url(id: dossier.id)}") }
+    it { expect(subject.subject).to eq("Votre dossier TPS N°#{dossier.id} a été validé") }
+  end
 end
