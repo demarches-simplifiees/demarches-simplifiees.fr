a738128f4acc18fd2edef479ddf870d406d936a0
Xavier J <xavijulien@gmail.com>
2016-01-19 17:40:45 +0100

    Suppression de FCEntreprise

--- config/routes.rb
+++ config/routes.rb
@@ -16,9 +16,6 @@ Rails.application.routes.draw do
   root 'root#index'
 
   namespace :france_connect do
-    # get 'entreprise' => 'entreprise#login'
-    # get 'entreprise/callback' => 'entreprise#callback'
-
     get 'particulier' => 'particulier#login'
     get 'particulier/callback' => 'particulier#callback'
 
