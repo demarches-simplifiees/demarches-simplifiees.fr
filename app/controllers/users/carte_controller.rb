008ca19befbc7998f379a2b8835ed922c6f093b1
Xavier J <xavijulien@gmail.com>
2016-01-26 16:47:13 +0100

    un-used files deleted

--- app/controllers/users/carte_controller.rb
+++ app/controllers/users/carte_controller.rb
@@ -1,5 +1,4 @@
 class Users::CarteController < UsersController
-  include DossierConcern
 
   before_action only: [:show] do
     authorized_routes? self.class
--- spec/controllers/dossiers_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe DossiersController, type: :controller do
-
-end
