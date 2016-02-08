2b2a4c3f0a2ac7681a7009a3935c24017b725753
Xavier J <xavijulien@gmail.com>
2016-02-08 19:07:33 +0100

    New landing page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,3 +27,12 @@
 //= require smart_listing
 //= require turf
 //= require franceconnect
+
+$(document).ready(function() {
+    $('.js-scrollTo').on('click', function() { // Au clic sur un élément
+        var page = $(this).attr('cible'); // Page cible
+        var speed = 600; // Durée de l'animation (en ms)
+        $('html, body').animate( { scrollTop: $(page).offset().top-100 }, speed ); // Go
+        return false;
+    });
+});
