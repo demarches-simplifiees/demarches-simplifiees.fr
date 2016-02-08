bd63116d5a83f12341b7b44ccf8d81d602b0796e
Xavier J <xavijulien@gmail.com>
2016-02-08 19:52:39 +0100

    Fix test : desactivate swagger

--- app/controllers/api/v1/procedures_controller.rb
+++ app/controllers/api/v1/procedures_controller.rb
@@ -1,16 +1,4 @@
 class API::V1::ProceduresController < APIController
-
-  swagger_controller :procedures, "Procédure API"
-
-  swagger_api :show do
-    summary "Récupérer les informations d'une procédure"
-    param :path, :id, :integer, :required
-    param :path, :token, :string, :required
-    response :ok, "Success", :Procedure
-    response :unauthorized
-    response :not_found
-  end
-
   def show
     @procedure = current_administrateur.procedures.find(params[:id]).decorate
 
