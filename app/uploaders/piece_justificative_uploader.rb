04c5432be15f2ba17f895ca6a6303b346b1745be
Xavier J <xavijulien@gmail.com>
2016-03-14 10:22:27 +0100

    Add libreoffice document on uploader

--- app/uploaders/piece_justificative_uploader.rb
+++ app/uploaders/piece_justificative_uploader.rb
@@ -39,7 +39,7 @@ class PieceJustificativeUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
-    %w(pdf xlsx xls doc docx)
+    %w(pdf doc docxxls xlsx ppt pptx odt ods odp)
   end
 
   # Override the filename of the uploaded files:
