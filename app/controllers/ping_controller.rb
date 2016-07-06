f176a730a5ec62b562f8732250f45ec31bf60022
Guillaume Lazzara <glazzara@octo.com>
2016-07-06 10:06:28 +0200

    Silent logs of ping rendering

--- app/controllers/ping_controller.rb
+++ app/controllers/ping_controller.rb
@@ -1,10 +1,12 @@
 class PingController < ApplicationController
 
   def index
-    if (ActiveRecord::Base.connected?)
-      render nothing: true, status: 200, content_type: "application/json"
-    else
-      render nothing: true, status: 500, content_type: "application/json"
+    Rails.logger.silence do
+      if (ActiveRecord::Base.connected?)
+        render nothing: true, status: 200, content_type: "application/json"
+      else
+        render nothing: true, status: 500, content_type: "application/json"
+      end
     end
   end
 
