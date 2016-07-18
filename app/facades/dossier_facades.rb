9221a0bb26d059759ac8afe902c87e2f2b3303e4
Xavier J <xjulien@octo.com>
2016-07-18 18:48:59 +0200

    Add tab to group all commentaires files in one (for gestionnaire)

--- app/facades/dossier_facades.rb
+++ app/facades/dossier_facades.rb
@@ -45,4 +45,8 @@ class DossierFacades
   def invites
     @dossier.invites
   end
+
+  def commentaires_files
+    PieceJustificative.where(dossier_id: @dossier.id, type_de_piece_justificative_id: nil)
+  end
 end
