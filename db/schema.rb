bce1b96fee17bf883063170f1d332b7bee9fba9a
Xavier J <xjulien@octo.com>
2016-08-02 14:49:41 +0200

    Build default pref list dossier for new gestionnaire

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160722135927) do
+ActiveRecord::Schema.define(version: 20160802113112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
