c4420cd6bc6f372bd5c22e40e96f82e97f74235c
Xavier J <xjulien@octo.com>
2016-08-22 12:37:24 +0200

    Fix seeds superadmin password changement

--- db/seeds/super_admin.rb
+++ db/seeds/super_admin.rb
@@ -1,1 +1,7 @@
-Administration.create(email: SUPERADMIN.email, password: SUPERADMIN.password)
+begin
+  Administration.create!(email: SUPERADMIN.email, password: SUPERADMIN.password)
+rescue ActiveRecord::RecordInvalid
+  admin = Administration.find_by_email(SUPERADMIN.email)
+  admin.password = SUPERADMIN.password
+  admin.save
+end
