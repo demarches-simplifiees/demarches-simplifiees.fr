3ebdab23a9cb320f15f32a13592ed3190570af22
Xavier J <xjulien@octo.com>
2016-08-24 15:32:41 +0200

    Enable french local on rich textarea

--- app/assets/javascripts/bootstrap_wysihtml5.js
+++ app/assets/javascripts/bootstrap_wysihtml5.js
@@ -3,6 +3,6 @@ $(document).on('page:load', wysihtml5_active);
 
 function wysihtml5_active (){
     $('.wysihtml5').each(function(i, elem) {
-        $(elem).wysihtml5({ toolbar:{ "fa": true, "link": false } });
+        $(elem).wysihtml5({ toolbar:{ "fa": true, "link": false, "color": true }, "locale": "fr-FR" });
     });
 }
