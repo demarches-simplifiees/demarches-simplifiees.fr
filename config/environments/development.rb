a48299353e76730e97f789614db52aacd4048d21
Xavier J <xjulien@octo.com>
2016-09-01 11:00:06 +0200

    Fix html interpretation in ReceivedMail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,10 +37,10 @@ Rails.application.configure do
   config.assets.raise_runtime_errors = true
 
   config.action_mailer.delivery_method = :test
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 end
