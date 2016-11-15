0fc81eb0da2aa3a65fe1a7512b58efefc9f88e2e
Xavier J <xjulien@octo.com>
2016-11-15 04:26:25 +0100

    Fix DEPRECATION WARNING for spec/controllers/ban/*.rb

--- spec/controllers/ban/search_controller_spec.rb
+++ spec/controllers/ban/search_controller_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper'
 
 describe Ban::SearchController, type: :controller do
   describe '#GET' do
-
     let (:request) { '' }
 
     before do
       stub_request(:get, "http://api-adresse.data.gouv.fr/search?limit=5&q=").
           to_return(:status => 200, :body => 'Missing query', :headers => {})
-      get :get, request: request
+
+      get :get, params: {request: request}
     end
 
     it { expect(response.status).to eq 200 }
