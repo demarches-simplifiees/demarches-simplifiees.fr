893bfaef4093331c3173f84b048007b56d495b82
Xavier J <xjulien@octo.com>
2016-11-17 10:50:28 +0100

    Fix JS

--- app/assets/javascripts/dossier_commentaires_modal.js
+++ app/assets/javascripts/dossier_commentaires_modal.js
@@ -1,13 +1,16 @@
-$(document).ready(function () {
-  var modal = $("#modalCommentairesDossierParChamp");
-  var body = modal.find(".modal-body");
-  var originalBody = body.html();
+$(document).on('page:load', init_modal_commentaire);
+$(document).ready(init_modal_commentaire);
 
-  modal.on("show.bs.modal", function (e) {
-    body.load(e.relatedTarget.getAttribute("data-href"));
-  });
+function init_modal_commentaire() {
+    var modal = $("#modalCommentairesDossierParChamp");
+    var body = modal.find(".modal-body");
+    var originalBody = body.html();
 
-  $("#modalCommentairesDossierParChamp").on("hidden.bs.modal", function (e) {
-    body.html(originalBody);
-  });
-});
+    modal.on("show.bs.modal", function (e) {
+        body.load(e.relatedTarget.getAttribute("data-href"));
+    });
+
+    $("#modalCommentairesDossierParChamp").on("hidden.bs.modal", function (e) {
+        body.html(originalBody);
+    });
+}
