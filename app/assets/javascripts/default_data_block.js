3772fee85fe0b37671f9e959019f3dcfb9531ae3
JC <blondel.jc@gmail.com>
2016-11-29 14:46:01 +0100

    Dossier content displayed by default on dossiers#show

--- app/assets/javascripts/default_data_block.js
+++ app/assets/javascripts/default_data_block.js
@@ -2,12 +2,14 @@ $(document).on('page:load', init_default_data_block);
 $(document).ready(init_default_data_block);
 
 function init_default_data_block() {
+    $('.default_data_block #dossier .body').toggle();
+
     $('.default_data_block .title').click(function () {
         toggle_default_data_bloc(this, 400);
     });
 
     $('.new-action').click(function () {
-        var messages_block = $(this).parents().closest(".default_data_block").find(".title")
+        var messages_block = $(this).parents().closest('.default_data_block').find('.title')
         toggle_default_data_bloc(messages_block, 400);
     });
 
@@ -17,13 +19,13 @@ function init_default_data_block() {
 
     function toggle_default_data_bloc(element, duration){
         var block = $(element).parents('.show-block');
-        if (block.attr("id") == "messages") {
-          block.children(".last-commentaire").toggle();
+        if (block.attr('id') == 'messages') {
+          block.children('.last-commentaire').toggle();
         }
 
-        block.children(".body").slideToggle(duration);
+        block.children('.body').slideToggle(duration);
 
-        block.find(".carret-right").toggle();
-        block.find(".carret-down").toggle();
+        block.find('.carret-right').toggle();
+        block.find('.carret-down').toggle();
     }
 }
