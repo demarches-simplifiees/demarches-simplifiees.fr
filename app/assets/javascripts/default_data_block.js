b698046e1ee9e240ddc2ea18a86b470fc0a99871
JC <blondel.jc@gmail.com>
2016-12-05 14:06:38 +0100

    Adding layout for credentials, this dropdown is shown when you pass hover sign_out button. It display email informations about the current gestionnaire/user

--- app/assets/javascripts/default_data_block.js
+++ app/assets/javascripts/default_data_block.js
@@ -7,28 +7,36 @@ function init_default_data_block() {
     $('.default_data_block #dossier .carret-down').toggle();
 
     $('.default_data_block .title').click(function () {
-        toggle_default_data_bloc(this, 400);
+      toggle_default_data_bloc(this, 400);
+    });
+
+    $('#sign_out').hover(function () {
+      $(this).find('#credentials').toggle();
+    });
+
+    $('#sign_out').off(function () {
+      $(this).find('credentials').toggle();
     });
 
     $('.new-action').click(function () {
-        var messages_block = $(this).parents().closest('.default_data_block').find('.title')
-        toggle_default_data_bloc(messages_block, 400);
+      var messages_block = $(this).parents().closest('.default_data_block').find('.title')
+      toggle_default_data_bloc(messages_block, 400);
     });
 
     $('.default_data_block.default_visible').each(function() {
-        toggle_default_data_bloc($(this).find('.title'), 0);
+      toggle_default_data_bloc($(this).find('.title'), 0);
     });
 
-    function toggle_default_data_bloc(element, duration){
-        var block = $(element).parents('.show-block');
-        if (block.attr('id') == 'messages') {
-          block.children('.last-commentaire').toggle();
-          $(".commentaires").animate({ scrollTop: $(this).height() }, "slow");
-        }
+    function toggle_default_data_bloc(element, duration) {
+      var block = $(element).parents('.show-block');
+      if (block.attr('id') == 'messages') {
+        block.children('.last-commentaire').toggle();
+        $(".commentaires").animate({ scrollTop: $(this).height() }, "slow");
+      }
 
-        block.children('.body').slideToggle(duration);
+      block.children('.body').slideToggle(duration);
 
-        block.find('.carret-right').toggle();
-        block.find('.carret-down').toggle();
+      block.find('.carret-right').toggle();
+      block.find('.carret-down').toggle();
     }
 }
