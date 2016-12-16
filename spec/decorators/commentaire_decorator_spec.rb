02ec9017775459e2c152f25ffcd4159ebef5881a
Xavier J <xjulien@octo.com>
2016-12-16 09:38:43 +0100

    Add CommentaireDecorator test

--- spec/decorators/commentaire_decorator_spec.rb
+++ spec/decorators/commentaire_decorator_spec.rb
@@ -1,4 +1,14 @@
 require 'spec_helper'
 
 describe CommentaireDecorator do
+  let(:commentaire) { Timecop.freeze(Time.local(2008, 9, 1, 10, 5, 0)) {create :commentaire} }
+  let(:decorator) { commentaire.decorate }
+
+  describe 'created_at_fr' do
+    subject { decorator.created_at_fr }
+
+    context 'when created_at have a value' do
+      it { is_expected.to eq '01/09/2008 - 10:05' }
+    end
+  end
 end
