7c20d1e9a0008e1bbc092d38f53844a3636ea60a
Xavier J <xjulien@octo.com>
2016-12-16 15:18:34 +0100

    Add tests for SearchController

--- spec/controllers/ban/search_controller_spec.rb
+++ spec/controllers/ban/search_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Ban::SearchController, type: :controller do
-  describe '#GET' do
+  describe '#GET get' do
     let (:request) { '' }
 
     before do
@@ -12,5 +12,26 @@ describe Ban::SearchController, type: :controller do
     end
 
     it { expect(response.status).to eq 200 }
+  end
+
+  describe '#GET get_address_point' do
+    let(:dossier_id) { "1" }
+    subject { get :get_address_point, params: {request: request, dossier_id: dossier_id} }
+
+    before do
+      subject
+    end
+
+    context 'when request return result', vcr: {cassette_name: 'ban_search_paris'} do
+      let(:request) { 'Paris' }
+
+      it { expect(response.body).to eq ({lon: '2.3469', lat: '48.8589', zoom: '14', dossier_id: dossier_id}).to_json }
+    end
+
+    context 'when request return nothing', vcr: {cassette_name: 'ban_search_nothing'} do
+      let(:request) { 'je recherche pas grand chose' }
+
+      it { expect(response.body).to eq ({lon: nil, lat: nil, zoom: '14', dossier_id: dossier_id}).to_json }
+    end
   end
 end
