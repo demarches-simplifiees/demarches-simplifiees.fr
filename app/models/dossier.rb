80c9165f08b06a9e451f62c5f2e1deedba741eae
Xavier J <xjulien@octo.com>
2016-12-19 16:02:35 +0100

    Delete death code Model/Dossier

--- app/models/dossier.rb
+++ app/models/dossier.rb
@@ -95,14 +95,6 @@ class Dossier < ActiveRecord::Base
     commentaires.order(created_at: :desc)
   end
 
-  def sous_domaine
-    if Rails.env.production?
-      'tps'
-    else
-      'tps-dev'
-    end
-  end
-
   def next_step! role, action
     unless %w(initiate follow update comment valid submit receive refuse without_continuation close).include?(action)
       fail 'action is not valid'
@@ -314,10 +306,6 @@ class Dossier < ActiveRecord::Base
     follows.size
   end
 
-  def total_commentaire
-    self.commentaires.size
-  end
-
   def submit!
     self.deposit_datetime= DateTime.now
 
@@ -335,13 +323,5 @@ class Dossier < ActiveRecord::Base
 
   def invite_by_user? email
     (invites_user.pluck :email).include? email
-  end
-
-  def self.word_is_an_integer word
-    return 0 if Float(word) > 2147483647
-
-    Float(word)
-  rescue ArgumentError
-    0
   end
 end
