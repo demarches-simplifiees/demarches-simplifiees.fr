e258118825aa9e7e104f3efc85295a3fdf7b9b1d
Xavier J <xjulien@octo.com>
2016-12-20 14:48:09 +0100

    Reactive cartographie

--- spec/features/admin/procedure_locked_spec.rb
+++ spec/features/admin/procedure_locked_spec.rb
@@ -16,45 +16,4 @@ feature 'procedure locked' do
       expect(page).not_to have_content('La procédure ne peut plus être modifiée car elle a été publiée')
     end
   end
-  context 'when procedure is published' do
-    pending 'à voir si on enlève complètement les référence dans le left panel ou pas'
-    # let(:published) { true }
-    # before do
-    #   visit admin_procedure_path(procedure)
-    # end
-    #
-    # scenario 'info label is present' do
-    #   expect(page).to have_content('La procédure ne peut plus être modifiée car elle a été publiée')
-    # end
-    #
-    # context 'when user click on Description tab' do
-    #   before do
-    #     page.click_on 'Description'
-    #   end
-    #
-    #   scenario 'page doest not change' do
-    #     expect(page).to have_css('#procedure_show')
-    #   end
-    # end
-    #
-    # context 'when user click on Champs tab' do
-    #   before do
-    #     page.click_on 'Champs'
-    #   end
-    #
-    #   scenario 'page doest not change' do
-    #     expect(page).to have_css('#procedure_show')
-    #   end
-    # end
-    #
-    # context 'when user click on Pieces Justificatives tab' do
-    #   before do
-    #     page.click_on 'Pièces justificatives'
-    #   end
-    #
-    #   scenario 'page doest not change' do
-    #     expect(page).to have_css('#procedure_show')
-    #   end
-    # end
-  end
 end
