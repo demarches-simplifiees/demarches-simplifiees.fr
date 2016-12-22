c533bb0a88fbbf3ac456476ae23a7b6e4f2958f7
Xavier J <xjulien@octo.com>
2016-12-22 22:27:02 +0100

    TEST : First implementation of notification with Action Cable : alert message

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,8 +1,14 @@
 class Notification < ActiveRecord::Base
   belongs_to :dossier
 
+  after_save :broadcast_notification
+
   enum type_notif: {
            commentaire: 'commentaire'
        }
 
+  def broadcast_notification
+    ActionCable.server.broadcast 'notifications',
+                                 message: "Nouveau commentaire posté sur le dossier #{self.dossier.id}"
+  end
 end
