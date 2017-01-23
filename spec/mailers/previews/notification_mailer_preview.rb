7e24adbca06e487c95a11a38123aa60fc35e6029
Mathieu Magnin <mathieu.magnin@gmail.com>
2017-01-23 15:49:19 +0100

    Add mail preview and small fix

--- spec/mailers/previews/notification_mailer_preview.rb
+++ spec/mailers/previews/notification_mailer_preview.rb
@@ -1,5 +1,11 @@
 class NotificationMailerPreview < ActionMailer::Preview
+
   def dossier_received
     NotificationMailer.dossier_received(Dossier.last)
   end
+
+  def dossier_validated
+    NotificationMailer.dossier_validated(Dossier.last)
+  end
+
 end
