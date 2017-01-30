83945b3366ddef5e0a4879c6a2f5d1f60c2a9c69
Mathieu Magnin <mathieu.magnin@gmail.com>
2017-01-30 15:21:47 +0100

    add mailcatcher comment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,6 +42,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  # mailcatcher settings https://mailcatcher.me/
   config.action_mailer.smtp_settings = {
     :address => "localhost",
     :port => 1025,
