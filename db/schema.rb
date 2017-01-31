8ba8135e6b4d33f41476782816aa9cf10d8b4987
Mathieu Magnin <mathieu.magnin@gmail.com>
2017-01-31 15:13:06 +0100

    Allow blank for lien_demarche

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170118144306) do
+ActiveRecord::Schema.define(version: 20170125152856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
