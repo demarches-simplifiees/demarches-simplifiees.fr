657ce53a604da4e0304ac6771d09a837f4d6eb6a
Simon Lehericey <simon.lehericey@gmail.com>
2017-03-15 17:47:26 +0100

    select_multiple: add multiple_drop_down_list type

--- app/models/drop_down_list.rb
+++ app/models/drop_down_list.rb
@@ -2,6 +2,19 @@ class DropDownList < ActiveRecord::Base
   belongs_to :type_de_champ
 
   def options
-    value.split(/[\r\n]|[\r]|[\n]|[\n\r]/).reject(&:empty?)
+    result = value.split(/[\r\n]|[\r]|[\n]|[\n\r]/).reject(&:empty?)
+    result.blank? ? [] : [''] + result
+  end
+
+  def disabled_options
+    options.select{ |v| !(v =~ /^--.*--$/).nil? }
+  end
+
+  def selected_options(champ)
+    champ.value.blank? ? [] : multiple ? JSON.parse(champ.value) : [champ.value]
+  end
+
+  def multiple
+    type_de_champ.type_champ == 'multiple_drop_down_list'
   end
 end
