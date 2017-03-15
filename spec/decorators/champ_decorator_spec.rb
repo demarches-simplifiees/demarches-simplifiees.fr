b3624256cd10449e9aff571e74073ff6bf6f955b
Simon Lehericey <simon.lehericey@gmail.com>
2017-03-15 17:51:12 +0100

    select_multiple: add decoration for view of multiple_select

--- spec/decorators/champ_decorator_spec.rb
+++ spec/decorators/champ_decorator_spec.rb
@@ -1,24 +1,37 @@
 require 'spec_helper'
 
 describe ChampDecorator do
-  let(:champ) {create :champ, type_de_champ: (create :type_de_champ_public, type_champ: :checkbox)}
+  let(:champ) {create :champ, type_de_champ: (create :type_de_champ_public, type_champ: type_champ)}
   let(:decorator) { champ.decorate }
+
 
   describe 'value' do
     subject { decorator.value }
 
-    context 'when type_champ is checkbox' do
+    describe 'for a checkbox' do
+      let(:type_champ) { :checkbox }
 
       context 'when value is on' do
-        before do
-          champ.update value: 'on'
-        end
-
+        before { champ.update value: 'on' }
         it { is_expected.to eq 'Oui' }
       end
 
       context 'when value is other' do
         it { is_expected.to eq 'Non' }
+      end
+    end
+
+    describe 'for a multiple_drop_down_list' do
+      let(:type_champ) { :multiple_drop_down_list }
+
+      context 'when value is an array' do
+        before { champ.update value: '["1", "2"]' }
+        it { is_expected.to eq '1, 2' }
+      end
+
+      context 'when value is empty' do
+        before { champ.update value: '' }
+        it { is_expected.to eq '' }
       end
     end
   end
