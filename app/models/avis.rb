b9fe0ac43f92026f1179c65faa2ac1d3ffc84954
Mathieu Magnin <mathieu.magnin@gmail.com>
2017-04-27 12:17:50 +0200

    Gestionnaire can list dossiers where he is invited for avis

--- app/models/avis.rb
+++ app/models/avis.rb
@@ -2,6 +2,9 @@ class Avis < ApplicationRecord
   belongs_to :dossier
   belongs_to :gestionnaire
 
+  scope :with_answer, -> { where.not(answer: nil) }
+  scope :without_answer, -> { where(answer: nil) }
+
   def find_email
     gestionnaire.try(:email) || email
   end
