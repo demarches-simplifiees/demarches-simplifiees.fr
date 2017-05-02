988947696e3ef24ca0dd653e37ae742f1775399e
gregoirenovel <log@kinko.fr>
2017-05-02 09:48:25 +0200

    [Fix #161] Remove incorrect commas from Dossier#text_summary

--- spec/models/dossier_spec.rb
+++ spec/models/dossier_spec.rb
@@ -817,7 +817,7 @@ describe Dossier do
 
       subject { dossier.text_summary }
 
-      it { is_expected.to eq("Dossier déposé le 31/12/2010, sur la démarche Démarche, gérée par l'organisme Organisation") }
+      it { is_expected.to eq("Dossier déposé le 31/12/2010 sur la démarche Démarche gérée par l'organisme Organisation") }
     end
 
     context 'when the dossier has not been initiated' do
@@ -825,7 +825,7 @@ describe Dossier do
 
       subject { dossier.text_summary }
 
-      it { is_expected.to eq("Dossier en brouillon répondant à la démarche Démarche, gérée par l'organisme Organisation") }
+      it { is_expected.to eq("Dossier en brouillon répondant à la démarche Démarche gérée par l'organisme Organisation") }
     end
   end
 
