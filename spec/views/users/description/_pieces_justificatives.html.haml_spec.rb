1a5f9fd06778a3f91b0fa755c0f36bf0c421abd7
gregoirenovel <log@kinko.fr>
2017-05-05 12:37:11 +0200

    [Fix #105] Show the pieces justificative’ descriptions

--- spec/views/users/description/_pieces_justificatives.html.haml_spec.rb
+++ spec/views/users/description/_pieces_justificatives.html.haml_spec.rb
@@ -5,12 +5,14 @@ describe 'users/description/_pieces_justificatives.html.haml', type: :view do
   let!(:tpj1) { create(:type_de_piece_justificative,
     procedure: procedure,
     libelle: "Première pièce jointe",
+    description: "Première description",
     order_place: 1,
     mandatory: true
   )}
   let!(:tpj2) { create(:type_de_piece_justificative,
     procedure: procedure,
     libelle: "Seconde pièce jointe",
+    description: "Seconde description",
     order_place: 2,
     lien_demarche: "https://www.google.fr"
   )}
@@ -24,6 +26,9 @@ describe 'users/description/_pieces_justificatives.html.haml', type: :view do
     expect(rendered).to include("Première pièce jointe *")
     expect(rendered).to include("Seconde pièce jointe")
     expect(rendered.index("Première pièce jointe")).to be < rendered.index("Seconde pièce jointe")
+
+    expect(rendered).to include("Première description")
+    expect(rendered).to include("Seconde description")
 
     expect(rendered).to have_selector("input[type=file]", count: 2)
   end
