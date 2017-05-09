054afb40a49b031ae315044a24c8afa584b5e5ab
Mathieu Magnin <mathieu.magnin@gmail.com>
2017-05-09 23:05:45 +0200

    [Fix #50, fix #239] Install chartkick the right way

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require highcharts
+//= require chartkick
 //= require_tree .
 //= require bootstrap-sprockets
 //= require bootstrap-datepicker/core
