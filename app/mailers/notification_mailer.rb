70f637f088806e4879b96aaf8a5ab60f40e8c78b
gregoirenovel <log@kinko.fr>
2017-05-11 18:07:37 +0200

    [Fix #198] Add a Commentaire when we send a Notification email to a user

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,13 +1,15 @@
 class NotificationMailer < ApplicationMailer
   default to: Proc.new { @user.email }
 
+  after_action :create_commentaire_for_notification, only: :send_notification
+
   def send_notification dossier, mail_template
     vars_mailer(dossier)
 
-    obj  = mail_template.object_for_dossier dossier
-    body = mail_template.body_for_dossier dossier
+    @obj  = mail_template.object_for_dossier dossier
+    @body = mail_template.body_for_dossier dossier
 
-    mail(subject: obj) { |format| format.html { body } }
+    mail(subject: @obj) { |format| format.html { @body } }
   end
 
   def new_answer dossier
@@ -15,6 +17,14 @@ class NotificationMailer < ApplicationMailer
   end
 
   private
+
+  def create_commentaire_for_notification
+    Commentaire.create(
+      dossier: @dossier,
+      email: "contact@tps.apientreprise.fr",
+      body: ["[#{@obj}]", @body].join("<br><br>")
+    )
+  end
 
   def vars_mailer dossier
     @dossier = dossier
