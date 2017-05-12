9c3cde2b2eaf6f7c64ebc43e20d62415afa7787d
gregoirenovel <log@kinko.fr>
2017-05-12 13:40:32 +0200

    Move CommentairesController#notify_user_user_with_mail to Commentaire

--- app/models/commentaire.rb
+++ app/models/commentaire.rb
@@ -5,6 +5,7 @@ class Commentaire < ActiveRecord::Base
   belongs_to :piece_justificative
 
   after_save :notify_gestionnaires
+  after_save :notify_user_with_mail
 
   def header
     "#{email}, " + I18n.l(created_at.localtime, format: '%d %b %Y %H:%M')
@@ -16,5 +17,9 @@ class Commentaire < ActiveRecord::Base
     if email == dossier.user.email || dossier.invites_user.pluck(:email).to_a.include?(email)
       NotificationService.new('commentaire', self.dossier.id).notify
     end
+  end
+
+  def notify_user_with_mail
+    NotificationMailer.new_answer(dossier).deliver_now! unless (current_user.try(:email) == dossier.user.email || email == 'contact@tps.apientreprise.fr')
   end
 end
