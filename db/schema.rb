e83782241879303e607a6104a87e3640ee066200
Simon Lehericey <simon.lehericey@gmail.com>
2017-05-23 10:35:48 +0200

    Schema: Fix mail object type

--- db/schema.rb
+++ db/schema.rb
@@ -106,7 +106,7 @@ ActiveRecord::Schema.define(version: 20170414095411) do
 
   create_table "closed_mails", force: :cascade do |t|
     t.text     "body"
-    t.string   "object"
+    t.text     "object"
     t.integer  "procedure_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
@@ -237,7 +237,7 @@ ActiveRecord::Schema.define(version: 20170414095411) do
   end
 
   create_table "initiated_mails", force: :cascade do |t|
-    t.string   "object"
+    t.text     "object"
     t.text     "body"
     t.integer  "procedure_id"
     t.datetime "created_at",   null: false
@@ -357,7 +357,7 @@ ActiveRecord::Schema.define(version: 20170414095411) do
 
   create_table "received_mails", force: :cascade do |t|
     t.text     "body"
-    t.string   "object"
+    t.text     "object"
     t.integer  "procedure_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
@@ -366,7 +366,7 @@ ActiveRecord::Schema.define(version: 20170414095411) do
 
   create_table "refused_mails", force: :cascade do |t|
     t.text     "body"
-    t.string   "object"
+    t.text     "object"
     t.integer  "procedure_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
@@ -427,7 +427,7 @@ ActiveRecord::Schema.define(version: 20170414095411) do
 
   create_table "without_continuation_mails", force: :cascade do |t|
     t.text     "body"
-    t.string   "object"
+    t.text     "object"
     t.integer  "procedure_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
