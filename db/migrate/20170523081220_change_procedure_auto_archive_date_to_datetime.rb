e0c253eee1501191033323fef8c9e2de0cce2d49
Simon Lehericey <simon.lehericey@gmail.com>
2017-05-24 11:56:31 +0200

    Revert "Procedure: set archive_date to datetime to allow localtime"
    This reverts commit 14dd75d0d2131d18579f5668b1b26bf1dc91802c.

--- db/migrate/20170523081220_change_procedure_auto_archive_date_to_datetime.rb
+++
@@ -1,9 +0,0 @@
-class ChangeProcedureAutoArchiveDateToDatetime < ActiveRecord::Migration[5.0]
-  def up
-    change_column :procedures, :auto_archive_on, :datetime
-  end
-
-  def down
-    change_column :procedures, :auto_archive_on, :date
-  end
-end
