67d0fb5ddc069668887ff5a2fa2d8e844a1a58d2
gregoirenovel <log@kinko.fr>
2017-05-26 19:42:05 +0200

    Further refactor of AutoArchiveProcedureWorker
    Call received! so that validation and save
    callbacks are called.
    
    Previously, we wouldn’t have update the
    received_at date

--- app/workers/auto_archive_procedure_worker.rb
+++ app/workers/auto_archive_procedure_worker.rb
@@ -3,7 +3,9 @@ class AutoArchiveProcedureWorker
 
   def perform(*args)
     Procedure.not_archived.where("auto_archive_on <= ?", Date.today).each do |procedure|
-      procedure.dossiers.state_en_construction.update_all(state: :received)
+      procedure.dossiers.state_en_construction.each do |dossier|
+        dossier.received!
+      end
 
       procedure.update_attributes!(archived: true)
     end
