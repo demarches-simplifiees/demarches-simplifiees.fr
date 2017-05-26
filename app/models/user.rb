843bb9fd2cc6c655d2e8f7fcdac998a3b8119fbe
gregoirenovel <log@kinko.fr>
2017-05-26 21:31:51 +0200

    Format the User loged_in_with_france_connect enum correctly

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
-  enum loged_in_with_france_connect: {particulier: 'particulier',
-                                      entreprise: 'entreprise'}
+  enum loged_in_with_france_connect: {
+    particulier: 'particulier',
+    entreprise: 'entreprise'
+  }
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
