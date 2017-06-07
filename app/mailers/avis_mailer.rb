2a74207d15f5622a51e8004ccb1eab9bfcee794e
gregoirenovel <log@kinko.fr>
2017-06-07 18:18:24 +0200

    you_are_invited_on_dossier -> avis_invitation
    - shorter
    - clearer

--- app/mailers/avis_mailer.rb
+++ app/mailers/avis_mailer.rb
@@ -1,6 +1,6 @@
 class AvisMailer < ApplicationMailer
 
-  def you_are_invited_on_dossier(avis)
+  def avis_invitation(avis)
     @avis = avis
     email = @avis.gestionnaire.try(:email) || @avis.email
     mail(to: email, subject: "Donnez votre avis sur le dossier nº #{@avis.dossier.id} (#{@avis.dossier.procedure.libelle})")
--- spec/mailers/previews/avis_mailer_preview.rb
+++ spec/mailers/previews/avis_mailer_preview.rb
@@ -1,8 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/avis_mailer
 class AvisMailerPreview < ActionMailer::Preview
 
-  def you_are_invited_on_dossier
-    AvisMailer.you_are_invited_on_dossier(Avis.last)
+  def avis_invitation
+    AvisMailer.avis_invitation(Avis.last)
   end
 
 end
