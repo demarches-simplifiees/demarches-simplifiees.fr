ba865215af53b8622f48e7ae122cf7f5097fbc09
gregoirenovel <log@kinko.fr>
2017-06-07 14:53:10 +0200

    Use the Chartkick colors option

--- config/initializers/chartkick.rb
+++ config/initializers/chartkick.rb
@@ -1,1 +1,4 @@
-Chartkick.options[:content_for] = :charts_js
+Chartkick.options = {
+  content_for: :charts_js,
+  colors: ["rgba(191, 220, 249, 1)", "rgba(113, 176, 239, 1)", "rgba(61, 149, 236, 1)"]
+}
