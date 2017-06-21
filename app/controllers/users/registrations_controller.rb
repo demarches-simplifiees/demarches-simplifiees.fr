ac5c00c26f0004eb2a26020df5225c1e0b6acd5a
Mathieu Magnin <mathieu.magnin@gmail.com>
2017-06-21 14:20:22 +0200

    [fix #452] redesign sign up form

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  layout "new_application"
+
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
