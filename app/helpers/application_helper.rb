d48501e3bce0c7c12ea277e8e04ba9da5e3ac98d
Mathieu Magnin <mathieu.magnin@gmail.com>
2017-06-22 16:45:57 +0200

    Account menu on header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,10 @@ module ApplicationHelper
     when "alert" then "alert-danger"
     end
   end
+
+  def current_email
+    current_user.try(:email) ||
+      current_gestionnaire.try(:email) ||
+      current_administrateur.try(:email)
+  end
 end
