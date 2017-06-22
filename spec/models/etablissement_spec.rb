d69d0f8a78aaaa92d72c38c765384ae3f8418969
Simon Lehericey <simon.lehericey@gmail.com>
2017-06-22 14:58:29 +0200

    Etablissement: add inline_adresse

--- spec/models/etablissement_spec.rb
+++ spec/models/etablissement_spec.rb
@@ -29,4 +29,10 @@ describe Etablissement do
 
     it { is_expected.to eq '6 RUE RAOUL NORDLING IMMEUBLE BORA 92270 BOIS COLOMBES' }
   end
+
+  describe '#inline_adresse' do
+    let(:etablissement) { create(:etablissement, nom_voie: 'green    moon') }
+
+    it { expect(etablissement.inline_adresse).to eq '6 RUE green moon, IMMEUBLE BORA, 92270 BOIS COLOMBES' }
+  end
 end
