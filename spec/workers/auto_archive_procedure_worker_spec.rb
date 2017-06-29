28f2ec1d860483b0231eaee0fcd00c50bb9a0d59
gregoirenovel <log@kinko.fr>
2017-06-29 12:20:27 +0200

    Reorder some tests

--- spec/workers/auto_archive_procedure_worker_spec.rb
+++ spec/workers/auto_archive_procedure_worker_spec.rb
@@ -18,43 +18,35 @@ RSpec.describe AutoArchiveProcedureWorker, type: :worker do
   end
 
   context "when procedures have auto_archive_on set on yesterday or today" do
-    describe "titi" do
-      before do
-        subject
-        procedure_hier.reload
-        procedure_aujourdhui.reload
-      end
+    let!(:dossier1) { create(:dossier, procedure: procedure_hier, state: 'draft', archived: false)}
+    let!(:dossier2) { create(:dossier, procedure: procedure_hier, state: 'initiated', archived: false)}
+    let!(:dossier3) { create(:dossier, procedure: procedure_hier, state: 'replied', archived: false)}
+    let!(:dossier4) { create(:dossier, procedure: procedure_hier, state: 'updated', archived: false)}
+    let!(:dossier5) { create(:dossier, procedure: procedure_hier, state: 'received', archived: false)}
+    let!(:dossier6) { create(:dossier, procedure: procedure_hier, state: 'closed', archived: false)}
+    let!(:dossier7) { create(:dossier, procedure: procedure_hier, state: 'refused', archived: false)}
+    let!(:dossier8) { create(:dossier, procedure: procedure_hier, state: 'without_continuation', archived: false)}
 
-      it { expect(procedure_hier.archived).to eq true }
-      it { expect(procedure_aujourdhui.archived).to eq true }
+    before do
+      subject
+      (1..8).each do |i|
+        eval "dossier#{i}.reload"
+      end
+      procedure_hier.reload
+      procedure_aujourdhui.reload
     end
 
-    context "with dossiers" do
-      let!(:dossier1) { create(:dossier, procedure: procedure_hier, state: 'draft', archived: false)}
-      let!(:dossier2) { create(:dossier, procedure: procedure_hier, state: 'initiated', archived: false)}
-      let!(:dossier3) { create(:dossier, procedure: procedure_hier, state: 'replied', archived: false)}
-      let!(:dossier4) { create(:dossier, procedure: procedure_hier, state: 'updated', archived: false)}
-      let!(:dossier5) { create(:dossier, procedure: procedure_hier, state: 'received', archived: false)}
-      let!(:dossier6) { create(:dossier, procedure: procedure_hier, state: 'closed', archived: false)}
-      let!(:dossier7) { create(:dossier, procedure: procedure_hier, state: 'refused', archived: false)}
-      let!(:dossier8) { create(:dossier, procedure: procedure_hier, state: 'without_continuation', archived: false)}
+    it { expect(dossier1.state).to eq 'draft' }
+    it { expect(dossier2.state).to eq 'received' }
+    it { expect(dossier3.state).to eq 'received' }
+    it { expect(dossier4.state).to eq 'received' }
+    it { expect(dossier5.state).to eq 'received' }
+    it { expect(dossier6.state).to eq 'closed' }
+    it { expect(dossier7.state).to eq 'refused' }
+    it { expect(dossier8.state).to eq 'without_continuation' }
 
-      before do
-        subject
-        (1..8).each do |i|
-          eval "dossier#{i}.reload"
-        end
-      end
-
-      it { expect(dossier1.state).to eq 'draft' }
-      it { expect(dossier2.state).to eq 'received' }
-      it { expect(dossier3.state).to eq 'received' }
-      it { expect(dossier4.state).to eq 'received' }
-      it { expect(dossier5.state).to eq 'received' }
-      it { expect(dossier6.state).to eq 'closed' }
-      it { expect(dossier7.state).to eq 'refused' }
-      it { expect(dossier8.state).to eq 'without_continuation' }
-    end
+    it { expect(procedure_hier.archived).to eq true }
+    it { expect(procedure_aujourdhui.archived).to eq true }
   end
 
   context "when procedures have auto_archive_on set on future" do
