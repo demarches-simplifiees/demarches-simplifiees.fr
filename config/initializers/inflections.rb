11c9dd436b95e6ccbabf0cac9c1b0fdf53990b39
Mathieu Magnin <mathieu.magnin@gmail.com>
2017-07-04 17:14:59 +0200

    Add french inflections rules for nouveau=>nouveaux

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -18,12 +18,28 @@ ActiveSupport::Inflector.inflections(:en) do |inflect|
   inflect.irregular('avis', 'avis')
 end
 
-# These inflection rules are supported but not enabled by default:
-# ActiveSupport::Inflector.inflections(:en) do |inflect|
-#   inflect.acronym 'RESTful'
-# end
+# From https://github.com/davidcelis/inflections
 ActiveSupport::Inflector.inflections(:fr) do |inflect|
+  inflect.clear
+
   inflect.plural(/$/, 's')
-  inflect.plural(/(hib|ch|bij|caill|p|gen|jouj)ou$/i, '\1oux')
-  inflect.irregular('avis', 'avis')
+  inflect.singular(/s$/, '')
+
+  inflect.plural(/(bijou|caillou|chou|genou|hibou|joujou|pou|au|eu|eau)$/, '\1x')
+  inflect.singular(/(bijou|caillou|chou|genou|hibou|joujou|pou|au|eu|eau)x$/, '\1')
+
+  inflect.plural(/(bleu|émeu|landau|lieu|pneu|sarrau)$/, '\1s')
+  inflect.plural(/al$/, 'aux')
+  inflect.plural(/ail$/, 'ails')
+  inflect.singular(/(journ|chev)aux$/, '\1al')
+  inflect.singular(/ails$/, 'ail')
+
+  inflect.plural(/(b|cor|ém|gemm|soupir|trav|vant|vitr)ail$/, '\1aux')
+  inflect.singular(/(b|cor|ém|gemm|soupir|trav|vant|vitr)aux$/, '\1ail')
+
+  inflect.plural(/(s|x|z)$/, '\1')
+
+  inflect.irregular('monsieur', 'messieurs')
+  inflect.irregular('madame', 'mesdames')
+  inflect.irregular('mademoiselle', 'mesdemoiselles')
 end
