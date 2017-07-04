82561980aa72bed06889a1da7f8a6ae8299b9bbc
Mathieu Magnin <mathieu.magnin@gmail.com>
2017-07-04 10:26:50 +0200

    convert simple quote to double quote

--- spec/controllers/new_gestionnaire/procedures_controller_spec.rb
+++ spec/controllers/new_gestionnaire/procedures_controller_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 describe NewGestionnaire::ProceduresController, type: :controller do
-  describe 'before_action: ensure_ownership!' do
-    it 'is present' do
+  describe "before_action: ensure_ownership!" do
+    it "is present" do
       before_actions = NewGestionnaire::ProceduresController
         ._process_action_callbacks
         .find_all{|process_action_callbacks| process_action_callbacks.kind == :before}
@@ -12,7 +12,7 @@ describe NewGestionnaire::ProceduresController, type: :controller do
     end
   end
 
-  describe 'ensure_ownership!' do
+  describe "ensure_ownership!" do
     let(:gestionnaire) { create(:gestionnaire) }
 
     before do
@@ -23,19 +23,19 @@ describe NewGestionnaire::ProceduresController, type: :controller do
       @controller.send(:ensure_ownership!)
     end
 
-    context 'when a gestionnaire asks for its procedure' do
+    context "when a gestionnaire asks for its procedure" do
       let(:asked_procedure) { create(:procedure, gestionnaires: [gestionnaire]) }
 
-      it 'does not redirects nor flash' do
+      it "does not redirects nor flash" do
         expect(@controller).not_to have_received(:redirect_to)
         expect(flash.alert).to eq(nil)
       end
     end
 
-    context 'when a gestionnaire asks for another procedure' do
+    context "when a gestionnaire asks for another procedure" do
       let(:asked_procedure) { create(:procedure) }
 
-      it 'redirects and flash' do
+      it "redirects and flash" do
         expect(@controller).to have_received(:redirect_to).with(root_path)
         expect(flash.alert).to eq("Vous n'avez pas accès à cette procédure")
       end
