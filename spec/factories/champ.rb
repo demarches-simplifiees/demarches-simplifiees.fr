a8351de5b8fb534c430c3c045799dfaa7a77af29
Mathieu Magnin <mathieu.magnin@gmail.com>
2017-07-11 16:50:29 +0200

    Add header section in form table

--- spec/factories/champ.rb
+++ spec/factories/champ.rb
@@ -1,5 +1,13 @@
 FactoryGirl.define do
   factory :champ do
     type_de_champ { FactoryGirl.create(:type_de_champ_public) }
+
+    trait :checkbox do
+      type_de_champ { FactoryGirl.create(:type_de_champ_public, :checkbox) }
+    end
+
+    trait :header_section do
+      type_de_champ { FactoryGirl.create(:type_de_champ_public, :header_section) }
+    end
   end
 end
