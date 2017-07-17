033f08524f465b32f2c01566250689470211c5fd
Simon Lehericey <simon.lehericey@gmail.com>
2017-07-17 13:13:20 +0200

    Gestionnaire: refactor follow?

--- spec/models/gestionnaire_spec.rb
+++ spec/models/gestionnaire_spec.rb
@@ -76,7 +76,7 @@ describe Gestionnaire, type: :model do
   describe '#follow?' do
     let!(:dossier) { create :dossier, procedure: procedure }
 
-    subject { gestionnaire.follow? dossier.id }
+    subject { gestionnaire.follow?(dossier) }
 
     context 'when gestionnaire follow a dossier' do
       before do
