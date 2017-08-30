feda7213f99d14fc9fa14d2b1ac2597f0d0ca3f6
Mathieu Magnin <mathieu.magnin@gmail.com>
2017-08-30 17:28:58 +0200

    Action button for instruction state

---
+++ app/assets/javascripts/new_design/state_button.js
@@ -0,0 +1,9 @@
+TPS.acceptDossier = function () {
+  $(".motivation").show();
+  $(".dropdown-items").hide();
+}
+
+TPS.motivationCancel = function () {
+  $(".motivation").hide();
+  $(".dropdown-items").show();
+}
