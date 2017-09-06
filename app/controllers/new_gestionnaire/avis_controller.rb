25374f2a1b24c655b141965e0df67ffb4dce9f4d
Simon Lehericey <simon.lehericey@gmail.com>
2017-09-06 16:14:03 +0200

    Avis: instruction

--- app/controllers/new_gestionnaire/avis_controller.rb
+++ app/controllers/new_gestionnaire/avis_controller.rb
@@ -21,10 +21,25 @@ module NewGestionnaire
       @dossier = avis.dossier
     end
 
+    def instruction
+      @avis = avis
+      @dossier = avis.dossier
+    end
+
+    def update
+      avis.update_attributes(avis_params)
+      flash.notice = 'Votre réponse est enregistrée.'
+      redirect_to instruction_avis_path(avis)
+    end
+
     private
 
     def avis
       current_gestionnaire.avis.includes(dossier: [:avis]).find(params[:id])
+    end
+
+    def avis_params
+      params.require(:avis).permit(:answer)
     end
   end
 end
