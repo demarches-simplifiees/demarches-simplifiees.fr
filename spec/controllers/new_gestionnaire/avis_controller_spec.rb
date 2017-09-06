091844ef0f4a94ec8eeef74af22e38e62aeebed5
Simon Lehericey <simon.lehericey@gmail.com>
2017-09-06 11:21:29 +0200

    Avis: index

---
+++ spec/controllers/new_gestionnaire/avis_controller_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe NewGestionnaire::AvisController, type: :controller do
+  let(:claimant) { create(:gestionnaire) }
+  let(:gestionnaire) { create(:gestionnaire) }
+  let(:procedure) { create(:procedure, :published, gestionnaires: [gestionnaire]) }
+  let(:dossier) { create(:dossier, :replied, procedure: procedure) }
+  let!(:avis_without_answer) { Avis.create(dossier: dossier, claimant: claimant, gestionnaire: gestionnaire) }
+  let!(:avis_with_answer) { Avis.create(dossier: dossier, claimant: claimant, gestionnaire: gestionnaire, answer: 'yop') }
+
+  before { sign_in(gestionnaire) }
+
+  describe '#index' do
+    before { get :index }
+
+    it { expect(response).to have_http_status(:success) }
+    it { expect(assigns(:avis_a_donner)).to match([avis_without_answer]) }
+    it { expect(assigns(:avis_donnes)).to match([avis_with_answer]) }
+    it { expect(assigns(:statut)).to eq('a-donner') }
+
+    context 'with a statut equal to donnes' do
+      before { get :index, statut: 'donnes' }
+
+      it { expect(assigns(:statut)).to eq('donnes') }
+    end
+  end
+end
