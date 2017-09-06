6f689be3fce436390b3ab1bb1b2d5328ae5500e0
Simon Lehericey <simon.lehericey@gmail.com>
2017-09-06 11:21:29 +0200

    Avis: index

---
+++ app/controllers/new_gestionnaire/avis_controller.rb
@@ -0,0 +1,19 @@
+module NewGestionnaire
+  class AvisController < ApplicationController
+    layout 'new_application'
+
+    def index
+      gestionnaire_avis = current_gestionnaire.avis.includes(dossier: [:procedure, :user])
+      @avis_a_donner, @avis_donnes = gestionnaire_avis.partition { |avis| avis.answer.nil? }
+
+      @statut = params[:statut].present? ? params[:statut] : 'a-donner'
+
+      @avis = case @statut
+      when 'a-donner'
+        @avis_a_donner
+      when 'donnes'
+        @avis_donnes
+      end
+    end
+  end
+end
