fbad15bc84d0b49686802e9ca9f29523d9fc9cba
Mathieu Magnin <mathieu.magnin@gmail.com>
2017-09-26 14:12:55 +0200

    Change AutoArchiveProcedureWorker for delayed_job

--- app/workers/auto_archive_procedure_worker.rb
+++ app/workers/auto_archive_procedure_worker.rb
@@ -1,7 +1,6 @@
 class AutoArchiveProcedureWorker
-  include Sidekiq::Worker
-
   def perform(*args)
+    Rails.logger.info("AutoArchiveProcedureWorker started at #{Time.now}")
     Procedure.publiees.where("auto_archive_on <= ?", Date.today).each do |procedure|
       procedure.dossiers.state_en_construction.each do |dossier|
         dossier.received!
@@ -9,5 +8,12 @@ class AutoArchiveProcedureWorker
 
       procedure.archive
     end
+    Rails.logger.info("AutoArchiveProcedureWorker ended at #{Time.now}")
   end
+
+  def queue_name
+    "cron"
+  end
+
+  handle_asynchronously :perform
 end
