c415614128ce5eb8ec28644e10d320ae4ac8306d
Simon Lehericey <simon.lehericey@gmail.com>
2017-09-26 11:50:21 +0200

    [Fix #420] allow floating number in number field

--- spec/services/champs_service_spec.rb
+++ spec/services/champs_service_spec.rb
@@ -7,14 +7,21 @@ describe ChampsService do
     champ_datetime = TypeDeChamp.new(type_champ: 'datetime')
     Champ.create(type_de_champ: champ_datetime)
   end
-  let!(:champs) { [champ, champ_mandatory_empty, champ_datetime] }
+
+  let!(:champ_number) do
+    champ_number = TypeDeChamp.new(type_champ: 'number')
+    Champ.create(type_de_champ: champ_number)
+  end
+
+  let!(:champs) { [champ, champ_mandatory_empty, champ_datetime, champ_number] }
 
   describe 'save_champs' do
     before :each do
       params_hash = {
         champs: {
           "'#{champ.id}'" => 'yop',
-          "'#{champ_datetime.id}'" => 'd'
+          "'#{champ_datetime.id}'" => 'd',
+          "'#{champ_number.id}'" => "#{number_value}"
         },
         time_hour:  { "'#{champ_datetime.id}'" => '12' },
         time_minute: { "'#{champ_datetime.id}'" => '24' }
@@ -23,12 +30,24 @@ describe ChampsService do
       champs.each(&:reload)
     end
 
+    let(:number_value) { '123,34.23' }
+
     it 'saves the changed champ' do
       expect(champ.value).to eq('yop')
     end
 
     it 'parses and save the date' do
       expect(champ_datetime.value).to eq('d 12:24')
+    end
+
+    it 'parses and save the number' do
+      expect(champ_number.value).to eq('123.34')
+    end
+
+    context 'when number value is blank' do
+      let(:number_value) { '' }
+
+      it { expect(champ_number.value).to be_nil }
     end
   end
 
