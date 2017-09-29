c395b3904ce3ade0febd35f660c10bbbe14ef5f4
Mathieu Magnin <mathieu.magnin@gmail.com>
2017-09-29 13:05:02 +0200

    Add some tests on dossier filtering

--- spec/features/new_gestionnaire/procedure_filters_spec.rb
+++ spec/features/new_gestionnaire/procedure_filters_spec.rb
@@ -5,7 +5,8 @@ feature "procedure filters" do
   let(:gestionnaire) { create(:gestionnaire, procedures: [procedure]) }
   let!(:type_de_champ) { procedure.types_de_champ.first }
   let!(:new_unfollow_dossier) { create(:dossier, procedure: procedure, state: "received") }
-  let(:champ) { Champ.find_by(type_de_champ_id: type_de_champ.id) }
+  let!(:champ) { Champ.find_by(type_de_champ_id: type_de_champ.id, dossier_id: new_unfollow_dossier.id) }
+  let!(:new_unfollow_dossier_2) { create(:dossier, procedure: procedure, state: "received") }
 
   before do
     champ.update_attributes(value: "Mon champ rempli")
@@ -14,8 +15,20 @@ feature "procedure filters" do
   end
 
   scenario "should display demandeur by default" do
-    expect(page).to have_content("Demandeur")
-    expect(page).to have_content(new_unfollow_dossier.user.email)
+    within ".dossiers-table" do
+      expect(page).to have_link("Demandeur")
+      expect(page).to have_link(new_unfollow_dossier.user.email)
+    end
+  end
+
+  scenario "should list all dossiers" do
+    within ".dossiers-table" do
+      expect(page).to have_link(new_unfollow_dossier.id)
+      expect(page).to have_link(new_unfollow_dossier.user.email)
+
+      expect(page).to have_link(new_unfollow_dossier_2.id)
+      expect(page).to have_link(new_unfollow_dossier_2.user.email)
+    end
   end
 
   scenario "should add be able to add created_at column", js: true do
@@ -32,6 +45,41 @@ feature "procedure filters" do
       expect(page).to have_link(type_de_champ.libelle)
       expect(page).to have_link(champ.value)
     end
+  end
+
+  scenario "should be able to add and remove filter", js: true do
+    add_filter(type_de_champ.libelle, champ.value)
+
+    expect(page).to have_content("#{type_de_champ.libelle} : #{champ.value}")
+
+    within ".dossiers-table" do
+      expect(page).to have_link(new_unfollow_dossier.id)
+      expect(page).to have_link(new_unfollow_dossier.user.email)
+
+      expect(page).not_to have_link(new_unfollow_dossier_2.id)
+      expect(page).not_to have_link(new_unfollow_dossier_2.user.email)
+    end
+
+    remove_filter
+
+    within ".dossiers-table" do
+      expect(page).to have_link(new_unfollow_dossier.id)
+      expect(page).to have_link(new_unfollow_dossier.user.email)
+
+      expect(page).to have_link(new_unfollow_dossier_2.id)
+      expect(page).to have_link(new_unfollow_dossier_2.user.email)
+    end
+  end
+
+  def remove_filter
+    find(:xpath, "//span[contains(@class, 'filter')]/a").click
+  end
+
+  def add_filter(column_name, filter_value)
+    find(:xpath, "//span[contains(text(), 'Filtrer')]").click
+    select column_name, from: "Colonne"
+    fill_in "Valeur", with: filter_value
+    click_button "Ajouter le filtre"
   end
 
   def add_column(column_name)
