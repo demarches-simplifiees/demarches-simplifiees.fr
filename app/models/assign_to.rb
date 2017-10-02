7036f9778b8581436d434fc7d038f035080ed617
gregoirenovel <log@kinko.fr>
2017-10-02 17:03:30 +0200

    Add ProcedurePresentation

--- app/models/assign_to.rb
+++ app/models/assign_to.rb
@@ -1,4 +1,9 @@
 class AssignTo < ActiveRecord::Base
   belongs_to :procedure
   belongs_to :gestionnaire
+  has_one :procedure_presentation, dependent: :destroy
+
+  def procedure_presentation_or_default
+    self.procedure_presentation || build_procedure_presentation
+  end
 end
---
+++ spec/models/assign_to_spec.rb
@@ -0,0 +1,16 @@
+describe AssignTo, type: :model do
+  describe '#procedure_presentation_or_default' do
+    context "without a procedure_presentation" do
+      let!(:assign_to) { AssignTo.create }
+
+      it { expect(assign_to.procedure_presentation_or_default.persisted?).to be_falsey }
+    end
+
+    context "with a procedure_presentation" do
+      let!(:assign_to) { AssignTo.create }
+      let!(:procedure_presentation) { ProcedurePresentation.create(assign_to: assign_to) }
+
+      it { expect(assign_to.procedure_presentation_or_default).to eq(procedure_presentation) }
+    end
+  end
+end
