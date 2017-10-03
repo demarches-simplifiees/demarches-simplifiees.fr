1aa70a837b1d0acb09fe4617d7e41d7b572b971d
gregoirenovel <log@kinko.fr>
2017-10-03 16:31:17 +0200

    Move the jobs logging handling in ApplicationJob

--- app/jobs/auto_archive_procedure_job.rb
+++ app/jobs/auto_archive_procedure_job.rb
@@ -2,7 +2,6 @@ class AutoArchiveProcedureJob < ApplicationJob
   queue_as :cron
 
   def perform(*args)
-    Rails.logger.info("AutoArchiveProcedureJob started at #{Time.now}")
     Procedure.publiees.where("auto_archive_on <= ?", Date.today).each do |procedure|
       procedure.dossiers.state_en_construction.each do |dossier|
         dossier.received!
@@ -10,6 +9,5 @@ class AutoArchiveProcedureJob < ApplicationJob
 
       procedure.archive
     end
-    Rails.logger.info("AutoArchiveProcedureJob ended at #{Time.now}")
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -24,6 +24,8 @@ require 'rspec/rails'
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
+ActiveJob::Base.queue_adapter = :test
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
