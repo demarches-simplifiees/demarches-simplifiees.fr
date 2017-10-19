fe84ff9e6cf81adb16b7e83bb025525fd697f57c
gregoirenovel <log@kinko.fr>
2017-10-19 11:20:12 +0200

    [Fix #864] Warn the user when they click the delete draft button

--- app/assets/javascripts/old_design/dossiers_list_link.js
+++ app/assets/javascripts/old_design/dossiers_list_link.js
@@ -1,7 +1,9 @@
 $(document).on('turbolinks:load', link_init);
 
 function link_init() {
-  $('#dossiers-list tr').on('click', function () {
-    $(location).attr('href', $(this).data('dossier_url'))
+  $('#dossiers-list tr').on('click', function (event) {
+    if (event.target.className !== 'btn-sm btn-danger') {
+      $(location).attr('href', $(this).data('dossier_url'));
+    }
   });
 }
