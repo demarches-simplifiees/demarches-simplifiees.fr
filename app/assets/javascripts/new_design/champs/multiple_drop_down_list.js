d40c0d3b0672ed1057001a632964121b06f0a1b2
Simon Lehericey <simon.lehericey@gmail.com>
2017-10-27 17:15:21 +0200

    Select2: fr localization

--- app/assets/javascripts/new_design/champs/multiple_drop_down_list.js
+++ app/assets/javascripts/new_design/champs/multiple_drop_down_list.js
@@ -1,7 +1,10 @@
 document.addEventListener('turbolinks:load', function() {
-  $('select.select2').select2();
+  $('select.select2').select2({
+    'language': 'fr'
+  });
 
   $('select.select2-limited').select2({
+    'language': 'fr',
     'placeholder': 'Sélectionnez des colonnes',
     'maximumSelectionLength': '5',
     'width': '300px'
