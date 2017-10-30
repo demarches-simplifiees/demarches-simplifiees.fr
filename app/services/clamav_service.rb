36491230bb62ef31ebcd00e08d387042f9a23352
Mathieu Magnin <mathieu.magnin@gmail.com>
2017-10-30 17:23:55 +0100

    Rename variable path_file => file_path

--- app/services/clamav_service.rb
+++ app/services/clamav_service.rb
@@ -1,13 +1,13 @@
 class ClamavService
-  def self.safe_file? path_file
+  def self.safe_file? file_path
     if Rails.env == 'development'
       return CLAMAV[:response] if CLAMAV[:mock?]
     end
 
-    FileUtils.chmod 0666, path_file
+    FileUtils.chmod 0666, file_path
 
     client = ClamAV::Client.new
-    response = client.execute(ClamAV::Commands::ScanCommand.new(path_file))
+    response = client.execute(ClamAV::Commands::ScanCommand.new(file_path))
 
     return false if response.first.class == ClamAV::VirusResponse
     true
