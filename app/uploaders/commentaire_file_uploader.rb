7f4652fd1ec0a2382578158c648a7ce7ff51ff67
Mathieu Magnin <mathieu.magnin@gmail.com>
2017-10-31 16:21:26 +0100

    New UI : Accept only few extensions

--- app/uploaders/commentaire_file_uploader.rb
+++ app/uploaders/commentaire_file_uploader.rb
@@ -12,4 +12,12 @@ class CommentaireFileUploader < BaseUploader
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+
+  def extension_white_list
+    %w(pdf doc docx xls xlsx ppt pptx odt ods odp jpg jpeg png zip txt)
+  end
+
+  def accept_extension_list
+    extension_white_list.map{ |e| ".#{e}" }.join(",")
+  end
 end
