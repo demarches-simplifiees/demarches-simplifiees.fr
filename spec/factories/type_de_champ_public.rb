f3c179765461b6ace150f31e4b46345c9ec17f98
Mathieu Magnin <mathieu.magnin@gmail.com>
2017-11-23 17:44:44 +0100

    Create TypeDeChamp trait for each type de champ, and standardize their name

--- spec/factories/type_de_champ_public.rb
+++ spec/factories/type_de_champ_public.rb
@@ -6,32 +6,38 @@ FactoryGirl.define do
     order_place 1
     mandatory false
 
-    trait :checkbox do
-      type_champ 'checkbox'
-    end
+    [
+      :text,
+      :textarea,
+      :checkbox,
+      :date,
+      :datetime,
+      :number,
+      :civilite,
+      :email,
+      :header_section,
+      :explication,
+      :phone,
+      :address,
+      :pays,
+      :regions,
+      :departements,
+      :engagement,
+      :header_section,
+      :explication,
+      :dossier_link,
+      :yes_no,
+      :drop_down_list,
+      :multiple_drop_down_list
+    ].each do |type|
+      trait type do
+        libelle type.to_s.humanize
+        type_champ type.to_s
 
-    trait :header_section do
-      type_champ 'header_section'
-    end
-
-    trait :explication do
-      type_champ 'explication'
-    end
-
-    trait :type_dossier_link do
-      libelle 'Référence autre dossier'
-      type_champ 'dossier_link'
-    end
-
-    trait :type_yes_no do
-      libelle 'Yes/no'
-      type_champ 'yes_no'
-    end
-
-    trait :type_drop_down_list do
-      libelle 'Menu déroulant'
-      type_champ 'drop_down_list'
-      drop_down_list { create(:drop_down_list) }
+        if type.in? [:drop_down_list, :multiple_drop_down_list]
+          drop_down_list { create(:drop_down_list) }
+        end
+      end
     end
   end
 end
