f5623c1794c1f0ffd052db84df13933239988530
gregoirenovel <log@kinko.fr>
2017-11-24 20:00:15 +0100

    Refactor ChampHelper#value

--- app/helpers/champ_helper.rb
+++ app/helpers/champ_helper.rb
@@ -4,20 +4,23 @@ module ChampHelper
   end
 
   def formatted_value(champ)
-    if champ.type_champ == "date" && champ.value.present?
-      Date.parse(champ.value).strftime("%d/%m/%Y")
-    elsif type_champ.in? ["checkbox", "engagement"]
-      champ.value == 'on' ? 'Oui' : 'Non'
-    elsif type_champ == 'yes_no'
-      if champ.value == 'true'
-        'Oui'
-      elsif champ.value == 'false'
-        'Non'
-      end
-    elsif type_champ == 'multiple_drop_down_list' && champ.value.present?
-      JSON.parse(champ.value).join(', ')
+    value = champ.value
+
+    if value.blank?
+      ""
     else
-      champ.value
+      case champ.type_champ
+      when "date"
+        Date.parse(value).strftime("%d/%m/%Y")
+      when "checkbox", "engagement"
+        value == 'on' ? 'Oui' : 'Non'
+      when 'yes_no'
+        value == 'true' ? 'Oui' : 'Non'
+      when 'multiple_drop_down_list'
+        JSON.parse(value).join(', ')
+      else
+        value
+      end
     end
   end
 end
