3c39c4d41dabb736b4ddd8320671cacabdce0fb8
Mathieu Magnin <mathieu.magnin@gmail.com>
2017-12-05 17:20:10 +0100

    [Fix #1016] display notifications for new messages

--- app/assets/javascripts/new_design/messagerie.js
+++ app/assets/javascripts/new_design/messagerie.js
@@ -1,7 +1,23 @@
 TPS.scrollMessagerie = function () {
+  var scrollTo = function ($container, $scrollTo) {
+    $container.scrollTop(
+      $scrollTo.offset().top - $container.offset().top + $container.scrollTop()
+    );
+  }
+
+  var scrollToBottom = function ($container) {
+    $container.scrollTop($container.prop('scrollHeight'));
+  }
+
   var $ul = $(".messagerie ul").first();
   if($ul.length) {
-    $ul.scrollTop($ul.prop('scrollHeight'));
+    var $elementToScroll = $('.date.highlighted').first();
+
+    if ($elementToScroll.length != 0) {
+      scrollTo($ul, $elementToScroll);
+    } else {
+      scrollToBottom($ul);
+    }
   }
 };
 
