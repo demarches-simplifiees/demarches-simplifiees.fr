c6be745067d61688b8561b121f615fca72f25396
Mathieu Magnin <mathieu.magnin@gmail.com>
2017-12-05 16:07:05 +0100

    [Fix #1016] display notifications for champs publics

--- spec/views/new_gestionnaire/dossiers/show.html.haml_spec.rb
+++ spec/views/new_gestionnaire/dossiers/show.html.haml_spec.rb
@@ -1,4 +1,6 @@
 describe 'new_gestionnaire/dossiers/show.html.haml', type: :view do
+  before { view.extend DossierHelper }
+
   let(:current_gestionnaire) { create(:gestionnaire) }
   let(:individual) { nil }
   let(:entreprise) { nil }
