c750ace0e68f5f66ad61c15c1fedc99ffb8ce51f
Simon Lehericey <simon.lehericey@gmail.com>
2017-12-11 16:31:54 +0100

    [fix #1053] An Administrateur can choose the final dossier states
    ! before going to production :
    - stopping the current AutoReceiveJob
    - relaunch the jobs with previous id and state = 'received'

--- app/jobs/auto_receive_dossiers_for_procedure_job.rb
+++ app/jobs/auto_receive_dossiers_for_procedure_job.rb
@@ -1,10 +1,10 @@
 class AutoReceiveDossiersForProcedureJob < ApplicationJob
   queue_as :cron
 
-  def perform(procedure_id)
+  def perform(procedure_id, state)
     procedure = Procedure.find_by(id: procedure_id)
     if procedure
-      procedure.dossiers.state_nouveaux.update_all(state: "received", received_at: Time.now)
+      procedure.dossiers.state_nouveaux.update_all(state: state, received_at: Time.now)
     end
   end
 end
