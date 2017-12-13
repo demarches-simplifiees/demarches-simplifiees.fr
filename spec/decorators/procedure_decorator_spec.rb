d901852e09d63259fcfdb383723a064f23e0cda8
Mathieu Magnin <mathieu.magnin@gmail.com>
2017-12-13 17:00:24 +0100

    Fix sentry issue #2139 when published_at is nil

--- spec/decorators/procedure_decorator_spec.rb
+++ spec/decorators/procedure_decorator_spec.rb
@@ -1,7 +1,10 @@
 require 'spec_helper'
 
 describe ProcedureDecorator do
-  let(:procedure) { create(:procedure, :published, created_at: Time.new(2015, 12, 24, 14, 10)) }
+  let(:published_at) { Time.new(2017, 12, 24, 14, 12) }
+  let(:procedure) { create(:procedure, published_at: published_at, created_at: Time.new(2015, 12, 24, 14, 10)) }
+  let!(:procedure_path) { create(:procedure_path, administrateur: create(:administrateur), procedure: procedure) }
+
   subject { procedure.decorate }
 
   describe 'lien' do
@@ -12,6 +15,16 @@ describe ProcedureDecorator do
   describe 'created_at_fr' do
     subject { super().created_at_fr }
     it { is_expected.to eq('24/12/2015 14:10') }
+  end
+
+  describe 'published_at_fr' do
+    subject { super().published_at_fr }
+    it { is_expected.to eq('24/12/2017 14:12') }
+
+    context 'published_at is nil' do
+      let(:published_at) { nil }
+      it { is_expected.to eq(nil) }
+    end
   end
 
   describe 'logo_img' do
