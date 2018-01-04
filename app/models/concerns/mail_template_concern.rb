63ca52792bf142409a299fb01d7a04df3c145247
Frederic Merizen <frederic.merizen@gmail.com>
2018-01-04 16:34:23 +0100

    [#1110] Remove legacy tags

--- app/models/concerns/mail_template_concern.rb
+++ app/models/concerns/mail_template_concern.rb
@@ -13,10 +13,8 @@ module MailTemplateConcern
     replace_tags(body, dossier)
   end
 
-  # TODO: remove legacy argument when removing legacy tags
-  def tags(reject_legacy: true, is_dossier_termine: self.class.const_get(:IS_DOSSIER_TERMINE))
-    super(is_dossier_termine: is_dossier_termine)
-      .reject { |tag| reject_legacy && tag[:is_legacy] }
+  def tags(is_dossier_termine: self.class.const_get(:IS_DOSSIER_TERMINE))
+    super
   end
 
   module ClassMethods
@@ -29,15 +27,7 @@ module MailTemplateConcern
   private
 
   def dossier_tags
-    super +
-      [{ libelle: 'lien dossier', description: '', lambda: -> (d) { users_dossier_recapitulatif_link(d) } },
-       # TODO: remove legacy tags
-       { libelle: 'numero_dossier', description: '', target: :id, is_legacy: true },
-       { libelle: 'lien_dossier', description: '', lambda: -> (d) { users_dossier_recapitulatif_link(d) }, is_legacy: true },
-       { libelle: 'libelle_procedure', description: '', lambda: -> (d) { d.procedure.libelle }, is_legacy: true },
-       { libelle: 'date_de_decision', description: '',
-         lambda: -> (d) { d.processed_at.present? ? d.processed_at.localtime.strftime('%d/%m/%Y') : '' },
-         dossier_termine_only: true, is_legacy: true }]
+    super + [{ libelle: 'lien dossier', description: '', lambda: -> (d) { users_dossier_recapitulatif_link(d) } }]
   end
 
   def users_dossier_recapitulatif_link(dossier)
