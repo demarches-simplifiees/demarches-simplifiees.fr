ef2a6e72ac717e869bdd683d89aeda492016617f
simon lehericey <simon.lehericey@gmail.com>
2018-01-05 11:16:50 +0100

    [fix #1097] Add job to monitor procedures

--- app/mailers/administration_mailer.rb
+++ app/mailers/administration_mailer.rb
@@ -1,9 +1,17 @@
 class AdministrationMailer < ApplicationMailer
+  layout 'mailers/layout'
+
   def new_admin_email admin, administration
     @admin = admin
     @administration = administration
 
     mail(to: 'tech@tps.apientreprise.fr',
          subject: "Création d'un compte Admin TPS")
+  end
+
+  def dubious_procedures(procedures_and_type_de_champs)
+    @procedures_and_type_de_champs = procedures_and_type_de_champs
+    mail(to: 'tech@tps.apientreprise.fr',
+         subject: "[RGS] De nouvelles procédures comportent des champs interdits")
   end
 end
