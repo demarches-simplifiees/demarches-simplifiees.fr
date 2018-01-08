9afdb4c2c41c30c1301ec37a591dfa0cc8f827be
Frederic Merizen <frederic.merizen@gmail.com>
2018-01-08 17:48:08 +0100

    Simplify factory girl configuration for mail templates

--- spec/factories/received_mail.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :received_mail, class: Mails::ReceivedMail do
-    subject "Mail d'accusé de bonne reception de votre dossier"
-    body "Votre dossier est correctement reçu"
-  end
-end
