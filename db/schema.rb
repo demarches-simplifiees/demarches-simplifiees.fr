39a138e48da6fcf030e21575dbe574cfd58bc4c7
Frederic Merizen <frederic.merizen@gmail.com>
2018-01-08 15:49:45 +0100

    Drop MAIL_TEMPLATES table
    it’s not used since 695dc16b8 / 335caed65ec44 anymore

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180108132507) do
+ActiveRecord::Schema.define(version: 20180108144114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -321,15 +321,6 @@ ActiveRecord::Schema.define(version: 20180108132507) do
     t.integer "dossier_id"
     t.integer "user_id"
     t.string  "type",         default: "InviteGestionnaire"
-  end
-
-  create_table "mail_templates", force: :cascade do |t|
-    t.string   "object"
-    t.text     "body"
-    t.string   "type"
-    t.integer  "procedure_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "module_api_cartos", force: :cascade do |t|
