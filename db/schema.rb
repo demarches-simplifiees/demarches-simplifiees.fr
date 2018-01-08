16bc44738ca70a4c908cf6c0bc94a4dbda00144f
simon lehericey <simon.lehericey@gmail.com>
2018-01-08 14:59:23 +0100

    [fix #1097] Accent insensitive search

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180105152235) do
+ActiveRecord::Schema.define(version: 20180108132507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "unaccent"
 
   create_table "administrateurs", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
