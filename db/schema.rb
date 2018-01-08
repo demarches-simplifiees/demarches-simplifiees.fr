e3412569962ca77600ca4d7081b861421a261997
Paul Chavard <git@paul.chavard.net>
2018-01-08 16:04:31 +0100

    Add active attribute to administrateur model

--- db/schema.rb
+++ db/schema.rb
@@ -17,12 +17,12 @@ ActiveRecord::Schema.define(version: 20180111153308) do
   enable_extension "unaccent"
 
   create_table "administrateurs", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20180111153308) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "api_token"
+    t.boolean  "active",                 default: false
     t.index ["email"], name: "index_administrateurs_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_administrateurs_on_reset_password_token", unique: true, using: :btree
   end
