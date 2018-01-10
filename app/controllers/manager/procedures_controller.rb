f772adc582127c4cc1b7895e442131d266bf27a1
Mathieu Magnin <mathieu.magnin@gmail.com>
2018-01-10 17:54:43 +0100

    Add flash when procedure is whitelisted

--- app/controllers/manager/procedures_controller.rb
+++ app/controllers/manager/procedures_controller.rb
@@ -3,6 +3,7 @@ module Manager
     def whitelist
       procedure = Procedure.find(params[:id])
       procedure.whitelist!
+      flash[:notice] = "Procédure whitelistée."
       redirect_to manager_procedure_path(procedure)
     end
   end
