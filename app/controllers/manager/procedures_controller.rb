af6b8268a8394b7171237b9a0d6c7f67f8a47900
Mathieu Magnin <mathieu.magnin@gmail.com>
2018-01-10 17:42:21 +0100

    Rails-ish route

--- app/controllers/manager/procedures_controller.rb
+++ app/controllers/manager/procedures_controller.rb
@@ -1,7 +1,7 @@
 module Manager
   class ProceduresController < Manager::ApplicationController
     def whitelist
-      procedure = Procedure.find(params[:procedure_id])
+      procedure = Procedure.find(params[:id])
       procedure.whitelist!
       redirect_to manager_procedure_path(procedure)
     end
