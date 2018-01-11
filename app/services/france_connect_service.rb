544aca0ebef6f01767b37e272b6ceaf86b260231
simon lehericey <simon.lehericey@gmail.com>
2018-01-11 15:29:58 +0100

    FC Particulier Controller: simplier login

--- app/services/france_connect_service.rb
+++ app/services/france_connect_service.rb
@@ -1,4 +1,10 @@
 class FranceConnectService
+  def self.authorization_uri
+    client = FranceConnectParticulierClient.new
+
+    client.authorization_uri(scope: [:profile, :email], state: SecureRandom.hex(16), nonce: SecureRandom.hex(16))
+  end
+
   def self.retrieve_user_informations_particulier code
     client = FranceConnectParticulierClient.new(code)
 
