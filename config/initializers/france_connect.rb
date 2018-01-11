b35a88ffd42f3465eb4da9fbbfb0e16cd702e7ae
simon lehericey <simon.lehericey@gmail.com>
2018-01-11 14:37:53 +0100

    FC initializers: simplify

--- config/initializers/france_connect.rb
+++ config/initializers/france_connect.rb
@@ -1,11 +1,4 @@
-FRANCE_CONNECT = if !Rails.env.test?
-  file_path = "#{Rails.root}/config/france_connect.yml"
-  config_hash = YAML.safe_load(File.read(file_path))
-    .reduce({}) { |acc, (key, value)| acc[key.gsub('particulier_', '')] = value, acc }
-    .symbolize_keys
-
-  { particulier: config_hash }
-else
+FRANCE_CONNECT = if Rails.env.test?
   {
     particulier: {
       identifier: 'plop',
@@ -17,4 +10,13 @@ else
       logout_endpoint: 'https://bidon.com/endpoint',
     }
   }
+else
+  fc_config_file_path = "#{Rails.root}/config/france_connect.yml"
+
+  # FIXME: with a yaml with a { particulier: {} } structure
+  config_hash = YAML.safe_load(File.read(fc_config_file_path))
+    .reduce({}) { |acc, (key, value)| acc[key.gsub('particulier_', '')] = value; acc }
+    .symbolize_keys
+
+  { particulier: config_hash }
 end
