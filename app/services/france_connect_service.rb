d001ae17e76a75a6c42df1d63dcafd48afa226d0
simon lehericey <simon.lehericey@gmail.com>
2018-01-11 17:07:35 +0100

    FC service: clean syntax

--- app/services/france_connect_service.rb
+++ app/services/france_connect_service.rb
@@ -5,12 +5,13 @@ class FranceConnectService
     client.authorization_uri(scope: [:profile, :email], state: SecureRandom.hex(16), nonce: SecureRandom.hex(16))
   end
 
-  def self.retrieve_user_informations_particulier code
+  def self.retrieve_user_informations_particulier(code)
     client = FranceConnectParticulierClient.new(code)
 
     access_token = client.access_token!(client_auth_method: :secret)
+
     user_info = access_token.userinfo!
-    hash = Hashie::Mash.new user_info.raw_attributes
+    hash = Hashie::Mash.new(user_info.raw_attributes)
 
     hash.france_connect_particulier_id = hash.sub
     hash
