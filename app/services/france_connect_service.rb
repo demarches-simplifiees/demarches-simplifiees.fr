229a66403eaa1b295e4640c1b052433d25e1ad83
simon lehericey <simon.lehericey@gmail.com>
2018-01-11 18:16:38 +0100

    FC service: return domain info

--- app/services/france_connect_service.rb
+++ app/services/france_connect_service.rb
@@ -11,12 +11,17 @@ class FranceConnectService
   def self.retrieve_user_informations_particulier(code)
     client = FranceConnectParticulierClient.new(code)
 
-    access_token = client.access_token!(client_auth_method: :secret)
+    user_info = client.access_token!(client_auth_method: :secret)
+      .userinfo!
+      .raw_attributes
 
-    user_info = access_token.userinfo!
-    hash = Hashie::Mash.new(user_info.raw_attributes)
-
-    hash.france_connect_particulier_id = hash.sub
-    hash
+    FranceConnectInformation.new(
+      gender: user_info[:gender],
+      given_name: user_info[:given_name],
+      family_name: user_info[:family_name],
+      email_france_connect: user_info[:email],
+      birthdate: user_info[:birthdate],
+      birthplace: user_info[:birthplace],
+      france_connect_particulier_id: user_info[:sub])
   end
 end
