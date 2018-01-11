2260801605cfd6c05fa6b3efedad7c9c14a0c0bf
simon lehericey <simon.lehericey@gmail.com>
2018-01-11 11:36:20 +0100

    FC ParticulierClient: simplier initializer

--- app/models/france_connect_particulier_client.rb
+++ app/models/france_connect_particulier_client.rb
@@ -1,14 +1,6 @@
 class FranceConnectParticulierClient < OpenIDConnect::Client
   def initialize(code=nil)
-    super(
-        identifier: FRANCE_CONNECT.identifier,
-        secret: FRANCE_CONNECT.secret,
-        redirect_uri: FRANCE_CONNECT.redirect_uri,
-        authorization_endpoint: FRANCE_CONNECT.authorization_endpoint,
-        token_endpoint: FRANCE_CONNECT.token_endpoint,
-        userinfo_endpoint: FRANCE_CONNECT.userinfo_endpoint,
-        logout_endpoint: FRANCE_CONNECT.logout_endpoint
-    )
+    super(FRANCE_CONNECT)
 
     if code.present?
       self.authorization_code = code
