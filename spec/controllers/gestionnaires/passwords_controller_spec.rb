5c78e6de65268bc7dc524a8cbcafae4c1b5f2134
gregoirenovel <log@kinko.fr>
2018-01-15 19:24:05 +0100

    Enable the Layout/MultilineHashBraceLayout cop

--- spec/controllers/gestionnaires/passwords_controller_spec.rb
+++ spec/controllers/gestionnaires/passwords_controller_spec.rb
@@ -18,11 +18,13 @@ describe Gestionnaires::PasswordsController, type: :controller do
       end
 
       it "also signs user in" do
-        put :update, params: {gestionnaire: {
-          reset_password_token: @token,
-          password: "supersecret",
-          password_confirmation: "supersecret"
-        }}
+        put :update, params: {
+          gestionnaire: {
+            reset_password_token: @token,
+            password: "supersecret",
+            password_confirmation: "supersecret"
+          }
+        }
         expect(subject.current_gestionnaire).to eq(gestionnaire)
         expect(subject.current_user).to eq(user)
       end
