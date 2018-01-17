9be1c14dfddbce8f9c0889ef509c9edea74ebf08
Simon Lehericey <simon.lehericey@gmail.com>
2018-01-17 17:33:26 +0100

    wip

---
+++ app/controllers/france_connect/agent_controller.rb
@@ -0,0 +1,53 @@
+class FranceConnect::AgentController < ApplicationController
+  before_action :redirect_to_login_if_fc_aborted, only: [:callback]
+
+  def login
+    redirect_to FranceConnectService.authorization_uri(FRANCE_CONNECT[:agent])
+  end
+
+  def callback
+    fetched_fci = FranceConnectService.fetch_info(FRANCE_CONNECT[:agent], params[:code])
+
+    fci = FranceConnectInformation
+      .find_by(france_connect_particulier_id: fetched_fci[:france_connect_particulier_id]) ||
+        fetched_fci.tap { |object| object.save }
+
+    if fci.user.nil?
+      user = User.find_or_create_by(email: fci.email_france_connect) do |new_user|
+        new_user.password = Devise.friendly_token[0, 20]
+      end
+
+      fci.update_attribute('user_id', user.id)
+    end
+
+    connect_france_connect_particulier(fci.user)
+  rescue Rack::OAuth2::Client::Error => e
+    Rails.logger.error e.message
+    redirect_france_connect_error_connection
+  end
+
+  private
+
+  def redirect_to_login_if_fc_aborted
+    if params[:code].empty?
+      redirect_to new_user_session_path
+    end
+  end
+
+  def connect_france_connect_particulier user
+    sign_out :user if user_signed_in?
+    sign_out :gestionnaire if gestionnaire_signed_in?
+    sign_out :administrateur if administrateur_signed_in?
+
+    sign_in user
+
+    user.update_attribute('loged_in_with_france_connect', 'particulier')
+
+    redirect_to stored_location_for(current_user) || root_path(current_user)
+  end
+
+  def redirect_france_connect_error_connection
+    flash.alert = t('errors.messages.france_connect.connexion')
+    redirect_to(new_user_session_path)
+  end
+end
