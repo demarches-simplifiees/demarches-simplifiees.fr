14b8ac614faf66566294e3e20ed3dbebd8bda360
Mathieu Magnin <mathieu.magnin@gmail.com>
2018-01-17 12:13:26 +0100

    Redirect to sign_in if not logged in

--- app/controllers/manager/application_controller.rb
+++ app/controllers/manager/application_controller.rb
@@ -7,5 +7,15 @@ module Manager
       params[:order] ||= "created_at"
       params[:direction] ||= "desc"
     end
+
+    protected
+
+    def authenticate_administration!
+      if administration_signed_in?
+        super
+      else
+        redirect_to manager_sign_in_path
+      end
+    end
   end
 end
