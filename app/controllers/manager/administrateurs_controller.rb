0396db9e5f15988640d21530c925940d6abdecf0
Mathieu Magnin <mathieu.magnin@gmail.com>
2018-01-17 10:13:38 +0100

    Clean Administrate templates and controllers

--- app/controllers/manager/administrateurs_controller.rb
+++ app/controllers/manager/administrateurs_controller.rb
@@ -1,21 +1,4 @@
 module Manager
   class AdministrateursController < Manager::ApplicationController
-    # To customize the behavior of this controller,
-    # simply overwrite any of the RESTful actions. For example:
-    #
-    # def index
-    #   super
-    #   @resources = Administrateur.
-    #     page(params[:page]).
-    #     per(10)
-    # end
-
-    # Define a custom finder by overriding the `find_resource` method:
-    # def find_resource(param)
-    #   Administrateur.find_by!(slug: param)
-    # end
-
-    # See https://administrate-prototype.herokuapp.com/customizing_controller_actions
-    # for more information
   end
 end
