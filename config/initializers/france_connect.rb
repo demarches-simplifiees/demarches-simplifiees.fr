0e8b75b12e583223d426c3b7669e18103e622d16
Simon Lehericey <simon.lehericey@gmail.com>
2018-01-17 12:20:11 +0100

    FC: allow new and old configuration file

--- config/initializers/france_connect.rb
+++ config/initializers/france_connect.rb
@@ -8,15 +8,37 @@ FRANCE_CONNECT = if Rails.env.test?
       token_endpoint: 'https://bidon.com/endpoint',
       userinfo_endpoint: 'https://bidon.com/endpoint',
       logout_endpoint: 'https://bidon.com/endpoint',
+    },
+    agent: {
+      identifier: 'plop',
+      secret: 'plip',
+      redirect_uri: 'https://bidon.com/endpoint',
+      authorization_endpoint: 'https://bidon.com/endpoint',
+      token_endpoint: 'https://bidon.com/endpoint',
+      userinfo_endpoint: 'https://bidon.com/endpoint',
+      logout_endpoint: 'https://bidon.com/endpoint',
     }
   }
 else
   fc_config_file_path = "#{Rails.root}/config/france_connect.yml"
+  conf = YAML.safe_load(File.read(fc_config_file_path))
 
   # FIXME: with a yaml with a { particulier: {} } structure
-  config_hash = YAML.safe_load(File.read(fc_config_file_path))
-    .reduce({}) { |acc, (key, value)| acc[key.gsub('particulier_', '')] = value; acc }
-    .symbolize_keys
+  old_file = File.readlines(fc_config_file_path).any? { |line| line.include?('particulier_') }
 
-  { particulier: config_hash }
+  if old_file
+    particulier_hash = conf
+      .select { |(key, _)| key.start_with?('particulier_') }
+      .reduce({}) { |acc, (key, value)| acc[key.gsub('particulier_', '')] = value; acc }
+      .symbolize_keys
+
+    agent_hash = conf
+      .select { |(key, _)| key.start_with?('agent_') }
+      .reduce({}) { |acc, (key, value)| acc[key.gsub('agent_', '')] = value; acc }
+      .symbolize_keys
+
+    { particulier: particulier_hash, agent: agent_hash }
+  else
+    conf.deep_symbolize_keys
+  end
 end
