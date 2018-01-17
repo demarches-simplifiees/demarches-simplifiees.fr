93a3a4b87f7b336544c630bd70e12b59b7eae642
Mathieu Magnin <mathieu.magnin@gmail.com>
2018-01-17 10:28:59 +0100

    Move administrateur creation in Administrate

--- app/controllers/manager/administrateurs_controller.rb
+++ app/controllers/manager/administrateurs_controller.rb
@@ -1,4 +1,21 @@
 module Manager
   class AdministrateursController < Manager::ApplicationController
+    def create
+      administrateur = current_administration.invite_admin(create_administrateur_params[:email])
+
+      if administrateur.errors.empty?
+        flash.notice = "Administrateur créé"
+      else
+        flash.alert = administrateur.errors.full_messages
+      end
+
+      redirect_to manager_administrateurs_path
+    end
+
+    private
+
+    def create_administrateur_params
+      params.require(:administrateur).permit(:email)
+    end
   end
 end
--- spec/controllers/administrations_controller_spec.rb
+++ spec/controllers/administrations_controller_spec.rb
@@ -18,35 +18,4 @@ describe AdministrationsController, type: :controller do
       it { expect(subject.status).to eq 200 }
     end
   end
-
-  describe 'POST #create' do
-    let(:email) { 'plop@plop.com' }
-    let(:password) { 'password' }
-
-    before do
-      sign_in administration
-    end
-
-    subject { post :create, administrateur: {email: email } }
-
-    context 'when email and password are correct' do
-      it 'add new administrateur in database' do
-        expect { subject }.to change(Administrateur, :count).by(1)
-      end
-
-      it 'alert new mail are send' do
-        expect(AdministrationMailer).to receive(:new_admin_email).and_return(AdministrationMailer)
-        expect(AdministrationMailer).to receive(:deliver_now!)
-        expect(AdministrationMailer).to receive(:invite_admin).and_return(AdministrationMailer)
-        expect(AdministrationMailer).to receive(:deliver_now!)
-        subject
-      end
-    end
-
-    context 'when email or password are missing' do
-      let(:email) { '' }
-
-      it { expect { subject }.to change(Administrateur, :count).by(0) }
-    end
-  end
 end
