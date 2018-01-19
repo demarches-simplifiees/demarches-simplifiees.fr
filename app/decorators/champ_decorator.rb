61667aa9636c1d8f59f433c0dddf2d995853fe38
Mathieu Magnin <mathieu.magnin@gmail.com>
2018-01-19 13:20:53 +0100

    Revert "Remove `data_provide` and `data_date_format` attributes"
    This reverts commit 01249a49d68084e6b9ba8c0a3b3eb9abbe90b169.

--- app/decorators/champ_decorator.rb
+++ app/decorators/champ_decorator.rb
@@ -19,16 +19,6 @@ class ChampDecorator < Draper::Decorator
     end
   end
 
-  def date_for_input
-    if object.value.present?
-      if type_champ == "date"
-        object.value
-      elsif type_champ == "datetime" && object.value != ' 00:00'
-        DateTime.parse(object.value, "%d/%m/%Y %H:%M").strftime("%d/%m/%Y")
-      end
-    end
-  end
-
   def description_with_links
     description.gsub(URI.regexp, '<a target="_blank" href="\0">\0</a>') if description
   end
