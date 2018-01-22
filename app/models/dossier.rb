tungla/tps

Use an instance method instead of a decorator

app/models/dossier.rb
@@ -364,11 +364,18 @@ class Dossier < ActiveRecord::Base
   end
 
   def statut
-    if closed?
+    case state
+    when 'draft'
+      'brouillon'
+    when 'initiated'
+      "en construction"
+    when 'received'
+      "en instruction"
+    when 'closed'
       'accepté'
-    elsif without_continuation?
+    when 'without_continuation'
       'classé sans suite'
-    elsif refused?
+    when 'refused'
       'refusé'
     end
   end

demarches-simplifiees/demarches-simplifiees.fr

app/models/dossier.rb
@@ -294,11 +294,18 @@ class Dossier < ActiveRecord::Base
   end
 
   def statut
-    if accepte?
+    case state
+    when 'brouillon'
+      'brouillon'
+    when 'en_construction'
+      "en construction"
+    when 'en_instruction'
+      "en instruction"
+    when 'accepte'
       'accepté'
-    elsif sans_suite?
+    when 'sans_suite'
       'classé sans suite'
-    elsif refuse?
+    when 'refuse'
       'refusé'
     end
   end