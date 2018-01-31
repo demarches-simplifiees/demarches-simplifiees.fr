57cf12c6be0c4aaa6f1a59577550ceb6ec8d9b2f
Frederic Merizen <frederic.merizen@gmail.com>
2018-01-31 12:14:04 +0100

    Simplify invite mailer

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -1,25 +1,18 @@
 class InviteMailer < ApplicationMailer
-  def invite_user invite
-    vars_mailer invite
-
-    send_mail invite.email, "TPS - Participez à l'élaboration d'un dossier", invite.email_sender if invite.user.present?
-  end
-
-  def invite_guest invite
-    vars_mailer invite
-
-    send_mail invite.email, "Invitation - #{invite.email_sender} vous invite à consulter un dossier sur la plateforme TPS", invite.email_sender
-  end
-
-  private
-
-  def vars_mailer invite
+  def invite_user(invite)
     @invite = invite
+
+    subject = "TPS - Participez à l'élaboration d'un dossier"
+    reply_to = if invite.user.present? then invite.email_sender end
+
+    mail(to: invite.email, subject: subject, reply_to: reply_to)
   end
 
-  def send_mail email, subject, reply_to
-    mail(to: email,
-         subject: subject,
-         reply_to: reply_to)
+  def invite_guest(invite)
+    @invite = invite
+
+    subject = "Invitation - #{invite.email_sender} vous invite à consulter un dossier sur la plateforme TPS"
+
+    mail(to: invite.email, subject: subject, reply_to: invite.email_sender)
   end
 end
