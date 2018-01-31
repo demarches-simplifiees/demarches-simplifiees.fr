d34fce86ae426d6c156a64b4f330a04b209927a9
Frederic Merizen <frederic.merizen@gmail.com>
2018-01-31 10:42:58 +0100

    Simplify gestionnaire mailer

--- app/mailers/gestionnaire_mailer.rb
+++ app/mailers/gestionnaire_mailer.rb
@@ -1,14 +1,17 @@
 class GestionnaireMailer < ApplicationMailer
   layout 'mailers/layout'
 
-  def new_gestionnaire email, password
-    send_mail email, password, "Vous avez été nommé accompagnateur sur la plateforme TPS"
+  def new_gestionnaire(email, password)
+    @password = password
+    @email = email
+    mail(to: email, subject: "Vous avez été nommé accompagnateur sur la plateforme TPS")
   end
 
   def last_week_overview(gestionnaire)
     headers['X-mailjet-campaign'] = 'last_week_overview'
     overview = gestionnaire.last_week_overview
-    send_mail gestionnaire.email, overview, 'Vos activités sur TPS'
+    @procedure_overviews = overview[:procedure_overviews]
+    mail(to: gestionnaire.email, subject: 'Vos activités sur TPS')
   end
 
   def send_dossier(sender, dossier, recipient)
@@ -17,18 +20,5 @@ class GestionnaireMailer < ApplicationMailer
     subject = "#{sender.email} vous a envoyé le dossier nº #{dossier.id}"
 
     mail(to: recipient.email, subject: subject)
-  end
-
-  private
-
-  def vars_mailer email, args
-    @args = args
-    @email = email
-  end
-
-  def send_mail email, args, subject
-    vars_mailer email, args
-
-    mail(to: email, subject: subject)
   end
 end
