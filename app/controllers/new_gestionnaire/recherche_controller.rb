7f5718cc69f2416dd1e56838734300d0b07dac96
Mathieu Magnin <mathieu.magnin@gmail.com>
2018-02-06 12:15:20 +0100

    [Fix #478] /recherche can deal with very big int :relaxed:

--- app/controllers/new_gestionnaire/recherche_controller.rb
+++ app/controllers/new_gestionnaire/recherche_controller.rb
@@ -4,10 +4,9 @@ module NewGestionnaire
       @search_terms = params[:q]
 
       # exact id match?
-      if @search_terms.to_i != 0
-        @dossiers = current_gestionnaire.dossiers.where(id: @search_terms.to_i) +
-          current_gestionnaire.dossiers_from_avis.where(id: @search_terms.to_i)
-        @dossiers.uniq!
+      id = @search_terms.to_i
+      if id != 0 && id_compatible?(id) # Sometimes gestionnaire is searching dossiers with a big number (ex: SIRET), ActiveRecord can't deal with them and throws ActiveModel::RangeError. id_compatible? prevents this.
+        @dossiers = dossiers_by_id(id)
       end
 
       if @dossiers.nil?
@@ -21,6 +20,23 @@ module NewGestionnaire
           query: @search_terms,
           page: params[:page]
         ).results
+      end
+    end
+
+    private
+
+    def dossiers_by_id(id)
+      dossiers = current_gestionnaire.dossiers.where(id: id) +
+        current_gestionnaire.dossiers_from_avis.where(id: id)
+      dossiers.uniq
+    end
+
+    def id_compatible?(number)
+      begin
+        ActiveRecord::Type::Integer.new.serialize(number)
+        true
+      rescue ActiveModel::RangeError
+        false
       end
     end
   end
