db42c0c0cfed06611068d61ce2971f32e4779c3c
Paul Chavard <git@paul.chavard.net>
2018-02-14 18:13:23 +0100

    Fix public and private champs scopes

--- spec/models/type_de_champ_spec.rb
+++ spec/models/type_de_champ_spec.rb
@@ -4,4 +4,13 @@ describe TypeDeChamp do
   require 'models/type_de_champ_shared_example'
 
   it_should_behave_like "type_de_champ_spec"
+
+  describe '#public_only' do
+    let(:procedure) { create(:procedure, :with_type_de_champ, :with_type_de_champ_private) }
+
+    it 'partition public and private' do
+      expect(procedure.types_de_champ.count).to eq(1)
+      expect(procedure.types_de_champ_private.count).to eq(1)
+    end
+  end
 end
