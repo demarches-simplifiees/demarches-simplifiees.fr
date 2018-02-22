e058942c971593c571e2dd36c8f0ebb805e40fb5
Mathieu Magnin <mathieu.magnin@gmail.com>
2018-02-22 10:28:24 +0100

    [Fix #1456] When job crashed notify Sentry

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -6,4 +6,8 @@ class ApplicationJob < ActiveJob::Base
   after_perform do |job|
     Rails.logger.info("#{job.class.name} ended at #{Time.now}")
   end
+
+  def error(job, exception)
+    Raven.capture_exception(exception)
+  end
 end
