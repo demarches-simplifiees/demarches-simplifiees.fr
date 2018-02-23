1b3b6e9d8f241a1258e47f5a227c093718444a52
simon lehericey <simon.lehericey@gmail.com>
2018-02-23 13:44:02 +0100

    MailPreview: add send_dossier

--- spec/mailers/previews/gestionnaire_mailer_preview.rb
+++ spec/mailers/previews/gestionnaire_mailer_preview.rb
@@ -3,4 +3,8 @@ class GestionnaireMailerPreview < ActionMailer::Preview
     gestionnaire = Gestionnaire.first
     GestionnaireMailer.last_week_overview(gestionnaire, gestionnaire.last_week_overview)
   end
+
+  def send_dossier
+    GestionnaireMailer.send_dossier(Gestionnaire.first, Dossier.first, Gestionnaire.last)
+  end
 end
