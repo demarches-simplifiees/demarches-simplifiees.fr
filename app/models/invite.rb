002f29bcb704dbbadf075b45bb3002fd2eb2add3
Mathieu Magnin <mathieu.magnin@gmail.com>
2018-02-26 14:51:25 +0100

    [Fix #1479] Sanitize email format for invite

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,6 +1,10 @@
 class Invite < ApplicationRecord
+  include EmailSanitizableConcern
+
   belongs_to :dossier
   belongs_to :user
+
+  before_validation -> { sanitize_email(:email) }
 
   validates :email, presence: true
   validates :email, uniqueness: { scope: :dossier_id }
