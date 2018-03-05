be5849a7f54ea371a8d4d6a34d35eda2d5656ea0
gregoirenovel <log@kinko.fr>
2018-03-05 17:33:02 +0100

    Remove unneeded quotes

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = "'demarches-simplifiees.fr' <#{I18n.t('dynamics.contact_email')}>"
+  config.mailer_sender = "demarches-simplifiees.fr <#{I18n.t('dynamics.contact_email')}>"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
