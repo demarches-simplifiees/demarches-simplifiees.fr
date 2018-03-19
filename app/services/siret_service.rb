1659e631fa29fe56b60e8907eeab926c2f922553
gregoirenovel <log@kinko.fr>
2018-03-19 14:48:15 +0100

    Remove useless success? methods

--- app/services/siret_service.rb
+++ app/services/siret_service.rb
@@ -1,21 +1,21 @@
 class SIRETService
   def self.fetch(siret, dossier = nil)
-    etablissement = ApiEntreprise::EtablissementAdapter.new(siret, dossier&.procedure_id)
-    entreprise = ApiEntreprise::EntrepriseAdapter.new(siren(siret), dossier&.procedure_id)
+    etablissement_params = ApiEntreprise::EtablissementAdapter.new(siret, dossier&.procedure_id).to_params
+    entreprise_params = ApiEntreprise::EntrepriseAdapter.new(siren(siret), dossier&.procedure_id).to_params
 
-    if etablissement.success? && entreprise.success?
+    if etablissement_params.present? && entreprise_params.present?
       association = ApiEntreprise::RNAAdapter.new(siret, dossier&.procedure_id)
       exercices = ApiEntreprise::ExercicesAdapter.new(siret, dossier&.procedure_id)
 
-      params = etablissement.to_params
-        .merge(entreprise.to_params.map { |k,v| ["entreprise_#{k}", v] }.to_h)
+      params = etablissement_params
+        .merge(entreprise_params.map { |k,v| ["entreprise_#{k}", v] }.to_h)
         .merge(association.to_params&.map { |k,v| ["association_#{k}", v] }.to_h)
         .merge(exercices_attributes: exercices.to_array)
 
       # This is to fill legacy models and relationships
       if dossier.present?
         return params.merge(
-          entreprise_attributes: entreprise.to_params
+          entreprise_attributes: entreprise_params
             .merge({
               dossier: dossier,
               rna_information_attributes: association.to_params
