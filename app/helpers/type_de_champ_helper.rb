322f93afbeeb300097ca5c279b0d9c67fd871f24
Paul Chavard <git@paul.chavard.net>
2018-03-27 15:43:59 +0200

    Add administrateur#feature_enabled? method

--- app/helpers/type_de_champ_helper.rb
+++ app/helpers/type_de_champ_helper.rb
@@ -2,7 +2,7 @@ module TypeDeChampHelper
   def tdc_options(current_administrateur)
     tdcs = TypeDeChamp.type_de_champs_list_fr
 
-    if !current_administrateur.id.in?(Features.champ_pj_allowed_for_admin_ids)
+    if !current_administrateur.feature_enabled?(:champ_pj_allowed)
       tdcs.reject! { |tdc| tdc.last == "piece_justificative" }
     end
 
