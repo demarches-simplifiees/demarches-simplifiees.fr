4c24c8346a396c75639b5b9c90e654787faf0376
gregoirenovel <log@kinko.fr>
2018-03-28 15:19:34 +0200

    Use better names for PipedriveService methods

--- app/services/pipedrive_service.rb
+++ app/services/pipedrive_service.rb
@@ -1,11 +1,11 @@
 class PipedriveService
-  def self.accept_deals_from_person(person_id, owner_id, stage_id)
+  def self.accept_demande_from_person(person_id, owner_id, stage_id)
     waiting_deal_ids = Pipedrive::DealAdapter.fetch_waiting_deal_ids(person_id)
     waiting_deal_ids.each { |deal_id| Pipedrive::DealAdapter.update_deal_owner_and_stage(deal_id, owner_id, stage_id) }
     Pipedrive::PersonAdapter.update_person_owner(person_id, owner_id)
   end
 
-  def self.refuse_deals_from_person(person_id, owner_id)
+  def self.refuse_demande_from_person(person_id, owner_id)
     waiting_deal_ids = Pipedrive::DealAdapter.fetch_waiting_deal_ids(person_id)
     waiting_deal_ids.each { |deal_id| Pipedrive::DealAdapter.refuse_deal(deal_id, owner_id) }
     Pipedrive::PersonAdapter.update_person_owner(person_id, owner_id)
