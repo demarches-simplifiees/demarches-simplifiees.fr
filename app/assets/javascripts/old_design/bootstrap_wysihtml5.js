d315a041252018cfa649fd6e7d60e19d8110be3e
Mathieu Magnin <mathieu.magnin@gmail.com>
2018-03-29 14:17:06 +0200

    [Fix #1729] Do not allow <code> tags in template editors

--- app/assets/javascripts/old_design/bootstrap_wysihtml5.js
+++ app/assets/javascripts/old_design/bootstrap_wysihtml5.js
@@ -2,6 +2,36 @@ $(document).on('turbolinks:load', wysihtml5_active);
 
 function wysihtml5_active (){
   $('.wysihtml5').each(function(i, elem) {
-    $(elem).wysihtml5({ toolbar:{ "fa": true, "link": false, "color": false }, "locale": "fr-FR" });
+    $(elem).wysihtml5({
+      toolbar: {
+        "fa": true,
+        "link": false,
+        "color": false
+      },
+      parserRules: {
+        tags: {
+          p: {},
+          h1: {},
+          h2: {},
+          h3: {},
+          h4: {},
+          h5: {},
+          h6: {},
+          b: {},
+          i: {},
+          u: {},
+          small: {},
+          blockquote: {},
+          ul: {},
+          ol: {},
+          li: {},
+          img: {},
+          code: {
+            unwrap: 1
+          }
+        }
+      },
+      "locale": "fr-FR"
+    });
   });
 }
