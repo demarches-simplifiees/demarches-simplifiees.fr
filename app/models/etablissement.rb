3cd80af6cb2d4c08f5da3f4d8265aa3f46f0d4c2
Paul Chavard <git@paul.chavard.net>
2018-04-03 16:27:59 +0200

    Add sign and verify to etablissement

--- app/models/etablissement.rb
+++ app/models/etablissement.rb
@@ -2,6 +2,7 @@ class Etablissement < ApplicationRecord
   belongs_to :dossier
   belongs_to :entreprise, dependent: :destroy
 
+  has_one :champ, class_name: 'Champs::SiretChamp'
   has_many :exercices, dependent: :destroy
 
   accepts_nested_attributes_for :exercices
@@ -16,5 +17,20 @@ class Etablissement < ApplicationRecord
   def inline_adresse
     # squeeze needed because of space in excess in the data
     "#{numero_voie} #{type_voie} #{nom_voie}, #{complement_adresse}, #{code_postal} #{localite}".squeeze(' ')
+  end
+  def verify
+    SignatureService.verify(signature, message_for_signature)
+  end
+
+  def sign
+    SignatureService.sign(message_for_signature)
+  end
+
+  attr_accessor :signature
+
+  def message_for_signature
+    JSON.pretty_generate(as_json(include: {
+      exercices: { only: [:ca, :date_fin_exercice, :date_fin_exercice_timestamp] }
+    }).delete_if { |k,v| v.blank? })
   end
 end
