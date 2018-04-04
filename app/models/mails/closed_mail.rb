SocialGouv/tps

[#1563] Include attestation link in default closed mail when it makes sense

app/models/mails/closed_mail.rb
@@ -5,9 +5,17 @@ module Mails
     belongs_to :procedure
 
     SLUG = "closed_mail"
-    TEMPLATE_NAME = "mails/closed_mail"
     DISPLAYED_NAME = "Accusé d'acceptation"
     DEFAULT_SUBJECT = 'Votre dossier demarches-simplifiees.fr nº --numéro du dossier-- a été accepté'
     DOSSIER_STATE = 'accepte'
+
+    def self.template_name_for_procedure(procedure)
+      template = procedure.attestation_template
+      if template.present? && template.activated?
+        "mails/closed_mail_with_attestation"
+      else
+        "mails/closed_mail"
+      end
+    end
   end
 end

demarches-simplifiees/demarches-simplifiees.fr

app/models/mails/closed_mail.rb
@@ -5,9 +5,17 @@ module Mails
     belongs_to :procedure
 
     SLUG = "closed_mail"
-    DEFAULT_TEMPLATE_NAME = "mails/closed_mail"
     DISPLAYED_NAME = "Accusé d'acceptation"
     DEFAULT_SUBJECT = 'Votre dossier demarches-simplifiees.fr nº --numéro du dossier-- a été accepté'
     DOSSIER_STATE = 'accepte'
+
+    def self.default_template_name_for_procedure(procedure)
+      attestation_template = procedure.attestation_template
+      if attestation_template.present? && attestation_template.activated?
+        "mails/closed_mail_with_attestation"
+      else
+        "mails/closed_mail"
+      end
+    end
   end
 end