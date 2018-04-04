3511399d22b02319e728885181b7d3f76c99cddb
Frederic Merizen <frederic.merizen@gmail.com>
2018-04-04 09:40:55 +0200

    [#1563] Clarify constant’s role

--- app/models/mails/received_mail.rb
+++ app/models/mails/received_mail.rb
@@ -5,7 +5,7 @@ module Mails
     belongs_to :procedure
 
     SLUG = "received_mail"
-    TEMPLATE_NAME = "mails/received_mail"
+    DEFAULT_TEMPLATE_NAME = "mails/received_mail"
     DISPLAYED_NAME = 'Accusé de passage en instruction'
     DEFAULT_SUBJECT = 'Votre dossier demarches-simplifiees.fr nº --numéro du dossier-- va être instruit'
     DOSSIER_STATE = 'en_instruction'
