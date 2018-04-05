408feaa52e373cdf1f3c317eea5cd0708a0aeea1
Paul Chavard <git@paul.chavard.net>
2018-04-05 18:54:33 +0200

    Display etablissement information for siret champ

--- app/models/etablissement.rb
+++ app/models/etablissement.rb
@@ -36,6 +36,31 @@ class Etablissement < ApplicationRecord
 
   attr_accessor :signature
 
+  def entreprise_raison_sociale_or_name
+    entreprise_raison_sociale.presence || "#{entreprise_nom} #{entreprise_prenom}"
+  end
+
+  def entreprise_effectif
+    {
+      'NN' => "Unités non employeuses (pas de salarié au cours de l'année de référence et pas d'effectif au 31/12).",
+      '00' => "0 salarié (n'ayant pas d'effectif au 31/12 mais ayant employé des salariés au cours de l'année de référence)",
+      '01' => '1 ou 2 salariés',
+      '02' => '3 à 5 salariés',
+      '03' => '6 à 9 salariés',
+      '11' => '10 à 19 salariés',
+      '12' => '20 à 49 salariés',
+      '21' => '50 à 99 salariés',
+      '22' => '100 à 199 salariés',
+      '31' => '200 à 249 salariés',
+      '32' => '250 à 499 salariés',
+      '41' => '500 à 999 salariés',
+      '42' => '1 000 à 1 999 salariés',
+      '51' => '2 000 à 4 999 salariés',
+      '52' => '5 000 à 9 999 salariés',
+      '53' => '10 000 salariés et plus'
+    }[entreprise_code_effectif_entreprise]
+  end
+
   private
 
   def validate_signature
