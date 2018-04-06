cba6ad4ff2f26bb7f590c822a37e2ec0545db915
kemar <marc@marcarea.com>
2018-04-06 13:09:37 +0200

    [Fix #1494] Display a warning when accepting a dossier if needed
    Show the accompagnateur the unspecified demande
    champs and attestations privées needed for the
    attestation

--- app/models/dossier.rb
+++ app/models/dossier.rb
@@ -283,6 +283,16 @@ class Dossier < ApplicationRecord
     end
   end
 
+  def unspecified_attestation_champs
+    attestation_template = procedure.attestation_template
+
+    if attestation_template.present? && attestation_template.activated?
+      attestation_template.unspecified_champs_for_dossier(self)
+    else
+      []
+    end
+  end
+
   def build_attestation
     if procedure.attestation_template.present? && procedure.attestation_template.activated?
       procedure.attestation_template.attestation_for(self)
