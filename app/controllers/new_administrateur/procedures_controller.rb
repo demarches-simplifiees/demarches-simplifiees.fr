90e0b6dc4fbafe4aa2e20bb8262c20c266f1ddb1
simon lehericey <simon.lehericey@gmail.com>
2018-04-13 17:29:34 +0200

    Procedure Preview: add annotations preview

--- app/controllers/new_administrateur/procedures_controller.rb
+++ app/controllers/new_administrateur/procedures_controller.rb
@@ -2,9 +2,14 @@ module NewAdministrateur
   class ProceduresController < AdministrateurController
     def apercu
       @dossier = procedure_without_control.new_dossier
+      @tab = apercu_tab
     end
 
     private
+
+    def apercu_tab
+      params[:tab] || 'dossier'
+    end
 
     def procedure_without_control
       Procedure.find(params[:id])
