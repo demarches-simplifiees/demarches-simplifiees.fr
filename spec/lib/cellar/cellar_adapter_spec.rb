729bbb4e3e530367282709d8e9b8486a081a2e2d
Frederic Merizen <frederic.merizen@gmail.com>
2018-04-24 13:35:36 +0200

    Cosmetic: nicer indentations for multiline strings

--- spec/lib/cellar/cellar_adapter_spec.rb
+++ spec/lib/cellar/cellar_adapter_spec.rb
@@ -6,28 +6,30 @@ describe 'CellarAdapter' do
 
   describe 'parse_bucket_listing' do
     let(:response) do
-      '<ListBucketResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
-         <Name>example-bucket</Name>
-         <Prefix></Prefix>
-         <KeyCount>2</KeyCount>
-         <MaxKeys>1000</MaxKeys>
-         <Delimiter>/</Delimiter>
-         <IsTruncated>false</IsTruncated>
-         <Contents>
-           <Key>sample1.jpg</Key>
-           <LastModified>2011-02-26T01:56:20.000Z</LastModified>
-           <ETag>&quot;bf1d737a4d46a19f3bced6905cc8b902&quot;</ETag>
-           <Size>142863</Size>
-           <StorageClass>STANDARD</StorageClass>
-         </Contents>
-         <Contents>
-           <Key>sample2.jpg</Key>
-           <LastModified>2011-02-26T01:56:20.000Z</LastModified>
-           <ETag>&quot;bf1d737a4d46a19f3bced6905cc8b902&quot;</ETag>
-           <Size>142863</Size>
-           <StorageClass>STANDARD</StorageClass>
-         </Contents>
-       </ListBucketResult>'
+      <<~EOS
+        <ListBucketResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+          <Name>example-bucket</Name>
+          <Prefix></Prefix>
+          <KeyCount>2</KeyCount>
+          <MaxKeys>1000</MaxKeys>
+          <Delimiter>/</Delimiter>
+          <IsTruncated>false</IsTruncated>
+          <Contents>
+            <Key>sample1.jpg</Key>
+            <LastModified>2011-02-26T01:56:20.000Z</LastModified>
+            <ETag>&quot;bf1d737a4d46a19f3bced6905cc8b902&quot;</ETag>
+            <Size>142863</Size>
+            <StorageClass>STANDARD</StorageClass>
+          </Contents>
+          <Contents>
+            <Key>sample2.jpg</Key>
+            <LastModified>2011-02-26T01:56:20.000Z</LastModified>
+            <ETag>&quot;bf1d737a4d46a19f3bced6905cc8b902&quot;</ETag>
+            <Size>142863</Size>
+            <StorageClass>STANDARD</StorageClass>
+          </Contents>
+        </ListBucketResult>'
+      EOS
     end
 
     subject { session.send(:parse_bucket_listing, response) }
@@ -37,16 +39,17 @@ describe 'CellarAdapter' do
 
   describe 'bulk_deletion_request_body' do
     let(:expected_response) do
-      '<?xml version="1.0" encoding="UTF-8"?>
-<Delete>
-  <Object>
-    <Key>chapi</Key>
-  </Object>
-  <Object>
-    <Key>chapo</Key>
-  </Object>
-</Delete>
-'
+      <<~EOS
+        <?xml version="1.0" encoding="UTF-8"?>
+        <Delete>
+          <Object>
+            <Key>chapi</Key>
+          </Object>
+          <Object>
+            <Key>chapo</Key>
+          </Object>
+        </Delete>
+      EOS
     end
 
     subject { session.send(:bulk_deletion_request_body, ['chapi', 'chapo']) }
