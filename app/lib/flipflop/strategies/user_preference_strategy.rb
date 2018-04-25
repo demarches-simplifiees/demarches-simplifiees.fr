fa3f848d0b0e37c9b2977712fc7c387a29bd1f31
Paul Chavard <git@paul.chavard.net>
2018-04-25 16:41:48 +0200

    Remove legacy features support

--- app/lib/flipflop/strategies/user_preference_strategy.rb
+++ app/lib/flipflop/strategies/user_preference_strategy.rb
@@ -11,28 +11,11 @@ module Flipflop::Strategies
     def enabled?(feature)
       # Can only check features if we have the user's session.
       if request?
-        legacy_enabled?(feature) || find_current_administrateur&.feature_enabled?(feature)
+        find_current_administrateur&.feature_enabled?(feature)
       end
     end
 
     private
-
-    def legacy_enabled?(feature)
-      if self.class.legacy_features_map.present?
-        ids = self.class.legacy_features_map["#{feature}_allowed_for_admin_ids"]
-        ids.present? && find_current_administrateur&.id&.in?(ids)
-      end
-    end
-
-    LEGACY_CONFIG_FILE = Rails.root.join("config", "initializers", "features.yml")
-
-    def self.legacy_features_map
-      @@legacy_features_map = begin
-        if File.exist?(LEGACY_CONFIG_FILE)
-          YAML.load_file(LEGACY_CONFIG_FILE)
-        end
-      end
-    end
 
     def find_current_administrateur
       if request.session["warden.user.administrateur.key"]
