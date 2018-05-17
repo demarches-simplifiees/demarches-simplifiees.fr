d4efc2c161a7041af8eab732c5133ecf71035a5d
Paul Chavard <git@paul.chavard.net>
2018-05-17 15:31:45 +0200

    Add test_procedure to procedure_path

--- app/models/procedure_path.rb
+++ app/models/procedure_path.rb
@@ -3,6 +3,7 @@ class ProcedurePath < ApplicationRecord
   validates :administrateur_id, presence: true, allow_blank: false, allow_nil: false
   validates :procedure_id, presence: true, allow_blank: false, allow_nil: false
 
+  belongs_to :test_procedure, class_name: 'Procedure'
   belongs_to :procedure
   belongs_to :administrateur
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_15_135415) do
+ActiveRecord::Schema.define(version: 2018_05_16_155238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -425,7 +425,9 @@ ActiveRecord::Schema.define(version: 2018_05_15_135415) do
     t.integer "administrateur_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.bigint "test_procedure_id"
     t.index ["path"], name: "index_procedure_paths_on_path"
+    t.index ["test_procedure_id"], name: "index_procedure_paths_on_test_procedure_id"
   end
 
   create_table "procedure_presentations", id: :serial, force: :cascade do |t|
