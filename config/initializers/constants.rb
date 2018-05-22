0c9709afd42369bcb64e765d107feb4be69b1dcb
gregoirenovel <log@kinko.fr>
2018-05-22 17:18:33 +0200

    Update FAQ_URL

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -3,4 +3,4 @@ LISTE_DES_DEMARCHES_URL = [DOC_URL, "listes-des-demarches"].join("/")
 CGU_URL = [DOC_URL, "cgu"].join("/")
 MENTIONS_LEGALES_URL = [CGU_URL, "4.-mentions-legales"].join("#")
 
-FAQ_URL = "http://demarches-simplifiees.helpscoutdocs.com/"
+FAQ_URL = "https://faq.demarches-simplifiees.fr"
