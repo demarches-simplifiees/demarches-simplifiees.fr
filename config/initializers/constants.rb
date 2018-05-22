7adfa0fe5a8c57b287cd70ee2f3b2c5a7d6b449b
gregoirenovel <log@kinko.fr>
2018-05-22 17:12:51 +0200

    Add LISTE_DES_DEMARCHES_URL

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,2 +1,3 @@
 DOC_URL = "https://demarches-simplifiees.gitbook.io/demarches-simplifiees/"
+LISTE_DES_DEMARCHES_URL = "https://demarches-simplifiees.gitbook.io/demarches-simplifiees/listes-des-demarches"
 CGU_URL = "https://demarches-simplifiees.gitbook.io/demarches-simplifiees/cgu"
