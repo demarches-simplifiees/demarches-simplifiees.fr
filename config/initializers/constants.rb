49d9ba4a800b2130520e4541836e8ba443834284
gregoirenovel <log@kinko.fr>
2018-05-22 17:16:09 +0200

    Remove trailing slash from DOC_URL

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,4 +1,4 @@
-DOC_URL = "https://demarches-simplifiees.gitbook.io/demarches-simplifiees/"
+DOC_URL = "https://demarches-simplifiees.gitbook.io/demarches-simplifiees"
 LISTE_DES_DEMARCHES_URL = "https://demarches-simplifiees.gitbook.io/demarches-simplifiees/listes-des-demarches"
 CGU_URL = "https://demarches-simplifiees.gitbook.io/demarches-simplifiees/cgu"
 MENTIONS_LEGALES_URL = "https://demarches-simplifiees.gitbook.io/demarches-simplifiees/cgu#4.-mentions-legales"
