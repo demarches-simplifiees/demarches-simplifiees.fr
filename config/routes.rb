905c6e27c7ca52cf923a86b2cccf6baf545f9f0f
simon lehericey <simon.lehericey@gmail.com>
2018-05-22 17:11:48 +0200

    Devise: move after_sign_up to after_confirmation

--- config/routes.rb
+++ config/routes.rb
@@ -43,7 +43,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations',
-    passwords: 'users/passwords'
+    passwords: 'users/passwords',
+    confirmations: 'users/confirmations'
   }
 
   devise_scope :user do
