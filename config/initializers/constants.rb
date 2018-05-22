4f5d8987550c1f43e51d0b0fd5fca114466b5cfc
gregoirenovel <log@kinko.fr>
2018-05-22 17:35:11 +0200

    Move constants to urls.rb

--- config/initializers/constants.rb
+++
@@ -1,6 +0,0 @@
-DOC_URL = "https://doc.demarches-simplifiees.fr"
-LISTE_DES_DEMARCHES_URL = [DOC_URL, "listes-des-demarches"].join("/")
-CGU_URL = [DOC_URL, "cgu"].join("/")
-MENTIONS_LEGALES_URL = [CGU_URL, "4-mentions-legales"].join("#")
-
-FAQ_URL = "https://faq.demarches-simplifiees.fr"
