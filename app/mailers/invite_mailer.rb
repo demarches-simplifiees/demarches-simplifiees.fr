69e95f62c839ad6ccd3c236d7a97b8d08e1b3615
gregoirenovel <log@kinko.fr>
2018-05-31 15:52:50 +0200

    Remove useless methods

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -1,25 +1,22 @@
 class InviteMailer < ApplicationMailer
   def invite_user(invite)
-    vars_mailer(invite)
     subject = "Participez à l'élaboration d'un dossier"
 
-    send_mail(invite.email, subject, invite.email_sender) if invite.user.present?
+    send_mail(invite, subject, invite.email_sender) if invite.user.present?
   end
 
   def invite_guest(invite)
-    vars_mailer(invite)
     subject = "#{invite.email_sender} vous invite à consulter un dossier"
 
-    send_mail(invite.email, subject, invite.email_sender)
+    send_mail(invite, subject, invite.email_sender)
   end
 
   private
 
-  def vars_mailer(invite)
+  def send_mail(invite, subject, reply_to)
     @invite = invite
-  end
+    email = invite.email
 
-  def send_mail(email, subject, reply_to)
     mail(to: email,
          subject: subject,
          reply_to: reply_to)
