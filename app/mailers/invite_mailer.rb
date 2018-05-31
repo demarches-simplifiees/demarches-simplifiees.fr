8782bdd9615aff867e3ebd369afa5b18eb9199e0
gregoirenovel <log@kinko.fr>
2018-05-31 15:53:04 +0200

    Do one thing per line

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -2,7 +2,9 @@ class InviteMailer < ApplicationMailer
   def invite_user(invite)
     subject = "Participez à l'élaboration d'un dossier"
 
-    send_mail(invite, subject, invite.email_sender) if invite.user.present?
+    if invite.user.present?
+      send_mail(invite, subject, invite.email_sender)
+    end
   end
 
   def invite_guest(invite)
