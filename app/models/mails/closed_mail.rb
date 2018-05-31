5c82b2cf20d26e5f6c762bb168bdf82c1b6741ee
gregoirenovel <log@kinko.fr>
2018-05-31 16:03:06 +0200

    [Fix #886] Move mail templates from /mails to /notification_mailers

--- app/models/mails/closed_mail.rb
+++ app/models/mails/closed_mail.rb
@@ -12,9 +12,9 @@ module Mails
     def self.default_template_name_for_procedure(procedure)
       attestation_template = procedure.attestation_template
       if attestation_template&.activated?
-        "mails/closed_mail_with_attestation"
+        "notification_mailer/closed_mail_with_attestation"
       else
-        "mails/closed_mail"
+        "notification_mailer/closed_mail"
       end
     end
   end
