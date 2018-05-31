0e690c90ccb977e0d445ec60a35d1b7dce1db556
simon lehericey <simon.lehericey@gmail.com>
2018-05-31 11:00:22 +0200

    Procedure: clone the deliberation

--- spec/controllers/admin/procedures_controller_spec.rb
+++ spec/controllers/admin/procedures_controller_spec.rb
@@ -473,7 +473,7 @@ describe Admin::ProceduresController, type: :controller do
   end
 
   describe 'PUT #clone' do
-    let!(:procedure) { create(:procedure, :with_notice, administrateur: admin) }
+    let!(:procedure) { create(:procedure, :with_notice, :with_deliberation, administrateur: admin) }
     let(:params) { { procedure_id: procedure.id } }
     subject { put :clone, params: params }
 
@@ -491,6 +491,7 @@ describe Admin::ProceduresController, type: :controller do
         expect(response).to redirect_to edit_admin_procedure_path(id: Procedure.last.id)
         expect(Procedure.last.cloned_from_library).to be_falsey
         expect(Procedure.last.notice.attached?).to be_truthy
+        expect(Procedure.last.deliberation.attached?).to be_truthy
         expect(flash[:notice]).to have_content 'Procédure clonée'
       end
 
