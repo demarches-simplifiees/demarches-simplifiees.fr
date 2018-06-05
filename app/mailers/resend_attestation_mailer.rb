40f33aacc81e6b383988d07e6394f50d51c7e5b3
gregoirenovel <log@kinko.fr>
2018-06-05 17:00:22 +0200

    Resend attestation for a procedure whose attestation template changed

---
+++ app/mailers/resend_attestation_mailer.rb
@@ -0,0 +1,29 @@
+module Mailers
+  class ResendAttestationMailer < ApplicationMailer
+    include Rails.application.routes.url_helpers
+
+    def resend_attestation(dossier)
+      to = dossier.user.email
+      subject = "Nouvelle attestation pour votre dossier nº #{dossier.id}"
+
+      mail(to: to, subject: subject, body: body(dossier))
+    end
+
+    private
+
+    def body(dossier)
+      <<~HEREDOC
+        Bonjour,
+
+        L'attestation de votre dossier nº #{dossier.id} (procédure "#{dossier.procedure.libelle}") a été modifiée.
+
+        Votre nouvelle attestation est disponible à l'adresse suivante :
+        #{dossier_attestation_url(dossier)}
+
+        Cordialement,
+
+        L’équipe demarches-simplifiees.fr
+      HEREDOC
+    end
+  end
+end
