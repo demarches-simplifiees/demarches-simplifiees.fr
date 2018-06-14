bb94c7d2080bbff48eb1600d284c94a8a7efb4a3
Frederic Merizen <frederic.merizen@gmail.com>
2018-06-14 17:41:31 +0200

    [#1421] Move type-specific before_save to typed champs

--- app/models/champs/date_champ.rb
+++ app/models/champs/date_champ.rb
@@ -1,2 +1,14 @@
 class Champs::DateChamp < Champ
+  before_save :format_before_save
+
+  private
+
+  def format_before_save
+    self.value =
+      begin
+        Date.parse(value).iso8601
+      rescue
+        nil
+      end
+  end
 end
--- app/models/champs/multiple_drop_down_list_champ.rb
+++ app/models/champs/multiple_drop_down_list_champ.rb
@@ -1,2 +1,17 @@
 class Champs::MultipleDropDownListChamp < Champ
+  before_save :format_before_save
+
+  private
+
+  def format_before_save
+    if value.present?
+      json = JSON.parse(value)
+      if json == ['']
+        self.value = nil
+      else
+        json = json - ['']
+        self.value = json.to_s
+      end
+    end
+  end
 end
