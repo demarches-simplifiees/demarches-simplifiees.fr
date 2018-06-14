d3df6e813c552be2fc02dbedf068e678e4f4ea1d
Frederic Merizen <frederic.merizen@gmail.com>
2018-06-14 15:22:47 +0200

    [#1421] DRY up type de champ toggles

--- app/helpers/type_de_champ_helper.rb
+++ app/helpers/type_de_champ_helper.rb
@@ -1,13 +1,15 @@
 module TypeDeChampHelper
+  TOGGLES = {
+    'piece_justificative' => :champ_pj?,
+    'siret' => :champ_siret?
+  }
+
   def tdc_options
     tdcs = TypeDeChamp.type_de_champs_list_fr
 
-    if !Flipflop.champ_pj?
-      tdcs.reject! { |tdc| tdc.last == "piece_justificative" }
-    end
-
-    if !Flipflop.champ_siret?
-      tdcs.reject! { |tdc| tdc.last == "siret" }
+    tdcs.select! do |tdc|
+      toggle = TOGGLES[tdc.last]
+      toggle.blank? || Flipflop.send(toggle)
     end
 
     tdcs
