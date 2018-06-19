c14725bb3c0333d1750f5fdf2cfb97163bb91d43
Pierre de La Morinerie <kemenaran@gmail.com>
2018-06-19 14:23:43 +0000

    dossier: display user infos on edit page

---
+++ spec/views/new_user/dossiers/identite.html.haml_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe 'new_user/dossiers/identite.html.haml', type: :view do
+  let(:dossier) { create(:dossier, :with_entreprise, :with_service, state: 'brouillon', procedure: create(:procedure, :with_api_carto, :with_two_type_de_piece_justificative, for_individual: true)) }
+  let(:footer) { view.content_for(:footer) }
+
+  before do
+    sign_in dossier.user
+    assign(:dossier, dossier)
+  end
+
+  context 'test de composition de la page' do
+    before do
+      render
+    end
+
+    it 'affiche les informations de la procédure' do
+      expect(rendered).to have_text(dossier.procedure.libelle)
+    end
+
+    it 'prépare le footer' do
+      expect(footer).to have_selector('footer')
+    end
+  end
+end
