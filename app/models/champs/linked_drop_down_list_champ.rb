2ded34af4484ee5d7b0dd32e37174a8e16c5f133
Frederic Merizen <frederic.merizen@gmail.com>
2018-06-21 09:50:34 +0200

    [#1421] Extract values for both drop down lists

--- app/models/champs/linked_drop_down_list_champ.rb
+++ app/models/champs/linked_drop_down_list_champ.rb
@@ -1,3 +1,32 @@
 class Champs::LinkedDropDownListChamp < Champ
+  attr_reader :master_value, :slave_value
   delegate :master_options, :slave_options, to: :type_de_champ
+
+  after_initialize :unpack_value
+
+  def unpack_value
+    if value.present?
+      master, slave = JSON.parse(value)
+    else
+      master = slave = ''
+    end
+    @master_value ||= master
+    @slave_value ||= slave
+  end
+
+  def master_value=(value)
+    @master_value = value
+    pack_value
+  end
+
+  def slave_value=(value)
+    @slave_value = value
+    pack_value
+  end
+
+  private
+
+  def pack_value
+    self.value = JSON.generate([ master_value, slave_value ])
+  end
 end
