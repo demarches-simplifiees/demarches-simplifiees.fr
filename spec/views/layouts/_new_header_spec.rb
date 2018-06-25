51e256e1229b82225d20f08b12d18507c4f89b43
Pierre de La Morinerie <kemenaran@gmail.com>
2018-06-25 11:26:37 +0200

    header: fix link to the contact email

--- spec/views/layouts/_new_header_spec.rb
+++ spec/views/layouts/_new_header_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe 'layouts/_new_header.html.haml', type: :view do
   describe 'logo link' do
     before do
+      sign_in user
       allow(controller).to receive(:nav_bar_profile).and_return(profile)
       render
     end
@@ -10,15 +11,22 @@ describe 'layouts/_new_header.html.haml', type: :view do
     subject { rendered }
 
     context 'when rendering for user' do
+      let(:user) { create(:user) }
       let(:profile) { :user }
 
       it { is_expected.to have_css("a.header-logo[href=\"#{users_dossiers_path}\"]") }
     end
 
     context 'when rendering for gestionnaire' do
+      let(:user) { create(:gestionnaire) }
       let(:profile) { :gestionnaire }
 
       it { is_expected.to have_css("a.header-logo[href=\"#{gestionnaire_procedures_path}\"]") }
+
+      it "displays the contact infos" do
+        expect(rendered).to have_text("Contact")
+        expect(rendered).to have_link(CONTACT_EMAIL, href: "mailto:#{CONTACT_EMAIL}")
+      end
     end
   end
 end
