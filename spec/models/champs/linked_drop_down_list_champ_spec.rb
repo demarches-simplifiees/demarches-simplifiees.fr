04892f6c55cbb88cf8e1fd6e39b0d9737b3d6de1
Frederic Merizen <frederic.merizen@gmail.com>
2018-06-28 08:36:18 +0000

    [#1421] Format value for display

--- spec/models/champs/linked_drop_down_list_champ_spec.rb
+++ spec/models/champs/linked_drop_down_list_champ_spec.rb
@@ -15,4 +15,29 @@ describe Champs::LinkedDropDownListChamp do
 
     it { expect(champ.value).to eq('["tata","tutu"]') }
   end
+
+  describe '#for_display' do
+    let(:champ) { described_class.new(primary_value: primary_value, secondary_value: secondary_value) }
+    let(:primary_value) { nil }
+    let(:secondary_value) { nil }
+
+    subject { champ.for_display }
+
+    context 'with no value' do
+      it { is_expected.to eq('') }
+    end
+
+    context 'with primary value' do
+      let(:primary_value) { 'primary' }
+
+      it { is_expected.to eq('primary') }
+    end
+
+    context 'with secondary value' do
+      let(:primary_value) { 'primary' }
+      let(:secondary_value) { 'secondary' }
+
+      it { is_expected.to eq('primary / secondary') }
+    end
+  end
 end
