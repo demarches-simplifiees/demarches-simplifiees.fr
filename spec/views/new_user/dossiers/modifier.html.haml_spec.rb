02829949908ae459d5f52a0ef2b7e107d302a244
Pierre de La Morinerie <kemenaran@gmail.com>
2018-07-11 17:11:44 +0200

    form: move the procedure notice link to a button

--- spec/views/new_user/dossiers/modifier.html.haml_spec.rb
+++ spec/views/new_user/dossiers/modifier.html.haml_spec.rb
@@ -16,11 +16,20 @@ describe 'new_user/dossiers/modifier.html.haml', type: :view do
     expect(rendered).to have_text(dossier.procedure.libelle)
   end
 
+  it 'affiche un lien vers la notice' do
+    expect(rendered).to have_link("Guide de la démarche", href: url_for(procedure.notice))
+  end
+
   it 'affiche les boutons de validation' do
     expect(rendered).to have_selector('.send-wrapper')
   end
 
   it 'prépare le footer' do
     expect(footer).to have_selector('footer')
+  end
+
+  context 'quand la procédure ne comporte pas de notice' do
+    let(:procedure) { create(:procedure) }
+    it { is_expected.not_to have_link("Guide de la démarche") }
   end
 end
