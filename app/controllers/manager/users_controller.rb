7faa363d59dee75d30442cfc92669cd784a34fdc
Pierre de La Morinerie <kemenaran@gmail.com>
2018-07-17 12:10:56 +0200

    manager: add confirmation email actions for the User
    - Allow to re-send the confirmation email
    - Allow to confirm the email address manually

--- app/controllers/manager/users_controller.rb
+++ app/controllers/manager/users_controller.rb
@@ -1,4 +1,17 @@
 module Manager
   class UsersController < Manager::ApplicationController
+    def resend_confirmation_instructions
+      user = User.find(params[:id])
+      user.resend_confirmation_instructions
+      flash[:notice] = "Le message de confirmation de l’adresse email a été renvoyé."
+      redirect_to manager_user_path(user)
+    end
+
+    def confirm
+      user = User.find(params[:id])
+      user.confirm
+      flash[:notice] = "L’adresse email de l’utilisateur a été marquée comme confirmée."
+      redirect_to manager_user_path(user)
+    end
   end
 end
