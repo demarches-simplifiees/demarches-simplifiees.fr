872e07b5e663bbe4ea1c90992c1471f10a54dffd
Paul Chavard <git@paul.chavard.net>
2018-08-01 11:22:12 +0200

    Fix global jQuery usage

---
+++ app/javascript/manager/fields/features.js
@@ -0,0 +1,19 @@
+// Administrate injects its own copy of jQuery, and it is the one
+// configured by rails to send csrf-token
+const $ = window.$;
+
+$(document).on('change', '#features input[type=checkbox]', ({ target }) => {
+  target = $(target);
+  const url = target.data('url');
+  const key = target.data('key');
+  const value = target.prop('checked');
+
+  $.ajax(url, {
+    method: 'put',
+    contentType: 'application/json',
+    dataType: 'json',
+    data: JSON.stringify({
+      features: { [key]: value }
+    })
+  });
+});
