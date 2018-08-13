40d0986a8242f182059f45515e3e9e8df94ca3c5
Pierre de La Morinerie <kemenaran@gmail.com>
2018-08-13 15:14:45 +0000

    javascript: transpile activestorage files (instead of copying them)

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
 const { environment } = require('@rails/webpacker')
 
+// By default don't transpile JS files in ./node_modules – except for some specific modules.
+const babelLoader = environment.loaders.get('babel');
+babelLoader.exclude = function(modulePath) {
+  let forcedModules = [
+    'activestorage' // ActiveStorage uses 'class', which is not supported by IE 11 and older Safari version
+  ];
+  return modulePath.includes('node_modules')
+    && forcedModules.every(forcedModule => !modulePath.includes('node_modules/' + forcedModule));
+}
+
 module.exports = environment
