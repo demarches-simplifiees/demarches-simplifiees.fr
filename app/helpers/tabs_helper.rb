d020b683dab42723b889de0e0779eb8508cb080b
Pierre de La Morinerie <kemenaran@gmail.com>
2018-08-16 14:30:55 +0000

    views: add `tab_item` and `active_tab_item` helpers

---
+++ app/helpers/tabs_helper.rb
@@ -0,0 +1,15 @@
+module TabsHelper
+  def tab_item(label, url, active: false, badge: nil, notification: false)
+    render partial: 'shared/tab_item', locals: {
+      label: label,
+      url: url,
+      active: active,
+      badge: badge,
+      notification: notification
+    }
+  end
+
+  def active_tab_item(label, url, badge: nil, notification: false)
+    tab_item(label, url, active: current_page?(url), badge: badge, notification: notification)
+  end
+end
