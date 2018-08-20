ee43650c32986d253cc059937a37a27810d124f8
simon lehericey <simon.lehericey@gmail.com>
2018-08-20 17:20:51 +0200

    Demande: add nb_of_procedure, deadline and nb_of_dossier fields

--- app/lib/pipedrive/deal_adapter.rb
+++ app/lib/pipedrive/deal_adapter.rb
@@ -10,6 +10,20 @@ class Pipedrive::DealAdapter
   PIPEDRIVE_LOST_STATUS = "lost"
   PIPEDRIVE_LOST_REASON = "refusé depuis DS"
 
+  PIPEDRIVE_NB_OF_PROCEDURES_ATTRIBUTE_ID = "b22f8710352a7fb548623c062bf82ed6d1b6b704"
+  PIPEDRIVE_NB_OF_PROCEDURES_DO_NOT_KNOW_VALUE = "Je ne sais pas"
+  PIPEDRIVE_NB_OF_PROCEDURES_1_VALUE = "juste 1"
+  PIPEDRIVE_NB_OF_PROCEDURES_1_TO_10_VALUE = "de 1 a 10"
+  PIPEDRIVE_NB_OF_PROCEDURES_10_TO_100_VALUE = "de 10 a 100"
+  PIPEDRIVE_NB_OF_PROCEDURES_ABOVE_100_VALUE = "Plus de 100"
+
+  PIPEDRIVE_DEADLINE_ATTRIBUTE_ID = "36a72c82af9d9f0d476b041ede8876844a249bf2"
+  PIPEDRIVE_DEADLINE_ASAP_VALUE = "Le plus vite possible"
+  PIPEDRIVE_DEADLINE_NEXT_3_MONTHS_VALUE = "Dans les 3 prochain mois"
+  PIPEDRIVE_DEADLINE_NEXT_6_MONTHS_VALUE = "Dans les 6 prochain mois"
+  PIPEDRIVE_DEADLINE_NEXT_12_MONTHS_VALUE = "Dans les 12 prochain mois"
+  PIPEDRIVE_DEADLINE_NO_DATE_VALUE = "Pas de date"
+
   def self.refuse_deal(deal_id, owner_id)
     params = {
       user_id: owner_id,
@@ -32,12 +46,15 @@ class Pipedrive::DealAdapter
     Pipedrive::API.put_deal(deal_id, params)
   end
 
-  def self.add_deal(organisation_id, person_id, title)
+  def self.add_deal(organisation_id, person_id, title, nb_of_procedures, nb_of_dossiers, deadline)
     params = {
       org_id: organisation_id,
       person_id: person_id,
       title: title,
-      user_id: Pipedrive::PersonAdapter::PIPEDRIVE_ROBOT_ID
+      user_id: Pipedrive::PersonAdapter::PIPEDRIVE_ROBOT_ID,
+      "#{PIPEDRIVE_NB_OF_PROCEDURES_ATTRIBUTE_ID}": nb_of_procedures,
+      value: nb_of_dossiers,
+      "#{PIPEDRIVE_DEADLINE_ATTRIBUTE_ID}": deadline,
     }
 
     Pipedrive::API.post_deal(params)
