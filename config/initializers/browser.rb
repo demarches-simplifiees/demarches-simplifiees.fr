fad18d617324e814e54b4ded6654e647d83a6574
gregoirenovel <log@kinko.fr>
2018-08-21 18:21:14 +0200

    Update the minimum browsers versions

--- config/initializers/browser.rb
+++ config/initializers/browser.rb
@@ -1,8 +1,8 @@
 # See .babelrc
 Browser.modern_rules.clear
-Browser.modern_rules << -> b { b.chrome? && b.version.to_i >= 40 }
+Browser.modern_rules << -> b { b.chrome? && b.version.to_i >= 50 }
 Browser.modern_rules << -> b { b.ie?([">=11"]) }
 Browser.modern_rules << -> b { b.edge? }
-Browser.modern_rules << -> b { b.firefox? && b.version.to_i >= 45 }
-Browser.modern_rules << -> b { b.opera? && b.version.to_i >= 19 }
+Browser.modern_rules << -> b { b.firefox? && b.version.to_i >= 50 }
+Browser.modern_rules << -> b { b.opera? && b.version.to_i >= 40 }
 Browser.modern_rules << -> b { b.safari? && b.version.to_i >= 8 }
