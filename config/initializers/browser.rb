9c94308209b66bc997dd41eef990f210c85afcb7
gregoirenovel <log@kinko.fr>
2018-08-21 18:30:04 +0200

    Enforce a minimum Edge version

--- config/initializers/browser.rb
+++ config/initializers/browser.rb
@@ -2,7 +2,7 @@
 Browser.modern_rules.clear
 Browser.modern_rules << -> b { b.chrome? && b.version.to_i >= 50 }
 Browser.modern_rules << -> b { b.ie? && b.version.to_i >= 11 && !b.compatibility_view? }
-Browser.modern_rules << -> b { b.edge? }
+Browser.modern_rules << -> b { b.edge? && b.version.to_i >= 14 && !b.compatibility_view? }
 Browser.modern_rules << -> b { b.firefox? && b.version.to_i >= 50 }
 Browser.modern_rules << -> b { b.opera? && b.version.to_i >= 40 }
 Browser.modern_rules << -> b { b.safari? && b.version.to_i >= 8 }
