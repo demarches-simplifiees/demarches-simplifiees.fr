1979536a6fc06a7cb51a024193b3bdb4c8fac089
gregoirenovel <log@kinko.fr>
2018-08-21 18:24:49 +0200

    Unify the syntax in browser.rb

--- config/initializers/browser.rb
+++ config/initializers/browser.rb
@@ -1,7 +1,7 @@
 # See .babelrc
 Browser.modern_rules.clear
 Browser.modern_rules << -> b { b.chrome? && b.version.to_i >= 50 }
-Browser.modern_rules << -> b { b.ie?([">=11"]) }
+Browser.modern_rules << -> b { b.ie? && b.version.to_i >= 11 && !b.compatibility_view? }
 Browser.modern_rules << -> b { b.edge? }
 Browser.modern_rules << -> b { b.firefox? && b.version.to_i >= 50 }
 Browser.modern_rules << -> b { b.opera? && b.version.to_i >= 40 }
