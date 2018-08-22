693a0dc88aeaf3d9630772b67b18d0668c080f92
gregoirenovel <log@kinko.fr>
2018-08-22 10:27:17 +0200

    Hide the user satisfaction survey for now

--- spec/views/new_user/dossiers/index.html.haml_spec.rb
+++ spec/views/new_user/dossiers/index.html.haml_spec.rb
@@ -71,17 +71,4 @@ describe 'new_user/dossiers/index.html.haml', type: :view do
       expect(rendered).to have_selector('ul.tabs li.active', count: 1)
     end
   end
-
-  context "quand le user n'a aucun feedback" do
-    it "affiche le formulaire de satisfaction" do
-      expect(rendered).to have_selector('#user-satisfaction', text: 'Que pensez-vous de ce service ?')
-    end
-  end
-
-  context "quand le user a un feedback" do
-    let(:user) { create(:user, feedbacks: [build(:feedback)]) }
-    it "n'affiche pas le formulaire de satisfaction" do
-      expect(rendered).to_not have_selector('#user-satisfaction')
-    end
-  end
 end
