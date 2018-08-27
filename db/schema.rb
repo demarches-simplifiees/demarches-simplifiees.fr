4103861f9088d5d5131ec4961edbd1d00ca595cf
gregoirenovel <log@kinko.fr>
2018-08-27 12:59:22 +0200

    Add the rating column to feedbacks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_22_162952) do
+ActiveRecord::Schema.define(version: 2018_08_27_102828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -323,6 +323,7 @@ ActiveRecord::Schema.define(version: 2018_08_22_162952) do
     t.integer "mark"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "rating"
     t.index ["user_id"], name: "index_feedbacks_on_user_id"
   end
 
