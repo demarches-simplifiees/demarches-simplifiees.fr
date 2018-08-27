9284afc6490116564cbe9f036baac60a9c43d934
gregoirenovel <log@kinko.fr>
2018-08-27 16:12:34 +0200

    Use a constant for the API Entreprise URL

--- config/initializers/urls.rb
+++ config/initializers/urls.rb
@@ -1,5 +1,7 @@
 API_CARTO_URL = "https://apicarto.sgmap.fr"
 
+API_ENTREPRISE_URL = "https://entreprise.api.gouv.fr/v2"
+
 API_GEO_URL = "https://geo.api.gouv.fr"
 
 PIPEDRIVE_API_URL = 'https://api.pipedrive.com/v1'
