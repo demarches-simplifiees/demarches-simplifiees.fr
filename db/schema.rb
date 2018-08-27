5059c12b34a3d46524a28f8a2c3619405372516d
gregoirenovel <log@kinko.fr>
2018-08-27 13:15:19 +0200

    Remove the now useless mark column on the feedbacks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_27_111328) do
+ActiveRecord::Schema.define(version: 2018_08_27_111451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -320,7 +320,6 @@ ActiveRecord::Schema.define(version: 2018_08_27_111328) do
 
   create_table "feedbacks", force: :cascade do |t|
     t.bigint "user_id"
-    t.integer "mark"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "rating", null: false
