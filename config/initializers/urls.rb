732bfd883207be742651e24b055cb47e1c33a910
gregoirenovel <log@kinko.fr>
2018-08-27 16:12:57 +0200

    Improve the formatting of the urls.rb file

--- config/initializers/urls.rb
+++ config/initializers/urls.rb
@@ -1,14 +1,12 @@
+# API URLs
 API_CARTO_URL = "https://apicarto.sgmap.fr"
-
 API_ENTREPRISE_URL = "https://entreprise.api.gouv.fr/v2"
-
 API_GEO_URL = "https://geo.api.gouv.fr"
-
 PIPEDRIVE_API_URL = 'https://api.pipedrive.com/v1'
 
+# External services URLs
 DOC_URL = "https://doc.demarches-simplifiees.fr"
 LISTE_DES_DEMARCHES_URL = [DOC_URL, "listes-des-demarches"].join("/")
 CGU_URL = [DOC_URL, "cgu"].join("/")
 MENTIONS_LEGALES_URL = [CGU_URL, "4-mentions-legales"].join("#")
-
 FAQ_URL = "https://faq.demarches-simplifiees.fr"
