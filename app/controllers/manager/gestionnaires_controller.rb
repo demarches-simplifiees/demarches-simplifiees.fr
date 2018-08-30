2c4b4f700ed5d23c2d3b9659c2b649043efd427f
gregoirenovel <log@kinko.fr>
2018-08-30 14:42:09 +0200

    Rename the last “Gestionnaire” occurence in the UI to “Instructeur”

--- app/controllers/manager/gestionnaires_controller.rb
+++ app/controllers/manager/gestionnaires_controller.rb
@@ -3,7 +3,7 @@ module Manager
     def reinvite
       gestionnaire = Gestionnaire.find(params[:id])
       gestionnaire.invite!
-      flash[:notice] = "Gestionnaire réinvité."
+      flash[:notice] = "Instructeur réinvité."
       redirect_to manager_gestionnaire_path(gestionnaire)
     end
   end
