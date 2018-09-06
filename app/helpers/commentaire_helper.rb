353ab812fca0de89b45dd824f2621c11a69c7f38
Pierre de La Morinerie <kemenaran@gmail.com>
2018-09-06 16:08:24 +0200

    messagerie: fix 'Invité' label in message sender

--- app/helpers/commentaire_helper.rb
+++ app/helpers/commentaire_helper.rb
@@ -5,6 +5,10 @@ module CommentaireHelper
     end
   end
 
+  def commentaire_is_from_guest(commentaire)
+    commentaire.dossier.invites.map(&:email).include?(commentaire.email)
+  end
+
   def commentaire_date(commentaire)
     is_current_year = (commentaire.created_at.year == Date.current.year)
     template = is_current_year ? :message_date : :message_date_with_year
