631df3197ab15cd48ac5e34b03ccde33867d7e46
Paul Chavard <git@paul.chavard.net>
2018-09-07 17:43:37 +0100

    Add uniqueness validation to procedure path

--- app/models/procedure_path.rb
+++ app/models/procedure_path.rb
@@ -1,5 +1,5 @@
 class ProcedurePath < ApplicationRecord
-  validates :path, format: { with: /\A[a-z0-9_\-]{3,50}\z/ }, presence: true, allow_blank: false, allow_nil: false
+  validates :path, format: { with: /\A[a-z0-9_\-]{3,50}\z/ }, uniqueness: true, presence: true, allow_blank: false, allow_nil: false
   validates :administrateur_id, presence: true, allow_blank: false, allow_nil: false
   validates :procedure_id, presence: true, allow_blank: false, allow_nil: false
 
