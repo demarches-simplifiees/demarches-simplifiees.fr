7cb7112cf8cba2bf33dff7b9a725299cb83520b8
gregoirenovel <log@kinko.fr>
2018-09-10 20:19:03 +0200

    FeedbacksController should inherit from UserController

--- app/controllers/new_user/feedbacks_controller.rb
+++ app/controllers/new_user/feedbacks_controller.rb
@@ -1,4 +1,4 @@
-class NewUser::FeedbacksController < ApplicationController
+class NewUser::FeedbacksController < UserController
   def create
     current_user.feedbacks.create!(rating: params[:rating])
     flash.notice = "Merci de votre retour, si vous souhaitez nous en dire plus, n'hésitez pas à #{view_context.contact_link('nous contacter', type: Helpscout::FormAdapter::TYPE_AMELIORATION)}."
