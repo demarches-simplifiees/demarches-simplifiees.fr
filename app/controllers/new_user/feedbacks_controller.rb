235ecba3fe4f29d8d3c3c0398870bcaa4578c14e
gregoirenovel <log@kinko.fr>
2018-09-10 20:20:58 +0200

    Format FeedbackController like the other controllers

--- app/controllers/new_user/feedbacks_controller.rb
+++ app/controllers/new_user/feedbacks_controller.rb
@@ -1,6 +1,8 @@
-class NewUser::FeedbacksController < UserController
-  def create
-    current_user.feedbacks.create!(rating: params[:rating])
-    flash.notice = "Merci de votre retour, si vous souhaitez nous en dire plus, n'hésitez pas à #{view_context.contact_link('nous contacter', type: Helpscout::FormAdapter::TYPE_AMELIORATION)}."
+module NewUser
+  class FeedbacksController < UserController
+    def create
+      current_user.feedbacks.create!(rating: params[:rating])
+      flash.notice = "Merci de votre retour, si vous souhaitez nous en dire plus, n'hésitez pas à #{view_context.contact_link('nous contacter', type: Helpscout::FormAdapter::TYPE_AMELIORATION)}."
+    end
   end
 end
