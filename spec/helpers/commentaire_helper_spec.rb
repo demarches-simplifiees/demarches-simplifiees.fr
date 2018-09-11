926dc3e1074100fd1d58ac0903de463689a8b7b3
Pierre de La Morinerie <kemenaran@gmail.com>
2018-09-11 18:40:10 +0200

    dossier: display latest message on summary page

--- spec/helpers/commentaire_helper_spec.rb
+++ spec/helpers/commentaire_helper_spec.rb
@@ -7,13 +7,27 @@ RSpec.describe CommentaireHelper, type: :helper do
     subject { commentaire_is_from_me_class(commentaire, me) }
 
     context "when commentaire is from me" do
-      let(:me) { "michel@pref.fr" }
+      let(:me) { User.new(email: "michel@pref.fr") }
       it { is_expected.to eq("from-me") }
     end
 
     context "when commentaire is not from me" do
-      let(:me) { "roger@usager.fr" }
+      let(:me) { User.new(email: "roger@usager.fr") }
       it { is_expected.to eq nil }
+    end
+  end
+
+  describe '.commentaire_answer_action' do
+    subject { commentaire_answer_action(commentaire, me) }
+
+    context "when commentaire is from me" do
+      let(:me) { User.new(email: "michel@pref.fr") }
+      it { is_expected.to include('Envoyer') }
+    end
+
+    context "when commentaire is not from me" do
+      let(:me) { User.new(email: "roger@usager.fr") }
+      it { is_expected.to include('Répondre') }
     end
   end
 
