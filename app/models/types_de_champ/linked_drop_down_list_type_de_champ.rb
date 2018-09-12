feb115cfdf1deaa39bee1abc74b368f7876950f9
Frederic Merizen <frederic.merizen@gmail.com>
2018-09-12 15:19:05 +0200

    [Fix #2258] Validate options for linked dropdown menus

--- app/models/types_de_champ/linked_drop_down_list_type_de_champ.rb
+++ app/models/types_de_champ/linked_drop_down_list_type_de_champ.rb
@@ -3,6 +3,8 @@ class TypesDeChamp::LinkedDropDownListTypeDeChamp < TypesDeChamp::TypeDeChampBas
 
   delegate :drop_down_list, to: :@type_de_champ
 
+  validate :check_presence_of_primary_options
+
   def primary_options
     primary_options = unpack_options.map(&:first)
     if primary_options.present?
@@ -20,6 +22,12 @@ class TypesDeChamp::LinkedDropDownListTypeDeChamp < TypesDeChamp::TypeDeChampBas
   end
 
   private
+
+  def check_presence_of_primary_options
+    if !PRIMARY_PATTERN.match?(drop_down_list.options.second)
+      errors.add(libelle, "doit commencer par une entrée de menu primaire de la forme <code style='white-space: pre-wrap;'>--texte--</code>")
+    end
+  end
 
   def unpack_options
     _, *options = drop_down_list.options
