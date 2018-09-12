998754ab739dd6e90d0e5f2a8a321c03832b7d9d
Frederic Merizen <frederic.merizen@gmail.com>
2018-09-12 15:19:05 +0200

    [Fix #2258] Validate options for linked dropdown menus

--- app/models/types_de_champ/type_de_champ_base.rb
+++ app/models/types_de_champ/type_de_champ_base.rb
@@ -1,6 +1,8 @@
 class TypesDeChamp::TypeDeChampBase
   include ActiveModel::Validations
 
+  delegate :libelle, to: :@type_de_champ
+
   def initialize(type_de_champ)
     @type_de_champ = type_de_champ
   end
