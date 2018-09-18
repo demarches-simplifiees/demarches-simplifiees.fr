1e14dcf3fa685f8e23297bbd4019351f79880863
Frederic Merizen <frederic.merizen@gmail.com>
2018-09-18 15:53:11 +0200

    [#2579] Fix injection SQL dans le filtrage instructeur

--- spec/services/dossier_field_service_spec.rb
+++ spec/services/dossier_field_service_spec.rb
@@ -2,7 +2,37 @@ require 'spec_helper'
 
 describe DossierFieldService do
   describe '#filtered_ids' do
-    let(:procedure) { create(:procedure) }
+    let(:procedure) { create(:procedure, :with_type_de_champ, :with_type_de_champ_private) }
+
+    context 'for type_de_champ table' do
+      let(:kept_dossier) { create(:dossier, procedure: procedure) }
+      let(:discarded_dossier) { create(:dossier, procedure: procedure) }
+      let(:type_de_champ) { procedure.types_de_champ.first }
+
+      before do
+        type_de_champ.champ.create(dossier: kept_dossier, value: 'keep me')
+        type_de_champ.champ.create(dossier: discarded_dossier, value: 'discard me')
+      end
+
+      subject { described_class.filtered_ids(procedure.dossiers, [{ 'table' => 'type_de_champ', 'column' => type_de_champ.id, 'value' => 'keep' }]) }
+
+      it { is_expected.to contain_exactly(kept_dossier.id) }
+    end
+
+    context 'for type_de_champ_private table' do
+      let(:kept_dossier) { create(:dossier, procedure: procedure) }
+      let(:discarded_dossier) { create(:dossier, procedure: procedure) }
+      let(:type_de_champ_private) { procedure.types_de_champ_private.first }
+
+      before do
+        type_de_champ_private.champ.create(dossier: kept_dossier, value: 'keep me')
+        type_de_champ_private.champ.create(dossier: discarded_dossier, value: 'discard me')
+      end
+
+      subject { described_class.filtered_ids(procedure.dossiers, [{ 'table' => 'type_de_champ_private', 'column' => type_de_champ_private.id, 'value' => 'keep' }]) }
+
+      it { is_expected.to contain_exactly(kept_dossier.id) }
+    end
 
     context 'for etablissement table' do
       context 'for entreprise_date_creation column' do
@@ -24,6 +54,15 @@ describe DossierFieldService do
 
         it { is_expected.to contain_exactly(kept_dossier.id) }
       end
+    end
+
+    context 'for user table' do
+      let!(:kept_dossier) { create(:dossier, procedure: procedure, user: create(:user, email: 'me@keepmail.com')) }
+      let!(:discarded_dossier) { create(:dossier, procedure: procedure, user: create(:user, email: 'me@discard.com')) }
+
+      subject { described_class.filtered_ids(procedure.dossiers, [{ 'table' => 'user', 'column' => 'email', 'value' => 'keepmail' }]) }
+
+      it { is_expected.to contain_exactly(kept_dossier.id) }
     end
   end
 end
