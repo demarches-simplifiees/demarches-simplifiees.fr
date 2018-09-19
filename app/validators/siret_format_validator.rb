491b05b80fecb4ef62204f114021da22e2302ae1
gregoirenovel <log@kinko.fr>
2018-09-19 10:28:07 +0200

    Further refactor SiretFormatValidator#validate_each

--- app/validators/siret_format_validator.rb
+++ app/validators/siret_format_validator.rb
@@ -1,15 +1,23 @@
 class SiretFormatValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    if !value&.match?(/^\d{14}$/)
+    if !format_is_valid(value)
       record.errors.add(attribute, :format)
     end
 
-    if value.present? && (luhn_checksum(value) % 10 != 0)
+    if !luhn_passed(value)
       record.errors.add(attribute, :checksum)
     end
   end
 
   private
+
+  def format_is_valid(value)
+    value&.match?(/^\d{14}$/)
+  end
+
+  def luhn_passed(value)
+    value.present? && (luhn_checksum(value) % 10 == 0)
+  end
 
   def luhn_checksum(value)
     accum = 0
