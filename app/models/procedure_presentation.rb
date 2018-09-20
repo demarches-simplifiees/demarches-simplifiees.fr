68608307ade6a38d75ff73a01b55909a19930ffe
Frederic Merizen <frederic.merizen@gmail.com>
2018-09-20 17:05:58 +0200

    [#2622] Change displayed_fields to proper JSONB

--- app/models/procedure_presentation.rb
+++ app/models/procedure_presentation.rb
@@ -1,12 +1,6 @@
 class ProcedurePresentation < ApplicationRecord
   belongs_to :assign_to
 
-  def displayed_fields
-    read_attribute(:displayed_fields).map do |field|
-      field = JSON.parse(field)
-    end
-  end
-
   def sort
     JSON.parse(read_attribute(:sort))
   end
