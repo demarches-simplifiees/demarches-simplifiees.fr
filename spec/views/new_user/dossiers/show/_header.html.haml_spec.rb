3b9d0589d07ba3a2fa4829c7387a378548b2cb8e
Pierre de La Morinerie <kemenaran@gmail.com>
2018-09-24 17:12:15 +0200

    fixup! dossier: allow inviting users on the dossier page

--- spec/views/new_user/dossiers/show/_header.html.haml_spec.rb
+++ spec/views/new_user/dossiers/show/_header.html.haml_spec.rb
@@ -5,7 +5,7 @@ describe 'new_user/dossiers/show/header.html.haml', type: :view do
     sign_in dossier.user
   end
 
-  subject! { render 'new_user/dossiers/show/header.html.haml', dossier: dossier, is_dossier_owner: true }
+  subject! { render 'new_user/dossiers/show/header.html.haml', dossier: dossier }
 
   it 'affiche les informations du dossier' do
     expect(rendered).to have_text(dossier.procedure.libelle)
