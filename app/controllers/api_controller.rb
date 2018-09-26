f7c23bb4359ba5447d8f9ef0e1ff128b74af041e
simon lehericey <simon.lehericey@gmail.com>
2018-09-26 16:24:19 +0200

    ApiController: remove ensure_token_is_present
    An admin without token answer false to admin.api_token_valid?(api_token) with api_token=*

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -7,11 +7,6 @@ class APIController < ApplicationController
     ```
   EOS
 
-  # deny request with an empty token as we do not want it
-  # to match the first admin with an empty token
-  # it should not happen as an empty token is serialized by ''
-  # and a administrateur without token has admin.api_token == nil
-  before_action :ensure_token_is_present
   before_action :default_format_json
 
   protected
@@ -22,12 +17,6 @@ class APIController < ApplicationController
 
   def default_format_json
     request.format = "json" if !request.params[:format]
-  end
-
-  def ensure_token_is_present
-    if params[:token].blank? && header_token.blank?
-      render json: {}, status: 401
-    end
   end
 
   def token
