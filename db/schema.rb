mfaure/tps

Api Token: store token in an encrypted form

db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_22_162952) do
+ActiveRecord::Schema.define(version: 2018_08_24_142849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2018_08_22_162952) do
     t.string "api_token"
     t.boolean "active", default: false
     t.jsonb "features", default: {}, null: false
+    t.string "encrypted_token"
     t.index ["email"], name: "index_administrateurs_on_email", unique: true
     t.index ["reset_password_token"], name: "index_administrateurs_on_reset_password_token", unique: true
   end

demarches-simplifiees/demarches-simplifiees.fr

db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_24_074121) do
+ActiveRecord::Schema.define(version: 2018_09_25_084403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2018_09_24_074121) do
     t.string "api_token"
     t.boolean "active", default: false
     t.jsonb "features", default: {}, null: false
+    t.string "encrypted_token"
     t.index ["email"], name: "index_administrateurs_on_email", unique: true
     t.index ["reset_password_token"], name: "index_administrateurs_on_reset_password_token", unique: true
   end