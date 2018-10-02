d2ed45b5996a25b2c1bea9b77d960ca30bf65bf4
Pierre de La Morinerie <kemenaran@gmail.com>
2018-10-02 13:47:32 +0000

    session: migrate the "token confirmation" page to the new design

--- config/routes.rb
+++ config/routes.rb
@@ -73,6 +73,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations',
+    confirmations: 'users/confirmations',
     passwords: 'users/passwords'
   }
 
