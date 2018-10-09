016b8975572fd6c58e51f78a11f3f08e1e2dbf3d
Paul Chavard <git@paul.chavard.net>
2018-10-09 12:11:13 +0200

    Add button in manager to transition démarche in brouillon state
    fix #2565

--- app/controllers/manager/procedures_controller.rb
+++ app/controllers/manager/procedures_controller.rb
@@ -6,5 +6,16 @@ module Manager
       flash[:notice] = "Démarche whitelistée."
       redirect_to manager_procedure_path(procedure)
     end
+
+    def draft
+      procedure = Procedure.find(params[:id])
+      if procedure.dossiers.empty?
+        procedure.draft!
+        flash[:notice] = "La démarche a bien été passée en brouillon."
+      else
+        flash[:alert] = "Impossible de repasser en brouillon une démarche à laquelle sont rattachés des dossiers."
+      end
+      redirect_to manager_procedure_path(procedure)
+    end
   end
 end
