b78607390b102ff104aa40e7b7dfc631d45d4799
Paul Chavard <git@paul.chavard.net>
2018-10-10 20:02:17 +0200

    Update carto tests

--- spec/views/shared/dossiers/_map_spec.rb
+++ spec/views/shared/dossiers/_map_spec.rb
@@ -6,14 +6,14 @@ describe 'shared/dossiers/map.html.haml', type: :view do
   end
 
   describe "javascript variables printing" do
-    let(:dossier) { create(:dossier, :with_entreprise, json_latlngs: json_latlngs) }
+    let(:dossier) { create(:dossier, json_latlngs: json_latlngs) }
 
     context 'with a correct json' do
       let(:json_latlngs) { "[[{\"lat\":50.659255436656736,\"lng\":3.080635070800781},{\"lat\":50.659255436656736,\"lng\":3.079690933227539},{\"lat\":50.659962770886516,\"lng\":3.0800342559814453},{\"lat\":50.659962770886516,\"lng\":3.0811500549316406},{\"lat\":50.659255436656736,\"lng\":3.080635070800781}]]" }
 
       before { subject }
 
-      it { expect(rendered).to have_content('dossierJsonLatLngs: [[{"lat":50.659255436656736,"lng":3.080635070800781},{"lat":50.659255436656736,"lng":3.079690933227539},{"lat":50.659962770886516,"lng":3.0800342559814453},{"lat":50.659962770886516,"lng":3.0811500549316406},{"lat":50.659255436656736,"lng":3.080635070800781}]],') }
+      it { expect(rendered).to have_content('selection: [[{"lat":50.659255436656736,"lng":3.080635070800781},{"lat":50.659255436656736,"lng":3.079690933227539},{"lat":50.659962770886516,"lng":3.0800342559814453},{"lat":50.659962770886516,"lng":3.0811500549316406},{"lat":50.659255436656736,"lng":3.080635070800781}]],') }
     end
 
     context 'without a correct json' do
@@ -21,7 +21,7 @@ describe 'shared/dossiers/map.html.haml', type: :view do
 
       before { subject }
 
-      it { expect(rendered).to have_content('dossierJsonLatLngs: {},') }
+      it { expect(rendered).to have_content('selection: {},') }
     end
   end
 end
