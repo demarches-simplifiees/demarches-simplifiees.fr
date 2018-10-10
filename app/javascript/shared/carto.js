9a4975a2dd5e17562f2302b5f16944780452ee11
Paul Chavard <git@paul.chavard.net>
2018-10-10 20:03:35 +0200

    Remove dead carto code

--- app/javascript/shared/carto.js
+++ app/javascript/shared/carto.js
@@ -1,9 +1,5 @@
-const LON = '2.428462';
-const LAT = '46.538192';
-const DEFAULT_POSITION = { lon: LON, lat: LAT, zoom: 5 };
 import L from 'leaflet';
 
-export { DEFAULT_POSITION, LAT, LON };
 const LAYERS = {};
 
 export function initMap(position) {
