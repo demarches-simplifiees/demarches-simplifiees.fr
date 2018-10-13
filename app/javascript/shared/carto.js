1d0c437cdac8462b5ebef4fb32a0e4690bef3933
Paul Chavard <git@paul.chavard.net>
2018-10-13 10:32:44 +0200

    carto: add createLayer and removeLayer methods

--- app/javascript/shared/carto.js
+++ app/javascript/shared/carto.js
@@ -1,5 +1,24 @@
 const LON = '2.428462';
 const LAT = '46.538192';
 const DEFAULT_POSITION = { lon: LON, lat: LAT, zoom: 5 };
+import L from 'leaflet';
 
 export { DEFAULT_POSITION, LAT, LON };
+const LAYERS = {};
+
+function createLayer(map, layerName) {
+  const layer = (LAYERS[layerName] = new L.GeoJSON(undefined, {
+    interactive: false
+  }));
+  layer.addTo(map);
+  return layer;
+}
+
+function removeLayer(map, layerName) {
+  const layer = LAYERS[layerName];
+
+  if (layer) {
+    delete LAYERS[layerName];
+    map.removeLayer(layer);
+  }
+}
