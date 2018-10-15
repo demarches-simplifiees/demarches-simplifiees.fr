6be26af7dbca020d4a7e99ac5bd1342e388b196f
gregoirenovel <log@kinko.fr>
2018-10-15 16:57:06 +0200

    Use parentheses

--- app/lib/api_adresse/geocodeur.rb
+++ app/lib/api_adresse/geocodeur.rb
@@ -4,7 +4,7 @@ module ApiAdresse
     def self.convert_adresse_to_point(address)
       ApiAdresse::PointRetriever.new(address).point
     rescue RestClient::Exception, JSON::ParserError => e
-      Rails.logger.error e.message
+      Rails.logger.error(e.message)
       nil
     end
   end
