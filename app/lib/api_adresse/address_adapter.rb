9c5ce210f338d5a37ae95b0c0d3eca8700f9daad
gregoirenovel <log@kinko.fr>
2018-10-15 23:21:22 +0200

    Refactor ApiAdresse::AddressAdapter

--- app/lib/api_adresse/address_adapter.rb
+++ app/lib/api_adresse/address_adapter.rb
@@ -1,26 +1,17 @@
-class ApiAdresse::AddressAdapter
+class ApiAdresse::AddressAdapter < ApiAdresse::Adapter
   def initialize(address)
-    @address = address
+    super(address, 5, [])
   end
 
   def get_suggestions
-    @list ||= convert_api_result_to_full_address
+    handle_result
   end
 
   private
 
-  def convert_api_result_to_full_address
-    result = JSON.parse(ApiAdresse::API.call(@address, 5))
-
-    if result['features'].empty?
-      Rails.logger.error "unable to find location for address #{@address}"
-      return []
-    end
-
-    result['features'].map do |feature|
+  def process_features
+    features.map do |feature|
       feature['properties']['label']
     end
-  rescue TypeError, JSON::ParserError
-    []
   end
 end
