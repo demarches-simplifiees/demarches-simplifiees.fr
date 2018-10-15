a6e7ca60a400c10ae77801ca1c037956111656e2
gregoirenovel <log@kinko.fr>
2018-10-15 17:24:55 +0200

    Remove comments in ApiAdresse

--- app/lib/api_adresse/geocodeur.rb
+++ app/lib/api_adresse/geocodeur.rb
@@ -1,5 +1,4 @@
 module ApiAdresse
-  # this class take a string in input and return a point
   class Geocodeur
     def self.convert_adresse_to_point(address)
       ApiAdresse::PointRetriever.new(address).point
