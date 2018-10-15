6102d828db4729bc655dd8682f1e4558b6197942
gregoirenovel <log@kinko.fr>
2018-10-15 17:26:06 +0200

    Unify scoping in app/lib

--- app/lib/api_adresse/point_retriever.rb
+++ app/lib/api_adresse/point_retriever.rb
@@ -1,22 +1,20 @@
-module ApiAdresse
-  class PointRetriever
-    def initialize(address)
-      @address = address
-    end
+class ApiAdresse::PointRetriever
+  def initialize(address)
+    @address = address
+  end
 
-    def point
-      @point ||= convert_api_result_to_point
-    end
+  def point
+    @point ||= convert_api_result_to_point
+  end
 
-    private
+  private
 
-    def convert_api_result_to_point
-      result = JSON.parse(ApiAdresse::API.call(@address))
-      if result['features'].empty?
-        Rails.logger.error "unable to find location for address #{@address}"
-        return nil
-      end
-      RGeo::GeoJSON.decode(result['features'][0]['geometry'], json_parser: :json)
+  def convert_api_result_to_point
+    result = JSON.parse(ApiAdresse::API.call(@address))
+    if result['features'].empty?
+      Rails.logger.error "unable to find location for address #{@address}"
+      return nil
     end
+    RGeo::GeoJSON.decode(result['features'][0]['geometry'], json_parser: :json)
   end
 end
