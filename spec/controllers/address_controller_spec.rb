79ebdc34437b533a037d192118127a74710ffb53
gregoirenovel <log@kinko.fr>
2018-10-15 23:35:51 +0200

    Ban::SearchController → AddressController

--- spec/controllers/address_controller_spec.rb
+++ spec/controllers/address_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe Ban::SearchController, type: :controller do
+describe AddressController, type: :controller do
   describe '#GET get' do
     subject { get :get, params: { request: request } }
 
