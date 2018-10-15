0018326451b6365523f9a89fc29b33f4c9934186
gregoirenovel <log@kinko.fr>
2018-10-15 22:23:50 +0200

    Add some spacing to specs

--- spec/lib/api_adresse/point_adapter_spec.rb
+++ spec/lib/api_adresse/point_adapter_spec.rb
@@ -7,18 +7,22 @@ describe ApiAdresse::PointAdapter do
     it 'return a point' do
       expect(described_class.new(address).geocode.class).to eq(RGeo::Cartesian::PointImpl)
     end
+
     context 'when RestClient::Exception' do
       before do
         allow(ApiAdresse::API).to receive(:call).and_raise(RestClient::Exception)
       end
+
       it 'return nil' do
         expect(described_class.new(address).geocode).to be_nil
       end
     end
+
     context 'when JSON::ParserError' do
       before do
         allow(JSON).to receive(:parse).and_raise(JSON::ParserError)
       end
+
       it 'return nil' do
         expect(described_class.new(address).geocode).to be_nil
       end
