4903a6b189d753e3d5808063c6135ec244821856
gregoirenovel <log@kinko.fr>
2018-10-15 16:30:27 +0200

    Carto::GeoAPI → ApiGeo

--- app/models/champs/departement_champ.rb
+++ app/models/champs/departement_champ.rb
@@ -1,5 +1,5 @@
 class Champs::DepartementChamp < Champs::TextChamp
   def self.departements
-    JSON.parse(Carto::GeoAPI::Driver.departements).map { |liste| "#{liste['code']} - #{liste['nom']}" }.push('99 - Étranger')
+    JSON.parse(ApiGeo::Driver.departements).map { |liste| "#{liste['code']} - #{liste['nom']}" }.push('99 - Étranger')
   end
 end
