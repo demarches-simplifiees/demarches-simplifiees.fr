c654122e90d2a6f5a22d267d14dfc58d35f734c2
gregoirenovel <log@kinko.fr>
2018-10-15 17:26:06 +0200

    Unify scoping in app/lib

--- app/lib/api_adresse/geocodeur.rb
+++ app/lib/api_adresse/geocodeur.rb
@@ -1,10 +1,8 @@
-module ApiAdresse
-  class Geocodeur
-    def self.convert_adresse_to_point(address)
-      ApiAdresse::PointRetriever.new(address).point
-    rescue RestClient::Exception, JSON::ParserError => e
-      Rails.logger.error(e.message)
-      nil
-    end
+class ApiAdresse::Geocodeur
+  def self.convert_adresse_to_point(address)
+    ApiAdresse::PointRetriever.new(address).point
+  rescue RestClient::Exception, JSON::ParserError => e
+    Rails.logger.error(e.message)
+    nil
   end
 end
--- app/lib/api_geo/driver.rb
+++ app/lib/api_geo/driver.rb
@@ -1,25 +1,23 @@
-module ApiGeo
-  class Driver
-    def self.regions
-      url = [API_GEO_URL, "regions"].join("/")
-      call(url)
-    end
+class ApiGeo::Driver
+  def self.regions
+    url = [API_GEO_URL, "regions"].join("/")
+    call(url)
+  end
 
-    def self.departements
-      url = [API_GEO_URL, "departements"].join("/")
-      call(url)
-    end
+  def self.departements
+    url = [API_GEO_URL, "departements"].join("/")
+    call(url)
+  end
 
-    def self.pays
-      File.open('app/lib/api_geo/pays.json').read
-    end
+  def self.pays
+    File.open('app/lib/api_geo/pays.json').read
+  end
 
-    private
+  private
 
-    def self.call(url)
-      RestClient.get(url, params: { fields: :nom })
-    rescue RestClient::ServiceUnavailable
-      nil
-    end
+  def self.call(url)
+    RestClient.get(url, params: { fields: :nom })
+  rescue RestClient::ServiceUnavailable
+    nil
   end
 end
