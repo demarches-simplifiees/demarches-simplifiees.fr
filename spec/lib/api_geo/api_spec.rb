ed46e2c3be99be99b1046cdea7b1cb1763ddf430
Paul Chavard <git@paul.chavard.net>
2018-10-23 15:35:03 +0200

    Use Typhoeus in ApiGeo adapter

--- spec/lib/api_geo/api_spec.rb
+++ spec/lib/api_geo/api_spec.rb
@@ -4,18 +4,21 @@ describe ApiGeo::API do
   describe '.regions', vcr: { cassette_name: 'api_geo_regions' } do
     subject { described_class.regions }
 
-    it { expect(subject.code).to eq 200 }
+    it { expect(subject.size).to eq 18 }
   end
 
   describe '.departements', vcr: { cassette_name: 'api_geo_departements' } do
     subject { described_class.departements }
 
-    it { expect(subject.code).to eq 200 }
+    it { expect(subject.size).to eq 101 }
   end
 
   describe '.pays' do
     subject { described_class.pays }
+    let(:pays) {
+      JSON.parse(File.open('app/lib/api_geo/pays.json').read, symbolize_names: true)
+    }
 
-    it { is_expected.to eq File.open('app/lib/api_geo/pays.json').read }
+    it { is_expected.to eq pays }
   end
 end
