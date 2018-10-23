d13204c3bcfea0e7178d7f27838c77ef1456f9e5
Paul Chavard <git@paul.chavard.net>
2018-10-23 17:01:15 +0200

    Test ApiGeo RPGAdapter

--- spec/lib/api_geo/api_spec.rb
+++ spec/lib/api_geo/api_spec.rb
@@ -21,4 +21,55 @@ describe ApiGeo::API do
 
     it { is_expected.to eq pays }
   end
+
+  describe '.search_rpg', vcr: { cassette_name: 'api_geo_search_rpg' } do
+    let(:coordinates) do
+      [
+        [
+          2.3945903778076176,
+          46.53312237252731
+        ],
+        [
+          2.394933700561524,
+          46.532590956418076
+        ],
+        [
+          2.3948478698730473,
+          46.53170525134736
+        ],
+        [
+          2.393732070922852,
+          46.530760483351195
+        ],
+        [
+          2.3909854888916016,
+          46.5309376286023
+        ],
+        [
+          2.391414642333985,
+          46.531232869403546
+        ],
+        [
+          2.3913288116455083,
+          46.53253190986272
+        ],
+        [
+          2.39278793334961,
+          46.53329951007484
+        ],
+        [
+          2.3945903778076176,
+          46.53312237252731
+        ]
+      ]
+    end
+
+    let(:geo_json) {
+      GeojsonService.to_json_polygon_for_rpg(coordinates)
+    }
+
+    subject { described_class.search_rpg(geo_json) }
+
+    it { expect(subject[:features].size).to eq 3 }
+  end
 end
