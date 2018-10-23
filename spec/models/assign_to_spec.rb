86f01a91205eae7541c5ec949ca821f71d42f859
Frederic Merizen <frederic.merizen@gmail.com>
2018-10-23 11:03:43 +0200

    [#2604] Factor AssignTo spec

--- spec/models/assign_to_spec.rb
+++ spec/models/assign_to_spec.rb
@@ -1,17 +1,18 @@
 describe AssignTo, type: :model do
   describe '#procedure_presentation_or_default' do
-    context "without a procedure_presentation" do
-      let!(:assign_to) { AssignTo.create }
+    let(:procedure) { create(:procedure) }
+    let(:assign_to) { AssignTo.create(procedure: procedure) }
 
-      it { expect(assign_to.procedure_presentation_or_default.persisted?).to be_falsey }
+    let(:procedure_presentation_or_default) { assign_to.procedure_presentation_or_default }
+
+    context "without a procedure_presentation" do
+      it { expect(procedure_presentation_or_default.persisted?).to be_falsey }
     end
 
     context "with a procedure_presentation" do
-      let(:procedure) { create(:procedure) }
-      let!(:assign_to) { AssignTo.create(procedure: procedure) }
       let!(:procedure_presentation) { ProcedurePresentation.create(assign_to: assign_to) }
 
-      it { expect(assign_to.procedure_presentation_or_default).to eq(procedure_presentation) }
+      it { expect(procedure_presentation_or_default).to eq(procedure_presentation) }
     end
   end
 end
