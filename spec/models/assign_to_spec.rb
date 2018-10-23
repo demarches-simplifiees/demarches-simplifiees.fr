7baa239095e1f79d72eb87c22e64a120b6477cb3
Frederic Merizen <frederic.merizen@gmail.com>
2018-10-23 11:39:54 +0200

    [#2604] Introduce errors when retrieving ProcedurePresentation

--- spec/models/assign_to_spec.rb
+++ spec/models/assign_to_spec.rb
@@ -1,18 +1,22 @@
 describe AssignTo, type: :model do
-  describe '#procedure_presentation_or_default' do
+  describe '#procedure_presentation_or_default_and_errors' do
     let(:procedure) { create(:procedure) }
     let(:assign_to) { AssignTo.create(procedure: procedure) }
 
-    let(:procedure_presentation_or_default) { assign_to.procedure_presentation_or_default }
+    let(:procedure_presentation_and_errors) { assign_to.procedure_presentation_or_default_and_errors }
+    let(:procedure_presentation_or_default) { procedure_presentation_and_errors.first }
+    let(:errors) { procedure_presentation_and_errors.second }
 
     context "without a procedure_presentation" do
       it { expect(procedure_presentation_or_default).not_to be_persisted }
+      it { expect(errors).to be_nil }
     end
 
     context "with a procedure_presentation" do
       let!(:procedure_presentation) { ProcedurePresentation.create(assign_to: assign_to) }
 
       it { expect(procedure_presentation_or_default).to eq(procedure_presentation) }
+      it { expect(errors).to be_nil }
     end
   end
 end
--- spec/models/gestionnaire_spec.rb
+++ spec/models/gestionnaire_spec.rb
@@ -197,20 +197,24 @@ describe Gestionnaire, type: :model do
     end
   end
 
-  describe "procedure_presentation_for_procedure_id" do
-    let(:procedure_presentation) { gestionnaire.procedure_presentation_for_procedure_id(procedure_id) }
+  describe "procedure_presentation_and_errors_for_procedure_id" do
+    let(:procedure_presentation_and_errors) { gestionnaire.procedure_presentation_and_errors_for_procedure_id(procedure_id) }
+    let(:procedure_presentation) { procedure_presentation_and_errors.first }
+    let(:errors) { procedure_presentation_and_errors.second }
 
     context 'with explicit presentation' do
       let(:procedure_id) { procedure.id }
       let!(:pp) { ProcedurePresentation.create(assign_to: procedure_assign) }
 
       it { expect(procedure_presentation).to eq(pp) }
+      it { expect(errors).to be_nil }
     end
 
     context 'with default presentation' do
       let(:procedure_id) { procedure_2.id }
 
       it { expect(procedure_presentation).not_to be_persisted }
+      it { expect(errors).to be_nil }
     end
   end
 
