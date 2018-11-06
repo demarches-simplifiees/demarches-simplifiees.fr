144dd16b88f0eba2bf60172e03fed73f34ccb303
Pierre de La Morinerie <kemenaran@gmail.com>
2018-11-06 10:22:17 +0000

    users: sign-in after confirming an account within a short time

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -25,8 +25,24 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   #   super(resource_name)
   # end
 
+  # If the user clicks the confirmation link before the maximum delay,
+  # they will be signed in directly.
+  def sign_in_after_confirmation?(resource)
+    # Avoid keeping auto-sign-in links in users inboxes for too long.
+    # 95% of users confirm their account within two hours.
+    auto_sign_in_timeout = 2.hours
+    resource.confirmation_sent_at + auto_sign_in_timeout > DateTime.current
+  end
+
   # The path used after confirmation.
-  # def after_confirmation_path_for(resource_name, resource)
-  #   super(resource_name, resource)
-  # end
+  def after_confirmation_path_for(resource_name, resource)
+    if sign_in_after_confirmation?(resource)
+      resource.remember_me = true
+      sign_in(resource)
+      resource.force_sync_credentials
+      after_sign_in_path_for(resource_name)
+    else
+      super(resource_name, resource)
+    end
+  end
 end
