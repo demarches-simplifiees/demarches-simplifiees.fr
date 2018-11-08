db6c86b242a40f15d4b9312f0a1ef6c5cc169b58
simon lehericey <simon.lehericey@gmail.com>
2018-11-08 10:19:52 +0100

    DossierController: user can destroy pjs

--- config/routes.rb
+++ config/routes.rb
@@ -284,6 +284,7 @@ Rails.application.routes.draw do
         post 'commentaire' => 'dossiers#create_commentaire'
         post 'ask_deletion'
         get 'attestation'
+        delete 'purge_champ_piece_justificative'
       end
 
       collection do
