e374a5c7262ac8aa83b1cc65f838f3b54321c097
Paul Chavard <git@paul.chavard.net>
2018-11-13 18:48:04 +0100

    Refactor ChampSerializer

--- spec/factories/champ.rb
+++ spec/factories/champ.rb
@@ -28,4 +28,41 @@ FactoryBot.define do
       end
     end
   end
+
+  factory :champ_integer_number, class: 'Champs::IntegerNumberChamp' do
+    type_de_champ { create(:type_de_champ_integer_number) }
+    value { '42' }
+  end
+
+  factory :champ_decimal_number, class: 'Champs::DecimalNumberChamp' do
+    type_de_champ { create(:type_de_champ_decimal_number) }
+    value { '42.1' }
+  end
+
+  factory :champ_linked_drop_down_list, class: 'Champs::LinkedDropDownListChamp' do
+    type_de_champ { create(:type_de_champ_linked_drop_down_list) }
+    value { '{}' }
+  end
+
+  factory :champ_carte, class: 'Champs::CarteChamp' do
+    type_de_champ { create(:type_de_champ_carte) }
+  end
+
+  factory :champ_siret, class: 'Champs::SiretChamp' do
+    type_de_champ { create(:type_de_champ_siret) }
+    value { '44011762001530' }
+    etablissement { create(:etablissement) }
+
+    before(:create) do |champ, evaluator|
+      champ.etablissement.signature = champ.etablissement.sign
+    end
+  end
+
+  factory :champ_piece_justificative, class: 'Champs::PieceJustificativeChamp' do
+    type_de_champ { create(:type_de_champ_piece_justificative) }
+
+    after(:create) do |champ, evaluator|
+      champ.piece_justificative_file.attach(io: StringIO.new("toto"), filename: "toto.txt", content_type: "text/plain")
+    end
+  end
 end
