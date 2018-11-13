LeSim/tps

Session: send a mail to confirm gestionnaire login

config/routes.rb
@@ -88,6 +88,8 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_in/demo' => redirect("/users/sign_in")
     get '/users/no_procedure' => 'users/sessions#no_procedure'
+    get 'connexion-par-jeton/:login_token' => 'users/sessions#sign_in_by_link', as: 'sign_in_by_link'
+    get 'lien-envoye/:email' => 'users/sessions#link_sent', constraints: { email: /.*/ }, as: 'link_sent'
   end
 
   devise_scope :gestionnaire do

demarches-simplifiees/demarches-simplifiees.fr

config/routes.rb
@@ -88,6 +88,8 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_in/demo' => redirect("/users/sign_in")
     get '/users/no_procedure' => 'users/sessions#no_procedure'
+    get 'connexion-par-jeton/:id' => 'users/sessions#sign_in_by_link', as: 'sign_in_by_link'
+    get 'lien-envoye/:email' => 'users/sessions#link_sent', constraints: { email: /.*/ }, as: 'link_sent'
   end
 
   devise_scope :gestionnaire do