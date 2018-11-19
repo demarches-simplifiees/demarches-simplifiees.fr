180ab8c8b143c3d242cb96be87e069ed2651b3fb
Pierre de La Morinerie <kemenaran@gmail.com>
2018-11-19 18:14:35 +0100

    mailers: add missing previews for notification emails

--- spec/mailers/previews/notification_mailer_preview.rb
+++ spec/mailers/previews/notification_mailer_preview.rb
@@ -1,5 +1,21 @@
 class NotificationMailerPreview < ActionMailer::Preview
-  def send_notification
+  def send_dossier_received
+    NotificationMailer.send_dossier_received(Dossier.last)
+  end
+
+  def send_initiated_notification
     NotificationMailer.send_initiated_notification(Dossier.last)
+  end
+
+  def send_closed_notification
+    NotificationMailer.send_closed_notification(Dossier.last)
+  end
+
+  def send_refused_notification
+    NotificationMailer.send_refused_notification(Dossier.last)
+  end
+
+  def send_without_continuation_notification
+    NotificationMailer.send_without_continuation_notification(Dossier.last)
   end
 end
