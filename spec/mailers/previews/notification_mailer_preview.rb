e5303fd9862142df6978dff789666d4e3693e53b
Pierre de La Morinerie <kemenaran@gmail.com>
2018-11-20 10:50:25 +0000

    mailers: streamline the NotificationMailer
    Notifications are now only for demarche-templated emails.

--- spec/mailers/previews/notification_mailer_preview.rb
+++ spec/mailers/previews/notification_mailer_preview.rb
@@ -2,8 +2,4 @@ class NotificationMailerPreview < ActionMailer::Preview
   def send_notification
     NotificationMailer.send_initiated_notification(Dossier.last)
   end
-
-  def send_draft_notification
-    NotificationMailer.send_draft_notification(Dossier.last)
-  end
 end
