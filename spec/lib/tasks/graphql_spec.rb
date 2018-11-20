9b87d67f690a55c3ba9afa8cfb239669cb9c80dc
Paul Chavard <git@paul.chavard.net>
2018-11-20 22:21:38 +0100

    Dump graphql schema for reference

---
+++ spec/lib/tasks/graphql_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe 'graphql' do
+  let(:current_defn) { Api::V2::Schema.to_definition }
+  let(:printout_defn) { File.read(Rails.root.join('app', 'graphql', 'schema.graphql')) }
+
+  it "update the printed schema with `bin/rake graphql:schema:idl`" do
+    result = GraphQL::SchemaComparator.compare(current_defn, printout_defn)
+    expect(result.identical?).to be_truthy
+  end
+end
