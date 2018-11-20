324d8d73e5c9cd3e70722b358942311023462038
Paul Chavard <git@paul.chavard.net>
2018-11-20 22:59:51 +0100

    Resolve models and types

--- app/graphql/mutations/base_mutation.rb
+++ app/graphql/mutations/base_mutation.rb
@@ -1,4 +1,18 @@
 module Mutations
   class BaseMutation < GraphQL::Schema::Mutation
+    private
+
+    def load_application_object(arg_kwarg, id)
+      super(arg_kwarg, id_with_type(arg_kwarg, id))
+    end
+
+    def id_with_type(type, id)
+      case type
+      when :dossier
+        Dossier.new(id: id).to_typed_id
+      else
+        id
+      end
+    end
   end
 end
