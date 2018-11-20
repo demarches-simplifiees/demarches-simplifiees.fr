643da7a6bcc85d2199835c45031c8bdd7e231e70
Paul Chavard <git@paul.chavard.net>
2018-11-20 22:59:51 +0100

    Resolve models and types

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,19 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.record_from_typed_id(id)
+    class_name, record_id = GraphQL::Schema::UniqueWithinType.decode(id)
+
+    if defined?(class_name)
+      Object.const_get(class_name).find(record_id)
+    else
+      raise ActiveRecord::RecordNotFound, "Unexpected object: #{class_name}"
+    end
+  rescue => e
+    raise ActiveRecord::RecordNotFound, e.message
+  end
+
+  def to_typed_id
+    GraphQL::Schema::UniqueWithinType.encode(self.class.name, id)
+  end
 end
