d43068ace5644e6679b51e19598d13d0883fec79
Paul Chavard <git@paul.chavard.net>
2018-11-20 11:20:10 +0100

    Disable carto for new procedures

--- spec/controllers/admin/procedures_controller_spec.rb
+++ spec/controllers/admin/procedures_controller_spec.rb
@@ -201,18 +201,6 @@ describe Admin::ProceduresController, type: :controller do
           it { expect(subject.duree_conservation_dossiers_hors_ds).to eq(duree_conservation_dossiers_hors_ds) }
         end
 
-        describe 'procedure module api carto attributs in database' do
-          let(:procedure) { Procedure.last }
-          let(:use_api_carto) { '1' }
-          let(:quartiers_prioritaires) { '1' }
-
-          subject { ModuleAPICarto.last }
-
-          it { expect(subject.procedure).to eq(procedure) }
-          it { expect(subject.use_api_carto).to be_truthy }
-          it { expect(subject.quartiers_prioritaires).to be_truthy }
-        end
-
         it { is_expected.to redirect_to(admin_procedure_types_de_champ_path(procedure_id: Procedure.last.id)) }
 
         it { expect(flash[:notice]).to be_present }
