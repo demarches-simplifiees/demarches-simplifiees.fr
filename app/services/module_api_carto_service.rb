cb6927399cd9bdff4879bb802fffb40736777270
Paul Chavard <git@paul.chavard.net>
2018-11-27 15:46:22 +0100

    Remove unused users/carte_controller

--- app/services/module_api_carto_service.rb
+++ app/services/module_api_carto_service.rb
@@ -1,28 +1,4 @@
 class ModuleApiCartoService
-  def self.save_qp!(dossier, json_latlngs)
-    if dossier.procedure.module_api_carto.quartiers_prioritaires?
-      qp_list = generate_qp(JSON.parse(json_latlngs))
-
-      qp_list.each do |qp|
-        qp[:dossier_id] = dossier.id
-        qp[:geometry] = qp[:geometry].to_json
-        QuartierPrioritaire.create(qp)
-      end
-    end
-  end
-
-  def self.save_cadastre!(dossier, json_latlngs)
-    if dossier.procedure.module_api_carto.cadastre?
-      cadastre_list = generate_cadastre JSON.parse(json_latlngs)
-
-      cadastre_list.each do |cadastre|
-        cadastre[:dossier_id] = dossier.id
-        cadastre[:geometry] = cadastre[:geometry].to_json
-        Cadastre.create(cadastre)
-      end
-    end
-  end
-
   def self.generate_qp(coordinates)
     coordinates.flat_map do |coordinate|
       ApiCarto::QuartiersPrioritairesAdapter.new(
