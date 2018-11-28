b9af07b845c0d63a8019e44affe3ef51d1e4a715
Paul Chavard <git@paul.chavard.net>
2018-11-28 15:19:12 +0100

    Add admin support contact form

--- spec/lib/helpscout/form_adapter_spec.rb
+++ spec/lib/helpscout/form_adapter_spec.rb
@@ -66,5 +66,45 @@ describe Helpscout::FormAdapter do
           .with(conversation_id, nil, nil)
       end
     end
+
+    context 'add_phone' do
+      before do
+        allow(api).to receive(:create_conversation)
+          .and_return(
+            double(
+              success?: true,
+              headers: {
+                'Resource-ID' => conversation_id
+              }
+            )
+          )
+
+        described_class.new(params, api).send_form
+      end
+
+      let(:params) {
+        {
+          email: email,
+          subject: subject,
+          text: text,
+          phone: '0666666666'
+        }
+      }
+      let(:phone) { '0666666666' }
+      let(:email) { 'paul.chavard@beta.gouv.fr' }
+      let(:subject) { 'Bonjour' }
+      let(:text) { "J'ai un problem" }
+      let(:tags) { ['info demarche'] }
+      let(:conversation_id) { '123' }
+
+      it 'should call method' do
+        expect(api).to have_received(:create_conversation)
+          .with(email, subject, text, nil)
+        expect(api).to have_received(:add_phone_number)
+          .with(email, phone)
+        expect(api).to have_received(:add_custom_fields)
+          .with(conversation_id, nil, nil)
+      end
+    end
   end
 end
