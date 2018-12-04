63afb5168ea05f07ebcd5b905f6f70f47b8a09d2
Paul Chavard <git@paul.chavard.net>
2018-12-04 11:36:45 +0100

    Update activestorage.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,11 @@
 import '../shared/polyfills';
 import Turbolinks from 'turbolinks';
 import Rails from 'rails-ujs';
-import ActiveStorage from '../shared/activestorage/ujs';
+import * as ActiveStorage from 'activestorage';
 import Chartkick from 'chartkick';
 import Highcharts from 'highcharts';
 
+import '../shared/activestorage/progress';
 import '../shared/sentry';
 import '../shared/rails-ujs-fix';
 import '../shared/safari-11-file-xhr-workaround';
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,20 +1,6 @@
 const path = require('path');
 const { environment } = require('@rails/webpacker');
 
-// By default don't transpile JS files in ./node_modules – except for some specific modules.
-const babelLoader = environment.loaders.get('babel');
-babelLoader.exclude = function(modulePath) {
-  let forcedModules = [
-    'activestorage' // ActiveStorage uses 'class', which is not supported by IE 11 and older Safari version
-  ];
-  return (
-    modulePath.includes('node_modules') &&
-    forcedModules.every(
-      forcedModule => !modulePath.includes('node_modules/' + forcedModule)
-    )
-  );
-};
-
 const resolve = {
   alias: {
     '@utils': path.resolve(__dirname, '..', '..', 'app/javascript/shared/utils')
