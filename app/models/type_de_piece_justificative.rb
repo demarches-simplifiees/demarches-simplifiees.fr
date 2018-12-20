eb9edc1cee70e7e36c4ccb936eb03d48fe7777df
Paul Chavard <git@paul.chavard.net>
2018-12-20 12:00:27 +0100

    Order types de champ in scope

--- app/models/type_de_piece_justificative.rb
+++ app/models/type_de_piece_justificative.rb
@@ -6,4 +6,5 @@ class TypeDePieceJustificative < ApplicationRecord
   validates :libelle, presence: true, allow_blank: false, allow_nil: false
 
   validates :lien_demarche, format: { with: URI.regexp }, allow_blank: true, allow_nil: true
+  scope :ordered, -> { order(order_place: :asc) }
 end
