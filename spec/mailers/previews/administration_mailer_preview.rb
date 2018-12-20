e1b97b3303700ef9e8018039a5f98f1c30147dfe
maatinito <15379878+maatinito@users.noreply.github.com>
2018-12-20 16:46:44 +0100

    Make more emails previewable

--- spec/mailers/previews/administration_mailer_preview.rb
+++ spec/mailers/previews/administration_mailer_preview.rb
@@ -14,4 +14,29 @@ class AdministrationMailerPreview < ActionMailer::Preview
   def refuse_admin
     AdministrationMailer.refuse_admin('bad_admin@pipo.com')
   end
+
+  def new_admin
+    administration = Administration.new(email: 'superadmin@demarches-simplifiees.fr')
+    AdministrationMailer.new_admin_email(administrateur, administration)
+  end
+
+  def dossier_expiration_summary
+    expiring_dossiers = [Dossier.new(id: 100, procedure: procedure_1)]
+    expired_dossiers = [Dossier.new(id: 100, procedure: procedure_2)]
+    AdministrationMailer.dossier_expiration_summary(expiring_dossiers, expired_dossiers)
+  end
+
+  private
+
+  def procedure_1
+    Procedure.new(id: 10, libelle: "Démarche des marches", administrateur: administrateur)
+  end
+
+  def procedure_2
+    Procedure.new(id: 20, libelle: "Démarche pieds", administrateur: administrateur)
+  end
+
+  def administrateur
+    Administrateur.new(id: 111, email: "chef.de.service@administration.gouv.fr")
+  end
 end
