b0a28210b6b13636c4740918b6bbfe157a168eba
simon lehericey <simon.lehericey@gmail.com>
2019-01-04 14:45:06 +0100

    Procedure: cloning set the current admin gestionnaire of the procedure

--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -218,6 +218,8 @@ class Procedure < ApplicationRecord
       procedure.service = self.service.clone_and_assign_to_administrateur(admin)
     end
 
+    admin.gestionnaire.assign_to_procedure(procedure)
+
     procedure
   end
 
