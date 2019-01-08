d36696442bf0eb9d33cc6a7ec141b4d8d2a9e309
Pierre de La Morinerie <kemenaran@gmail.com>
2019-01-08 07:20:49 +0000

    layouts: migrate sign_in to the shared layout

--- spec/views/layouts/procedure_context.html.haml_spec.rb
+++ spec/views/layouts/procedure_context.html.haml_spec.rb
@@ -4,25 +4,31 @@ describe 'layouts/procedure_context.html.haml', type: :view do
   let(:procedure) { create(:simple_procedure, :with_service) }
   let(:dossier) { create(:dossier, procedure: procedure) }
 
-  before do
-    assign(:dossier, dossier)
-  end
-
   subject do
     render html: 'Column content', layout: 'layouts/procedure_context.html.haml'
   end
 
-  it 'renders a description of the procedure' do
-    expect(subject).to have_text(dossier.procedure.libelle)
-    expect(subject).to have_text(dossier.procedure.description)
   end
 
-  it 'renders the inner content' do
-    expect(subject).to have_text('Column content')
+  context 'when a dossier is assigned' do
+    before do
+      assign(:dossier, dossier)
+    end
+
+    it 'renders a description of the procedure' do
+      expect(subject).to have_text(dossier.procedure.libelle)
+      expect(subject).to have_text(dossier.procedure.description)
+    end
+
+    it 'renders the inner content' do
+      expect(subject).to have_text('Column content')
+    end
+
+    it 'renders the procedure footer' do
+      expect(subject).to have_text(dossier.procedure.service.nom)
+      expect(subject).to have_text(dossier.procedure.service.email)
+    end
   end
 
-  it 'renders the dossier footer' do
-    expect(subject).to have_text(dossier.procedure.service.nom)
-    expect(subject).to have_text(dossier.procedure.service.email)
   end
 end
