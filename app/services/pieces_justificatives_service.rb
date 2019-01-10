1672c265f9831303b0774397d62e0cfd7a333cee
Frederic Merizen <frederic.merizen@gmail.com>
2019-01-10 15:55:04 +0100

    [#2180] Expose migrated types PJ as legacy types PJ in procedure API

--- app/services/pieces_justificatives_service.rb
+++ app/services/pieces_justificatives_service.rb
@@ -67,4 +67,21 @@ class PiecesJustificativesService
       []
     end
   end
+
+  def self.serialize_types_de_champ_as_type_pj(procedure)
+    tdcs = procedure.types_de_champ.select { |type_champ| type_champ.old_pj.present? }
+    tdcs.map.with_index do |type_champ, order_place|
+      description = type_champ.description
+      if /^(?<original_description>.*?)(?:[\r\n]+)Récupérer le formulaire vierge pour mon dossier : (?<lien_demarche>http.*)$/m =~ description
+        description = original_description
+      end
+      {
+        id: type_champ.old_pj[:stable_id],
+        libelle: type_champ.libelle,
+        description: description,
+        order_place: order_place,
+        lien_demarche: lien_demarche
+      }
+    end
+  end
 end
