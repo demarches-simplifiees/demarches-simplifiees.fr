2d2314543c9db59e19524e70a0ba5413fc2e1ed8
Pierre de La Morinerie <kemenaran@gmail.com>
2019-01-16 15:16:15 +0000

    commencer: redirect to the procedure page after sign-in and sign-up

--- spec/features/new_user/linked_dropdown_spec.rb
+++ spec/features/new_user/linked_dropdown_spec.rb
@@ -52,10 +52,12 @@ feature 'linked dropdown lists' do
 
     expect(page).to have_current_path(new_user_session_path)
 
-    fill_in 'user_email', with: email
-    fill_in 'user_password', with: password
-    click_on 'Se connecter'
+    sign_in_with(email, password)
 
+    expect(page).to have_current_path(commencer_path(path: procedure.path))
+    click_on 'Commencer la démarche'
+
+    expect(page).to have_content("Données d'identité")
     expect(page).to have_current_path(identite_dossier_path(user_dossier))
   end
 
