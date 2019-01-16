34c9dee89c8a85282d84a9165b1418acd091832f
simon lehericey <simon.lehericey@gmail.com>
2019-01-16 10:47:32 +0100

    [fix #3268] Jobs: use automatic_operation

--- app/jobs/auto_archive_procedure_job.rb
+++ app/jobs/auto_archive_procedure_job.rb
@@ -3,11 +3,10 @@ class AutoArchiveProcedureJob < ApplicationJob
 
   def perform(*args)
     Procedure.publiees.where("auto_archive_on <= ?", Date.today).each do |procedure|
-      gestionnaire = procedure.gestionnaire_for_cron_job
-
-      procedure.dossiers.state_en_construction.find_each do |dossier|
-        dossier.passer_en_instruction!(gestionnaire)
-      end
+      procedure
+        .dossiers
+        .state_en_construction
+        .find_each(&:passer_automatiquement_en_instruction!)
 
       procedure.archive!
     end
