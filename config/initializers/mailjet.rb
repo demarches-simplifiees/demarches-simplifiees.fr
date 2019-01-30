8d90d36431ad248084ed6184b671f6f5cca20f01
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2019-01-29 15:46:18 -1000

    [DEM-38] Allows staging & prod to run on same machine through use of .env configuration file

--- config/initializers/mailjet.rb
+++ config/initializers/mailjet.rb
@@ -2,4 +2,6 @@ Mailjet.configure do |config|
   config.api_key = Rails.application.secrets.mailjet[:api_key]
   config.secret_key = Rails.application.secrets.mailjet[:secret_key]
   config.default_from = CONTACT_EMAIL
+
+  # puts "Mail configuration: default_from=#{config.default_from}, api_key=#{config.api_key[0..8]}... secret_key=#{config.secret_key[0..8]}..."
 end
