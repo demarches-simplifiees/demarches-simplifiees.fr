60d88feb18319f59efa1916d9e6c4f97997be56a
Frederic Merizen <frederic.merizen@beta.gouv.fr>
2019-02-05 16:28:26 +0100

    [#3356] Let dynamic type handle tag generation

--- app/models/type_de_champ.rb
+++ app/models/type_de_champ.rb
@@ -38,6 +38,7 @@ class TypeDeChamp < ApplicationRecord
   has_many :types_de_champ, -> { ordered }, foreign_key: :parent_id, class_name: 'TypeDeChamp', dependent: :destroy
 
   store_accessor :options, :cadastres, :quartiers_prioritaires, :parcelles_agricoles, :old_pj
+  delegate :tags_for_template, to: :dynamic_type
 
   # TODO simplify after migrating `options` column to (non YAML encoded) JSON
   class MaybeYaml
@@ -172,19 +173,6 @@ class TypeDeChamp < ApplicationRecord
     if piece_justificative_template.attached?
       piece_justificative_template.filename
     end
-  end
-
-  def tags_for_template
-    l = libelle
-    [
-      {
-        libelle: l,
-        description: description,
-        lambda: -> (champs) {
-          champs.detect { |champ| champ.libelle == l }
-        }
-      }
-    ]
   end
 
   private
