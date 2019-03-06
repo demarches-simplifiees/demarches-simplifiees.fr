183f1a5fa8ef35ce7832e4cceeddeb4aa080da36
clemkeirua <clement@keiruaprod.fr>
2019-03-06 11:13:17 +0100

    suppression de l'ajout en PJ

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -17,20 +17,20 @@ class NotificationMailer < ApplicationMailer
   end
 
   def send_closed_notification(dossier)
-    send_final_notification(dossier, dossier.procedure.closed_mail_template, dossier.justificatif_motivation)
+    send_final_notification(dossier, dossier.procedure.closed_mail_template)
   end
 
   def send_refused_notification(dossier)
-    send_final_notification(dossier, dossier.procedure.refused_mail_template, dossier.justificatif_motivation)
+    send_final_notification(dossier, dossier.procedure.refused_mail_template)
   end
 
   def send_without_continuation_notification(dossier)
-    send_final_notification(dossier, dossier.procedure.without_continuation_mail_template, dossier.justificatif_motivation)
+    send_final_notification(dossier, dossier.procedure.without_continuation_mail_template)
   end
 
   private
 
-  def send_notification(dossier, mail_template, attachment_file = nil)
+  def send_notification(dossier, mail_template)
     email = dossier.user.email
 
     subject = mail_template.subject_for_dossier(dossier)
