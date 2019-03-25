30d11e0dacdbcfc352da04ba8f74db04650d8164
Pierre de La Morinerie <kemenaran@gmail.com>
2019-03-25 15:54:46 +0000

    app: rename `new_gestionnaire` to `gestionnaires`

---
+++ app/controllers/gestionnaires/recherche_controller.rb
@@ -0,0 +1,12 @@
+module Gestionnaires
+  class RechercheController < GestionnaireController
+    def index
+      @search_terms = params[:q]
+      @dossiers = DossierSearchService.matching_dossiers_for_gestionnaire(@search_terms, current_gestionnaire)
+      @followed_dossiers_id = current_gestionnaire
+        .followed_dossiers
+        .where(procedure_id: @dossiers.pluck(:procedure_id))
+        .pluck(:id)
+    end
+  end
+end
