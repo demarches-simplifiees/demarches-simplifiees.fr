5a357e8d4413dcc30adaee1c8df2d590ff17b4cd
Pierre de La Morinerie <kemenaran@gmail.com>
2019-03-27 11:43:04 +0100

    dossiers: displays an "Start an other dossier" item in action dropdown

--- spec/views/users/dossiers/_dossier_actions.html.haml_spec.rb
+++ spec/views/users/dossiers/_dossier_actions.html.haml_spec.rb
@@ -4,6 +4,7 @@ describe 'users/dossiers/dossier_actions.html.haml', type: :view do
 
   subject { render 'users/dossiers/dossier_actions.html.haml', dossier: dossier }
 
+  it { is_expected.to have_link('Commencer un autre dossier', href: commencer_url(path: procedure.path)) }
   it { is_expected.to have_link('Supprimer le dossier', href: ask_deletion_dossier_path(dossier)) }
 
   context 'when the dossier cannot be deleted' do
@@ -11,7 +12,18 @@ describe 'users/dossiers/dossier_actions.html.haml', type: :view do
     it { is_expected.not_to have_link('Supprimer le dossier') }
   end
 
+  context 'when the procedure doesn’t expect multiple submissions' do
+    let(:procedure) { create(:procedure, :published, expects_multiple_submissions: false) }
+    it { is_expected.not_to have_link('Commencer un autre dossier') }
+  end
+
+  context 'when the procedure is closed' do
+    let(:procedure) { create(:procedure, :archived, expects_multiple_submissions: true) }
+    it { is_expected.not_to have_link('Commencer un autre dossier') }
+  end
+
   context 'when there are no actions to display' do
+    let(:procedure) { create(:procedure, :published, expects_multiple_submissions: false) }
     let(:dossier) { create(:dossier, :accepte, procedure: procedure) }
 
     it 'doesn’t render the menu at all' do
