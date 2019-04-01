526531c268036d31d8ca6744e7787504f2e460ae
Pierre de La Morinerie <kemenaran@gmail.com>
2019-04-01 15:04:11 +0200

    procedure_context: display the procedure description on small screens
    Fix #3658

---
+++ app/javascript/new_design/procedure-context.js
@@ -0,0 +1,20 @@
+import { delegate } from '@utils';
+
+function updateReadMoreVisibility() {
+  const descBody = document.querySelector('.procedure-description-body');
+  if (descBody) {
+    // If the description text overflows, display a "Read more" button.
+    const isOverflowing = descBody.scrollHeight > descBody.clientHeight;
+    descBody.classList.toggle('read-more-enabled', isOverflowing);
+  }
+}
+
+function expandProcedureDescription() {
+  const descBody = document.querySelector('.procedure-description-body');
+  descBody.classList.remove('read-more-collapsed');
+}
+
+addEventListener('turbolinks:load', updateReadMoreVisibility);
+addEventListener('resize', updateReadMoreVisibility);
+
+delegate('click', '.read-more-button', expandProcedureDescription);
