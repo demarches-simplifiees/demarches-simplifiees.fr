ad9a3c173f7d0ceb410a900c38d4142694e97bed
Pierre de La Morinerie <kemenaran@gmail.com>
2019-04-03 15:15:35 +0000

    types_de_champ_editor: add title to actions

--- app/javascript/components/TypesDeChampEditor/components/MoveButton.js
+++ app/javascript/components/TypesDeChampEditor/components/MoveButton.js
@@ -2,9 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-function MoveButton({ isEnabled, icon, onClick }) {
+function MoveButton({ isEnabled, icon, title, onClick }) {
   return (
-    <button className="button small icon-only move" disabled={!isEnabled} onClick={onClick}>
+    <button
+      className="button small icon-only move"
+      disabled={!isEnabled}
+      title={title}
+      onClick={onClick}
+    >
       <FontAwesomeIcon icon={icon} />
     </button>
   );
@@ -13,6 +18,7 @@ function MoveButton({ isEnabled, icon, onClick }) {
 MoveButton.propTypes = {
   isEnabled: PropTypes.bool,
   icon: PropTypes.string,
+  title: PropTypes.string,
   onClick: PropTypes.func
 };
 
