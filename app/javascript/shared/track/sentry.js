c068e0a96c28ad032f0b3f0453a42a5b802cfdc2
Paul Chavard <git@paul.chavard.net>
2019-04-03 12:13:34 +0200

    Sentry should send environment information

--- app/javascript/shared/track/sentry.js
+++ app/javascript/shared/track/sentry.js
@@ -1,10 +1,10 @@
 import * as Sentry from '@sentry/browser';
 
-const { key, enabled, user } = gon.sentry || {};
+const { key, enabled, user, environment } = gon.sentry || {};
 
 // We need to check for key presence here as we do not have a dsn for browser yet
 if (enabled && key) {
-  Sentry.init({ dsn: key });
+  Sentry.init({ dsn: key, environment });
 
   if (user.email) {
     Sentry.configureScope(scope => {
