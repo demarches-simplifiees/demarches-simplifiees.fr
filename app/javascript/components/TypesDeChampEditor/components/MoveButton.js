1880e5161b7ddee88bcd4ec49701f4baea33150a
Pierre de La Morinerie <kemenaran@gmail.com>
2019-04-03 15:35:49 +0200

    types_de_champs_editor: improve apparence of Move buttons

--- app/javascript/components/TypesDeChampEditor/components/MoveButton.js
+++ app/javascript/components/TypesDeChampEditor/components/MoveButton.js
@@ -2,19 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-function MoveButton({ isVisible, icon, onClick }) {
-  if (isVisible) {
-    return (
-      <button className="button small icon-only move" onClick={onClick}>
-        <FontAwesomeIcon icon={icon} />
-      </button>
-    );
-  }
-  return null;
+function MoveButton({ isEnabled, icon, onClick }) {
+  return (
+    <button className="button small icon-only move" disabled={!isEnabled} onClick={onClick}>
+      <FontAwesomeIcon icon={icon} />
+    </button>
+  );
 }
 
 MoveButton.propTypes = {
-  isVisible: PropTypes.bool,
+  isEnabled: PropTypes.bool,
   icon: PropTypes.string,
   onClick: PropTypes.func
 };
