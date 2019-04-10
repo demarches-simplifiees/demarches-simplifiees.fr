abcd58c35d06c07674f86759045f665cb8e36a87
pedong <mail@peng-fei.name>
2019-04-10 15:49:13 +0200

    [fix #3710] date with letter
    Co-Authored-By: simon lehericey <mail@simon.lehericey.net>

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -17,4 +17,42 @@ describe ApplicationHelper do
       it { is_expected.to be_nil }
     end
   end
+
+  describe "#try_format_date" do
+    subject { try_format_date(date) }
+
+    describe 'try formatting a date' do
+      let(:date) { Date.new(2019, 01, 24) }
+      it { is_expected.to eq("24 janvier 2019") }
+    end
+
+    describe 'try formatting a blank string' do
+      let(:date) { "" }
+      it { is_expected.to eq("") }
+    end
+
+    describe 'try formatting a nil string' do
+      let(:date) { nil }
+      it { is_expected.to eq("") }
+    end
+  end
+
+  describe "#try_format_datetime" do
+    subject { try_format_datetime(datetime) }
+
+    describe 'try formatting 31/01/2019 11:25' do
+      let(:datetime) { Time.zone.local(2019, 01, 31, 11, 25, 00) }
+      it { is_expected.to eq("31 janvier 2019 11:25") }
+    end
+
+    describe 'try formatting a blank string' do
+      let(:datetime) { "" }
+      it { is_expected.to eq("") }
+    end
+
+    describe 'try formatting a nil string' do
+      let(:datetime) { nil }
+      it { is_expected.to eq("") }
+    end
+  end
 end
