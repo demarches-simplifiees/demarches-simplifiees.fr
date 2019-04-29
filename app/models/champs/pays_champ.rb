2308b3bc5cc9fd1b1c72bb508651de58f1a152a4
maatinito <15379878+maatinito@users.noreply.github.com>
2019-04-29 10:55:10 -1000

    [Fix 3827] Champ Pays: la ligne de tirets ne devrait pas être selectionnable

--- app/models/champs/pays_champ.rb
+++ app/models/champs/pays_champ.rb
@@ -2,4 +2,8 @@ class Champs::PaysChamp < Champs::TextChamp
   def self.pays
     ApiGeo::API.pays.pluck(:nom)
   end
+
+  def self.disabled_options
+    pays.select { |v| (v =~ /^--.*--$/).present? }
+  end
 end
