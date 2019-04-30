fd95ea2871513c4b24b4d746651dc8a0118ac4e3
Pierre de La Morinerie <kemenaran@gmail.com>
2019-04-30 16:18:56 +0200

    spec: cleanup exercices_adapter_spec

--- spec/lib/api_entreprise/exercices_adapter_spec.rb
+++ spec/lib/api_entreprise/exercices_adapter_spec.rb
@@ -10,25 +10,15 @@ describe ApiEntreprise::ExercicesAdapter do
       .to_return(body: File.read('spec/fixtures/files/api_entreprise/exercices.json', status: 200))
   end
 
-  it '#to_params class est un Hash ?' do
-    expect(subject).to be_an_instance_of(Hash)
-  end
+  it { is_expected.to be_an_instance_of(Hash) }
 
-  it 'have 3 exercices' do
+  it 'contains several exercices attributes' do
     expect(subject[:exercices_attributes].size).to eq(3)
   end
 
-  context 'Attributs Exercices' do
-    it 'L\'exercice contient bien un ca' do
-      expect(subject[:exercices_attributes][0][:ca]).to eq('21009417')
-    end
-
-    it 'L\'exercice contient bien une date de fin d\'exercice' do
-      expect(subject[:exercices_attributes][0][:date_fin_exercice]).to eq("2013-12-31T00:00:00+01:00")
-    end
-
-    it 'L\'exercice contient bien une date_fin_exercice_timestamp' do
-      expect(subject[:exercices_attributes][0][:date_fin_exercice_timestamp]).to eq(1388444400)
-    end
+  it 'contains informations in each exercices_attributes' do
+    expect(subject[:exercices_attributes][0][:ca]).to eq('21009417')
+    expect(subject[:exercices_attributes][0][:date_fin_exercice]).to eq("2013-12-31T00:00:00+01:00")
+    expect(subject[:exercices_attributes][0][:date_fin_exercice_timestamp]).to eq(1388444400)
   end
 end
