ffeb82b947b445a5bf95306e8b5213165ee45249
clemkeirua <clement@keiruaprod.fr>
2019-05-03 16:12:24 +0200

    handle Gon + add report-uri URL

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,20 +1,22 @@
 Rails.application.config.content_security_policy do |policy|
   # En cas de non respect d'une des règles, faire un POST sur cette URL
-  policy.report_uri  "/csp-violation-report-endpoint"
-  # Nos whitelist
-  policy.img_src     :self, "https://*.openstreetmap.org"
-  # sendinblue et matomo, et… miniprofiler :(
-  # https://github.com/MiniProfiler/rack-mini-profiler/issues/327
+  policy.report_uri "https://e30e0ed9c14194254481124271b34a72.report-uri.com/r/d/csp/reportOnly"
+  # Whitelist image
+  policy.img_src :self, "https://*.openstreetmap.org"
+  # Whitelist JS: nous, sendinblue et matomo, et… miniprofiler :(
   if Rails.env.development?
-    #policy.script_src  :self, "https://sibautomation.com", "//stats.data.gouv.fr", :unsafe_eval, :unsafe_inline
-    policy.script_src  :self, "https://sibautomation.com", "//stats.data.gouv.fr", :unsafe_eval
+    # https://github.com/MiniProfiler/rack-mini-profiler/issues/327
+    policy.script_src :self, "https://sibautomation.com", "//stats.data.gouv.fr", :unsafe_eval, :unsafe_inline
   else
-    policy.script_src  :self, "https://sibautomation.com", "//stats.data.gouv.fr"
+    policy.script_src :self, "https://sibautomation.com", "//stats.data.gouv.fr"
   end
+  # Génération d'un nonce pour les balises script inline qu'on maitrise (Gon)
+  Rails.application.config.content_security_policy_nonce_generator = -> _request { SecureRandom.base64(16) }
+
   # Pour les CSS, on a beaucoup de style inline et quelques balises <style>
   # c'est trop compliqué pour être rectifié immédiatement (et sans valeur ajoutée:
-  # c'est hardocodé dans les vues, donc pas injectable).
-  policy.style_src   :self, :unsafe_inline
+  # c'est hardcodé dans les vues, donc pas injectable).
+  policy.style_src :self, :unsafe_inline
   # Pour tout le reste, par défaut on accepte uniquement ce qui vient de chez nous
   # et dans la notification on inclue la source de l'erreur
   policy.default_src :self, :data, :report_sample
