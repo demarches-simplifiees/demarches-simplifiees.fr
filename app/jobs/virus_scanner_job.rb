63d9c769b49b9dd4770c478a7f79789d2d93a987
clemkeirua <clement@keiruaprod.fr>
2019-05-16 17:17:10 +0200

    fix missing metadata

--- app/jobs/virus_scanner_job.rb
+++ app/jobs/virus_scanner_job.rb
@@ -1,7 +1,11 @@
 class VirusScannerJob < ApplicationJob
   def perform(blob)
     metadata = extract_metadata_via_virus_scanner(blob)
-    blob.update!(metadata: blob.metadata.merge(metadata))
+    if blob.metadata.present?
+      blob.update!(metadata: blob.metadata.merge(metadata))
+    else
+      blob.update!(metadata: metadata)
+    end
   end
 
   def extract_metadata_via_virus_scanner(blob)
