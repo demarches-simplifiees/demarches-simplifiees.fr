ff44b7a6002805ac221b7b1f447c27bf87fa6e1b
Paul Chavard <git@paul.chavard.net>
2019-05-29 12:05:28 +0200

    Refactor purge pj to be more generic

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -6,4 +6,11 @@ class AttachmentsController < ApplicationController
     @attachment = @blob.attachments.find(params[:id])
     @user_can_upload = params[:user_can_upload]
   end
+
+  def destroy
+    attachment = @blob.attachments.find(params[:id])
+    @attachment_id = attachment.id
+    attachment.purge_later
+    flash.now.notice = 'La pièce jointe a bien été supprimée.'
+  end
 end
