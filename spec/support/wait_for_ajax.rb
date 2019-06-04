6d312e17162868ca59b0f92f1ec52bba487eda78
Paul Chavard <git@paul.chavard.net>
2019-06-04 15:06:49 +0200

    spec: refactor `wait_for_ajax` to work with out new ajax utils
    `wait_for_ajax` is not used anymore, but it may be in the future.

--- spec/support/wait_for_ajax.rb
+++ spec/support/wait_for_ajax.rb
@@ -1,12 +1,6 @@
 module WaitForAjax
   def wait_for_ajax
-    Timeout.timeout(Capybara.default_max_wait_time) do
-      loop until finished_all_ajax_requests?
-    end
-  end
-
-  def finished_all_ajax_requests?
-    page.evaluate_script('jQuery.active').zero?
+    expect(page).to have_selector('body[data-active-requests-count="0"]')
   end
 end
 
