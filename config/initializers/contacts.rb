5653d78a17541095594a9f2c0d212109395c5f34
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2019-06-21 09:23:00 -1000

    [DEM-120] corrected rgpd mail address

--- config/initializers/contacts.rb
+++ config/initializers/contacts.rb
@@ -1,6 +1,6 @@
 if !defined?(CONTACT_EMAIL)
   CONTACT_EMAIL = "mes-demarches@modernisation.gov.pf" # "mes-demarches@modernisation.gov.pf"
-  EQUIPE_EMAIL = "rgpd-mes-demarches@modernisation.gov.pf"
+  EQUIPE_EMAIL = "rgpd-mes-demarches@informatique.gov.pf"
   TECH_EMAIL = CONTACT_EMAIL
 
   CONTACT_PHONE = "40 47 24 75"
