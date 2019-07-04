91000461ae85a5201b9044c6c138a8027ab091a5
simon lehericey <mail@simon.lehericey.net>
2019-07-04 16:49:33 +0200

    Activate device email change confirmation

--- db/schema.rb
+++ db/schema.rb
@@ -605,6 +605,7 @@ ActiveRecord::Schema.define(version: 2019_07_04_144304) do
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.text "unconfirmed_email"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
