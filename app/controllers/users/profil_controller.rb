5ebb807df1a4f837d7775f6d4cfeb1071aa85d1a
simon lehericey <mail@simon.lehericey.net>
2019-07-08 10:40:50 +0200

    [fix #1709] A user can change its email

--- app/controllers/users/profil_controller.rb
+++ app/controllers/users/profil_controller.rb
@@ -8,5 +8,24 @@ module Users
       flash.now.notice = 'Votre jeton a été regénéré.'
       render :show
     end
+
+    def update_email
+      if @current_user.update(update_email_params)
+        flash.notice = t('devise.registrations.update_needs_confirmation')
+      # to avoid leaking who has signed in
+      elsif @current_user.errors&.details&.dig(:email)&.any? { |e| e[:error] == :taken }
+        flash.notice = t('devise.registrations.update_needs_confirmation')
+      else
+        flash.alert = @current_user.errors.full_messages
+      end
+
+      redirect_to profil_path
+    end
+
+    private
+
+    def update_email_params
+      params.require(:user).permit(:email)
+    end
   end
 end
