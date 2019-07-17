f88d7c28fadfbe63be3f0eae314581664b276c7e
Pierre de La Morinerie <kemenaran@gmail.com>
2019-07-17 16:42:34 +0200

    emails: add a "Read message" button to the new answer email

--- app/mailers/dossier_mailer.rb
+++ app/mailers/dossier_mailer.rb
@@ -1,6 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/dossier_mailer
 class DossierMailer < ApplicationMailer
   helper ServiceHelper
+  helper MailerHelper
 
   layout 'mailers/layout'
 
