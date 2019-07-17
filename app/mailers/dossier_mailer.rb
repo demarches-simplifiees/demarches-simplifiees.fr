71dfb3878765de806dfeedcb6eee6fdeb0599ec6
Pierre de La Morinerie <kemenaran@gmail.com>
2019-07-17 13:42:14 +0200

    emails: improve typography of titles

--- app/mailers/dossier_mailer.rb
+++ app/mailers/dossier_mailer.rb
@@ -4,7 +4,7 @@ class DossierMailer < ApplicationMailer
 
   def notify_new_draft(dossier)
     @dossier = dossier
-    subject = "Retrouvez votre brouillon pour la démarche \"#{dossier.procedure.libelle}\""
+    subject = "Retrouvez votre brouillon pour la démarche « #{dossier.procedure.libelle} »"
 
     mail(to: dossier.user.email, subject: subject)
   end
@@ -20,21 +20,21 @@ class DossierMailer < ApplicationMailer
 
   def notify_deletion_to_user(deleted_dossier, to_email)
     @deleted_dossier = deleted_dossier
-    subject = "Votre dossier n° #{@deleted_dossier.dossier_id} a bien été supprimé"
+    subject = "Votre dossier nº #{@deleted_dossier.dossier_id} a bien été supprimé"
 
     mail(to: to_email, subject: subject)
   end
 
   def notify_deletion_to_administration(deleted_dossier, to_email)
     @deleted_dossier = deleted_dossier
-    subject = "Le dossier n° #{@deleted_dossier.dossier_id} a été supprimé à la demande de l'usager"
+    subject = "Le dossier nº #{@deleted_dossier.dossier_id} a été supprimé à la demande de l'usager"
 
     mail(to: to_email, subject: subject)
   end
 
   def notify_unhide_to_user(dossier)
     @dossier = dossier
-    subject = "Votre dossier n° #{@dossier.id} n'a pas pu être supprimé"
+    subject = "Votre dossier nº #{@dossier.id} n'a pas pu être supprimé"
 
     mail(to: dossier.user.email, subject: subject)
   end
@@ -42,14 +42,14 @@ class DossierMailer < ApplicationMailer
   def notify_undelete_to_user(dossier)
     @dossier = dossier
     @dossier_kind = dossier.brouillon? ? 'brouillon' : 'dossier'
-    @subject = "Votre #{@dossier_kind} n° #{@dossier.id} est à nouveau accessible"
+    @subject = "Votre #{@dossier_kind} nº #{@dossier.id} est à nouveau accessible"
 
     mail(to: dossier.user.email, subject: @subject)
   end
 
   def notify_revert_to_instruction(dossier)
     @dossier = dossier
-    @subject = "Votre dossier n° #{@dossier.id} est en train d'être réexaminé"
+    @subject = "Votre dossier nº #{@dossier.id} est en train d'être réexaminé"
 
     mail(to: dossier.user.email, subject: @subject)
   end
