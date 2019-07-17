b493f7ff6fa86e27e3f02f14899858bb01a57ddd
Pierre de La Morinerie <kemenaran@gmail.com>
2019-07-17 13:42:51 +0200

    emails: mention the procedure name in the email title

--- app/models/mails/initiated_mail.rb
+++ app/models/mails/initiated_mail.rb
@@ -7,7 +7,7 @@ module Mails
     SLUG = "initiated_mail"
     DEFAULT_TEMPLATE_NAME = "notification_mailer/initiated_mail"
     DISPLAYED_NAME = 'Accusé de réception'
-    DEFAULT_SUBJECT = 'Votre dossier demarches-simplifiees.fr nº --numéro du dossier-- a bien été reçu'
+    DEFAULT_SUBJECT = 'Votre dossier nº --numéro du dossier-- a bien été reçu (--libellé démarche--)'
     DOSSIER_STATE = Dossier.states.fetch(:en_construction)
   end
 end
