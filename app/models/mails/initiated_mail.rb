81970b9e8f7b1fb07a7040fd76f04d72150c8380
Pierre de La Morinerie <kemenaran@gmail.com>
2019-07-18 18:25:11 +0200

    mailers: move default templates to a specific subdirectory
    It makes more understandable that they are templates (and not views
    rendered directly by Rails).

--- app/models/mails/initiated_mail.rb
+++ app/models/mails/initiated_mail.rb
@@ -5,7 +5,7 @@ module Mails
     belongs_to :procedure
 
     SLUG = "initiated_mail"
-    DEFAULT_TEMPLATE_NAME = "notification_mailer/initiated_mail"
+    DEFAULT_TEMPLATE_NAME = "notification_mailer/default_templates/initiated_mail"
     DISPLAYED_NAME = 'Accusé de réception'
     DEFAULT_SUBJECT = 'Votre dossier nº --numéro du dossier-- a bien été reçu (--libellé démarche--)'
     DOSSIER_STATE = Dossier.states.fetch(:en_construction)
