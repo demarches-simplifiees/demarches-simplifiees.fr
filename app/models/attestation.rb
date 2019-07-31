fba195c0b5a181fe79e4c4be6977cf5aecd8b55e
Paul Chavard <git@paul.chavard.net>
2019-07-31 16:09:28 +0200

    Add attestation url to dossier on api

--- app/models/attestation.rb
+++ app/models/attestation.rb
@@ -2,4 +2,13 @@ class Attestation < ApplicationRecord
   belongs_to :dossier
 
   mount_uploader :pdf, AttestationUploader
+
+  def pdf_url
+    if Rails.application.secrets.fog[:enabled]
+      RemoteDownloader.new(pdf.path).url
+    elsif pdf&.url
+      # FIXME: this is horrible but used only in dev and will be removed after migration
+      File.join(LOCAL_DOWNLOAD_URL, pdf.url)
+    end
+  end
 end
