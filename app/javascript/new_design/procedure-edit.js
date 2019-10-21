02384b078c7161140fcd7b86c9838ac4140bdbab
Pierre de La Morinerie <kemenaran@gmail.com>
2019-10-21 17:18:05 +0200

    procedure: remove UI to edit the european flag
    This is an option we want to deprecate, and remove entirely in the
    future.
    
    Admins can still add the EU flag manually to their uploaded logo.

--- app/javascript/new_design/procedure-edit.js
+++ app/javascript/new_design/procedure-edit.js
@@ -1,4 +1,4 @@
-import { show, hide, delegate } from '@utils';
+import { delegate } from '@utils';
 
 function syncInputToElement(fromSelector, toSelector) {
   const fromElement = document.querySelector(fromSelector);
@@ -11,14 +11,8 @@ function syncInputToElement(fromSelector, toSelector) {
 function syncFormToPreview() {
   syncInputToElement('#procedure_libelle', 'h2.procedure-title');
   syncInputToElement('#procedure_description', '.procedure-description-body');
-
-  const euroFlagCheckbox = document.querySelector('#procedure_euro_flag');
-  const euroFlagLogo = document.querySelector('#euro_flag');
-  if (euroFlagCheckbox && euroFlagLogo) {
-    euroFlagCheckbox.checked ? show(euroFlagLogo) : hide(euroFlagLogo);
-  }
 }
 
 delegate('input', '#procedure-edit #procedure_libelle', syncFormToPreview);
 delegate('input', '#procedure-edit #procedure_description', syncFormToPreview);
-delegate('change', '#procedure-edit #procedure_euro_flag', syncFormToPreview);
+
