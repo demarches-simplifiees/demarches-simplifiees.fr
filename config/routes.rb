d8cc99b84a564a83902c685cd9678a9fd62b851a
simon lehericey <mail@simon.lehericey.net>
2019-10-23 20:51:25 +0200

    Groupe instructeur create

--- config/routes.rb
+++ config/routes.rb
@@ -350,7 +350,7 @@ Rails.application.routes.draw do
         get 'annotations'
       end
 
-      resources :groupe_instructeurs, only: [:index, :show]
+      resources :groupe_instructeurs, only: [:index, :show, :create]
 
       resources :administrateurs, controller: 'procedure_administrateurs', only: [:index, :create, :destroy]
 
