9bd0221ec3f4491601e932fd5b9e602c4c3d60e4
clemkeirua <clement@keiruaprod.fr>
2019-10-31 16:31:40 +0100

    disable retry sending a mail with an invalid email adress

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,6 +3,10 @@ class ApplicationMailer < ActionMailer::Base
   default from: "demarches-simplifiees.fr <#{CONTACT_EMAIL}>"
   layout 'mailer'
 
+  rescue_from Net::SMTPSyntaxError do |_error|
+    message.perform_deliveries = false
+  end
+
   # Attach the procedure logo to the email (if any).
   # Returns the attachment url.
   def attach_logo(procedure)
