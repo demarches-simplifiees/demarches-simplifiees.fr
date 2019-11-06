f2fed6555859de89909e102cead8158ddabdb874
Pierre de La Morinerie <kemenaran@gmail.com>
2019-11-06 11:59:15 +0100

    assigns: fix search among the unassigned instructeurs
    This was broken since moving the email out of the Instructeur column.

--- app/controllers/admin/assigns_controller.rb
+++ app/controllers/admin/assigns_controller.rb
@@ -18,7 +18,7 @@ class Admin::AssignsController < AdminController
     not_assign_scope = current_administrateur.instructeurs.where.not(id: assign_scope.ids)
 
     if params[:filter]
-      not_assign_scope = not_assign_scope.where("email LIKE ?", "%#{params[:filter]}%")
+      not_assign_scope = not_assign_scope.where('users.email LIKE ?', "%#{params[:filter]}%")
     end
 
     @instructeurs_not_assign = smart_listing_create :instructeurs_not_assign,
