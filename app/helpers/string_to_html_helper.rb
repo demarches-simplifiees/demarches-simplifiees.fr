4f899f94f4c369b3e5295325c2250c03de19c91d
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2019-11-08 15:47:09 -1000

    Merge fr tag 2019-11-07-01 into devpf
    # Conflicts:
    #	app/helpers/string_to_html_helper.rb
    #	app/views/root/_footer.html.haml
    #	app/views/root/landing.html.haml
    #	app/views/users/_general_footer_row.html.haml
    #	app/views/users/_procedure_footer.html.haml

--- app/helpers/string_to_html_helper.rb
+++ app/helpers/string_to_html_helper.rb
@@ -3,7 +3,7 @@ module StringToHtmlHelper
     tags = ['a', 'abbr', 'acronym', 'address', 'b', 'big', 'blockquote', 'br', 'cite', 'code', 'dd', 'del', 'dfn', 'div', 'dl', 'dt', 'em', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'i', 'img', 'ins', 'kbd', 'li', 'ol', 'p', 'pre', 'samp', 'small', 'span', 'sub', 'sup', 'table', 'td', 'th', 'tr', 'tt', 'u', 'ul', 'var', 'strong']
     atts = ['target', 'rel', 'href', 'class']
     html_formatted = "<#{wrapper_tag}>" + str.to_s.gsub(/(?<=[^>])\r?\n/, "\n<br>") + "</#{wrapper_tag}>"
-    #html_formatted = simple_format(str, {}, { wrapper_tag: wrapper_tag })
+    # html_formatted = simple_format(str, {}, { wrapper_tag: wrapper_tag })
     with_links = html_formatted.gsub(URI.regexp, '<a target="_blank" rel="noopener" href="\0">\0</a>')
     sanitize(with_links, tags: tags, attributes: atts)
   end
