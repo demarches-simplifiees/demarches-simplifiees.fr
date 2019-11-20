ca52141f2c4d723a980a483571157b7d6fca54a4
Paul Chavard <git@paul.chavard.net>
2019-11-20 16:03:40 +0100

    ActiveStorage url should expire after an hour

---
+++ app/models/concerns/blob_signed_id_concern.rb
@@ -0,0 +1,12 @@
+module BlobSignedIdConcern
+  extend ActiveSupport::Concern
+
+  included do
+    # We override signed_id to add `expires_in` option to generated hash.
+    # This is a measure to ensure that we never under any circumstance
+    # expose permanent attachment url
+    def signed_id
+      ActiveStorage.verifier.generate(id, purpose: :blob_id, expires_in: ActiveStorage::Service.url_expires_in)
+    end
+  end
+end
