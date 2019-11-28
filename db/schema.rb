0d6675a2aecc046dd0a950fbb72b8dbbbbdc3d6a
Paul Chavard <git@paul.chavard.net>
2019-11-28 18:22:06 +0100

    Fix schema timestamp

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_27_084643) do
+ActiveRecord::Schema.define(version: 2019_11_14_113700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
