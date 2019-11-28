ff4f5b75ad4b606e6bd82ccfce16d80a33bd9ff8
Pierre de La Morinerie <kemenaran@gmail.com>
2019-11-28 13:39:17 +0000

    app: display a deprecation banner for IE 11

--- config/initializers/browser.rb
+++ config/initializers/browser.rb
@@ -1,7 +1,6 @@
 # See .browserslistrc
 Browser.modern_rules.clear
 Browser.modern_rules << -> b { b.chrome? && b.version.to_i >= 50 }
-Browser.modern_rules << -> b { b.ie? && b.version.to_i >= 11 && !b.compatibility_view? }
 Browser.modern_rules << -> b { b.edge? && b.version.to_i >= 14 && !b.compatibility_view? }
 Browser.modern_rules << -> b { b.firefox? && b.version.to_i >= 50 }
 Browser.modern_rules << -> b { b.opera? && b.version.to_i >= 40 }
