80b68afab582610c9a300f0681e4f7baff85a344
clemkeirua <clement@keiruaprod.fr>
2019-12-02 14:20:26 +0100

    implement email suggestion

--- app/javascript/new_design/user-sign_up.js
+++ app/javascript/new_design/user-sign_up.js
@@ -1,20 +1,30 @@
 import { on, show, hide } from '@utils';
+import { suggest } from 'email-butler';
 
-const USER_NEW_EMAIL_SELECTOR = '#new_user > #user_email';
+const user_new_email_selector = '#new_user > #user_email';
 const suspectSuggestionsBox = document.querySelector('.suspect-email');
-const emailSuggestionSpan = document.querySelector(".suspect-email .question .suggested-email");
+const emailSuggestionSpan = document.querySelector(
+  '.suspect-email .question .suggested-email'
+);
 
-on(USER_NEW_EMAIL_SELECTOR, 'blur', _ => {
-  emailSuggestionSpan.innerHTML = 'bidou@plop.com';
-  show(suspectSuggestionsBox)
+on(user_new_email_selector, 'blur', () => {
+  // When the user leaves the email input during account creation, we check if this account looks correct.
+  // If not (e.g if its "bidou@gmail.coo" or "john@yahoo.rf") we attempt to suggest a fix for the invalid email.
+  const suggestion = suggest(
+    document.querySelector(user_new_email_selector).value
+  );
+  if (suggestion.full) {
+    emailSuggestionSpan.innerHTML = suggestion.full;
+    show(suspectSuggestionsBox);
+  }
 });
 
 export function acceptEmailSuggestion() {
-  document.querySelector(USER_NEW_EMAIL_SELECTOR).value = emailSuggestionSpan.innerHTML;
+  document.querySelector(user_new_email_selector).value =
+    emailSuggestionSpan.innerHTML;
   hide(suspectSuggestionsBox);
 }
 
 export function discardEmailSuggestionBox() {
   hide(suspectSuggestionsBox);
 }
-
