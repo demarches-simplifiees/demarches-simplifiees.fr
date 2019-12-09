2f81f8338135c693ec45d81784bf40c77dfa3362
simon lehericey <mail@simon.lehericey.net>
2019-12-09 17:11:12 +0100

    [link to #4557] An instructeur cannot change its email on its own

--- spec/controllers/users/profil_controller_spec.rb
+++ spec/controllers/users/profil_controller_spec.rb
@@ -60,5 +60,18 @@ describe Users::ProfilController, type: :controller do
       it { expect(response).to redirect_to(profil_path) }
       it { expect(flash.alert).to eq(['Email invalide']) }
     end
+
+    context 'when the user has an instructeur role' do
+      let(:instructeur_email) { 'instructeur_email@a.com' }
+      let!(:user) { create(:instructeur, email: instructeur_email).user }
+
+      before do
+        patch :update_email, params: { user: { email: 'loulou@lou.com' } }
+        user.reload
+      end
+
+      it { expect(user.unconfirmed_email).to be_nil }
+      it { expect(response).to redirect_to(profil_path) }
+    end
   end
 end
