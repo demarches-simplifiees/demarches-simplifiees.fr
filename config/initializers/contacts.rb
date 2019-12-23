c36623b6e1990fa6754fd9603a458ade13c39dc2
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2019-12-23 12:43:15 -1000

    Temp fix for gmail account not receiving emails mes-demarches sender set to mes-demarches@modernisation.gov.pf to use mailjet instead of SIPf gateway.

--- config/initializers/contacts.rb
+++ config/initializers/contacts.rb
@@ -2,7 +2,7 @@ if !defined?(CONTACT_EMAIL)
   CONTACT_EMAIL = "mes-demarches@modernisation.gov.pf" # "mes-demarches@modernisation.gov.pf"
   EQUIPE_EMAIL = "rgpd-mes-demarches@informatique.gov.pf"
   TECH_EMAIL = CONTACT_EMAIL
-  NO_REPLY_EMAIL = "Ne pas répondre <ne-pas-repondre@mes-demarches.gov.pf>"
+  NO_REPLY_EMAIL = "Ne pas répondre <mes-demarches@modernisation.gov.pf>"
   CONTACT_PHONE = "40 47 24 75"
 
   OLD_CONTACT_EMAIL = "contact@tps.apientreprise.fr"
