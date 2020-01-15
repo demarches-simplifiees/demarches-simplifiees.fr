d4de5769ee10bb17e35cb6d1681ddf5491ed5231
Christophe Robillard <geek@robiweb.net>
2020-01-15 14:31:30 +0100

    empêche la suppression d'un user qui est admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -97,7 +97,7 @@ class User < ApplicationRecord
   end
 
   def can_be_deleted?
-    dossiers.state_instruction_commencee.empty?
+    administrateur.nil? && dossiers.state_instruction_commencee.empty?
   end
 
   def delete_and_keep_track_dossiers(administration)
