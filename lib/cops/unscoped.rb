95d151b3ca60f6c3d25ac02b5de0faf2000f077f
clemkeirua <clement@keiruaprod.fr>
2020-01-30 15:14:33 +0100

    only load unscoped custom rule in development

--- lib/cops/unscoped.rb
+++ lib/cops/unscoped.rb
@@ -1,16 +1,18 @@
-module RuboCop
-  module Cop
-    module DS
-      class Unscoped < Cop
-        MSG = "Avoid using `unscoped`. Instead unscope specific clauses by using `unscope(where: :attribute)`."
+if ENV["RAILS_ENV"] == "development"
+  module RuboCop
+    module Cop
+      module DS
+        class Unscoped < Cop
+          MSG = "Avoid using `unscoped`. Instead unscope specific clauses by using `unscope(where: :attribute)`."
 
-        def_node_matcher :unscoped?, <<-END
-          (send _ :unscoped)
-        END
+          def_node_matcher :unscoped?, <<-END
+            (send _ :unscoped)
+          END
 
-        def on_send(node)
-          return unless unscoped?(node)
-          add_offense(node)
+          def on_send(node)
+            return unless unscoped?(node)
+            add_offense(node)
+          end
         end
       end
     end
