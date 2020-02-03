58894537d14c7ad500c6172bb566d86b81e0871b
Christophe Robillard <geek@robiweb.net>
2020-02-03 16:33:47 +0100

    remove useless condition to admin that can be deleted
    administrateur can be deleted only if he/she has a procedure where
    he/she is the only admin

--- spec/models/administrateur_spec.rb
+++ spec/models/administrateur_spec.rb
@@ -24,19 +24,19 @@ describe Administrateur, type: :model do
   describe "#can_be_deleted?" do
     subject { administrateur.can_be_deleted? }
 
-    context 'when the administrateur has a dossier in instruction' do
-      let!(:dossier) { create(:dossier, :en_instruction) }
-      let(:administrateur) { dossier.procedure.administrateurs.first }
-
-      it { is_expected.to be false }
-    end
-
     context "when the administrateur's procedures have other administrateurs" do
       let!(:administrateur) { create(:administrateur) }
       let!(:autre_administrateur) { create(:administrateur) }
       let!(:procedure) { create(:procedure, administrateurs: [administrateur, autre_administrateur]) }
 
       it { is_expected.to be true }
+    end
+
+    context "when the administrateur has a procedure where he/she is the only admin" do
+      let!(:administrateur) { create(:administrateur) }
+      let!(:procedure) { create(:procedure, administrateurs: [administrateur]) }
+
+      it { is_expected.to be false }
     end
 
     context "when the administrateur has no procedure" do
