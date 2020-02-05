3c967aa653106e81db055657e5bb9a4e05538d05
clemkeirua <clement@keiruaprod.fr>
2020-02-05 10:32:20 +0100

    openly fail when not delivering mail

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,11 +3,6 @@ class ApplicationMailer < ActionMailer::Base
   default from: "demarches-simplifiees.fr <#{CONTACT_EMAIL}>"
   layout 'mailer'
 
-  # Don’t retry to send a message if the server rejects the recipient address
-  rescue_from Net::SMTPSyntaxError do |_error|
-    message.perform_deliveries = false
-  end
-
   # Attach the procedure logo to the email (if any).
   # Returns the attachment url.
   def attach_logo(procedure)
