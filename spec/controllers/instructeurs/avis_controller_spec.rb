7afeae14083f0237dddd9df1e52d04a60ece2b79
Paul Chavard <git@paul.chavard.net>
2020-02-11 17:38:04 +0100

    remove dossier.linked_dossiers

--- spec/controllers/instructeurs/avis_controller_spec.rb
+++ spec/controllers/instructeurs/avis_controller_spec.rb
@@ -204,7 +204,7 @@ describe Instructeurs::AvisController, type: :controller do
           context 'and the expert can access the linked dossiers' do
             let(:created_avis) { Avis.last(2).first }
             let(:linked_avis) { Avis.last }
-            let(:linked_dossier) { dossier.reload.linked_dossiers.first }
+            let(:linked_dossier) { Dossier.find_by(id: dossier.reload.champs.filter(&:dossier_link?).map(&:value).compact) }
             let(:invite_linked_dossiers) do
               instructeur.assign_to_procedure(linked_dossier.procedure)
               true
