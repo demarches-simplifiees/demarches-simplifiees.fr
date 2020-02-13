f8ed7ec051d760f72cf88c732a53de0edddbaad3
Pierre de La Morinerie <kemenaran@gmail.com>
2020-02-13 10:07:10 +0000

    form: display index of section in header

--- app/models/champs/header_section_champ.rb
+++ app/models/champs/header_section_champ.rb
@@ -2,4 +2,10 @@ class Champs::HeaderSectionChamp < Champ
   def search_terms
     # The user cannot enter any information here so it doesn’t make much sense to search
   end
+
+  def section_index
+    siblings
+      .filter { |c| c.type_champ == TypeDeChamp.type_champs.fetch(:header_section) }
+      .index(self) + 1
+  end
 end
