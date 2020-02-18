4e116f06a6504ded258af327f7cea3885db98e6e
Paul Chavard <git@paul.chavard.net>
2020-02-18 17:15:32 +0100

    Add notify_automatic_deletion_to_user mailer

--- app/mailers/dossier_mailer.rb
+++ app/mailers/dossier_mailer.rb
@@ -83,4 +83,11 @@ class DossierMailer < ApplicationMailer
 
     mail(to: user.email, subject: @subject)
   end
+
+  def notify_automatic_deletion_to_user(user, dossier_hashes)
+    @subject = default_i18n_subject(count: dossier_hashes.count)
+    @dossier_hashes = dossier_hashes
+
+    mail(to: user.email, subject: @subject)
+  end
 end
