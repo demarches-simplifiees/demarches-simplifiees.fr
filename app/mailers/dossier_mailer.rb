318baf316ee06921ea5a2a2b3f31ddbef5bcfcd3
Paul Chavard <git@paul.chavard.net>
2020-02-18 17:18:14 +0100

    Add notify_en_construction_near_deletion mailer

--- app/mailers/dossier_mailer.rb
+++ app/mailers/dossier_mailer.rb
@@ -97,4 +97,12 @@ class DossierMailer < ApplicationMailer
 
     mail(to: user.email, subject: @subject)
   end
+
+  def notify_en_construction_near_deletion(user, dossiers, for_user)
+    @subject = default_i18n_subject(count: dossiers.count)
+    @dossiers = dossiers
+    @for_user = for_user
+
+    mail(to: user.email, subject: @subject)
+  end
 end
