227c5d5125c86154f1deddbad86c7c20fd5f4eb9
simon lehericey <mail@simon.lehericey.net>
2020-03-05 10:24:15 +0100

    refacto

--- app/services/expired_dossiers_deletion_service.rb
+++ app/services/expired_dossiers_deletion_service.rb
@@ -10,21 +10,16 @@ class ExpiredDossiersDeletionService
   end
 
   def self.send_brouillon_expiration_notices
-    dossiers_close_to_expiration = Dossier.brouillon_close_to_expiration
+    dossiers_close_to_expiration = Dossier
+      .brouillon_close_to_expiration
       .without_brouillon_expiration_notice_sent
-
-    users_to_notify = {}
 
     dossiers_close_to_expiration
       .includes(:user, :procedure)
-      .find_each do |dossier|
-        users_to_notify[dossier.user.email] ||= [dossier.user, Set.new]
-        users_to_notify[dossier.user.email].last.add(dossier)
+      .group_by(&:user)
+      .each do |(user, dossiers)|
+        DossierMailer.notify_brouillon_near_deletion(user, dossiers).deliver_later
       end
-
-    users_to_notify.each_value do |(user, dossiers)|
-      DossierMailer.notify_brouillon_near_deletion(user, dossiers).deliver_later
-    end
 
     dossiers_close_to_expiration.update_all(brouillon_close_to_expiration_notice_sent_at: Time.zone.now)
   end
@@ -57,26 +52,16 @@ class ExpiredDossiersDeletionService
   end
 
   def self.delete_expired_brouillons_and_notify
-    dossier_to_remove = []
-    users_to_notify = {}
+    dossiers_to_remove = Dossier.brouillon_expired
 
-    Dossier.brouillon_expired
+    dossiers_to_remove
       .includes(:user, :procedure)
-      .find_each do |dossier|
-        dossier_to_remove << dossier
-
-        users_to_notify[dossier.user.email] ||= [dossier.user, Set.new]
-        users_to_notify[dossier.user.email].last.add(dossier)
+      .group_by(&:user)
+      .each do |(user, dossiers)|
+        DossierMailer.notify_brouillon_deletion(user, dossiers.map(&:hash_for_deletion_mail)).deliver_later
       end
 
-    users_to_notify.each_value do |(user, dossiers)|
-      DossierMailer.notify_brouillon_deletion(
-        user,
-        dossiers.map(&:hash_for_deletion_mail)
-      ).deliver_later
-    end
-
-    dossier_to_remove.each do |dossier|
+    dossiers_to_remove.each do |dossier|
       DeletedDossier.create_from_dossier(dossier)
       dossier.destroy
     end
