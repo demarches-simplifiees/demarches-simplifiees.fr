43d49910f967e5671b380a2ebc0ce53a417492d2
Paul Chavard <git@paul.chavard.net>
2020-03-11 16:57:29 +0100

    Fix attestation_template spec

--- spec/factories/attestation_template.rb
+++ spec/factories/attestation_template.rb
@@ -10,4 +10,9 @@ FactoryBot.define do
     logo { Rack::Test::UploadedFile.new("./spec/fixtures/files/logo_test_procedure.png", 'image/png') }
     signature { Rack::Test::UploadedFile.new("./spec/fixtures/files/logo_test_procedure.png", 'image/png') }
   end
+
+  trait :with_gif_files do
+    logo { Rack::Test::UploadedFile.new('./spec/fixtures/files/french-flag.gif', 'image/gif') }
+    signature { Rack::Test::UploadedFile.new('./spec/fixtures/files/beta-gouv.gif', 'image/gif') }
+  end
 end
