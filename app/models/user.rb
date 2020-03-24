08a5dd6a27658ea97065b78afe36b06ff1cc97b7
Paul Chavard <git@paul.chavard.net>
2020-03-24 15:56:07 +0100

    User should not be deleted if they have hidden dossiers with state_instruction_commencee
    fix #4933

--- app/models/user.rb
+++ app/models/user.rb
@@ -111,7 +111,7 @@ class User < ApplicationRecord
   end
 
   def can_be_deleted?
-    administrateur.nil? && instructeur.nil? && dossiers.state_instruction_commencee.empty?
+    administrateur.nil? && instructeur.nil? && dossiers.with_discarded.state_instruction_commencee.empty?
   end
 
   def delete_and_keep_track_dossiers(administration)
