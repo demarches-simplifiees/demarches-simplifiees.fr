6fe2bf5f27a992d10992a453f52c4cb70a1fe1a4
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2020-03-25 14:55:44 -1000

    FreeScout: contact mail set to mes.demarches.en.polynesie at gmail

--- config/initializers/contacts.rb
+++ config/initializers/contacts.rb
@@ -1,8 +1,8 @@
 if !defined?(CONTACT_EMAIL)
-  CONTACT_EMAIL = "mes-demarches@modernisation.gov.pf" # "mes-demarches@modernisation.gov.pf"
+  CONTACT_EMAIL = "mes.demarches.en.polynesie" + '@gmail.com' # "mes-demarches@modernisation.gov.pf"
   EQUIPE_EMAIL = "rgpd-mes-demarches@informatique.gov.pf"
   TECH_EMAIL = CONTACT_EMAIL
-  NO_REPLY_EMAIL = "Ne pas répondre <mes-demarches@modernisation.gov.pf>"
+  NO_REPLY_EMAIL = "Ne pas répondre <mes.demarches.en.polynesie" + "@gmail.com>"
   CONTACT_PHONE = "40 47 24 75"
 
   OLD_CONTACT_EMAIL = "contact@tps.apientreprise.fr"
