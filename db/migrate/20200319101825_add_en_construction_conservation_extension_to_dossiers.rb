105893accba8c8cff0993df17981ff6569aa9af2
Paul Chavard <git@paul.chavard.net>
2020-03-26 11:45:48 +0100

    Do not add default to en_construction_conservation_extension

--- db/migrate/20200319101825_add_en_construction_conservation_extension_to_dossiers.rb
+++ db/migrate/20200319101825_add_en_construction_conservation_extension_to_dossiers.rb
@@ -1,5 +1,5 @@
 class AddEnConstructionConservationExtensionToDossiers < ActiveRecord::Migration[5.2]
   def change
-    add_column :dossiers, :en_construction_conservation_extension, :interval, :default => 0.days
+    add_column :dossiers, :en_construction_conservation_extension, :interval
   end
 end
