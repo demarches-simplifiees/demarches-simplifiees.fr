4824369cea8a2d1fbbf657d3af6cb43c2c8b0468
Pierre de La Morinerie <kemenaran@gmail.com>
2020-03-30 14:20:23 +0000

    specs: remove unused wait-for-ajax helper
    This helper is:
    - no longer used;
    - buggy (not all requests increment it);
    - discouraged (we should instead match an UI change that signals the end
    of an ajax request).
    
    Good riddance.

--- spec/support/wait_for_ajax.rb
+++
@@ -1,9 +0,0 @@
-module WaitForAjax
-  def wait_for_ajax
-    expect(page).to have_selector('body[data-active-requests-count="0"]')
-  end
-end
-
-RSpec.configure do |config|
-  config.include WaitForAjax, type: :feature
-end
