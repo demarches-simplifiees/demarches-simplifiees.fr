3e21b781421467fce62c43bc4102b4a0356012f2
Pierre de La Morinerie <kemenaran@gmail.com>
2020-04-07 15:32:14 +0200

    Revert "feat/4893 - migrate the mapReader to mapbox-gl with react"

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -9,9 +9,6 @@ const resolve = {
 
 environment.splitChunks();
 environment.config.merge({ resolve });
-environment.loaders.get(
-  'nodeModules'
-).exclude = /(?:@?babel(?:\/|\\{1,2}|-).+)|regenerator-runtime|core-js|^webpack$|^webpack-assets-manifest$|^webpack-cli$|^webpack-sources$|^@rails\/webpacker$|^mapbox-gl$|/;
 
 // Uncoment next lines to run webpack-bundle-analyzer
 // const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
