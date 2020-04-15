e97f6899b5a90b0a133b1475235a7ee07fdd79ce
Pierre de La Morinerie <kemenaran@gmail.com>
2020-04-15 14:20:31 +0000

    javascript: move auto-upload attachment to the Uploader class
    Rationale:
    
    - It makes more sense to handle the progress bar updates in a single class;
    - This will allow us to unify the error handling.

--- app/javascript/shared/activestorage/uploader.js
+++ app/javascript/shared/activestorage/uploader.js
@@ -1,4 +1,5 @@
 import { DirectUpload } from '@rails/activestorage';
+import { ajax } from '@utils';
 import ProgressBar from './progress-bar';
 import errorFromDirectUploadMessage from './errors';
 
@@ -7,27 +8,63 @@ import errorFromDirectUploadMessage from './errors';
   used to track lifecycle and progress of an upload.
   */
 export default class Uploader {
-  constructor(input, file, directUploadUrl) {
+  constructor(input, file, directUploadUrl, autoAttachUrl) {
     this.directUpload = new DirectUpload(file, directUploadUrl, this);
     this.progressBar = new ProgressBar(input, this.directUpload.id, file);
+    this.autoAttachUrl = autoAttachUrl;
   }
 
-  start() {
+  /**
+    Upload (and optionally attach) the file.
+    Returns the blob signed id on success.
+    */
+  async start() {
     this.progressBar.start();
 
+    try {
+      let blobSignedId = await this._upload();
+
+      if (this.autoAttachUrl) {
+        await this._attach(blobSignedId);
+      }
+
+      this.progressBar.end();
+      this.progressBar.destroy();
+
+      return blobSignedId;
+    } catch (error) {
+      this.progressBar.error(error.message);
+      throw error;
+    }
+  }
+
+  /**
+    Upload the file using the DirectUpload instance, and return the blob signed_id.
+    */
+  async _upload() {
     return new Promise((resolve, reject) => {
       this.directUpload.create((errorMsg, attributes) => {
         if (errorMsg) {
-          this.progressBar.error(errorMsg);
           let error = errorFromDirectUploadMessage(errorMsg);
           reject(error);
         } else {
           resolve(attributes.signed_id);
         }
-        this.progressBar.end();
-        this.progressBar.destroy();
       });
     });
+  }
+
+  /**
+    Attach the file by sending a POST request to the autoAttachUrl.
+    */
+  async _attach(blobSignedId) {
+    const attachmentRequest = {
+      url: this.autoAttachUrl,
+      type: 'PUT',
+      data: `blob_signed_id=${blobSignedId}`
+    };
+
+    await ajax(attachmentRequest);
   }
 
   uploadRequestDidProgress(event) {
