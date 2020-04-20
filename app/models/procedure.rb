3c751ac69681adee0bdca80899f881923dcaad05
clemkeirua <clement@keiruaprod.fr>
2020-04-20 16:27:40 +0200

    small logo for procedures

--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -506,7 +506,7 @@ class Procedure < ApplicationRecord
     if logo.attached?
       Rails.application.routes.url_helpers.url_for(logo)
     else
-      ActionController::Base.helpers.image_url("marianne.svg")
+      ActionController::Base.helpers.image_url("republique-francaise-logo.svg")
     end
   end
 
