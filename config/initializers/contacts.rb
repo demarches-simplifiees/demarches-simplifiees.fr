88813fcba544b81b7adf56d2d1cedfb3c81c8aad
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2020-04-25 17:09:17 -1000

    Move to mes-demarches at administration.gov pf Tech email stays to mes-demarches@modernisation Old email set to mask old email in messagerie

--- config/initializers/contacts.rb
+++ config/initializers/contacts.rb
@@ -1,9 +1,9 @@
 if !defined?(CONTACT_EMAIL)
-  CONTACT_EMAIL = "mes-demarches" + '@administration.gov.pf' # "mes-demarches at modernisation.gov.pf"
-  EQUIPE_EMAIL = "rgpd-mes-demarches@informatique.gov.pf"
-  TECH_EMAIL = CONTACT_EMAIL
-  NO_REPLY_EMAIL = "Ne pas répondre <ne-pas-repondre" + "@administration.gov.pf>"
-  CONTACT_PHONE = "40 47 24 75"
+  CONTACT_EMAIL  = 'mes-demarches' + 64.chr + 'administration.gov.pf' # "mes-demarches at modernisation.gov.pf"
+  EQUIPE_EMAIL   = 'rgpd-mes-demarches' + 64.chr + 'informatique.gov.pf'
+  TECH_EMAIL     = 'mes-demarches' + 64.chr + 'modernisation.gov.pf'
+  NO_REPLY_EMAIL = 'Ne pas répondre <ne-pas-repondre' + 64.chr + 'administration.gov.pf>'
+  CONTACT_PHONE  = '40 47 24 75'
 
-  OLD_CONTACT_EMAIL = "contact@tps.apientreprise.fr"
+  OLD_CONTACT_EMAIL = 'mes.demarches.en.polynesie' + 64.chr + 'gmail.com'
 end
