ba71091c135bab83c4734d3fc3186160da677870
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2020-04-26 18:02:46 -1000

    Uses modernisation.gov.pf for no reply address

--- config/initializers/contacts.rb
+++ config/initializers/contacts.rb
@@ -1,8 +1,8 @@
 if !defined?(CONTACT_EMAIL)
-  CONTACT_EMAIL  = 'mes-demarches' + 64.chr + 'administration.gov.pf' # "mes-demarches at modernisation.gov.pf"
+  CONTACT_EMAIL  = 'mes-demarches' + 64.chr + 'modernisation.gov.pf' # "mes-demarches at modernisation.gov.pf"
   EQUIPE_EMAIL   = 'rgpd-mes-demarches' + 64.chr + 'informatique.gov.pf'
   TECH_EMAIL     = 'mes-demarches' + 64.chr + 'modernisation.gov.pf'
-  NO_REPLY_EMAIL = 'Ne pas répondre <ne-pas-repondre' + 64.chr + 'administration.gov.pf>'
+  NO_REPLY_EMAIL = 'Ne pas répondre <ne-pas-repondre' + 64.chr + 'modernisation.gov.pf>'
   CONTACT_PHONE  = '40 47 24 75'
 
   OLD_CONTACT_EMAIL = 'mes.demarches.en.polynesie' + 64.chr + 'gmail.com'
