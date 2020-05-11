84888be255bf4cb2a46a693e50e35bcbf3c0ff71
clemkeirua <clement@keiruaprod.fr>
2020-05-11 16:21:55 +0200

    clarification de l'archivage/suppression

--- config/initializers/urls.rb
+++ config/initializers/urls.rb
@@ -21,6 +21,7 @@ CGU_URL = [DOC_URL, "cgu"].join("/")
 MENTIONS_LEGALES_URL = [DOC_URL, "mentions-legales"].join("/")
 API_DOC_URL = [DOC_URL, "pour-aller-plus-loin", "api"].join("/")
 WEBHOOK_DOC_URL = [DOC_URL, "pour-aller-plus-loin", "webhook"].join("/")
+ARCHIVAGE_DOC_URL = [DOC_URL, "pour-aller-plus-loin", "archivage-longue-duree-des-procedures"].join("/")
 FAQ_URL = "https://faq.demarches-simplifiees.fr"
 FAQ_ADMIN_URL = "https://faq.demarches-simplifiees.fr/collection/1-administrateur"
 COMMENT_TROUVER_MA_DEMARCHE_URL = [FAQ_URL, "article", "59-comment-trouver-ma-demarche"].join("/")
