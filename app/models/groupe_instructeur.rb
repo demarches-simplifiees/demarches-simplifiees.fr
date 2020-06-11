01285c924300dfd91111147119a0b3ac92c9f8f5
Christophe Robillard <christophe.robillard@beta.gouv.fr>
2020-06-11 10:08:04 +0200

    remove useless dependent option

--- app/models/groupe_instructeur.rb
+++ app/models/groupe_instructeur.rb
@@ -1,8 +1,8 @@
 class GroupeInstructeur < ApplicationRecord
   DEFAULT_LABEL = 'défaut'
   belongs_to :procedure, -> { with_discarded }, inverse_of: :groupe_instructeurs
-  has_many :assign_tos
-  has_many :instructeurs, through: :assign_tos, dependent: :destroy
+  has_many :assign_tos, dependent: :destroy
+  has_many :instructeurs, through: :assign_tos
   has_many :dossiers
   has_and_belongs_to_many :exports
 
