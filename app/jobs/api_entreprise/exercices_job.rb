b90513f4561b64fe368fe2321b0da593eac33448
Christophe Robillard <christophe.robillard@beta.gouv.fr>
2020-06-11 18:16:19 +0200

    no sentry report when error 400 for exercices

--- app/jobs/api_entreprise/exercices_job.rb
+++ app/jobs/api_entreprise/exercices_job.rb
@@ -1,4 +1,7 @@
 class ApiEntreprise::ExercicesJob < ApiEntreprise::Job
+  rescue_from(ApiEntreprise::API::BadFormatRequest) do |exception|
+  end
+
   def perform(etablissement_id, procedure_id)
     etablissement = Etablissement.find(etablissement_id)
     etablissement_params = ApiEntreprise::ExercicesAdapter.new(etablissement.siret, procedure_id).to_params
