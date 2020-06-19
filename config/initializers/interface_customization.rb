290e8f9ba0b17c3ce203661ba7061036e590e1c7
jgauthier <julie.gauthier@adullact.org>
2020-06-19 16:31:50 +0200

    FEAT add ENV variables location to tests

---
+++ config/initializers/interface_customization.rb
@@ -0,0 +1,8 @@
+# Set environment variables for DS interfaces customization
+# - Logo: image
+# - Instance name: text
+# - Short instance name: text
+
+MAIN_LOGO_INSTANCE = ENV.fetch("MAIN_LOGO_INSTANCE", "marianne.svg")
+FULL_INSTANCE_NAME = ENV.fetch("FULL_INSTANCE_NAME", "demarches-simplifiees.fr")
+SHORT_INSTANCE_NAME = ENV.fetch("SHORT_INSTANCE_NAME", "d-s.fr")
