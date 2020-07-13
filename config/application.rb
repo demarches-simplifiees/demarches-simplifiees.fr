a1be888f7c8df38e2493ac16828c082267e79ad4
Pierre de La Morinerie <kemenaran@gmail.com>
2020-07-13 18:10:16 +0200

    config: use Rails 5.2 config defaults

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,7 @@ Dotenv::Railtie.load
 
 module TPS
   class Application < Rails::Application
-    config.load_defaults 5.1
-
+    config.load_defaults 5.2
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
