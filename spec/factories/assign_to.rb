0a1c4fc60ea740a50a64e9dfe7f7afc14129f7c2
Pierre de La Morinerie <kemenaran@gmail.com>
2020-07-20 14:13:51 +0000

    models: require belong_to associations on assign_to

--- spec/factories/assign_to.rb
+++ spec/factories/assign_to.rb
@@ -1,11 +1,5 @@
 FactoryBot.define do
   factory :assign_to do
-    after(:build) do |assign_to, evaluator|
-      if evaluator.groupe_instructeur.persisted?
-        assign_to.groupe_instructeur = evaluator.groupe_instructeur
-      else
-        assign_to.groupe_instructeur = assign_to.procedure.defaut_groupe_instructeur
-      end
-    end
+    groupe_instructeur { procedure.defaut_groupe_instructeur }
   end
 end
