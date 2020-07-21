169b8c9b3ebb790b1c3e9c9d01fefe7e03855359
Christophe Robillard <christophe.robillard@beta.gouv.fr>
2020-07-21 10:51:53 +0200

    explicite revive meaning

--- spec/controllers/instructeurs/avis_controller_spec.rb
+++ spec/controllers/instructeurs/avis_controller_spec.rb
@@ -291,7 +291,7 @@ describe Instructeurs::AvisController, type: :controller do
         allow(AvisMailer).to receive(:avis_invitation).and_return(double(deliver_later: nil))
       end
 
-      it 'revive the expert' do
+      it 'sends a reminder to the expert' do
         get :revive, params: { procedure_id: procedure.id, id: avis.id }
         expect(AvisMailer).to have_received(:avis_invitation).once.with(avis)
         expect(flash.notice).to eq("Un mail de relance a été envoyé à #{avis.email}")
