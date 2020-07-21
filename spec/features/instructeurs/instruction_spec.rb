679a45940aad1c07aa5a1d8f0c60d25fb08f0fef
clemkeirua <clement@keiruaprod.fr>
2020-07-21 16:49:26 +0200

    spec: replace reference to ds in password

--- spec/features/instructeurs/instruction_spec.rb
+++ spec/features/instructeurs/instruction_spec.rb
@@ -1,7 +1,7 @@
 feature 'Instructing a dossier:' do
   include ActiveJob::TestHelper
 
-  let(:password) { 'démarches-simplifiées-pwd' }
+  let(:password) { 'my-s3cure-p4ssword' }
   let!(:instructeur) { create(:instructeur, password: password) }
 
   let!(:procedure) { create(:procedure, :published, instructeurs: [instructeur]) }
--- spec/features/users/invite_spec.rb
+++ spec/features/users/invite_spec.rb
@@ -26,7 +26,7 @@ feature 'Invitations' do
 
     context 'when inviting someone without an existing account' do
       let(:invite) { create(:invite, dossier: dossier, user: nil) }
-      let(:user_password) { 'démarches-simplifiées-pwd' }
+      let(:user_password) { 'my-s3cure-p4ssword' }
 
       scenario 'an invited user can register using the registration link sent in the invitation email' do
         # Click the invitation link
