ef2525e04531e321f0a071d5525104e3d2d3b8ec
clemkeirua <clement@keiruaprod.fr>
2020-07-30 11:16:21 +0200

    last_commentaire_updated_at does not take system email into account

--- app/jobs/tmp_set_dossiers_last_updated_at_job.rb
+++ app/jobs/tmp_set_dossiers_last_updated_at_job.rb
@@ -7,7 +7,10 @@ class TmpSetDossiersLastUpdatedAtJob < ApplicationJob
       .limit(100)
 
     dossiers.find_each do |dossier|
-      last_commentaire_updated_at = dossier.commentaires.maximum(:updated_at)
+      last_commentaire_updated_at = dossier.commentaires
+        .where.not(email: OLD_CONTACT_EMAIL)
+        .where.not(email: CONTACT_EMAIL)
+        .maximum(:updated_at)
       last_avis_updated_at = dossier.avis.maximum(:updated_at)
       last_champ_updated_at = dossier.champs.maximum(:updated_at)
       last_champ_private_updated_at = dossier.champs_private.maximum(:updated_at)
