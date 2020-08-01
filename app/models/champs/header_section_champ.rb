d27e2810342a014bca734c3b20cfe4f15c635258
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2020-07-31 16:28:34 -1000

    [DEM-196] Displays section index for all header fields whatever level is This respect the hierarchy: 1 Title 1.2 Sub-Title 1.2.1 Sub-Sub-Title

--- app/models/champs/header_section_champ.rb
+++ app/models/champs/header_section_champ.rb
@@ -6,6 +6,13 @@ class Champs::HeaderSectionChamp < Champ
   def section_index
     siblings
       .filter { |c| c.type_champ == TypeDeChamp.type_champs.fetch(:header_section) }
-      .index(self) + 1
+      .take_while { |c| c != self }
+      .push(self)
+      .reduce([0, 0, 0]) do |index, c|
+      level = c.type_de_champ.level.to_i
+      level -= 1 if level > 0
+      r = level > 0 ? index[0..(level - 1)] : []
+      r << index[level].to_i + 1
+    end.reject(&:zero?).join('.')
   end
 end
