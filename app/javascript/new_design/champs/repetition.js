96037069ff434f6edcc855db9ebf796bdb28b410
Pierre de La Morinerie <kemenaran@gmail.com>
2020-08-20 15:56:41 +0200

    autosave: remove the repetition row after deletion
    Before, when autosaving a draft, removing a repetition row would
    send `_destroy` inputs to the controller – but not remove the row
    from the DOM. This led to the `_destroy` inputs being sent again
    on the next autosave request, which made the controller raise
    (because the row fields were already deleted before).
    
    To fix this, we let the controller response remove the deleted
    row(s) from the DOM.
    
    Doing it using a controller response avoids the need to keep track
    of operations on the Javascript side: the controller can easily
    know which row was just deleted, and emit the relevant changes for
    the DOM. This keeps the autosave requests robust: even if a request
    is skipped (e.g. because of a network interruption), the next request
    will still contain the relevant informations to succeed, and not let the
    form in an unstable state.
    
    Fix #5470

--- app/javascript/new_design/champs/repetition.js
+++ app/javascript/new_design/champs/repetition.js
@@ -1,8 +1,9 @@
 import { delegate } from '@utils';
 
+const CHAMP_SELECTOR = '.editable-champ';
 const BUTTON_SELECTOR = '.button.remove-row';
 const DESTROY_INPUT_SELECTOR = 'input[type=hidden][name*=_destroy]';
-const CHAMP_SELECTOR = '.editable-champ';
+const DOM_ID_INPUT_SELECTOR = 'input[type=hidden][name*=deleted_row_dom_ids]';
 
 delegate('click', BUTTON_SELECTOR, (evt) => {
   evt.preventDefault();
@@ -13,6 +14,8 @@ delegate('click', BUTTON_SELECTOR, (evt) => {
     input.disabled = false;
     input.value = true;
   }
+  row.querySelector(DOM_ID_INPUT_SELECTOR).disabled = false;
+
   for (let champ of row.querySelectorAll(CHAMP_SELECTOR)) {
     champ.remove();
   }
