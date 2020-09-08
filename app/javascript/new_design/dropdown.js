cb335f4fae7db732f5e163d0c1edaa5f7e64114b
Christophe Robillard <christophe.robillard@beta.gouv.fr>
2020-09-08 18:32:06 +0200

    add aria-disclosure for Mon compte button

--- app/javascript/new_design/dropdown.js
+++ app/javascript/new_design/dropdown.js
@@ -2,16 +2,21 @@ import { delegate } from '@utils';
 
 delegate('click', 'body', (event) => {
   if (!event.target.closest('.dropdown')) {
-    [...document.querySelectorAll('.dropdown')].forEach((element) =>
-      element.classList.remove('open', 'fade-in-down')
-    );
+    [...document.querySelectorAll('.dropdown')].forEach((element) => {
+      const button = element.querySelector('.dropdown-button');
+      button.setAttribute('aria-expanded', false);
+      element.classList.remove('open', 'fade-in-down');
+    });
   }
 });
 
 delegate('click', '.dropdown-button', (event) => {
   event.stopPropagation();
-  const parent = event.target.closest('.dropdown-button').parentElement;
+  const button = event.target.closest('.dropdown-button');
+  const parent = button.parentElement;
   if (parent.classList.contains('dropdown')) {
     parent.classList.toggle('open');
+    var buttonExpanded = button.getAttribute('aria-expanded') === 'true';
+    button.setAttribute('aria-expanded', !buttonExpanded);
   }
 });
