63f1b4a7dfc45048827c9005c1ddbea3b511f693
Paul Chavard <github@paul.chavard.net>
2020-10-13 17:04:51 +0200

    replace deprecated react-loadable with suspense

--- app/javascript/components/Loadable.js
+++ app/javascript/components/Loadable.js
@@ -1,8 +1,24 @@
-import React from 'react';
-import Loadable from 'react-loadable';
+import React, { Suspense, lazy } from 'react';
+import PropTypes from 'prop-types';
 
-const loading = () => <div className="spinner left" />;
+const Loader = () => <div className="spinner left" />;
 
-export default function (loader) {
-  return Loadable({ loader, loading });
+function LazyLoad({ component: Component, ...props }) {
+  return (
+    <Suspense fallback={<Loader />}>
+      <Component {...props} />
+    </Suspense>
+  );
+}
+
+LazyLoad.propTypes = {
+  component: PropTypes.object
+};
+
+export default function Loadable(loader) {
+  const LazyComponent = lazy(loader);
+
+  return function PureComponent(props) {
+    return <LazyLoad component={LazyComponent} {...props} />;
+  };
 }
