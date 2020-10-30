2ac7f7da3e0aea01d0a8fdb4b18bb1dbea9d068e
Kara Diaby <kdiaby.pro@gmail.com>
2020-10-30 18:28:08 +0100

    Revert "feat 5701 - Ajoute l'information lorsqu'un usager s'authentifie via France Connect"

--- spec/views/instructeur/dossiers/show.html.haml_spec.rb
+++ spec/views/instructeur/dossiers/show.html.haml_spec.rb
@@ -18,19 +18,4 @@ describe 'instructeurs/dossiers/show.html.haml', type: :view do
     expect(rendered).to have_text('Identité')
     expect(rendered).to have_text('Demande')
   end
-
-  context 'when the user is logged in with france connect' do
-    let(:france_connect_information) { build(:france_connect_information) }
-    let(:user) { build(:user, france_connect_information: france_connect_information) }
-    let(:procedure1) { create(:procedure, :with_type_de_champ, for_individual: true) }
-    let(:dossier) { create(:dossier, procedure: procedure1, user: user) }
-
-    before do
-      render
-    end
-
-    it 'fills the individual with the informations from France Connect' do
-      expect(rendered).to have_text("Le dossier a été déposé par le compte de #{dossier.individual.prenom} #{dossier.individual.nom}, authentifié par France Connect le #{dossier.individual.updated_at.strftime('%d/%m/%Y')}")
-    end
-  end
 end
