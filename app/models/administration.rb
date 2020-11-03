e303f456e679d72d754ac200bc682014b687f0f0
Christophe Robillard <christophe.robillard@beta.gouv.fr>
2020-11-03 18:21:19 +0100

    add recoverable and two_factor stragegy for administration

--- app/models/administration.rb
+++ app/models/administration.rb
@@ -2,27 +2,32 @@
 #
 # Table name: administrations
 #
-#  id                     :integer          not null, primary key
-#  current_sign_in_at     :datetime
-#  current_sign_in_ip     :string
-#  email                  :string           default(""), not null
-#  encrypted_password     :string           default(""), not null
-#  failed_attempts        :integer          default(0), not null
-#  last_sign_in_at        :datetime
-#  last_sign_in_ip        :string
-#  locked_at              :datetime
-#  remember_created_at    :datetime
-#  reset_password_sent_at :datetime
-#  reset_password_token   :string
-#  sign_in_count          :integer          default(0), not null
-#  unlock_token           :string
-#  created_at             :datetime
-#  updated_at             :datetime
+#  id                        :integer          not null, primary key
+#  consumed_timestep         :integer
+#  current_sign_in_at        :datetime
+#  current_sign_in_ip        :string
+#  email                     :string           default(""), not null
+#  encrypted_otp_secret      :string
+#  encrypted_otp_secret_iv   :string
+#  encrypted_otp_secret_salt :string
+#  encrypted_password        :string           default(""), not null
+#  failed_attempts           :integer          default(0), not null
+#  last_sign_in_at           :datetime
+#  last_sign_in_ip           :string
+#  locked_at                 :datetime
+#  otp_required_for_login    :boolean
+#  remember_created_at       :datetime
+#  reset_password_sent_at    :datetime
+#  reset_password_token      :string
+#  sign_in_count             :integer          default(0), not null
+#  unlock_token              :string
+#  created_at                :datetime
+#  updated_at                :datetime
 #
 class Administration < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :rememberable, :trackable, :validatable, :lockable, :async
+  devise :rememberable, :trackable, :validatable, :lockable, :async, :recoverable,
+    :two_factor_authenticatable, :otp_secret_encryption_key => ENV['OTP_SECRET_KEY']
+
 
   def invite_admin(email)
     user = User.create_or_promote_to_administrateur(email, SecureRandom.hex)
