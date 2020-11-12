4ecd9f281789829d1222b1b93547e55bf76fc18f
kara Diaby <kdiaby.pro@gmail.com>
2020-11-12 10:38:10 +0100

    add new columns to deleted dossiers

---
+++ db/migrate/20201110155516_add_relations_to_deleted_dossiers.rb
@@ -0,0 +1,7 @@
+class AddRelationsToDeletedDossiers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :deleted_dossiers, :user_id, :bigint
+    add_column :deleted_dossiers, :groupe_instructeur_id, :bigint
+    add_column :deleted_dossiers, :revision_id, :bigint
+  end
+end
