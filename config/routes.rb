9347951cea10ce151e08cc644fec9fecd27b9a1b
Christophe Robillard <christophe.robillard@beta.gouv.fr>
2020-11-12 16:09:21 +0100

    act as an saml identity provider

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get '/saml/auth' => 'saml_idp#new'
+  post '/saml/auth' => 'saml_idp#create'
+  get '/saml/metadata' => 'saml_idp#metadata'
+
   #
   # Manager
   #
