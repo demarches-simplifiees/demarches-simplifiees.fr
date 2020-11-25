a8ed9ef172681b7c88d99c37d7ed551036dd3e63
Christophe Robillard <christophe.robillard@beta.gouv.fr>
2020-11-25 15:28:27 +0100

    remove dead code

--- app/models/champs/pays_champ.rb
+++ app/models/champs/pays_champ.rb
@@ -15,13 +15,4 @@
 #  type_de_champ_id :integer
 #
 class Champs::PaysChamp < Champs::TextChamp
-  PAYS = JSON.parse(Rails.root.join('app', 'lib', 'api_geo', 'pays.json').read, symbolize_names: true)
-
-  def self.pays
-    PAYS.pluck(:nom)
-  end
-
-  def self.disabled_options
-    pays.filter { |v| (v =~ /^--.*--$/).present? }
-  end
 end
