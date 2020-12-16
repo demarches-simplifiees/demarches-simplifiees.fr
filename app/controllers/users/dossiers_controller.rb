72f6ec15bfd3f93ed6017bdebdc80af0b2f0a8d4
simon lehericey <mail@simon.lehericey.net>
2020-12-16 10:27:46 +0100

    fix typo

--- app/controllers/users/dossiers_controller.rb
+++ app/controllers/users/dossiers_controller.rb
@@ -304,7 +304,7 @@ module Users
 
     def show_demarche_en_test_banner
       if @dossier.present? && @dossier.procedure.brouillon?
-        flash.now.alert = "Ce dossier est déposé sur une démarche en test. Toute modification de la démarche par l'administrateur (ajout d'un champ, publication de la démarche...) entrainera sa suppression."
+        flash.now.alert = "Ce dossier est déposé sur une démarche en test. Toute modification de la démarche par l'administrateur (ajout d'un champ, publication de la démarche...) entraînera sa suppression."
       end
     end
 
