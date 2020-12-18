dc08993a8993ee26f7dee70f916ece473edaeb32
Paul Chavard <github@paul.chavard.net>
2020-12-18 11:21:03 +0100

    Add DateTime champ to GraphQL

--- app/graphql/types/champs/date_champ_type.rb
+++ app/graphql/types/champs/date_champ_type.rb
@@ -2,11 +2,18 @@ module Types::Champs
   class DateChampType < Types::BaseObject
     implements Types::ChampType
 
-    field :value, GraphQL::Types::ISO8601DateTime, null: true
+    field :value, GraphQL::Types::ISO8601DateTime, "La valeur du champ formaté en ISO8601 (DateTime).", null: true, deprecation_reason: "Utilisez le champ 'date' ou le fragment 'DatetimeChamp' à la place."
+    field :date, GraphQL::Types::ISO8601Date, "La valeur du champ formaté en ISO8601 (Date).", null: true
 
     def value
       if object.value.present?
         Time.zone.parse(object.value)
+      end
+    end
+
+    def date
+      if object.value.present?
+        Date.parse(object.value)
       end
     end
   end
