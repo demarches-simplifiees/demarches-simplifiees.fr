4906fe2c55becfb16ea0d4dab47c747bd809d0ce
simon lehericey <mail@simon.lehericey.net>
2021-01-07 15:45:02 +0100

    Procedure and expert link

---
+++ db/migrate/20210112120658_create_experts_procedures.rb
@@ -0,0 +1,11 @@
+class CreateExpertsProcedures < ActiveRecord::Migration[6.0]
+  def change
+    create_table :experts_procedures do |t|
+      t.references :expert, null: false, foreign_key: true
+      t.references :procedure, null: false, foreign_key: true
+      t.boolean :allow_decision_access, default: false, null: false
+
+      t.timestamps
+    end
+  end
+end
