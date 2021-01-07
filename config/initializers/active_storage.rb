3f17f87544637c144cea254fa6b85d135e1ed200
simon lehericey <mail@simon.lehericey.net>
2021-01-07 09:56:40 +0100

    ! i guess v2 is not used anymore

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -15,30 +15,6 @@ end
 # are performed through DS_Proxy.
 #
 # https://github.com/fog/fog-openstack/blob/37621bb1d5ca78d037b3c56bd307f93bba022ae1/lib/fog/openstack/auth/catalog/v2.rb#L16
-require 'fog/openstack/auth/catalog/v2'
-
-module Fog::OpenStack::Auth::Catalog
-  class V2
-    def endpoint_url(endpoint, interface)
-      url = endpoint["#{interface}URL"]
-
-      if interface == 'public'
-        publicize(url)
-      else
-        url
-      end
-    end
-
-    private
-
-    def publicize(url)
-      search = %r{^https://[^/]+/}
-      replace = "#{ENV['DS_PROXY_URL']}/"
-      url.gsub(search, replace)
-    end
-  end
-end
-
 require 'fog/openstack/auth/catalog/v3'
 module Fog::OpenStack::Auth::Catalog
   class V3
