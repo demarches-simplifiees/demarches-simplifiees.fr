bc2ca52a1a159963b4a2cd16cf081d6d959c31a6
simon lehericey <mail@simon.lehericey.net>
2021-01-07 10:29:18 +0100

    add test to initializer

---
+++ spec/initializers/active_storage_spec.rb
@@ -0,0 +1,23 @@
+RSpec.describe 'active_storage initializer' do
+  let(:catalogue) { Fog::OpenStack::Auth::Catalog::V3.new({}) }
+
+  describe '#endpoint_url' do
+    let(:url) { 'https://default.fr/toto' }
+
+    before { ENV['DS_PROXY_URL'] = 'https://proxy.fr' }
+
+    subject { catalogue.endpoint_url({ "url" => url }, interface) }
+
+    context 'when the interface is not public' do
+      let(:interface) { 'private' }
+
+      it { is_expected.to eq(url) }
+    end
+
+    context 'when the interface is public' do
+      let(:interface) { 'public' }
+
+      it { is_expected.to eq('https://proxy.fr/toto') }
+    end
+  end
+end
