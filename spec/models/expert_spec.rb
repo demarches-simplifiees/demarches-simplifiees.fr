e0cb0eebab88eb780d4b69cc77dc9cfcc84b8aef
kara Diaby <kdiaby.pro@gmail.com>
2021-01-12 15:08:57 +0100

    Tests : add an expert to a procedure and put give him the access to see the dossier final decision

--- spec/models/expert_spec.rb
+++ spec/models/expert_spec.rb
@@ -1,8 +1,5 @@
-require 'rails_helper'
-
 RSpec.describe Expert, type: :model do
   describe 'an expert could be add to a procedure' do
-
     let(:procedure) { create(:procedure) }
     let(:expert) { Expert.create }
 
@@ -12,5 +9,7 @@ RSpec.describe Expert, type: :model do
     end
 
     it { expect(procedure.experts).to eq([expert]) }
+    it { expect(ExpertsProcedure.where(expert: expert, procedure: procedure).count).to eq(1) }
+    it { expect(ExpertsProcedure.where(expert: expert, procedure: procedure).first.allow_decision_access).to be_falsy }
   end
 end
