ab4ad687aa8c66e0288fae769f91f223daa5375e
kara Diaby <kdiaby.pro@gmail.com>
2021-01-15 16:33:36 +0100

    add expert factory

--- spec/models/expert_spec.rb
+++ spec/models/expert_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe Expert, type: :model do
   describe 'an expert could be add to a procedure' do
     let(:procedure) { create(:procedure) }
-    let(:expert) { Expert.create }
+    let(:expert) { create(:expert) }
 
     before do
       procedure.experts << expert
