3071af220a1832f0fde0c1f70d871cd0063c46cf
Paul Chavard <github@paul.chavard.net>
2021-01-21 19:02:23 +0100

    Add revision date_publication

--- app/graphql/types/revision_type.rb
+++ app/graphql/types/revision_type.rb
@@ -2,6 +2,7 @@ module Types
   class RevisionType < Types::BaseObject
     global_id_field :id
     field :date_creation, GraphQL::Types::ISO8601DateTime, "Date de la création.", null: false, method: :created_at
+    field :date_publication, GraphQL::Types::ISO8601DateTime, "Date de la publication.", null: true, method: :published_at
 
     field :champ_descriptors, [Types::ChampDescriptorType], null: false
     field :annotation_descriptors, [Types::ChampDescriptorType], null: false
