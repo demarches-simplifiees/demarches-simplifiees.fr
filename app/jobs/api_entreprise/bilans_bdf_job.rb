9156754cf60e21fceb6c9922b00c54b837a3af55
Paul Chavard <github@paul.chavard.net>
2021-02-04 19:27:16 +0100

    After maximum ammount of retries discard the job and log exception

--- app/jobs/api_entreprise/bilans_bdf_job.rb
+++ app/jobs/api_entreprise/bilans_bdf_job.rb
@@ -1,6 +1,6 @@
 class ApiEntreprise::BilansBdfJob < ApiEntreprise::Job
   def perform(etablissement_id, procedure_id)
-    etablissement = Etablissement.find(etablissement_id)
+    find_etablissement(etablissement_id)
     etablissement_params = ApiEntreprise::BilansBdfAdapter.new(etablissement.siret, procedure_id).to_params
     etablissement.update!(etablissement_params)
   end
