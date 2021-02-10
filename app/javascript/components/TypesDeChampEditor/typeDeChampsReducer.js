a090820875d5442a92d9b72c710721237464a4ef
Paul Chavard <github@paul.chavard.net>
2021-02-10 19:01:18 +0100

    remove react-scroll-to-component

--- app/javascript/components/TypesDeChampEditor/typeDeChampsReducer.js
+++ app/javascript/components/TypesDeChampEditor/typeDeChampsReducer.js
@@ -1,4 +1,3 @@
-import scrollToComponent from 'react-scroll-to-component';
 import { debounce } from '@utils';
 import {
   createTypeDeChampOperation,
@@ -53,9 +52,7 @@ function addTypeDeChamp(state, typeDeChamps, insertAfter, done) {
       state.flash.success();
       done();
       if (insertAfter) {
-        scrollToComponent(insertAfter.target.nextElementSibling, {
-          duration: 300
-        });
+        insertAfter.target.nextElementSibling.scrollIntoView(true);
       }
     })
     .catch((message) => state.flash.error(message));
