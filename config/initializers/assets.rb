e51948ab5c4b9abf2b72cf7e1337c2380accd1c6
Pierre de La Morinerie <kemenaran@gmail.com>
2021-02-23 14:09:01 +0100

    stylesheets: merge print rules with the application stylesheet
    - Having a separate stylesheet for print rules is not a best practice
      (it makes an extra network request)
    - It makes migrating to Sprockets 4 easier

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,5 +10,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules', '@reach
 Rails.application.config.assets.paths << Rails.root.join('node_modules', '@mapbox', 'mapbox-gl-draw', 'dist')
 
 # Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += ['print.css']
+# application.js, application.css, and all non-JS/CSS in the app/assets
+# folder are already added.
+# Rails.application.config.assets.precompile += %w( admin.js admin.css )
