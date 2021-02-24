fb305e31c41d6e671261ba5fea321d4e35b686cd
Pierre de La Morinerie <kemenaran@gmail.com>
2021-02-24 14:06:33 +0000

    config: move application_name to an initializer
    It seems cleaner not to require a file before the Rails configuration
    runs.

--- config/initializers/01_application_name.rb
+++ config/initializers/01_application_name.rb
@@ -1,3 +1,6 @@
+# This file is named '01-application-name.rb' to load it before the other
+# initializers, and thus make the APPLICATION_ constants available in
+# the other initializers.
 APPLICATION_NAME = ENV.fetch("APPLICATION_NAME", "demarches-simplifiees.fr")
 APPLICATION_SHORTNAME = ENV.fetch("APPLICATION_SHORTNAME", "d-s.fr")
 APPLICATION_BASE_URL = ENV.fetch("APPLICATION_BASE_URL", "https://www.demarches-simplifiees.fr")
