75a104631525d1da558f0d0ea30e052663e29be6
Pierre de La Morinerie <kemenaran@gmail.com>
2021-03-11 13:42:57 +0000

    active_storage: refactor concerns
    Follow-up of #5953.
    
    Refactor the concerns with two goals:
    
    - Getting closer from the way ActiveStorage adds its own hooks.
      Usually ActiveStorage does this using an `Attachment#after_create`
      hook, which then delegates to the blob to enqueue the job.
    - Enqueuing each job only once. By hooking on `Attachment#after_create`,
      we guarantee each job will be added only once.
    
    We then let the jobs themselves check if they are relevant or not, and
    retry or discard themselves if necessary.
    
    We also need to update the tests a bit, because Rails'
    `perform_enqueued_jobs(&block)` test helper doesn't honor the `retry_on`
    clause of jobs. Instead it forwards the exception to the caller – which
    makes the test fail.
    
    Instead we use the inline version of `perform_enqueued_jobs()`, without
    a block, which properly ignores errors catched by retry_on.

--- app/jobs/titre_identite_watermark_job.rb
+++ app/jobs/titre_identite_watermark_job.rb
@@ -1,9 +1,23 @@
 class TitreIdentiteWatermarkJob < ApplicationJob
+  class FileNotScannedYetError < StandardError
+  end
+
+  # If by the time the job runs the blob has been deleted, ignore the error
+  discard_on ActiveRecord::RecordNotFound
+  # If the file is deleted during the scan, ignore the error
+  discard_on ActiveStorage::FileNotFoundError
+  # If the file is not analyzed or scanned for viruses yet, retry later
+  # (to avoid modifying the file while it is being scanned).
+  retry_on FileNotScannedYetError, wait: :exponentially_longer, attempts: 10
+
   MAX_IMAGE_SIZE = 1500
   SCALE = 0.9
   WATERMARK = Rails.root.join("app/assets/images/#{WATERMARK_FILE}")
 
   def perform(blob)
+    if blob.virus_scanner.pending? then raise FileNotScannedYetError end
+    if blob.watermark_done? then return end
+
     blob.open do |file|
       watermark = resize_watermark(file)
 
