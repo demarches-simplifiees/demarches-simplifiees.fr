de54647962d30710ce49eca98ae958a30add7e52
kara Diaby <kdiaby.pro@gmail.com>
2021-03-18 12:49:50 +0100

    fix dossier link helper

--- app/helpers/dossier_link_helper.rb
+++ app/helpers/dossier_link_helper.rb
@@ -4,11 +4,6 @@ module DossierLinkHelper
       if user.groupe_instructeurs.include?(dossier.groupe_instructeur)
         instructeur_dossier_path(dossier.procedure, dossier)
       end
-    elsif user.is_a?(Expert)
-      avis = dossier.avis.find_by(expert: user.user)
-      if avis.present?
-        expert_avis_path(avis.procedure, avis)
-      end
     elsif user.owns_or_invite?(dossier)
       dossier_path(dossier)
     end
