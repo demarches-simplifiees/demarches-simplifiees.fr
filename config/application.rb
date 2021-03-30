28b7c7a1a1c22f94e5acfd7bfdcff3f568863034
Pierre de La Morinerie <kemenaran@gmail.com>
2021-03-30 16:42:25 +0200

    config: migrate all config to Rails 6.1 defaults

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ Dotenv::Railtie.load
 module TPS
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.1
 
     # Configuration for the application, engines, and railties goes here.
     #
