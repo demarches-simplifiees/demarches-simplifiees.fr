e865a894f2bc033e378c78b9b8ec304b49a5c8da
Pierre de La Morinerie <kemenaran@gmail.com>
2021-03-30 10:52:30 +0000

    db: add last part of ActiveStorage::Blob migration
    On DS, we split the ActiveStorage schema migration from Rails 6.1 into two migrations:
    1. one for adding the column and filling the data asynchronousely
    2. one for marking the column as non-null
    
    This is the second part of this migration: it marks the column as non-null.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_18_090001) do
+ActiveRecord::Schema.define(version: 2021_03_30_112235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 2021_03_18_090001) do
     t.bigint "byte_size", null: false
     t.string "checksum", null: false
     t.datetime "created_at", null: false
-    t.string "service_name"
+    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
