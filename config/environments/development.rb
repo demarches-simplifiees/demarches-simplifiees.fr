4b229b0934c3d36df23a8c2694780f1e53caa6d7
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2021-04-16 15:25:45 -1000

    Allows to switch between s3 et local service using STORAGE .env variable

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  config.active_storage.service = ENV['FOG_ENABLED'] == 'enabled' ? :openstack : :local
+  config.active_storage.service = ENV.fetch("STORAGE", 'local').to_sym
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,7 +110,7 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
-  config.active_storage.service = :local
+  config.active_storage.service = ENV.fetch("STORAGE", 'local').to_sym
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
