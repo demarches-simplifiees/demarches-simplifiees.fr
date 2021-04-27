d1777d8cd69414e04bedef9848fb87538e199589
Pierre de La Morinerie <kemenaran@gmail.com>
2021-04-27 16:57:46 +0200

    db: fix migration adding unique index to Individuals

--- db/migrate/20210427120002_add_unique_index_to_individuals.rb
+++ db/migrate/20210427120002_add_unique_index_to_individuals.rb
@@ -5,10 +5,12 @@ class AddUniqueIndexToIndividuals < ActiveRecord::Migration[6.1]
 
   def up
     delete_duplicates :individuals, [:dossier_id]
+    remove_index :individuals, [:dossier_id]
     add_concurrent_index :individuals, [:dossier_id], unique: true
   end
 
   def down
-    remove_index :individuals, [:dossier_id]
+    remove_index :individuals, [:dossier_id], unique: true
+    add_concurrent_index :individuals, [:dossier_id]
   end
 end
