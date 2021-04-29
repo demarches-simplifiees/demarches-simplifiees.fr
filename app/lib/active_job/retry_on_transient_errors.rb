684af77e35f411f4d685cd84c27bba0da0d7c347
Pierre de La Morinerie <kemenaran@gmail.com>
2021-04-29 11:55:43 +0000

    jobs: extract an `RetryOnTranscientErrors` concern

---
+++ app/lib/active_job/retry_on_transient_errors.rb
@@ -0,0 +1,15 @@
+module ActiveJob::RetryOnTransientErrors
+  extend ActiveSupport::Concern
+
+  TRANSIENT_ERRORS = [
+    Excon::Error::BadRequest
+  ]
+
+  included do
+    if handler_for_rescue(TRANSIENT_ERRORS.first).nil?
+      TRANSIENT_ERRORS.each do |error_type|
+        retry_on error_type, attempts: 5, wait: :exponentially_longer
+      end
+    end
+  end
+end
---
+++ spec/lib/active_job/retry_on_transient_errors_spec.rb
@@ -0,0 +1,9 @@
+describe ActiveJob::RetryOnTransientErrors do
+  # rubocop:disable Rails/ApplicationJob
+  class Job < ActiveJob::Base
+    include ActiveJob::RetryOnTransientErrors
+  end
+  # rubocop:enable Rails/ApplicationJob
+
+  it_behaves_like 'a job retrying transient errors', Job
+end
