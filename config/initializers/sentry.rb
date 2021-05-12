bc6d5aca0e0264e82e46e630c300e652e257c299
François Vantomme <fvantomme@synbioz.com>
2021-05-12 15:00:13 +0200

    Fix (Sentry): prefer Rails secrets over ENV variables

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,10 +1,12 @@
 Sentry.init do |config|
-  config.dsn = ENV['SENTRY_ENABLED'] == 'enabled' ? ENV['SENTRY_DSN_RAILS'] : nil
+  secrets = Rails.application.secrets.sentry
+
+  config.dsn = secrets[:enabled] ? secrets[:rails_client_key] : nil
   config.send_default_pii = false
-  config.environment = ENV.fetch('SENTRY_ENVIRONMENT', Rails.env)
-  config.enabled_environments = ['production', ENV['SENTRY_ENVIRONMENT'].presence].compact
+  config.environment = secrets[:environment] || Rails.env
+  config.enabled_environments = ['production', secrets[:environment].presence].compact
   config.breadcrumbs_logger = [:active_support_logger]
-  config.traces_sample_rate = ENV['SENTRY_ENABLED'] == 'enabled' ? 0.001 : nil
+  config.traces_sample_rate = secrets[:enabled] ? 0.001 : nil
   config.excluded_exceptions += [
     # Ignore exceptions caught by ActiveJob.retry_on
     # https://github.com/getsentry/sentry-ruby/issues/1347
