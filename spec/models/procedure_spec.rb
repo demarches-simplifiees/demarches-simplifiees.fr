83f45266b359b6d9799be8c9fe4cafce664612c5
Paul Chavard <github@paul.chavard.net>
2021-05-27 12:17:10 +0200

    Fix destoying discarded procedures

--- spec/models/procedure_spec.rb
+++ spec/models/procedure_spec.rb
@@ -1032,9 +1032,14 @@ describe Procedure do
   end
 
   describe "#destroy" do
-    let(:procedure) { create(:procedure, :with_type_de_champ) }
+    let(:procedure) { create(:procedure, :closed, :with_type_de_champ) }
+
+    before do
+      procedure.discard!
+    end
 
     it "can destroy procedure" do
+      expect(procedure.revisions.count).to eq(2)
       expect(procedure.destroy).to be_truthy
     end
   end
