9d76e4f78c20c10cddfc084027a524c399c225e7
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2021-06-02 10:38:10 -1000

    worker process needs S3 variablesrefactoring to allow download/upload at the same time as upload will be splitted in multiple sequences, and checking files are uploaded is slow ==> use local base to detect if file has already been uploaded

--- app/models/s3_synchronization.rb
+++ app/models/s3_synchronization.rb
@@ -24,9 +24,9 @@ class S3Synchronization < ApplicationRecord
 
     def synchronize(under_rake, until_time)
       if Rails.configuration.active_storage.service == :local
-        upload(:local, :s3, under_rake, until_time)
+        upload2(:local, :s3, under_rake, until_time)
       else
-        upload(:s3, :local, under_rake, until_time)
+        upload2(:s3, :local, under_rake, until_time)
       end
       AdministrationMailer.s3_synchronization_report.deliver_now
     end
@@ -104,8 +104,6 @@ class S3Synchronization < ApplicationRecord
       end
     end
 
-    private
-
     def process_blob(blob, block, configs, from, from_service, to, progress, until_time)
       begin
         return if until_time.present? && Time.zone.now > until_time
@@ -130,6 +128,78 @@ class S3Synchronization < ApplicationRecord
         end
       end
       progress.inc if progress
+    end
+
+    #---------------------------------------------------------------------
+    def upload2(from, to, under_rake, until_time)
+      ActiveStorage::Blob.service
+      configs = Rails.configuration.active_storage.service_configurations
+      from_service = ActiveStorage::Service.configure from, configs
+
+      ActiveStorage::Blob.service = from_service
+
+      S3Synchronization.all.count # load class before multi-threading
+
+      progress = ProgressReport.new(blobs_to_upload(from).count) if under_rake
+      pool = Concurrent::FixedThreadPool.new(POOL_SIZE)
+
+      blobs_to_upload(from).find_each do |blob|
+        pool.post do
+          upload_blob_if_present(from_service, configs, to, progress, until_time, blob)
+        end
+      end
+      pool.shutdown
+      pool.wait_for_termination
+      progress.finish if progress
+    end
+
+    private
+
+    def blobs_to_upload(from_service)
+      ActiveStorage::Blob.joins('left join s3_synchronizations on  s3_synchronizations.active_storage_blob_id = active_storage_blobs.id')
+        .where(s3_synchronizations: { checked: [false, nil] })
+    end
+
+    def upload_blob_if_present(from_service, configs, to, progress, until_time, blob)
+      return if until_time.present? && Time.zone.now > until_time
+      if from_service.exist?(blob.key)
+        blob.open do |file|
+          upload_blob2(blob, file, configs, to, progress)
+        end
+      else
+        puts "\nFichier non présent à la source : #{blob.key}"
+      end
+    rescue => e
+      puts "\nErreur inconnue #{blob.key} #{e} #{e.message}"
+      e.backtrace.each { |line| puts line }
+    end
+
+    def upload_blob2(blob, file, configs, to, progress, &block)
+      if File.file?(file)
+        to_service = ActiveStorage::Service.configure to, configs
+        synchronization = S3Synchronization.find_or_create_by(target: to, active_storage_blob_id: blob.id)
+        upload_and_verify(to_service, blob, file, synchronization)
+        upload_and_verify(to_service, blob, file, synchronization) # second try
+      end
+      progress.inc if progress
+    end
+
+    def upload_and_verify(to_service, blob, file, synchronization)
+      unless synchronization&.checked
+        upload_file(to_service, blob, file)
+        check_integrity(to_service, blob, synchronization)
+      end
+    end
+
+    def check_integrity(to_service, blob, synchronization)
+      begin
+        to_service.open blob.key, checksum: blob.checksum do |f|
+        end
+        synchronization.checked = true
+        synchronization.save
+      rescue => e
+        puts "\nIntegrity error on #{blob.key} #{e} #{e.message} ==> force upload"
+      end
     end
   end
 end
