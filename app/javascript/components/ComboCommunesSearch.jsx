843240bbb6591d8b05b766c066e9603fa4f7064c
Paul Chavard <github@paul.chavard.net>
2021-06-15 14:36:37 +0200

    expand commune searches with multiple post codes

--- app/javascript/components/ComboCommunesSearch.jsx
+++ app/javascript/components/ComboCommunesSearch.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import { QueryClientProvider } from 'react-query';
+import { matchSorter } from 'match-sorter';
 
 import ComboSearch from './ComboSearch';
 import { queryClient } from './shared/queryClient';
+
+function expandResultsWithMultiplePostalCodes(term, results) {
+  const expandedResults = results.flatMap((result) =>
+    result.codesPostaux.map((codePostal) => ({
+      ...result,
+      codesPostaux: [codePostal]
+    }))
+  );
+  const limit = term.length > 5 ? 10 : 5;
+  if (expandedResults.length > limit) {
+    return matchSorter(expandedResults, term, {
+      keys: [(item) => `${item.nom} (${item.codesPostaux[0]})`, 'code'],
+      sorter: (rankedItems) => rankedItems
+    }).slice(0, limit + 1);
+  }
+
+  return expandedResults;
+}
 
 function ComboCommunesSearch(params) {
   return (
@@ -16,6 +35,7 @@ function ComboCommunesSearch(params) {
           code,
           `${nom} (${codesPostaux[0]})`
         ]}
+        transformResults={expandResultsWithMultiplePostalCodes}
       />
     </QueryClientProvider>
   );
