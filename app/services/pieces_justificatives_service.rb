fdec9b2fd5c380f1c5353cbf43262fb2289c8c64
Christophe Robillard <christophe.robillard@beta.gouv.fr>
2021-06-18 09:06:05 +0200

    archives: don't update dossiers
    this fix avoid to touch dossier after attaching pdf_export_for_instructeur

--- app/services/pieces_justificatives_service.rb
+++ app/services/pieces_justificatives_service.rb
@@ -56,7 +56,9 @@ class PiecesJustificativesService
                 include_infos_administration: true,
                 dossier: dossier
               })
-    dossier.pdf_export_for_instructeur.attach(io: StringIO.open(pdf), filename: "export-#{dossier.id}.pdf", content_type: 'application/pdf')
+    ActiveRecord::Base.no_touching do
+      dossier.pdf_export_for_instructeur.attach(io: StringIO.open(pdf), filename: "export-#{dossier.id}.pdf", content_type: 'application/pdf')
+    end
     dossier.pdf_export_for_instructeur
   end
 
