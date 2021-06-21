37c6e1c641a32b133aeb57060f8ecd7dec49b5c5
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2021-06-21 11:32:11 -1000

    [DEM-165] Perform S3 synchronisation a 21h for 170 minutes and at 1h for 5.5 hours

---
+++ app/jobs/cron/blob_synchronization_evening_job.rb
@@ -0,0 +1,7 @@
+class Cron::BlobSynchronizationEveningJob < Cron::CronJob
+  self.schedule_expression = "every day at 9 pm"
+
+  def perform(*args)
+    S3Synchronization.synchronize(false, Time.zone.now + 170.minutes)
+  end
+end
