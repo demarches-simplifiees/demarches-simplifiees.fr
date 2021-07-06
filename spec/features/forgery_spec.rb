831672391ebddae67469e1c89551d1cdaa9a8ef3
Pierre de La Morinerie <kemenaran@gmail.com>
2021-07-06 14:29:23 +0000

    app: use a long-lived cookie for CSRF token
    See the ADR document for rationale.

--- spec/features/forgery_spec.rb
+++ spec/features/forgery_spec.rb
@@ -2,14 +2,34 @@ feature 'Protecting against request forgeries:', :allow_forgery_protection, :sho
   let(:user) { create(:user, password: password) }
   let(:password) { 'ThisIsTheUserPassword' }
 
-  scenario 'a form without a matching CSRF token is rejected' do
+  before do
     visit new_user_session_path
+  end
 
-    delete_session_cookie
-    fill_sign_in_form
+  context 'when the browser send a request after the session cookie expired' do
+    before do
+      delete_session_cookie
+    end
 
-    click_on 'Se connecter'
-    expect(page).to have_text('L’action demandée a été rejetée')
+    context 'when the long-lived CSRF cookie is still present' do
+      scenario 'the change is allowed' do
+        fill_sign_in_form
+        click_on 'Se connecter'
+        expect(page).to have_content('Connecté')
+      end
+    end
+
+    context 'when the long-lived CSRF cookie is invalid or missing' do
+      before do
+        delete_long_lived_csrf_cookie
+      end
+
+      scenario 'the user sees an error page' do
+        fill_sign_in_form
+        click_on 'Se connecter'
+        expect(page).to have_text('L’action demandée a été rejetée')
+      end
+    end
   end
 
   private
@@ -21,6 +41,16 @@ feature 'Protecting against request forgeries:', :allow_forgery_protection, :sho
 
   def delete_session_cookie
     session_cookie_name = Rails.application.config.session_options[:key]
-    page.driver.browser.set_cookie("#{session_cookie_name}=''")
+    delete_cookie(session_cookie_name)
+  end
+
+  def delete_long_lived_csrf_cookie
+    csrf_cookie_name = ApplicationController::LongLivedAuthenticityToken::COOKIE_NAME
+    delete_cookie(csrf_cookie_name)
+  end
+
+  def delete_cookie(cookie_name)
+    raise 'The cookie to be deleted can’t be nil' if cookie_name.nil?
+    page.driver.browser.set_cookie("#{cookie_name}=''")
   end
 end
