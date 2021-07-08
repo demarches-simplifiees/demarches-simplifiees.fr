52e6bb108bd283f11bd0c5dfb51fa7d92f571fb6
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2021-07-07 17:15:17 -1000

    [DEM-165] Launch synchronization job at 1pm to transfer all files before switching

--- app/jobs/cron/blob_synchronization_evening_job.rb
+++ app/jobs/cron/blob_synchronization_evening_job.rb
@@ -1,5 +1,5 @@
 class Cron::BlobSynchronizationEveningJob < Cron::CronJob
-  self.schedule_expression = "every day at 9 pm"
+  self.schedule_expression = "every day at 1 pm"
 
   def perform(*args)
     S3Synchronization.synchronize(false, Time.zone.now + 170.minutes)
