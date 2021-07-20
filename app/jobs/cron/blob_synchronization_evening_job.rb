0be329e561dba54d9fea9f41e811bcf1d6f27a66
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2021-07-19 16:02:10 -1000

    [DEM-239] Temp removal of S3 Synchronisation waiting for definitive bucket.

--- app/jobs/cron/blob_synchronization_evening_job.rb
+++
@@ -1,7 +0,0 @@
-class Cron::BlobSynchronizationEveningJob < Cron::CronJob
-  self.schedule_expression = "every day at 1 pm"
-
-  def perform(*args)
-    S3Synchronization.synchronize(false, Time.zone.now + 170.minutes)
-  end
-end
--- app/jobs/cron/blob_synchronization_job.rb
+++
@@ -1,7 +0,0 @@
-class Cron::BlobSynchronizationJob < Cron::CronJob
-  self.schedule_expression = "every day at 1 am"
-
-  def perform(*args)
-    S3Synchronization.synchronize(false, Time.zone.now + 225.minutes)
-  end
-end
