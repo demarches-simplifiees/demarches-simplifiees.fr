02120c7c5f46380d8305634a572d7c9d518dc36a
Pierre de La Morinerie <kemenaran@gmail.com>
2021-07-22 15:57:14 +0000

    db: add uniqueness constraints to Etablissement

---
+++ db/migrate/20210722133553_add_unique_index_to_etablissement.rb
@@ -0,0 +1,16 @@
+class AddUniqueIndexToEtablissement < ActiveRecord::Migration[6.1]
+  include Database::MigrationHelpers
+
+  disable_ddl_transaction!
+
+  def up
+    delete_duplicates :etablissements, [:dossier_id]
+    remove_index :etablissements, [:dossier_id]
+    add_concurrent_index :etablissements, [:dossier_id], unique: true
+  end
+
+  def down
+    remove_index :etablissements, [:dossier_id]
+    add_concurrent_index :etablissements, [:dossier_id]
+  end
+end
