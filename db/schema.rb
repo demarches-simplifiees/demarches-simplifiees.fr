5f84f94cfadf9b39766c724269206fcc0f1ff75e
Pierre de La Morinerie <kemenaran@gmail.com>
2021-07-22 15:48:13 +0000

    db: fix schema.rb leftovers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_05_095054) do
+ActiveRecord::Schema.define(version: 2021_06_04_095054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -276,9 +276,9 @@ ActiveRecord::Schema.define(version: 2021_06_05_095054) do
     t.string "api_entreprise_job_exceptions", array: true
     t.interval "conservation_extension", default: "PT0S"
     t.string "deleted_user_email_never_send"
+    t.datetime "declarative_triggered_at"
     t.index "to_tsvector('french'::regconfig, (search_terms || private_search_terms))", name: "index_dossiers_on_search_terms_private_search_terms", using: :gin
     t.index "to_tsvector('french'::regconfig, search_terms)", name: "index_dossiers_on_search_terms", using: :gin
-    t.datetime "declarative_triggered_at"
     t.index ["archived"], name: "index_dossiers_on_archived"
     t.index ["groupe_instructeur_id"], name: "index_dossiers_on_groupe_instructeur_id"
     t.index ["hidden_at"], name: "index_dossiers_on_hidden_at"
