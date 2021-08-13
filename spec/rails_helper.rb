b81734650a1337a421b6034e1ed6ddc6c742abcd
Christophe Robillard <christophe.robillard@beta.gouv.fr>
2021-08-13 15:39:03 +0200

    replace deprecated axe-matchers by axe-core-spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,7 +10,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
-require 'axe/rspec'
+require 'axe-rspec'
 require 'devise'
 require 'shoulda-matchers'
 
