f1ff6da81b96205fdf2ee5c30eb91c4990fed12c
Paul Chavard <github@paul.chavard.net>
2021-09-01 18:16:01 +0200

    feat(i18n): add locale to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_18_083349) do
+ActiveRecord::Schema.define(version: 2021_08_26_161956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -753,6 +753,7 @@ ActiveRecord::Schema.define(version: 2021_08_18_083349) do
     t.bigint "instructeur_id"
     t.bigint "administrateur_id"
     t.bigint "expert_id"
+    t.string "locale"
     t.index ["administrateur_id"], name: "index_users_on_administrateur_id"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
