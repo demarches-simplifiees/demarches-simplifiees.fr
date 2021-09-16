eef90efd49ff194ec04b7d0f6951de4c6876c684
Pierre de La Morinerie <kemenaran@gmail.com>
2021-09-16 11:42:18 -0500

    app: fix exception during password reset when the token has expired

--- app/controllers/concerns/devise_populated_resource.rb
+++ app/controllers/concerns/devise_populated_resource.rb
@@ -2,9 +2,12 @@ module DevisePopulatedResource
   extend ActiveSupport::Concern
 
   # During a GET /password/edit, the resource is a brand new object.
-  # This method gives access to the actual resource record, complete with email, relationships, etc.
+  # This method gives access to the actual resource record (if available), complete with email, relationships, etc.
+  #
+  # If the resource can't be found (typically because the reset password token has expired),
+  # returns the default blank record.
   def populated_resource
-    resource_class.with_reset_password_token(resource.reset_password_token)
+    resource_class.with_reset_password_token(resource.reset_password_token) || resource
   end
 
   included do
