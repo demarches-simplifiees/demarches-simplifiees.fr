d23a8dfb5ca150404a8a98048877f0df76c271e6
simon lehericey <mail@simon.lehericey.net>
2021-09-17 12:21:38 +0200

    add sanitize method to service

--- app/lib/api_particulier/services/sources_service.rb
+++ app/lib/api_particulier/services/sources_service.rb
@@ -12,7 +12,29 @@ module APIParticulier
           .reduce({}) { |acc, el| acc.deep_merge(el) }
       end
 
+      # Remove sources not available for the procedure
+      def sanitize(requested_sources)
+        requested_sources_a = h_to_a(requested_sources)
+        available_sources_a = h_to_a(available_sources)
+
+        filtered_sources_a = requested_sources_a.intersection(available_sources_a)
+
+        a_to_h(filtered_sources_a)
+      end
+
       private
+
+      # { 'cnaf' => { 'scope' => ['a', 'b'] }} => [['cnaf', 'scope', 'a'], ['cnaf', 'scope', 'b']]
+      def h_to_a(h)
+        h.reduce([]) { |acc, (provider, scopes)| scopes.each { |scope, values| values.each { |s, _| acc << [provider, scope, s] } }; acc }
+      end
+
+      # [['cnaf', 'scope', 'a'], ['cnaf', 'scope', 'b']] => { 'cnaf' => { 'scope' => ['a', 'b'] }}
+      def a_to_h(a)
+        h = Hash.new { |h, k| h[k] = Hash.new { |h2, k2| h2[k2] = [] } }
+
+        a.reduce(h) { |acc, (provider, scope, source)| h[provider][scope] << source; acc }
+      end
 
       def extract_sources(provider, scope)
         { provider => { scope => providers[provider][scope] } }
