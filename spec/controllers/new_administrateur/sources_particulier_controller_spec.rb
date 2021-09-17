77ee0678b46f1d09d8d94d47e4577ec19f4d2aae
simon lehericey <mail@simon.lehericey.net>
2021-09-17 12:22:01 +0200

    an admin can save its sources

--- spec/controllers/new_administrateur/sources_particulier_controller_spec.rb
+++ spec/controllers/new_administrateur/sources_particulier_controller_spec.rb
@@ -18,4 +18,43 @@ describe NewAdministrateur::SourcesParticulierController, type: :controller do
       expect(subject.body).not_to have_selector("input#api_particulier_sources_cnaf_enfants_[value=date_de_naissance][checked=checked]")
     end
   end
+
+  describe "#update" do
+    let(:procedure) { create(:procedure, administrateur: admin, api_particulier_scopes: ['cnaf_enfants'], api_particulier_sources: {}) }
+    let(:params) { { procedure_id: procedure.id }.merge(requested_sources) }
+
+    before do
+      patch :update, params: params
+      procedure.reload
+    end
+
+    context 'when no source is requested' do
+      let(:requested_sources) { {} }
+
+      it { expect(procedure.api_particulier_sources).to be_empty }
+    end
+
+    context 'when a forbidden source is requested' do
+      let(:requested_sources) do
+        {
+          api_particulier_sources: { cnaf: { enfants: ['forbidden'] } }
+        }
+      end
+
+      it { expect(procedure.api_particulier_sources).to be_empty }
+    end
+
+    context 'when an authorized source is requested' do
+      let(:requested_sources) do
+        {
+          api_particulier_sources: { cnaf: { enfants: ['noms_prenoms'] } }
+        }
+      end
+
+      it 'saves the source' do
+        expect(procedure.api_particulier_sources).to eq("cnaf" => { "enfants" => ["noms_prenoms"] })
+        expect(flash.notice).to eq(I18n.t(".new_administrateur.sources_particulier.update.sources_ok"))
+      end
+    end
+  end
 end
