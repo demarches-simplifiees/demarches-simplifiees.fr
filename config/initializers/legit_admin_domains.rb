081d74cb3fa083982b3b4533e7e248474188524c
simon lehericey <mail@simon.lehericey.net>
2021-10-19 18:04:28 +0200

    change in whitelist

--- config/initializers/legit_admin_domains.rb
+++ config/initializers/legit_admin_domains.rb
@@ -1,2 +1,2 @@
-domains = ["gouv.fr", "sante.fr", "cnafmail.fr", "cnamts.fr", "cci.fr", "caf.fr", "msa.fr", "archi.fr", "ira-lille.fr"]
+domains = ["gouv.fr", "sante.fr", "cnafmail.fr", "cnamts.fr", "cci.fr", "caf.fr", "msa.fr"]
 LEGIT_ADMIN_DOMAINS = ENV["LEGIT_ADMIN_DOMAINS"]&.split(';') || domains
