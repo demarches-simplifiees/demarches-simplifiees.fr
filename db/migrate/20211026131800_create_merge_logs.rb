cc0ae08799a0f8e939b6750220154168d2dc14b4
simon lehericey <mail@simon.lehericey.net>
2021-10-26 14:41:30 +0200

    Add merge log

---
+++ db/migrate/20211026131800_create_merge_logs.rb
@@ -0,0 +1,11 @@
+class CreateMergeLogs < ActiveRecord::Migration[6.1]
+  def change
+    create_table :merge_logs do |t|
+      t.bigint :from_user_id, null: false
+      t.string :from_user_email, null: false
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
