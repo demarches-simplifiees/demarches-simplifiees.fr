4b13ad2ab5a3da198453ce1f336b9be316e1f9ee
mfo <mfo@users.noreply.github.com>
2021-11-15 16:19:46 +0100

    feat(mails): send it on mailer queue

--- app/jobs/notify_new_answer_with_delay_job.rb
+++ app/jobs/notify_new_answer_with_delay_job.rb
@@ -1,10 +1,10 @@
 class NotifyNewAnswerWithDelayJob < ApplicationJob
-  queue_as :exports # for now?
+  queue_as :mailers # use default queue for email, same priority
 
   discard_on ActiveRecord::RecordNotFound
 
   def perform(dossier, body, commentaire)
     return if commentaire.soft_deleted?
-    DossierMailer.notify_new_answer(dossier, body)
+    DossierMailer.notify_new_answer(dossier, body).deliver_now
   end
 end
