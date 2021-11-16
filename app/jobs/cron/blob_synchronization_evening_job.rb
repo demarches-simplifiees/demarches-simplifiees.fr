4b1bed6a45dd444a31f1b11c74cd179b991292ae
maatinito <15379878+maatinito@users.noreply.github.com>
2021-11-15 16:31:04 -1000

    [DEM-165] In the evening, synchronize from 5pm to 8pm.

--- app/jobs/cron/blob_synchronization_evening_job.rb
+++ app/jobs/cron/blob_synchronization_evening_job.rb
@@ -1,7 +1,7 @@
 class Cron::BlobSynchronizationEveningJob < Cron::CronJob
-  self.schedule_expression = "every day at 1 pm"
+  self.schedule_expression = "every day at 5 pm"
 
   def perform(*args)
-    S3Synchronization.synchronize(false, Time.zone.now + 170.minutes)
+    S3Synchronization.synchronize(false, Time.zone.now + 180.minutes)
   end
 end
