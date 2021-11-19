03eb6a7b8a5464f0b3d3693815636f45b563d3a1
Martin <martin@sharypic.com>
2021-11-19 14:36:21 +0100

    lint(rubocop): come on, maybe i will find the inspiration to setup it up in my ide

--- spec/factories/traitement.rb
+++ spec/factories/traitement.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :traitement do
     trait :accepte do
-      process_expired {true}
-      state {:accepte}
+      process_expired { true }
+      state { :accepte }
     end
   end
 end
