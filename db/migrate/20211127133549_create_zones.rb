b2902340aff99164e123b8e7ea033fa621665b1c
krichtof <christophe.robillard@beta.gouv.fr>
2021-11-24 06:09:51 +0100

    add db constraints for ministere
    Co-authored-by: Paul Chavard <paul.chavard@beta.gouv.fr>

--- db/migrate/20211127133549_create_zones.rb
+++ db/migrate/20211127133549_create_zones.rb
@@ -1,7 +1,7 @@
 class CreateZones < ActiveRecord::Migration[6.1]
   def change
     create_table :zones do |t|
-      t.string :acronym
+      t.string :acronym, null: false, index: { unique: true }
       t.string :label
 
       t.timestamps
