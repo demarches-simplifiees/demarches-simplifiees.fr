ab84897808065d7592e34a8969ea7ae19ddf376c
maatinito <15379878+maatinito@users.noreply.github.com>
2021-12-01 15:39:45 -1000

    Fix stat tests with timezone

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -63,11 +63,11 @@ class Stat < ApplicationRecord
     end
 
     def last_four_months_hash(association, date_attribute)
-      min_date = 3.months.ago.beginning_of_month.to_date
+      min_date = 3.months.ago.beginning_of_month
 
       association
         .where(date_attribute => min_date..max_date)
-        .group("DATE_TRUNC('month', #{date_attribute}::TIMESTAMPTZ AT TIME ZONE '#{Time.zone.now.formatted_offset}'::INTERVAL)")
+        .group("DATE_TRUNC('month', #{date_attribute}::TIMESTAMPTZ AT TIME ZONE '#{Time.zone.formatted_offset}'::INTERVAL)")
         .count
         .to_a
         .sort_by { |a| a[0] }
@@ -78,7 +78,7 @@ class Stat < ApplicationRecord
       sum = 0
       association
         .where("#{date_attribute} < ?", max_date)
-        .group("DATE_TRUNC('month', #{date_attribute}::TIMESTAMPTZ AT TIME ZONE '#{Time.zone.now.formatted_offset}'::INTERVAL)")
+        .group("DATE_TRUNC('month', #{date_attribute}::TIMESTAMPTZ AT TIME ZONE '#{Time.zone.formatted_offset}'::INTERVAL)")
         .count
         .to_a
         .sort_by { |a| a[0] }
