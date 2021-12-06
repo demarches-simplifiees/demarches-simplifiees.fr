09c37435adaae2bb3e5247fe8dc291e635e4e508
Paul Chavard <pro@paul.chavard.net>
2021-12-06 21:03:26 +0100

    feat(procedure_revision_types_de_champ): add parent_id

--- app/models/procedure_revision_type_de_champ.rb
+++ app/models/procedure_revision_type_de_champ.rb
@@ -6,6 +6,7 @@
 #  position         :integer          not null
 #  created_at       :datetime         not null
 #  updated_at       :datetime         not null
+#  parent_id        :bigint
 #  revision_id      :bigint           not null
 #  type_de_champ_id :bigint           not null
 #
@@ -13,6 +14,8 @@ class ProcedureRevisionTypeDeChamp < ApplicationRecord
   belongs_to :revision, class_name: 'ProcedureRevision'
   belongs_to :type_de_champ
 
+  belongs_to :parent, class_name: 'ProcedureRevisionTypeDeChamp', optional: true
+  has_many :revision_types_de_champ, -> { ordered }, foreign_key: :parent_id, class_name: 'ProcedureRevisionTypeDeChamp', inverse_of: :parent, dependent: :destroy
   scope :ordered, -> { order(:position) }
   scope :public_only, -> { joins(:type_de_champ).where(types_de_champ: { private: false }) }
   scope :private_only, -> { joins(:type_de_champ).where(types_de_champ: { private: true }) }
