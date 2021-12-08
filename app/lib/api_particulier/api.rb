0676da3c744ddc9c3f5ce5b6c1b096f921914f62
François Vantomme <fvantomme@synbioz.com>
2021-12-08 17:41:27 +0100

    feat(api particulier): add MESRI adapter

--- app/lib/api_particulier/api.rb
+++ app/lib/api_particulier/api.rb
@@ -5,6 +5,7 @@ class APIParticulier::API
   COMPOSITION_FAMILIALE_RESOURCE_NAME = "v2/composition-familiale"
   AVIS_IMPOSITION_RESOURCE_NAME = "v2/avis-imposition"
   SITUATION_POLE_EMPLOI = "v2/situations-pole-emploi"
+  ETUDIANTS_RESOURCE_NAME = "v2/etudiants"
 
   TIMEOUT = 20
 
@@ -32,6 +33,16 @@ class APIParticulier::API
 
   def situation_pole_emploi(identifiant)
     get(SITUATION_POLE_EMPLOI, identifiant: identifiant)
+  end
+
+  def etudiants(ine)
+    # NOTE: Paramètres d'appel mutuellement exclusifs,
+    # l'appel par INE est réservé aux acteurs de la sphère de l'enseignement
+    # - INE, l'Identifiant National Étudiant
+    # - état civil, constitué du nom, prénom, date de naissance, sexe et lieu de naissance
+
+    # TODO: ajouter le support de l'état civil
+    get(ETUDIANTS_RESOURCE_NAME, ine: ine)
   end
 
   private
