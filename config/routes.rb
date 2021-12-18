71e4fefa63726b5eaa3b675996887c0ab9db26c3
Christophe Robillard <christophe.robillard@beta.gouv.fr>
2021-12-18 17:28:03 +0100

    add Zones dashboard for superadmin

--- config/routes.rb
+++ config/routes.rb
@@ -54,6 +54,8 @@ Rails.application.routes.draw do
 
     resources :super_admins, only: [:index, :show, :destroy]
 
+    resources :zones
+
     post 'demandes/create_administrateur'
     post 'demandes/refuse_administrateur'
 
