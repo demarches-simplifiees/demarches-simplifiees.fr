73e282fcfc4510c40a08fe8154eb8bb7309ef964
maatinito <15379878+maatinito@users.noreply.github.com>
2022-01-12 16:37:08 -1000

    Update graphql repasser_en_instruction according passer_en_instruction

--- app/graphql/mutations/dossier_repasser_en_instruction.rb
+++ app/graphql/mutations/dossier_repasser_en_instruction.rb
@@ -6,17 +6,18 @@ module Mutations
 
     argument :dossier_id, ID, "Dossier ID", required: true, loads: Types::DossierType
     argument :instructeur_id, ID, "Instructeur qui prend la décision sur le dossier.", required: true, loads: Types::ProfileType
+    argument :disable_notification, Boolean, "Désactiver l’envoi de l’email de notification après l’opération", required: false, default_value: false
 
     field :dossier, Types::DossierType, null: true
     field :errors, [Types::ValidationErrorType], null: true
 
-    def resolve(dossier:, instructeur:)
-      dossier.repasser_en_instruction!(instructeur)
+    def resolve(dossier:, instructeur:, disable_notification:)
+      dossier.repasser_en_instruction!(instructeur: instructeur, disable_notification: disable_notification)
 
       { dossier: dossier }
     end
 
-    def authorized?(dossier:, instructeur:)
+    def authorized?(dossier:, instructeur:, **args)
       unless dossier.accepte? || dossier.refuse? || dossier.sans_suite?
         return false, { errors: ["Le dossier ne peut repasser en instruction lorsqu'il est #{dossier_display_state(dossier, lower: true)}"] }
       end
