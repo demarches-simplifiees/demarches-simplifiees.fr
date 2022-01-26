e8e37cce15b305e432b2fd54b78f8999bf6aa3ac
Pierre de La Morinerie <kemenaran@gmail.com>
2022-01-26 16:52:32 +0000

    models: refactor the types_de_champ validators
    Context: we want to validate public and private types_de_champ
    separately.
    
    Before we validated the whole revision (and then validators themselves
    enumerated all champs, public and private).
    
    Now we validate the actual public types_de_champ, which will let us
    validate separately the private types_de_champ.

--- app/validators/revisions/no_empty_repetition_validator.rb
+++ app/validators/revisions/no_empty_repetition_validator.rb
@@ -1,10 +1,6 @@
 class Revisions::NoEmptyRepetitionValidator < ActiveModel::EachValidator
-  def validate_each(procedure, attribute, revision)
-    return if revision.nil?
-
-    revision_tdcs = revision.types_de_champ + revision.types_de_champ_private
-    repetitions = revision_tdcs.filter(&:repetition?)
-    repetitions.each do |repetition|
+  def validate_each(procedure, attribute, types_de_champ)
+    types_de_champ.filter(&:repetition?).each do |repetition|
       validate_repetition_not_empty(procedure, attribute, repetition)
     end
   end
