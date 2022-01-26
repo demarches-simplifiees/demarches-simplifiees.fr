0ffd453c8b370e244c1a1bd382bb01d9cd342f87
Paul Chavard <pro@paul.chavard.net>
2022-01-26 18:27:57 +0100

    fix(attestation_template): fix migration task

---
+++ spec/lib/tasks/deployment/20220112184331_revise_attestation_templates_spec.rb
@@ -0,0 +1,22 @@
+describe '20220112184331_revise_attestation_templates' do
+  let(:rake_task) { Rake::Task['after_party:revise_attestation_templates'] }
+  let(:procedure) { create(:procedure) }
+  let(:attestation_template) { create(:attestation_template, procedure: procedure) }
+
+  subject(:run_task) do
+    attestation_template
+    rake_task.invoke
+    attestation_template.reload
+  end
+
+  after { rake_task.reenable }
+
+  context 'add attestation_template_id' do
+    it 'to revisions' do
+      expect(attestation_template.procedure.revisions.first.attestation_template_id).to be_nil
+      run_task
+      expect(attestation_template.procedure_id).to eq(procedure.id)
+      expect(attestation_template.procedure.revisions.first.attestation_template_id).to eq(attestation_template.id)
+    end
+  end
+end
