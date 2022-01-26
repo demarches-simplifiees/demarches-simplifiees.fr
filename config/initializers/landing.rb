ac915494df1242688c582ecdc3b887f4ce911b9e
François Vantomme <fvantomme@synbioz.com>
2022-01-26 16:45:33 +0100

    feat(landing): hide testimonial & users sections

---
+++ config/initializers/landing.rb
@@ -0,0 +1,3 @@
+# Hide or show the landing page sections
+LANDING_TESTIMONIALS_ENABLED = ENV.fetch("LANDING_TESTIMONIALS_ENABLED", "enabled") == "enabled"
+LANDING_USERS_ENABLED = ENV.fetch("LANDING_USERS_ENABLED", "enabled") == "enabled"
