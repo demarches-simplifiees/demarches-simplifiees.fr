9dcd92a67820fbdda40a92b9976f41bb2f726ec3
Pierre de La Morinerie <kemenaran@gmail.com>
2022-03-22 12:31:34 +0000

    db: add not-null constraints to AdministrateursInstructeur

--- app/models/administrateurs_instructeur.rb
+++ app/models/administrateurs_instructeur.rb
@@ -4,8 +4,8 @@
 #
 #  created_at        :datetime
 #  updated_at        :datetime
-#  administrateur_id :integer
-#  instructeur_id    :integer
+#  administrateur_id :integer          not null
+#  instructeur_id    :integer          not null
 #
 class AdministrateursInstructeur < ApplicationRecord
   belongs_to :administrateur
