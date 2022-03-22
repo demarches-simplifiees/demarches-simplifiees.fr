0638dbc2432e18aa38afcadaaf9b77f9d062dfd4
maatinito <15379878+maatinito@users.noreply.github.com>
2022-03-22 12:20:57 -1000

    Graphql: added comments on traitements fields

--- app/graphql/types/traitement_type.rb
+++ app/graphql/types/traitement_type.rb
@@ -1,9 +1,9 @@
 module Types
   class TraitementType < Types::BaseObject
     global_id_field :id
-    field :state, Types::DossierType::DossierState, null: false
-    field :date_traitement, GraphQL::Types::ISO8601DateTime, null: false, method: :processed_at
-    field :email_agent_traitant, String, null: true, method: :instructeur_email
-    field :motivation, String, null: true
+    field :state, Types::DossierType::DossierState, "La décision sur le dossier.", null: false
+    field :date_traitement, GraphQL::Types::ISO8601DateTime, 'La date de la décision', null: false, method: :processed_at
+    field :email_agent_traitant, String, "L'instructeur ayant pris la décision.", null: true, method: :instructeur_email
+    field :motivation, String, 'La motivation de la décision.', null: true
   end
 end
