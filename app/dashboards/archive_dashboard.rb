e97b332b35f27d8d42a3881ebc6166e4394ab914
Martin <martin@sharypic.com>
2022-03-30 16:19:14 +0200

    feat(archive): extract archive status management within job to simplify the main service as well as to isolate this part for a merge with exports csv/xslx [maybe?]
    Update app/dashboards/archive_dashboard.rb
    
    Co-authored-by: LeSim <mail@simon.lehericey.net>

---
+++ app/dashboards/archive_dashboard.rb
@@ -0,0 +1,46 @@
+require "administrate/base_dashboard"
+
+class ArchiveDashboard < Administrate::BaseDashboard
+  # ATTRIBUTE_TYPES
+  # a hash that describes the type of each of the model's fields.
+  #
+  # Each different type represents an Administrate::Field object,
+  # which determines how the attribute is displayed
+  # on pages throughout the dashboard.
+  ATTRIBUTE_TYPES = {
+    id: Field::Number,
+    created_at: Field::DateTime,
+    updated_at: Field::DateTime,
+    status: Field::String,
+    file: Field::HasOne
+  }.freeze
+
+  # COLLECTION_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's index page.
+  #
+  # By default, it's limited to four items to reduce clutter on index pages.
+  # Feel free to add, remove, or rearrange items.
+  COLLECTION_ATTRIBUTES = [
+    :id,
+    :created_at,
+    :updated_at,
+    :status
+  ].freeze
+
+  # SHOW_PAGE_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's show page.
+  SHOW_PAGE_ATTRIBUTES = [
+    :id,
+    :created_at,
+    :updated_at,
+    :status,
+    :file
+  ].freeze
+
+  # Overwrite this method to customize how users are displayed
+  # across all pages of the admin dashboard.
+  #
+  def display_resource(archive)
+    "Archive : #{archive&.file.&byte_size}"
+  end
+end
--- app/jobs/archive_creation_job.rb
+++ app/jobs/archive_creation_job.rb
@@ -2,8 +2,14 @@ class ArchiveCreationJob < ApplicationJob
   queue_as :archives
 
   def perform(procedure, archive, instructeur)
+    archive.restart! if archive.failed? # restart for AASM
     ProcedureArchiveService
       .new(procedure)
-      .collect_files_archive(archive, instructeur)
+      .make_and_upload_archive(archive, instructeur)
+    archive.make_available!
+    InstructeurMailer.send_archive(instructeur, procedure, archive).deliver_later
+  rescue => e
+    archive.fail! # fail for observability
+    raise e       # re-raise for retryable behaviour
   end
 end
