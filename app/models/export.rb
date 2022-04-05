27ebfb4923f374cd5cc3aec96ff2b31dc6592b05
Paul Chavard <pro@paul.chavard.net>
2022-04-05 15:57:19 +0200

    feat(exports): add ability to create exports with filters

--- app/models/export.rb
+++ app/models/export.rb
@@ -46,10 +46,13 @@ class Export < ApplicationRecord
 
   after_create_commit :compute_async
 
-  FORMATS = [:xlsx, :ods, :csv].flat_map do |format|
-    Export.time_span_types.values.map do |time_span_type|
-      [format, time_span_type]
+  FORMATS_WITH_TIME_SPAN = [:xlsx, :ods, :csv].flat_map do |format|
+    time_span_types.keys.map do |time_span_type|
+      { format: format.to_sym, time_span_type: time_span_type }
     end
+  end
+  FORMATS = [:xlsx, :ods, :csv].map do |format|
+    { format: format.to_sym }
   end
 
   def compute_async
@@ -58,7 +61,7 @@ class Export < ApplicationRecord
 
   def compute
     file.attach(
-      io: io(since: since),
+      io: io,
       filename: filename,
       content_type: content_type,
       # We generate the exports ourselves, so they are safe
@@ -78,44 +81,96 @@ class Export < ApplicationRecord
     updated_at < 20.minutes.ago
   end
 
-  def self.find_or_create_export(format, time_span_type, groupe_instructeurs)
-    create_with(groupe_instructeurs: groupe_instructeurs)
+  def filtered?
+    procedure_presentation_id.present?
+  end
+
+  def xlsx?
+    format == self.class.formats.fetch(:xlsx)
+  end
+
+  def ods?
+    format == self.class.formats.fetch(:ods)
+  end
+
+  def csv?
+    format == self.class.formats.fetch(:csv)
+  end
+
+  def self.find_or_create_export(format, groupe_instructeurs, time_span_type: time_span_types.fetch(:everything), statut: statuts.fetch(:tous), procedure_presentation: nil)
+    create_with(groupe_instructeurs: groupe_instructeurs, procedure_presentation: procedure_presentation)
+      .includes(:procedure_presentation)
       .create_or_find_by(format: format,
         time_span_type: time_span_type,
-        key: generate_cache_key(groupe_instructeurs.map(&:id)))
+        statut: statut,
+        key: generate_cache_key(groupe_instructeurs.map(&:id), procedure_presentation&.id))
   end
 
-  def self.find_for_groupe_instructeurs(groupe_instructeurs_ids)
-    exports = where(key: generate_cache_key(groupe_instructeurs_ids))
+  def self.find_for_groupe_instructeurs(groupe_instructeurs_ids, procedure_presentation)
+    exports = if procedure_presentation.present?
+      where(key: generate_cache_key(groupe_instructeurs_ids))
+        .or(where(key: generate_cache_key(groupe_instructeurs_ids, procedure_presentation.id)))
+    else
+      where(key: generate_cache_key(groupe_instructeurs_ids))
+    end
+    filtered, not_filtered = exports.partition(&:filtered?)
 
-    [:xlsx, :csv, :ods].map do |format|
-      [
-        format,
-        Export.time_span_types.values.map do |time_span_type|
-          [time_span_type, exports.find { |export| export.format == format.to_s && export.time_span_type == time_span_type }]
-        end.filter { |(_, export)| export.present? }.to_h
-      ]
-    end.filter { |(_, exports)| exports.present? }.to_h
+    {
+      xlsx: {
+        time_span_type: not_filtered.filter(&:xlsx?).index_by(&:time_span_type),
+        statut: filtered.filter(&:xlsx?).index_by(&:statut)
+      },
+      ods: {
+        time_span_type: not_filtered.filter(&:ods?).index_by(&:time_span_type),
+        statut: filtered.filter(&:ods?).index_by(&:statut)
+      },
+      csv: {
+        time_span_type: not_filtered.filter(&:csv?).index_by(&:time_span_type),
+        statut: filtered.filter(&:csv?).index_by(&:statut)
+      }
+    }
   end
 
-  def self.generate_cache_key(groupe_instructeurs_ids)
-    groupe_instructeurs_ids.sort.join('-')
+  def self.generate_cache_key(groupe_instructeurs_ids, procedure_presentation_id = nil)
+    if procedure_presentation_id.present?
+      "#{groupe_instructeurs_ids.sort.join('-')}--#{procedure_presentation_id}"
+    else
+      groupe_instructeurs_ids.sort.join('-')
+    end
+  end
+
+  def count
+    if procedure_presentation_id.present?
+      dossiers_for_export.size
+    end
   end
 
   private
 
-  def filename
-    procedure_identifier = procedure.path || "procedure-#{procedure.id}"
-    "dossiers_#{procedure_identifier}_#{Time.zone.now.strftime('%Y-%m-%d_%H-%M')}.#{format}"
+  def dossiers_for_export
+    @dossiers_for_export ||= begin
+      dossiers = Dossier.where(groupe_instructeur: groupe_instructeurs)
+
+      if since.present?
+        dossiers.visible_by_administration.where('dossiers.depose_at > ?', since)
+      elsif procedure_presentation.present?
+        filtered_sorted_ids = procedure_presentation
+          .filtered_sorted_ids(dossiers, dossiers.size, statut)
+
+        dossiers.where(id: filtered_sorted_ids)
+      else
+        dossiers
+      end
+    end
   end
 
-  def io(since: nil)
-    dossiers = Dossier.visible_by_administration
-      .where(groupe_instructeur: groupe_instructeurs)
-    if since.present?
-      dossiers = dossiers.where('dossiers.depose_at > ?', since)
-    end
-    service = ProcedureExportService.new(procedure, dossiers)
+  def filename
+    procedure_identifier = procedure.path || "procedure-#{procedure.id}"
+    "dossiers_#{procedure_identifier}_#{statut}_#{Time.zone.now.strftime('%Y-%m-%d_%H-%M')}.#{format}"
+  end
+
+  def io
+    service = ProcedureExportService.new(procedure, dossiers_for_export)
 
     case format.to_sym
     when :csv
