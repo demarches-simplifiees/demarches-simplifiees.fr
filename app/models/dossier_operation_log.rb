8f053ddc513b104cc829caf06bd5917a25aa9003
Martin <martin@sharypic.com>
2022-04-06 15:52:46 +0200

    fix(user.merge): when merging old_user having an instructeur with dossier_operation_logs containing the old_user.instructeur_id, should be able to merge to new instructeur
    feat(user.merge): when user have an instructeur having dossier_opertions_logs containing instructeur_id

--- app/models/dossier_operation_log.rb
+++ app/models/dossier_operation_log.rb
@@ -12,7 +12,6 @@
 #  updated_at          :datetime         not null
 #  bill_signature_id   :bigint
 #  dossier_id          :bigint
-#  instructeur_id      :bigint
 #
 class DossierOperationLog < ApplicationRecord
   enum operation: {
