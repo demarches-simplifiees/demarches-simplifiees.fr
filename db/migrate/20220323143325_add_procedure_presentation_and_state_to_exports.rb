f84c7ac5e4fb56084328e39e31b84894234d3da0
Martin <martin@sharypic.com>
2022-04-07 10:18:12 +0200

    fix(migration): apply what strong migration recommends

--- db/migrate/20220323143325_add_procedure_presentation_and_state_to_exports.rb
+++ db/migrate/20220323143325_add_procedure_presentation_and_state_to_exports.rb
@@ -3,9 +3,8 @@ class AddProcedurePresentationAndStateToExports < ActiveRecord::Migration[6.1]
 
   def change
     add_reference :exports, :procedure_presentation, null: true, index: { algorithm: :concurrently }
-    StrongMigrations.disable_check(:add_column)
-    add_column :exports, :statut, :string, default: 'tous'
-    StrongMigrations.enable_check(:add_column)
+    add_column :exports, :statut, :string
+    change_column_default :exports, :statut, "tous"
     remove_index :exports, [:format, :time_span_type, :key]
     add_index :exports, [:format, :time_span_type, :statut, :key], unique: true, algorithm: :concurrently
   end
---
+++ db/migrate/20220407081538_backfill_export_status.rb
@@ -0,0 +1,10 @@
+class BackfillExportStatus < ActiveRecord::Migration[6.1]
+  disable_ddl_transaction!
+
+  def change
+    Export.in_batches do |relation|
+      relation.update_all statut: "tous"
+      sleep(0.01)
+    end
+  end
+end
