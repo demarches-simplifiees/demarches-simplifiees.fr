21945b6df085f6b8978a2a459e450f9662a6ce5c
Paul Chavard <pro@paul.chavard.net>
2022-04-07 09:31:44 +0200

    fix(exports): disable strong migration checks because we are operating on a “small” table

--- db/migrate/20220323143325_add_procedure_presentation_and_state_to_exports.rb
+++ db/migrate/20220323143325_add_procedure_presentation_and_state_to_exports.rb
@@ -3,7 +3,9 @@ class AddProcedurePresentationAndStateToExports < ActiveRecord::Migration[6.1]
 
   def change
     add_reference :exports, :procedure_presentation, null: true, index: { algorithm: :concurrently }
+    StrongMigrations.disable_check(:add_column)
     add_column :exports, :statut, :string, default: 'tous'
+    StrongMigrations.enable_check(:add_column)
     remove_index :exports, [:format, :time_span_type, :key]
     add_index :exports, [:format, :time_span_type, :statut, :key], unique: true, algorithm: :concurrently
   end
