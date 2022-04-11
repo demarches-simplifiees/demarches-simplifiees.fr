dd4fa3fb74a5bd66dfe31b9f1b538447cb2569f1
simon lehericey <mail@simon.lehericey.net>
2022-04-11 17:22:32 +0200

    dolist config

---
+++ config/initializers/dolist.rb
@@ -0,0 +1,27 @@
+ActiveSupport.on_load(:action_mailer) do
+  module Dolist
+    class SMTP < ::Mail::SMTP
+      def deliver!(mail)
+        mail.from(ENV['DOLIST_NO_REPLY_EMAIL'])
+        mail.sender(ENV['DOLIST_NO_REPLY_EMAIL'])
+        mail['X-ACCOUNT-ID'] = Rails.application.secrets.dolist[:account_id]
+
+        mail['X-Dolist-Message-Name'] = mail.subject # for tracking in Dolist UI
+        mail['X-Dolist-Sending-Type'] = 'TransactionalService' # send even if the target is not active
+
+        super(mail)
+      end
+    end
+  end
+
+  ActionMailer::Base.add_delivery_method :dolist, Dolist::SMTP
+
+  ActionMailer::Base.dolist_settings = {
+    user_name: Rails.application.secrets.dolist[:username],
+    password: Rails.application.secrets.dolist[:password],
+    address: 'smtp.dolist.net',
+    port: 587,
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+end
