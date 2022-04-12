53de567b59f913624f5f98f9cefadab14982db81
simon lehericey <mail@simon.lehericey.net>
2022-04-12 11:59:39 +0200

    revert fix(user.merge) : waiting for ignore column instructeur_id in ruby code

--- db/migrate/20220406144202_remove_column_instructeur_id_from_dossier_operation_log.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnInstructeurIdFromDossierOperationLog < ActiveRecord::Migration[6.1]
-  def change
-    safety_assured { remove_column :dossier_operation_logs, :instructeur_id }
-  end
-end
