22aefbaa4ad0bcaec99af70a8887e6b0ac643320
Paul Chavard <pro@paul.chavard.net>
2022-04-21 18:50:08 +0200

    refactor(js): remove geo-area event handlers

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -30,7 +30,6 @@ import '../new_design/messagerie';
 import '../new_design/dossiers/auto-save';
 import '../new_design/dossiers/auto-upload';
 
-import '../new_design/champs/carte';
 import '../new_design/champs/linked-drop-down-list';
 import '../new_design/champs/repetition';
 import '../new_design/champs/drop-down-list';
