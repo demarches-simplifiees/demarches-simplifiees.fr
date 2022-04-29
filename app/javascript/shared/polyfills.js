b1cacc0f1f868b396d527f272fecc6cb0a3af7a6
Paul Chavard <pro@paul.chavard.net>
2022-04-29 14:11:00 +0200

    fix(js): more polyfills for IE

--- app/javascript/shared/polyfills.js
+++ app/javascript/shared/polyfills.js
@@ -6,8 +6,12 @@ import 'regenerator-runtime/runtime';
 import 'dom4';
 import 'intersection-observer';
 import 'whatwg-fetch';
-import '@webcomponents/webcomponentsjs';
+import '@webcomponents/custom-elements';
+import '@webcomponents/template';
 import '@stimulus/polyfills';
+import 'formdata-polyfill';
+import 'event-target-polyfill';
+import 'yet-another-abortcontroller-polyfill';
 
 import './polyfills/insertAdjacentElement';
 import './polyfills/dataset';
