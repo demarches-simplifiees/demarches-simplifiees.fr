9bc5364ca242f208451bd4237484a36581f637c8
Pierre de La Morinerie <kemenaran@gmail.com>
2022-05-04 06:57:14 +0000

    app: add controller and view to generate a deposit receipt

---
+++ app/helpers/papertrail_helper.rb
@@ -0,0 +1,19 @@
+module PapertrailHelper
+  def papertrail_requester_identity(dossier)
+    if dossier.etablissement.present?
+      raison_sociale_or_name(dossier.etablissement)
+    else
+      [dossier.individual.prenom, dossier.individual.nom.upcase].join(' ')
+    end
+  end
+
+  def papertrail_dossier_state(dossier)
+    raise "Dossiers in 'brouillon' state are not supported" if dossier.brouillon?
+    # i18n-tasks-use t('users.dossiers.papertrail.dossier_state.en_construction')
+    # i18n-tasks-use t('users.dossiers.papertrail.dossier_state.en_instruction')
+    # i18n-tasks-use t('users.dossiers.papertrail.dossier_state.accepte')
+    # i18n-tasks-use t('users.dossiers.papertrail.dossier_state.refuse')
+    # i18n-tasks-use t('users.dossiers.papertrail.dossier_state.sans_suite')
+    I18n.t("users.dossiers.papertrail.states.#{dossier.state}")
+  end
+end
---
+++ spec/views/users/dossiers/papertrail.pdf.prawl_spec.rb
@@ -0,0 +1,25 @@
+describe 'users/dossiers/papertrail.pdf.prawn', type: :view do
+  before do
+    assign(:dossier, dossier)
+  end
+
+  subject { render }
+
+  context 'for a dossier with an individual' do
+    let(:dossier) { create(:dossier, :en_construction, :with_service, :with_individual) }
+
+    it 'renders a PDF document with the dossier state' do
+      subject
+      expect(rendered).to be_present
+    end
+  end
+
+  context 'for a dossier with a SIRET' do
+    let(:dossier) { create(:dossier, :en_construction, :with_service, :with_entreprise) }
+
+    it 'renders a PDF document with the dossier state' do
+      subject
+      expect(rendered).to be_present
+    end
+  end
+end
