adb19a941f202a51186dfab51957ecc453eb2bda
Paul Chavard <pro@paul.chavard.net>
2022-05-06 19:42:40 +0200

    feat(autosave): add stimulus autosave controller

--- app/javascript/controllers/index.ts
+++ app/javascript/controllers/index.ts
@@ -4,9 +4,11 @@ import { ReactController } from './react_controller';
 import { TurboEventController } from './turbo_event_controller';
 import { GeoAreaController } from './geo_area_controller';
 import { TurboInputController } from './turbo_input_controller';
+import { AutosaveController } from './autosave_controller';
 
 const Stimulus = Application.start();
 Stimulus.register('react', ReactController);
 Stimulus.register('turbo-event', TurboEventController);
 Stimulus.register('geo-area', GeoAreaController);
 Stimulus.register('turbo-input', TurboInputController);
+Stimulus.register('autosave', AutosaveController);
