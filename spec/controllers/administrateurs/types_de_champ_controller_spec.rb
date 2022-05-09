a1cce991965abec90dc3c0e8049cae60243fedaa
Martin <martin@sharypic.com>
2022-05-09 13:48:21 +0200

    bug(adminstrateurs/types_de_champs#destroy): when type_de_champs is piece_justificative in repetition, it crashes

--- spec/controllers/administrateurs/types_de_champ_controller_spec.rb
+++ spec/controllers/administrateurs/types_de_champ_controller_spec.rb
@@ -2,60 +2,77 @@ describe Administrateurs::TypesDeChampController, type: :controller do
   let(:admin) { create(:administrateur) }
 
   describe '#types_de_champs editor api' do
-    let(:procedure) { create(:procedure) }
-
-    before do
-      admin.procedures << procedure
-      sign_in(admin.user)
-    end
-
-    let(:type_champ) { TypeDeChamp.type_champs.fetch(:text) }
-
-    context "create type_de_champ text" do
-      before do
-        post :create, params: {
-          procedure_id: procedure.id,
-          type_de_champ: {
-            type_champ: type_champ,
-            libelle: 'Nouveau champ'
-          }
-        }
-      end
-
-      it { expect(response).to have_http_status(:created) }
-    end
-
-    context "validate type_de_champ linked_drop_down_list" do
-      let(:type_champ) { TypeDeChamp.type_champs.fetch(:linked_drop_down_list) }
+    describe 'create' do
+      let(:procedure) { create(:procedure) }
 
       before do
-        post :create, params: {
-          procedure_id: procedure.id,
-          type_de_champ: {
-            type_champ: type_champ,
-            libelle: 'Nouveau champ'
-          }
-        }
+        admin.procedures << procedure
+        sign_in(admin.user)
       end
 
-      it { expect(response).to have_http_status(:unprocessable_entity) }
+      let(:type_champ) { TypeDeChamp.type_champs.fetch(:text) }
+
+      context "create type_de_champ text" do
+        before do
+          post :create, params: {
+            procedure_id: procedure.id,
+            type_de_champ: {
+              type_champ: type_champ,
+              libelle: 'Nouveau champ'
+            }
+          }
+        end
+
+        it { expect(response).to have_http_status(:created) }
+      end
+
+      context "validate type_de_champ linked_drop_down_list" do
+        let(:type_champ) { TypeDeChamp.type_champs.fetch(:linked_drop_down_list) }
+
+        before do
+          post :create, params: {
+            procedure_id: procedure.id,
+            type_de_champ: {
+              type_champ: type_champ,
+              libelle: 'Nouveau champ'
+            }
+          }
+        end
+
+        it { expect(response).to have_http_status(:unprocessable_entity) }
+      end
+
+      context "create type_de_champ linked_drop_down_list" do
+        let(:type_champ) { TypeDeChamp.type_champs.fetch(:linked_drop_down_list) }
+
+        before do
+          post :create, params: {
+            procedure_id: procedure.id,
+            type_de_champ: {
+              type_champ: type_champ,
+              libelle: 'Nouveau champ',
+              drop_down_list_value: '--value--'
+            }
+          }
+        end
+
+        it { expect(response).to have_http_status(:created) }
+      end
     end
 
-    context "create type_de_champ linked_drop_down_list" do
-      let(:type_champ) { TypeDeChamp.type_champs.fetch(:linked_drop_down_list) }
+    describe 'destroy' do
+      let(:procedure) { create(:procedure, :with_repetition_piece_justificative) }
 
-      before do
-        post :create, params: {
-          procedure_id: procedure.id,
-          type_de_champ: {
-            type_champ: type_champ,
-            libelle: 'Nouveau champ',
-            drop_down_list_value: '--value--'
-          }
-        }
+      context 'repetition, type de champ pj' do
+        before do
+          admin.procedures << procedure
+          sign_in(admin.user)
+          delete :destroy, params: { procedure_id: procedure, id: procedure.types_de_champ.repetition.first.types_de_champ.first.stable_id }
+        end
+        it 'works' do
+          expect(response).to have_http_status(:no_content)
+        end
       end
-
-      it { expect(response).to have_http_status(:created) }
     end
   end
 end
--- spec/factories/procedure.rb
+++ spec/factories/procedure.rb
@@ -196,6 +196,12 @@ FactoryBot.define do
       end
     end
 
+    trait :with_repetition_piece_justificative do
+      after(:build) do |procedure, _evaluator|
+        build(:type_de_champ_repetition, types_de_champ: [build(:type_de_champ_piece_justificative)], procedure: procedure)
+      end
+    end
+
     trait :with_number do
       after(:build) do |procedure, _evaluator|
         build(:type_de_champ_number, procedure: procedure)
