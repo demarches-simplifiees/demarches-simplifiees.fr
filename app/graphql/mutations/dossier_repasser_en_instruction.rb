15c1abdeefb667bcbdc3d15f8f941494a5528c48
Paul Chavard <pro@paul.chavard.net>
2022-05-20 19:01:54 +0200

    feat(graphql): expose repasser_en_construction and repasser_en_instruction

---
+++ app/graphql/mutations/dossier_repasser_en_instruction.rb
@@ -0,0 +1,27 @@
+module Mutations
+  class DossierRepasserEnInstruction < Mutations::BaseMutation
+    include DossierHelper
+
+    description "Re-passer le dossier en instruction."
+
+    argument :dossier_id, ID, "Dossier ID", required: true, loads: Types::DossierType
+    argument :instructeur_id, ID, "Instructeur qui prend la décision sur le dossier.", required: true, loads: Types::ProfileType
+    argument :disable_notification, Boolean, "Désactiver l’envoi de l’email de notification après l’opération", required: false, default_value: false
+
+    field :dossier, Types::DossierType, null: true
+    field :errors, [Types::ValidationErrorType], null: true
+
+    def resolve(dossier:, instructeur:, disable_notification:)
+      dossier.repasser_en_instruction!(instructeur: instructeur, disable_notification: disable_notification)
+
+      { dossier: dossier }
+    end
+
+    def authorized?(dossier:, instructeur:, **args)
+      if !dossier.termine?
+        return false, { errors: ["Le dossier est déjà #{dossier_display_state(dossier, lower: true)}"] }
+      end
+      dossier_authorized_for?(dossier, instructeur)
+    end
+  end
+end
