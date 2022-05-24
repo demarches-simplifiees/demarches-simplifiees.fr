f7da9d0895e6b1bd435dc85da13da87946d3a744
Christophe Robillard <christophe.robillard@beta.gouv.fr>
2022-05-24 11:56:14 +0200

    administrators can specify if a demarche is internal

--- app/controllers/administrateurs/procedures_controller.rb
+++ app/controllers/administrateurs/procedures_controller.rb
@@ -297,6 +297,7 @@ module Administrateurs
         :duree_conservation_dossiers_dans_ds,
         :zone_id,
         :lien_dpo,
+        :internal,
         :procedure_expires_when_termine_enabled
       ]
       permited_params = if @procedure&.locked?
