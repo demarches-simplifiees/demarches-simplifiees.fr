cf97215d75fa508cc328866cb0362c38557b0cc0
simon lehericey <mail@simon.lehericey.net>
2022-06-09 13:43:35 +0200

    add compatibility checks

--- spec/models/logic_spec.rb
+++ spec/models/logic_spec.rb
@@ -9,4 +9,9 @@ describe Logic do
 
     expect(Logic.from_h(greater_than(constant(1), constant(2)).to_h)).to eq(greater_than(constant(1), constant(2)))
   end
+
+  describe '.compatible_type?' do
+    it { expect(Logic.compatible_type?(constant(true), constant(true))).to be true }
+    it { expect(Logic.compatible_type?(constant(1), constant(true))).to be false }
+  end
 end
