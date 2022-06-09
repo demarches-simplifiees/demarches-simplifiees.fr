58da4805fa78f1a581723a5953e73785dc2ff0ec
simon lehericey <mail@simon.lehericey.net>
2022-06-09 11:48:30 +0200

    add constant

---
+++ app/models/logic.rb
@@ -0,0 +1,17 @@
+module Logic
+  def self.from_h(h)
+    class_from_name(h['op']).from_h(h)
+  end
+
+  def self.from_json(s)
+    from_h(JSON.parse(s))
+  end
+
+  def self.class_from_name(name)
+    [Constant]
+      .find { |c| c.name == name }
+  end
+
+  def constant(value) = Logic::Constant.new(value)
+
+end
---
+++ spec/models/logic_spec.rb
@@ -0,0 +1,8 @@
+describe Logic do
+  include Logic
+
+  it 'serializes deserializes' do
+    expect(Logic.from_h(constant(1).to_h)).to eq(constant(1))
+    expect(Logic.from_json(constant(1).to_json)).to eq(constant(1))
+  end
+end
