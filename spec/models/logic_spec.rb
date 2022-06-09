77f463717013224d331181aa1e044e77568035ed
simon lehericey <mail@simon.lehericey.net>
2022-06-09 11:54:29 +0200

    add empty

--- spec/models/logic_spec.rb
+++ spec/models/logic_spec.rb
@@ -4,5 +4,7 @@ describe Logic do
   it 'serializes deserializes' do
     expect(Logic.from_h(constant(1).to_h)).to eq(constant(1))
     expect(Logic.from_json(constant(1).to_json)).to eq(constant(1))
+
+    expect(Logic.from_h(empty.to_h)).to eq(empty)
   end
 end
