c680be387de0ad59c956937708db29cc0a2b6426
Paul Chavard <pro@paul.chavard.net>
2022-06-09 13:01:22 +0100

    feat(procedure_presentation): add since date filters

--- app/models/concerns/dossier_filtering_concern.rb
+++ app/models/concerns/dossier_filtering_concern.rb
@@ -4,9 +4,16 @@ module DossierFilteringConcern
   included do
     scope :filter_by_datetimes, lambda { |column, dates|
       if dates.present?
-        dates
-          .map { |date| self.where(column => date..(date + 1.day)) }
-          .reduce(:or)
+        case column
+        when 'depose_since'
+          where('dossiers.depose_at >= ?', dates.sort.first)
+        when 'updated_since'
+          where('dossiers.updated_at >= ?', dates.sort.first)
+        else
+          dates
+            .map { |date| self.where(column => date..(date + 1.day)) }
+            .reduce(:or)
+        end
       else
         none
       end
