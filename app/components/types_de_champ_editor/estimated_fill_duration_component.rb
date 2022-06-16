f0b47aa7f4e698295e69b10de0b01c0549cc3f34
Paul Chavard <pro@paul.chavard.net>
2022-06-16 14:56:53 +0200

    feat(type_de_champ): type_de_champ editor in stimulus/turbo

---
+++ app/components/types_de_champ_editor/estimated_fill_duration_component.rb
@@ -0,0 +1,22 @@
+class TypesDeChampEditor::EstimatedFillDurationComponent < ApplicationComponent
+  def initialize(revision:, is_annotation: false)
+    @revision = revision
+    @is_annotation = is_annotation
+  end
+
+  private
+
+  def annotations?
+    @is_annotation
+  end
+
+  def show?
+    !annotations? && @revision.types_de_champ_public.present?
+  end
+
+  def estimated_fill_duration_minutes
+    seconds = @revision.estimated_fill_duration
+    minutes = (seconds / 60.0).round
+    [1, minutes].max
+  end
+end
