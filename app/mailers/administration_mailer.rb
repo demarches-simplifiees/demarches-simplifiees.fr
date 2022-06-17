fe57ebaaf48228aefbfc7b96cf92b90b0e0bec88
Christian <15379878+maatinito@users.noreply.github.com>
2022-06-17 10:31:26 -1000

    Fix Administration_mailer

--- app/mailers/administration_mailer.rb
+++ app/mailers/administration_mailer.rb
@@ -28,4 +28,23 @@ class AdministrationMailer < ApplicationMailer
       subject: subject,
       reply_to: CONTACT_EMAIL)
   end
+
+  def procedure_published(procedure)
+    @procedure = procedure
+    @champs = procedure.types_de_champ
+    subject = "Une nouvelle démarche vient d'être publiée"
+    mail(to: EQUIPE_EMAIL, subject: subject)
+  end
+
+  def s3_synchronization_report(log)
+    uploaded_stats = S3Synchronization.uploaded_stats
+    @uploaded_stats = to_array(uploaded_stats)
+    checked_stats = S3Synchronization.checked_stats
+    @checked_stats = to_array(checked_stats)
+
+    @status = S3Synchronization.blob_status
+    @log = log
+
+    mail(to: CONTACT_EMAIL, subject: "Statistiques de synchronisation")
+  end
 end
