7fb85c97cb37e143c13587341e7a566532030cd3
Martin <martin@sharypic.com>
2022-06-23 13:51:58 +0200

    feat(Administrateur::ArchivesControllers): add archives #index and #create for admin

--- app/jobs/archive_creation_job.rb
+++ app/jobs/archive_creation_job.rb
@@ -1,13 +1,13 @@
 class ArchiveCreationJob < ApplicationJob
   queue_as :archives
 
-  def perform(procedure, archive, instructeur)
+  def perform(procedure, archive, administrateur_or_instructeur)
     archive.restart! if archive.failed? # restart for AASM
     ProcedureArchiveService
       .new(procedure)
-      .make_and_upload_archive(archive, instructeur)
+      .make_and_upload_archive(archive)
     archive.make_available!
-    InstructeurMailer.send_archive(instructeur, procedure, archive).deliver_later
+    UserMailer.send_archive(administrateur_or_instructeur, procedure, archive).deliver_later
   rescue => e
     archive.fail! # fail for observability
     raise e       # re-raise for retryable behaviour
