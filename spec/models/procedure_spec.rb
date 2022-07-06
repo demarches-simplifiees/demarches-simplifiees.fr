d4458b63d96fad671a93d604cfdb6348a4f09b48
Paul Chavard <pro@paul.chavard.net>
2022-07-06 15:55:09 +0200

    fix(dossier): fix some reset draft changes edge cases

--- spec/models/procedure_spec.rb
+++ spec/models/procedure_spec.rb
@@ -906,17 +906,21 @@ describe Procedure do
     end
 
     context "published procedure" do
-      let(:procedure) { create(:procedure, :published) }
+      let(:procedure) { create(:procedure, :published, attestation_template: create(:attestation_template), dossier_submitted_message: create(:dossier_submitted_message)) }
 
       it "should reset draft revision" do
         procedure.draft_revision.add_type_de_champ(tdc_attributes)
         previous_draft_revision = procedure.draft_revision
+        previous_attestation_template = previous_draft_revision.attestation_template
+        previous_dossier_submitted_message = previous_draft_revision.dossier_submitted_message
 
         expect(procedure.draft_changed?).to be_truthy
         procedure.reset_draft_revision!
         expect(procedure.draft_changed?).to be_falsey
         expect(procedure.draft_revision).not_to eq(previous_draft_revision)
         expect { previous_draft_revision.reload }.to raise_error(ActiveRecord::RecordNotFound)
+        expect(procedure.draft_revision.attestation_template).to eq(previous_attestation_template)
+        expect(procedure.draft_revision.dossier_submitted_message).to eq(previous_dossier_submitted_message)
       end
     end
   end
