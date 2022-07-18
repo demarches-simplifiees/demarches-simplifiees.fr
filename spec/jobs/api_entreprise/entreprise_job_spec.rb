bfe9728e609b50728d456ff988335746bf97784b
Colin Darie <colin@darie.eu>
2022-07-18 12:24:41 +0200

    chore(entreprises): save etat_administratif from API

--- spec/jobs/api_entreprise/entreprise_job_spec.rb
+++ spec/jobs/api_entreprise/entreprise_job_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe APIEntreprise::EntrepriseJob, type: :job do
   let(:siret) { '41816609600051' }
   let(:siren) { '418166096' }
-  let(:etablissement) { create(:etablissement, siret: siret) }
+  let(:etablissement) { create(:etablissement, siret: siret, entreprise_etat_administratif: nil) }
   let(:procedure) { create(:procedure) }
   let(:procedure_id) { procedure.id }
   let(:body) { File.read('spec/fixtures/files/api_entreprise/entreprises.json') }
@@ -18,5 +18,13 @@ RSpec.describe APIEntreprise::EntrepriseJob, type: :job do
   it 'updates etablissement' do
     subject
     expect(Etablissement.find(etablissement.id).entreprise_numero_tva_intracommunautaire).to eq('FR16418166096')
+  end
+
+  it 'convert entreprise etat_administratif source to an enum' do
+    subject
+    etablissement.reload
+
+    expect(etablissement.entreprise_etat_administratif).to eq("actif")
+    expect(etablissement.entreprise_etat_administratif_actif?)
   end
 end
