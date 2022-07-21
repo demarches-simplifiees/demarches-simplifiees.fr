1067a1590f6b5262c5dae809b36b2f302a8e533f
Paul Chavard <pro@paul.chavard.net>
2022-07-21 14:54:14 +0200

    fix(graphql): handle annotations inside blocks

--- app/graphql/mutations/dossier_modifier_annotation_integer_number.rb
+++ app/graphql/mutations/dossier_modifier_annotation_integer_number.rb
@@ -6,12 +6,17 @@ module Mutations
 
     def resolve(dossier:, annotation_id:, instructeur:, value:)
       resolve_with_type(
-        :integer_number,
-        dossier,
-        annotation_id,
-        instructeur,
-        value
+        dossier: dossier,
+        annotation_id: annotation_id,
+        instructeur: instructeur,
+        value: value
       )
+    end
+
+    private
+
+    def input_type
+      :integer_number
     end
   end
 end
