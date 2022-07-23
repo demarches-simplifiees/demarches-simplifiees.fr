de572d6ab562300908c6d6db45d528fd029b5ec0
Paul Chavard <pro@paul.chavard.net>
2022-07-23 12:29:18 +0200

    test(factory): add a new way to build/create types_de_champ

--- spec/factories/procedure_revision.rb
+++ spec/factories/procedure_revision.rb
@@ -6,17 +6,36 @@ FactoryBot.define do
 
     after(:build) do |revision, evaluator|
       if evaluator.from_original
-        original = evaluator.from_original
+        from_revision = evaluator.from_original
 
-        revision.procedure = original.procedure
-        revision.attestation_template_id = original.attestation_template_id
-        revision.dossier_submitted_message_id = original.dossier_submitted_message_id
-        original.revision_types_de_champ_public.each do |r_tdc|
-          revision.revision_types_de_champ_public << build(:procedure_revision_type_de_champ, from_original: r_tdc)
+        revision.procedure = from_revision.procedure
+        revision.attestation_template_id = from_revision.attestation_template_id
+        revision.dossier_submitted_message_id = from_revision.dossier_submitted_message_id
+
+        coordinate_map = {}
+        revision.association(:revision_types_de_champ).target = from_revision.revision_types_de_champ.map do |from_coordinate|
+          parent = from_coordinate.parent.present? ? coordinate_map[from_coordinate.parent] : nil
+
+          coordinate = build(:procedure_revision_type_de_champ,
+            revision: revision,
+            type_de_champ: from_coordinate.type_de_champ,
+            position: from_coordinate.position,
+            parent: parent)
+
+          if parent.present?
+            parent.association(:revision_types_de_champ).target << coordinate
+          elsif from_coordinate.private?
+            revision.association(:revision_types_de_champ_private).target << coordinate
+          else
+            revision.association(:revision_types_de_champ_public).target << coordinate
+          end
+
+          coordinate_map[from_coordinate] = coordinate
+          coordinate
         end
-        original.revision_types_de_champ_private.each do |r_tdc|
-          revision.revision_types_de_champ_private << build(:procedure_revision_type_de_champ, from_original: r_tdc)
-        end
+        revision.association(:types_de_champ).target = revision.revision_types_de_champ.map(&:type_de_champ)
+        revision.association(:types_de_champ_public).target = revision.revision_types_de_champ_public.map(&:type_de_champ)
+        revision.association(:types_de_champ_private).target = revision.revision_types_de_champ_private.map(&:type_de_champ)
       end
     end
   end
