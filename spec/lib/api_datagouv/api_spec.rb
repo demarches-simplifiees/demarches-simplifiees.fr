058d6baae60f8e99f19c9a2cf0b05fc15bfbd1d4
Kara Diaby <kdiaby.pro@gmail.com>
2022-07-25 09:16:07 +0200

    tests

--- spec/lib/api_datagouv/api_spec.rb
+++ spec/lib/api_datagouv/api_spec.rb
@@ -1,9 +1,12 @@
 describe APIDatagouv::API do
   describe '#upload' do
-    let(:subject) { APIDatagouv::API.upload(Tempfile.new.path) }
+    let(:dataset) { :descriptif_demarches_dataset }
+    let(:resource) { :descriptif_demarches_resource }
+    let(:datagouv_secret) { Rails.application.secrets.datagouv }
+    let(:subject) { APIDatagouv::API.upload(Tempfile.new, dataset, resource) }
 
     before do
-      stub_request(:post, /https:\/\/www.data.gouv.fr\/api/)
+      stub_request(:post, /https:\/\/www.data.gouv.fr\/api\/1\/datasets\/#{datagouv_secret[dataset]}\/resources\/#{datagouv_secret[resource]}\/upload\//)
         .to_return(body: body, status: status)
     end
 
