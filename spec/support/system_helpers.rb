1601a18c34e6762254f9a445c173279fc94b3c9c
Colin Darie <colin@darie.eu>
2022-08-03 16:13:52 +0200

    test(combobox): increase wait time because component may be slow to render
    Fix erreur type 'Unable to find css "li[role=\"option\"]'
    
    Ce problème peut-être reproduit en local, en diminuant le wait à 0.001 ;
    avec une plus grande valuer il disparaît.

--- spec/support/system_helpers.rb
+++ spec/support/system_helpers.rb
@@ -105,7 +105,7 @@ module SystemHelpers
 
   def select_combobox(libelle, fill_with, value, check: true)
     fill_in libelle, with: fill_with
-    find('li[role="option"]', text: value).click
+    find('li[role="option"]', text: value, wait: 5).click
     if check
       check_selected_value(libelle, with: value)
     end
