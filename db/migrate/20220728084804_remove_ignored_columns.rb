e2cd9385a1cd073eca7835cbfaa4c5cf9a44ff05
Paul Chavard <pro@paul.chavard.net>
2022-08-03 14:22:34 +0200

    fix(db): types_de_champ migration is too long

--- db/migrate/20220728084804_remove_ignored_columns.rb
+++ db/migrate/20220728084804_remove_ignored_columns.rb
@@ -3,10 +3,6 @@ class RemoveIgnoredColumns < ActiveRecord::Migration[6.1]
     safety_assured do
       remove_column :commentaires, :user_id
       remove_column :dossiers, :en_construction_conservation_extension
-      remove_column :types_de_champ, :migrated_parent
-      remove_column :types_de_champ, :revision_id
-      remove_column :types_de_champ, :parent_id
-      remove_column :types_de_champ, :order_place
     end
   end
 end
