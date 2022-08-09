14df8c54fd35835462f96d8a5a55381bb69a6dd2
Christophe Robillard <christophe.robillard@beta.gouv.fr>
2022-08-09 18:54:36 +0200

    log saml response

--- app/controllers/saml_idp_controller.rb
+++ app/controllers/saml_idp_controller.rb
@@ -6,7 +6,8 @@ class SamlIdpController < ActionController::Base
   def new
     if super_admin_signed_in?
       @saml_response = encode_SAMLResponse(current_super_admin.email, saml_attributes)
-      Rails.logger.info "SAML ACS URL: #{@saml_acs_url}"
+      Rails.logger.debug "SAML ACS URL: #{@saml_acs_url}"
+      Rails.logger.debug "SAML RESPONSE: #{@saml_response}"
       render :template => "saml_idp/idp/saml_post", :layout => false
     else
       redirect_to root_path, alert: t("errors.messages.saml_not_authorized")
