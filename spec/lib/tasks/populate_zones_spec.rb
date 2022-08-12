bc5ea8e9062232bd080be706a7b93c82fbd418b1
Christophe Robillard <christophe.robillard@beta.gouv.fr>
2022-08-12 17:19:42 +0200

    update zones with new government

--- spec/lib/tasks/populate_zones_spec.rb
+++ spec/lib/tasks/populate_zones_spec.rb
@@ -10,5 +10,9 @@ describe 'populate_zones' do
   it 'populates zones' do
     run_task
     expect(Zone.find_by(acronym: 'PM').label).to eq "Premier ministre"
+    expect(Zone.find_by(acronym: 'MTEI').labels.first.designated_on).to eq Date.parse('2022-05-20')
+    expect(Zone.find_by(acronym: 'MTEI').labels.first.name).to eq "Ministère du Travail, du Plein emploi et de l'Insertion"
+    expect(Zone.find_by(acronym: 'MTEI').labels.last.designated_on).to eq Date.parse('2020-07-06')
+    expect(Zone.find_by(acronym: 'MTEI').labels.last.name).to eq "Ministère du Travail"
   end
 end
