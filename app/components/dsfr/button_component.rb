1107b811e77bfe153e06fd73cb86f80a230951ae
Martin <martin@sharypic.com>
2022-08-17 21:41:16 +0200

    feat(Administrateur::Procedure::Service.form): enhance wording and styl

---
+++ app/components/dsfr/button_component.rb
@@ -0,0 +1,12 @@
+class Dsfr::ButtonComponent < ApplicationComponent
+  private
+
+  def initialize(label:, form:nil, url: nil, class_names: [])
+    @form = form
+    @label = label
+    @url = url
+    @class_names = Array(class_names)
+  end
+
+  attr_reader :form, :url, :label, :class_names
+end
