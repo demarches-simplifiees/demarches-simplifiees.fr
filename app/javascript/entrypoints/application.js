1e333cc901005e157dd548c6b8cfdff032dc189c
Martin <martin@sharypic.com>
2022-08-17 18:02:27 +0200

    feat(Administrateurs::Procedure::Service#form): wrap explanation within DSFR::CallOut

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -72,3 +72,4 @@ import('../shared/track/sentry');
 import '../../../node_modules/@gouvfr/dsfr/dist/core/core.css';
 import '../../../node_modules/@gouvfr/dsfr/dist/component/button/button.css';
 import '../../../node_modules/@gouvfr/dsfr/dist/component/alert/alert.css';
+import '../../../node_modules/@gouvfr/dsfr/dist/component/callout/callout.css';
