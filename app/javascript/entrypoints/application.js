d225e1cd69ff43df3e5814407b3612f18e7b0e7f
Martin <martin@sharypic.com>
2022-08-17 21:46:17 +0200

    feat(Administrateur::Service.form): use callout

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -74,3 +74,4 @@ import '../../../node_modules/@gouvfr/dsfr/dist/core/core.css';
 import '../../../node_modules/@gouvfr/dsfr/dist/component/button/button.css';
 import '../../../node_modules/@gouvfr/dsfr/dist/component/alert/alert.css';
 import '../../../node_modules/@gouvfr/dsfr/dist/component/breadcrumb/breadcrumb.css';
+import '../../../node_modules/@gouvfr/dsfr/dist/component/callout/callout.css';
