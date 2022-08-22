d8da21761d553f7856c21ee9803100c9b2b037fe
Christian <15379878+maatinito@users.noreply.github.com>
2022-08-22 09:16:33 -1000

    Fix flipper test: flipper needs a symbol

--- app/models/type_de_champ.rb
+++ app/models/type_de_champ.rb
@@ -18,7 +18,7 @@ class TypeDeChamp < ApplicationRecord
   self.ignored_columns = [:migrated_parent, :revision_id, :parent_id, :order_place]
 
   FILE_MAX_SIZE = 200.megabytes
-  FEATURE_FLAGS = { 'visa' => 'visa' }
+  FEATURE_FLAGS = { :visa => 'visa' }
 
   INSTANCE_TYPE_CHAMPS = {
     nationalites: 'nationalites',
