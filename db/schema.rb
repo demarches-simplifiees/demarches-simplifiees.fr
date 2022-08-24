e6eb6bbbb91e0595984f9010f47f438a23bf1765
Christophe Robillard <christophe.robillard@beta.gouv.fr>
2022-08-24 12:44:13 +0200

    backfill procedures_zones

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_06_190110) do
+ActiveRecord::Schema.define(version: 2022_10_06_193737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
