5b9bb83a0af68d36feed0460f444481136606691
Damien Le Thiec <damien.lethiec@gmail.com>
2022-09-21 15:56:01 +0200

    Clean exports tests

--- spec/lib/tasks/deployment/20220705164551_remove_unused_champs_spec.rb
+++ spec/lib/tasks/deployment/20220705164551_remove_unused_champs_spec.rb
@@ -14,9 +14,9 @@ describe '20220705164551_remove_unused_champs' do
 
   describe 'remove_unused_champs' do
     it "with bad champs" do
-      expect(Champ.where(dossier: dossier).count).to eq(39)
+      expect(Champ.where(dossier: dossier).count).to eq(40)
       run_task
-      expect(Champ.where(dossier: dossier).count).to eq(38)
+      expect(Champ.where(dossier: dossier).count).to eq(39)
     end
   end
 end
