7a9c5e8d1946da5055340e84090c9de73cc99a2b
simon lehericey <mail@simon.lehericey.net>
2022-09-29 16:47:55 +0200

    refactor(ujs): turbo use an id for `turbo_stream.update id` not a class

--- app/javascript/new_design/fc-fusion.js
+++ app/javascript/new_design/fc-fusion.js
@@ -3,11 +3,11 @@ import { show, hide } from '@utils';
 export function showFusion() {
   show(document.querySelector('.fusion'));
   hide(document.querySelector('.new-account'));
-  hide(document.querySelector('.new-account-password-confirmation'));
+  hide(document.querySelector('#new-account-password-confirmation'));
 }
 
 export function showNewAccount() {
   hide(document.querySelector('.fusion'));
   show(document.querySelector('.new-account'));
-  hide(document.querySelector('.new-account-password-confirmation'));
+  hide(document.querySelector('#new-account-password-confirmation'));
 }
