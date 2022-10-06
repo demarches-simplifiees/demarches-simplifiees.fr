b9ef8c500115f1c64ae244aaa265690248006c44
simon lehericey <mail@simon.lehericey.net>
2022-10-06 15:22:24 +0200

    fix: add_instructeur preview

--- spec/mailers/previews/groupe_instructeur_mailer_preview.rb
+++ spec/mailers/previews/groupe_instructeur_mailer_preview.rb
@@ -1,6 +1,7 @@
 class GroupeInstructeurMailerPreview < ActionMailer::Preview
   def add_instructeurs
-    groupe = GroupeInstructeur.new(label: 'Val-De-Marne')
+    procedure = Procedure.new(id: 1, libelle: 'une superbe procedure')
+    groupe = GroupeInstructeur.new(id: 1, label: 'Val-De-Marne', procedure:)
     current_instructeur_email = 'admin@dgfip.com'
     instructeurs = Instructeur.limit(2)
     GroupeInstructeurMailer.add_instructeurs(groupe, instructeurs, current_instructeur_email)
