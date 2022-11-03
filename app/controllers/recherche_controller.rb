ba0799b684e4fda4e0d81d0876249c7a496d01f3
Lisa Durand <lisa.c.durand@gmail.com>
2022-11-03 17:39:12 +0100

    it displays a message if instructor is looking for a dossier that is not in his instructor group

--- app/controllers/recherche_controller.rb
+++ app/controllers/recherche_controller.rb
@@ -28,6 +28,15 @@ class RechercheController < ApplicationController
     @dossiers_count = matching_dossiers_ids.count
     @followed_dossiers_id = current_instructeur&.followed_dossiers&.where(id: @paginated_ids)&.ids || []
     @dossier_avis_ids_h = current_expert&.avis&.where(dossier_id: @paginated_ids)&.pluck(:dossier_id, :id).to_h || {}
+
+    # we want to retrieve dossiers that are not accessible to the instructor because he is not in the instructor group
+    # to display an alert in the view
+    instructeur_procedure_dossiers_ids = DossierSearchService
+      .matching_dossiers(current_instructeur&.procedures&.map(&:dossiers)&.first, @search_terms, with_annotation: true)
+
+    not_in_instructor_group_dossiers_ids = instructeur_procedure_dossiers_ids - matching_dossiers_ids
+
+    @not_in_instructor_group_dossiers = Dossier.where(id: not_in_instructor_group_dossiers_ids)
   end
 
   private
