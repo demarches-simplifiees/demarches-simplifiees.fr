5df2643b642db68ef79524d76f900aafb976f2e1
Christophe Robillard <christophe.robillard@beta.gouv.fr>
2022-11-04 12:22:39 +0100

    add debounce for date_publication autosubmit

--- app/javascript/controllers/autosubmit_controller.ts
+++ app/javascript/controllers/autosubmit_controller.ts
@@ -1,5 +1,6 @@
 import { ApplicationController } from './application_controller';
 import { toggle } from '@utils';
+const AUTOSUBMIT_DEBOUNCE_DELAY = 5000;
 
 export class AutosubmitController extends ApplicationController {
   static targets = ['form', 'spinner'];
@@ -10,6 +11,11 @@ export class AutosubmitController extends ApplicationController {
   submit() {
     this.formTarget.requestSubmit();
   }
+
+  debouncedSubmit() {
+    this.debounce(this.submit, AUTOSUBMIT_DEBOUNCE_DELAY);
+  }
+
   connect() {
     this.onGlobal('turbo:submit-start', () => {
       toggle(this.spinnerTarget);
