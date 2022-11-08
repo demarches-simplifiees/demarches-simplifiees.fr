a726350f0d727218a5814d01a5956d15eeb82c43
Colin Darie <colin@darie.eu>
2022-11-08 11:39:34 +0100

    feat(procedure): allow admin to enable multiple uploads for piece justificative on existing procedures

--- app/controllers/administrateurs/procedures_controller.rb
+++ app/controllers/administrateurs/procedures_controller.rb
@@ -396,6 +396,9 @@ module Administrateurs
         :procedure_expires_when_termine_enabled,
         :tags
       ]
+
+      editable_params << :piece_justificative_multiple if @procedure&.piece_justificative_multiple == false
+
       permited_params = if @procedure&.locked?
         params.require(:procedure).permit(*editable_params)
       else
