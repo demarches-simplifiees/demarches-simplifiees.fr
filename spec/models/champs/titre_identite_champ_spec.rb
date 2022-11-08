a974805e0640f4bc18d5c09878ccedbe8899f012
Lisa Durand <lisa.c.durand@gmail.com>
2022-11-08 18:00:28 +0100

    remove change on api V1 and update graphql for sensitive data

--- spec/models/champs/titre_identite_champ_spec.rb
+++ spec/models/champs/titre_identite_champ_spec.rb
@@ -11,17 +11,4 @@ describe Champs::TitreIdentiteChamp do
       it { is_expected.to eq('absent') }
     end
   end
-
-  describe '#for_api' do
-    let(:champ_titre_identite) { create(:champ_titre_identite) }
-
-    subject { champ_titre_identite.for_api }
-
-    it { is_expected.to eq('présent') }
-
-    context 'without attached file' do
-      before { champ_titre_identite.piece_justificative_file.purge }
-      it { is_expected.to eq('absent') }
-    end
-  end
 end
