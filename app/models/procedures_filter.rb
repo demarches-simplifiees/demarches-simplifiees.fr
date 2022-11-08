7044b426ea7764cbef6cba766fb5acb9cdd5b95d
Christophe Robillard <christophe.robillard@beta.gouv.fr>
2022-11-08 15:59:00 +0100

    list all admins

--- app/models/procedures_filter.rb
+++ app/models/procedures_filter.rb
@@ -5,7 +5,7 @@ class ProceduresFilter
 
   def initialize(admin, params)
     @admin = admin
-    @params = params.permit(:page, :from_publication_date, zone_ids: [], statuses: [])
+    @params = params.permit(:page, :from_publication_date, :view_admins, zone_ids: [], statuses: [])
   end
 
   def admin_zones
@@ -36,6 +36,10 @@ class ProceduresFilter
     nil
   end
 
+  def view_admins?
+    params[:view_admins] == 'true'
+  end
+
   def zone_filtered?(zone_id)
     zone_ids&.map(&:to_i)&.include?(zone_id)
   end
@@ -53,12 +57,32 @@ class ProceduresFilter
     end
   end
 
+  def with_view_admins(view_admins)
+    params.to_h.merge(view_admins: view_admins)
+  end
+
   def procedures_result
     return @procedures_result if @procedures_result
-    @procedures_result = Procedure.joins(:procedures_zones).publiees_ou_closes
-    @procedures_result = @procedures_result.where(procedures_zones: { zone_id: zone_ids }) if zone_ids.present?
-    @procedures_result = @procedures_result.where(aasm_state: statuses) if statuses.present?
-    @procedures_result = @procedures_result.where('published_at >= ?', from_publication_date) if from_publication_date.present?
-    @procedures_result = @procedures_result.page(params[:page]).per(ITEMS_PER_PAGE).order(published_at: :desc)
+    @procedures_result = paginate(filter_procedures, published_at: :desc)
+  end
+
+  def admins_result
+    return @admins_result if @admins_result
+    @admins_result = Administrateur.includes(:user).where(id: AdministrateursProcedure.where(procedure: filter_procedures).select(:administrateur_id))
+    @admins_result = paginate(@admins_result, 'users.email')
+  end
+
+  private
+
+  def filter_procedures
+    procedures_result = Procedure.joins(:procedures_zones).publiees_ou_closes
+    procedures_result = procedures_result.where(procedures_zones: { zone_id: zone_ids }) if zone_ids.present?
+    procedures_result = procedures_result.where(aasm_state: statuses) if statuses.present?
+    procedures_result = procedures_result.where('published_at >= ?', from_publication_date) if from_publication_date.present?
+    procedures_result
+  end
+
+  def paginate(result, ordered_by)
+    result.page(params[:page]).per(ITEMS_PER_PAGE).order(ordered_by)
   end
 end
