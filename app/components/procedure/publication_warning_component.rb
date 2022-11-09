ba99a52d46b3c8a9a5edb0f806dcc391a3c90b8c
Paul Chavard <pro@paul.chavard.net>
2022-11-09 11:10:19 +0000

    feat(attestation): validate attestation tags

--- app/components/procedure/publication_warning_component.rb
+++ app/components/procedure/publication_warning_component.rb
@@ -23,6 +23,8 @@ class Procedure::PublicationWarningComponent < ApplicationComponent
     case attribute
     when :draft_revision
       champs_admin_procedure_path(@procedure)
+    when :attestation_template
+      edit_admin_procedure_attestation_template_path(@procedure)
     when :initiated_mail, :received_mail, :closed_mail, :refused_mail, :without_continuation_mail
       admin_procedure_mail_templates_path(@procedure)
     end
