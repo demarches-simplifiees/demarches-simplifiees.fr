bd99aaedac9ba598406c00f0fd34aa071819da9a
Christophe Robillard <christophe.robillard@beta.gouv.fr>
2022-11-21 17:40:54 +0100

    search admins by email

--- app/models/procedures_filter.rb
+++ app/models/procedures_filter.rb
@@ -5,7 +5,7 @@ class ProceduresFilter
 
   def initialize(admin, params)
     @admin = admin
-    @params = params.permit(:page, :libelle, :from_publication_date, zone_ids: [], statuses: [])
+    @params = params.permit(:page, :libelle, :email, :from_publication_date, zone_ids: [], statuses: [])
   end
 
   def admin_zones
@@ -38,6 +38,10 @@ class ProceduresFilter
 
   def libelle
     ActiveRecord::Base.sanitize_sql_like(params[:libelle]) if params[:libelle].present?
+  end
+
+  def email
+    ActiveRecord::Base.sanitize_sql_like(params[:email]) if params[:email].present?
   end
 
   def zone_filtered?(zone_id)
