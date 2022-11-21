7e7363c8c3c4114f1e09cc79003cfeda0e0f7052
simon lehericey <mail@simon.lehericey.net>
2022-11-21 12:22:41 +0100

    refactor(application_component): add current_administrateur

--- app/components/application_component.rb
+++ app/components/application_component.rb
@@ -9,4 +9,8 @@ class ApplicationComponent < ViewComponent::Base
   def current_user
     controller.current_user
   end
+
+  def current_administrateur
+    controller.current_administrateur
+  end
 end
