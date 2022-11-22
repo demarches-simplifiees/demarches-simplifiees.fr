7fb34bcbdf67c64b3a0f1d99cd19150210b8d93d
simon lehericey <mail@simon.lehericey.net>
2022-11-22 16:25:10 +0100

    fix(procedure): add not null constraint on duree_conservation

---
+++ db/migrate/20221122145030_clean_duree_conservation.rb
@@ -0,0 +1,9 @@
+class CleanDureeConservation < ActiveRecord::Migration[6.1]
+  def change
+    # We ignore strong_migrations safety warnings, because these table is relatively small
+    safety_assured do
+      change_column_null :procedures, :duree_conservation_etendue_par_ds, false
+      change_column_null :procedures, :max_duree_conservation_dossiers_dans_ds, false
+    end
+  end
+end
