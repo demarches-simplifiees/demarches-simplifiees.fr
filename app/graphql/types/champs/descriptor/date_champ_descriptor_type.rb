ed1754e1fbdab1828defa8401d1a1a20d1e04e5d
Paul Chavard <pro@paul.chavard.net>
2022-11-24 13:14:27 +0100

    refactor(graphql): make champ descriptor an interface

---
+++ app/graphql/types/champs/descriptor/date_champ_descriptor_type.rb
@@ -0,0 +1,5 @@
+module Types::Champs::Descriptor
+  class DateChampDescriptorType < Types::BaseObject
+    implements Types::ChampDescriptorType
+  end
+end
---
+++ app/graphql/types/champs/descriptor/pays_champ_descriptor_type.rb
@@ -0,0 +1,5 @@
+module Types::Champs::Descriptor
+  class PaysChampDescriptorType < Types::BaseObject
+    implements Types::ChampDescriptorType
+  end
+end
