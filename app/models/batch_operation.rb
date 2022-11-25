4266ab93c51b862ff64fbdc6c5dd9d035c91a860
Martin <martin@sharypic.com>
2022-11-25 12:30:06 +0100

    poc(batch_operation): track dossier processed safely

--- app/models/batch_operation.rb
+++ app/models/batch_operation.rb
@@ -31,6 +31,29 @@ class BatchOperation < ApplicationRecord
       .map { |dossier| BatchOperationProcessOneJob.perform_later(self, dossier) }
   end
 
+  def track_dossier_processed(success, dossier)
+    transaction do
+      dossier.update(batch_operation: nil)
+      reload
+      manager = Arel::UpdateManager.new.table(arel_table).where(arel_table[:id].eq(id))
+      values = []
+      values.push([arel_table[:run_at], Time.zone.now]) if called_for_first_time?
+      values.push([arel_table[:finished_at], Time.zone.now]) if called_for_last_time?
+      if success
+        values.push([arel_table[:success_dossier_ids],Arel::Nodes::NamedFunction.new('array_append', [arel_table[:success_dossier_ids], dossier.id])])
+        values.push([arel_table[:failed_dossier_ids], Arel::Nodes::NamedFunction.new('array_remove', [arel_table[:failed_dossier_ids], dossier.id])])
+      else
+        values.push([arel_table[:failed_dossier_ids], Arel::Nodes::NamedFunction.new('array_append', [arel_table[:failed_dossier_ids], dossier.id])])
+      end
+      manager.set(values)
+      ActiveRecord::Base.connection.update(manager.to_sql)
+    end
+  end
+
+  def arel_table
+    BatchOperation.arel_table
+  end
+
   def process_one(dossier)
     case operation
     when BatchOperation.operations.fetch(:archiver)
