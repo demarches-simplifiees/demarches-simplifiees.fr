ed4d5cb36a042715820ecaf48a92676db510b8d8
Paul Chavard <pro@paul.chavard.net>
2022-12-02 18:15:43 +0100

    refactor(demarche): make declarative demarche processing syncroneous

--- app/controllers/users/dossiers_controller.rb
+++ app/controllers/users/dossiers_controller.rb
@@ -165,6 +165,7 @@ module Users
 
       if errors.blank?
         @dossier.passer_en_construction!
+        @dossier.process_declarative!
         NotificationMailer.send_en_construction_notification(@dossier).deliver_later
         @dossier.groupe_instructeur.instructeurs.with_instant_email_dossier_notifications.each do |instructeur|
           DossierMailer.notify_new_dossier_depose_to_instructeur(@dossier, instructeur.email).deliver_later
