c9c172ce69f3667d44ec9b5da4d289995c3ccd7d
Paul Chavard <pro@paul.chavard.net>
2022-12-04 12:36:01 +0100

    feat(event_store): add event_store tables

---
+++ db/migrate/20221203113551_create_event_store_events.rb
@@ -0,0 +1,26 @@
+class CreateEventStoreEvents < ActiveRecord::Migration[4.2]
+  def change
+    create_table(:event_store_events_in_streams, id: :bigserial, force: false) do |t|
+      t.string      :stream,      null: false
+      t.integer     :position,    null: true
+      t.references  :event,       null: false, type: :uuid
+      t.datetime    :created_at,  null: false
+    end
+    add_index :event_store_events_in_streams, [:stream, :position], unique: true
+    add_index :event_store_events_in_streams, [:created_at]
+    add_index :event_store_events_in_streams, [:stream, :event_id], unique: true
+
+    create_table(:event_store_events, id: :bigserial, force: false) do |t|
+      t.references  :event,       null: false, type: :uuid
+      t.string      :event_type,  null: false
+      t.jsonb       :metadata
+      t.jsonb       :data, null: false
+      t.datetime    :created_at,  null: false
+      t.datetime    :valid_at,    null: true
+    end
+    add_index :event_store_events, :event_id, unique: true
+    add_index :event_store_events, :created_at
+    add_index :event_store_events, :valid_at
+    add_index :event_store_events, :event_type
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -436,6 +436,29 @@ ActiveRecord::Schema.define(version: 2022_12_05_144624) do
     t.index ["dossier_id"], name: "index_etablissements_on_dossier_id", unique: true
   end
 
+  create_table "event_store_events", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.jsonb "data", null: false
+    t.uuid "event_id", null: false
+    t.string "event_type", null: false
+    t.jsonb "metadata"
+    t.datetime "valid_at"
+    t.index ["created_at"], name: "index_event_store_events_on_created_at"
+    t.index ["event_id"], name: "index_event_store_events_on_event_id", unique: true
+    t.index ["event_type"], name: "index_event_store_events_on_event_type"
+    t.index ["valid_at"], name: "index_event_store_events_on_valid_at"
+  end
+
+  create_table "event_store_events_in_streams", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.uuid "event_id", null: false
+    t.integer "position"
+    t.string "stream", null: false
+    t.index ["created_at"], name: "index_event_store_events_in_streams_on_created_at"
+    t.index ["stream", "event_id"], name: "index_event_store_events_in_streams_on_stream_and_event_id", unique: true
+    t.index ["stream", "position"], name: "index_event_store_events_in_streams_on_stream_and_position", unique: true
+  end
+
   create_table "exercices", id: :serial, force: :cascade do |t|
     t.string "ca"
     t.datetime "created_at"
