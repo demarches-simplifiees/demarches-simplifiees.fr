95a42d26cb224d8fc7d4506802964f56a5fa25b6
Sébastien Carceles <sebastien.carceles@gmail.com>
2022-12-05 09:32:00 +0100

    review: prefer if to unless
    Co-authored-by: LeSim <mail@simon.lehericey.net>

--- app/models/concerns/dossier_prefillable_concern.rb
+++ app/models/concerns/dossier_prefillable_concern.rb
@@ -4,7 +4,7 @@ module DossierPrefillableConcern
   extend ActiveSupport::Concern
 
   def prefill!(champs_public_attributes)
-    return unless champs_public_attributes.any?
+    return if champs_public_attributes.empty?
 
     assign_attributes(champs_public_attributes: champs_public_attributes.map { |h| h.merge(prefilled: true) })
     save(validate: false)
