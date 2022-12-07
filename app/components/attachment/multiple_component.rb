24325988342726cbc042407796a7b4aa48c9238d
Colin Darie <colin@darie.eu>
2022-12-08 00:15:05 +0100

    refactor(attachment): rename user_can_download -> explicit view_as
    - link: shows a simple link to open attachment
    - download: shows complete DSFR download UI

--- app/components/attachment/multiple_component.rb
+++ app/components/attachment/multiple_component.rb
@@ -9,18 +9,17 @@ class Attachment::MultipleComponent < ApplicationComponent
   attr_reader :champ
   attr_reader :form_object_name
   attr_reader :max
+  attr_reader :view_as
   attr_reader :user_can_destroy
   alias user_can_destroy? user_can_destroy
-  attr_reader :user_can_download
-  alias user_can_download? user_can_download
 
   delegate :count, :empty?, to: :attachments, prefix: true
 
-  def initialize(champ:, attached_file:, form_object_name: nil, user_can_download: false, user_can_destroy: true, max: nil)
+  def initialize(champ:, attached_file:, form_object_name: nil, view_as: :link, user_can_destroy: true, max: nil)
     @champ = champ
     @attached_file = attached_file
     @form_object_name = form_object_name
-    @user_can_download = user_can_download
+    @view_as = view_as
     @user_can_destroy = user_can_destroy
     @max = max || DEFAULT_MAX_ATTACHMENTS
 
