53db7ef7dddd22f3649450755458c91b4e7eddf5
sebastiencarceles <sebastien.carceles@gmail.com>
2022-12-09 13:52:26 +0100

    spec: cover the case

--- spec/system/users/dossier_prefill_spec.rb
+++ spec/system/users/dossier_prefill_spec.rb
@@ -1,4 +1,5 @@
 describe 'Prefilling a dossier:' do
+  let(:password) { 'my-s3cure-p4ssword' }
   let(:siret) { '41816609600051' }
 
   let(:procedure) { create(:procedure, :published, opendata: true) }
@@ -13,20 +14,16 @@ describe 'Prefilling a dossier:' do
     stub_request(:get, /https:\/\/entreprise.api.gouv.fr\/v2\/etablissements\/#{siret}/)
       .to_return(status: 200, body: File.read('spec/fixtures/files/api_entreprise/etablissements.json'))
 
-    visit new_dossier_path(
-      procedure_id: procedure.id,
+    visit commencer_path(
+      path: procedure.path,
       "champ_#{type_de_champ_text.to_typed_id}" => text_value,
       "champ_#{type_de_champ_phone.to_typed_id}" => phone_value
     )
   end
 
-  context 'when the user already exists' do
-    let(:password) { 'my-s3cure-p4ssword' }
-    let!(:user) { create(:user, password: password) }
-
-    scenario "the user has got a prefilled dossier after signing in" do
-      expect(page).to have_content("Connectez-vous")
-      sign_in_with user.email, password
+  shared_examples "the user has got a prefilled dossier" do
+    scenario "the user has got a prefilled dossier" do
+      click_on "Commencer la démarche"
 
       expect(page).to have_current_path siret_dossier_path(procedure.dossiers.last)
       fill_in 'Numéro SIRET', with: siret
@@ -43,28 +40,41 @@ describe 'Prefilling a dossier:' do
     end
   end
 
-  context 'when this is a new user' do
-    before do
-      allow_any_instance_of(FranceConnectParticulierClient).to receive(:authorization_uri).and_return(france_connect_particulier_callback_path(code: "c0d3"))
-      allow(FranceConnectService).to receive(:retrieve_user_informations_particulier).and_return(build(:france_connect_information))
+  context 'when the user signs in with email and password' do
+    it_behaves_like "the user has got a prefilled dossier" do
+      let!(:user) { create(:user, password: password) }
+
+      before do
+        click_on "J’ai déjà un compte"
+        sign_in_with user.email, password
+      end
     end
+  end
 
-    scenario "the user has got a prefilled dossier after signing up" do
-      expect(page).to have_content("Connectez-vous")
-      page.find('.fr-connect').click
+  context 'when the user signs up with email and password' do
+    it_behaves_like "the user has got a prefilled dossier" do
+      let(:user_email) { generate :user_email }
 
-      expect(page).to have_current_path siret_dossier_path(procedure.dossiers.last)
-      fill_in 'Numéro SIRET', with: siret
-      click_on 'Valider'
+      before do
+        click_on "Créer un compte #{APPLICATION_NAME}"
 
-      expect(page).to have_current_path(etablissement_dossier_path(dossier))
-      expect(page).to have_content('OCTO TECHNOLOGY')
-      click_on 'Continuer avec ces informations'
+        sign_up_with user_email, password
+        expect(page).to have_content "nous avons besoin de vérifier votre adresse #{user_email}"
 
-      expect(page).to have_current_path(brouillon_dossier_path(dossier))
-      expect(page).to have_field(type_de_champ_text.libelle, with: text_value)
-      expect(page).to have_field(type_de_champ_phone.libelle, with: phone_value)
-      expect(page).to have_css('.field_with_errors', text: type_de_champ_phone.libelle)
+        click_confirmation_link_for user_email
+        expect(page).to have_content('Votre compte a bien été confirmé.')
+      end
+    end
+  end
+
+  context 'when the user signs up with FranceConnect' do
+    it_behaves_like "the user has got a prefilled dossier" do
+      before do
+        allow_any_instance_of(FranceConnectParticulierClient).to receive(:authorization_uri).and_return(france_connect_particulier_callback_path(code: "c0d3"))
+        allow(FranceConnectService).to receive(:retrieve_user_informations_particulier).and_return(build(:france_connect_information))
+
+        page.find('.fr-connect').click
+      end
     end
   end
 end
