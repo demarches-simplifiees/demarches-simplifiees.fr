013a05f24a67b89928ad049a08d38b6b98731900
Colin Darie <colin@darie.eu>
2022-12-13 16:13:31 +0100

    feat(piece_justificative): replace with multiple attachments for dossier en_construction

--- app/javascript/controllers/replace_attachment_controller.ts
+++ app/javascript/controllers/replace_attachment_controller.ts
@@ -6,8 +6,25 @@ export class ReplaceAttachmentController extends ApplicationController {
 
   declare readonly inputTarget: HTMLInputElement;
 
-  open() {
+  open(event: Event) {
     show(this.inputTarget);
     this.inputTarget.click(); // opens input prompt
+
+    const target = event.currentTarget as HTMLButtonElement;
+
+    if (target.dataset.autoAttachUrl) {
+      // set the auto attach url specific to this button to replace the related attachment
+      this.inputTarget.dataset.originalAutoAttachUrl =
+        this.inputTarget.dataset.autoAttachUrl;
+
+      this.inputTarget.dataset.autoAttachUrl = target.dataset.autoAttachUrl;
+
+      // reset autoAttachUrl which would add an attachment
+      // when replace is not finalized
+      this.inputTarget.addEventListener('cancel', () => {
+        this.inputTarget.dataset.autoAttachUrl =
+          this.inputTarget.dataset.originalAutoAttachUrl;
+      });
+    }
   }
 }
