5069ee700d68e23d2d67a836dfe469ba3ff43899
Paul Chavard <pro@paul.chavard.net>
2022-12-14 09:14:14 +0100

    clean(admin): remove unused code from old controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,20 +4,4 @@ class AdminController < ApplicationController
   def index
     redirect_to(admin_procedures_path)
   end
-
-  def retrieve_procedure
-    id = params[:procedure_id] || params[:id]
-
-    @procedure = current_administrateur.procedures.find(id)
-
-  rescue ActiveRecord::RecordNotFound
-    flash.alert = 'Démarche inexistante'
-    redirect_to admin_procedures_path, status: 404
-  end
-
-  def reset_procedure
-    if @procedure.brouillon?
-      @procedure.reset!
-    end
-  end
 end
