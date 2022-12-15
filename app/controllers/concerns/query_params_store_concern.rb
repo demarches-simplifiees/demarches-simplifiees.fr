36a0fd1b8c3e90d90fd301eddc84218d01aa428e
sebastiencarceles <sebastien.carceles@gmail.com>
2022-12-15 10:46:26 +0100

    review: comment why we dont override already stored params

--- app/controllers/concerns/query_params_store_concern.rb
+++ app/controllers/concerns/query_params_store_concern.rb
@@ -2,6 +2,8 @@ module QueryParamsStoreConcern
   extend ActiveSupport::Concern
 
   def store_query_params
+    # Don't override already stored params, because we could do goings and comings with authentication, and
+    # lost previously stored params
     return if session[:stored_params].present? || request.query_parameters.empty?
 
     session[:stored_params] = request.query_parameters.to_json
