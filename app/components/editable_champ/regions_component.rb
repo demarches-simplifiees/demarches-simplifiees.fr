dd6c0350cf8fc8a569b860796d2a3724ad6d4f54
Paul Chavard <pro@paul.chavard.net>
2022-12-20 21:29:37 +0100

    refactor(regions): champ regions to use simple select

--- app/components/editable_champ/regions_component.rb
+++ app/components/editable_champ/regions_component.rb
@@ -1,3 +1,13 @@
 class EditableChamp::RegionsComponent < EditableChamp::EditableChampBaseComponent
   include ApplicationHelper
+
+  private
+
+  def options
+    APIGeoService.regions.map { [_1[:name], _1[:code]] }
+  end
+
+  def select_options
+    { selected: @champ.selected }.merge(@champ.mandatory? ? { prompt: '' } : { include_blank: '' })
+  end
 end
