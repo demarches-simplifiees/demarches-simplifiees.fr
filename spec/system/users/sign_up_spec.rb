c368e034b19b752f6dc550026c5793aad6ca8073
Martin <martin@sharypic.com>
2022-12-20 17:51:36 +0100

    accessibilite(pages-authentification): evolutions des pages de connexion/creation de compte pour respecter le DSFR et supporter une meilleure accessibilite
    Update app/components/dsfr/input_component/input_component.html.haml
    
    Co-authored-by: Colin Darie <colin@darie.eu>

--- spec/system/users/sign_up_spec.rb
+++ spec/system/users/sign_up_spec.rb
@@ -21,27 +21,27 @@ describe 'Signing up:' do
       visit commencer_path(path: procedure.path)
       click_on "Créer un compte #{APPLICATION_NAME}"
       expect(page).to have_selector('.suspect-email', visible: false)
-      fill_in 'Email', with: 'bidou@yahoo.rf'
+      fill_in 'Adresse éléctronique', with: 'bidou@yahoo.rf'
       fill_in 'Mot de passe', with: '12345'
     end
 
     scenario 'they can accept the suggestion', js: true do
       expect(page).to have_selector('.suspect-email', visible: true)
       click_on 'Oui'
-      expect(page).to have_field("Email", :with => 'bidou@yahoo.fr')
+      expect(page).to have_field("Adresse éléctronique", :with => 'bidou@yahoo.fr')
       expect(page).to have_selector('.suspect-email', visible: false)
     end
 
     scenario 'they can discard the suggestion', js: true do
       expect(page).to have_selector('.suspect-email', visible: true)
       click_on 'Non'
-      expect(page).to have_field("Email", :with => 'bidou@yahoo.rf')
+      expect(page).to have_field("Adresse éléctronique", :with => 'bidou@yahoo.rf')
       expect(page).to have_selector('.suspect-email', visible: false)
     end
 
     scenario 'they can fix the typo themselves', js: true do
       expect(page).to have_selector('.suspect-email', visible: true)
-      fill_in 'Email', with: 'bidou@yahoo.fr'
+      fill_in 'Adresse éléctronique', with: 'bidou@yahoo.fr'
       blur
       expect(page).to have_selector('.suspect-email', visible: false)
     end
@@ -54,7 +54,7 @@ describe 'Signing up:' do
     expect(page).to have_current_path new_user_registration_path
     sign_up_with user_email, '1234567'
     expect(page).to have_current_path user_registration_path
-    expect(page).to have_content 'Le mot de passe est trop court'
+    expect(page).to have_content "Le champ « Mot de passe » est trop court. Saisir un mot de passe avec au moins 8 caractères"
 
     # Then with a good password
     sign_up_with user_email, user_password
