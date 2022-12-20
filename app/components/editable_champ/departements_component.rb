a86a06017611a604bff420bd4b9e4456a459f5f0
Paul Chavard <pro@paul.chavard.net>
2022-12-20 21:29:18 +0100

    refactor(departements): champ departements to use simple select

--- app/components/editable_champ/departements_component.rb
+++ app/components/editable_champ/departements_component.rb
@@ -1,3 +1,13 @@
 class EditableChamp::DepartementsComponent < EditableChamp::EditableChampBaseComponent
   include ApplicationHelper
+
+  private
+
+  def options
+    APIGeoService.departements.map { ["#{_1[:code]} – #{_1[:name]}", _1[:code]] }
+  end
+
+  def select_options
+    { selected: @champ.selected }.merge(@champ.mandatory? ? { prompt: '' } : { include_blank: '' })
+  end
 end
