75d726dc7c3be07d7c4bdeba6fcec3a6c31e4c68
sebastiencarceles <sebastien.carceles@gmail.com>
2022-12-21 15:30:32 +0100

    extract parent for yes no and checkbox champs

--- app/models/champs/yes_no_champ.rb
+++ app/models/champs/yes_no_champ.rb
@@ -20,36 +20,8 @@
 #  parent_id                      :bigint
 #  type_de_champ_id               :integer
 #
-class Champs::YesNoChamp < Champ
-  def search_terms
-    if true?
-      [libelle]
-    end
-  end
-
-  def to_s
-    processed_value
-  end
-
-  def for_tag
-    processed_value
-  end
-
-  def for_export
-    processed_value
-  end
-
+class Champs::YesNoChamp < Champs::BooleanChamp
   def true?
     value == 'true'
-  end
-
-  def for_api_v2
-    true? ? 'true' : 'false'
-  end
-
-  private
-
-  def processed_value
-    true? ? 'Oui' : 'Non'
   end
 end
