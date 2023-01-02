fa7174b9700c9aac9ece1f005fe6a1292f8e9094
Eric Leroy-Terquem <eric.leroyterquem@beta.gouv.fr>
2023-01-02 15:21:10 +0100

    fix category title

--- app/models/type_de_champ.rb
+++ app/models/type_de_champ.rb
@@ -67,7 +67,7 @@ class TypeDeChamp < ApplicationRecord
     dgfip: REFERENTIEL_EXTERNE,
     pole_emploi: REFERENTIEL_EXTERNE,
     mesri: REFERENTIEL_EXTERNE,
-    routage: CADRE
+    routage: STRUCTURE
   }
 
   enum type_champs: {
