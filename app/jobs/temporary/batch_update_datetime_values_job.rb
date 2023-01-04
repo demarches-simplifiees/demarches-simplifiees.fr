e83fe27ab6d7678a5357ff78a8c0f9a405de8431
Damien Le Thiec <damien.lethiec@gmail.com>
2023-01-04 16:22:22 +0100

    Clean batch update datetime job and add test

--- app/jobs/temporary/batch_update_datetime_values_job.rb
+++ app/jobs/temporary/batch_update_datetime_values_job.rb
@@ -2,9 +2,24 @@ class Temporary::BatchUpdateDatetimeValuesJob < ApplicationJob
   def perform(ids)
     ids.each do |id|
       datetime_champ = Champs::DatetimeChamp.find(id)
-      datetime_champ.update_columns(value: DateTime.iso8601(value).to_s)
-    rescue ArgumentError, Date::Error # rubocop:disable Lint/ShadowedException
-      datetime_champ.update_columns(value: nil) unless datetime_champ.value.nil?
+      current_value_in_time = Time.zone.parse(datetime_champ.value)
+
+      if current_value_in_time.present?
+        datetime_champ.update_columns(value: current_value_in_time.iso8601)
+      else
+        update_value_to_nil_if_possible(datetime_champ)
+      end
+
+    rescue TypeError
+      update_value_to_nil_if_possible(datetime_champ)
     end
+  end
+
+  private
+
+  def update_value_to_nil_if_possible(datetime_champ)
+    return if datetime_champ.value.nil?
+
+    datetime_champ.update_columns(value: nil) unless datetime_champ.required?
   end
 end
