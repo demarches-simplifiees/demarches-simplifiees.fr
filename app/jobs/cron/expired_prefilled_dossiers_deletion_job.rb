08931309b0ba5588034cee54e5d37a82ceefbf6a
Damien Le Thiec <damien.lethiec@gmail.com>
2023-01-05 11:58:14 +0100

    Run CRON du delete prefilled dossiers every day

--- app/jobs/cron/expired_prefilled_dossiers_deletion_job.rb
+++ app/jobs/cron/expired_prefilled_dossiers_deletion_job.rb
@@ -1,5 +1,5 @@
 class Cron::ExpiredPrefilledDossiersDeletionJob < Cron::CronJob
-  self.schedule_expression = "every month at 3:00"
+  self.schedule_expression = "every day at 3:00"
 
   def perform
     Dossier.prefilled.state_brouillon.where(user_id: nil, updated_at: ..5.days.ago).destroy_all
