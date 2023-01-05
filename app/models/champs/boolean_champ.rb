fa6fc077b44e7bab691c7847a417699d82707566
Sébastien Carceles <sebastien.carceles@beta.gouv.fr>
2023-01-05 12:18:27 +0100

    normalize boolean values (#8320)
    * extract parent for yes no and checkbox champs
    
    * checkbox stores true / false instead of on / off
    
    * normalize blank value to nil
    
    * normalize invalid value to false
    
    * after party task: normalize checkbox values
    
    * after party task: normalize yes_no values

--- app/models/champs/boolean_champ.rb
+++ app/models/champs/boolean_champ.rb
@@ -20,5 +20,50 @@
 #  parent_id                      :bigint
 #  type_de_champ_id               :integer
 #
-class Champs::YesNoChamp < Champs::BooleanChamp
+class Champs::BooleanChamp < Champ
+  TRUE_VALUE = 'true'
+  FALSE_VALUE = 'false'
+
+  before_validation :set_value_to_nil, if: -> { value.blank? }
+  before_validation :set_value_to_false, unless: -> { ([nil, TRUE_VALUE, FALSE_VALUE]).include?(value) }
+
+  def true?
+    value == TRUE_VALUE
+  end
+
+  def search_terms
+    if true?
+      [libelle]
+    end
+  end
+
+  def to_s
+    processed_value
+  end
+
+  def for_tag
+    processed_value
+  end
+
+  def for_export
+    processed_value
+  end
+
+  def for_api_v2
+    true? ? 'true' : 'false'
+  end
+
+  private
+
+  def processed_value
+    true? ? 'Oui' : 'Non'
+  end
+
+  def set_value_to_nil
+    self.value = nil
+  end
+
+  def set_value_to_false
+    self.value = FALSE_VALUE
+  end
 end
