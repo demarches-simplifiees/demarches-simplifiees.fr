06b8a0f7346f0c2c1cef04d2849b68dd7476f712
Sébastien Carceles <sebastien.carceles@beta.gouv.fr>
2023-01-06 14:01:59 +0100

    feat(dossier): prefill boolean champs (#8381)

--- app/models/champs/boolean_champ.rb
+++ app/models/champs/boolean_champ.rb
@@ -27,6 +27,8 @@ class Champs::BooleanChamp < Champ
   before_validation :set_value_to_nil, if: -> { value.blank? }
   before_validation :set_value_to_false, unless: -> { ([nil, TRUE_VALUE, FALSE_VALUE]).include?(value) }
 
+  validates :value, inclusion: [TRUE_VALUE, FALSE_VALUE], allow_nil: true, allow_blank: false
+
   def true?
     value == TRUE_VALUE
   end
