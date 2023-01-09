f00fd6d844ae29dba077a6b8c6bf4b0eff00adcf
Sébastien Carceles <sebastien.carceles@gmail.com>
2023-01-09 16:43:06 +0100

    review: refactor for readability

--- app/models/types_de_champ/prefill_drop_down_list_type_de_champ.rb
+++ app/models/types_de_champ/prefill_drop_down_list_type_de_champ.rb
@@ -1,13 +1,13 @@
 class TypesDeChamp::PrefillDropDownListTypeDeChamp < TypesDeChamp::PrefillTypeDeChamp
   def possible_values
-    possible_values = drop_down_list_enabled_non_empty_options
     if drop_down_other?
-      possible_values.insert(
+      drop_down_list_enabled_non_empty_options.insert(
         0,
         I18n.t("views.prefill_descriptions.edit.possible_values.drop_down_list_other")
       )
+    else
+      drop_down_list_enabled_non_empty_options
     end
-    possible_values
   end
 
   def example_value
