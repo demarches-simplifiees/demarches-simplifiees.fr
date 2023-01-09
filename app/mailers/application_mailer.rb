223dcafb76b2c0edf61387c382abf51cd5aa450a
Colin Darie <colin@darie.eu>
2023-01-10 00:40:16 +0100

    refactor(mailer): shared error concern

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,24 +1,11 @@
 class ApplicationMailer < ActionMailer::Base
+  include MailerErrorConcern
+
   helper :application # gives access to all helpers defined within `application_helper`.
   default from: "#{APPLICATION_NAME} <#{CONTACT_EMAIL}>"
   layout 'mailer'
 
   before_action :add_dolist_header
-
-  # Don’t retry to send a message if the server rejects the recipient address
-  rescue_from Net::SMTPSyntaxError do |_exception|
-    message.perform_deliveries = false
-  end
-
-  rescue_from Net::SMTPServerBusy do |exception|
-    if /unexpected recipients/.match?(exception.message)
-      message.perform_deliveries = false
-    else
-      log_smtp_error(exception)
-    end
-  end
-
-  rescue_from Net::SMTPError, with: :log_smtp_error
 
   # Attach the procedure logo to the email (if any).
   # Returns the attachment url.
@@ -28,7 +15,6 @@ class ApplicationMailer < ActionMailer::Base
       attachments.inline[logo_filename] = procedure.logo.download
       attachments[logo_filename].url
     end
-
   rescue StandardError => e
     # A problem occured when reading logo, maybe the logo is missing and we should clean the procedure to remove logo reference ?
     Sentry.capture_exception(e, extra: { procedure_id: procedure.id })
@@ -41,15 +27,5 @@ class ApplicationMailer < ActionMailer::Base
   # so we add the dolist header for everyone
   def add_dolist_header
     headers['X-Dolist-Message-Name'] = action_name
-  end
-
-  def log_smtp_error(exception)
-    if defined?(message) && message.to.present?
-      EmailEvent.create_from_message!(message, status: "dispatch_error")
-    end
-
-    Sentry.capture_exception(exception, extra: { to: message&.to, subject: message&.subject })
-
-    # TODO find a way to re attempt the job
   end
 end
---
+++ app/mailers/concerns/mailer_error_concern.rb
@@ -0,0 +1,33 @@
+module MailerErrorConcern
+  extend ActiveSupport::Concern
+
+  included do
+    # Don’t retry to send a message if the server rejects the recipient address
+    rescue_from Net::SMTPSyntaxError do |_exception|
+      message.perform_deliveries = false
+    end
+
+    rescue_from Net::SMTPServerBusy do |exception|
+      if /unexpected recipients/.match?(exception.message)
+        message.perform_deliveries = false
+      else
+        log_delivery_error(exception)
+      end
+    end
+
+    rescue_from StandardError, with: :log_delivery_error
+
+    protected
+
+    def log_delivery_error(exception)
+      if defined?(message) && message.to.present?
+        EmailEvent.create_from_message!(message, status: "dispatch_error")
+        Sentry.capture_exception(exception, extra: { to: message&.to, subject: message&.subject })
+      else
+        Sentry.capture_exception(exception)
+      end
+
+      # TODO find a way to re attempt the job
+    end
+  end
+end
