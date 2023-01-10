fc8d2e656d897770341198c56915f7905c1912d3
Colin Darie <colin@darie.eu>
2023-01-10 15:47:00 +0100

    refactor(email): remove pseudonymization, it will be done at dump-time

--- spec/models/email_event_spec.rb
+++ spec/models/email_event_spec.rb
@@ -1,7 +1,2 @@
 RSpec.describe EmailEvent, type: :model do
-  describe "#pseudonymize_email" do
-    it { expect(EmailEvent.pseudonymize_email("example@rspec.com")).to eq("ex****e@rspec.com") }
-    it { expect(EmailEvent.pseudonymize_email("exa@rspec.com")).to eq("***@rspec.com") }
-    it { expect(EmailEvent.pseudonymize_email("e@rspec.com")).to eq("*@rspec.com") }
-  end
 end
