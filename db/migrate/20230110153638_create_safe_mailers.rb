2f43ffc9402264592060ee9a3e48142e7e16d6f5
Martin <martin@sharypic.com>
2023-01-11 09:37:26 +0100

    data(safe_mailer): ajout d'un object permetant d'orienter les mails vers un unique fournisseur au cas ou l'autre soit down

---
+++ db/migrate/20230110153638_create_safe_mailers.rb
@@ -0,0 +1,9 @@
+class CreateSafeMailers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :safe_mailers do |t|
+      t.string :forced_delivery_method
+
+      t.timestamps
+    end
+  end
+end
