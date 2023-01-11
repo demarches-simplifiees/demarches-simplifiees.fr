01f227e1c7941b3705e250077eda9e3a99b1dfdd
Colin Darie <colin@darie.eu>
2023-01-11 18:50:35 +0100

    fix(mailer): dolist header in devise mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -5,8 +5,6 @@ class ApplicationMailer < ActionMailer::Base
   default from: "#{APPLICATION_NAME} <#{CONTACT_EMAIL}>"
   layout 'mailer'
 
-  before_action :add_dolist_header
-
   # Attach the procedure logo to the email (if any).
   # Returns the attachment url.
   def attach_logo(procedure)
