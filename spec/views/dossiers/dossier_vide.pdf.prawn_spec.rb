ece5eef8210e6caddb5bb79b84af9a80254e4da9
François Vantomme <fvantomme@synbioz.com>
2023-01-12 10:12:10 +0100

    test(pdf): use VCR to test remote images in PDF

--- spec/views/dossiers/dossier_vide.pdf.prawn_spec.rb
+++ spec/views/dossiers/dossier_vide.pdf.prawn_spec.rb
@@ -9,8 +9,28 @@ describe 'dossiers/dossier_vide.pdf.prawn', type: :view do
 
   subject { render }
 
-  it 'renders a PDF document with empty fields' do
-    subject
-    expect(rendered).to be_present
+  describe "with local images" do
+    before do
+      stub_const("DOSSIER_PDF_EXPORT_LOGO_SRC", "app/assets/images/header/logo-ds-wide.png")
+    end
+
+    it 'renders a PDF document with empty fields' do
+      subject
+      expect(rendered).to be_present
+    end
+  end
+
+  describe "with remote images" do
+    before do
+      stub_const("DOSSIER_PDF_EXPORT_LOGO_SRC", "https://example.org/images/logo.png")
+    end
+
+    it 'renders a PDF document with empty fields' do
+      VCR.use_cassette("dossiers/dossier_vide") do
+        subject
+      end
+
+      expect(rendered).to be_present
+    end
   end
 end
