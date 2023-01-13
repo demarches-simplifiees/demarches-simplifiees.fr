28ef10a740286c9ebf636f7c9899fa88dba156f7
Paul Chavard <pro@paul.chavard.net>
2023-01-13 11:13:09 +0100

    refactor(turbo): use @coldwired/actions

--- app/javascript/entrypoints/manager.ts
+++ app/javascript/entrypoints/manager.ts
@@ -1,7 +1,6 @@
 import * as Turbo from '@hotwired/turbo';
 import { Application } from '@hotwired/stimulus';
 
-import '../shared/turbo-actions';
 import '../manager/fields/features';
 import { registerControllers } from '../shared/stimulus-loader';
 
@@ -9,3 +8,7 @@ const application = Application.start();
 registerControllers(application);
 
 Turbo.session.drive = false;
+
+addEventListener('DOMContentLoaded', () => {
+  document.body.setAttribute('data-controller', 'turbo');
+});
