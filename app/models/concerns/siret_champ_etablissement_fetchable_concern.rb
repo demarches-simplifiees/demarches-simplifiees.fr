2ad4e4c01f02c3f17449580a82a8b2040e3d8bdd
Sébastien Carceles <sebastien.carceles@gmail.com>
2023-01-14 10:13:26 +0100

    refacto: better error management

--- app/models/concerns/siret_champ_etablissement_fetchable_concern.rb
+++ app/models/concerns/siret_champ_etablissement_fetchable_concern.rb
@@ -1,34 +1,38 @@
 module SiretChampEtablissementFetchableConcern
   extend ActiveSupport::Concern
 
-  def fetch_etablissement!(siret, user)
-    return clear if siret.empty?
-    return clear(error: :invalid) unless Siret.new(siret: siret).valid? # i18n-tasks-use t('errors.messages.invalid_siret')
-    return clear(error: :not_found) unless (etablissement = APIEntrepriseService.create_etablissement(self, siret, user.id)) # i18n-tasks-use t('errors.messages.siret_not_found')
+  attr_reader :etablissement_fetch_error_key
 
-    return update_etablissement!(etablissement)
+  def fetch_etablissement!(siret, user)
+    return clear_etablissement!(:empty) if siret.empty?
+    return clear_etablissement!(:invalid) unless Siret.new(siret: siret).valid? # i18n-tasks-use t('errors.messages.invalid_siret')
+    return clear_etablissement!(:not_found) unless (etablissement = APIEntrepriseService.create_etablissement(self, siret, user&.id)) # i18n-tasks-use t('errors.messages.siret_not_found')
+
+    update!(value: siret, etablissement: etablissement)
   rescue => error
     if error.try(:network_error?) && !APIEntrepriseService.api_up?
       # TODO: notify ops
-      etablissement = APIEntrepriseService.create_etablissement_as_degraded_mode(self, siret, user.id)
-      return update_etablissement!(etablissement, error: :api_entreprise_down)
+      update!(
+        value: siret,
+        etablissement: APIEntrepriseService.create_etablissement_as_degraded_mode(self, siret, user.id)
+      )
+      @etablissement_fetch_error_key = :api_entreprise_down
+      false
     else
       Sentry.capture_exception(error, extra: { dossier_id: dossier_id, siret: siret })
-      return clear(error: :network_error) # i18n-tasks-use t('errors.messages.siret_network_error')
+      clear_etablissement!(:network_error) # i18n-tasks-use t('errors.messages.siret_network_error')
     end
   end
 
   private
 
-  def update_etablissement!(etablissement, error: nil)
-    update!(value: etablissement.siret, etablissement: etablissement)
-    error.presence || etablissement.siret
-  end
+  def clear_etablissement!(error_key)
+    @etablissement_fetch_error_key = error_key
 
-  def clear(error: nil)
     etablissement_to_destroy = etablissement
     update!(etablissement: nil)
     etablissement_to_destroy&.destroy
-    error.presence
+
+    false
   end
 end
