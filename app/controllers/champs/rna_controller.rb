4fe496a636d7784bf22b875457c6d359b20ca81d
sebastiencarceles <sebastien.carceles@gmail.com>
2023-01-16 15:47:22 +0100

    review: readability

--- app/controllers/champs/rna_controller.rb
+++ app/controllers/champs/rna_controller.rb
@@ -3,6 +3,10 @@ class Champs::RNAController < ApplicationController
 
   def show
     @champ = policy_scope(Champ).find(params[:champ_id])
-    @error = @champ.association_fetch_error_key unless @champ.fetch_association!(read_param_value(@champ.input_name, 'value'))
+    rna = read_param_value(@champ.input_name, 'value')
+
+    unless @champ.fetch_association!(rna)
+      @error = @champ.association_fetch_error_key
+    end
   end
 end
