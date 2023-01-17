abbe5cfc2ebdc8d39dc1b3f507a7c0390d386f37
Paul Chavard <pro@paul.chavard.net>
2023-01-17 15:28:41 +0100

    feat(dossier): implement refresh_after_update on champs

--- app/models/champ.rb
+++ app/models/champ.rb
@@ -67,6 +67,7 @@ class Champ < ApplicationRecord
     :stable_id,
     :mandatory?,
     :prefillable?,
+    :refresh_after_update?,
     to: :type_de_champ
 
   scope :updated_since?, -> (date) { where('champs.updated_at > ?', date) }
