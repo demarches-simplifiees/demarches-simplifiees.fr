961563ce7285ad315b5af75ed69d68371a3749d5
Damien Le Thiec <damien.lethiec@gmail.com>
2023-01-18 12:24:18 +0100

    Fix lint and tests

--- app/models/types_de_champ/prefill_drop_down_list_type_de_champ.rb
+++ app/models/types_de_champ/prefill_drop_down_list_type_de_champ.rb
@@ -3,7 +3,7 @@ class TypesDeChamp::PrefillDropDownListTypeDeChamp < TypesDeChamp::PrefillTypeDe
     if drop_down_other?
       drop_down_list_enabled_non_empty_options.insert(
         0,
-        I18n.t("views.prefill_descriptions.edit.possible_values.drop_down_list_other")
+        I18n.t("views.prefill_descriptions.edit.possible_values.drop_down_list_other_html")
       )
     else
       drop_down_list_enabled_non_empty_options
