ce09a0fb7ee014e186b3ac106d1c59903e9e2789
Lisa Durand <lisa.c.durand@gmail.com>
2023-01-20 11:21:06 +0100

    injected dossier_ids in front

--- app/javascript/controllers/batch_operation_controller.ts
+++ app/javascript/controllers/batch_operation_controller.ts
@@ -1,5 +1,5 @@
 import { ApplicationController } from './application_controller';
-import { disable, enable } from '@utils';
+import { disable, enable, show, hide } from '@utils';
 import invariant from 'tiny-invariant';
 
 export class BatchOperationController extends ApplicationController {
@@ -11,6 +11,7 @@ export class BatchOperationController extends ApplicationController {
 
   onCheckOne() {
     this.toggleSubmitButtonWhenNeeded();
+    deleteSelection();
   }
 
   onCheckAll(event: Event) {
@@ -18,6 +19,25 @@ export class BatchOperationController extends ApplicationController {
 
     this.inputTargets.forEach((e) => (e.checked = target.checked));
     this.toggleSubmitButtonWhenNeeded();
+
+    displayNotice(this.inputTargets);
+  }
+
+  onSelectMore(event) {
+    event.preventDefault();
+
+    const target = event.target as HTMLInputElement;
+    const dossierIds = target.getAttribute('data-dossiers');
+    const hidden_checkbox_multiple_ids = document.querySelector('#checkbox_multiple_batch_operation');
+    hidden_checkbox_multiple_ids.value = dossierIds;
+    hide(document.querySelector('#not_selected'));
+    show(document.querySelector('#selected'));
+  }
+
+  onDeleteSelection(event) {
+    event.preventDefault();
+    emptyCheckboxes();
+    deleteSelection();
   }
 
   toggleSubmitButtonWhenNeeded() {
@@ -62,4 +82,29 @@ function switchButton(button: HTMLButtonElement, flag: boolean) {
     disable(button);
     button.querySelectorAll('button').forEach((button) => disable(button));
   }
+}
+
+function displayNotice(inputs) {
+  if (document.querySelector('#checkbox_all_batch_operation').checked) {
+    show(document.querySelector('.fr-notice'));
+    hide(document.querySelector('#selected'));
+    show(document.querySelector('#not_selected'));
+  } else {
+    hide(document.querySelector('.fr-notice'));
+    deleteSelection();
+  };
+
+  document.querySelector('#dynamic_number').textContent = (inputs.length - 1);
+}
+
+function deleteSelection() {
+  const hidden_checkbox_multiple_ids = document.querySelector('#checkbox_multiple_batch_operation');
+  hidden_checkbox_multiple_ids.value = "";
+
+  hide(document.querySelector('.fr-notice'));
+}
+
+function emptyCheckboxes() {
+  const inputs = document.querySelectorAll('div[data-controller="batch-operation"] input[type=checkbox]')
+  inputs.forEach((e) => (e.checked = false));
 }
