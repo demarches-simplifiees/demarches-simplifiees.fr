557a0680a3eedc73d5e5d7505da3c797bce72f1e
Damien Le Thiec <damien@dlet.fr>
2023-01-23 11:31:06 +0100

    Schema graph_ql (#8406)
    * First draft schema graph_ql
    
    * Add tests for json schema procedures

---
+++ app/controllers/api/public/v1/json_description_procedures_controller.rb
@@ -0,0 +1,25 @@
+class API::Public::V1::JSONDescriptionProceduresController < API::Public::V1::BaseController
+  skip_before_action :check_content_type_is_json
+  before_action :retrieve_procedure
+
+  def show
+    render json: procedure_graph_ql_schema, status: 200
+  end
+
+  private
+
+  def retrieve_procedure
+    @procedure = Procedure.publiees_ou_brouillons.opendata.find_by(path: params[:path])
+    render_not_found("procedure", params[:path]) if @procedure.blank?
+  end
+
+  def procedure_graph_ql_schema
+    API::V2::Schema.execute(API::V2::StoredQuery.get('ds-query-v2'),
+      variables: {
+        demarche: { "number": @procedure.id },
+        includeRevision: true
+      },
+      operation_name: "getDemarcheDescriptor")
+      .to_h.dig("data", "demarcheDescriptor").to_json
+  end
+end
---
+++ spec/controllers/api/public/v1/json_description_procedures_controller_spec.rb
@@ -0,0 +1,37 @@
+RSpec.describe API::Public::V1::JSONDescriptionProceduresController, type: :controller do
+  include Rails.application.routes.url_helpers
+
+  describe '#show' do
+    let(:procedure) { create(:procedure, :published, :with_type_de_champ) }
+    subject(:show_request) do
+      get :show, params: params
+    end
+
+    before { show_request }
+
+    context 'the procedure is found' do
+      let(:params) { { path: procedure.path } }
+      let(:expected_response) do
+        API::V2::Schema.execute(API::V2::StoredQuery.get('ds-query-v2'),
+          variables: {
+            demarche: { "number": procedure.id },
+            includeRevision: true
+          },
+          operation_name: "getDemarcheDescriptor")
+          .to_h.dig("data", "demarcheDescriptor").to_json
+      end
+
+      it { expect(response).to have_http_status(:success) }
+
+      it { expect(response.body).to eq(expected_response) }
+    end
+
+    context "the procedure is not found" do
+      let(:params) { { path: "error" } }
+
+      it { expect(response).to have_http_status(:not_found) }
+
+      it { expect(response).to have_failed_with("procedure error is not found") }
+    end
+  end
+end
