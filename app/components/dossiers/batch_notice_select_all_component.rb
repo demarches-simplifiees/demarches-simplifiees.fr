9aa6c55d1f300902be4b83db206e86977cadf38b
Lisa Durand <lisa.c.durand@gmail.com>
2023-01-25 11:01:44 +0100

    replace partial by component

---
+++ app/components/dossiers/batch_notice_select_all_component.rb
@@ -0,0 +1,6 @@
+class Dossiers::BatchNoticeSelectAllComponent < ApplicationComponent
+  def initialize(dossiers_count:, filtered_sorted_ids:)
+    @dossiers_count = dossiers_count
+    @filtered_sorted_ids = filtered_sorted_ids
+  end
+end
