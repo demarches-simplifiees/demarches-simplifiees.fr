ba331ccc84f4f2374658cc05d3f13f985af1a716
Lisa Durand <lisa.c.durand@gmail.com>
2023-01-30 17:03:30 +0100

    dont'use DSFR component - add a line inside the table instead

--- app/components/dossiers/batch_select_more_component.rb
+++ app/components/dossiers/batch_select_more_component.rb
@@ -1,4 +1,4 @@
-class Dossiers::BatchNoticeSelectAllComponent < ApplicationComponent
+class Dossiers::BatchSelectMoreComponent < ApplicationComponent
   def initialize(dossiers_count:, filtered_sorted_ids:)
     @dossiers_count = dossiers_count
     @filtered_sorted_ids = filtered_sorted_ids
