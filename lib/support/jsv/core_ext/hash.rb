ed4af14f72bc298771ce6bdae4073cfe126ee4d1
Colin Darie <colin@darie.eu>
2023-02-02 14:50:43 +0100

    feat: jsv support for primitives

---
+++ lib/support/jsv/core_ext/hash.rb
@@ -0,0 +1,11 @@
+class Hash
+  def to_jsv
+    js_array = filter_map do |key, value|
+      next if value.nil? # skip nil values
+
+      "#{key.to_jsv}:#{value.to_jsv}"
+    end
+
+    "{" + js_array.join(",") + "}"
+  end
+end
---
+++ lib/support/jsv/core_ext/string.rb
@@ -0,0 +1,12 @@
+class String
+  JSV_REGEX_SPECIAL_CHARS = /[\[\]\{\}"\,]/.freeze
+
+  def to_jsv
+    double_quoted = self.gsub('"', '""')
+    if match?(JSV_REGEX_SPECIAL_CHARS)
+      "\"#{double_quoted}\""
+    else
+      double_quoted
+    end
+  end
+end
