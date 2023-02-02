41c196f2ec47949da0e4729c54c4126cdb19bf25
Colin Darie <colin@darie.eu>
2023-02-02 14:50:26 +0100

    feat(dolist): send email from API
    Co-authored-by: mfo <mfo@users.noreply.github.com>

--- app/lib/dolist/api.rb
+++ app/lib/dolist/api.rb
@@ -3,6 +3,10 @@ class Dolist::API
   EMAIL_LOGS_URL = "https://apiv9.dolist.net/v1/statistics/email/sendings/transactional/search?AccountID=%{account_id}"
   EMAIL_KEY = 7
   DOLIST_WEB_DASHBOARD = "https://campaign.dolist.net/#/%{account_id}/contacts/%{contact_id}/sendings"
+  EMAIL_MESSAGES_ADRESSES_REPLIES = "https://apiv9.dolist.net/v1/email/messages/addresses/replies?AccountID=%{account_id}"
+  EMAIL_MESSAGES_ADRESSES_PACKSENDERS = "https://apiv9.dolist.net/v1/email/messages/addresses/packsenders?AccountID=%{account_id}"
+  EMAIL_SENDING_TRANSACTIONAL = "https://apiv9.dolist.net/v1/email/sendings/transactional?AccountID=%{account_id}"
+  EMAIL_SENDING_TRANSACTIONAL_SEARCH = "https://apiv9.dolist.net/v1/email/sendings/transactional/search?AccountID=%{account_id}"
 
   class_attribute :limit_remaining, :limit_reset_at
 
@@ -29,6 +33,40 @@ class Dolist::API
     client_key.present?
   end
 
+  def send_email(mail)
+    url = format(EMAIL_SENDING_TRANSACTIONAL, account_id: account_id)
+
+    body = {
+      "TransactionalSending": {
+        "Type": "TransactionalService",
+        "Contact": {
+          "FieldList": [
+            {
+              "ID": EMAIL_KEY,
+              "Value": mail.to.first
+            }
+          ]
+        },
+        "Message": {
+          "Name": mail['X-Dolist-Message-Name'].value,
+          "Subject": mail.subject,
+          "SenderID": sender_id,
+          "ForceHttp": true,
+          "Format": "html",
+          "DisableOpenTracking": true,
+          "IsTrackingValidated": true
+        },
+        "MessageContent": {
+          "SourceCode": mail_source_code(mail),
+          "EncodingType": "UTF8",
+          "EnableTrackingDetection": false
+        }
+      }
+    }
+
+    post(url, body.to_json)
+  end
+
   def sent_mails(email_address)
     contact_id = fetch_contact_id(email_address)
     if contact_id.nil?
@@ -44,7 +82,37 @@ class Dolist::API
     []
   end
 
+  def senders
+    url = format(EMAIL_MESSAGES_ADRESSES_PACKSENDERS, account_id: account_id)
+    get(url)
+  end
+
+  def replies
+    url = format(EMAIL_MESSAGES_ADRESSES_REPLIES, account_id: account_id)
+    get(url)
+  end
+
   private
+
+  def sender_id
+    senders.dig("ItemList", 0, "Sender", "ID")
+  end
+
+  def get(url)
+    response = Typhoeus.get(url, headers:).tap do
+      self.class.save_rate_limit_headers(_1.headers)
+    end
+
+    JSON.parse(response.response_body)
+  end
+
+  def post(url, body)
+    response = Typhoeus.post(url, body:, headers:).tap do
+      self.class.save_rate_limit_headers(_1.headers)
+    end
+
+    JSON.parse(response.response_body)
+  end
 
   def headers
     {
@@ -82,14 +150,6 @@ class Dolist::API
     post(url, body)["ItemList"]
   end
 
-  def post(url, body)
-    response = Typhoeus.post(url, body:, headers:).tap do
-      self.class.save_rate_limit_headers(_1.headers)
-    end
-
-    JSON.parse(response.response_body)
-  end
-
   def to_sent_mail(email_address, contact_id, dolist_message)
     SentMail.new(
       from: ENV['DOLIST_NO_REPLY_EMAIL'],
@@ -110,6 +170,14 @@ class Dolist::API
       "sent (delivered ?)"
     in [status, _]
       status
+    end
+  end
+
+  def mail_source_code(mail)
+    if mail.html_part.nil? && mail.text_part.nil?
+      mail.decoded
+    else
+      mail.html_part.body.decoded
     end
   end
 end
