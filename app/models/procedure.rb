984ef67827aef7560ff78eac2877f96b3216f637
Kara Diaby <kdiaby.pro@gmail.com>
2023-02-02 07:04:30 +0100

    Prend en compte uniquement les procédures publiées dans les propositions de tags

--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -825,7 +825,7 @@ class Procedure < ApplicationRecord
 
   def self.tags
     unnest = Arel::Nodes::NamedFunction.new('UNNEST', [self.arel_table[:tags]])
-    query = self.select(unnest.as('tags')).publiees_ou_closes.distinct.order('tags')
+    query = self.select(unnest.as('tags')).publiees.distinct.order('tags')
     self.connection.query(query.to_sql).flatten
   end
 
