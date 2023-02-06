6cbc514d562d09f55b5bff7d9d93e597dde61d3e
Eric Leroy-Terquem <eric.leroyterquem@beta.gouv.fr>
2023-02-06 16:11:08 +0100

    feat(groupe instructeurs): remove mailing when add instructeurs

--- app/mailers/groupe_instructeur_mailer.rb
+++ app/mailers/groupe_instructeur_mailer.rb
@@ -1,17 +1,6 @@
 class GroupeInstructeurMailer < ApplicationMailer
   layout 'mailers/layout'
 
-  def add_instructeurs(group, new_instructeurs, current_instructeur_email)
-    @new_instructeur_emails = new_instructeurs.map(&:email)
-    @group = group
-    @current_instructeur_email = current_instructeur_email
-
-    subject = "Ajout d’un instructeur dans le groupe \"#{group.label}\""
-
-    emails = @group.instructeurs.map(&:email)
-    mail(bcc: emails, subject: subject)
-  end
-
   def remove_instructeurs(group, removed_instructeurs, current_instructeur_email)
     @removed_instructeur_emails = removed_instructeurs.map(&:email)
     @group = group
