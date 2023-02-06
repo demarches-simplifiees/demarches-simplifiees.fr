db76daed4a9981fe93114188a2f3d6b8c2b30c53
Eric Leroy-Terquem <eric.leroyterquem@beta.gouv.fr>
2023-02-06 17:46:29 +0100

    clean(groupe instructeurs): remove unused method

--- app/mailers/groupe_instructeur_mailer.rb
+++ app/mailers/groupe_instructeur_mailer.rb
@@ -11,15 +11,4 @@ class GroupeInstructeurMailer < ApplicationMailer
     emails = @group.instructeurs.map(&:email)
     mail(bcc: emails, subject: subject)
   end
-
-  def remove_instructeur(group, instructeur, current_instructeur_email)
-    @email = instructeur.email
-    @group = group
-    @current_instructeur_email = current_instructeur_email
-
-    subject = "Suppression d’un instructeur dans le groupe \"#{group.label}\""
-
-    emails = @group.instructeurs.map(&:email)
-    mail(bcc: emails, subject: subject)
-  end
 end
