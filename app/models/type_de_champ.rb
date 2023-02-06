bb569d9789c26de8f6ddfc979cf6e3d8f8e350ad
sebastiencarceles <sebastien.carceles@gmail.com>
2023-02-06 09:33:27 +0100

    let epci champ be prefillable

--- app/models/type_de_champ.rb
+++ app/models/type_de_champ.rb
@@ -268,7 +268,8 @@ class TypeDeChamp < ApplicationRecord
       TypeDeChamp.type_champs.fetch(:checkbox),
       TypeDeChamp.type_champs.fetch(:drop_down_list),
       TypeDeChamp.type_champs.fetch(:regions),
-      TypeDeChamp.type_champs.fetch(:departements)
+      TypeDeChamp.type_champs.fetch(:departements),
+      TypeDeChamp.type_champs.fetch(:epci)
     ])
   end
 
