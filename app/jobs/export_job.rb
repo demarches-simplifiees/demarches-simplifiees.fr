57982ed236b43a58f1db280cc941f785b7f72eb1
Colin Darie <colin@darie.eu>
2023-02-08 18:00:43 +0100

    fix(export/job): race condition when export was already generated

--- app/jobs/export_job.rb
+++ app/jobs/export_job.rb
@@ -4,6 +4,8 @@ class ExportJob < ApplicationJob
   discard_on ActiveRecord::RecordNotFound
 
   def perform(export)
+    return if export.generated?
+
     export.compute_with_safe_stale_for_purge do
       export.compute
     end
