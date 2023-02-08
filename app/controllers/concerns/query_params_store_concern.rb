e212055ef8929eeaeaa8c2dd319d0840552ef8f8
sebastiencarceles <sebastien.carceles@gmail.com>
2023-02-08 14:12:46 +0100

    tell when query params have been stored

--- app/controllers/concerns/query_params_store_concern.rb
+++ app/controllers/concerns/query_params_store_concern.rb
@@ -1,17 +1,25 @@
 module QueryParamsStoreConcern
   extend ActiveSupport::Concern
 
+  included do
+    helper_method :stored_query_params?
+  end
+
   def store_query_params
     # Don't override already stored params, because we could do goings and comings with authentication, and
     # lost previously stored params
-    return if session[:stored_params].present? || request.query_parameters.empty?
+    return if stored_query_params? || request.query_parameters.empty?
 
     session[:stored_params] = request.query_parameters.to_json
   end
 
   def retrieve_and_delete_stored_query_params
-    return {} if session[:stored_params].blank?
+    return {} unless stored_query_params?
 
     JSON.parse(session.delete(:stored_params))
+  end
+
+  def stored_query_params?
+    session[:stored_params].present?
   end
 end
