ea126612a1aa094734fbb0b9f4ea054735eb4eee
Damien Le Thiec <damien.lethiec@gmail.com>
2023-02-12 13:02:30 +0100

    Refacto to_assignable_attributes for prefill_repetition

--- spec/models/types_de_champ/prefill_repetition_type_de_champ_spec.rb
+++ spec/models/types_de_champ/prefill_repetition_type_de_champ_spec.rb
@@ -3,6 +3,7 @@
 RSpec.describe TypesDeChamp::PrefillRepetitionTypeDeChamp, type: :model, vcr: { cassette_name: 'api_geo_regions' } do
   let(:procedure) { build(:procedure) }
   let(:type_de_champ) { build(:type_de_champ_repetition, :with_types_de_champ, :with_region_types_de_champ, procedure: procedure) }
+  let(:champ) { create(:champ_repetition, type_de_champ: type_de_champ) }
   let(:prefillable_subchamps) { TypesDeChamp::PrefillRepetitionTypeDeChamp.new(type_de_champ).send(:prefillable_subchamps) }
   let(:region_repetition) { prefillable_subchamps.third }
   let(:memory_store) { ActiveSupport::Cache.lookup_store(:memory_store) }
@@ -34,5 +35,37 @@ RSpec.describe TypesDeChamp::PrefillRepetitionTypeDeChamp, type: :model, vcr: { 
     let(:expected_value) { ["{\"sub type de champ\":\"Texte court\", \"sub type de champ2\":\"42\", \"region sub_champ\":\"53\"}", "{\"sub type de champ\":\"Texte court\", \"sub type de champ2\":\"42\", \"region sub_champ\":\"53\"}"] }
 
     it { expect(example_value).to eq(expected_value) }
+  end
+
+  describe '#to_assignable_attributes' do
+    subject(:to_assignable_attributes) { described_class.build(type_de_champ).to_assignable_attributes(champ, value) }
+    let(:type_de_champ_child) { champ.rows.first.first.type_de_champ }
+
+    context 'when the value is nil' do
+      let(:value) { nil }
+      it { is_expected.to match([]) }
+    end
+
+    context 'when the value is empty' do
+      let(:value) { '' }
+      it { is_expected.to match([]) }
+    end
+
+    context 'when the value is a string' do
+      let(:value) { 'hello' }
+      it { is_expected.to match([]) }
+    end
+
+    context 'when the value is an array with wrong keys' do
+      let(:value) { ["{\"blabla\":\"value\"}", "{\"blabla\":\"value2\"}"] }
+
+      it { is_expected.to match([]) }
+    end
+
+    context 'when the value is an array with right keys' do
+      let(:value) { ["{\"#{type_de_champ_child.libelle}\":\"value\"}", "{\"#{type_de_champ_child.libelle}\":\"value2\"}"] }
+
+      it { is_expected.to match([[{ id: type_de_champ_child.champ.first.id, value: "value" }], [{ id: type_de_champ_child.champ.second.id, value: "value2" }]]) }
+    end
   end
 end
