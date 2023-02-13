64f79bb6d854720845f7ffb89d88adbb2f02d119
Paul Chavard <pro@paul.chavard.net>
2023-02-13 14:22:21 +0100

    feat(api_token): add allowed_procedure_ids to api tokens

---
+++ db/migrate/20230213123229_add_read_only_and_procedure_ids_to_api_tokens.rb
@@ -0,0 +1,6 @@
+class AddReadOnlyAndProcedureIdsToAPITokens < ActiveRecord::Migration[6.1]
+  def change
+    add_column :api_tokens, :write_access, :boolean, default: true, null: false
+    add_column :api_tokens, :allowed_procedure_ids, :bigint, array: true, null: true
+  end
+end
