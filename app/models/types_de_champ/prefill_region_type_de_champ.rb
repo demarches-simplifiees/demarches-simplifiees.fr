0159a1096930aa69edff7c83646e5d02cda87cfa
sebastiencarceles <sebastien.carceles@gmail.com>
2023-02-14 11:41:35 +0100

    :bug: make possible_values_list method public

--- app/models/types_de_champ/prefill_region_type_de_champ.rb
+++ app/models/types_de_champ/prefill_region_type_de_champ.rb
@@ -1,9 +1,9 @@
 class TypesDeChamp::PrefillRegionTypeDeChamp < TypesDeChamp::PrefillTypeDeChamp
-  private
-
   def possible_values_list
     regions.map { |region| "#{region[:code]} (#{region[:name]})" }
   end
+
+  private
 
   def regions
     @regions ||= APIGeoService.regions.sort_by { |region| region[:code] }
