c11c8fe687127b4655926c71960a0d68e8bfb599
Martin <martin@sharypic.com>
2023-02-16 05:17:22 +0100

    perf(db): supprime un index qui etait utilisé uniquement pour une migration de donnee [normalize_regions/normalize_departements]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_07_105539) do
+ActiveRecord::Schema.define(version: 2023_02_16_041517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -233,7 +233,6 @@ ActiveRecord::Schema.define(version: 2023_02_07_105539) do
     t.jsonb "value_json"
     t.index ["dossier_id"], name: "index_champs_on_dossier_id"
     t.index ["etablissement_id"], name: "index_champs_on_etablissement_id"
-    t.index ["external_id"], name: "index_champs_on_external_id"
     t.index ["parent_id"], name: "index_champs_on_parent_id"
     t.index ["private"], name: "index_champs_on_private"
     t.index ["row_id"], name: "index_champs_on_row_id"
