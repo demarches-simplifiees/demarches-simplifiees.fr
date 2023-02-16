b29ff41c1b728e373883cf223583f82cde52ceb0
Martin <martin@sharypic.com>
2023-02-16 15:43:35 +0100

    correctif(db): force active_storage_attachments.blob fk on active_storage_blobs

---
+++ db/migrate/20230216141558_validate_foreign_key_between_attachments_and_blobs.rb
@@ -0,0 +1,5 @@
+class ValidateForeignKeyBetweenAttachmentsAndBlobs < ActiveRecord::Migration[6.1]
+  def up
+    validate_foreign_key :active_storage_attachments, :active_storage_blobs, column: :blob_id
+  end
+end
