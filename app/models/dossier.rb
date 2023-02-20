d7880f7e58ac48936fabc21e8ebd99aec2258030
Colin Darie <colin@darie.eu>
2023-02-20 18:48:20 +0100

    feat(dossier/pdf): header sections numbering

--- app/models/dossier.rb
+++ app/models/dossier.rb
@@ -1295,6 +1295,18 @@ class Dossier < ApplicationRecord
     sections_for(champ)&.none?(&:libelle_with_section_index?)
   end
 
+  def index_for_section_header(champ)
+    champs = champ.private? ? champs_private : champs_public
+
+    index = 1
+    champs.each do |c|
+      return index if c.stable_id == champ.stable_id
+      next unless c.visible?
+
+      index += 1 if c.type_de_champ.header_section?
+    end
+  end
+
   private
 
   def create_missing_traitemets
