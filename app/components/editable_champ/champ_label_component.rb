a584279020bcba4d596623d7fcee30a2a49868c6
Martin <martin@sharypic.com>
2023-02-22 10:54:22 +0100

    chore(deadcode): drop deadcode

--- app/components/editable_champ/champ_label_component.rb
+++ app/components/editable_champ/champ_label_component.rb
@@ -1,6 +1,4 @@
 class EditableChamp::ChampLabelComponent < ApplicationComponent
-  include StringToHtmlHelper
-
   def initialize(form:, champ:, seen_at: nil)
     @form, @champ, @seen_at = form, champ, seen_at
   end
