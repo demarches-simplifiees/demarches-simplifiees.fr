a934746176fca791971df48166ac6c2ab6d529a3
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2023-02-23 10:34:15 -1000

    better refactoring for numero_dn

--- app/javascript/controllers/numero_dn_controller.ts
+++ app/javascript/controllers/numero_dn_controller.ts
@@ -3,48 +3,37 @@ import { httpRequest } from '@utils';
 import { ApplicationController } from './application_controller';
 
 export class NumeroDnController extends ApplicationController {
-  static values = {
-    url: String,
-    type: String
-  };
+  static values = { url: String };
 
   declare readonly urlValue: string;
-  declare readonly typeValue: string;
+
+  static targets = ['dn', 'ddn', 'info'];
+
+  declare readonly dnTarget: HTMLInputElement;
+  declare readonly ddnTarget: HTMLInputElement;
+  declare readonly infoTarget: HTMLInputElement;
 
   connect(): void {
-    this.on('input', () => this.debounce(this.load, 200));
+    this.onTarget(this.dnTarget, 'input', () => this.debounce(this.load, 200));
+    this.onTarget(this.ddnTarget, 'input', () => this.debounce(this.load, 200));
   }
-
   private load(): void {
-    const target = this.element as HTMLInputElement;
-    if (!target?.validity?.patternMismatch) {
-      const side_type = this.typeValue == 'dn' ? 'ddn' : 'dn';
-      const side_element = this.sideElement(target, side_type);
-      if (side_element.checkValidity()) {
+    if (!this.dnTarget?.validity?.patternMismatch) {
+      if (this.ddnTarget.checkValidity()) {
         const url = new URL(this.urlValue, document.baseURI);
-        url.searchParams.append(this.typeValue, target.value);
-        url.searchParams.append(side_type, side_element.value);
+        url.searchParams.append('dn', this.dnTarget.value);
+        url.searchParams.append('ddn', this.ddnTarget.value);
         httpRequest(url.toString()).turbo();
       } else {
-        this.clearInfo(target);
+        this.clearInfo();
       }
     } else {
-      this.clearInfo(target);
+      this.clearInfo();
     }
   }
 
-  private clearInfo(target: HTMLInputElement) {
-    const info = target?.parentElement?.querySelector('.numero-dn-info');
+  private clearInfo() {
+    const info = this.infoTarget;
     if (info && info.innerHTML.length > 0) info.innerHTML = '';
-  }
-
-  selector(type: string): string {
-    return 'input[data-numero-dn-type-value="' + type + '"]';
-  }
-
-  sideElement(input: HTMLInputElement, side_type: string): HTMLInputElement {
-    return input.parentElement?.querySelector(
-      this.selector(side_type)
-    ) as HTMLInputElement;
   }
 }
