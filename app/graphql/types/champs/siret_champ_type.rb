f31241e0677fc1a854ad3c37b44602f98365eb56
Paul Chavard <pro@paul.chavard.net>
2023-02-28 18:09:13 +0100

    fix(graphql): fix etablissement as_degraded_mode?

--- app/graphql/types/champs/siret_champ_type.rb
+++ app/graphql/types/champs/siret_champ_type.rb
@@ -6,7 +6,10 @@ module Types::Champs
 
     def etablissement
       if object.etablissement_id.present?
-        Loaders::Record.for(Etablissement).load(object.etablissement_id)
+        Loaders::Record.for(Etablissement).load(object.etablissement_id).then do |etablissement|
+          return nil if etablissement.as_degraded_mode?
+          etablissement
+        end
       end
     end
   end
