6851a9ee52deab361f9817f1065e8eed483b3892
Christophe Robillard <christophe.robillard@beta.gouv.fr>
2023-03-01 11:06:58 +0100

    falsify opendata flag for some procedures
    `procedures:falsify_opendata_procedures` rake task allows to falsify
    opndata flag for procedures listed in csv file given in input (with only
    one column : id column)

---
+++ app/services/falsify_opendata_service.rb
@@ -0,0 +1,15 @@
+class FalsifyOpendataService
+  def self.call(lines)
+    errors = []
+    lines.each do |line|
+      id = line["id"]
+      procedure = Procedure.find_by(id: id)
+      if procedure
+        procedure.update(opendata: false)
+      else
+        errors << "Procedure #{id} introuvable"
+      end
+    end
+    errors
+  end
+end
