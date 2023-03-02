be02dd965f763020fa04920fdb503c13635e1892
Damien Le Thiec <damien.lethiec@gmail.com>
2023-03-02 01:14:34 +0100

    Add link and query examples with identity prefill

--- app/models/prefill_description.rb
+++ app/models/prefill_description.rb
@@ -4,22 +4,25 @@ class PrefillDescription < SimpleDelegator
   MAX_PREFILL_LINK_LENGTH = 2000
 
   attr_reader :selected_type_de_champ_ids
+  attr_reader :identity_items_selected
 
   def initialize(procedure)
     super(procedure)
     @selected_type_de_champ_ids = []
+    @identity_items_selected = []
   end
 
   def update(attributes)
     @selected_type_de_champ_ids = attributes[:selected_type_de_champ_ids]&.split(' ') || []
+    @identity_items_selected = attributes[:identity_items_selected]&.split(' ') || []
   end
 
   def types_de_champ
     TypesDeChamp::PrefillTypeDeChamp.wrap(active_revision.types_de_champ_public.fillable.partition(&:prefillable?).flatten, active_revision)
   end
 
-  def include?(type_de_champ_id)
-    selected_type_de_champ_ids.include?(type_de_champ_id.to_s)
+  def include?(entity)
+    selected_type_de_champ_ids.include?(entity) || identity_items_selected.include?(entity)
   end
 
   def link_too_long?
@@ -27,7 +30,7 @@ class PrefillDescription < SimpleDelegator
   end
 
   def prefill_link
-    @prefill_link ||= CGI.unescape(commencer_url({ path: path }.merge(prefilled_champs_as_params)))
+    @prefill_link ||= CGI.unescape(commencer_url({ path: path }.merge(prefilled_champs_as_params).merge(prefilled_identity_as_params)))
   end
 
   def prefill_query
@@ -35,7 +38,7 @@ class PrefillDescription < SimpleDelegator
       <<~TEXT
         curl --request POST '#{api_public_v1_dossiers_url(self)}' \\
              --header 'Content-Type: application/json' \\
-             --data '#{prefilled_champs_as_params.to_json}'
+             --data '#{prefilled_identity_as_params.merge(prefilled_champs_as_params).to_json}'
       TEXT
   end
 
@@ -51,5 +54,9 @@ class PrefillDescription < SimpleDelegator
 
   def prefilled_champs_as_params
     prefilled_champs.map { |type_de_champ| ["champ_#{type_de_champ.to_typed_id_for_query}", type_de_champ.example_value] }.to_h
+  end
+
+  def prefilled_identity_as_params
+    identity_items_selected.map { |item| ["identite_#{item}", I18n.t("views.prefill_descriptions.edit.examples.#{item}")] }.to_h
   end
 end
