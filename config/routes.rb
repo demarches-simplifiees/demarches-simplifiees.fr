efdfea26853e2aeaff9d8b7f0c27468dbb384ba8
Julie Salha <julie.salha@gmail.com>
2023-03-15 14:31:23 +0100

    fix PR URL and DSFR styles

--- config/routes.rb
+++ config/routes.rb
@@ -195,7 +195,7 @@ Rails.application.routes.draw do
 
   get "contact-admin", to: "support#admin"
 
-  get "mentions_legales", to: "legal_notice#index"
+  get "mentions-legales", to: "legal_notice#index"
 
   post "webhooks/sendinblue", to: "webhook#sendinblue"
   post "webhooks/helpscout", to: "webhook#helpscout"
