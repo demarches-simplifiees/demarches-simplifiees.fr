061ba584d4ff95c061bcca422564f751ae523e5f
Paul Chavard <pro@paul.chavard.net>
2023-03-21 14:40:33 +0100

    chore(champ): remove unused react code

--- app/javascript/components/shared/queryClient.ts
+++ app/javascript/components/shared/queryClient.ts
@@ -1,22 +1,11 @@
 import { QueryClient, QueryFunction } from 'react-query';
-import { httpRequest, isNumeric, getConfig } from '@utils';
-import { matchSorter } from 'match-sorter';
+import { httpRequest, getConfig } from '@utils';
 
 const API_EDUCATION_QUERY_LIMIT = 5;
-const API_GEO_QUERY_LIMIT = 5;
 const API_ADRESSE_QUERY_LIMIT = 5;
 
-// When searching for short strings like "mer", le exact match shows up quite far in
-// the ordering (~50).
-//
-// That's why we deliberately fetch a lot of results, and then let the local matching
-// (match-sorter) do the work.
-//
-// NB: 60 is arbitrary, we may add more if needed.
-const API_GEO_COMMUNES_QUERY_LIMIT = 60;
-
 const {
-  autocomplete: { api_geo_url, api_adresse_url, api_education_url }
+  autocomplete: { api_adresse_url, api_education_url }
 } = getConfig();
 
 type QueryKey = readonly [
@@ -25,10 +14,10 @@ type QueryKey = readonly [
   extra: string | undefined
 ];
 
-function buildURL(scope: string, term: string, extra?: string) {
+function buildURL(scope: string, term: string) {
   term = term.replace(/\(|\)/g, '');
   const params = new URLSearchParams();
-  let path = `${api_geo_url}/${scope}`;
+  let path = '';
 
   if (scope == 'adresse') {
     path = `${api_adresse_url}/search`;
@@ -39,40 +28,15 @@ function buildURL(scope: string, term: string, extra?: string) {
     params.set('q', term);
     params.set('rows', `${API_EDUCATION_QUERY_LIMIT}`);
     params.set('dataset', 'fr-en-annuaire-education');
-  } else if (scope == 'communes') {
-    if (extra) {
-      params.set('codeDepartement', extra);
-    }
-    if (isNumeric(term)) {
-      params.set('codePostal', term);
-    } else {
-      params.set('nom', term);
-      params.set('boost', 'population');
-    }
-    params.set('limit', `${API_GEO_COMMUNES_QUERY_LIMIT}`);
-  } else {
-    if (isNumeric(term)) {
-      params.set('code', term.padStart(2, '0'));
-    } else {
-      params.set('nom', term);
-    }
-    if (scope == 'departements') {
-      params.set('zone', 'metro,drom,com');
-    }
-    params.set('limit', `${API_GEO_QUERY_LIMIT}`);
   }
 
   return `${path}?${params}`;
 }
 
 const defaultQueryFn: QueryFunction<unknown, QueryKey> = async ({
-  queryKey: [scope, term, extra],
+  queryKey: [scope, term],
   signal
 }) => {
-  if (scope == 'pays') {
-    return matchSorter(await getPays(signal), term, { keys: ['label'] });
-  }
-
   // BAN will error with queries less then 3 chars long
   if (scope == 'adresse' && term.length < 3) {
     return {
@@ -83,22 +47,9 @@ const defaultQueryFn: QueryFunction<unknown, QueryKey> = async ({
     };
   }
 
-  const url = buildURL(scope, term, extra);
+  const url = buildURL(scope, term);
   return httpRequest(url, { csrf: false, signal }).json();
 };
-
-let paysCache: { label: string }[];
-async function getPays(signal?: AbortSignal): Promise<{ label: string }[]> {
-  if (!paysCache) {
-    const data = await httpRequest('/api/pays', { signal }).json<
-      typeof paysCache
-    >();
-    if (data) {
-      paysCache = data;
-    }
-  }
-  return paysCache;
-}
 
 export const queryClient = new QueryClient({
   defaultOptions: {
