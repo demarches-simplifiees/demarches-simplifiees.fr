b93e3776c4f99d3a8802d123b27dbbd19ec0ecff
simon lehericey <mail@simon.lehericey.net>
2023-03-24 10:27:32 +0100

    feat(routing): add controller

---
+++ app/controllers/administrateurs/routing_controller.rb
@@ -0,0 +1,32 @@
+module Administrateurs
+  class RoutingController < AdministrateurController
+    include Logic
+
+    before_action :retrieve_procedure
+
+    def update
+      left = champ_value(targeted_champ)
+      right = constant(value)
+
+      @procedure.groupe_instructeurs.find(groupe_instructeur_id).update!(routing_rule: ds_eq(left, right))
+    end
+
+    private
+
+    def targeted_champ
+      routing_params[:targeted_champ].to_i
+    end
+
+    def value
+      routing_params[:value]
+    end
+
+    def groupe_instructeur_id
+      routing_params[:groupe_instructeur_id]
+    end
+
+    def routing_params
+      params.permit(:targeted_champ, :value, :groupe_instructeur_id)
+    end
+  end
+end
