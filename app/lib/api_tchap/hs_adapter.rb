53dd2955e4a34fca7749d0428f3afe7550b0efdd
Christophe Robillard <christophe.robillard@beta.gouv.fr>
2023-03-31 17:19:02 +0200

    add tchap hs adapter

---
+++ app/lib/api_tchap/hs_adapter.rb
@@ -0,0 +1,15 @@
+class APITchap::HsAdapter
+  def initialize(email)
+    @email = email
+  end
+
+  def to_hs
+    data_source[:hs]
+  end
+
+  private
+
+  def data_source
+    @data_source ||= JSON.parse(APITchap::API.get_hs(@email), symbolize_names: true)
+  end
+end
