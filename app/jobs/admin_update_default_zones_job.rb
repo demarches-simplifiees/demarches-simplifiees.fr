ca922005f98050c3171ff4999d54be0b91041a2b
Christophe Robillard <christophe.robillard@beta.gouv.fr>
2023-03-31 15:25:17 +0200

    add admin update default zones job

---
+++ app/jobs/admin_update_default_zones_job.rb
@@ -0,0 +1,6 @@
+class AdminUpdateDefaultZonesJob < ApplicationJob
+  def perform(admin)
+    tchap_hs = APITchap::HsAdapter.new(admin.email).to_hs
+    admin.default_zones << Zone.default_for(tchap_hs)
+  end
+end
