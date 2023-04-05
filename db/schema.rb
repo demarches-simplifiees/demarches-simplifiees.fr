c68be3471f8e2d78fcf85baad6ecef5950dc760c
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2023-04-04 16:00:37 -1000

    fix(wcag): footer link mixes title/aria-label

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_30_113745) do
+ActiveRecord::Schema.define(version: 2023_03_27_230248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
