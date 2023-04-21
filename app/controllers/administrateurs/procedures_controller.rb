cbc2de69466903dc800a91ddcaad366f3239d823
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2023-04-20 15:01:37 -1000

    fix(procedure): typo on sql request

--- app/controllers/administrateurs/procedures_controller.rb
+++ app/controllers/administrateurs/procedures_controller.rb
@@ -372,7 +372,7 @@ module Administrateurs
     private
 
     def filter_procedures(filter)
-      procedures_result = Procedure.left_joins(:procedures_zones).disctinct.publiees_ou_closes
+      procedures_result = Procedure.select(:id).left_joins(:procedures_zones).distinct.publiees_ou_closes
       procedures_result = procedures_result.where(procedures_zones: { zone_id: filter.zone_ids }) if filter.zone_ids.present?
       procedures_result = procedures_result.where(aasm_state: filter.statuses) if filter.statuses.present?
       procedures_result = procedures_result.where('published_at >= ?', filter.from_publication_date) if filter.from_publication_date.present?
