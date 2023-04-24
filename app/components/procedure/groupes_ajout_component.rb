11a166aa51f38daf1470762eb813be59d67b9a83
simon lehericey <mail@simon.lehericey.net>
2023-04-24 15:48:52 +0200

    add group_ajout_component

---
+++ app/components/procedure/groupes_ajout_component.rb
@@ -0,0 +1,6 @@
+class Procedure::GroupesAjoutComponent < ApplicationComponent
+  def initialize(procedure:, groupe_instructeurs:)
+    @procedure = procedure
+    @groupe_instructeurs = groupe_instructeurs
+  end
+end
