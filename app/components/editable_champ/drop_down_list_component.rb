a39a93e4a2f650d1969ceb47b378e6e03ccff9e4
Martin <martin@sharypic.com>
2023-04-25 15:56:56 +0200

    correctif(ui): les balises <select> avec un balise <option> à rallonge prenent la largueur du contenu de l'option

--- app/components/editable_champ/drop_down_list_component.rb
+++ app/components/editable_champ/drop_down_list_component.rb
@@ -1,2 +1,10 @@
 class EditableChamp::DropDownListComponent < EditableChamp::EditableChampBaseComponent
+  def select_class_names
+    class_names('width-100': contains_long_option?)
+  end
+
+  def contains_long_option?
+    max_length = 100
+    @champ.options.map(&:size).any? { _1 > max_length }
+  end
 end
