5cc2712873c878ecb10b0e15d8d0c0de0bcf00c8
Colin Darie <colin@darie.eu>
2023-04-27 09:50:12 +0200

    refactor(dolist): drop smtp deliver method

--- config/initializers/dolist.rb
+++ config/initializers/dolist.rb
@@ -1,15 +1,3 @@
 ActiveSupport.on_load(:action_mailer) do
-  require "dolist/smtp"
-
-  ActionMailer::Base.add_delivery_method :dolist_smtp, Dolist::SMTP
-  ActionMailer::Base.dolist_smtp_settings = {
-    user_name: Rails.application.secrets.dolist[:username],
-    password: Rails.application.secrets.dolist[:password],
-    address: 'smtp.dolist.net',
-    port: 587,
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
-
   ActionMailer::Base.add_delivery_method :dolist_api, Dolist::APISender
 end
