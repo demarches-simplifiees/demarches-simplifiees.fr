ce1429b850195eb38918435b65587003c7461ff8
Eric Leroy-Terquem <eric.leroyterquem@beta.gouv.fr>
2023-05-17 11:23:43 +0200

    migration : add forced_group_instructeur_to_dossier

---
+++ db/migrate/20230517085816_add_forced_groupe_instructeur_to_dossier.rb
@@ -0,0 +1,5 @@
+class AddForcedGroupeInstructeurToDossier < ActiveRecord::Migration[7.0]
+  def change
+    add_column :dossiers, :forced_groupe_instructeur, :boolean, default: false, null: false
+  end
+end
