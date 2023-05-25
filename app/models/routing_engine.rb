e4641a1819bdaf2423560675f73c4f25d505f98c
Eric Leroy-Terquem <eric.leroyterquem@beta.gouv.fr>
2023-05-25 16:02:47 +0200

    feat(routing): do not run routing engine if dossier has been re routed

--- app/models/routing_engine.rb
+++ app/models/routing_engine.rb
@@ -1,6 +1,7 @@
 module RoutingEngine
   def self.compute(dossier)
     return if !dossier.procedure.feature_enabled?(:routing_rules)
+    return if dossier.forced_groupe_instructeur
 
     matching_groupe = dossier.procedure.groupe_instructeurs.active.find do |gi|
       gi.routing_rule&.compute(dossier.champs)
