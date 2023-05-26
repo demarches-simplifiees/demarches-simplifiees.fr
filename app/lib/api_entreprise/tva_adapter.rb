f3a4aec0bf3bbc54137b91a34885d2ddafc3f3c8
Christophe Robillard <christophe.robillard@beta.gouv.fr>
2023-05-26 10:06:01 +0200

    add reference doc for api

--- app/lib/api_entreprise/tva_adapter.rb
+++ app/lib/api_entreprise/tva_adapter.rb
@@ -1,4 +1,7 @@
 class APIEntreprise::TvaAdapter < APIEntreprise::Adapter
+  # Doc métier : https://entreprise.api.gouv.fr/catalogue/commission_europeenne/numero_tva
+  # Swagger : https://entreprise.api.gouv.fr/developpeurs/openapi#tag/Informations-generales/paths/~1v3~1european_commission~1unites_legales~1%7Bsiren%7D~1numero_tva/get
+
   private
 
   def get_resource
