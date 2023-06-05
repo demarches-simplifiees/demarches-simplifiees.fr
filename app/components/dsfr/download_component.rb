74da12bd1e9fa46a3af224cb7469614f4c0ff16d
Lisa Durand <lisa.c.durand@gmail.com>
2023-06-05 17:37:52 +0200

    fix typo

--- app/components/dsfr/download_component.rb
+++ app/components/dsfr/download_component.rb
@@ -3,15 +3,15 @@ class Dsfr::DownloadComponent < ApplicationComponent
   attr_reader :html_class
   attr_reader :name
   attr_reader :ephemeral_link
-  attr_reader :virus_not_analized
+  attr_reader :virus_not_analyzed
   attr_reader :new_tab
 
-  def initialize(attachment:, name: nil, url: nil, ephemeral_link: false, virus_not_analized: false, new_tab: false)
+  def initialize(attachment:, name: nil, url: nil, ephemeral_link: false, virus_not_analyzed: false, new_tab: false)
     @attachment = attachment
     @name = name || attachment.filename.to_s
     @url = url
     @ephemeral_link = ephemeral_link
-    @virus_not_analized = virus_not_analized
+    @virus_not_analyzed = virus_not_analyzed
     @new_tab = new_tab
   end
 
