1f5b551b24feb3ada4f1fd9893fa5b8a38cd3cc6
Lisa Durand <lisa.c.durand@gmail.com>
2023-06-26 18:27:40 +0200

    change design from table to cards for usager dashboard

--- app/components/dsfr/alert_component.rb
+++ app/components/dsfr/alert_component.rb
@@ -13,20 +13,25 @@ class Dsfr::AlertComponent < ApplicationComponent
   end
 
   def alert_class(state)
-    ["fr-alert fr-alert--#{state}", extra_class_names].compact.flatten
+    if size == 'small'
+      ["fr-alert fr-alert--sm fr-alert--#{state}", extra_class_names].compact.flatten
+    else
+      ["fr-alert fr-alert--#{state}", extra_class_names].compact.flatten
+    end
   end
 
   private
 
-  def initialize(state:, title:, extra_class_names: nil, heading_level: 'h3')
+  def initialize(state:, title: '', size: '', extra_class_names: nil, heading_level: 'h3')
     @state = state
     @title = title
+    @size = size
     @block = block
     @extra_class_names = extra_class_names
     @heading_level = heading_level
   end
 
-  attr_reader :state, :title, :block, :extra_class_names, :heading_level
+  attr_reader :state, :title, :size, :block, :extra_class_names, :heading_level
 
   private
 end
