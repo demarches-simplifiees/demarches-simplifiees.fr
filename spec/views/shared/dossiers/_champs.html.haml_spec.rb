2476fa7eb81c3309b4e9621d5c388b9ee19a06d6
Colin Darie <colin@darie.eu>
2023-07-03 23:58:52 +0200

    test(champs): remove obsolete test about updated champ of routed procedure

--- spec/views/shared/dossiers/_champs.html.haml_spec.rb
+++ spec/views/shared/dossiers/_champs.html.haml_spec.rb
@@ -57,39 +57,6 @@ describe 'shared/dossiers/champs', type: :view do
     end
   end
 
-  context "with a routed procedure" do
-    let(:procedure) do
-      create(:procedure, :routee)
-    end
-    let(:dossier) { create(:dossier, :en_construction, procedure: procedure) }
-    let(:champs) { [] }
-
-    context "with seen_at" do
-      let(:dossier) { create(:dossier) }
-      let(:nouveau_groupe_instructeur) { create(:groupe_instructeur, procedure: dossier.procedure) }
-      let(:champ1) { create(:champ_checkbox, dossier: dossier, value: 'true') }
-      let(:champs) { [champ1] }
-
-      context "with a demande_seen_at after groupe_instructeur_updated_at" do
-        let(:demande_seen_at) { dossier.groupe_instructeur_updated_at + 1.hour }
-
-        it "expect to not highlight new group instructeur label" do
-          dossier.assign_to_groupe_instructeur(nouveau_groupe_instructeur)
-          expect(subject).not_to have_css(".highlighted")
-        end
-      end
-
-      context "with a demande_seen_at before groupe_instructeur_updated_at" do
-        let(:demande_seen_at) { dossier.groupe_instructeur_updated_at - 1.hour }
-
-        it "expect to not highlight new group instructeur label" do
-          dossier.assign_to_groupe_instructeur(nouveau_groupe_instructeur)
-          expect(subject).to have_css(".highlighted")
-        end
-      end
-    end
-  end
-
   context "with a dossier champ, but we are not authorized to acces the dossier" do
     let(:dossier) { create(:dossier) }
     let(:champ) { create(:champ_dossier_link, dossier: dossier, value: dossier.id) }
