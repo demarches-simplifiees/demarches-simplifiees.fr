e06d0d88ad9c988a228f08f5c8944cab1b781be6
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2023-07-04 11:29:36 -1000

    Fix datetime_champ validation with negative time zone

--- app/models/champs/datetime_champ.rb
+++ app/models/champs/datetime_champ.rb
@@ -74,6 +74,6 @@ class Champs::DatetimeChamp < Champ
   end
 
   def valid_iso8601?
-    /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}(:\d{2}\+\d{2}:\d{2})?$/.match?(value)
+    /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}(:\d{2}[\+\-]\d{2}:\d{2})?$/.match?(value)
   end
 end
