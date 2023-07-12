da507ff0ea7d858a3e7f1e243b37acb5d06398c3
simon lehericey <mail@simon.lehericey.net>
2023-07-12 15:48:37 +0200

    conf: add redis

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,7 +25,11 @@ Rails.application.configure do
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
-    config.cache_store = :memory_store
+    if ENV['REDIS_CACHE_URL'].present?
+      config.cache_store = :redis_cache_store, { url: ENV['REDIS_CACHE_URL'] }
+    else
+      config.cache_store = :memory_store
+    end
     config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
