dcf92a149cf4c480f2c408287c8533a39d68fcb8
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2023-07-12 13:28:02 -1000

    string_to_html_helper: factorize variable

--- app/helpers/string_to_html_helper.rb
+++ app/helpers/string_to_html_helper.rb
@@ -10,12 +10,13 @@ module StringToHtmlHelper
   def sanitize_html(html_formatted, allow_a: nil)
     html_formatted&.gsub!(/[\r\n]/, ' ')
     with_links = Anchored::Linker.auto_link(html_formatted, target: '_blank', rel: 'noopener')
+    sanitized_allowed_tags = Rails.configuration.action_view.sanitized_allowed_tags
     tags = if allow_a.nil?
-      Rails.configuration.action_view.sanitized_allowed_tags
+      sanitized_allowed_tags
     elsif allow_a
-      Rails.configuration.action_view.sanitized_allowed_tags + ['a']
+      sanitized_allowed_tags + ['a']
     else
-      Rails.configuration.action_view.sanitized_allowed_tags - ['a']
+      sanitized_allowed_tags - ['a']
     end
     sanitize(with_links, tags:)
   end
