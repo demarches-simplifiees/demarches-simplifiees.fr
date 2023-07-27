41fa67bd616f95494a7f79ef6516afec601a8a9b
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2023-07-27 10:10:45 -1000

    pf allws img where <a anchor is allowed

--- app/helpers/sanitize_with_link_helper.rb
+++ app/helpers/sanitize_with_link_helper.rb
@@ -1,6 +1,6 @@
 module SanitizeWithLinkHelper
   def sanitize_with_link(value)
-    tags = Rails.configuration.action_view.sanitized_allowed_tags + ['a']
+    tags = Rails.configuration.action_view.sanitized_allowed_tags + ['a', 'img']
     sanitize(value, tags:)
   end
 end
