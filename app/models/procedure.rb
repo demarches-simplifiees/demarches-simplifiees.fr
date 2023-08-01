cdbaf987c08303d02eddfed139adb04d80960df8
Eric Leroy-Terquem <eric.leroyterquem@beta.gouv.fr>
2023-08-01 16:21:13 +0200

    db(routing): remove migrated champ routage columns

--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -78,8 +78,7 @@ class Procedure < ApplicationRecord
     :durees_conservation_required,
     :cerfa_flag,
     :test_started_at,
-    :lien_demarche,
-    :migrated_champ_routage
+    :lien_demarche
   ]
 
   default_scope -> { kept }
--- app/models/procedure_revision.rb
+++ app/models/procedure_revision.rb
@@ -13,7 +13,6 @@
 #
 class ProcedureRevision < ApplicationRecord
   self.implicit_order_column = :created_at
-  self.ignored_columns += [:migrated_champ_routage]
   belongs_to :procedure, -> { with_discarded }, inverse_of: :revisions, optional: false
   belongs_to :dossier_submitted_message, inverse_of: :revisions, optional: true, dependent: :destroy
 
