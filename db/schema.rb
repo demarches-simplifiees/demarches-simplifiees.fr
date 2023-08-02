fd24f283debc08c366fca2d3b5a39c45577941a3
Paul Chavard <pro@paul.chavard.net>
2023-08-02 11:43:30 +0200

    chore(schema): update timestamp

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_18_113920) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_01_121131) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
