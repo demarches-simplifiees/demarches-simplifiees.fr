ec3097a6cb913336ed4c660cf6a5479670c774e3
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2023-08-01 16:20:22 -1000

    fix backfill TAHITI number: added traces

--- app/jobs/cron/backfill_siret_degraded_mode_job.rb
+++ app/jobs/cron/backfill_siret_degraded_mode_job.rb
@@ -9,10 +9,11 @@ class Cron::BackfillSiretDegradedModeJob < Cron::CronJob
   def fix_etablissement_with_dossier
     Etablissement.joins(:dossier).where(adresse: nil).find_each do |etablissement|
       begin
+        puts "Backfill siret for #{etablissement.siret} for dossier #{etablissement.dossier.id}"
         procedure_id = etablissement.dossier.procedure.id
-
         APIEntrepriseService.update_etablissement_from_degraded_mode(etablissement, procedure_id)
       rescue => e
+        puts "Exception dans BackfillSiretDegradedMode: #{e.message}\n   #{e.backtrace.join('\n   ')}"
         Sentry.capture_exception(e)
       end
     end
@@ -21,10 +22,12 @@ class Cron::BackfillSiretDegradedModeJob < Cron::CronJob
   def fix_etablissement_with_champs
     Etablissement.joins(:champ).where(adresse: nil).find_each do |etablissement|
       begin
+        puts "Backfill siret for #{etablissement.siret} for champ #{etablissement.champ.libelle} in dossier #{etablissement.champ.dossier.id}"
         procedure_id = etablissement.champ.procedure.id
 
         APIEntrepriseService.update_etablissement_from_degraded_mode(etablissement, procedure_id)
       rescue => e
+        puts "Exception dans BackfillSiretDegradedMode: #{e.message}\n   #{e.backtrace.join('\n   ')}"
         Sentry.capture_exception(e)
       end
     end
