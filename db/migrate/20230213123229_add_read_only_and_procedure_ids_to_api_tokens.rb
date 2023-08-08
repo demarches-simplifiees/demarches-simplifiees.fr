a19b5631b93988f30178b76824da5e903042dec8
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2023-08-07 16:03:53 -1000

    fix migration strong check error

--- db/migrate/20230213123229_add_read_only_and_procedure_ids_to_api_tokens.rb
+++ db/migrate/20230213123229_add_read_only_and_procedure_ids_to_api_tokens.rb
@@ -1,6 +1,7 @@
 class AddReadOnlyAndProcedureIdsToAPITokens < ActiveRecord::Migration[6.1]
   def change
-    add_column :api_tokens, :write_access, :boolean, default: true, null: false
+    add_column :api_tokens, :write_access, :boolean
+    change_column_default :api_tokens, :write_access, true
     add_column :api_tokens, :allowed_procedure_ids, :bigint, array: true, null: true
   end
 end
