f7c19e0f3538876f276c75ea881e8c79438ea3f3
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2023-08-08 10:47:24 -1000

    fix rules_full_scenario_spec

--- spec/system/routing/rules_full_scenario_spec.rb
+++ spec/system/routing/rules_full_scenario_spec.rb
@@ -255,7 +255,7 @@ describe 'The routing with rules', js: true do
 
     visit "users/activate?#{token_params}"
     fill_in :user_password, with: password
-    click_button 'Définir le mot de passe'
+    click_button 'Continuer'
 
     expect(page).to have_text('Mot de passe enregistré')
   end
