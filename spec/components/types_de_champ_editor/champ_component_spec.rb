43448e8246d5aed292aefed4bd12784883e4aa48
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2023-08-10 12:29:54 -1000

    fix champ_component_spec

--- spec/components/types_de_champ_editor/champ_component_spec.rb
+++ spec/components/types_de_champ_editor/champ_component_spec.rb
@@ -8,6 +8,8 @@ describe TypesDeChampEditor::ChampComponent, type: :component do
 
     before do
       allow_any_instance_of(Procedure).to receive(:stable_ids_used_by_routing_rules).and_return(routing_rules_stable_ids)
+      # pf visa & tefenua champs are activated via Flipper which requires current_user which is not active in these tests
+      allow_any_instance_of(TypesDeChampEditor::ChampComponent).to receive(:filter_featured_type_champ).and_return(true)
       render_inline(component)
     end
 
