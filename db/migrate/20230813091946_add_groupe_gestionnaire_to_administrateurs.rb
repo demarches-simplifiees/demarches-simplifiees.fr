b11495a7a685ef5f04eac29ee378cf645831bcdc
Colin Darie <colin@darie.eu>
2023-10-10 11:00:05 +0200

    chore(schema): fixup failed migrations, ensure exports->instructeurs foreign key exists

--- db/migrate/20230813091946_add_groupe_gestionnaire_to_administrateurs.rb
+++ db/migrate/20230813091946_add_groupe_gestionnaire_to_administrateurs.rb
@@ -2,6 +2,7 @@ class AddGroupeGestionnaireToAdministrateurs < ActiveRecord::Migration[7.0]
   disable_ddl_transaction!
 
   def change
-    add_reference :administrateurs, :groupe_gestionnaire, index: { algorithm: :concurrently }, null: true
+    # Foreign key is added in a later migration
+    add_reference :administrateurs, :groupe_gestionnaire, index: { algorithm: :concurrently }, null: true, foreign_key: false
   end
 end
