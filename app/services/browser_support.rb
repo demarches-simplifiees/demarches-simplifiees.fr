8b23f2a5555fb42406cbef27be417add8d2ccbd8
Paul Chavard <pro@paul.chavard.net>
2023-10-11 23:22:13 +0200

    chore(browser): reduce browser support scope

--- app/services/browser_support.rb
+++ app/services/browser_support.rb
@@ -1,12 +1,12 @@
 class BrowserSupport
   def self.supported?(browser)
     [
-      browser.chrome? && browser.version.to_i >= 50 && !browser.platform.ios?,
-      browser.edge? && browser.version.to_i >= 14 && !browser.compatibility_view?,
-      browser.firefox? && browser.version.to_i >= 50 && !browser.platform.ios?,
-      browser.opera? && browser.version.to_i >= 40,
-      browser.safari? && browser.version.to_i >= 8,
-      browser.platform.ios? && browser.platform.version.to_i >= 8
+      browser.chrome? && browser.version.to_i >= 79 && !browser.platform.ios?,
+      browser.edge? && browser.version.to_i >= 79 && !browser.compatibility_view?,
+      browser.firefox? && browser.version.to_i >= 67 && !browser.platform.ios?,
+      browser.opera? && browser.version.to_i >= 50,
+      browser.safari? && browser.version.to_i >= 12,
+      browser.platform.ios? && browser.platform.version.to_i >= 12
     ].any?
   end
 end
