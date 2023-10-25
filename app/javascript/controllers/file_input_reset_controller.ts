c60494c97546c525534e46ce2bf230ad8f45a846
Paul Chavard <pro@paul.chavard.net>
2023-10-25 12:04:12 +0200

    refactor to use event delegation

--- app/javascript/controllers/file_input_reset_controller.ts
+++ app/javascript/controllers/file_input_reset_controller.ts
@@ -2,18 +2,36 @@ import { ApplicationController } from './application_controller';
 import { hide, show } from '@utils';
 
 export class FileInputResetController extends ApplicationController {
-  static targets = ['input', 'reset'];
+  static targets = ['reset'];
 
-  declare readonly inputTarget: HTMLInputElement;
   declare readonly resetTarget: HTMLElement;
+
+  connect() {
+    this.on('change', (event) => {
+      if (event.target == this.fileInput) {
+        this.showResetButton();
+      }
+    });
+  }
 
   reset(event: Event) {
     event.preventDefault();
-    this.inputTarget.value = '';
+    this.fileInput.value = '';
     hide(this.resetTarget);
   }
 
   showResetButton() {
     show(this.resetTarget);
+  }
+
+  private get fileInput() {
+    const inputs =
+      this.element.querySelectorAll<HTMLInputElement>('input[type="file"]');
+    if (inputs.length == 0) {
+      throw new Error('No file input found');
+    } else if (inputs.length > 1) {
+      throw new Error('Multiple file inputs found');
+    }
+    return inputs[0];
   }
 }
