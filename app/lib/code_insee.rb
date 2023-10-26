42d3052c4ff12af838a609a982eb3e0339dedab3
Christophe Robillard <geek@robiweb.net>
2023-10-26 11:34:55 +0200

    convert code_insee to departement

---
+++ app/lib/code_insee.rb
@@ -0,0 +1,14 @@
+class CodeInsee
+  def initialize(code_insee)
+    @code_insee = code_insee
+  end
+
+  def to_departement
+    dept = @code_insee.strip.first(2)
+    if dept < "97"
+      dept
+    else
+      @code_insee.strip.first(3)
+    end
+  end
+end
