c4272e3bacf0419273862f9409df014f116d50d2
Colin Darie <colin@darie.eu>
2023-10-30 10:50:54 +0100

    fix(dropdown-other): other input alignement/markup

--- app/components/editable_champ/drop_down_list_component.rb
+++ app/components/editable_champ/drop_down_list_component.rb
@@ -1,4 +1,8 @@
 class EditableChamp::DropDownListComponent < EditableChamp::EditableChampBaseComponent
+  def render?
+    @champ.options?
+  end
+
   def select_class_names
     class_names('width-100': contains_long_option?, 'fr-select': true)
   end
@@ -9,6 +13,10 @@ class EditableChamp::DropDownListComponent < EditableChamp::EditableChampBaseCom
 
   def dsfr_champ_container
     @champ.render_as_radios? ? :fieldset : :div
+  end
+
+  def other_element_class_names
+    class_names("fr-fieldset__element" => dsfr_champ_container == :fieldset)
   end
 
   def contains_long_option?
