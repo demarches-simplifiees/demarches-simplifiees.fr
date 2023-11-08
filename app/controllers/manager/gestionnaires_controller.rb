7a3a022ed48291711cb7a3ff832271829d0824fb
seb-by-ouidou <sebastien.delcour@sebastien.delcour>
2023-11-08 13:54:40 +0000

    feat: replace fail by flast alert

--- app/controllers/manager/gestionnaires_controller.rb
+++ app/controllers/manager/gestionnaires_controller.rb
@@ -4,12 +4,12 @@ module Manager
       gestionnaire = Gestionnaire.find(params[:id])
 
       if !gestionnaire.can_be_deleted?
-        fail "Impossible de supprimer ce gestionnaire car il est gestionnaire d'un groupe racine"
+        flash[:alert] = "Impossible de supprimer ce gestionnaire car il est gestionnaire d'un groupe racine"
+      else
+        gestionnaire.destroy!
+        logger.info("Le gestionnaire #{gestionnaire.id} est supprimé par #{current_super_admin.id}")
+        flash[:notice] = "Le gestionnaire #{gestionnaire.id} est supprimé"
       end
-      gestionnaire.destroy!
-
-      logger.info("Le gestionnaire #{gestionnaire.id} est supprimé par #{current_super_admin.id}")
-      flash[:notice] = "Le gestionnaire #{gestionnaire.id} est supprimé"
 
       redirect_to manager_gestionnaires_path
     end
