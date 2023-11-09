01aaec6e907215eca10e82fa147ff984bc994fab
Colin Darie <colin@darie.eu>
2023-11-09 16:36:51 +0100

    chore(geocoder): enable caching for geocoder

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,1 +1,7 @@
-Geocoder.configure(lookup: :ban_data_gouv_fr, use_https: true)
+cache_configuration = if ENV['REDIS_CACHE_URL'].present?
+  { cache: Redis.new(url: ENV['REDIS_CACHE_URL']), cache_options: { prefix: "geocoder:", expiration: 6.hours } }
+else
+  { cache: Geocoder::CacheStore::Generic.new(Rails.cache, { prefix: "geocoder:" }) } # generic has no specific expiration support as of geocoder 1.8
+end
+
+Geocoder.configure(lookup: :ban_data_gouv_fr, use_https: true, **cache_configuration)
