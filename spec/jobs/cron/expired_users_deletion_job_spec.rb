fd81936baf7521d8b1821cddfc0627730dc9b133
Martin <martin@sharypic.com>
2023-11-21 10:18:58 +0100

    correctif(users.expires): maj l'appel a process_expired car le service est instanciable maintenant

--- spec/jobs/cron/expired_users_deletion_job_spec.rb
+++ spec/jobs/cron/expired_users_deletion_job_spec.rb
@@ -5,19 +5,19 @@ describe Cron::ExpiredUsersDeletionJob do
     before { expect(ENV).to receive(:[]).with('EXPIRE_USER_DELETION_JOB_LIMIT').and_return('anything') }
 
     it 'calls Expired::UsersDeletionService.process_expired' do
-      expect(Expired::UsersDeletionService).to receive(:process_expired)
+      expect_any_instance_of(Expired::UsersDeletionService).to receive(:process_expired)
       subject
     end
 
     it 'fails gracefuly by catching any error (to prevent re-enqueue and sending too much email)' do
-      expect(Expired::UsersDeletionService).to receive(:process_expired).and_raise(StandardError)
+      expect_any_instance_of(Expired::UsersDeletionService).to receive(:process_expired).and_raise(StandardError)
       expect { subject }.not_to raise_error
     end
   end
 
   context 'when env[EXPIRE_USER_DELETION_JOB_LIMIT] is absent' do
     it 'does not call Expired::UsersDeletionService.process_expired' do
-      expect(Expired::UsersDeletionService).not_to receive(:process_expired)
+      expect_any_instance_of(Expired::UsersDeletionService).not_to receive(:process_expired)
       subject
     end
   end
