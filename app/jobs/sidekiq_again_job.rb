1880a15d6952d239639cec55314f316dc441149b
simon lehericey <mail@simon.lehericey.net>
2023-11-22 09:56:05 +0100

    sidekiq test job can raise an exception

--- app/jobs/sidekiq_again_job.rb
+++ app/jobs/sidekiq_again_job.rb
@@ -2,7 +2,10 @@ class SidekiqAgainJob < ApplicationJob
   self.queue_adapter = :sidekiq
   queue_as :default
 
-  def perform(user)
+  def perform(user, with_exception: false)
+    if with_exception
+      raise 'Nop'
+    end
     Sentry.capture_message('this is a message from sidekiq')
     UserMailer.new_account_warning(user).deliver_now
   end
