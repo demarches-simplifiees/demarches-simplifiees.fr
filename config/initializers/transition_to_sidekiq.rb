4f75fbe7ec1b29ea578af6f097cb5dc08dafeb7d
simon lehericey <mail@simon.lehericey.net>
2023-12-07 15:47:47 +0100

    move rebase jobs to sidekiq

--- config/initializers/transition_to_sidekiq.rb
+++ config/initializers/transition_to_sidekiq.rb
@@ -9,5 +9,9 @@ if Rails.env.production? && sidekiq_enabled
     class VirusScannerJob
       self.queue_adapter = :sidekiq
     end
+
+    class DossierRebaseJob < ApplicationJob
+      self.queue_adapter = :sidekiq
+    end
   end
 end
