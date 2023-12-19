b7c48acf8678fd36781c7fd332056bf5b34e065c
simon lehericey <mail@simon.lehericey.net>
2023-12-19 16:31:57 +0100

    feat(file retrieval): basic UI skeleton

---
+++ app/controllers/recoveries_controller.rb
@@ -0,0 +1,36 @@
+class RecoveriesController < ApplicationController
+  def nature
+  end
+
+  def post_nature
+    if nature_params == 'collectivite'
+      redirect_to identification_recovery_path
+    else
+      redirect_to support_recovery_path
+    end
+  end
+
+  def identification
+  end
+
+  def post_identification
+    redirect_to selection_recovery_path
+  end
+
+  def selection
+  end
+
+  def post_selection
+    redirect_to terminee_recovery_path
+  end
+
+  def terminee
+  end
+
+  def support
+  end
+
+  private
+
+  def nature_params = params[:nature]
+end
