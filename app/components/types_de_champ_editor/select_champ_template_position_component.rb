dce06f22a06766824dd54cc3a7b688a42e32a0f7
Martin <martin@sharypic.com>
2024-01-03 11:06:29 +0100

    feat(type_de_champ_editor): add SelectChampTemplatePosition which feeds SelectChampPositionComponent options

---
+++ app/components/types_de_champ_editor/select_champ_template_position_component.rb
@@ -0,0 +1,10 @@
+class TypesDeChampEditor::SelectChampTemplatePositionComponent < ApplicationComponent
+  def initialize(block:, coordinates:)
+    @block = block
+    @coordinates = coordinates
+  end
+
+  def block_id
+    dom_id(@block, :types_de_champ_editor_select_champ_template)
+  end
+end
---
+++ app/javascript/controllers/select_champ_position_template_controller.ts
@@ -0,0 +1,74 @@
+import { ApplicationController } from './application_controller';
+
+export class SelectChampPositionTemplateController extends ApplicationController {
+  static targets = ['select', 'template'];
+  static values = {
+    templateId: String
+  };
+  // this element is updated via turbostream as the source of truth for all select
+  declare readonly templateIdValue: string;
+
+  declare readonly selectTargets: HTMLSelectElement[];
+
+  selectTargetConnected(selectElement: HTMLSelectElement) {
+    selectElement.addEventListener('blur', this);
+    selectElement.addEventListener('focus', this);
+    selectElement.addEventListener('change', this);
+  }
+
+  selectTargetDisconnected(selectElement: HTMLSelectElement) {
+    console.log('selectTargetDisconnected');
+    selectElement.removeEventListener('blur', this);
+    selectElement.removeEventListener('focus', this);
+    selectElement.removeEventListener('change', this);
+  }
+
+  handleEvent(event: Event) {
+    switch (event.type) {
+      case 'focus':
+        this.onFocus(event);
+        break;
+      case 'change':
+        this.onChange(event);
+        break;
+    }
+  }
+
+  private onFocus(event: Event): void {
+    const focusedSelect = event.target as HTMLSelectElement;
+    const focusedSelectStableId = this.getStableIdForSelect(focusedSelect);
+    const template = this.element.querySelector<HTMLElement>(
+      `#${this.templateIdValue}`
+    );
+
+    if (template) {
+      const fragment = template.cloneNode(true) as HTMLSelectElement;
+
+      const options = Array.from(fragment.querySelectorAll('option'))
+        .map((option) => {
+          if (option.value == focusedSelectStableId) {
+            option.setAttribute('selected', 'selected');
+          }
+
+          return option.outerHTML;
+        })
+        .join('');
+      focusedSelect.innerHTML = options;
+    }
+  }
+
+  private onChange(event: Event): void {
+    const changedSelectTarget = event.target as HTMLSelectElement;
+    const stableIdDidChange =
+      changedSelectTarget.value !=
+      this.getStableIdForSelect(changedSelectTarget);
+    if (!stableIdDidChange) {
+      // prevent to bulble up
+      event.stopImmediatePropagation();
+    }
+  }
+
+  private getStableIdForSelect(select: HTMLSelectElement): string | null {
+    return select.getAttribute('data-selected');
+  }
+}
