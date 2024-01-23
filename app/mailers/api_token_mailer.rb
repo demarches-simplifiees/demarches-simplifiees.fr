6353c10955c73f36293c16e25f07ef83cf0f81db
simon lehericey <mail@simon.lehericey.net>
2024-01-23 17:21:30 +0100

    add APIToken expiration mailer

---
+++ app/mailers/api_token_mailer.rb
@@ -0,0 +1,18 @@
+# Preview all emails at http://localhost:3000/rails/mailers/api_token_mailer
+class APITokenMailer < ApplicationMailer
+  helper MailerHelper
+
+  layout 'mailers/layout'
+
+  def expiration(api_token)
+    @api_token = api_token
+    user = api_token.administrateur.user
+    subject = "Votre jeton d'accès à la plateforme #{APPLICATION_NAME} expire le #{l(@api_token.expires_at, format: :long)}"
+
+    mail(to: user.email, subject:)
+  end
+
+  def self.critical_email?(action_name)
+    false
+  end
+end
