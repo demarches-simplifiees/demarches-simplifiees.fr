69788e114ee09a8b75a3dc68de175976f2c45c52
Colin Darie <colin@darie.eu>
2024-02-05 10:43:54 +0100

    chore(schema): migrate flipper value string to text

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_26_071130) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_05_093954) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -610,7 +610,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_26_071130) do
     t.string "feature_key", null: false
     t.string "key", null: false
     t.datetime "updated_at", precision: nil, null: false
-    t.string "value"
+    t.text "value"
     t.index ["feature_key", "key", "value"], name: "index_flipper_gates_on_feature_key_and_key_and_value", unique: true
   end
 
