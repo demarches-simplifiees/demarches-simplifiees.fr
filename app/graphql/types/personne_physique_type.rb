9b328828fd96995735ac1faa38d2a8621cb02a4b
mfo <mfo@users.noreply.github.com>
2024-02-28 06:20:25 +0100

    feat(api.dossier.for_tiers): expose email of tier
    Co-authored-by: Colin Darie <colin@darie.eu>

--- app/graphql/types/personne_physique_type.rb
+++ app/graphql/types/personne_physique_type.rb
@@ -6,6 +6,6 @@ module Types
     field :prenom, String, null: false
     field :civilite, Types::Civilite, null: true, method: :gender
     field :date_de_naissance, GraphQL::Types::ISO8601Date, null: true, method: :birthdate
-    field :email, String, "Email du bénéficiaire (dans le cas d'un dossier déposé par et pour l'usager connecté, l'email est celui de l'usager connecté. dans le cas d'un dossier déposé pour un bénéficiaire, l'email est l'email saisi du bénéficiaire [si saisi])", null: true
+    field :email, String, "Email du bénéficiaire (dans le cas d'un dossier déposé par et pour l'usager connecté, l'email est celui de l'usager connecté. Dans le cas d'un dossier déposé pour un bénéficiaire, l'email est celui du bénéficiaire, s'il a été renseigné)", null: true
   end
 end
