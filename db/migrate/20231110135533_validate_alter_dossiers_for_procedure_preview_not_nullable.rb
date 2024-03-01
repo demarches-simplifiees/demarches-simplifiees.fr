b57d7b5ddbc4171d4f7e5c5da0d65dda81f2e7ae
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2024-02-29 14:22:18 -1000

    fix lint

--- db/migrate/20231110135533_validate_alter_dossiers_for_procedure_preview_not_nullable.rb
+++ db/migrate/20231110135533_validate_alter_dossiers_for_procedure_preview_not_nullable.rb
@@ -1,9 +1,9 @@
 class ValidateAlterDossiersForProcedurePreviewNotNullable < ActiveRecord::Migration[7.0]
   def change
-    safety_assured {
+    safety_assured do
       validate_check_constraint :dossiers, name: "dossiers_for_procedure_preview_null"
       change_column_null :dossiers, :for_procedure_preview, false, false
       remove_check_constraint :dossiers, name: "dossiers_for_procedure_preview_null"
-    }
+    end
   end
 end
