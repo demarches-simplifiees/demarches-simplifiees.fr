d44822cc1cf519097e40bbfdab7559599df50721
Paul Chavard <pro@paul.chavard.net>
2024-03-04 16:35:09 +0100

    chore(js): remove vite legacy build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import ViteReact from '@vitejs/plugin-react';
-import ViteLegacy from '@vitejs/plugin-legacy';
-import FullReload from 'vite-plugin-full-reload';
 import RubyPlugin from 'vite-plugin-ruby';
+import FullReload from 'vite-plugin-full-reload';
 
 const plugins = [
   RubyPlugin(),
@@ -13,45 +12,8 @@ const plugins = [
   )
 ];
 
-if (shouldBuildLegacy()) {
-  plugins.push(
-    ViteLegacy({
-      targets: [
-        'defaults',
-        'Chrome >= 50',
-        'Edge >= 14',
-        'Firefox >= 50',
-        'Opera >= 40',
-        'Safari >= 8',
-        'iOS >= 8',
-        'IE >= 11'
-      ],
-      additionalLegacyPolyfills: [
-        'dom4',
-        'core-js/stable',
-        '@stimulus/polyfills',
-        'turbo-polyfills',
-        'intersection-observer',
-        'regenerator-runtime/runtime',
-        'whatwg-fetch',
-        'yet-another-abortcontroller-polyfill'
-      ]
-    })
-  );
-}
-
 export default defineConfig({
   resolve: { alias: { '@utils': '/shared/utils.ts' } },
   build: { sourcemap: true, assetsInlineLimit: 0 },
   plugins
 });
-
-function shouldBuildLegacy() {
-  if (process.env.VITE_LEGACY == 'disabled') {
-    return false;
-  }
-  return (
-    process.env.RAILS_ENV == 'production' ||
-    process.env.VITE_LEGACY == 'enabled'
-  );
-}
