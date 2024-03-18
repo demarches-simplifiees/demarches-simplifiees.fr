a16f88f8afefd9a007caa6042af735b6bcdbecfd
Colin Darie <colin@darie.eu>
2024-03-18 16:32:57 +0100

    chore(schema): add preferred_domain to users

---
+++ app/models/concerns/domain_migratable_concern.rb
@@ -0,0 +1,9 @@
+module DomainMigratableConcern
+  extend ActiveSupport::Concern
+
+  included do
+    enum preferred_domain: { demarches_gouv_fr: 0, demarches_simplifiees_fr: 1 }, _prefix: true
+
+    validates :preferred_domain, inclusion: { in: User.preferred_domains.keys, allow_nil: true }
+  end
+end
