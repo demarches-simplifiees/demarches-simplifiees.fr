6a77bbb6c9d332ae2049e6b48ec0d53e79c611bf
Eric Leroy-Terquem <eric.leroyterquem@beta.gouv.fr>
2024-03-18 16:20:23 +0100

    fix(administrateur): can be deleted only if other admin or dossiers on all procedures included with discarded

--- app/models/administrateur.rb
+++ app/models/administrateur.rb
@@ -69,7 +69,7 @@ class Administrateur < ApplicationRecord
   end
 
   def can_be_deleted?
-    procedures.all? { |p| p.administrateurs.count > 1 || p.dossiers.empty? }
+    procedures.with_discarded.all? { |p| p.administrateurs.count > 1 || p.dossiers.empty? }
   end
 
   def delete_and_transfer_services
