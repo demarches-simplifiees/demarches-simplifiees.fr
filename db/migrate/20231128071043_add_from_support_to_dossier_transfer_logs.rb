ef859080192f5978b41434f46298e85f166077b2
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2024-03-21 15:42:33 -1000

    ignore strong migration error

--- db/migrate/20231128071043_add_from_support_to_dossier_transfer_logs.rb
+++ db/migrate/20231128071043_add_from_support_to_dossier_transfer_logs.rb
@@ -1,5 +1,5 @@
 class AddFromSupportToDossierTransferLogs < ActiveRecord::Migration[7.0]
   def change
-    add_column :dossier_transfer_logs, :from_support, :boolean, default: false, null: false
+    safety_assured { add_column :dossier_transfer_logs, :from_support, :boolean, default: false, null: false }
   end
 end
