5e015306935601a743457b97d0d2adce7bd37750
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2024-03-22 07:44:31 -1000

    StrongMigrations: get back to target version 10 to prevent crash at deployment

--- db/migrate/20231114140154_combine_dossier_and_individual_updates.rb
+++ db/migrate/20231114140154_combine_dossier_and_individual_updates.rb
@@ -1,12 +1,15 @@
 class CombineDossierAndIndividualUpdates < ActiveRecord::Migration[7.0]
   disable_ddl_transaction!
-  def change
-    add_column :dossiers, :mandataire_first_name, :string
-    add_column :dossiers, :mandataire_last_name, :string
-    add_column :dossiers, :for_tiers, :boolean, default: false, null: false
 
-    add_column :individuals, :notification_method, :string
-    add_column :individuals, :email, :string
-    add_index :individuals, :email, algorithm: :concurrently
+  def change
+    safety_assured do
+      add_column :dossiers, :mandataire_first_name, :string
+      add_column :dossiers, :mandataire_last_name, :string
+      add_column :dossiers, :for_tiers, :boolean, default: false, null: false
+
+      add_column :individuals, :notification_method, :string
+      add_column :individuals, :email, :string
+      add_index :individuals, :email, algorithm: :concurrently
+    end
   end
 end
--- db/migrate/20231221142727_add_last_used_ips_column_to_api_token.rb
+++ db/migrate/20231221142727_add_last_used_ips_column_to_api_token.rb
@@ -1,5 +1,7 @@
 class AddLastUsedIpsColumnToAPIToken < ActiveRecord::Migration[7.0]
   def change
-    add_column :api_tokens, :stored_ips, :inet, array: true, default: []
+    safety_assured do
+      add_column :api_tokens, :stored_ips, :inet, array: true, default: []
+    end
   end
 end
