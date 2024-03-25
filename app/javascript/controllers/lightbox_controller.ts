41a10b0290ace3bd5f7a40ea30abbdecbb9d9267
Eric Leroy-Terquem <eric.leroyterquem@beta.gouv.fr>
2024-03-25 10:36:55 +0100

    third test

--- app/javascript/controllers/lightbox_controller.ts
+++ app/javascript/controllers/lightbox_controller.ts
@@ -18,6 +18,7 @@ export default class extends Controller {
   // static targets = [ 'preview' ]
   // new comment
   // new one
+  // third test
   // declare readonly previewTarget: HTMLIFrameElement;
 
 
