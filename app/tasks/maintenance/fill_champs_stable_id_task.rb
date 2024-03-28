3c336c437aeda92e2ed2d5c5bfa5071041bec15e
Paul Chavard <pro@paul.chavard.net>
2024-03-28 17:35:17 +0100

    chore(task): optimize maintenance task

--- app/tasks/maintenance/fill_champs_stable_id_task.rb
+++ app/tasks/maintenance/fill_champs_stable_id_task.rb
@@ -2,16 +2,21 @@
 
 module Maintenance
   class FillChampsStableIdTask < MaintenanceTasks::Task
+    BATCH = 1_000
+
     def collection
-      Dossier.all
+      (Dossier.last.id / BATCH).ceil.times.to_a
     end
 
-    def process(dossier)
-      dossier.champs
-        .includes(:type_de_champ)
-        .where(stable_id: nil)
+    def process(batch_number)
+      dossier_id_start = batch_number * BATCH
+      dossier_id_end = dossier_id_start + BATCH
+      Champ
+        .where(dossier_id: dossier_id_start..dossier_id_end, stable_id: nil)
+        .joins(:type_de_champ)
+        .select('champs.id, types_de_champ.stable_id as type_de_champ_stable_id')
         .find_each do |champ|
-          champ.update_columns(stable_id: champ.stable_id, stream: 'main')
+          champ.update_columns(stable_id: champ.type_de_champ_stable_id, stream: 'main')
         end
     end
   end
