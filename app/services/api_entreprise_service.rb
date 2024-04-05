2e2f4706c51a9521a4d06f2b1930e95a1831875d
simon lehericey <mail@simon.lehericey.net>
2024-04-05 12:26:32 +0200

    deduplicate and add error management

--- app/services/api_entreprise_service.rb
+++ app/services/api_entreprise_service.rb
@@ -60,21 +60,25 @@ class APIEntrepriseService
 
     # See: https://entreprise.api.gouv.fr/developpeurs#surveillance-etat-fournisseurs
     def api_insee_up?
-      response = Typhoeus.get("https://entreprise.api.gouv.fr/ping/insee/sirene", timeout: 1)
-      if response.success?
-        JSON.parse(response.body).fetch('status') == 'ok'
-      else
-        false
-      end
+      api_up?("https://entreprise.api.gouv.fr/ping/insee/sirene")
     end
 
     def api_djepva_up?
-      response = Typhoeus.get("https://entreprise.api.gouv.fr/ping/djepva/api-association", timeout: 1)
+      api_up?("https://entreprise.api.gouv.fr/ping/djepva/api-association")
+    end
+
+    private
+
+    def api_up?(url)
+      response = Typhoeus.get(url, timeout: 1)
       if response.success?
         JSON.parse(response.body).fetch('status') == 'ok'
       else
         false
       end
+    rescue => e
+      Sentry.capture_exception(e)
+      false
     end
   end
 end
