35c92ee906ef5b730d749e7f20b656bdbd01b772
mfo <mfo@users.noreply.github.com>
2024-04-08 16:36:37 +0200

    feat(champs.errors.format): better format for error below champs
    Co-authored-by: Corinne Durrmeyer <corinne@inseo.fr>

--- app/components/dsfr/input_status_message_component.rb
+++ app/components/dsfr/input_status_message_component.rb
@@ -1,9 +1,10 @@
 module Dsfr
   class InputStatusMessageComponent < ApplicationComponent
-    def initialize(errors_on_attribute:, error_full_messages:, described_by:)
+    def initialize(errors_on_attribute:, error_full_messages:, described_by:, champ:)
       @errors_on_attribute = errors_on_attribute
       @error_full_messages = error_full_messages
       @described_by = described_by
+      @champ = champ
     end
 
     def render?
