ce5f0df1ea635f4ec84daa9d50d8e7552c3907d1
mfo <mfo@users.noreply.github.com>
2024-04-09 16:35:52 +0200

    fix(Champ.rnf): highlight error with DSFR classname on RNF champ

--- app/components/editable_champ/rnf_component.rb
+++ app/components/editable_champ/rnf_component.rb
@@ -1,2 +1,10 @@
 class EditableChamp::RNFComponent < EditableChamp::EditableChampBaseComponent
+  def initialize(form:, champ:, seen_at: nil, opts: {})
+    @form, @champ, @seen_at, @opts = form, champ, seen_at, opts
+    @attribute = :value
+  end
+
+  def dsfr_input_classname
+    'fr-input'
+    end
 end
