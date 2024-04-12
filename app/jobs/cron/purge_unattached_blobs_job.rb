d419508b684a622ffc314e988135a7c396903bf9
simon lehericey <mail@simon.lehericey.net>
2024-04-12 10:12:26 +0200

    fix: reduce a join query size

--- app/jobs/cron/purge_unattached_blobs_job.rb
+++ app/jobs/cron/purge_unattached_blobs_job.rb
@@ -5,12 +5,19 @@ class Cron::PurgeUnattachedBlobsJob < Cron::CronJob
     # .in_batches { _1.each... } is more efficient in this case that in_batches.each_record or find_each
     # because it plucks only ids in a preliminary query, then load records with selected columns in batches by ids.
     # This is faster than other batch strategies, which load at once selected columns with an ORDER BY in the same query, triggering timeouts.
-    ActiveStorage::Blob.unattached.select(:id, :service_name, :created_at).in_batches do |relation|
-      relation.each do |blob|
-        return if blob.created_at > 24.hours.ago # not in where() because it's not an indexed column
-
-        blob.purge_later
-      end
+    #
+    # .where(created_at: 1.week.ago..1.day.ago) to limit the number of records to be joined
+    # to the attachments table because of the unattached scope. Otherwise, it is triggering timeouts.
+    #
+    # the creation of an index on created_at does not seem required yet.
+    #
+    # caveats: the job needs to be run at least once a week to avoid missing blobs
+    ActiveStorage::Blob
+      .where(created_at: 1.week.ago..1.day.ago)
+      .unattached
+      .select(:id, :service_name)
+      .in_batches do |relation|
+      relation.each(&:purge_later)
     end
   end
 end
