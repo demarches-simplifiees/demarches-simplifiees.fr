ca4ab4cfe1735f0b17f734e026e3cc62c5d1f91b
simon lehericey <mail@simon.lehericey.net>
2024-04-17 15:33:37 +0200

    fix: multi FC do not change const

--- app/models/france_connect_particulier_client.rb
+++ app/models/france_connect_particulier_client.rb
@@ -1,12 +1,12 @@
 class FranceConnectParticulierClient < OpenIDConnect::Client
   def initialize(code = nil)
-    config = FRANCE_CONNECT[:particulier]
+    config = FRANCE_CONNECT[:particulier].deep_dup
 
     # TODO: remove this block when migration to new domain is done
     # dirty hack to redirect to the right domain
-    # if !Rails.env.test? && Current.host != ENV.fetch("APP_HOST")
-    #   config[:redirect_uri] = config[:redirect_uri].gsub(ENV.fetch("APP_HOST"), Current.host)
-    # end
+    if !Rails.env.test? && Current.host != ENV.fetch("APP_HOST")
+      config[:redirect_uri] = config[:redirect_uri].gsub(ENV.fetch("APP_HOST"), Current.host)
+    end
 
     super(config)
 
