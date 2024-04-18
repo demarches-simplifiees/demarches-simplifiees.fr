8f8b63ac6acd8f9ce80276b1060dfae674e6b840
simon lehericey <mail@simon.lehericey.net>
2024-04-18 11:18:53 +0200

    fix: load the dols day by day

--- app/jobs/cron/operations_signature_job.rb
+++ app/jobs/cron/operations_signature_job.rb
@@ -2,14 +2,16 @@ class Cron::OperationsSignatureJob < Cron::CronJob
   self.schedule_expression = "every day at 6 am"
 
   def perform(*args)
+    start_date = DossierOperationLog.where(bill_signature: nil).order(:executed_at).pick(:executed_at).beginning_of_day
     last_midnight = Time.zone.today.beginning_of_day
-    operations_by_day = BillSignatureService.grouped_unsigned_operation_until(last_midnight)
-    operations_by_day.each do |day, operations|
-      begin
-        BillSignatureService.sign_operations(operations, day)
-      rescue
-        raise # let errors show up on Sentry and delayed_jobs
-      end
+
+    while start_date < last_midnight
+      operations = DossierOperationLog
+        .where(executed_at: start_date...start_date.tomorrow, bill_signature: nil)
+
+      BillSignatureService.sign_operations(operations, start_date) if operations.present?
+
+      start_date = start_date.tomorrow
     end
   end
 end
