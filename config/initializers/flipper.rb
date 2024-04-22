388470f1868f283857ce04a7e1f0ccc4be230654
Eric Leroy-Terquem <eric.leroyterquem@beta.gouv.fr>
2024-04-22 20:55:13 +0200

    fix(gallery): add a feature flag on gallery demande

--- config/initializers/flipper.rb
+++ config/initializers/flipper.rb
@@ -26,6 +26,7 @@ features = [
   :engagement_juridique_type_de_champ,
   :export_order_by_revision,
   :expression_reguliere_type_de_champ,
+  :gallery_demande,
   :groupe_instructeur_api_hack,
   :hide_instructeur_email,
   :sva,
