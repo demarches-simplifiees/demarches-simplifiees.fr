1307de225fef3d5d270e26325ca955f9f1c6e2a7
Lisa Durand <lisa.c.durand@gmail.com>
2024-04-29 16:53:21 +0200

    create a job for all image treatments

--- app/jobs/image_processor_job.rb
+++ app/jobs/image_processor_job.rb
@@ -4,11 +4,11 @@ class ImageProcessorJob < ApplicationJob
 
   # If by the time the job runs the blob has been deleted, ignore the error
   discard_on ActiveRecord::RecordNotFound
-  # If the file is deleted during the scan, ignore the error
-  discard_on ActiveStorage::FileNotFoundError
-  # If the file is not analyzed or scanned for viruses yet, retry later
-  # (to avoid modifying the file while it is being scanned).
-  retry_on FileNotScannedYetError, wait: :exponentially_longer, attempts: 10
+   # If the file is deleted during the scan, ignore the error
+   discard_on ActiveStorage::FileNotFoundError
+   # If the file is not analyzed or scanned for viruses yet, retry later
+   # (to avoid modifying the file while it is being scanned).
+   retry_on FileNotScannedYetError, wait: :exponentially_longer, attempts: 10
 
   def perform(blob)
     return if blob.nil?
