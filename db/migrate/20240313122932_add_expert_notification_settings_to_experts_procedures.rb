5f614a50e733b60b3cb4cf21d218609a967a520b
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2024-04-30 18:16:24 -1000

    Ignore strong_migrations as MD migrations are run while MD is down

--- db/migrate/20240313122932_add_expert_notification_settings_to_experts_procedures.rb
+++ db/migrate/20240313122932_add_expert_notification_settings_to_experts_procedures.rb
@@ -1,6 +1,8 @@
 class AddExpertNotificationSettingsToExpertsProcedures < ActiveRecord::Migration[7.0]
   def change
-    add_column :experts_procedures, :notify_on_new_avis, :boolean, default: true, null: false
-    add_column :experts_procedures, :notify_on_new_message, :boolean, default: false, null: false
+    safety_assured do
+      add_column :experts_procedures, :notify_on_new_avis, :boolean, default: true, null: false
+      add_column :experts_procedures, :notify_on_new_message, :boolean, default: false, null: false
+    end
   end
 end
