659d35e11f0fe69a252ef28485895246733e70ef
Colin Darie <colin@darie.eu>
2024-05-13 16:38:37 +0200

    test: fix for mutable string

--- db/migrate/20240718100430_rename_supprimes_recemment_from_procedure_presentation.rb
+++ db/migrate/20240718100430_rename_supprimes_recemment_from_procedure_presentation.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RenameSupprimesRecemmentFromProcedurePresentation < ActiveRecord::Migration[7.0]
   def up
     ProcedurePresentation.update_all(%Q(filters = filters || '{"supprimes": []}'))
