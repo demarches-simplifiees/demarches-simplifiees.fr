4ea601de79fffeb1080a6f4aff974f7ea3ea22ab
mfo <mfo@users.noreply.github.com>
2024-05-21 09:50:27 +0200

    fix(champs.pj.clone): stop cloning private piece_justificative_file when user clone his dossier

--- app/models/champ.rb
+++ app/models/champ.rb
@@ -238,7 +238,7 @@ class Champ < ApplicationRecord
         kopy.write_attribute(:stable_id, original.stable_id)
         kopy.write_attribute(:stream, 'main')
       end
-      ClonePiecesJustificativesService.clone_attachments(original, kopy)
+      ClonePiecesJustificativesService.clone_attachments(original, kopy) if fork || !private?
     end
   end
 
