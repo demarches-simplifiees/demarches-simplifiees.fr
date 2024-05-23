3f40e11686b632fe8e55c75ede1d6c866e75dfac
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2024-05-23 13:40:55 -1000

    rubocop

--- db/migrate/20240318152314_add_preferred_domain_to_users.rb
+++ db/migrate/20240318152314_add_preferred_domain_to_users.rb
@@ -2,8 +2,8 @@
 
 class AddPreferredDomainToUsers < ActiveRecord::Migration[7.0]
   def change
-    safety_assured {
+    safety_assured do
       add_column :users, :preferred_domain, :integer, default: nil
-    }
+    end
   end
 end
