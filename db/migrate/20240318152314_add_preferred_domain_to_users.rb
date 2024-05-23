f366f3a03cbc58a4c5536d8c66712581b7e0bfa1
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2024-05-23 09:36:06 -1000

    ignore strong_migrations warning as MD migrations are performed when MD is down

--- db/migrate/20240318152314_add_preferred_domain_to_users.rb
+++ db/migrate/20240318152314_add_preferred_domain_to_users.rb
@@ -2,6 +2,8 @@
 
 class AddPreferredDomainToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :preferred_domain, :integer, default: nil
+    safety_assured {
+      add_column :users, :preferred_domain, :integer, default: nil
+    }
   end
 end
