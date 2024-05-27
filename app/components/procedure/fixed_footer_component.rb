2659d0f966a25d06404891ba9f5e2ce86a22c16a
Lisa Durand <lisa.c.durand@gmail.com>
2024-05-27 17:56:05 +0200

    create component for footer

---
+++ app/components/procedure/fixed_footer_component.rb
@@ -0,0 +1,10 @@
+class Procedure::FixedFooterComponent < ApplicationComponent
+  def initialize(procedure:, form: nil, is_form_disabled: nil, extra_class_names: nil)
+    @procedure = procedure
+    @form = form
+    @is_form_disabled = is_form_disabled
+    @extra_class_names = extra_class_names
+  end
+
+  attr_reader :form, :is_form_disabled, :extra_class_names
+end
