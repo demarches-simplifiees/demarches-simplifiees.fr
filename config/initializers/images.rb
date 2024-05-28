58a22d8b9c5b7a623aefa4fe1c0dcef60f933a7e
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2024-05-28 10:18:36 -1000

    fix default MAILER_LOGO_SRC

--- config/initializers/images.rb
+++ config/initializers/images.rb
@@ -14,7 +14,7 @@ HEADER_LOGO_WIDTH = ENV.fetch("HEADER_LOGO_WIDTH", "65")
 HEADER_LOGO_HEIGHT = ENV.fetch("HEADER_LOGO_HEIGHT", "56")
 
 # Mailer logos
-MAILER_LOGO_SRC = ENV.fetch("MAILER_LOGO_SRC", "republique-francaise-logo.png")
+MAILER_LOGO_SRC = ENV.fetch("MAILER_LOGO_SRC", "mailer/republique-francaise-logo.png")
 
 # Default logo of a procedure
 PROCEDURE_DEFAULT_LOGO_SRC = ENV.fetch("PROCEDURE_DEFAULT_LOGO_SRC", "polynesie.png")
