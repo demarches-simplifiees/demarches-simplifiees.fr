2dda5e44f9f158f3c6d27df712fbfddf90223e05
Colin Darie <colin@darie.eu>
2024-06-14 11:34:41 +0200

    chore(kredis): use default shared connection name, fixing dossier index debounce

--- config/initializers/kredis.rb
+++ config/initializers/kredis.rb
@@ -1,7 +1,7 @@
-redis_volatile_options = {
+redis_shared_options = {
   url: ENV['REDIS_CACHE_URL'], # will fallback to default redis url if empty, and won't fail if there is no redis server
   ssl: ENV['REDIS_CACHE_SSL'] == 'enabled'
 }
-redis_volatile_options[:ssl_params] = { verify_mode: OpenSSL::SSL::VERIFY_NONE } if ENV['REDIS_CACHE_SSL_VERIFY_NONE'] == 'enabled'
+redis_shared_options[:ssl_params] = { verify_mode: OpenSSL::SSL::VERIFY_NONE } if ENV['REDIS_CACHE_SSL_VERIFY_NONE'] == 'enabled'
 
-Kredis::Connections.connections[:volatile] = Redis.new(redis_volatile_options)
+Kredis::Connections.connections[:shared] = Redis.new(redis_shared_options)
