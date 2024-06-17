c288d340d8707d252bdacee05903c5b0b3749f9c
Colin Darie <colin@darie.eu>
2024-06-17 11:53:01 +0200

    style(stats): better responsive & dark theme for procedure stats

--- config/initializers/chartkick.rb
+++ config/initializers/chartkick.rb
@@ -5,14 +5,30 @@ Chartkick.options = {
   decimal: ',',
   default_library_config: {
     chart: { backgroundColor: 'var(--background-contrast-grey)' },
-             xAxis: {
-               lineColor: 'var(--border-action-high-grey)',
-                      labels: { style: { color: "var(--text-default-grey)" } }
-             },
-             yAxis: {
-               gridLineColor: 'var(--border-plain-grey)',
-                      lineColor: 'var(--border-action-high-grey)',
-                      labels: { style: { color: "var(--text-default-grey)" } }
-             }
+    xAxis: {
+      lineColor: 'var(--border-action-high-grey)',
+      labels: { style: { color: "var(--text-default-grey)" } }
+    },
+    yAxis: {
+      gridLineColor: 'var(--border-plain-grey)',
+      lineColor: 'var(--border-action-high-grey)',
+      labels: { style: { color: "var(--text-default-grey)" } }
+    },
+    legend: {
+      itemStyle: {
+        color: "var(--text-default-grey)"
+      }
+    },
+    plotOptions: {
+      pie: {
+        dataLabels: {
+          color: "var(--text-default-grey)",
+          enabled: true, format: '{point.name} : {point.percentage: .1f}%',
+          style: {
+            textOutline: 'none'
+          }
+        }
+      }
+    }
   }
 }
