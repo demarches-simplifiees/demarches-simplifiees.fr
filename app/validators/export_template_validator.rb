f9a5336223c83b5619c61bebe80c77e15a8c129c
simon lehericey <mail@simon.lehericey.net>
2024-06-28 10:35:37 +0200

    fix validation

--- app/validators/export_template_validator.rb
+++ app/validators/export_template_validator.rb
@@ -8,38 +8,33 @@ class ExportTemplateValidator < ActiveModel::Validator
   private
 
   def validate_default_dossier_directory(record)
-    mention = attribute_content_mention(record, :default_dossier_directory)
+    content = record.default_dossier_directory['content']&.first&.fetch('content', nil)
+    mention = attribute_content_mention(content)
     if mention&.fetch("id", nil) != "dossier_number"
       record.errors.add :default_dossier_directory, :dossier_number_mandatory
     end
   end
 
   def validate_pdf_name(record)
-    if attribute_content_text(record, :pdf_name).blank? && attribute_content_mention(record, :pdf_name).blank?
+    content = record.pdf_name['template']['content']&.first&.fetch('content', nil)
+    if attribute_content_text(content).blank? && attribute_content_mention(content).blank?
       record.errors.add :pdf_name, :blank
     end
   end
 
-  def attribute_content_text(record, attribute)
-    attribute_content(record, attribute)&.find { |elem| elem["type"] == "text" }&.fetch("text", nil)
+  def attribute_content_text(content)
+    content&.find { |elem| elem["type"] == "text" }&.fetch("text", nil)
   end
 
-  def attribute_content_mention(record, attribute)
-    attribute_content(record, attribute)&.find { |elem| elem["type"] == "mention" }&.fetch("attrs", nil)
-  end
-
-  def attribute_content(record, attribute)
-    content = record.content[attribute.to_s]&.fetch("content", nil)
-    if content.is_a?(Array)
-      content.first&.fetch("content", nil)
-    end
+  def attribute_content_mention(content)
+    content&.find { |elem| elem["type"] == "mention" }&.fetch("attrs", nil)
   end
 
   def validate_pjs(record)
-    record.content["pjs"]&.each do |pj|
+    record.pjs.each do |pj|
       pj_sym = pj.symbolize_keys
-      libelle = record.groupe_instructeur.procedure.exportables_pieces_jointes.find { _1.stable_id.to_s == pj_sym[:stable_id] }&.libelle&.to_sym
-      validate_content(record, pj_sym[:path], libelle)
+      libelle = record.groupe_instructeur.procedure.exportables_pieces_jointes.find { _1.stable_id.to_s == pj_sym[:stable_id] }&.libelle
+      validate_content(record, pj_sym[:template], libelle)
     end
   end
 
