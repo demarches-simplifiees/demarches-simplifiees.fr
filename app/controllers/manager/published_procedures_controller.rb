0a70c6b90f8565b3a5266a07057fb29595abf593
Eric Leroy-Terquem <eric.leroyterquem@beta.gouv.fr>
2024-06-28 15:40:13 +0200

    manager: add a page for demarches publiees

---
+++ app/controllers/manager/published_procedures_controller.rb
@@ -0,0 +1,20 @@
+module Manager
+  class PublishedProceduresController < Manager::ApplicationController
+    def index
+      @records_per_page = params[:records_per_page] || "25"
+      resources = Procedure
+        .publiee
+        .order(published_at: :desc)
+        .page(params[:_page])
+        .per(@records_per_page)
+      page = Administrate::Page::Collection.new(dashboard)
+
+      render locals: {
+        resources: resources,
+        page: page,
+        show_search_bar: false,
+        search_term: nil
+      }
+    end
+  end
+end
