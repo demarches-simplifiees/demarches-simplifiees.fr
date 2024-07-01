3011c0cad3a5df37f04e848457f55cb60aad4a5c
Kara Diaby <kdiaby.pro@gmail.com>
2024-07-01 14:37:33 +0000

    France connect particulier controller : permet de gérer le cas ou le mail n'est pas connu de DS
    Signed-off-by: Kara Diaby <kdiaby.pro@gmail.com>

--- app/controllers/france_connect/particulier_controller.rb
+++ app/controllers/france_connect/particulier_controller.rb
@@ -11,6 +11,10 @@ class FranceConnect::ParticulierController < ApplicationController
     end
   end
 
+  def choose_email
+    @france_connect_email = params[:france_connect_email]
+  end
+
   def callback
     fci = FranceConnectService.find_or_retrieve_france_connect_information(params[:code])
 
@@ -18,8 +22,9 @@ class FranceConnect::ParticulierController < ApplicationController
       preexisting_unlinked_user = User.find_by(email: sanitize(fci.email_france_connect))
 
       if preexisting_unlinked_user.nil?
-        fci.associate_user!(fci.email_france_connect)
-        connect_france_connect_particulier(fci.user)
+        session[:france_connect_information] = fci.to_json
+        render :choose_email, locals: { france_connect_email: fci.email_france_connect }
+
       elsif !preexisting_unlinked_user.can_france_connect?
         fci.destroy
         redirect_to new_user_session_path, alert: t('errors.messages.france_connect.forbidden_html', reset_link: new_user_password_path)
@@ -33,7 +38,7 @@ class FranceConnect::ParticulierController < ApplicationController
       if user.can_france_connect?
         fci.update(updated_at: Time.zone.now)
         connect_france_connect_particulier(user)
-      else # same behaviour as redirect nicely with message when instructeur/administrateur
+      else
         fci.destroy
         redirect_to new_user_session_path, alert: t('errors.messages.france_connect.forbidden_html', reset_link: new_user_password_path)
       end
@@ -42,6 +47,34 @@ class FranceConnect::ParticulierController < ApplicationController
   rescue Rack::OAuth2::Client::Error => e
     Rails.logger.error e.message
     redirect_france_connect_error_connection
+  end
+
+  def associate_user
+    fci_json = session[:france_connect_information]
+    fci = FranceConnectInformation.new(JSON.parse(fci_json))
+
+    if fci.nil?
+      redirect_to new_user_session_path, alert: t('france_connect.particulier.associate_user.errors.unable_to_retrieve_information')
+      return
+    end
+
+    use_france_connect_email = ActiveModel::Type::Boolean.new.cast(params[:use_france_connect_email])
+    email = use_france_connect_email ? fci.email_france_connect : params[:alternative_email]
+
+    if email.present?
+      if fci.associate_user!(email)
+        render :confirmation_sent, locals: { email: email, fci: fci }
+      else
+        redirect_to new_user_session_path, alert: t('france_connect.particulier.associate_user.errors.unable_to_send_confirmation')
+      end
+    else
+      redirect_to new_user_session_path, alert: 'Veuillez fournir un email.'
+    end
+  end
+
+  def email_field
+    show = params[:use_france_connect_email] == "false"
+    render partial: 'france_connect/particulier/email_field', locals: { show: show }
   end
 
   def merge
@@ -109,6 +142,21 @@ class FranceConnect::ParticulierController < ApplicationController
     merge_token = @fci.create_merge_token!
     redirect_to france_connect_particulier_merge_path(merge_token),
                 notice: t('france_connect.particulier.flash.confirmation_mail_sent')
+  end
+
+  def confirm_email
+    user = User.find_by(confirmation_token: params[:token])
+    if user && user.confirmation_sent_at > 2.days.ago
+      user.update(email_verified_at: Time.zone.now, confirmation_token: nil)
+      redirect_to stored_location_for(user) || root_path(user), notice: 'Votre email est bien vérifié, vous recevrez les notifications relatives à vos dossiers'
+    else
+      redirect_to root_path, alert: "Invalid or expired confirmation link."
+    end
+  end
+
+  def connect_france_connect_particulier_redirect
+    user = User.find(params[:user_id])
+    connect_france_connect_particulier(user)
   end
 
   private
