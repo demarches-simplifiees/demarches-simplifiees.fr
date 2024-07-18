977cfc87ce24f0eca4620f882f0998def5f3dbe4
simon lehericey <mail@simon.lehericey.net>
2024-07-18 09:57:23 +0200

    Add export_item type

---
+++ app/types/export_item_type.rb
@@ -0,0 +1,40 @@
+class ExportItemType < ActiveRecord::Type::Value
+  # form_input, or setter -> type
+  def cast(value)
+    value = value.deep_symbolize_keys if value.respond_to?(:deep_symbolize_keys)
+
+    case value
+    in ExportItem
+      value
+    in NilClass # default value
+      nil
+    # from db
+    in { template: Hash, enabled: TrueClass | FalseClass } => h
+
+      ExportItem.new(**h.slice(:template, :enabled, :stable_id))
+    # from form
+    in { template: String } => h
+
+      template = JSON.parse(h[:template]).deep_symbolize_keys
+      enabled = h[:enabled] == 'true'
+      stable_id = h[:stable_id]&.to_i
+      ExportItem.new(template:, enabled:, stable_id:)
+    end
+  end
+
+  # db -> ruby
+  def deserialize(value) = cast(value&.then { JSON.parse(_1) })
+
+  # ruby -> db
+  def serialize(value)
+    if value.is_a?(ExportItem)
+      JSON.generate({
+        template: value.template,
+        enabled: value.enabled,
+        stable_id: value.stable_id
+      }.compact)
+    else
+      raise ArgumentError, "Invalid value for ExportItem serialization: #{value}"
+    end
+  end
+end
---
+++ config/initializers/types.rb
@@ -0,0 +1,5 @@
+require Rails.root.join("app/types/export_item_type")
+
+ActiveSupport.on_load(:active_record) do
+  ActiveRecord::Type.register(:export_item, ExportItemType)
+end
