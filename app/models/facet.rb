cf149a6f639c06e60135ded5b745db06170ebdde
mfo <mfo@users.noreply.github.com>
2024-07-19 11:55:44 +0200

    add the facet

---
+++ app/models/facet.rb
@@ -0,0 +1,34 @@
+class Facet
+  def initialize(table:, column:, label: nil, virtual: false, type: :text, value_column: :value, filterable: true, classname: nil, scope: '')
+    @table = table
+    @column = column
+    @label = label || I18n.t(column, scope: [:activerecord, :attributes, :procedure_presentation, :fields, table])
+    @classname = classname
+    @virtual = virtual
+    @type = type
+    @scope = scope
+    @value_column = value_column
+    @filterable = filterable
+  end
+
+  attr_reader :table, :column, :label, :classname, :virtual, :type, :scope, :value_column, :filterable
+
+  # ??? p-e que le filtre peut avoir
+  # -> .serialize (field_id)
+  # -> .type
+  # -> .to_enum ? (field_enum(field_id))
+  # -> .to_sql? ou .to_filter? (filtered_ids)
+  # -> .humanize_value (human_value_for_filter)
+  # -> .sorted_on_me(ProcedurePresentation.sort)
+  # -> .aria_sort # p-e a extraire en vue
+  #
+  def ==(other)
+    other.to_json == to_json
+  end
+
+  def to_json
+    {
+      table:, column:, label:, classname:, virtual:, type:, scope:, value_column:, filterable:
+    }
+  end
+end
