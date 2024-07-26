a758c82658240654e52507f3323a2c51a8172a4a
Christophe Robillard <christophe.robillard@mail.numerique.gouv.fr>
2024-07-26 16:20:11 +0200

    export_template model for tabular

--- app/models/export_template.rb
+++ app/models/export_template.rb
@@ -3,13 +3,11 @@
 class ExportTemplate < ApplicationRecord
   include TagsSubstitutionConcern
 
-  self.ignored_columns += ["content"]
-
   belongs_to :groupe_instructeur
   has_one :procedure, through: :groupe_instructeur
   has_many :exports, dependent: :nullify
 
-  enum kind: { zip: "zip" }, _prefix: :template
+  enum kind: { zip: 'zip', csv: 'csv', xlsx: 'xlsx', ods: 'ods' }, _prefix: :template
 
   attribute :dossier_folder, :export_item
   attribute :export_pdf, :export_item
@@ -28,9 +26,13 @@ class ExportTemplate < ApplicationRecord
   end
 
   def self.default(name: nil, kind: 'zip', groupe_instructeur:)
-    dossier_folder = ExportItem.default(prefix: 'dossier')
-    export_pdf = ExportItem.default(prefix: 'export')
-    pjs = groupe_instructeur.procedure.exportables_pieces_jointes.map { |tdc| ExportItem.default_pj(tdc) }
+    if kind == 'zip'
+      dossier_folder = ExportItem.default(prefix: 'dossier')
+      export_pdf = ExportItem.default(prefix: 'export')
+      pjs = groupe_instructeur.procedure.exportables_pieces_jointes.map { |tdc| ExportItem.default_pj(tdc) }
+    else
+      dossier_folder, export_pdf, pjs = nil, nil, []
+    end
 
     new(name:, kind:, groupe_instructeur:, dossier_folder:, export_pdf:, pjs:)
   end
@@ -58,11 +60,180 @@ class ExportTemplate < ApplicationRecord
     File.join(dossier_folder.path(dossier), file_path) if file_path.present?
   end
 
+  def columns=(columns)
+    content["columns"] = columns.compact_blank
+      .each { _1[:libelle] = libelle_for_column_hash(_1) }
+      .filter { _1[:libelle].present? }
+  end
+
+  def columns
+    content["columns"]&.map(&:symbolize_keys)
+  end
+
+def all_tdc_columns
+    procedure.types_de_champ_for_procedure_presentation.not_repetition.map do |tdc|
+      tdc.columns_for_export.map do
+        _1.merge({ libelle: saved_libelle(_1) || _1[:libelle] })
+      end
+    end
+  end
+
+  def repetable_columns
+    columns.filter { _1[:source] == 'repet' }
+      .group_by { _1[:repetition_champ_stable_id] }
+  end
+
+  def all_repetable_tdc_columns
+    procedure
+      .types_de_champ_for_procedure_presentation
+      .repetition
+      .filter_map do |type_de_champ_repetition|
+        types_de_champ = procedure.types_de_champ_for_procedure_presentation(type_de_champ_repetition)
+
+        if types_de_champ.present?
+          h = {}
+          h[:libelle] = type_de_champ_repetition.libelle
+          h[:types_de_champ] = types_de_champ.map do |tdc|
+            tdc.columns_for_export(repetition_champ_stable_id: type_de_champ_repetition.stable_id).map do
+              _1.merge({ libelle: saved_libelle(_1) || _1[:libelle] })
+            end
+          end
+          h
+        end
+      end
+  end
+
+  def all_usager_columns
+    dossier_columns_for(all_usager_column_keys)
+  end
+
+  def all_dossier_columns
+    dossier_columns_for(all_dossier_column_keys)
+  end
+
   private
 
   def ensure_pjs_are_legit
     legitimate_pj_stable_ids = procedure.exportables_pieces_jointes_for_all_versions.map(&:stable_id)
 
     self.pjs = pjs.filter { _1.stable_id.in?(legitimate_pj_stable_ids) }
+  end
+
+  def libelle_for_column_hash(current_column)
+    case current_column[:source]
+    when 'tdc', 'repet'
+      active_type_de_champ = procedure.active_revision.types_de_champ.find_by(stable_id: current_column[:stable_id])
+      if active_type_de_champ
+        active_libelle = active_type_de_champ.libelle_for_path(current_column[:path])
+        saved_libelle(current_column) || active_libelle
+      end
+    when 'dossier'
+      columns_meta[current_column[:path].to_sym][:libelle]
+    end
+  end
+
+  def saved_libelle(path_h)
+    columns&.find { _1.slice(:path, :stable_id) == path_h.slice(:path, :stable_id) }&.dig(:libelle)
+  end
+
+  def dossier_columns_for(columns)
+    columns.map { { path: _1.to_s, source: 'dossier', libelle: columns_meta[_1][:libelle] } }
+  end
+
+  def all_usager_column_keys
+    column_keys = []
+    column_keys.push :id, :email, :france_connecte
+    if procedure.for_individual?
+      column_keys.push :civilite, :last_name, :first_name, :for_tiers, :mandataire_last_name, :mandataire_first_name
+    else
+      column_keys.push(
+        :etablissement_siret, :etablissement_siege_social, :etablissement_naf, :etablissement_libelle_naf,
+        :etablissement_adresse, :etablissement_numero_voie, :etablissement_type_voie, :etablissement_nom_voie, :etablissement_complement_adresse, :etablissement_code_postal, :etablissement_localite, :etablissement_code_insee_localite,
+        :entreprise_siren, :entreprise_capital_social, :entreprise_numero_tva_intracommunautaire,
+        :entreprise_forme_juridique, :entreprise_forme_juridique_code,
+        :entreprise_nom_commercial, :entreprise_raison_sociale,
+        :entreprise_siret_siege_social,
+        :entreprise_code_effectif_entreprise
+      )
+    end
+
+    if procedure.chorusable? && procedure.chorus_configuration.complete?
+      column_keys.push(:domaine_fonctionnel, :referentiel_prog, :centre_de_cout)
+    end
+
+    column_keys
+  end
+
+  def all_dossier_column_keys
+    column_keys = []
+    column_keys.push(
+      :archived, :dossier_state, :updated_at, :last_champ_updated_at,
+      :depose_at, :en_instruction_at,
+      procedure.sva_svr_enabled? ? :sva_svr_decision_on : nil,
+      :processed_at,
+      :motivation,
+      :instructeurs,
+      procedure.routing_enabled? ? :groupe_instructeur : nil
+    ).compact
+  end
+
+  def columns_meta
+    {
+      id: { libelle: "ID", get_value: -> (d) { d.id.to_s } },
+      email: { libelle: "Email", get_value: -> (d) { d.user_email_for(:display) } },
+      france_connecte: { libelle: 'FranceConnect ?', get_value: -> (d) { d.user_from_france_connect? } },
+      civilite: { libelle: "Civilité", get_value: -> (d) { d.individual&.gender } },
+      last_name: { libelle: 'Nom', get_value: -> (d) { d.individual&.nom } },
+      first_name: { libelle: 'Prénom', get_value: -> (d) { d.individual&.prenom } },
+      for_tiers: { libelle: "Dépôt pour un tiers", get_value: -> (d) { d.for_tiers } },
+      mandataire_last_name: { libelle: 'Nom du mandataire', get_value: -> (d) { d.mandataire_last_name } },
+      mandataire_first_name: { libelle: 'Prénom du mandataire', get_value: -> (d) { d.mandataire_first_name } },
+      date_de_naissance: { libelle: 'Date de naissance', get_value: -> (d) { d.individual&.birthdate } },
+      etablissement_siret: { libelle: 'Établissement SIRET', get_value: -> (d) { d.etablissement&.siret } },
+      etablissement_siege_social: { libelle: 'Établissement siège social', get_value: -> (d) { d.etablissement&.siege_social } },
+      etablissement_naf: { libelle: 'Établissement NAF', get_value: -> (d) { d.etablissement&.naf } },
+      etablissement_libelle_naf: { libelle: 'Établissement libellé NAF', get_value: -> (d) { d.etablissement&.libelle_naf } },
+      etablissement_adresse: { libelle: 'Établissement Adresse', get_value: -> (d) { d.etablissement&.adresse } },
+      etablissement_numero_voie: { libelle: 'Établissement numero voie', get_value: -> (d) { d.etablissement&.numero_voie } },
+      etablissement_type_voie: { libelle: 'Établissement type voie', get_value: -> (d) { d.etablissement&.type_voie } },
+      etablissement_nom_voie: { libelle: 'Établissement nom voie', get_value: -> (d) { d.etablissement&.nom_voie } },
+      etablissement_complement_adresse: { libelle: 'Établissement complément adresse', get_value: -> (d) { d.etablissement&.complement_adresse } },
+      etablissement_code_postal: { libelle: 'Établissement code postal', get_value: -> (d) { d.etablissement&.code_postal } },
+      etablissement_localite: { libelle: 'Établissement localité', get_value: -> (d) { d.etablissement&.localite } },
+      etablissement_code_insee_localite: { libelle: 'Établissement code INSEE localité', get_value: -> (_d) { etablissement&.code_insee_localite } },
+      entreprise_siren: { libelle: 'Entreprise SIREN', get_value: -> (d) { d.etablissement&.entreprise_siren } },
+      entreprise_capital_social: { libelle: 'Entreprise capital social', get_value: -> (d) { d.etablissement&.entreprise_capital_social } },
+      entreprise_numero_tva_intracommunautaire: { libelle: 'Entreprise numero TVA intracommunautaire', get_value: -> (d) { d.etablissement&.entreprise_numero_tva_intracommunautaire } },
+      entreprise_forme_juridique: { libelle: 'Entreprise forme juridique', get_value: -> (d) { d.etablissement&.entreprise_forme_juridique } },
+      entreprise_forme_juridique_code: { libelle: 'Entreprise forme juridique code', get_value: -> (d) { d.etablissement&.entreprise_forme_juridique_code } },
+      entreprise_nom_commercial: { libelle: 'Entreprise nom commercial', get_value: -> (d) { d.etablissement&.entreprise_nom_commercial } },
+      entreprise_raison_sociale: { libelle: 'Entreprise raison sociale', get_value: -> (d) { d.etablissement&.entreprise_raison_sociale } },
+      entreprise_siret_siege_social: { libelle: 'Entreprise SIRET siège social', get_value: -> (d) { d.etablissement&.entreprise_siret_siege_social } },
+      entreprise_code_effectif_entreprise: { libelle: 'Entreprise code effectif entreprise', get_value: -> (d) { d.etablissement&.entreprise_code_effectif_entreprise } },
+      entreprise_date_creation: { libelle: 'Entreprise date de création', get_value: -> (d) { d.etablissement&.entreprise_date_creation } },
+      entreprise_etat_administratif: { libelle: 'Entreprise état administratif', get_value: -> (d) { d.etablissement&.entreprise_etat_administratif } },
+      entreprise_nom: { libelle: 'Entreprise nom', get_value: -> (d) { d.etablissement&.entreprise_nom } },
+      entreprise_prenom: { libelle: 'Entreprise prénom', get_value: -> (d) { d.etablissement&.entreprise_prenom } },
+      association_rna: { libelle: 'Association RNA', get_value: -> (d) { d.etablissement&.association_rna } },
+      association_titre: { libelle: 'Association titre', get_value: -> (d) { d.etablissement&.association_titre } },
+      association_objet: { libelle: 'Association objet', get_value: -> (d) { d.etablissement&.association_objet } },
+      association_date_creation: { libelle: 'Association date de création', get_value: -> (d) { d.etablissement&.association_date_creation } },
+      association_date_declaration: { libelle: 'Association date de déclaration', get_value: -> (d) { d.etablissement&.association_date_declaration } },
+      association_date_publication: { libelle: 'Association date de publication', get_value: -> (d) { d.etablissement&.association_date_publication } },
+      domaine_fonctionnel: { libelle: 'Domaine Fonctionnel', get_value: -> (d) { d.procedure.chorus_configuration.domaine_fonctionnel&.fetch("code") { '' } } },
+      referentiel_prog: { libelle: 'Référentiel De Programmation', get_value: -> (d) { d.procedure.chorus_configuration.referentiel_de_programmation&.fetch("code") { '' } } },
+      centre_de_cout: { libelle: 'Centre De Coût', get_value: -> (d) { d.procedure.chorus_configuration.centre_de_cout&.fetch("code") { '' } } },
+      archived: { libelle: 'Archivé', get_value: -> (d) { d.archived } },
+      dossier_state: { libelle: 'État du dossier', get_value: -> (d) { Dossier.human_attribute_name("state.#{d.state}") } },
+      updated_at: { libelle: 'Dernière mise à jour le', get_value: -> (d) { d.updated_at } },
+      last_champ_updated_at: { libelle: 'Dernière mise à jour du dossier le', get_value: -> (d) { d.last_champ_updated_at } },
+      depose_at: { libelle: 'Déposé le', get_value: -> (d) { d.depose_at } },
+      en_instruction_at: { libelle: 'Passé en instruction le', get_value: -> (d) { d.en_instruction_at } },
+      sva_svr_decision_on: { libelle: "Date décision #{procedure.sva_svr_configuration.human_decision}", get_value: -> (d) { d.sva_svr_decision_on } },
+      processed_at: { libelle: 'Traité le', get_value: -> (d) { d.processed_at } },
+      motivation: { libelle: 'Motivation de la décision', get_value: -> (d) { d.motivation } },
+      instructeurs: { libelle: 'Instructeurs', get_value: -> (d) { d.followers_instructeurs.map(&:email).join(' ') } },
+      groupe_instructeur: { libelle: 'Groupe instructeur', get_value: -> (d) { d.groupe_instructeur.label } }
+    }
   end
 end
---
+++ spec/models/export_template_tabular_spec.rb
@@ -0,0 +1,252 @@
+describe ExportTemplate do
+  let(:groupe_instructeur) { create(:groupe_instructeur, procedure:) }
+  let(:export_template) { build(:export_template, kind: 'csv', groupe_instructeur:) }
+  let(:tabular_export_template) { build(:tabular_export_template, groupe_instructeur:) }
+  let(:procedure) { create(:procedure_with_dossiers, :published, types_de_champ_public:, for_individual:) }
+  let(:for_individual) { true }
+  let(:types_de_champ_public) do
+    [
+      { type: :text, libelle: "Ca va ?", mandatory: true, stable_id: 1 },
+      { type: :communes, libelle: "Commune", mandatory: true, stable_id: 17 },
+      { type: :siret, libelle: 'siret', stable_id: 20 },
+      { type: :repetition, mandatory: true, stable_id: 7, libelle: "Champ répétable", children: [{ type: 'text', libelle: 'Qqchose à rajouter?', stable_id: 8 }] }
+    ]
+  end
+  let(:dossier) { procedure.dossiers.first }
+
+  describe '#columns=' do
+    let(:columns) {
+  [
+    { :path => "id", :source => "dossier" },
+    { :path => "email", :source => "dossier" },
+    { :path => "archived", :source => "dossier" },
+    { :path => "dossier_state", :source => "dossier" },
+    { :path => "value", :source => "tdc", :stable_id => 1 },
+    { :path => "value", :source => "tdc", :stable_id => 17 },
+    { :path => "code", :source => "tdc", :stable_id => 17 },
+    { :path => "value", :repetition_champ_stable_id => 7, :source => "repet", :stable_id => 8 }
+  ]
+}
+
+    it 'update columns when assiging columns' do
+      export_template.columns = columns
+      expect(export_template.columns).to match_array [
+        { :libelle => "ID", :path => "id", :source => "dossier" },
+        { :libelle => "Email", :path => "email", :source => "dossier" },
+        { :libelle => "Archivé", :path => "archived", :source => "dossier" },
+        { :libelle => "État du dossier", :path => "dossier_state", :source => "dossier" },
+        { :libelle => "Ca va ?", :path => "value", :source => "tdc", :stable_id => 1 },
+        { :libelle => "Commune", :path => "value", :source => "tdc", :stable_id => 17 },
+        { :libelle => "Commune (Code INSEE)", :path => "code", :source => "tdc", :stable_id => 17 },
+        { :libelle => "Qqchose à rajouter?", :path => "value", :repetition_champ_stable_id => 7, :source => "repet", :stable_id => 8 }
+      ]
+    end
+
+    context 'when there is a previous revision with a renamed tdc' do
+
+      let(:previous_tdc) { procedure.published_revision.types_de_champ_public.find_by(stable_id: 1) }
+      let(:changed_tdc) { { libelle: "Ca roule ?" } }
+
+      context 'with already column in export template' do
+        before do
+          export_template.columns = columns
+          type_de_champ = procedure.draft_revision.find_and_ensure_exclusive_use(previous_tdc.stable_id)
+          type_de_champ.update(changed_tdc)
+          procedure.publish_revision!
+          export_template.columns = columns
+        end
+
+        it 'update columns with original libelle for champs with new revision' do
+          expect(export_template.columns.find { _1[:stable_id] == 1 }).to eq({ :libelle => "Ca va ?", :path => "value", :source => "tdc", :stable_id => 1 })
+        end
+      end
+
+      context 'without columns in export template' do
+        before do
+          type_de_champ = procedure.draft_revision.find_and_ensure_exclusive_use(previous_tdc.stable_id)
+          type_de_champ.update(changed_tdc)
+          procedure.publish_revision!
+          export_template.columns = columns
+        end
+
+        it 'update columns with new libelle for champs with new revision' do
+          expect(export_template.columns.find { _1[:stable_id] == 1 }).to eq({ :libelle => "Ca roule ?", :path => "value", :source => "tdc", :stable_id => 1 })
+        end
+      end
+    end
+    it 'ignores columns when invalid stable_id' do
+      export_template.columns = [{ :path => "value", :source => "tdc", :stable_id => 987 }]
+      expect(export_template.columns).to match_array []
+    end
+
+    it 'raises when invalid path' do
+      expect { export_template.columns = ['blabla'] }.to raise_exception
+    end
+  end
+
+  describe '#all_tdc_columns' do
+    it "returns all tdc columns (without repetition) based upon procedure's type de champs" do
+      expect(export_template.all_tdc_columns).to match_array [
+        [{ :source => "tdc", :stable_id => 1, :path => "value", :libelle => "Ca va ?" }],
+        [
+          { :source => "tdc", :stable_id => 17, :path => "value", :libelle => "Commune" },
+          { :source => "tdc", :stable_id => 17, :path => "code", :libelle => "Commune (Code INSEE)" },
+          { :source => "tdc", :stable_id => 17, :path => "departement", :libelle => "Commune (Département)" }
+        ],
+        [{ :source => "tdc", :stable_id => 20, :path => "value", :libelle => "siret" }]
+      ]
+    end
+  end
+
+  describe '#all_repetable_tdc_columns' do
+    it "returns all repetable columns based upon procedure's type de champs" do
+      expect(export_template.all_repetable_tdc_columns).to match_array [
+        {
+          :libelle => "Champ répétable",
+         :types_de_champ =>  [
+           [
+             {
+               :source => "repet",
+              :repetition_champ_stable_id => 7,
+              :path => "value",
+              :stable_id => 8,
+              :libelle => "Qqchose à rajouter?"
+             }
+           ]
+         ]
+        }
+      ]
+    end
+  end
+
+  describe '#all_usager_columns' do
+    context 'for individual procedure' do
+      let(:for_individual) { true }
+
+      it "returns all usager columns" do
+        expect(export_template.all_usager_columns).to match_array [
+          { :path => "id", :source => "dossier", :libelle => "ID" },
+          { :path => "email", :source => "dossier", :libelle => "Email" },
+          { :path => "france_connecte", :source => "dossier", :libelle => "FranceConnect ?" },
+          { :path => "civilite", :source => "dossier", :libelle => "Civilité" },
+          { :path => "last_name", :source => "dossier", :libelle => "Nom" },
+          { :path => "first_name", :source => "dossier", :libelle => "Prénom" },
+          { :path => "for_tiers", :source => "dossier", :libelle => "Dépôt pour un tiers" },
+          { :path => "mandataire_last_name", :source => "dossier", :libelle => "Nom du mandataire" },
+          { :path => "mandataire_first_name", :source => "dossier", :libelle => "Prénom du mandataire" }
+        ]
+
+        expect(export_template.all_usager_columns.any? { _1[:path] == "etablissement_siret" }).to eq false
+      end
+    end
+
+    context 'for entreprise procedure' do
+      let(:for_individual) { false }
+
+      it "returns all usager columns" do
+        expect(export_template.all_usager_columns).to match_array [
+          { :path => "id", :source => "dossier", :libelle => "ID" },
+          { :path => "email", :source => "dossier", :libelle => "Email" },
+          { :path => "france_connecte", :source => "dossier", :libelle => "FranceConnect ?" },
+          { :path => "etablissement_siret", :source => "dossier", :libelle => "Établissement SIRET" },
+          { :path => "etablissement_siege_social", :source => "dossier", :libelle => "Établissement siège social" },
+          { :path => "etablissement_naf", :source => "dossier", :libelle => "Établissement NAF" },
+          { :path => "etablissement_libelle_naf", :source => "dossier", :libelle => "Établissement libellé NAF" },
+          { :path => "etablissement_adresse", :source => "dossier", :libelle => "Établissement Adresse" },
+          { :path => "etablissement_numero_voie", :source => "dossier", :libelle => "Établissement numero voie" },
+          { :path => "etablissement_type_voie", :source => "dossier", :libelle => "Établissement type voie" },
+          { :path => "etablissement_nom_voie", :source => "dossier", :libelle => "Établissement nom voie" },
+          { :path => "etablissement_complement_adresse", :source => "dossier", :libelle => "Établissement complément adresse" },
+          { :path => "etablissement_code_postal", :source => "dossier", :libelle => "Établissement code postal" },
+          { :path => "etablissement_localite", :source => "dossier", :libelle => "Établissement localité" },
+          { :path => "etablissement_code_insee_localite", :source => "dossier", :libelle => "Établissement code INSEE localité" },
+          { :path => "entreprise_siren", :source => "dossier", :libelle => "Entreprise SIREN" },
+          { :path => "entreprise_capital_social", :source => "dossier", :libelle => "Entreprise capital social" },
+          { :path => "entreprise_numero_tva_intracommunautaire", :source => "dossier", :libelle => "Entreprise numero TVA intracommunautaire" },
+          { :path => "entreprise_forme_juridique", :source => "dossier", :libelle => "Entreprise forme juridique" },
+          { :path => "entreprise_forme_juridique_code", :source => "dossier", :libelle => "Entreprise forme juridique code" },
+          { :path => "entreprise_nom_commercial", :source => "dossier", :libelle => "Entreprise nom commercial" },
+          { :path => "entreprise_raison_sociale", :source => "dossier", :libelle => "Entreprise raison sociale" },
+          { :path => "entreprise_siret_siege_social", :source => "dossier", :libelle => "Entreprise SIRET siège social" },
+          { :path => "entreprise_code_effectif_entreprise", :source => "dossier", :libelle => "Entreprise code effectif entreprise" }
+        ]
+
+        expect(export_template.all_usager_columns.any? { _1[:path] == "first_name" }).to eq false
+      end
+    end
+
+    context 'when procedure chorusable' do
+      before { expect_any_instance_of(Procedure).to receive(:chorusable?).and_return(true) }
+      let(:procedure) { create(:procedure_with_dossiers, :filled_chorus, types_de_champ_public:) }
+      it 'returns specific chorus columns' do
+        allow(Procedure).to receive(:chorusable?).and_return(true)
+        expect(export_template.all_usager_columns.include?({ :path => "domaine_fonctionnel", :source => "dossier", :libelle => "Domaine Fonctionnel" })).to be true
+      end
+    end
+  end
+
+  describe '#all_dossier_columns' do
+    it "returns all dossier columns" do
+      expect(export_template.all_dossier_columns).to match_array [
+        { :path => "archived", :source => "dossier", :libelle => "Archivé" },
+        { :path => "dossier_state", :source => "dossier", :libelle => "État du dossier" },
+        { :path => "updated_at", :source => "dossier", :libelle => "Dernière mise à jour le" },
+        { :path => "last_champ_updated_at", :source => "dossier", :libelle => "Dernière mise à jour du dossier le" },
+        { :path => "depose_at", :source => "dossier", :libelle => "Déposé le" },
+        { :path => "en_instruction_at", :source => "dossier", :libelle => "Passé en instruction le" },
+        { :path => "processed_at", :source => "dossier", :libelle => "Traité le" },
+        { :path => "motivation", :source => "dossier", :libelle => "Motivation de la décision" },
+        { :path => "instructeurs", :source => "dossier", :libelle => "Instructeurs" },
+        { :path => "groupe_instructeur", :source => "dossier", :libelle => "Groupe instructeur" }
+      ]
+    end
+  end
+
+  describe '#columns and #repetable columns' do
+    let(:tabular_export_template) { create(:export_template, kind: 'ods', content:, groupe_instructeur: ) }
+    let(:content) {
+      {
+        "columns" => [
+          { :path => "email", :source => "dossier", :libelle => "Email" },
+          { :path => "value", :source => "tdc", :libelle => "Ca va ?", "stable_id" => 1 },
+          { :path => "code", :source => "tdc", :libelle => "Commune", "stable_id" => 2 },
+          { :path => "value", :source => "repet", :libelle => "PJ répétable", "stable_id" => 4, "repetition_champ_stable_id" => 3 },
+          { :path => "value", :source => "repet", :libelle => "Champ repetable", "stable_id" => 5, "repetition_champ_stable_id" => 3 },
+          { :path => "value", :source => "repet", :libelle => "PJ", "stable_id" => 7, "repetition_champ_stable_id" => 6 }
+        ]
+      }
+    }
+
+    describe '#columns' do
+      it 'returns all columns stored in export template' do
+        expect(tabular_export_template.columns).to match_array [
+          { :path => "email", :source => "dossier", :libelle => "Email" },
+          { :path => "value", :source => "tdc", :libelle => "Ca va ?", :stable_id => 1 },
+          { :path => "code", :source => "tdc", :libelle => "Commune", :stable_id => 2 },
+          { :path => "value", :source => "repet", :libelle => "PJ répétable", :stable_id => 4, :repetition_champ_stable_id => 3 },
+          { :path => "value", :source => "repet", :libelle => "Champ repetable", :stable_id => 5, :repetition_champ_stable_id => 3 },
+          { :path => "value", :source => "repet", :libelle => "PJ", :stable_id => 7, :repetition_champ_stable_id => 6 }
+        ]
+      end
+    end
+
+    describe '#repetable_columns' do
+      it 'returns repetable columns stored in export template grouped by repetition champ' do
+        expect(tabular_export_template.repetable_columns).to eq(
+          {
+            3 => [
+              { :path => "value", :source => "repet", :libelle => "PJ répétable", :stable_id => 4, :repetition_champ_stable_id => 3 },
+              { :path => "value", :source => "repet", :libelle => "Champ repetable", :stable_id => 5, :repetition_champ_stable_id => 3 }
+            ],
+            6 => [
+              { :path => "value", :source => "repet", :libelle => "PJ", :stable_id => 7, :repetition_champ_stable_id => 6 }
+            ]
+          }
+        )
+      end
+    end
+  end
+
+  describe '#all_usager_columns' do
+  end
+end
