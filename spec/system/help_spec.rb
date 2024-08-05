e358f196e92d796877c0b24c7d841c68b22db264
Corinne Durrmeyer <corinne@inseo.fr>
2024-08-05 15:41:08 +0200

    Change help__content to help-content to conform linting Regex

--- spec/system/help_spec.rb
+++ spec/system/help_spec.rb
@@ -16,7 +16,7 @@ describe 'Getting help:' do
         expect(page).to have_help_menu
       end
 
-      within('.help__content') do
+      within('.help-content') do
         expect(page).to have_content(procedure.service.email)
         expect(page).to have_content(procedure.service.telephone)
         expect(page).to have_link(nil, href: I18n.t("links.common.faq.url"))
@@ -49,7 +49,7 @@ describe 'Getting help:' do
           expect(page).to have_help_menu
         end
 
-        within('.help__content') do
+        within('.help-content') do
           expect(page).to have_content(dossier.procedure.service.email)
           expect(page).to have_content(dossier.procedure.service.telephone)
           expect(page).to have_link(nil, href: I18n.t("links.common.faq.url"))
@@ -67,7 +67,7 @@ describe 'Getting help:' do
           expect(page).to have_help_menu
         end
 
-        within('.help__content') do
+        within('.help-content') do
           expect(page).to have_link(nil, href: messagerie_dossier_path(dossier))
           expect(page).to have_link(nil, href: I18n.t("links.common.faq.url"))
         end
