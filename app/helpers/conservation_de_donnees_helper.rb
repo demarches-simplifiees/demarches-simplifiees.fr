acdd259e1d9c50df13e148c97f5f2753c65bb123
Corinne Durrmeyer <corinne@inseo.fr>
2024-08-07 16:13:54 +0200

    Uniform title with link content

--- app/helpers/conservation_de_donnees_helper.rb
+++ app/helpers/conservation_de_donnees_helper.rb
@@ -10,7 +10,6 @@ module ConservationDeDonneesHelper
   def conservation_dans_ds(procedure)
     if procedure.duree_conservation_dossiers_dans_ds.present?
       I18n.t('users.procedure_footer.legals.data_retention',
-             application_name: Current.application_name,
              duree_conservation_dossiers_dans_ds: procedure.duree_conservation_dossiers_dans_ds)
     end
   end
