6cd91015cff9c2036d55466288de82a66d72de33
Corinne Durrmeyer <corinne@inseo.fr>
2024-08-07 17:39:13 +0200

    Update spec file

--- spec/helpers/conservation_de_donnees_helper_spec.rb
+++ spec/helpers/conservation_de_donnees_helper_spec.rb
@@ -4,9 +4,7 @@ RSpec.describe ConservationDeDonneesHelper, type: :helper do
   let(:procedure) { build(:procedure, duree_conservation_dossiers_dans_ds: dans_ds) }
 
   describe "politiques_conservation_de_donnees" do
-    let(:app_name) { "une superbe application" }
-
-    before { Current.application_name = app_name }
+    let(:label) { "Rétention et archivage des données" }
 
     subject { politiques_conservation_de_donnees(procedure) }
 
@@ -14,7 +12,7 @@ RSpec.describe ConservationDeDonneesHelper, type: :helper do
       let(:dans_ds) { 3 }
       let(:hors_ds) { 6 }
 
-      it { is_expected.to eq(["#{app_name} : 3 mois"]) }
+      it { is_expected.to eq(["#{label} : 3 mois"]) }
     end
 
     context "when the retention time is not set" do
