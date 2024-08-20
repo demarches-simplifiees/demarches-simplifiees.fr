92ffd69bf1a85155a8f30dbceeab57dfbbca6092
mfo <mfo@users.noreply.github.com>
2024-08-20 11:37:21 +0200

    doc(instance): explicite quand/comment faire tourner ces taches

--- app/tasks/maintenance/populate_siret_value_json_task.rb
+++ app/tasks/maintenance/populate_siret_value_json_task.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+# Dans le cadre de la story pour pouvoir rechercher un dossier en fonction des valeurs des champs branchées sur une API, voici une première pièce qui cible les champs RNA/RNF/SIRET (notamment les adresses pour de la recherche). Cette PR intègre :
+#     la normalisation des adresses des champs RNA/RNF/SIRET
+#     le fait de stocker ces données normalisées dans le champs.value_json (un jsonb)
+#     le backfill les anciens champs RNA/RNF/SIRET
 module Maintenance
   class PopulateSiretValueJSONTask < MaintenanceTasks::Task
     def collection
