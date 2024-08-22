ff88a0a2a6c982917b86006b43ffdeaa17e05c40
Kara Diaby <kdiaby.pro@gmail.com>
2024-08-22 21:13:41 +0000

    Fix add requested_email column to france_connect_information table Lint error

--- db/migrate/20240730130933_add_alternative_email_column_to_france_connect_information_table.rb
+++ db/migrate/20240730130933_add_alternative_email_column_to_france_connect_information_table.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddAlternativeEmailColumnToFranceConnectInformationTable < ActiveRecord::Migration[7.0]
   def change
     add_column :france_connect_informations, :requested_email, :string
