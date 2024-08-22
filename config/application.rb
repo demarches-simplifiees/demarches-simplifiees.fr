5859ea4a23c58e18f5b03a509fe64ec7ede80284
Colin Darie <colin@darie.eu>
2024-08-22 17:37:09 +0200

    chore: load rails 7.0 defaults

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ Dotenv::Railtie.load
 module TPS
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
