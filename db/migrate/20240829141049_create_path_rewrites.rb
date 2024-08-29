516205efd949173f62345e00d7bafaee4a77acfe
simon lehericey <mail@simon.lehericey.net>
2024-08-29 16:15:07 +0200

    add path_rewrite model

---
+++ db/migrate/20240829141049_create_path_rewrites.rb
@@ -0,0 +1,11 @@
+class CreatePathRewrites < ActiveRecord::Migration[7.0]
+  def change
+    create_table :path_rewrites do |t|
+      t.string :from, null: false
+      t.string :to, null: false
+
+      t.timestamps
+    end
+    add_index :path_rewrites, :from, unique: true
+  end
+end
