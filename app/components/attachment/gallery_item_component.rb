30a48459a9cfaaadd24b3c2263cec3524ce8b82f
Eric Leroy-Terquem <eric.leroyterquem@beta.gouv.fr>
2024-09-09 09:48:45 +0200

    refactor(gallery): extract gallery_link method

--- app/components/attachment/gallery_item_component.rb
+++ app/components/attachment/gallery_item_component.rb
@@ -19,4 +19,16 @@ class Attachment::GalleryItemComponent < ApplicationComponent
   def title
     "#{libelle} -- #{sanitize(blob.filename.to_s)}"
   end
+
+  def gallery_link(blob, &block)
+    if displayable_image?(blob)
+      link_to image_url(blob_url(attachment)), title: title, data: { src: blob.url }, class: 'gallery-link' do
+        yield
+      end
+    elsif displayable_pdf?(blob)
+      link_to blob.url, id: blob.id, data: { iframe: true, src: blob.url }, class: 'gallery-link', type: blob.content_type, title: title do
+        yield
+      end
+    end
+  end
 end
