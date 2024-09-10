6af07dd721895f149a8ecc7d03c275f30f3d4162
mfo <mfo@users.noreply.github.com>
2024-09-10 18:16:16 +0200

    data(fix): backfill invalid procedure presentation filters having a value greater than pg max integer

---
+++ app/tasks/maintenance/clean_invalid_procedure_presentation_task.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module Maintenance
+  # PR: 10774
+  # why: postgres does not support integer greater than ProcedurePresentation::PG_INTEGER_MAX_VALUE)
+  #      it occures when user copypaste the dossier id twice (like missed copy paste,paste)
+  #      once this huge integer is saved on procedure presentation, page with this filter can't be loaded
+  # when: run this migration when it appears in your maintenance tasks list, this file fix the data and we added some validations too
+  class CleanInvalidProcedurePresentationTask < MaintenanceTasks::Task
+    def collection
+      ProcedurePresentation.all
+    end
+
+    def process(element)
+      element.filters = element.filters.transform_values do |filters_by_status|
+        filters_by_status.reject do |filter|
+          filter.is_a?(Hash) &&
+          filter['column'] == 'id' &&
+          (filter['value']&.to_i&. >= ProcedurePresentation::PG_INTEGER_MAX_VALUE)
+        end
+      end
+      element.save
+    end
+
+    def count
+      # Optionally, define the number of rows that will be iterated over
+      # This is used to track the task's progress
+    end
+  end
+end
--- spec/models/procedure_presentation_spec.rb
+++ spec/models/procedure_presentation_spec.rb
@@ -57,8 +57,8 @@ describe ProcedurePresentation do
       it { expect(build(:procedure_presentation, filters: { "suivis" => [{ table: "user", column: "email", "value" => "exceedingly long filter value" * 10 }] })).to be_invalid }
 
       describe 'check_filters_max_integer' do
-        it { expect(build(:procedure_presentation, filters: { "suivis" => [{ table: "self", column: "id", "value" => 2147483647.to_s }] })).to be_invalid }
-        it { expect(build(:procedure_presentation, filters: { "suivis" => [{ table: "self", column: "id", "value" => (2147483647 - 1).to_s }] })).to be_valid }
+        it { expect(build(:procedure_presentation, filters: { "suivis" => [{ table: "self", column: "id", "value" => ProcedurePresentation::PG_INTEGER_MAX_VALUE.to_s }] })).to be_invalid }
+        it { expect(build(:procedure_presentation, filters: { "suivis" => [{ table: "self", column: "id", "value" => (ProcedurePresentation::PG_INTEGER_MAX_VALUE - 1).to_s }] })).to be_valid }
       end
     end
   end
