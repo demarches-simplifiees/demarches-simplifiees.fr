ce22adf8119e76d468008a1cb3204bd0679b5288
Mathieu Magnin <mathieu@magnin.sh>
2024-09-13 16:47:31 +0200

    [#10798] Indicate in manager if api_entreprise_token is present / valid / expired

---
+++ app/fields/jwt_field.rb
@@ -0,0 +1,22 @@
+require "administrate/field/base"
+
+class JwtField < Administrate::Field::Base
+  def to_s
+    if data.present?
+      begin
+        decoded_token = JWT.decode(data, nil, false)
+        expiration = Time.zone.at(decoded_token[0]['exp'])
+
+        if expiration < Time.zone.now
+          "Token présent, expiré le #{expiration.strftime('%d/%m/%Y à %H:%M')}"
+        else
+          "Token présent, expirera le #{expiration.strftime('%d/%m/%Y à %H:%M')}"
+        end
+      rescue JWT::DecodeError => e
+        "Token invalide : #{e.message}"
+      end
+    else
+      "Pas de token"
+    end
+  end
+end
