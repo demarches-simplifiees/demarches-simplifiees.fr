3e20187de5e7ac8f5e48f350362450ad166ddc99
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2024-09-22 14:25:01 -1000

    Maintenance task to examine dossier champs

--- app/tasks/maintenance/dossier_attributes_task.rb
+++ app/tasks/maintenance/dossier_attributes_task.rb
@@ -3,22 +3,22 @@
 module Maintenance
   class DossierAttributesTask < MaintenanceTasks::Task
     attribute :dossier, default: ""
+    no_collection
 
-    def collection
+    def process
       Champ.private_only.where(dossier:)
         .joins(type_de_champ: :revision_types_de_champ)
         .left_joins(parent: { type_de_champ: :revision_types_de_champ })
-        .includes(:type_de_champ)
+        .includes(type_de_champ: :revision_type_de_champ)
         .order(Arel.sql("coalesce(revision_types_de_champ_types_de_champ.position, procedure_revision_types_de_champ.position)," +
                           " COALESCE(champs.row_id,' '), procedure_revision_types_de_champ.position"))
-    end
-
-    def process(champ)
-      Rails.logger.info("#{champ.dossier_id}:" +
-                          (champ.parent_id ? champ.parent.libelle + '.' : '').to_s +
-                          "#{champ.libelle}=#{champ.value}" +
-                          ":#{champ.type_de_champ.revision_type_de_champ.revision_id}" +
-                          ":#{champ.type_de_champ.revision_type_de_champ.id}")
+        .each do |champ|
+        Rails.logger.info("#{champ.dossier_id}:" +
+                            (champ.parent_id ? champ.parent.libelle + '.' : '').to_s +
+                            "#{champ.libelle}=#{champ.value}" +
+                            ":#{champ.type_de_champ.revision_type_de_champ.revision_id}" +
+                            ":#{champ.type_de_champ.revision_type_de_champ.id}")
+      end
     end
   end
 end
