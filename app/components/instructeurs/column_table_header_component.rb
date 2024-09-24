b582a2afc60722f14ca728f31bc4f6e85a848aca
simon lehericey <mail@simon.lehericey.net>
2024-09-24 15:05:25 +0200

    column_table_header_component use sorted_column

--- app/components/instructeurs/column_table_header_component.rb
+++ app/components/instructeurs/column_table_header_component.rb
@@ -1,47 +1,21 @@
 # frozen_string_literal: true
 
 class Instructeurs::ColumnTableHeaderComponent < ApplicationComponent
-  attr_reader :procedure_presentation, :column
-  # maybe extract a ColumnSorter class?
-  #
-
-  def initialize(procedure_presentation:, column:)
+  def initialize(procedure_presentation:)
     @procedure_presentation = procedure_presentation
-    @column = column
+    @columns = procedure_presentation.displayed_fields_for_headers
+    @sorted_column = procedure_presentation.sorted_column
   end
 
-  def column_id
-    column.id
+  def label_and_arrow(column)
+    return column.label if @sorted_column.column != column
+
+    @sorted_column.ascending? ? "#{column.label} ↑" : "#{column.label} ↓"
   end
 
-  def sorted_by_current_column?
-    procedure_presentation.sort['table'] == column.table &&
-    procedure_presentation.sort['column'] == column.column
-  end
+  def aria_sort(column)
+    return {} if @sorted_column.column != column
 
-  def sorted_ascending?
-    current_sort_order == 'asc'
-  end
-
-  def sorted_descending?
-    current_sort_order == 'desc'
-  end
-
-  def aria_sort
-    if sorted_by_current_column?
-      if sorted_ascending?
-        { "aria-sort": "ascending" }
-      elsif sorted_descending?
-        { "aria-sort": "descending" }
-      end
-    else
-      {}
-    end
-  end
-
-  private
-
-  def current_sort_order
-    procedure_presentation.sort['order']
+    @sorted_column.ascending? ? { "aria-sort": "ascending" } : { "aria-sort": "descending" }
   end
 end
