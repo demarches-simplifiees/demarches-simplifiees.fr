402346379fc8596a41fe205d5157fabe85a16794
mfo <mfo@users.noreply.github.com>
2024-09-24 09:14:30 +0200

    fix(attestations): ensure to use v1 link

--- app/components/procedure/card/attestation_component.rb
+++ app/components/procedure/card/attestation_component.rb
@@ -8,7 +8,7 @@ class Procedure::Card::AttestationComponent < ApplicationComponent
   private
 
   def edit_attestation_path
-    if @procedure.attestation_templates_v2.any? || @procedure.feature_enabled?(:attestation_v2)
+    if v2?
       helpers.edit_admin_procedure_attestation_template_v2_path(@procedure)
     else
       helpers.edit_admin_procedure_attestation_template_path(@procedure)
@@ -17,5 +17,11 @@ class Procedure::Card::AttestationComponent < ApplicationComponent
 
   def error_messages
     @procedure.errors.messages_for(:attestation_template).to_sentence
+  end
+
+  def v2?
+    return false if @procedure.attestation_template.published?
+
+    @procedure.attestation_templates_v2.any? || @procedure.feature_enabled?(:attestation_v2)
   end
 end
