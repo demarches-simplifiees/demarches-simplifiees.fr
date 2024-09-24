e58c81f5850a9b2556fae8ebe4d0319f5c832dee
Mathieu Magnin <mathieu@magnin.sh>
2024-09-24 18:33:35 +0200

    [#10799] Handle the case when api_entreprise_token is not nil then set to nil

--- spec/models/procedure_spec.rb
+++ spec/models/procedure_spec.rb
@@ -1866,7 +1866,7 @@ describe Procedure do
   end
 
   describe '#set_api_entreprise_token_expires_at (before_save)' do
-    let(:procedure) { create(:procedure) }
+    let(:procedure) { create(:procedure, api_entreprise_token: initial_api_entreprise_token) }
 
     before do
       procedure.api_entreprise_token = api_entreprise_token
@@ -1874,29 +1874,44 @@ describe Procedure do
 
     subject { procedure.save }
 
-    context 'when the api_entreprise_token is nil' do
-      let(:api_entreprise_token) { nil }
+    context "when procedure had no api_entreprise_token" do
+      let(:initial_api_entreprise_token) { nil }
 
-      it 'does not set the api_entreprise_token_expires_at' do
-        expect { subject }.not_to change { procedure.api_entreprise_token_expires_at }.from(nil)
+      context 'when the api_entreprise_token is nil' do
+        let(:api_entreprise_token) { nil }
+
+        it 'does not set the api_entreprise_token_expires_at' do
+          expect { subject }.not_to change { procedure.api_entreprise_token_expires_at }.from(nil)
+        end
+      end
+
+      context 'when the api_entreprise_token is not valid' do
+        let(:api_entreprise_token) { "not a token" }
+
+        it do
+          expect { subject }.not_to change { procedure.api_entreprise_token_expires_at }.from(nil)
+        end
+      end
+
+      context 'when the api_entreprise_token is valid' do
+        let(:expiration_date) { Time.zone.now.beginning_of_minute }
+        let(:api_entreprise_token) { JWT.encode({ exp: expiration_date.to_i }, nil, 'none') }
+
+        it do
+          expect { subject }.to change { procedure.api_entreprise_token_expires_at }.from(nil).to(expiration_date)
+        end
       end
     end
 
-    context 'when the api_entreprise_token is not valid' do
-      let(:api_entreprise_token) { "not a token" }
+    context "when procedure had an api_entreprise_token" do
+      let(:initial_api_entreprise_token) { JWT.encode({ exp: 2.months.from_now.to_i }, nil, "none") }
 
-      it do
-        expect { subject }.not_to change { procedure.api_entreprise_token_expires_at }.from(nil)
-      end
-    end
+      context 'when the api_entreprise_token is set to nil' do
+        let(:api_entreprise_token) { nil }
 
-    context 'when the api_entreprise_token is valid' do
-      let(:expiration_date) { Time.zone.now.beginning_of_minute }
-      let(:api_entreprise_token) { JWT.encode({ exp: expiration_date.to_i }, nil, 'none') }
-
-      it do
-        puts "expiration_date: #{expiration_date.to_i}"
-        expect { subject }.to change { procedure.api_entreprise_token_expires_at }.from(nil).to(expiration_date)
+        it do
+          expect { subject }.to change { procedure.api_entreprise_token_expires_at }.to(nil)
+        end
       end
     end
   end
