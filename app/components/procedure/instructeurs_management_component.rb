6147f5f0f48420ca13eda34d48cc940fb68138ea
Eric Leroy-Terquem <eric.leroyterquem@beta.gouv.fr>
2024-09-25 14:36:26 +0200

    refactor(instructeurs import): extract import in a component

--- app/components/procedure/instructeurs_management_component.rb
+++ app/components/procedure/instructeurs_management_component.rb
@@ -8,20 +8,4 @@ class Procedure::InstructeursManagementComponent < ApplicationComponent
     @available_instructeur_emails = available_instructeur_emails
     @disabled_as_super_admin = disabled_as_super_admin
   end
-
-  def csv_template
-    template_path.open
-  end
-
-  def template_path
-    Rails.public_path.join('csv/import-instructeurs-test.csv')
-  end
-
-  def template_url
-    template_path.to_s
-  end
-
-  def template_detail
-    "#{File.extname(csv_template.to_path).upcase.delete_prefix('.')} – #{number_to_human_size(csv_template.size)}"
-  end
 end
