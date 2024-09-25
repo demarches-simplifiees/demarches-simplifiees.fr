ab2424b5307e5c7c60bf99787f3fe3b32675fcb8
Lisa Durand <lisa.c.durand@gmail.com>
2024-09-25 15:08:10 +0200

    create generic labels for old and new procedure

--- app/models/procedure_label.rb
+++ app/models/procedure_label.rb
@@ -3,5 +3,11 @@
 class ProcedureLabel < ApplicationRecord
   belongs_to :procedure
 
+  GENERIC_LABELS = [
+    { name: 'à relancer', color: 'brown-caramel' },
+    { name: 'complet', color: 'green-bourgeon' },
+    { name: 'prêt pour validation', color: 'green-archipel' }
+  ]
+
   validates :name, :color, presence: true
 end
