ee71a8479f7a95921471f1f5b84e499f2fca11ee
Eric Leroy-Terquem <eric.leroyterquem@beta.gouv.fr>
2024-09-25 14:36:26 +0200

    refactor(instructeurs import): extract import in a component

--- app/components/procedure/one_groupe_management_component.rb
+++ app/components/procedure/one_groupe_management_component.rb
@@ -8,20 +8,4 @@ class Procedure::OneGroupeManagementComponent < ApplicationComponent
     @groupe_instructeur = groupe_instructeur
     @procedure = revision.procedure
   end
-
-  def csv_template
-    template_path.open
-  end
-
-  def template_path
-    Rails.public_path.join('csv/import-instructeurs-test.csv')
-  end
-
-  def template_url
-    template_path.to_s
-  end
-
-  def template_detail
-    "#{File.extname(csv_template.to_path).upcase.delete_prefix('.')} – #{number_to_human_size(csv_template.size)}"
-  end
 end
