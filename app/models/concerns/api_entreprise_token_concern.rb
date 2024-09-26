1ba9f201054d40b8470a783e3a3e759f7faa37ce
Mathieu Magnin <mathieu@magnin.sh>
2024-09-26 15:13:51 +0200

    [#10799] reorder methods A->Z

--- app/models/concerns/api_entreprise_token_concern.rb
+++ app/models/concerns/api_entreprise_token_concern.rb
@@ -7,6 +7,7 @@ module APIEntrepriseTokenConcern
 
   included do
     validates :api_entreprise_token, jwt_token: true, allow_blank: true
+
     before_save :set_api_entreprise_token_expires_at, if: :will_save_change_to_api_entreprise_token?
 
     def api_entreprise_role?(role)
@@ -17,16 +18,16 @@ module APIEntrepriseTokenConcern
       self[:api_entreprise_token].presence || Rails.application.secrets.api_entreprise[:key]
     end
 
-    def has_custom_api_entreprise_token?
-      self[:api_entreprise_token].present?
-    end
-
     def api_entreprise_token_expired?
       APIEntrepriseToken.new(api_entreprise_token).expired?
     end
 
     def api_entreprise_token_expired_or_expires_soon?
       api_entreprise_token_expires_at && api_entreprise_token_expires_at <= SOON_TO_EXPIRE_DELAY.from_now
+    end
+
+    def has_custom_api_entreprise_token?
+      self[:api_entreprise_token].present?
     end
 
     def set_api_entreprise_token_expires_at
