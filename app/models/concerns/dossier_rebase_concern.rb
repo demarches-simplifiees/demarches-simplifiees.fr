b5a52baa62c18130c437459e8f33bb7e62bb4193
Paul Chavard <pro@paul.chavard.net>
2024-09-27 15:44:57 +0200

    refactor(repetition): remove public_id

--- app/models/concerns/dossier_rebase_concern.rb
+++ app/models/concerns/dossier_rebase_concern.rb
@@ -65,9 +65,7 @@ module DossierRebaseConcern
       .tap { _1.default = Champ.none }
 
     # remove champ
-    children_champ, root_champ = changes_by_op[:remove].partition(&:child?)
-    children_champ.each { champs_by_stable_id[_1.stable_id].destroy_all }
-    root_champ.each { champs_by_stable_id[_1.stable_id].destroy_all }
+    changes_by_op[:remove].each { champs_by_stable_id[_1.stable_id].destroy_all }
 
     # update champ
     changes_by_op[:update].each { apply(_1, champs_by_stable_id[_1.stable_id]) }
@@ -78,8 +76,6 @@ module DossierRebaseConcern
     # add champ (after changing dossier revision to avoid errors)
     changes_by_op[:add]
       .map { target_coordinates_by_stable_id[_1.stable_id] }
-      # add parent champs first so we can then add children
-      .sort_by { _1.child? ? 1 : 0 }
       .each { add_new_champs_for_revision(_1) }
   end
 
@@ -119,28 +115,24 @@ module DossierRebaseConcern
 
   def add_new_champs_for_revision(target_coordinate)
     if target_coordinate.child?
-      # If this type de champ is a child, we create a new champ for each row of the parent
-      parent_stable_id = target_coordinate.parent.stable_id
+      row_ids = repetition_row_ids(target_coordinate.parent.type_de_champ)
 
-      champs.filter { _1.stable_id == parent_stable_id }.each do |champ_repetition|
-        if champ_repetition.champs.present?
-          champ_repetition.champs.map(&:row_id).uniq.each do |row_id|
-            champs << create_champ(target_coordinate, champ_repetition, row_id:)
-          end
-        elsif target_coordinate.parent.mandatory?
-          champs << create_champ(target_coordinate, champ_repetition, row_id: ULID.generate)
+      if row_ids.present?
+        row_ids.each do |row_id|
+          create_champ(target_coordinate, row_id:)
         end
+      elsif target_coordinate.parent.mandatory?
+        create_champ(target_coordinate, row_id: ULID.generate)
       end
     else
-      create_champ(target_coordinate, self)
+      create_champ(target_coordinate)
     end
   end
 
-  def create_champ(target_coordinate, parent, row_id: nil)
-    target_coordinate
+  def create_champ(target_coordinate, row_id: nil)
+    self.champs << target_coordinate
       .type_de_champ
       .build_champ(rebased_at: Time.zone.now, row_id:)
-      .tap { parent.champs << _1 }
   end
 
   def purge_piece_justificative_file(champ)
