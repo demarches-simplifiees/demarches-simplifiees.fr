33141841c963879c946e37c6055fc6af64c5ea07
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2024-10-08 06:58:40 -1000

    rebase on masterpf

--- app/components/editable_champ/table_row_selector_component.rb
+++ app/components/editable_champ/table_row_selector_component.rb
@@ -1,2 +1,13 @@
 class EditableChamp::TableRowSelectorComponent < EditableChamp::ComboSearchComponent
+
+  def dsfr_input_classname
+    'fr-input'
+  end
+
+  def react_input_opts
+    opts = input_opts(id: @champ.input_id, required: @champ.required?, aria: { describedby: @champ.describedby_id }, scopeExtra: @champ.table_id)
+    opts[:className] = "#{opts.delete(:class)} fr-mt-1w"
+
+    opts
+  end
 end
