a550f58dd8950581240136c8b0a3253a824a3e68
Paul Chavard <pro@paul.chavard.net>
2024-10-08 16:17:43 +0200

    refactor(dossier): rebase should not change champ type

--- app/models/types_de_champ/drop_down_list_type_de_champ.rb
+++ app/models/types_de_champ/drop_down_list_type_de_champ.rb
@@ -1,4 +1,21 @@
 # frozen_string_literal: true
 
 class TypesDeChamp::DropDownListTypeDeChamp < TypesDeChamp::TypeDeChampBase
+  class << self
+    def champ_value(champ)
+      if champ.value.present? && champ.drop_down_options.include?(champ.value)
+        champ.value
+      else
+        champ_default_value
+      end
+    end
+
+    def champ_value_for_export(champ, path = :value)
+      if path == :value && champ.value.present? && champ.drop_down_options.include?(champ.value)
+        champ.value
+      else
+        champ_default_export_value(path)
+      end
+    end
+  end
 end
