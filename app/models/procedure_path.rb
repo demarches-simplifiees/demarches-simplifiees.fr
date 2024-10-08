eed8738bf78cd0431ffd74831955493f8d5ddcff
Mathieu Magnin <mathieu@magnin.sh>
2024-10-08 18:51:52 +0200

    Use a partial index to ensure uniqueness

--- app/models/procedure_path.rb
+++ app/models/procedure_path.rb
@@ -3,5 +3,12 @@
 class ProcedurePath < ApplicationRecord
   belongs_to :procedure
 
-  validates :path, presence: true, format: { with: /\A[a-z0-9_\-]{3,200}\z/ }, uniqueness: true
+  scope :activated, -> { where(deactivated_at: nil) }
+  scope :deactivated, -> { where.not(deactivated_at: nil) }
+
+  validates :path, presence: true, format: { with: /\A[a-z0-9_\-]{3,200}\z/ }, uniqueness: { scope: [:path, :deactivated_at], conditions: -> { where(deactivated_at: nil) }, case_sensitive: false }
+
+  def activate!
+    update!(activated: true)
+  end
 end
