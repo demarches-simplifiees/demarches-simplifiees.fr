3677f3b2d3b47d44e022927f8c14ef4936d86d7a
simon lehericey <mail@simon.lehericey.net>
2024-10-09 09:51:47 +0200

    tech(clean): remove Column classname: attribute
    Co-authored-by: mfo <mfo@users.noreply.github.com>

--- app/models/column.rb
+++ app/models/column.rb
@@ -3,14 +3,13 @@
 class Column
   TYPE_DE_CHAMP_TABLE = 'type_de_champ'
 
-  attr_reader :table, :column, :label, :classname, :type, :scope, :value_column, :filterable, :displayable
+  attr_reader :table, :column, :label, :type, :scope, :value_column, :filterable, :displayable
 
-  def initialize(procedure_id:, table:, column:, label: nil, type: :text, value_column: :value, filterable: true, displayable: true, classname: '', scope: '')
+  def initialize(procedure_id:, table:, column:, label: nil, type: :text, value_column: :value, filterable: true, displayable: true, scope: '')
     @procedure_id = procedure_id
     @table = table
     @column = column
     @label = label || I18n.t(column, scope: [:activerecord, :attributes, :procedure_presentation, :fields, table])
-    @classname = classname
     @type = type
     @scope = scope
     @value_column = value_column
@@ -29,7 +28,7 @@ class Column
 
   def to_json
     {
-      table:, column:, label:, classname:, type:, scope:, value_column:, filterable:, displayable:
+      table:, column:, label:, type:, scope:, value_column:, filterable:, displayable:
     }
   end
 
