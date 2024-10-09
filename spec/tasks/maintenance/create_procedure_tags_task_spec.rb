f7ffc4820875cdda564a50f670eaad5b03c4ac13
Kara Diaby <kdiaby.pro@gmail.com>
2024-10-09 09:34:09 +0200

    Créé la liste de nouveaux tags en base et les associe aux démarches

---
+++ spec/tasks/maintenance/create_procedure_tags_task_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+module Maintenance
+  RSpec.describe CreateProcedureTagsTask do
+    describe "#process" do
+      subject(:process) { described_class.new.process(tag) }
+
+      let(:tag) { "Accompagnement" }
+      let!(:procedure) { create(:procedure, tags: ["Accompagnement"]) }
+
+      it "creates the ProcedureTag if it does not exist" do
+        expect { process }.to change { ProcedureTag.count }.by(1)
+        expect(ProcedureTag.last.name).to eq(tag)
+      end
+
+      context "when the ProcedureTag already exists" do
+        let!(:procedure_tag) { ProcedureTag.create(name: tag) }
+
+        it "does not create a duplicate ProcedureTag" do
+          expect { process }.not_to change { ProcedureTag.count }
+        end
+      end
+
+      it "associates procedures with the ProcedureTag" do
+        process
+        expect(procedure.reload.procedure_tags.map(&:name)).to include(tag)
+      end
+    end
+  end
+end
