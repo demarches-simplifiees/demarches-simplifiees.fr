17aaa7b32cf664060edf0be12212a5c061a29fad
Eric Leroy-Terquem <eric.leroyterquem@beta.gouv.fr>
2024-10-10 10:49:33 +0200

    feat(gallery): display right tag for annotation privée

--- spec/components/attachment/gallery_item_component_spec.rb
+++ spec/components/attachment/gallery_item_component_spec.rb
@@ -4,9 +4,10 @@ require 'rails_helper'
 
 RSpec.describe Attachment::GalleryItemComponent, type: :component do
   let(:instructeur) { create(:instructeur) }
-  let(:procedure) { create(:procedure, :published, types_de_champ_public:) }
+  let(:procedure) { create(:procedure, :published, types_de_champ_public:, types_de_champ_private:) }
   let(:types_de_champ_public) { [{ type: :piece_justificative }] }
-  let(:dossier) { create(:dossier, :with_populated_champs, :en_construction, procedure:) }
+  let(:types_de_champ_private) { [{ type: :piece_justificative }] }
+  let(:dossier) { create(:dossier, :with_populated_champs, :with_populated_annotations, :en_construction, procedure:) }
   let(:filename) { attachment.blob.filename.to_s }
   let(:gallery_demande) { false }
   let(:seen_at) { nil }
@@ -16,8 +17,10 @@ RSpec.describe Attachment::GalleryItemComponent, type: :component do
 
   subject { render_inline(component).to_html }
 
-  context "when attachment is from a piece justificative champ" do
-    let(:champ) { dossier.champs.first }
+  context "when attachment is from a public piece justificative champ" do
+    let(:champ) do
+      dossier.champs.where(private: false).first
+    end
     let(:libelle) { champ.libelle }
     let(:attachment) { champ.piece_justificative_file.attachments.first }
 
@@ -45,6 +48,38 @@ RSpec.describe Attachment::GalleryItemComponent, type: :component do
       it 'displays the right text' do
         expect(subject).to have_text('Modifiée le')
       end
+    end
+
+    context "when gallery item is in page Demande" do
+      let(:gallery_demande) { true }
+
+      it "does not display libelle" do
+        expect(subject).not_to have_text(libelle)
+      end
+    end
+  end
+
+  context "when attachment is from a private piece justificative champ" do
+    let(:annotation) do
+      dossier.champs.where(private: true).first
+    end
+    let(:libelle) { annotation.libelle }
+    let(:attachment) { annotation.piece_justificative_file.attachments.first }
+
+    # Correspond au cas standard où le blob est créé avant le dépôt du dossier
+    before { dossier.touch(:depose_at) }
+
+    it "displays libelle, link, tag and renders title" do
+      expect(subject).to have_text(libelle)
+      expect(subject).to have_link(filename)
+      expect(subject).to have_text('Annotation privée')
+      expect(component.title).to eq("#{libelle} -- #{filename}")
+    end
+
+    it "displays when gallery item has been added" do
+      expect(subject).to have_text('Ajoutée le')
+      expect(subject).not_to have_css('.highlighted')
+      expect(subject).to have_text(component.helpers.try_format_datetime(attachment.record.created_at, format: :veryshort))
     end
 
     context "when gallery item is in page Demande" do
