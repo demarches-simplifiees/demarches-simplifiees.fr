065d380b701c2d6b158e6f2841a333fa5dca0940
Colin Darie <colin@darie.eu>
2024-10-15 18:26:19 +0200

    feat(service): prefill type organisme from API Entreprise

--- spec/models/concerns/prefillable_from_service_public_concern_spec.rb
+++ spec/models/concerns/prefillable_from_service_public_concern_spec.rb
@@ -9,12 +9,13 @@ RSpec.describe PrefillableFromServicePublicConcern, type: :model do
   describe '#prefill_from_siret' do
     let(:service) { Service.new(siret:) }
     subject { service.prefill_from_siret }
-    context 'when API call is successful' do
+    context 'when API call is successful with collectivite' do
       it 'prefills service attributes' do
         VCR.use_cassette('annuaire_service_public_success_20004021000060') do
-          expect(subject).to be_success
+          expect(subject.all?(&:success?)).to be_truthy
 
           expect(service.nom).to eq("Communauté de communes - Lacs et Gorges du Verdon")
+          expect(service).to be_collectivite_territoriale
           expect(service.email).to eq("redacted@email.fr")
           expect(service.telephone).to eq("04 94 70 00 00")
           expect(service.horaires).to eq("Lundi au Jeudi : de 8:00 à 12:00 et de 13:30 à 17:30\nVendredi : de 8:00 à 12:00")
@@ -39,7 +40,46 @@ RSpec.describe PrefillableFromServicePublicConcern, type: :model do
       let(:siret) { '20004021000000' }
       it 'returns a failure result' do
         VCR.use_cassette('annuaire_service_public_failure_20004021000000') do
-          expect(subject).to be_failure
+          expect(subject.all?(&:failure?)).to be_truthy
+        end
+      end
+    end
+
+    context 'when SIRET is enseignement' do
+      let(:siret) { '19750664500013' }
+      it 'prefills for enseignement' do
+        VCR.use_cassette('annuaire_service_public_success_19750664500013') do
+          expect(subject.one?(&:success?)).to be_truthy
+          expect(service.nom).to eq("LYCEE GENERAL ET TECHNOLOGIQUE RACINE")
+          expect(service).to be_etablissement_enseignement
+          expect(service.adresse).to eq("20 Rue du Rocher 75008 Paris")
+          expect(service.horaires).to be_nil
+        end
+      end
+    end
+
+    context 'when SIRET is a ministere' do
+      let(:siret) { '11004601800013' }
+      it 'prefills for administration centrale' do
+        VCR.use_cassette('annuaire_service_public_success_11004601800013') do
+          expect(subject.one?(&:success?)).to be_truthy
+          expect(service.nom).to eq("MINISTERE DE LA CULTURE")
+          expect(service).to be_administration_centrale
+          expect(service.adresse).to eq("182 Rue Saint-Honoré 75001 Paris")
+          expect(service.horaires).to be_nil
+        end
+      end
+    end
+
+    context 'when SIRET is La Poste' do
+      let(:siret) { '35600082800018' }
+      it 'prefills for administration centrale' do
+        VCR.use_cassette('annuaire_service_public_success_35600082800018') do
+          expect(subject.one?(&:success?)).to be_truthy
+          expect(service.nom).to eq("LA POSTE (REGION RHONE ALPES)")
+          expect(service).to be_service_deconcentre_de_l_etat
+          expect(service.adresse).to eq("4 Quai du Point du Jour 92100 Boulogne-Billancourt")
+          expect(service.horaires).to be_nil
         end
       end
     end
