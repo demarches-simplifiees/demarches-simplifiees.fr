8398ff2c4c238d05224b4b7c5bc19dada712f40a
mfo <mfo@users.noreply.github.com>
2024-10-17 11:34:45 +0200

    feat(Champ.commune): makes it filterable and exportable. not yet displayable due to PR under review waiting for last phase of column refacto

--- app/models/types_de_champ/commune_type_de_champ.rb
+++ app/models/types_de_champ/commune_type_de_champ.rb
@@ -29,6 +29,31 @@ class TypesDeChamp::CommuneTypeDeChamp < TypesDeChamp::TypeDeChampBase
     end
   end
 
+  def columns(procedure_id:, displayable: true, prefix: nil)
+    super.concat(
+      [
+        Column.new(
+          procedure_id:,
+          table: Column::TYPE_DE_CHAMP_TABLE,
+          column: "#{stable_id}.external_id",
+          label: "#{libelle_with_prefix(prefix)} (Code INSEE)",
+          type: :integer,
+          displayable: false,
+          value_column: :external_id
+        ),
+        Columns::JSONPathColumn.new(
+          procedure_id:,
+          table: Column::TYPE_DE_CHAMP_TABLE,
+          column: stable_id,
+          label: "#{libelle_with_prefix(prefix)} (Département)",
+          type: :integer,
+          displayable: false,
+          value_column: ['code_departement']
+        )
+      ]
+    )
+  end
+
   private
 
   def paths
