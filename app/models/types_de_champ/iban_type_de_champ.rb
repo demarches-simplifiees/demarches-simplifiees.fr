1ded040730eae26e890454208a7155d7ae41b144
Paul Chavard <pro@paul.chavard.net>
2024-10-21 11:43:39 +0200

    fix(iban): format iban through type_de_champ

--- app/models/types_de_champ/iban_type_de_champ.rb
+++ app/models/types_de_champ/iban_type_de_champ.rb
@@ -4,4 +4,8 @@ class TypesDeChamp::IbanTypeDeChamp < TypesDeChamp::TypeDeChampBase
   def estimated_fill_duration(revision)
     FILL_DURATION_MEDIUM
   end
+
+  def champ_value_for_api(champ, version: 2)
+    champ_value(champ).gsub(/\s+/, '')
+  end
 end
