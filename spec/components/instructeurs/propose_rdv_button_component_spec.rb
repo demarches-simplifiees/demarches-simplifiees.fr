9a452d9eb19ade88cd41bb008feb86f9417eb747
Mathieu Magnin <mathieu@magnin.sh>
2024-10-25 18:48:11 +0200

    [#10457] Propose RDV button

---
+++ spec/components/instructeurs/propose_rdv_button_component_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Instructeurs::ProposeRdvButtonComponent, type: :component do
+  let(:procedure) { create(:procedure) }
+  let(:dossier) { create(:dossier, procedure: procedure) }
+
+  subject(:component) { described_class.new(dossier: dossier) }
+
+  it "renders the button with correct attributes" do
+    render_inline(component)
+
+    expect(page).to have_css("button.fr-btn")
+    expect(page).to have_button("Proposer un rendez-vous")
+  end
+end
