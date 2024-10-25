d66487138f2578c6df7e24fa014137d4627d9c0d
mfo <mfo@users.noreply.github.com>
2024-10-25 13:49:32 +0200

    fix(sentry#6013588148): allowing recasting from drop_down_list to multiple_drop_down_list raises due to call to .selected_options

--- app/models/type_de_champ.rb
+++ app/models/type_de_champ.rb
@@ -763,7 +763,7 @@ class TypeDeChamp < ApplicationRecord
     when ['integer_number', 'decimal_number'], # recast numbers automatically
       ['decimal_number', 'integer_number'], # may lose some data, but who cares ?
       ['text', 'textarea'], # allow short text to long text
-      ['drop_down_list', 'multiple_drop_down_list'], # single list can become multi
+      # ['drop_down_list', 'multiple_drop_down_list'], # single list can become multi
       ['date', 'datetime'], # date <=> datetime
       ['datetime', 'date'] # may lose some data, but who cares ?
       true
