b57209670148ae74a9f985739c4accc52a962f68
simon lehericey <mail@simon.lehericey.net>
2024-11-08 14:09:52 +0100

    move last call to procedure_presentation controller

--- app/components/dossiers/notified_toggle_component.rb
+++ app/components/dossiers/notified_toggle_component.rb
@@ -3,6 +3,7 @@
 class Dossiers::NotifiedToggleComponent < ApplicationComponent
   def initialize(procedure:, procedure_presentation:)
     @procedure = procedure
+    @procedure_presentation = procedure_presentation
     @sorted_column = procedure_presentation.sorted_column
   end
 end
