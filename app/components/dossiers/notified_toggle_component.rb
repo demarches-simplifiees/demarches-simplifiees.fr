ec9a88a64e8db61f10fb7a6500d9d415f4ef6e3d
simon lehericey <mail@simon.lehericey.net>
2024-11-12 09:43:27 +0100

    simplify new

--- app/components/dossiers/notified_toggle_component.rb
+++ app/components/dossiers/notified_toggle_component.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class Dossiers::NotifiedToggleComponent < ApplicationComponent
-  def initialize(procedure:, procedure_presentation:)
-    @procedure = procedure
+  def initialize(procedure_presentation:)
     @procedure_presentation = procedure_presentation
+    @procedure = procedure_presentation.procedure
     @sorted_column = procedure_presentation.sorted_column
   end
 end
