9a70a9526de3529f0049b0fd541a52e073f54799
mfo <mfo@users.noreply.github.com>
2024-11-15 09:42:44 +0100

    feat(rna/rnf): expose rnf/rna title

--- app/models/concerns/addressable_column_concern.rb
+++ app/models/concerns/addressable_column_concern.rb
@@ -4,8 +4,8 @@ module AddressableColumnConcern
   extend ActiveSupport::Concern
 
   included do
-    def columns(procedure:, displayable: true, prefix: nil)
-      addressable_columns = [
+    def addressable_columns(procedure:, displayable: true, prefix: nil)
+      [
         ["code postal (5 chiffres)", '$.postal_code', :text, []],
         ["commune", '$.city_name', :text, []],
         ["département", '$.departement_code', :enum, APIGeoService.departement_options],
@@ -22,8 +22,6 @@ module AddressableColumnConcern
           type:
         )
       end
-
-      super.concat(addressable_columns)
     end
   end
 end
--- spec/models/columns/champ_column_spec.rb
+++ spec/models/columns/champ_column_spec.rb
@@ -41,6 +41,8 @@ describe Columns::ChampColumn do
         expect_type_de_champ_values('mesri', eq([nil]))
         expect_type_de_champ_values('cojo', eq([nil]))
         expect_type_de_champ_values('expression_reguliere', eq([nil]))
+        expect_type_de_champ_values('rna', eq(["W173847273", "postal_code", "city_name", "departement_code", "region_name", "LA PRÉVENTION ROUTIERE"]))
+        expect_type_de_champ_values('rnf', eq(["075-FDD-00003-01", "postal_code", "city_name", "departement_code", "region_name", "Fondation SFR"]))
       end
     end
 
