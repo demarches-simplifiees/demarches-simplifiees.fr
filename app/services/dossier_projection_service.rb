eee0f3dc95b96e12aead6185b576620959371e5d
simon lehericey <mail@simon.lehericey.net>
2024-11-19 13:55:00 +0100

    load the corrections on dossier

--- app/services/dossier_projection_service.rb
+++ app/services/dossier_projection_service.rb
@@ -1,24 +1,11 @@
 # frozen_string_literal: true
 
 class DossierProjectionService
-  class DossierProjection < Struct.new(:dossier, :corrections, :columns) do
-      def pending_correction?
-        return false if corrections.blank?
-
-        corrections.any?(&:nil?)
-      end
-
-      def resolved_corrections?
-        return false if corrections.blank?
-
-        corrections.all?(&:present?)
-      end
-    end
+  class DossierProjection < Struct.new(:dossier, :columns)
   end
 
   def self.project(dossier_ids, columns)
-    dossiers = Dossier.find(dossier_ids)
-    corrections_for = corrections_by_dossier_id(dossier_ids)
+    dossiers = Dossier.includes(:corrections, :pending_corrections).find(dossier_ids)
 
     # each project should return
     # { dossier_id => { column_id => value, column_id2 ... }, dossier_id2 ... }
@@ -29,19 +16,8 @@ class DossierProjectionService
     dossiers.map do |dossier|
       DossierProjection.new(
         dossier,
-        corrections_for[dossier.id],
         columns.map { |column| projection[dossier.id][column.id] }
       )
     end
-  end
-
-  private
-
-  def self.corrections_by_dossier_id(dossier_ids)
-    DossierCorrection.where(dossier_id: dossier_ids)
-      .pluck(:dossier_id, :resolved_at)
-      .each_with_object(Hash.new { |h, k| h[k] = [] }) do |(dossier_id, resolved_at), h|
-        h[dossier_id] << resolved_at
-      end
   end
 end
