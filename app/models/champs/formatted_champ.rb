a4040962565e6469f7babd39ae305f52328e57b8
Christophe Robillard <christophe.robillard@mail.numerique.gouv.fr>
2024-11-20 10:50:17 +0100

    add simple formatted validator

--- app/models/champs/formatted_champ.rb
+++ app/models/champs/formatted_champ.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class Champs::FormattedChamp < Champ
-  validates_with ExpressionReguliereValidator, if: :validate_champ_value_or_prefill?
+  validates_with FormattedChampValidator, if: :validate_champ_value?
 end
