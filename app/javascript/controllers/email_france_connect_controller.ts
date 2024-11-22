2a2517173803a48cef3a88478eadf9f1c826e10d
simon lehericey <mail@simon.lehericey.net>
2024-11-22 17:17:11 +0100

    Fix: allow custom email (!), require a mail confirmation
    previous code was wrong, it was impossible to choose another email than the FC one

--- app/javascript/controllers/email_france_connect_controller.ts
+++ app/javascript/controllers/email_france_connect_controller.ts
@@ -1,12 +1,27 @@
 import { ApplicationController } from './application_controller';
 
 export class EmailFranceConnectController extends ApplicationController {
-  static targets = ['useFranceConnectEmail', 'emailField', 'submit', 'emailInput'];
+  static targets = [
+    'useFranceConnectEmail',
+    'emailField',
+    'submit',
+    'emailInput',
+    'form'
+  ];
 
   emailFieldTarget!: HTMLElement;
   useFranceConnectEmailTargets!: HTMLInputElement[];
   submitTarget!: HTMLButtonElement;
   emailInputTarget!: HTMLInputElement;
+  formTarget!: HTMLFormElement;
+
+  fcEmailPathValue!: string;
+  customEmailPathValue!: string;
+
+  static values = {
+    fcEmailPath: String,
+    customEmailPath: String
+  };
 
   triggerEmailField() {
     if (this.useFCEmail()) {
@@ -15,11 +30,15 @@ export class EmailFranceConnectController extends ApplicationController {
 
       this.emailInputTarget.removeAttribute('required');
       this.emailInputTarget.value = '';
+
+      this.formTarget.action = this.fcEmailPathValue;
     } else {
       this.emailFieldTarget.classList.remove('hidden');
       this.emailFieldTarget.setAttribute('aria-hidden', 'false');
 
       this.emailInputTarget.setAttribute('required', '');
+
+      this.formTarget.action = this.customEmailPathValue;
     }
   }
 
@@ -32,9 +51,10 @@ export class EmailFranceConnectController extends ApplicationController {
   }
 
   useFCEmail() {
-    return this.useFranceConnectEmailTargets.find(
-      (target) => target.checked
-    )?.value === 'true' || false;
+    return (
+      this.useFranceConnectEmailTargets.find((target) => target.checked)
+        ?.value === 'true' || false
+    );
   }
 
   isEmailInputFilled() {
