183dd1138fa9ee17d33b92ca21aa3d6196cc3366
Eric Leroy-Terquem <eric.leroyterquem@beta.gouv.fr>
2024-11-22 14:09:52 +0100

    db(migration): create referentiels and referentiel_items

---
+++ db/migrate/20241203151339_add_referentiel_id_to_types_de_champ.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class AddReferentielIdToTypesDeChamp < ActiveRecord::Migration[7.0]
+  disable_ddl_transaction!
+
+  def change
+    add_reference :types_de_champ, :referentiel, null: true, default: nil, index: { algorithm: :concurrently }, foreign_key: false
+  end
+end
