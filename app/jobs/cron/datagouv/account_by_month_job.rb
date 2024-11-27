b1ab2432fc58577f8183a2475d249ef4287cdab6
benoitqueyron <72251526+Benoit-MINT@users.noreply.github.com>
2024-11-27 11:10:15 +0100

    refacto process actuel de publication, sur la ressource : nb_comptes_crees

--- app/jobs/cron/datagouv/account_by_month_job.rb
+++ app/jobs/cron/datagouv/account_by_month_job.rb
@@ -3,19 +3,22 @@
 class Cron::Datagouv::AccountByMonthJob < Cron::CronJob
   include DatagouvCronSchedulableConcern
   self.schedule_expression = "every month at 4:30"
-  FILE_NAME = "nb_comptes_crees_par_mois"
+  HEADERS = ["mois", "nb_comptes_crees_par_mois"]
+  FILE_NAME = "#{HEADERS[1]}.csv"
 
   def perform(*args)
-    GenerateOpenDataCsvService.save_csv_to_tmp(FILE_NAME, data) do |file|
-      begin
-        APIDatagouv::API.upload(file, :statistics_dataset)
-      ensure
-        FileUtils.rm(file)
-      end
+    GenerateOpenDataCsvService.save_csv_to_tmp(FILE_NAME, HEADERS, data) do |file|
+      APIDatagouv::API.upload(file, :statistics_dataset)
     end
   end
 
+  private
+
   def data
-    User.where(created_at: 1.month.ago.all_month).count
+    [[date_last_month, User.where(created_at: 1.month.ago.all_month).count]]
+  end
+
+  def date_last_month
+    Date.today.prev_month.strftime("%Y %B")
   end
 end
--- app/services/generate_open_data_csv_service.rb
+++ app/services/generate_open_data_csv_service.rb
@@ -1,21 +1,11 @@
 # frozen_string_literal: true
 
 class GenerateOpenDataCsvService
-  def self.save_csv_to_tmp(file_name, data)
-    f = Tempfile.create(["#{file_name}_#{date_last_month}", '.csv'], 'tmp')
-    f << generate_csv(file_name, data)
-    f.rewind
-    yield f if block_given?
-    f.close
-  end
-
-  def self.date_last_month
-    Date.today.prev_month.strftime("%B")
-  end
-
-  def self.generate_csv(file_name, data)
-    headers = ["mois", file_name]
-    data = [[date_last_month, data]]
-    SpreadsheetArchitect.to_csv(headers: headers, data: data)
+  def self.save_csv_to_tmp(file_name, headers, data)
+    Tempfile.create(file_name, 'tmp') do |file|
+      file << SpreadsheetArchitect.to_csv(headers:, data:)
+      file.rewind
+      yield file if block_given?
+    end
   end
 end
