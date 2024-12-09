221eb4cc0542fea905204c701363761b732a7449
benoitqueyron <72251526+Benoit-MINT@users.noreply.github.com>
2024-12-09 17:30:24 +0100

    resource: instructeur_connected_by_month

--- app/jobs/cron/datagouv/instructeur_connected_by_month_job.rb
+++ app/jobs/cron/datagouv/instructeur_connected_by_month_job.rb
@@ -3,19 +3,26 @@
 class Cron::Datagouv::InstructeurConnectedByMonthJob < Cron::CronJob
   include DatagouvCronSchedulableConcern
   self.schedule_expression = "every month at 4:45"
-  FILE_NAME = "nb_instructeurs_connectes_par_mois"
+  HEADERS = ["mois", "nb_instructeurs_connectes_par_mois"]
+  FILE_NAME = HEADERS[1]
+  DATASET = '6745cdbb3aee5fa1f498d5ef'
+  RESOURCE = 'a69785f1-1b96-471c-ad61-5b5c66fa3d21'
 
   def perform(*args)
-    GenerateOpenDataCsvService.save_csv_to_tmp(FILE_NAME, data) do |file|
-      begin
-        APIDatagouv::API.upload(file, :statistics_dataset)
-      ensure
-        FileUtils.rm(file)
-      end
-    end
+    csv = GenerateOpenDataService.existing_csv(DATASET, RESOURCE, HEADERS)
+    resource = csv.empty? ? nil : RESOURCE
+
+    GenerateOpenDataService.months_to_query(csv).map { |period| csv << data_of_range(period) }
+
+    APIDatagouv::API.upload_csv(FILE_NAME, csv, DATASET, resource)
   end
 
-  def data
-    Instructeur.joins(:user).where(user: { last_sign_in_at: 1.month.ago.all_month }).count
+  private
+
+  def data_of_range(range)
+    [
+      range.min.strftime("%Y-%m"),
+      Instructeur.joins(:user).where(user: { last_sign_in_at: range }).count
+    ]
   end
 end
--- spec/jobs/cron/datagouv/instructeur_connected_by_month_job_spec.rb
+++ spec/jobs/cron/datagouv/instructeur_connected_by_month_job_spec.rb
@@ -1,22 +1,44 @@
 # frozen_string_literal: true
 
 RSpec.describe Cron::Datagouv::InstructeurConnectedByMonthJob, type: :job do
-  let!(:user) { create(:user) }
-  let!(:instructeur) { create(:instructeur, user: user, created_at: 1.month.ago) }
   let(:status) { 200 }
   let(:body) { "ok" }
-  let(:stub) { stub_request(:post, /https:\/\/www.data.gouv.fr\/api\/.*\/upload\//) }
 
   describe 'perform' do
-    before do
-      stub
-    end
-
     subject { Cron::Datagouv::InstructeurConnectedByMonthJob.perform_now }
 
-    it 'send POST request to datagouv' do
+    it 'sends the correct CSV file to datagouv API' do
+      # we simulate the case where there is no existing file
+      allow(APIDatagouv::API).to receive(:existing_file_url).and_return(nil)
+
+      allow(APIDatagouv::API).to receive(:upload_csv) do |_, csv_table, _, _|
+        csv = CSV.parse(csv_table.to_csv, headers: true)
+
+        expect(csv.first['mois']).to eq(Date.today.prev_month.strftime("%Y-%m"))
+        expect(csv.first['nb_instructeurs_connectes_par_mois']).to eq('0')
+      end
+
       subject
-      expect(stub).to have_been_requested
+    end
+  end
+
+  describe 'data_of_range' do
+    let(:range) { Date.parse('01/01/2024').all_month }
+
+    subject { Cron::Datagouv::InstructeurConnectedByMonthJob.new.send(:data_of_range, range) }
+
+    context 'when instructeurs have been connected during the target month' do
+      let!(:user) { create(:user, last_sign_in_at: Date.parse('15/01/2024')) }
+      let!(:instructeur) { create(:instructeur, user: user) }
+
+      it { is_expected.to eq(['2024-01', 1]) }
+    end
+
+    context 'when instructeurs have not been connected during the target month' do
+      let!(:user) { create(:user, last_sign_in_at: Date.parse('15/12/2023')) }
+      let!(:instructeur) { create(:instructeur, user: user) }
+
+      it { is_expected.to eq(['2024-01', 0]) }
     end
   end
 end
