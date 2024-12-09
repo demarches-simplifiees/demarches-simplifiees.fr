69548ed37717ea2c34eeac010402739f09afe847
benoitqueyron <72251526+Benoit-MINT@users.noreply.github.com>
2024-12-09 17:34:53 +0100

    resource: procedure_by_month

--- app/jobs/cron/datagouv/procedure_by_month_job.rb
+++ app/jobs/cron/datagouv/procedure_by_month_job.rb
@@ -1,21 +1,24 @@
 # frozen_string_literal: true
 
-class Cron::Datagouv::ProcedureByMonthJob < Cron::CronJob
-  include DatagouvCronSchedulableConcern
+class Cron::Datagouv::ProcedureByMonthJob < Cron::Datagouv::BaseJob
   self.schedule_expression = "every month at 4:15"
-  FILE_NAME = "nb_procedures_creees_par_mois"
+  HEADERS = ["mois", "nb_procedures_creees_par_mois"]
+  FILE_NAME = HEADERS[1]
+  RESOURCE = '5a1a8489-367b-4242-8de1-811572da5147'
 
-  def perform(*args)
-    GenerateOpenDataCsvService.save_csv_to_tmp(FILE_NAME, data) do |file|
-      begin
-        APIDatagouv::API.upload(file, :statistics_dataset)
-      ensure
-        FileUtils.rm(file)
-      end
-    end
+  def perform
+    csv = data_gouv_csv(RESOURCE, HEADERS)
+
+    missing_months(csv)
+      .map { |month| data_for(month:) }
+      .each { |data| csv << data }
+
+    APIDatagouv::API.upload_csv(FILE_NAME, csv, DATASET, RESOURCE)
   end
 
-  def data
-    Procedure.where(created_at: 1.month.ago.all_month).count
+  private
+
+  def data_for(month:)
+    [month.strftime(DATE_FORMAT), Procedure.where(created_at: month.all_month).count]
   end
 end
