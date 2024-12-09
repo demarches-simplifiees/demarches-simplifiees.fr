2e63c31fcf6bcc81649ef5f458194a4554d3bd97
benoitqueyron <72251526+Benoit-MINT@users.noreply.github.com>
2024-12-09 16:40:59 +0100

    resource: file_by_month

--- app/jobs/cron/datagouv/file_by_month_job.rb
+++ app/jobs/cron/datagouv/file_by_month_job.rb
@@ -3,19 +3,23 @@
 class Cron::Datagouv::FileByMonthJob < Cron::CronJob
   include DatagouvCronSchedulableConcern
   self.schedule_expression = "every month at 3:15"
-  FILE_NAME = "nb_dossiers_crees_par_mois"
+  HEADERS = ["mois", "nb_dossiers_crees_par_mois"]
+  FILE_NAME = HEADERS[1]
+  DATASET = '6745cdbb3aee5fa1f498d5ef'
+  RESOURCE = '5712aaa8-9f96-4761-aa61-1931d848c874'
 
   def perform(*args)
-    GenerateOpenDataCsvService.save_csv_to_tmp(FILE_NAME, data) do |file|
-      begin
-        APIDatagouv::API.upload(file, :statistics_dataset)
-      ensure
-        FileUtils.rm(file)
-      end
-    end
+    csv = GenerateOpenDataService.existing_csv(DATASET, RESOURCE, HEADERS)
+    resource = csv.empty? ? nil : RESOURCE
+
+    GenerateOpenDataService.months_to_query(csv).map { |period| csv << data_of_range(period) }
+
+    APIDatagouv::API.upload_csv(FILE_NAME, csv, DATASET, resource)
   end
 
-  def data
-    Dossier.visible_by_user_or_administration.where(created_at: 1.month.ago.all_month).count
+  private
+
+  def data_of_range(range)
+    [range.min.strftime("%Y-%m"), Dossier.visible_by_user_or_administration.where(created_at: range).count]
   end
 end
