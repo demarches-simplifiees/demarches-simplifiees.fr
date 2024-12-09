20980f19259fb9bd25fcdc6bd840aeea6c538c43
benoitqueyron <72251526+Benoit-MINT@users.noreply.github.com>
2024-12-09 17:15:21 +0100

    resource: file_depose_by_month

--- spec/jobs/cron/datagouv/file_depose_by_month_job_spec.rb
+++ spec/jobs/cron/datagouv/file_depose_by_month_job_spec.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
 
 RSpec.describe Cron::Datagouv::FileDeposeByMonthJob, type: :job do
-  let!(:dossier) { create(:dossier, created_at: 1.month.ago) }
-  let(:status) { 200 }
-  let(:body) { "ok" }
-  let(:stub) { stub_request(:post, /https:\/\/www.data.gouv.fr\/api\/.*\/upload\//) }
+  describe 'data_for' do
+    let(:month) { Date.parse('01/01/2024') }
 
-  describe 'perform' do
-    before do
-      stub
+    subject { Cron::Datagouv::FileDeposeByMonthJob.new.send(:data_for, month:) }
+
+    context 'when dossiers have been deposited during the target month' do
+      let!(:dossier) { create(:dossier, depose_at: Date.parse('15/01/2024')) }
+
+      it { is_expected.to eq(['2024-01', 1]) }
     end
 
-    subject { Cron::Datagouv::FileByMonthJob.perform_now }
+    context 'when dossiers have not been deposited during the target month' do
+      let!(:dossier) { create(:dossier, depose_at: Date.parse('15/12/2023')) }
 
-    it 'send POST request to datagouv' do
-      subject
-      expect(stub).to have_been_requested
+      it { is_expected.to eq(['2024-01', 0]) }
     end
   end
 end
