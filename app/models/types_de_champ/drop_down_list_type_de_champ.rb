a47d5174785d96053ec84d19589f458e6fa57aca
Eric Leroy-Terquem <eric.leroyterquem@beta.gouv.fr>
2024-12-09 11:15:51 +0100

    feat(referentiel): display form to user

--- app/models/types_de_champ/drop_down_list_type_de_champ.rb
+++ app/models/types_de_champ/drop_down_list_type_de_champ.rb
@@ -8,7 +8,11 @@ class TypesDeChamp::DropDownListTypeDeChamp < TypesDeChamp::TypeDeChampBase
   private
 
   def champ_value_in_options?(champ)
-    champ_with_other_value?(champ) || drop_down_options.include?(champ.value)
+    if champ.referentiel?
+      champ_with_other_value?(champ) || referentiel_drop_down_options.map { _1['id'] }.include?(champ.value.to_i)
+    else
+      champ_with_other_value?(champ) || drop_down_options.include?(champ.value)
+    end
   end
 
   def champ_with_other_value?(champ)
