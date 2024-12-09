6e3a250c94c39a3434c6e6f8ba3405eea1500751
benoitqueyron <72251526+Benoit-MINT@users.noreply.github.com>
2024-12-09 17:55:39 +0100

    resource: user_connected_with_france_connect_by_month

--- app/jobs/cron/datagouv/user_connected_with_france_connect_by_month_job.rb
+++ app/jobs/cron/datagouv/user_connected_with_france_connect_by_month_job.rb
@@ -3,19 +3,26 @@
 class Cron::Datagouv::UserConnectedWithFranceConnectByMonthJob < Cron::CronJob
   include DatagouvCronSchedulableConcern
   self.schedule_expression = "every month at 3:45"
-  FILE_NAME = "nb_utilisateurs_connectes_france_connect_par_mois"
+  HEADERS = ["mois", "nb_utilisateurs_connectes_france_connect_par_mois"]
+  FILE_NAME = HEADERS[1]
+  DATASET = '6745cdbb3aee5fa1f498d5ef'
+  RESOURCE = '2e73fb50-557c-41c9-8b9e-7716f3c63be4'
 
   def perform(*args)
-    GenerateOpenDataCsvService.save_csv_to_tmp(FILE_NAME, data) do |file|
-      begin
-        APIDatagouv::API.upload(file, :statistics_dataset)
-      ensure
-        FileUtils.rm(file)
-      end
-    end
+    csv = GenerateOpenDataService.existing_csv(DATASET, RESOURCE, HEADERS)
+    resource = csv.empty? ? nil : RESOURCE
+
+    GenerateOpenDataService.months_to_query(csv).map { |period| csv << data_of_range(period) }
+
+    APIDatagouv::API.upload_csv(FILE_NAME, csv, DATASET, resource)
   end
 
-  def data
-    User.where(created_at: 1.month.ago.all_month, loged_in_with_france_connect: "particulier").count
+  private
+
+  def data_of_range(range)
+    [
+      range.min.strftime("%Y-%m"),
+      User.where(created_at: range, loged_in_with_france_connect: "particulier").count
+    ]
   end
 end
