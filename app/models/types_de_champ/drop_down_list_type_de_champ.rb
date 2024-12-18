d365dd57ae4d6605ee7248c43bd0b9a6b8c95a41
Eric Leroy-Terquem <eric.leroyterquem@beta.gouv.fr>
2024-12-18 10:26:41 +0100

    feat(referentiel): display referentiel columns

--- app/models/types_de_champ/drop_down_list_type_de_champ.rb
+++ app/models/types_de_champ/drop_down_list_type_de_champ.rb
@@ -1,4 +1,21 @@
 # frozen_string_literal: true
 
 class TypesDeChamp::DropDownListTypeDeChamp < TypesDeChamp::TypeDeChampBase
+  def columns(procedure:, displayable: true, prefix: nil)
+    if referentiel?
+      referentiel.headers.map do |header|
+        Columns::JSONPathColumn.new(
+          procedure_id: procedure.id,
+          stable_id:,
+          tdc_type: type_champ,
+          label: "#{libelle_with_prefix(prefix)} – Référentiel #{header}",
+          type: :text,
+          jsonpath: "$.referentiel.data.#{header.parameterize.underscore}",
+          displayable:
+        )
+      end
+    else
+      super
+    end
+  end
 end
