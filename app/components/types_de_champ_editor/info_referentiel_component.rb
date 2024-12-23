ab571e7dd5e0e989e837e720b2983cf13e797673
mfo <mfo@users.noreply.github.com>
2024-12-23 15:30:13 +0100

    feat(champ.referentiel): 1st step of setup wizard [some options are present but disable. will be enabled once implemented]

--- app/components/types_de_champ_editor/info_referentiel_component.rb
+++ app/components/types_de_champ_editor/info_referentiel_component.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
 class TypesDeChampEditor::InfoReferentielComponent < ApplicationComponent
-  attr_reader :type_de_champ
+  attr_reader :procedure, :type_de_champ
 
-  def initialize(type_de_champ:)
+  def initialize(procedure:, type_de_champ:)
+    @procedure = procedure
     @type_de_champ = type_de_champ
+  end
+
+  def new_referentiel_url
+    new_admin_procedure_referentiel_path(procedure, type_de_champ.stable_id)
   end
 
   def configured?
