41344d11ad557c315ae116a80a827a898990e4bb
mfo <mfo@users.noreply.github.com>
2024-12-27 11:09:00 +0100

    feat(referentiel): admin can edit/update a referentiel which had been setuped earlier

--- app/components/types_de_champ_editor/info_referentiel_component.rb
+++ app/components/types_de_champ_editor/info_referentiel_component.rb
@@ -2,17 +2,38 @@
 
 class TypesDeChampEditor::InfoReferentielComponent < ApplicationComponent
   attr_reader :procedure, :type_de_champ
+  delegate :referentiel, to: :type_de_champ
 
   def initialize(procedure:, type_de_champ:)
     @procedure = procedure
     @type_de_champ = type_de_champ
   end
 
-  def new_referentiel_url
-    new_admin_procedure_referentiel_path(procedure, type_de_champ.stable_id)
+  def edit_referentiel_on_draft_or_clone_url
+    if should_create_new_referentiel?
+      dup_existing_referentiel_options = referentiel ? { referentiel_id: referentiel.id } : {}
+      new_admin_procedure_referentiel_path(procedure, type_de_champ.stable_id, dup_existing_referentiel_options)
+    else
+      edit_admin_procedure_referentiel_path(procedure, type_de_champ.stable_id, type_de_champ.referentiel)
+    end
   end
 
   def configured?
     false
+  end
+
+  private
+
+  def should_create_new_referentiel?
+    return true if referentiel.nil?
+    return true if procedure.publiee?
+
+    already_in_use_in_another_procedure?
+  end
+
+  def already_in_use_in_another_procedure?
+    Procedure.joins(revisions: :types_de_champ)
+      .where.not(published_at: nil)
+      .exists?(types_de_champ: { referentiel_id: referentiel.id })
   end
 end
--- spec/factories/referentiel.rb
+++ spec/factories/referentiel.rb
@@ -17,4 +17,10 @@ FactoryBot.define do
     factory :api_referentiel, class: 'Referentiels::APIReferentiel' do
     end
   end
+
+  trait :configured do
+    url { 'https://rnb-api.beta.gouv.fr/api/alpha/buildings/{id}/' }
+    mode { 'exact_match' }
+    test_data { 'PG46YY6YWCX8' }
+  end
 end
