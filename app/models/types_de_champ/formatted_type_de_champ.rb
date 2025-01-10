38c6df421d40e33e82f16c83d6c88ec02d5b6d8b
Christophe Robillard <christophe.robillard@mail.numerique.gouv.fr>
2025-01-10 09:26:09 +0100

    default options for formatted type de champ

--- app/models/types_de_champ/formatted_type_de_champ.rb
+++ app/models/types_de_champ/formatted_type_de_champ.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class TypesDeChamp::FormattedTypeDeChamp < TypesDeChamp::TypeDeChampBase
+  def initialize(*args)
+    super
+    if @type_de_champ.options.empty?
+      @type_de_champ.options = { formatted_mode: 'simple', letters_accepted: true, numbers_accepted: true, special_characters_accepted: true }
+    end
+  end
 end
