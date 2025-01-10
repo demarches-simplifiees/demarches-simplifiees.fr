052e34b8605e50984b9d7758a23f153f2ea018a9
Christophe Robillard <christophe.robillard@mail.numerique.gouv.fr>
2025-01-10 17:43:14 +0100

    change formatted type de champ and validators

--- spec/models/champs/formatted_champ_spec.rb
+++ spec/models/champs/formatted_champ_spec.rb
@@ -14,11 +14,51 @@ describe Champs::FormattedChamp do
     subject { champ.validate(:champs_public_value) }
 
     context 'with simple mode' do
-      let(:type_de_champ) { build(:type_de_champ_formatted, :numbers_accepted) }
+      context 'only numbers accepted' do
+        let(:type_de_champ) { build(:type_de_champ_formatted, :numbers_accepted) }
 
-      let(:value) { "coucou" }
+        context 'with value' do
+          let(:value) { "2534" }
 
-      it { is_expected.to be_falsey }
+          it { is_expected.to be_truthy }
+        end
+        context 'with invalid value' do
+          let(:type_de_champ) { build(:type_de_champ_formatted, :numbers_accepted) }
+          let(:value) { "cou3872cou" }
+
+          it { is_expected.to be_falsey }
+        end
+      end
+    end
+    context 'with advanced mode' do
+      context 'with invalid value' do
+        let(:value) { "blop" }
+        context 'with expression reguliere error message defined' do
+          let(:type_de_champ) do
+            build(:type_de_champ_formatted, :advanced).tap do |tdc|
+              tdc.options[:expression_reguliere_error_message] = "certainement pas"
+              tdc.options[:expression_reguliere] = "/coucou/"
+            end
+          end
+
+          it { is_expected.to be_falsey }
+          it 'has specific error message defined' do
+            subject
+            expect(champ.errors.full_messages_for(:value).first).to eq "certainement pas"
+          end
+        end
+        context 'without expression reguliere error message defined' do
+          let(:type_de_champ) do
+            build(:type_de_champ_formatted, :advanced).tap do |tdc|
+              tdc.options[:expression_reguliere] = "/coucou/"
+            end
+          end
+          it 'has default error message' do
+            subject
+            expect(champ.errors.full_messages_for(:value).first).to eq "n'est pas valide"
+          end
+        end
+      end
     end
   end
 end
