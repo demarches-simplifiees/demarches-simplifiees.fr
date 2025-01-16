077fcd585d51d15fc164b5f5fe1187aad1ed6140
simon lehericey <mail@simon.lehericey.net>
2025-01-16 16:36:33 +0100

    fix: try another way to update address champ

---
+++ app/tasks/maintenance/t20250116_backfill_address_value_json_task.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Maintenance
+  class T20250116BackfillAddressValueJSONTask < MaintenanceTasks::Task
+    # task run on 15/01/2025, needed for address columns
+    include RunnableOnDeployConcern
+    include StatementsHelpersConcern
+
+    run_on_first_deploy
+
+    def collection
+      Champs::AddressChamp.where.not(data: nil)
+    end
+
+    def process(champ)
+      return if champ.data.nil?
+
+      champ.update!(value_json: champ.data)
+
+    rescue ActiveRecord::RecordNotUnique
+      # noop, just a champ without dossier
+    end
+  end
+end
