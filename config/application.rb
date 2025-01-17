ccbdbc6ee7f0ed767eb54ba3dfbc43f455bc7992
Colin Darie <colin@darie.eu>
2025-01-17 12:47:48 +0100

    chore(config): start flipping rails 7.1 defaults

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module TPS
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks cops data generators linters prawn templates))
+    # config.add_autoload_paths_to_load_path = false
 
     # Configuration for the application, engines, and railties goes here.
     #
