6a55cf81cb5b9828414adffba96e5cc6c72f37a3
Colin Darie <colin@darie.eu>
2025-01-20 11:43:32 +0100

    fix: autoload of core extensions

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module TPS
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks cops data generators linters prawn templates))
+    config.autoload_lib(ignore: %w(assets tasks cops data generators linters prawn templates core_ext))
     # config.add_autoload_paths_to_load_path = false
 
     # Configuration for the application, engines, and railties goes here.
