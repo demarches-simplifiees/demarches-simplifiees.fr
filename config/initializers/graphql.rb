91c461a8377c1d478262577443552825c96e0071
Paul Chavard <pro@paul.chavard.net>
2025-01-20 12:44:14 +0100

    chore(graphql): update to 2.4.4

--- config/initializers/graphql.rb
+++ config/initializers/graphql.rb
@@ -1,11 +1,5 @@
 # frozen_string_literal: true
 
-GraphQL::RailsLogger.configure do |config|
-  config.white_list = {
-    'API::V2::GraphqlController' => ['execute']
-  }
-end
-
 module GraphQL
   class Schema
     class Member
