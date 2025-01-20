967756625e1e26e8301cc2a7c57a86876d2bd4c2
Colin Darie <colin@darie.eu>
2025-01-20 12:01:07 +0100

    fix: enum values must not be empty

--- app/models/safe_mailer.rb
+++ app/models/safe_mailer.rb
@@ -5,7 +5,9 @@ class SafeMailer < ApplicationRecord
     raise if SafeMailer.count == 1
   end
 
-  enum :forced_delivery_method, (Rails.application.config.action_mailer&.balancer_settings&.keys || []).to_h { |k| [k.to_sym, k.to_s] }
+  if Rails.application.config.action_mailer.balancer_settings.present?
+    enum :forced_delivery_method, (Rails.application.config.action_mailer.balancer_settings.keys || []).to_h { |k| [k.to_sym, k.to_s] }
+  end
 
   def self.forced_delivery_method
     first&.forced_delivery_method
