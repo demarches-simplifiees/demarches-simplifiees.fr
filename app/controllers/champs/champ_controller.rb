c542a31bd7912fd7711e67a74a5314f34991cdc6
Paul Chavard <pro@paul.chavard.net>
2025-01-20 17:52:56 +0100

    feat(dossier): enable dossier update by stream

--- app/controllers/champs/champ_controller.rb
+++ app/controllers/champs/champ_controller.rb
@@ -8,6 +8,7 @@ class Champs::ChampController < ApplicationController
 
   def find_champ
     dossier = policy_scope(Dossier).includes(:champs, revision: [:types_de_champ]).find(params[:dossier_id])
+    dossier.with_update_stream(current_user)
     type_de_champ = dossier.find_type_de_champ_by_stable_id(params[:stable_id])
     if type_de_champ.repetition?
       dossier.project_champ(type_de_champ)
