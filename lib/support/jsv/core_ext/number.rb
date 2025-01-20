899d170b60309a7e32cbf539f157ec4327e7a639
Colin Darie <colin@darie.eu>
2025-01-20 11:43:16 +0100

    refactor(jsv): drop unused JSV support code

--- lib/support/jsv/core_ext/number.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class Numeric
-  def to_jsv
-    self
-  end
-end
--- lib/support/jsv/core_ext/true_class.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class FalseClass
-  def to_jsv
-    "False"
-  end
-end
