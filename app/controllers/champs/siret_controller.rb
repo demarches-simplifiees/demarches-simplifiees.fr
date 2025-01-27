c508b968ef14ec8192473845282301d8120e3244
Colin Darie <colin@darie.eu>
2025-01-27 10:30:50 +0100

    fix(siret): validate on submit

--- app/controllers/champs/siret_controller.rb
+++ app/controllers/champs/siret_controller.rb
@@ -5,11 +5,12 @@ class Champs::SiretController < Champs::ChampController
     champs_attributes = params.dig(:dossier, :champs_public_attributes) || params.dig(:dossier, :champs_private_attributes)
     siret = champs_attributes.values.first[:value]
 
-    if @champ.fetch_etablissement!(siret, current_user)
-      @siret = @champ.etablissement.siret
+    @siret = if @champ.fetch_etablissement!(siret, current_user)
+      @champ.etablissement.siret
     else
-      @siret = @champ.etablissement_fetch_error_key
+      @champ.etablissement_fetch_error_key
     end
+
     @champ.dossier.touch_champs_changed([:last_champ_updated_at])
   end
 end
--- app/models/champs/siret_champ.rb
+++ app/models/champs/siret_champ.rb
@@ -3,7 +3,37 @@
 class Champs::SiretChamp < Champ
   include SiretChampEtablissementFetchableConcern
 
+  validate :validate_etablissement, if: :validate_champ_value?
+
   def search_terms
     etablissement.present? ? etablissement.search_terms : [value]
+  end
+
+  private
+
+  # We want to validate if SIRET really exists
+  # It's valid when an etablissement have been created in turbo with SIRET controller
+  # NOTE: when API Entreprise is down, user won't be stuck because
+  # SIRET controller creates an etablissement in degraded mode
+  def validate_etablissement
+    return if value.blank?
+    return if etablissement.present?
+
+    validator = SiretFormatValidator.new(attributes: { value: true })
+
+    # siret may have been formatted with spaces
+    validator.validate_each(self, :value, value.gsub(/[[:space:]]/, ""))
+
+    error_type = if errors.empty?
+      :not_found
+    else
+      # length or checksum
+      errors.where(:value).first.type
+    end
+
+    # replace error with same message used by SIRET controller
+    errors.delete(:value)
+    errors.add(:value, error_type,
+      message: I18n.t(error_type, scope: [:errors, :messages, :siret]))
   end
 end
