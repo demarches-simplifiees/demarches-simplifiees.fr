c9ee00b3f97a3b92839c3572503807211061c7b0
Colin Darie <colin@darie.eu>
2025-01-28 16:32:22 +0100

    refactor(siret): cleanup champ etablissement fetchable concern

--- app/models/concerns/siret_champ_etablissement_fetchable_concern.rb
+++ app/models/concerns/siret_champ_etablissement_fetchable_concern.rb
@@ -3,13 +3,12 @@
 module SiretChampEtablissementFetchableConcern
   extend ActiveSupport::Concern
 
-  attr_reader :etablissement_fetch_error_key
-
   def fetch_etablissement!(siret, user)
-    return clear_etablissement!(:empty) if siret.empty?
-    return clear_etablissement!(:invalid_length) if invalid_because?(siret, :length) # i18n-tasks-use t('errors.messages.siret.length')
-    return clear_etablissement!(:invalid_checksum) if invalid_because?(siret, :checksum) # i18n-tasks-use t('errors.messages.siret.checksum')
-    return clear_etablissement!(:not_found) unless (etablissement = APIEntrepriseService.create_etablissement(self, siret, user&.id)) # i18n-tasks-use t('errors.messages.siret.not_found')
+    return clear_etablissement! if siret.empty?
+    return clear_etablissement! if Siret.new(siret:).invalid?
+
+    etablissement = APIEntrepriseService.create_etablissement(self, siret, user&.id)
+    return clear_etablissement! if etablissement.blank?
 
     update!(etablissement:)
   rescue APIEntreprise::API::Error, APIEntrepriseToken::TokenError => error
@@ -17,30 +16,20 @@ module SiretChampEtablissementFetchableConcern
       update!(
         etablissement: APIEntrepriseService.create_etablissement_as_degraded_mode(self, siret, user.id)
       )
-      @etablissement_fetch_error_key = :api_entreprise_down
       false
     else
       Sentry.capture_exception(error, extra: { dossier_id:, siret: })
-      clear_etablissement!(:network_error) # i18n-tasks-use t('errors.messages.siret.network_error')
+      clear_etablissement!
     end
   end
 
   private
 
-  def clear_etablissement!(error_key)
-    @etablissement_fetch_error_key = error_key
-
+  def clear_etablissement!
     etablissement_to_destroy = etablissement
     update!(etablissement: nil)
     etablissement_to_destroy&.destroy
 
     false
-  end
-
-  def invalid_because?(siret, criteria)
-    validatable_siret = Siret.new(siret: siret)
-    return false if validatable_siret.valid?
-
-    validatable_siret.errors.details[:siret].any? && validatable_siret.errors.details[:siret].first[:error] == criteria
   end
 end
