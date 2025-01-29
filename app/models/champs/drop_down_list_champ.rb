36b66e2a8516c2a2d3cfcde7ce7301ea3d54f695
Eric Leroy-Terquem <eric.leroyterquem@beta.gouv.fr>
2025-01-29 14:21:27 +0100

    feat(referentiel): display referentiel value(s) in demande page

--- app/models/champs/drop_down_list_champ.rb
+++ app/models/champs/drop_down_list_champ.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 class Champs::DropDownListChamp < Champ
-  store_accessor :value_json, :other
+  store_accessor :value_json, :other, :referentiel
   THRESHOLD_NB_OPTIONS_AS_RADIO = 5
   THRESHOLD_NB_OPTIONS_AS_AUTOCOMPLETE = 20
   OTHER = '__other__'
   delegate :options_without_empty_value_when_mandatory, to: :type_de_champ
   validate :value_is_in_options, if: -> { validate_champ_value? && !(value.blank? || drop_down_other?) }
+  before_save :store_referentiel
 
   def render_as_radios?
     options = referentiel? ? referentiel_drop_down_options : drop_down_options
@@ -48,6 +49,23 @@ class Champs::DropDownListChamp < Champ
     end
   end
 
+  def store_referentiel
+    return if !self.referentiel_mode?
+    if self.other?
+      self.referentiel = nil
+    else
+      self.referentiel = referentiel_from(value) if value
+    end
+  end
+
+  def referentiel_from(value)
+    if value.present?
+      referentiel_item = self.type_de_champ.referentiel.items.find(value)
+      headers = referentiel_item.referentiel.headers
+      { data: referentiel_item.data.merge(headers:) }
+    end
+  end
+
   def value_other=(value)
     if other?
       write_attribute(:value, value)
@@ -56,6 +74,21 @@ class Champs::DropDownListChamp < Champ
 
   def value_other
     other? ? value : ""
+  end
+
+  def referentiel_item_data
+    return nil if self.referentiel.nil?
+    self.referentiel&.dig('data', 'row')
+  end
+
+  def referentiel_item_first_column_value
+    return nil if self.referentiel.nil?
+    header = self.referentiel&.dig('data', 'headers')&.first
+    self.referentiel&.dig('data', 'row', header&.parameterize&.underscore)
+  end
+
+  def referentiel_headers
+    type_de_champ.referentiel.headers
   end
 
   private
