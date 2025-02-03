f458e6f96b26350802c85e20a242fcaf7283590e
Colin Darie <colin@darie.eu>
2025-02-03 18:30:49 +0100

    feat(ProcedureRevision): add schema_to_llm to expose a ProcedureRevision schema to an LLM
    Co-Authored-By: Colin Darie <colin@darie.eu>

---
+++ app/models/concerns/revision_describable_to_llm_concern.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module RevisionDescribableToLLMConcern
+  def schema_to_llm
+    revision_types_de_champ_public.map do |rtdc|
+      {
+        stable_id: rtdc.stable_id,
+        type: rtdc.type_champ,
+        libelle: rtdc.libelle,
+        mandatory: rtdc.mandatory?,
+        description: rtdc.description,
+        choices: (rtdc.type_de_champ.drop_down_options if rtdc.type_de_champ.choice_type?)
+      }.compact
+    end
+  end
+end
