7bd39387e4f3716377e7e9671d8c96d6b7a2c8b7
Colin Darie <colin@darie.eu>
2025-02-03 18:31:30 +0100

    feat: make a llm suggesting a better revision

---
+++ app/services/llm/openai_client.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'langchain'
+
+# Albert uses OpenAI interface.
+
+module LLM
+  class OpenAIClient
+    def self.instance
+      Langchain::LLM::OpenAI.new(
+        api_key: ENV["LLM_API_KEY"],
+        llm_options: {
+          uri_base: ENV.fetch("LLM_URI_BASE")
+        },
+        default_options: {
+          temperature: ENV.fetch("LLM_TEMPERATURE", 0.2).to_f,
+          chat_model: ENV.fetch("LLM_MODEL_NAME")
+        }
+      )
+    end
+  end
+end
