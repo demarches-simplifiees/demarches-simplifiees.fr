b90836a4c7eb2810e3926651e4356c7b945da264
mfo <mfo@users.noreply.github.com>
2025-02-05 16:39:54 +0100

    feat(llm): add quality score gauge and enhance ProcedureLinter

---
+++ app/components/procedure/simplify_gauge_component.rb
@@ -0,0 +1,12 @@
+class Procedure::SimplifyGaugeComponent < ApplicationComponent
+  def initialize(procedure, revision)
+    @linter = ProcedureLinter.new(procedure, revision)
+  end
+
+  def call
+    safe_join([
+      tag.div(id: 'password_hint', class: "password-complexity complexity-#{@linter.rate % 2}"),
+      tag.h3(class: 'text-center fr-alert__title') { @linter.quali_score }
+    ])
+  end
+end
