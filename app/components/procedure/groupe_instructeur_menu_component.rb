679a3c5c4cf73e3386950fa950ffc3e0bdbfd3d9
Eric Leroy-Terquem <eric.leroyterquem@beta.gouv.fr>
2025-02-07 18:15:23 +0100

    feat(group configuration): make a new sticky sidemenu

---
+++ app/components/procedure/groupe_instructeur_menu_component.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class Procedure::GroupeInstructeurMenuComponent < ApplicationComponent
+  attr_reader :sticky
+  def initialize(procedure:, sticky: false)
+    @procedure = procedure
+    @sticky = sticky
+  end
+
+  private
+
+  def links
+    links = [
+      { name: 'Paramètres du groupe', url: '#parametres-groupe' },
+      { name: 'Dossiers affectés', url: '#dossiers-affectes' },
+      { name: 'Règle(s) de routage', url: '#regles-routage' },
+      { name: 'Affectation des instructeurs', url: '#affectation-instructeurs' },
+      { name: 'Informations de contact', url: '#informations-contact' }
+    ]
+    links.push({ name: 'Tampon de l’attestation', url: '#tampon-attestation' }) if @procedure.attestation_template&.activated?
+    links
+  end
+end
