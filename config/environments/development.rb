24225b8a3cbc55006f3264c2d06f85da74edd9b2
Colin Darie <colin@darie.eu>
2025-02-10 18:45:01 +0100

    chore(mailers): raise error in dev like in prod otherwise it's hard to follow complex rescue/retry mechanism

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
   config.public_file_server.headers = { 'Cache-Control' => 'public, max-age=3600' }
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = ENV.fetch("ACTIVE_STORAGE_SERVICE").to_sym
