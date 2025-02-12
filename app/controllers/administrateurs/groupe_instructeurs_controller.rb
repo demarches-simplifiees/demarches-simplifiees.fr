38b65e1da1e3e534ec120180d07c594441fdefa6
Christophe Robillard <christophe.robillard@mail.numerique.gouv.fr>
2025-02-12 18:16:12 +0100

    fix show typo suggestions when admin add instructeur

--- app/controllers/administrateurs/groupe_instructeurs_controller.rb
+++ app/controllers/administrateurs/groupe_instructeurs_controller.rb
@@ -22,6 +22,7 @@ module Administrateurs
 
       @instructeurs = paginated_instructeurs
       @available_instructeur_emails = available_instructeur_emails
+      @maybe_typos = JSON.parse(params[:maybe_typos]) if params[:maybe_typos]
     end
 
     def options
@@ -131,6 +132,7 @@ module Administrateurs
       @groupe_instructeur = groupe_instructeur
       @instructeurs = paginated_instructeurs
       @available_instructeur_emails = available_instructeur_emails
+      @maybe_typos = JSON.parse(params[:maybe_typos]) if params[:maybe_typos]
     end
 
     def create
@@ -277,12 +279,13 @@ module Administrateurs
       @instructeurs = paginated_instructeurs
       @available_instructeur_emails = available_instructeur_emails
 
+      query_param = { maybe_typos: @maybe_typos.to_json } if @maybe_typos.present?
       if procedure.routing_enabled?
         @groupe_instructeur = groupe_instructeur
-        redirect_to admin_procedure_groupe_instructeur_path(@procedure, @groupe_instructeur)
+        redirect_to admin_procedure_groupe_instructeur_path(@procedure, @groupe_instructeur, query_param)
       else
         @groupes_instructeurs = paginated_groupe_instructeurs
-        redirect_to admin_procedure_groupe_instructeurs_path(@procedure)
+        redirect_to admin_procedure_groupe_instructeurs_path(@procedure, query_param)
       end
     end
 
