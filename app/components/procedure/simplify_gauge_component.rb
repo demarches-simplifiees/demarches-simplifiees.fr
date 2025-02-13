8e57a17ffb393d98ddec5cbbd1fd4c880abcdd60
mfo <mfo@users.noreply.github.com>
2025-02-13 08:53:38 +0100

    wip

--- app/components/procedure/simplify_gauge_component.rb
+++ app/components/procedure/simplify_gauge_component.rb
@@ -5,7 +5,7 @@ class Procedure::SimplifyGaugeComponent < ApplicationComponent
 
   def call
     safe_join([
-      tag.div(id: 'password_hint', class: "password-complexity complexity-#{@linter.rate % 2}"),
+      tag.div(id: 'password_hint', class: "password-complexity complexity-#{@linter.rate / 2 - 1}"),
       tag.h3(class: 'text-center fr-alert__title') { @linter.quali_score }
     ])
   end
