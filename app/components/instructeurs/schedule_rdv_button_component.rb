7e64a8ef09f9309ac74207d8d5f9711f70ef4190
Mathieu Magnin <mathieu@magnin.sh>
2025-02-19 16:06:22 +0100

    [#10457] fix after review

--- app/components/instructeurs/schedule_rdv_button_component.rb
+++ app/components/instructeurs/schedule_rdv_button_component.rb
@@ -4,4 +4,16 @@ class Instructeurs::ScheduleRdvButtonComponent < ApplicationComponent
   def initialize(dossier:)
     @dossier = dossier
   end
+
+  def call
+    button_to instructeur_rdvs_path(@dossier.procedure, @dossier),
+      data: { turbo: false },
+      method: :post,
+      class: 'fr-btn' do
+      safe_join([
+        t('.schedule_rdv'),
+        content_tag(:span, nil, class: 'fr-ml-1w fr-icon-external-link-line', 'aria-hidden': 'true')
+      ])
+    end
+  end
 end
