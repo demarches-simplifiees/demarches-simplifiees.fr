4e8ce3bd3ecfb7192d31fc8c4a433fbaf6db6fee
Mathieu Magnin <mathieu@magnin.sh>
2025-02-19 15:55:02 +0100

    Rename button

--- spec/components/previews/instructeurs/propose_rdv_button_component_preview.rb
+++ spec/components/previews/instructeurs/propose_rdv_button_component_preview.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-class Instructeurs::ProposeRdvButtonComponentPreview < ViewComponent::Preview
+class Instructeurs::ScheduleRdvButtonComponentPreview < ViewComponent::Preview
   def default
-    render(Instructeurs::ProposeRdvButtonComponent.new(dossier: Dossier.new(id: 1, procedure: Procedure.new(id: 1))))
+    render(Instructeurs::ScheduleRdvButtonComponent.new(dossier: Dossier.new(id: 1, procedure: Procedure.new(id: 1))))
   end
 end
