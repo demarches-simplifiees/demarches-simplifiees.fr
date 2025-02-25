c72fa749e28e2cb7c9ad7b8fcae0a1ab11cffb3c
Corinne Durrmeyer <corinne@inseo.fr>
2025-02-25 10:48:46 +0100

    feat(a11y.attachment.destroy): Add translations

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -28,11 +28,11 @@ class AttachmentsController < ApplicationController
         champ.update_timestamps
       end
       champ.piece_justificative_file.reload
-      flash.notice = "La pièce jointe (#{@attachment.blob.filename}) a bien été supprimée. Vous pouvez en <a href=\"##{@champ.input_id}\">ajouter une autre</a>"
+      flash.notice = t("activerecord.models.attachment.successfully_deleted_with_anchor", attachment: @attachment.blob.filename, champ: @champ.input_id)
     else
       @attachment.purge_later
       @attachment_options = attachment_options
-      flash.notice = "La pièce jointe a bien été supprimée."
+      flash.notice = t("activerecord.models.attachment.successfully_deleted_without_anchor", attachment: @attachment.blob.filename)
     end
 
     respond_to do |format|
