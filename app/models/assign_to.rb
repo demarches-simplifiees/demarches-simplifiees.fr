a6536b5c8bdb495e242b31ad0e0e4d8d6018701a
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2025-02-26 09:07:44 -1000

    every night, cancel notifications of new dossier for all inactive instructeurs

--- app/models/assign_to.rb
+++ app/models/assign_to.rb
@@ -6,6 +6,8 @@ class AssignTo < ApplicationRecord
 
   scope :with_email_notifications, -> { where(daily_email_notifications_enabled: true) }
 
+  MIN_INACTIVE_DAYS = 40
+
   def procedure_presentation_or_default_and_errors
     errors = reset_procedure_presentation_if_invalid
     if self.procedure_presentation.nil?
@@ -13,6 +15,28 @@ class AssignTo < ApplicationRecord
       self.procedure_presentation.save if procedure_presentation.valid? && !procedure_presentation.persisted?
     end
     [self.procedure_presentation, errors]
+  end
+
+  def self.cancel_notifications_for_inactive_instructeurs
+    # given a procedure, notifications where instructeur has not followed a dossier since MIN_INACTIVE_DAYS
+    AssignTo.joins(:procedure)
+      .joins(instructeur: { all_follows: { dossier: :procedure } })
+      .where('assign_tos.updated_at < ?', MIN_INACTIVE_DAYS.days.ago)
+      .where(instant_email_dossier_notifications_enabled: true)
+      .where("procedures.id = procedures_dossiers.id")
+      .group("assign_tos.id")
+      .having('max(greatest(follows.demande_seen_at, follows.messagerie_seen_at, follows.annotations_privees_seen_at, follows.avis_seen_at)) < ?', MIN_INACTIVE_DAYS.days.ago)
+      .update_all(instant_email_dossier_notifications_enabled: false)
+
+    # Given a procedure, notifications for a procedure where instructeur has never followed a dossier
+    AssignTo.joins(:procedure)
+      .joins(:instructeur)
+      .left_outer_joins(instructeur: :all_follows)
+      .where('assign_tos.updated_at < ?', MIN_INACTIVE_DAYS.days.ago)
+      .where(instant_email_dossier_notifications_enabled: true)
+      .where(follows: { id: nil })
+      .group("assign_tos.id")
+      .update_all(instant_email_dossier_notifications_enabled: false)
   end
 
   private
