8cb5115eca329f957c5dfd48a9276e4e2e9bc3e1
Colin Darie <colin@darie.eu>
2025-02-27 12:00:03 +0100

    fix(task): make migration champ formatted performant

--- spec/tasks/maintenance/t20241127_migrate_champ_expression_reguliere_to_formatted_task_spec.rb
+++ spec/tasks/maintenance/t20241127_migrate_champ_expression_reguliere_to_formatted_task_spec.rb
@@ -9,25 +9,13 @@ module Maintenance
 
     describe "#process" do
       let(:champ_regex) { Champ.where(type: ["Champs::ExpressionReguliereChamp"]).first }
-      let(:champ_formatted) { Champ.where(type: ["Champs::FormattedChamp"]).first }
 
       it "update type of regex champ" do
         dossier
-        described_class.process(champ_regex)
-        expect(champ_regex.type).to eq "Champs::FormattedChamp"
-      end
+        champ_id = champ_regex.id
+        described_class.process(0)
 
-      it "doesn't change type of formatted champ" do
-        dossier
-        described_class.process(champ_formatted)
-        expect(champ_formatted.type).to eq "Champs::FormattedChamp"
-      end
-    end
-
-    describe '#collection' do
-      it "works" do
-        dossier # create dossier
-        expect(described_class.collection.map(&:type)).to eq(["Champs::ExpressionReguliereChamp", "Champs::FormattedChamp"])
+        expect(Champ.find(champ_id).type).to eq "Champs::FormattedChamp"
       end
     end
   end
