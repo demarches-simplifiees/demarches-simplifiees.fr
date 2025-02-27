111c67adfb6402d4493cb365d75cc3993c4ede56
Colin Darie <colin@darie.eu>
2025-02-27 12:00:03 +0100

    fix(task): make migration champ formatted performant

--- app/tasks/maintenance/t20241127_migrate_champ_expression_reguliere_to_formatted_task.rb
+++ app/tasks/maintenance/t20241127_migrate_champ_expression_reguliere_to_formatted_task.rb
@@ -13,14 +13,16 @@ module Maintenance
     # If possible, leave commented for manual execution later.
     run_on_first_deploy
 
+    BATCH_SIZE = 100_000
+
     def collection
-      with_statement_timeout("15min") do
-        Champ.where(type: ["Champs::ExpressionReguliereChamp", "Champs::FormattedChamp"])
-      end
+      rows_count = with_statement_timeout("5min") { Champ.where(type: "Champs::ExpressionReguliereChamp").count }
+      ((rows_count / BATCH_SIZE) + 1).times.to_a
     end
 
-    def process(champ)
-      champ.update(type: "Champs::FormattedChamp")
+    def process(_)
+      champ_ids = with_statement_timeout("5min") { Champ.where(type: "Champs::ExpressionReguliereChamp").limit(BATCH_SIZE).ids }
+      with_statement_timeout("5min") { Champ.where(id: champ_ids).update_all(type: "Champs::FormattedChamp") }
     end
 
     def count = nil
