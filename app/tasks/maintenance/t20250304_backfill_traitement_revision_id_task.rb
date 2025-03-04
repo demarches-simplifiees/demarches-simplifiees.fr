fe33b5e5d9a90b171937432f016a2fb6d500f69e
Paul Chavard <pro@paul.chavard.net>
2025-03-04 11:14:45 +0100

    chore(dossier): backfill dossier depose traitement revision

---
+++ app/tasks/maintenance/t20250304_backfill_traitement_revision_id_task.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Maintenance
+  class T20250304BackfillTraitementRevisionIdTask < MaintenanceTasks::Task
+    # Documentation: cette tâche assigne la revision_id qui manque aux traitements des vieux dossiers
+
+    include RunnableOnDeployConcern
+    include StatementsHelpersConcern
+
+    # Uncomment only if this task MUST run imperatively on its first deployment.
+    # If possible, leave commented for manual execution later.
+    # run_on_first_deploy
+
+    def collection
+      Traitement.joins(dossier: :procedure)
+        .where(state: Dossier.states.fetch(:en_construction), revision_id: nil)
+        .where('traitements.processed_at = dossiers.depose_at')
+        .order(:id)
+    end
+
+    def process(traitement)
+      revisions = traitement.dossier
+        .procedure
+        .revisions
+        .where.not(published_at: nil)
+        .reorder(:published_at)
+
+      revision = revisions.where(published_at: ..traitement.processed_at).last
+      traitement.update!(revision:) if revision.present?
+    end
+  end
+end
