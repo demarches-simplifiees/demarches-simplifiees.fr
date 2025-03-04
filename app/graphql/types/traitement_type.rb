98e68675b08127ba4b4b3de1a6a2560ee0e00450
Paul Chavard <pro@paul.chavard.net>
2025-03-04 20:52:01 +0100

    feat(traitement): expose traitement event

--- app/graphql/types/traitement_type.rb
+++ app/graphql/types/traitement_type.rb
@@ -2,8 +2,15 @@
 
 module Types
   class TraitementType < Types::BaseObject
+    class TraitementEvent < Types::BaseEnum
+      Traitement::EVENT.each do |event, name|
+        value(event, name, value: event)
+      end
+    end
+
     global_id_field :id
-    field :state, Types::DossierType::DossierState, null: false
+    field :state, Types::DossierType::DossierState, null: false, deprecation_reason: 'Utilisez le champ `event` à la place.'
+    field :event, TraitementEvent, null: false
     field :date_traitement, GraphQL::Types::ISO8601DateTime, null: false, method: :processed_at
     field :email_agent_traitant, String, null: true, method: :instructeur_email
     field :motivation, String, null: true
