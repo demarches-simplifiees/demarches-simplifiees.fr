7c454ba189b236d7e01fbe69206b514d2935f437
Mathieu Magnin <mathieu@magnin.sh>
2025-03-04 14:51:37 +0100

    Try to fix flaky test without downgrading concurrent-ruby https://github.com/rails/rails/pull/54264\#issuecomment-2596149819

--- config/boot.rb
+++ config/boot.rb
@@ -2,5 +2,6 @@
 
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
+require "logger" # force logger to be loaded before bundler/setup https://github.com/rails/rails/pull/54264#issuecomment-2596149819, to be removed after rails 7.2.x 
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
