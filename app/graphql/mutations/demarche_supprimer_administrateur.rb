e8c8e83c94e4fa73f359d6ab2227e762c228105f
Mathieu HIREL <m.hirel@senat.fr>
2025-03-05 12:30:05 +0100

    feat(graphql): Pouvoir supprimer un administrateur a une démarche via l'api graphql

---
+++ app/graphql/mutations/demarche_supprimer_administrateur.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module Mutations
+  class DemarcheSupprimerAdministrateur < Mutations::BaseMutation
+    description "Supprimer un administrateur d'une démarche"
+
+    argument :demarche, Types::DemarcheDescriptorType::FindDemarcheInput, "La démarche", required: true
+    argument :administrateurs, [Types::ProfileInput], "Administrateur à retirer.", required: true
+
+    field :demarche, Types::DemarcheDescriptorType, null: true
+    field :errors, [Types::ValidationErrorType], null: true
+    field :warnings, [Types::WarningMessageType], null: true
+
+    def resolve(demarche:, administrateurs:)
+      demarche_number = demarche.number.presence || ApplicationRecord.id_from_typed_id(demarche.id)
+      demarche = Procedure.find_by(id: demarche_number)
+      ids, emails = partition_administrators_by(administrateurs)
+
+      administrateurs = demarche.administrateurs.find_all_by_identifier(ids:, emails:)
+
+      if administrateurs.present?
+        administrateurs.each { demarche.administrateurs.delete(_1) }
+        demarche.reload
+      end
+
+      { demarche: }
+    end
+
+  end
+end
