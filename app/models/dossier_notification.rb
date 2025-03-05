5ab9ffdd376793142007ae1e2cd5bccd1a4491fa
benoitqueyron <72251526+Benoit-MINT@users.noreply.github.com>
2025-03-05 15:17:52 +0100

    create notification

--- app/models/dossier_notification.rb
+++ app/models/dossier_notification.rb
@@ -6,4 +6,23 @@ class DossierNotification < ApplicationRecord
   validates :groupe_instructeur_id, presence: true, if: -> { instructeur_id.nil? }
   validates :instructeur_id, presence: true, if: -> { groupe_instructeur_id.nil? }
 
+  enum :notification_type, {
+    dossier_depose: 'dossier_depose'
+  }
+
+  def self.create_notification(dossier, notification_type, instructeur = nil)
+    params = {
+      dossier_id: dossier.id,
+      notification_type:,
+      instructeur_id: instructeur&.id,
+      groupe_instructeur_id: dossier.groupe_instructeur_id
+    }
+
+    DossierNotification.create_or_find_by!(params) do |notification|
+      case notification_type
+      when :dossier_depose
+        notification.display_at = dossier.depose_at + 7.days
+      end
+    end
+  end
 end
