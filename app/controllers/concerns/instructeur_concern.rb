cc733a5aece9bfb451798287f93a84dd184d5f60
benoitqueyron <72251526+Benoit-MINT@users.noreply.github.com>
2025-03-11 19:33:31 +0100

    display notifications on dossier#show

--- app/controllers/concerns/instructeur_concern.rb
+++ app/controllers/concerns/instructeur_concern.rb
@@ -7,5 +7,9 @@ module InstructeurConcern
     def retrieve_procedure_presentation
       @procedure_presentation ||= current_instructeur.procedure_presentation_for_procedure_id(params[:procedure_id])
     end
+
+    def set_notifications_dossier
+      @notifications = DossierNotification.notifications_for_instructeur_dossier(current_instructeur, dossier)
+    end
   end
 end
