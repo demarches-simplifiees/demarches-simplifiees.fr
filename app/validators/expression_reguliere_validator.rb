18a833d629a7e062f3d81573002821c404fcd43e
Colin Darie <colin@darie.eu>
2025-03-18 17:42:49 +0100

    fix(admin): no error on preview dossier without regex configured

--- app/validators/expression_reguliere_validator.rb
+++ app/validators/expression_reguliere_validator.rb
@@ -4,13 +4,15 @@ class ExpressionReguliereValidator < ActiveModel::Validator
   TIMEOUT = 1.second.freeze
 
   def validate(record)
+    return if record.value.blank?
+
     expression_reguliere = record.expression_reguliere.presence || options[:expression_reguliere]
     expression_reguliere_error_message = record.expression_reguliere_error_message.presence || options[:expression_reguliere_error_message]
 
-    if record.value.present?
-      if !record.value.match?(Regexp.new(expression_reguliere, timeout: TIMEOUT))
-        record.errors.add(:value, :invalid_regexp, expression_reguliere_error_message: expression_reguliere_error_message)
-      end
+    return if expression_reguliere.blank? # an admin may not yet filled regex on preview dossier
+
+    if !record.value.match?(Regexp.new(expression_reguliere, timeout: TIMEOUT))
+      record.errors.add(:value, :invalid_regexp, expression_reguliere_error_message: expression_reguliere_error_message)
     end
   rescue Regexp::TimeoutError
     record.errors.add(:expression_reguliere, :evil_regexp)
