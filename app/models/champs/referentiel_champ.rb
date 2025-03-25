d9d9af8a0d04e353e96e80910ded82a53c249d7b
mfo <mfo@users.noreply.github.com>
2025-03-25 15:55:11 +0100

    feat(ReferentielComponent): wire referentiel champ with FetchExternalDataJob design

--- app/models/champs/referentiel_champ.rb
+++ app/models/champs/referentiel_champ.rb
@@ -1,4 +1,29 @@
 # frozen_string_literal: true
 
 class Champs::ReferentielChamp < Champ
+  delegate :referentiel, to: :type_de_champ
+
+  def fetch_external_data
+    ReferentielService.new(referentiel:).call(external_id)
+  end
+
+  def update_with_external_data!(data:)
+    update!(data:, value_json: todo_map_stuff(data:))
+  end
+
+  def todo_map_stuff(data:)
+    data
+  end
+
+  def fetch_external_data?
+    true
+  end
+
+  def poll_external_data?
+    true
+  end
+
+  def displayable_value
+    external_id
+  end
 end
--- app/services/referentiel_service.rb
+++ app/services/referentiel_service.rb
@@ -9,21 +9,30 @@ class ReferentielService
     @referentiel = referentiel
   end
 
+  def call(query_params)
+    result = API::Client.new.call(url: referentiel.url.gsub('{id}', query_params))
+
+    case result
+    in Success(body:)
+      Success(body)
+    in Failure
+      result
+    end
+  end
+
   def test
     case referentiel
     when Referentiels::APIReferentiel
-      result = API::Client.new.call(url: referentiel.url.gsub('{id}', referentiel.test_data))
+      result = call(referentiel.test_data)
 
       case result
-      in Success(data)
-        referentiel.update(last_response: { status: 200, body: data.body })
+      in Success(body)
+        referentiel.update(last_response: { status: 200, body: })
         true
       in Failure(data)
         referentiel.update(last_response: { status: data.code, body: data.try(:body) })
         false
       end
-    else
-      fail "not yet implemented: #{referentiel.type}"
     end
   end
 end
