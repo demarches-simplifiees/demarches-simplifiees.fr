869e7a917031d7ea48bca316fbdfe76b93096c06
mfo <mfo@users.noreply.github.com>
2025-03-25 17:28:19 +0100

    feat(Champs::Referentiel): make it works with validation

--- app/models/champs/referentiel_champ.rb
+++ app/models/champs/referentiel_champ.rb
@@ -3,6 +3,13 @@
 class Champs::ReferentielChamp < Champ
   delegate :referentiel, to: :type_de_champ
 
+  validates :value, presence: true, allow_blank: true, allow_nil: true, if: -> { api? && validate_champ_value? }
+  validate :api_response_stored, if: -> { value.present? && api? && validate_champ_value? }
+
+  def value
+    external_id
+  end
+
   def fetch_external_data
     ReferentielService.new(referentiel:).(external_id)
   end
@@ -23,7 +30,15 @@ class Champs::ReferentielChamp < Champ
     true
   end
 
-  def displayable_value
-    external_id
+  private
+
+  def api?
+    type_de_champ.api?
+  end
+
+  def api_response_stored
+    return if data.present?
+
+    errors.add(:value, :api_response_not_stored)
   end
 end
