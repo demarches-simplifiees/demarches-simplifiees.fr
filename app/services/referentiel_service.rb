65d64e3722b0f7d684f3837abf1b44c7c07c8746
mfo <mfo@users.noreply.github.com>
2025-03-25 17:28:19 +0100

    feat(Champs::Referentiel): make it works with validation

--- app/services/referentiel_service.rb
+++ app/services/referentiel_service.rb
@@ -3,6 +3,9 @@
 class ReferentielService
   include Dry::Monads[:result]
 
+  RETRYABLE_STATUS_CODES = [429, 500, 503, 408, 502].freeze
+  NON_RETRYABLE_STATUS_CODES = [404, 400, 403, 401].freeze
+
   attr_reader :referentiel, :service
 
   def initialize(referentiel:)
@@ -15,8 +18,12 @@ class ReferentielService
     case result
     in Success(body:)
       Success(body)
+    in Failure(code:) if code.in?(NON_RETRYABLE_STATUS_CODES) # search may not have been found
+      Failure(retryable: false, reason: StandardError.new('Not retryable: 404, 400, 403, 401'), code:)
+    in Failure(code:) if code.in?(RETRYABLE_STATUS_CODES) # api may be rate limited, or down etc..
+      Failure(retryable: true, reason: StandardError.new('Retryable: 429, 500, 503, 408, 502'), code:)
     in Failure
-      result
+      Failure(retryable: false, reason: StandardError.new('Unknown error'), code:)
     end
   end
 
@@ -24,13 +31,12 @@ class ReferentielService
     case referentiel
     when Referentiels::APIReferentiel
       result = call(referentiel.test_data)
-
       case result
       in Success(body)
-        referentiel.update(last_response: { status: 200, body: })
+        referentiel.update_column(:last_response, { status: 200, body: })
         true
       in Failure(data)
-        referentiel.update(last_response: { status: data.code, body: data.try(:body) })
+        referentiel.update_column(:last_response, { status: data[:code], body: data[:body] })
         false
       end
     end
