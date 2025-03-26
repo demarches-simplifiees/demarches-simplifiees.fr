fcfff399469fdbed6e5a665e971b4353e27edf47
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2025-03-25 14:36:24 -1000

    ci_tests: reinsert integer min max validations specific to pf

--- app/models/champs/integer_number_champ.rb
+++ app/models/champs/integer_number_champ.rb
@@ -11,6 +11,19 @@ class Champs::IntegerNumberChamp < Champ
     }
   }, if: :validate_champ_value_or_prefill?
 
+  validate :min_max_validation, if: :validate_champ_value_or_prefill?
+
+  def min_max_validation
+    return if value.blank?
+
+    if type_de_champ.min.present? && value.to_i < type_de_champ.min.to_i
+      errors.add(:value, :greater_than_or_equal_to, value: value, count: type_de_champ.min.to_i)
+    end
+    if type_de_champ.max.present? && value.to_i > type_de_champ.max.to_i
+      errors.add(:value, :less_than_or_equal_to, value: value, count: type_de_champ.max.to_i)
+    end
+  end
+
   def format_value
     return if value.blank?
 
