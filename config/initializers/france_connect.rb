43ec22409a66a02c310c557cbb4aa4488449ac28
simon lehericey <mail@simon.lehericey.net>
2025-03-31 19:18:11 +0200

    feat: add FranceConnect jwks and issuer entries retrieved by the discover mechanism

--- config/initializers/france_connect.rb
+++ config/initializers/france_connect.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
-FRANCE_CONNECT = {
-  particulier: Rails.application.secrets.france_connect_particulier
-}
+if ENV['FC_PARTICULIER_BASE_URL'].present?
+  discover = OpenIDConnect::Discovery::Provider::Config.discover!("#{ENV.fetch('FC_PARTICULIER_BASE_URL')}/api/v2")
+
+  FRANCE_CONNECT = {
+    particulier: Rails.application.secrets.france_connect_particulier
+  }
+
+  FRANCE_CONNECT[:particulier][:jwks] = discover.jwks
+  FRANCE_CONNECT[:particulier][:issuer] = discover.issuer
+end
