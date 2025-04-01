ecd3b8582eadd364aae1c21e65eb064022bbcd3c
simon lehericey <mail@simon.lehericey.net>
2025-04-01 09:42:33 +0200

    clean: sort PRO_CONNECT entry for easier comparison

--- config/initializers/pro_connect.rb
+++ config/initializers/pro_connect.rb
@@ -4,16 +4,16 @@ if ENV['PRO_CONNECT_BASE_URL'].present?
   discover = OpenIDConnect::Discovery::Provider::Config.discover!("#{ENV.fetch('PRO_CONNECT_BASE_URL')}/api/v2")
 
   PRO_CONNECT = {
+    authorization_endpoint: discover.authorization_endpoint,
+    client_id: ENV.fetch('PRO_CONNECT_ID'),
+    end_session_endpoint: discover.end_session_endpoint,
+    identifier: ENV.fetch('PRO_CONNECT_ID'),
     issuer: discover.issuer,
     jwks: discover.jwks,
-    authorization_endpoint: discover.authorization_endpoint,
-    token_endpoint: discover.token_endpoint,
-    userinfo_endpoint: discover.userinfo_endpoint,
-    end_session_endpoint: discover.end_session_endpoint,
-    client_id: ENV.fetch('PRO_CONNECT_ID'),
-    identifier: ENV.fetch('PRO_CONNECT_ID'),
+    redirect_uri: ENV.fetch('PRO_CONNECT_REDIRECT'),
     secret: ENV.fetch('PRO_CONNECT_SECRET'),
-    redirect_uri: ENV.fetch('PRO_CONNECT_REDIRECT')
+    token_endpoint: discover.token_endpoint,
+    userinfo_endpoint: discover.userinfo_endpoint
   }
 
   if ENV['PRO_CONNECT_GOUV_ID'].present?
