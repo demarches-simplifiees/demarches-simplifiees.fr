e72c1c477ef83852ae16e9697fd733ca7bcce0a0
simon lehericey <mail@simon.lehericey.net>
2025-04-01 10:47:43 +0200

    refactor: remove secrets indirection, use discover for conf

--- config/initializers/france_connect.rb
+++ config/initializers/france_connect.rb
@@ -1,12 +1,21 @@
 # frozen_string_literal: true
 
-if ENV['FC_PARTICULIER_BASE_URL'].present?
+if ENV.fetch("FRANCE_CONNECT_ENABLED", "enabled") == "enabled"
   discover = OpenIDConnect::Discovery::Provider::Config.discover!("#{ENV.fetch('FC_PARTICULIER_BASE_URL')}/api/v2")
 
-  FRANCE_CONNECT = {
-    particulier: Rails.application.secrets.france_connect_particulier
-  }
+  FRANCE_CONNECT = {}
 
-  FRANCE_CONNECT[:particulier][:jwks] = discover.jwks
-  FRANCE_CONNECT[:particulier][:issuer] = discover.issuer
+  protocol = Rails.env.production? ? 'https' : 'http'
+
+  FRANCE_CONNECT[:particulier] = {
+    authorization_endpoint: discover.authorization_endpoint,
+    identifier: ENV.fetch('FC_PARTICULIER_ID'),
+    issuer: discover.issuer,
+    jwks: discover.jwks,
+    logout_endpoint: discover.end_session_endpoint,
+    redirect_uri: "#{protocol}://#{ENV['APP_HOST']}/france_connect/particulier/callback",
+    secret: ENV.fetch('FC_PARTICULIER_SECRET'),
+    token_endpoint: discover.token_endpoint,
+    userinfo_endpoint: discover.userinfo_endpoint
+  }
 end
