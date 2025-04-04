03d468ecb5032c84a26e13df666773222190bbe2
mfo <mfo@users.noreply.github.com>
2025-04-04 15:54:49 +0200

    feat(Champs::ReferentielChamp): when setting external_id, ensure to clean polled attributes

--- app/models/champs/referentiel_champ.rb
+++ app/models/champs/referentiel_champ.rb
@@ -3,12 +3,9 @@
 class Champs::ReferentielChamp < Champ
   delegate :referentiel, to: :type_de_champ
 
-  validates :value, presence: true, allow_blank: true, allow_nil: true, if: -> { api? && validate_champ_value? }
-  validate :api_response_stored, if: -> { value.present? && api? && validate_champ_value? }
-
-  def value
-    external_id
-  end
+  validates :value, presence: true, allow_blank: true, allow_nil: true, if: -> { validate_champ_value? }
+  validate :api_response_stored, if: -> { value.present? && validate_champ_value? && !fetch_external_data_pending? }
+  before_save :clear_previous_result, if: -> { external_id_changed? }
 
   def fetch_external_data
     ReferentielService.new(referentiel:).(external_id)
@@ -32,13 +29,18 @@ class Champs::ReferentielChamp < Champ
 
   private
 
-  def api?
-    type_de_champ.api?
-  end
-
   def api_response_stored
     return if data.present?
 
     errors.add(:value, :api_response_not_stored)
+  end
+
+  def clear_previous_result
+    return if !external_id_changed?
+
+    self.value = nil
+    self.data = nil
+    self.value_json = nil
+    self.fetch_external_data_exceptions = []
   end
 end
