458704ecbee9e34f988edabe7edfb01bd8e36563
benoitqueyron <72251526+Benoit-MINT@users.noreply.github.com>
2025-04-09 17:37:50 +0200

    TS: truncate notifications on index procedures

---
+++ app/javascript/controllers/truncate_notifications_controller.ts
@@ -0,0 +1,67 @@
+import { ApplicationController } from './application_controller';
+
+export class TruncateNotificationsController extends ApplicationController {
+  connect() {
+    const notificationContainerWidth =
+      this.calculateNotificationContainerWidth();
+
+    const notifications = document.querySelectorAll<HTMLElement>(
+      '.notification-dossiers'
+    );
+    notifications.forEach((notificationType) => {
+      const notificationDossiers = Array.from(
+        notificationType.querySelectorAll<HTMLElement>('.notification-dossier')
+      );
+      const indicator = notificationType.querySelector<HTMLElement>(
+        '.notification-indicator'
+      );
+
+      let usedWidth = 0;
+      let visibleCount = 0;
+      let truncateContainer = false;
+
+      for (const notification of notificationDossiers) {
+        const notificationWidth = notification.offsetWidth;
+        if (usedWidth + notificationWidth < notificationContainerWidth) {
+          usedWidth += notificationWidth;
+          visibleCount++;
+        } else {
+          const hiddenCount = notificationDossiers.length - visibleCount - 1;
+          truncateContainer = this.truncateNotification(
+            truncateContainer,
+            hiddenCount,
+            indicator!,
+            notification
+          );
+        }
+      }
+    });
+  }
+
+  private calculateNotificationContainerWidth() {
+    const container = document.querySelector<HTMLElement>(
+      '.notification-container-type'
+    );
+    const notificationBadge = document.querySelector<HTMLElement>(
+      '.notification-badge'
+    );
+    return container!.offsetWidth - notificationBadge!.offsetWidth;
+  }
+
+  private truncateNotification(
+    truncateContainer: boolean,
+    hiddenCount: number,
+    indicator: HTMLElement,
+    notification: HTMLElement
+  ) {
+    if (truncateContainer == false) {
+      if (hiddenCount > 0) {
+        indicator!.textContent = `... +${hiddenCount}`;
+      }
+      notification.style.overflow = 'hidden';
+    } else {
+      notification.classList.add('hidden');
+    }
+    return true;
+  }
+}
