57938d3b48e98b81bae2f88f1609c9a2256ed633
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2025-04-30 15:23:24 -1000

    fix crash on procedure editor when regex champ has invalid regex definition

--- app/validators/types_de_champ/expression_reguliere_validator.rb
+++ app/validators/types_de_champ/expression_reguliere_validator.rb
@@ -4,7 +4,14 @@ class TypesDeChamp::ExpressionReguliereValidator < ActiveModel::EachValidator
       .flat_map { _1.repetition? ? procedure.draft_revision.children_of(_1) : _1 }
       .each do |tdc|
       if tdc.expression_reguliere? && tdc.invalid_regexp?
-        procedure.errors.add(:expression_reguliere, type_de_champ: tdc)
+        # Pf DS next line crashes the page when there's an error
+        # procedure.errors.add(:expression_reguliere, type_de_champ: tdc)
+        # pf replaced previous line by this based on drop_down mecanism
+        procedure.errors.add(
+          attribute,
+          tdc.errors.map(&:message).join(","),
+          type_de_champ: tdc
+        )
       end
     end
   end
