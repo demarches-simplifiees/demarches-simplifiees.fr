c79b61e217803de5e5073c43b1dd4dc1b07658a8
Mathieu Magnin <mathieu@magnin.sh>
2025-05-06 11:53:59 +0200

    Add aria labelled_by

--- app/components/editable_champ/repetition_row_component.rb
+++ app/components/editable_champ/repetition_row_component.rb
@@ -8,9 +8,17 @@ class EditableChamp::RepetitionRowComponent < ApplicationComponent
 
   attr_reader :row_id, :row_number
 
+  def has_fieldset?
+    @types_de_champ.size > 1
+  end
+
+  def fieldset_legend_id
+    "#{@type_de_champ.html_id}-legend"
+  end
+
   private
 
   def section_component
-    EditableChamp::SectionComponent.new(dossier: @dossier, types_de_champ: @types_de_champ, row_id:)
+    EditableChamp::SectionComponent.new(dossier: @dossier, types_de_champ: @types_de_champ, row_id:, row_number: has_fieldset? ? nil : @row_number, input_labelled_by_prefix: has_fieldset? ? fieldset_legend_id : nil)
   end
 end
