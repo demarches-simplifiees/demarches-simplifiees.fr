1fe5197e0ac24047c4e45d9893cc61fd49c575f7
Paul Chavard <pro@paul.chavard.net>
2025-05-12 11:30:23 +0200

    chore(react): use PortalProvider

--- app/javascript/components/Layout.tsx
+++ app/javascript/components/Layout.tsx
@@ -1,12 +1,19 @@
 import { I18nProvider } from 'react-aria-components';
+import { UNSAFE_PortalProvider } from 'react-aria';
 import { StrictMode, type ReactNode } from 'react';
+import { findOrCreateContainerElement } from '@coldwired/react';
+
+const getContainer = () =>
+  findOrCreateContainerElement('rac-portal') as HTMLElement;
 
 export function Layout({ children }: { children: ReactNode }) {
   const locale = document.documentElement.lang;
   console.debug(`locale: ${locale}`);
   return (
     <I18nProvider locale={locale}>
-      <StrictMode>{children}</StrictMode>
+      <UNSAFE_PortalProvider getContainer={getContainer}>
+        <StrictMode>{children}</StrictMode>
+      </UNSAFE_PortalProvider>
     </I18nProvider>
   );
 }
