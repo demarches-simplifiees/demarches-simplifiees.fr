034519b4b72897b1c3d9a20fbf7671b328b05686
Colin Darie <colin@darie.eu>
2025-05-21 12:15:02 +0200

    chore: disable lock timeout for index creation on champs

--- db/migrate/20250520110339_add_champs_nulls_not_distinct_index.rb
+++ db/migrate/20250520110339_add_champs_nulls_not_distinct_index.rb
@@ -4,6 +4,7 @@ class AddChampsNullsNotDistinctIndex < ActiveRecord::Migration[7.1]
   disable_ddl_transaction!
 
   def change
+    StrongMigrations.lock_timeout = 0 # default is 10s
     add_index :champs, [:dossier_id, :stream, :stable_id, :row_id], unique: true, algorithm: :concurrently, nulls_not_distinct: true, name: 'index_champs_on_stream_and_public_id'
   end
 end
