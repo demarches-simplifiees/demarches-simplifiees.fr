b622b16ebcd8139996695e9ffde2f42ada9bd44f
Colin Darie <colin@darie.eu>
2025-05-22 17:10:45 +0200

    chore(maintenancetask): clarify when we can use with_statement_timeout

--- app/tasks/maintenance/concerns/statements_helpers_concern.rb
+++ app/tasks/maintenance/concerns/statements_helpers_concern.rb
@@ -7,11 +7,20 @@ module Maintenance
     included do
       # Execute block in transaction with a local statement timeout.
       # A value of 0 disable the timeout.
+      # IMPORTANT: it has NO effect if the block returns an lazy loaded collection, like an ActiveRecord::Relation!
+      # Instead it should be used either when `collection` returns an enumerable object, or in `count`.
       #
-      # Example:
+      # Examples:
       # def collection
-      #   with_statement_timeout("5min") do
-      #     Dossier.all
+      #   with_statement_timeout("0") do
+      #     Champ.all.pluck(:id)
+      #     # No effect with Dossier.all because the collection will be lazy loaded later in batches by MaintenanceTask.
+      #   end
+      # end
+      #
+      # def count
+      #   with_statement_timeout("15min") do
+      #     collection.count(:id)
       #   end
       # end
       def with_statement_timeout(timeout)
