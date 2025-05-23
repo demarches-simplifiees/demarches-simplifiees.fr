6f2e6d18aeb0db2712623af95d5cccdd0d9b81fb
Mathieu Magnin <mathieu@magnin.sh>
2025-05-23 11:14:11 +0200

    [#11159] Add pro connect restricted card

---
+++ spec/components/procedure/card/pro_connect_restricted_component_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Procedure::Card::ProConnectRestrictedComponent, type: :component do
+  before do
+    allow_any_instance_of(described_class).to receive(:feature_enabled?).and_return(true)
+  end
+
+  subject { render_inline(described_class.new(procedure:)) }
+
+  let(:procedure) { create(:procedure, pro_connect_restricted:) }
+
+  context "when ProConnect restriction is enabled" do
+    let(:pro_connect_restricted) { true }
+
+    it { is_expected.to have_css('p.fr-badge.fr-badge--success', text: "Activée") }
+    it { is_expected.to have_css('h3.fr-h6', text: "ProConnect") }
+  end
+
+  context "when ProConnect restriction is disabled" do
+    let(:pro_connect_restricted) { false }
+
+    it { is_expected.to have_css('p.fr-badge', text: "Désactivée") }
+    it { is_expected.to have_css('h3.fr-h6', text: "ProConnect") }
+  end
+end
