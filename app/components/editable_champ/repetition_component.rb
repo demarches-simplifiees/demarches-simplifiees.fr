245d42df6e5c19e0c75753c122a0c3d10846d071
Mathieu Magnin <mathieu@magnin.sh>
2025-05-27 11:43:29 +0200

    fix fieldset id when only one champ per repetition

--- app/components/editable_champ/repetition_component.rb
+++ app/components/editable_champ/repetition_component.rb
@@ -5,6 +5,10 @@ class EditableChamp::RepetitionComponent < EditableChamp::EditableChampBaseCompo
     @champ.description.present? ? { describedby: dom_id(@champ, :repetition) } : {}
   end
 
+  def legend_id
+    "#{@champ.html_id}-legend"
+  end
+
   def notice_params
     @champ.description.present? ? { id: dom_id(@champ, :repetition) } : {}
   end
