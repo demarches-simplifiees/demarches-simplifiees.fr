6e9ae83545629b42fbe6284d5bf097511c0aebdb
baptiste <baptiste.a.coulon@capgemini.com>
2025-05-28 15:17:16 +0200

    [5030](feat) Update dossier_champ user
    refactor form and add simplecombobox

--- app/components/editable_champ/dossier_link_component.rb
+++ app/components/editable_champ/dossier_link_component.rb
@@ -8,4 +8,24 @@ class EditableChamp::DossierLinkComponent < EditableChamp::EditableChampBaseComp
   def dossier
     @dossier ||= @champ.blank? ? nil : Dossier.visible_by_administration.find_by(id: @champ.to_s)
   end
+
+  def dossier_options
+    dossiers = @champ.procedure.types_de_champ_for_tags.last.procedures.first.dossiers
+    dossiers.map do |dossier|
+      {
+        value: dossier.id.to_s,
+        label: dossier.id.to_s
+      }
+    end
+  end
+
+  def react_props
+    {
+      items: dossier_options,
+      placeholder: "Sélectionnez un dossier",
+      name: "dossier[champs_public_attributes][#{@champ.public_id}][value]",
+      id: @champ.input_id,
+      class: "#{@champ.blank? ? '' : 'small-margin'}"
+    }
+  end
 end
