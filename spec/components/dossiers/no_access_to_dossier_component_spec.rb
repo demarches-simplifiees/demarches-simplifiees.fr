59bfab8cc49b7718fa2eb76a5e67f4990cf26df4
opalvadeau <213420035+opalvadeau@users.noreply.github.com>
2025-06-02 17:07:18 +0200

    test(dossiers) added tests for no_access_to_dossier_component

---
+++ spec/components/dossiers/no_access_to_dossier_component_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+RSpec.describe Dossiers::NoAccessToDossierComponent, type: :component do
+  let(:dossier) { create(:dossier) }
+  subject { render_inline(described_class.new(dossier)) }
+
+  context "when has administrators" do
+    it "renders text properly" do
+      expect(subject).to have_text("Pour consulter ce dossier, vous devez")
+      expect(subject).to have_text("contacter l'un des administrateurs")
+      expect(subject).to have_text("de la démarche")
+      expect(subject).to have_text("pour lui demander de")
+      expect(subject).to have_text("vous ajouter comme instructeur :")
+    end
+
+    it "renders procedure name" do
+      expect(subject).to have_text(dossier.procedure.libelle)
+    end
+
+    it "renders administrator email" do
+      expect(subject).to have_text("default_admin@admin.com")
+    end
+
+    it "renders title" do
+      expect(subject).to have_text("Vous n'avez pas accès à ce dossier")
+    end
+
+    it "renders dossier number link" do
+      expect(subject).to have_link("Dossier nº #{dossier.id}")
+    end
+
+    it "renders close button" do
+      expect(subject).to have_button("Fermer")
+    end
+  end
+end
