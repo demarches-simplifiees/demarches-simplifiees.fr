b189fc6dfb1aceca8a134d389c9b32e26c8dea8b
maxim.joseau <maxim.joseau@ouidou.fr>
2025-06-03 16:58:21 +0200

    fix: resolve specs

--- spec/system/experts/expert_spec.rb
+++ spec/system/experts/expert_spec.rb
@@ -146,10 +146,7 @@ describe 'Inviting an expert:' do
         click_on '1 avis à donner'
         click_on avis.dossier.user.email
 
-        # click_on 'Télécharger le dossier et toutes ses pièces jointes'
-        # For some reason, clicking the download link does not trigger the download in the headless browser
-        # for some member of the team, so we need to go to the download link directly
-        visit telecharger_pjs_expert_avis_path(avis.dossier.procedure, avis)
+        click_on 'Télécharger le dossier et toutes ses pièces jointes'
 
         DownloadHelpers.wait_for_download
         files = ZipTricks::FileReader.read_zip_structure(io: File.open(DownloadHelpers.download))
