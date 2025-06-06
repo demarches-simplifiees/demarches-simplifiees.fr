c8e1a276e26b51af6b0fd22c7aa3c12bf623362f
mfo <mfo@users.noreply.github.com>
2025-06-06 15:49:33 +0200

    bugfix(export): as an admin, a filled champs filled by a user, then removed in a new revision, should be readable in exports

--- app/models/concerns/dossier_export_concern.rb
+++ app/models/concerns/dossier_export_concern.rb
@@ -17,9 +17,9 @@ module DossierExportConcern
 
   def champ_values_for_export(types_de_champ, row_id: nil, export_template: nil, format:)
     types_de_champ.flat_map do |type_de_champ|
-      # TODO: filled_champ? or another method that should be able to fetch champ value no matter if the champ is no more in revision
-
       champ = filled_champ(type_de_champ, row_id:)
+      champ ||= champs.find { _1.public_id == type_de_champ.public_id(_1.row_id) && _1.visible? }
+
       if export_template.present?
         export_template
           .columns_for_stable_id(type_de_champ.stable_id)
