37259f7ae7d1710cffa0bafb15c9add79e7103ea
Colin Darie <colin@darie.eu>
2025-06-06 12:29:12 +0200

    fix(address): bad data on french address with missing or incoherent department_code

--- app/tasks/maintenance/t20250602fix_bad_address_data_task.rb
+++ app/tasks/maintenance/t20250602fix_bad_address_data_task.rb
@@ -2,7 +2,8 @@
 
 module Maintenance
   class T20250602fixBadAddressDataTask < MaintenanceTasks::Task
-    # Documentation: cette tâche modifie les données pour…
+    # Documentation: rattrape des champs adresses avec des données incohérentes ou incomplètes
+    # après le déploiement du nouveau champ adresse hors BAN.
 
     include RunnableOnDeployConcern
     include StatementsHelpersConcern
@@ -20,11 +21,18 @@ module Maintenance
       is_international_address = champ.country_code != 'FR'
 
       if is_partial_address && is_international_address
-
         champ.update_column(:value_json, champ.value_json.merge(
           department_code: '99',
           department_name: 'Etranger'
         ))
+      end
+
+      if champ.department_code == "99" && !is_international_address && champ.city_code.present? && champ.postal_code.present?
+        city_data = APIGeoService.parse_city_code_and_postal_code("#{champ.city_code}-#{champ.postal_code}")
+
+        return if city_data.blank?
+
+        champ.update_column(:value_json, champ.value_json.merge!(city_data))
       end
     end
 
