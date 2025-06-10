9f2fce598fb8cbb773c2b74b308a371f7fad3e2f
Mathieu Magnin <mathieu@magnin.sh>
2025-06-10 09:30:00 +0200

    not in ban inputs should be in a fieldset

--- app/components/editable_champ/address_component.rb
+++ app/components/editable_champ/address_component.rb
@@ -18,7 +18,9 @@ class EditableChamp::AddressComponent < EditableChamp::EditableChampBaseComponen
       items: @champ.selected_items,
       loader: data_sources_data_source_adresse_path,
       minimum_input_length: 2,
-      is_disabled: !@champ.ban?)
+      is_disabled: !@champ.ban?,
+      'aria-labelledby': labelledby_id
+      )
   end
 
   def commune_react_props
