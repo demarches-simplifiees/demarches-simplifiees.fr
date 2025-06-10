1ca016de1e82dba89d61fa316a15ec0faaff543b
Mathieu Magnin <mathieu@magnin.sh>
2025-06-10 17:43:07 +0200

    fix(repeatable-block): aria labelledby for champ adress in repetition

--- app/components/editable_champ/address_component.rb
+++ app/components/editable_champ/address_component.rb
@@ -41,10 +41,55 @@ class EditableChamp::AddressComponent < EditableChamp::EditableChampBaseComponen
       translations: {
         search_error: t('.search_error'),
       },
+      'aria-labelledby': city_aria_labelledby,
     }
   end
 
   def pays_options
     APIGeoService.countries.map { [_1[:name], _1[:code]] }
+  end
+
+  def not_in_ban_label_id
+    "#{@champ.focusable_input_id(:not_in_ban)}-label"
+  end
+
+  def not_in_ban_aria_labelledby
+    "#{aria_labelledby_prefix} #{not_in_ban_label_id}"
+  end
+
+  def country_label_id
+    "#{@champ.focusable_input_id(:country_code)}-label"
+  end
+
+  def country_aria_labelledby
+    "#{aria_labelledby_prefix} #{fieldset_legend_id} #{country_label_id}"
+  end
+
+  def street_label_id
+    "#{@champ.focusable_input_id(:street_address)}-label"
+  end
+
+  def street_aria_labelledby
+    "#{aria_labelledby_prefix} #{fieldset_legend_id} #{street_label_id}"
+  end
+
+  def city_label_id
+    "#{@champ.focusable_input_id(:city_name)}-label"
+  end
+
+  def city_aria_labelledby
+    "#{aria_labelledby_prefix} #{fieldset_legend_id} #{city_label_id}"
+  end
+
+  def postal_code_label_id
+    "#{@champ.focusable_input_id(:postal_code)}-label"
+  end
+
+  def postal_code_aria_labelledby
+    "#{aria_labelledby_prefix} #{fieldset_legend_id} #{postal_code_label_id}"
+  end
+
+  def fieldset_legend_id
+    "#{@champ.html_id}-legend"
   end
 end
