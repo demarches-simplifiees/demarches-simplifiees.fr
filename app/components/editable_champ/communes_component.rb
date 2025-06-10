10848c25ed55823e9d4bd77c8bc4b7b96593c0f9
Mathieu Magnin <mathieu@magnin.sh>
2025-06-10 17:43:07 +0200

    fix(repeatable-block): aria labelledby for champ adress in repetition

--- app/components/editable_champ/communes_component.rb
+++ app/components/editable_champ/communes_component.rb
@@ -23,7 +23,6 @@ class EditableChamp::CommunesComponent < EditableChamp::EditableChampBaseCompone
       loader: data_sources_data_source_commune_path(with_combined_code: true),
       limit: 20,
       minimum_input_length: 2,
-      'aria-labelledby': labelledby_id
-      )
+      'aria-labelledby': labelledby_id)
   end
 end
