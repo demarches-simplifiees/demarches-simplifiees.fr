9c3b504a1833dcf15d9e8753d6ef147f6155d1d7
Eric Leroy-Terquem <eric.leroyterquem@beta.gouv.fr>
2025-06-11 10:34:12 +0200

    db(task): backfill dossiers expired_at

---
+++ spec/tasks/maintenance/t20250611backfill_dossiers_expired_at_task_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+module Maintenance
+  RSpec.describe T20250611backfillDossiersExpiredAtTask do
+    describe "#process" do
+      subject(:process) { described_class.process(dossier) }
+
+      context "when dossier is en brouillon" do
+        let(:dossier) { create(:dossier, :brouillon, expired_at: nil) }
+        it "raises an error" do
+          expect { process }.to change { dossier.reload.expired_at }.from(nil).to be_within(1.second).of(dossier.expiration_date)
+        end
+      end
+
+      context "when dossier is en construction" do
+        let(:dossier) { create(:dossier, :en_construction, expired_at: nil) }
+        it "updates dossier expired_at attribute" do
+          expect { process }.to change { dossier.reload.expired_at }.from(nil).to be_within(1.second).of(dossier.expiration_date)
+        end
+      end
+
+      context "when dossier is en instruction" do
+        let(:dossier) { create(:dossier, :en_instruction, expired_at: nil) }
+        it "raises an error" do
+          expect { process }.to raise_error(RuntimeError, 'expiration_date_reference should not be called in state en_instruction')
+        end
+      end
+    end
+  end
+end
