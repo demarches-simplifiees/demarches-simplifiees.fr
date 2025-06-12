14b33508a23efc4bd25860ddf9bcdd2de6a2c761
Mathieu Magnin <mathieu@magnin.sh>
2025-06-12 17:29:49 +0200

    Aria labelledby for champ carte

--- app/javascript/components/react-aria/hooks.ts
+++ app/javascript/components/react-aria/hooks.ts
@@ -20,6 +20,7 @@ export interface ComboBoxProps
   extends Omit<AriaComboBoxProps<Item>, 'children'> {
   children: React.ReactNode | ((item: Item) => React.ReactNode);
   label?: string;
+  ariaLabelledbyPrefix?: string;
   description?: string;
   isLoading?: boolean;
 }
