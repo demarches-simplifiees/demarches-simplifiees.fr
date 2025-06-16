0c4b6533df50baee0f4850210d0c33e6bb47b21e
madelaun <maxence.delaunay@capgemini.com>
2025-06-16 10:17:31 +0200

    Update(dossier_link_component_spec): Stop to using 'instance_double' and update tests

--- spec/components/editable_champ/dossier_link_component/dossier_link_component_spec.rb
+++ spec/components/editable_champ/dossier_link_component/dossier_link_component_spec.rb
@@ -1,100 +1,161 @@
 RSpec.describe EditableChamp::DossierLinkComponent, type: :component do
-  let(:form) { instance_double("Form", object: instance_double("Object")) }
+  let(:form) { instance_double('Form') }
+  let(:current_user) { create(:user) }
+  let(:procedure) { create(:procedure, types_de_champ_public: [{ type: :dossier_link}]) }
+  let(:dossier) { create(:dossier, :en_construction, user: current_user, depose_at: Time.zone.now, procedure: procedure) }
+  let(:champ) { dossier.champs.first }
 
-  let(:component) { described_class.new(form: form, champ: champ) }
-  let(:champ) { instance_double(Champ, describedby_id: 'describedby_id', error_id: 'error_id', description: 'description', input_id: 'input_id', public_id: 'public_id', blank?: false, to_s: '123') }
-  let(:type_de_champ) { instance_double(TypeDeChamp, procedures: [procedure]) }
-  let(:procedure) { instance_double(Procedure, id: 1, libelle: 'Procedure 1', dossiers: [dossier]) }
-  let(:dossier) { instance_double(Dossier, id: 123, user: current_user, state: 'deposé', depose_at: Time.zone.now) }
-  let(:current_user) { instance_double(User) }
+  let(:procedures) do
+    [
+      create(:procedure, id: 1, libelle: "Procedure 1", aasm_state: "publiee"),
+      create(:procedure, id: 2, libelle: "Procedure 2", aasm_state: "brouillon"),
+      create(:procedure, id: 3, libelle: "Procedure 3", aasm_state: "close")
+    ]
+  end
+
+  let(:procedures_no_in_the_limitation) do
+    [
+      create(:procedure, id: 4, libelle: "Procedure 4 - pas dans la limitation", aasm_state: "publiee"),
+      create(:procedure, id: 5, libelle: "Procedure 5 - pas dans la limitation", aasm_state: "publiee"),
+    ]
+  end
+
+  def create_dossiers(number, status, procedure_limited, hidden_by_user_at = nil)
+    Array.new(number) do
+      create(:dossier, status, user: current_user, depose_at: Time.zone.now, procedure: procedure_limited, hidden_by_user_at: hidden_by_user_at)
+    end
+  end
 
   before do
-    allow(champ).to receive(:type_de_champ).and_return(type_de_champ)
-    allow(dossier).to receive(:user).and_return(current_user)
-    allow(component).to receive(:current_user).and_return(current_user)
-    allow(Dossier).to receive(:visible_by_administration).and_return(Dossier)
-    allow(Dossier).to receive(:find_by).with(id: '123').and_return(dossier)
+    allow_any_instance_of(described_class).to receive(:current_user).and_return(current_user)
+    procedure.active_revision.types_de_champ.first.procedures = procedures
   end
+
+  subject { described_class.new(form: form, champ: champ) }
 
   describe '#dsfr_input_classname' do
-    it 'renvoie le nom de classe pour l\'élément d\'entrée' do
-      expect(component.send(:dsfr_input_classname)).to eq('fr-input')
-    end
-  end
-  describe '#dossier' do
-    it 'renvoie le dossier associé au champ' do
-      expect(component.send(:dossier)).to eq(dossier)
-    end
-  end
-
-  describe '#other_element_class_names' do
-    it 'renvoie les noms de classe pour les autres éléments' do
-      expect(component.send(:other_element_class_names)).to include('fr-fieldset__element')
-    end
-  end
-
-  describe '#dossier' do
-    it 'renvoie le dossier associé au champ' do
-      expect(component.send(:dossier)).to eq(dossier)
+    it 'returns the class name for the input element' do
+      expect(subject.send(:dsfr_input_classname)).to eq('fr-input')
     end
   end
 
   describe '#dossier_options_for' do
-    it 'renvoie les options pour la sélection de dossier' do
-      options = component.send(:dossier_options_for, champ)
-      expect(options).to include({ value: "separator_1", label: "-- Démarche : Procedure 1 --" })
-      expect(options).to include({ value: "123", label: "N° 123 - déposé le #{dossier.depose_at.strftime('%d/%m/%Y')}" })
+    let(:dossiers_en_construction) { create_dossiers(1, :en_construction, procedures[0]) }
+
+    it 'returns the options for dossier selection'do
+      procedures[0].dossiers = dossiers_en_construction
+      subject.send(:before_render)
+      options = subject.send(:dossier_options_for, champ)
+
+      expect(options).to include({ :label => "-- Démarche : Procedure 1 --", :value => "separator_1" })
+      expect(options).to include({ :label => "N° " + dossiers_en_construction[0].id.to_s + " - déposé le " + dossiers_en_construction[0].depose_at.strftime('%d/%m/%Y') , :value => dossiers_en_construction[0].id.to_s})
+      expect(options).to include({ :label => "-- Démarche : Procedure 2 --", :value => "separator_2" })
+      expect(options).to include({ :label => "Vous n’avez déposé aucun dossier sur cette démarche. ", :value => "no_dossier_2" })
+      expect(options).to include({ :label => "-- Démarche : Procedure 3 --", :value => "separator_3" })
+      expect(options).to include({ :label => "Vous n’avez déposé aucun dossier sur cette démarche. ", :value => "no_dossier_3" })
     end
   end
 
   describe '#react_props' do
-    it 'renvoie les props pour le composant React' do
-      props = component.send(:react_props)
+    it 'returns the props for the React component' do
+      champ.value = "toto"
+      props = subject.send(:react_props)
       expect(props[:items]).to include({ value: "separator_1", label: "-- Démarche : Procedure 1 --" })
       expect(props[:placeholder]).to eq('Sélectionnez un dossier')
-      expect(props[:name]).to eq('dossier[champs_public_attributes][public_id][value]')
-      expect(props[:id]).to eq('input_id')
+      expect(props[:name]).to eq("dossier[champs_public_attributes][#{champ.public_id}][value]")
+      expect(props[:id]).to eq(champ.input_id)
       expect(props[:class]).to eq('small-margin')
     end
   end
 
-  describe '#render_dossiers' do
-    it 'renvoie les dossiers à rendre' do
-      expect(component.send(:render_dossiers)).to eq([dossier])
+  context '#render_as_radios?' do
+    describe '1 dossier' do
+      let(:dossiers_en_construction) { create_dossiers(1, :en_construction, procedures[0]) }
+      it {
+        procedures[0].dossiers = dossiers_en_construction
+        subject.send(:before_render)
+        expect(subject.send(:render_as_radios?)).to be_truthy
+      }
+    end
+
+    describe '5 dossiers' do
+      let(:dossiers_en_construction) { create_dossiers(3, :en_construction, procedures[0]) }
+      let(:dossiers_accepte) { create_dossiers(1, :accepte, procedures[0]) }
+      let(:dossiers_refuse) { create_dossiers(1, :refuse, procedures[0]) }
+      it {
+        procedures[0].dossiers = dossiers_en_construction + dossiers_accepte + dossiers_refuse
+        subject.send(:before_render)
+        expect(subject.send(:render_as_radios?)).to be_truthy
+      }
+    end
+
+    describe '5 dossiers + 2 dossiers in other procedure no limited' do
+      let(:dossiers_en_construction) { create_dossiers(3, :en_construction, procedures[0]) }
+      let(:dossiers_accepte) { create_dossiers(1, :accepte, procedures[0]) }
+      let(:dossiers_refuse) { create_dossiers(1, :refuse, procedures[0]) }
+
+      let(:dossiers_en_construction_no_limited_1) { create_dossiers(1, :en_construction, procedures_no_in_the_limitation[0]) }
+      let(:dossiers_en_construction_no_limited_2) { create_dossiers(1, :en_construction, procedures_no_in_the_limitation[1]) }
+
+      it {
+        procedures[0].dossiers = dossiers_en_construction + dossiers_accepte + dossiers_refuse
+        procedures_no_in_the_limitation[0] = dossiers_en_construction_no_limited_1
+        procedures_no_in_the_limitation[1] = dossiers_en_construction_no_limited_2
+        subject.send(:before_render)
+        expect(subject.send(:render_as_radios?)).to be_truthy
+      }
+    end
+
+    describe '5 dossiers + 1 dossier brouillon' do
+      let(:dossiers_en_construction) { create_dossiers(5, :en_construction, procedures[0]) }
+      let(:dossiers_brouillon) { create_dossiers(1, :brouillon, procedures[0]) }
+      it {
+        procedures[0].dossiers = dossiers_en_construction + dossiers_brouillon
+        subject.send(:before_render)
+        expect(subject.send(:render_as_radios?)).to be_truthy
+      }
+    end
+
+    describe '5 dossiers + 1 dossier supprime' do
+      let(:dossiers_en_construction) { create_dossiers(5, :en_construction, procedures[0]) }
+      let(:dossiers_supprime) { create_dossiers(1, :en_construction, procedures[0], 1.day.ago) }
+      it {
+        procedures[0].dossiers = dossiers_en_construction + dossiers_supprime
+        subject.send(:before_render)
+        expect(subject.send(:render_as_radios?)).to be_truthy
+      }
+    end
+
+    describe '6 dossiers' do
+      let(:dossiers_en_construction) { create_dossiers(6, :en_construction, procedures[0]) }
+
+      it {
+        procedures[0].dossiers = dossiers_en_construction
+        subject.send(:before_render)
+        expect(subject.send(:render_as_radios?)).to be_falsey
+      }
     end
   end
 
-  describe '#render_as_radios?' do
-    it 'renvoie true s\'il y a 5 dossiers ou moins' do
-      expect(component.send(:render_as_radios?)).to be_truthy
+  context '#render_as_combobox?' do
+    describe 'returns true if there 20 dossiers or more' do
+      let(:dossiers_en_construction) { create_dossiers(20, :en_construction, procedures[0]) }
+
+      it {
+        procedures[0].dossiers = dossiers_en_construction
+        subject.send(:before_render)
+        expect(subject.send(:render_as_combobox?)).to be_truthy
+      }
     end
 
-    it 'renvoie false s\'il y a plus de 5 dossiers' do
-      allow(procedure).to receive(:dossiers).and_return(Array.new(6) { dossier })
-      expect(component.send(:render_as_radios?)).to be_falsy
-    end
-  end
+    describe 'returns false if there are less than 20 dossiers' do
+      let(:dossiers_en_construction) { create_dossiers(19, :en_construction, procedures[0]) }
 
-  describe '#render_as_combobox?' do
-    it 'renvoie true s\'il y a 20 dossiers ou plus' do
-      allow(procedure).to receive(:dossiers).and_return(Array.new(20) { dossier })
-      expect(component.send(:render_as_combobox?)).to be_truthy
-    end
-
-    it 'renvoie false s\'il y a moins de 20 dossiers' do
-      expect(component.send(:render_as_combobox?)).to be_falsy
-    end
-  end
-
-  describe '#contains_long_option?' do
-    it 'renvoie true si l\'une des étiquettes d\'option est plus longue que 100 caractères' do
-      long_option = { value: 'long_option', label: 'a' * 101 }
-      allow(component).to receive(:dossier_options_for).and_return([long_option])
-      expect(component.send(:contains_long_option?)).to be_truthy
-    end
-
-    it 'renvoie false si toutes les étiquettes d\'option ont 100 caractères ou moins' do
-      expect(component.send(:contains_long_option?)).to be_falsy
+      it {
+        procedures[0].dossiers = dossiers_en_construction
+        subject.send(:before_render)
+        expect(subject.send(:render_as_combobox?)).to be_falsey
+      }
     end
   end
 end
