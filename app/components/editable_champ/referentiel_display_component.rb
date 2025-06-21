485c829c343be5c21964e6238f2163e32a774147
mfo <mfo@users.noreply.github.com>
2025-06-21 07:07:43 +0200

    feat(ViewableChamps::ReferentielDisplayComponent): display stored data from api response to user and/or instructeur on dossier page

--- app/components/editable_champ/referentiel_display_component.rb
+++ app/components/editable_champ/referentiel_display_component.rb
@@ -5,8 +5,6 @@ class EditableChamp::ReferentielDisplayComponent < Referentiels::ReferentielDisp
   delegate :referentiel_mapping_displayable_for_usager, to: :type_de_champ
 
   def data
-    safe_value_json = Hash(@champ.value_json).with_indifferent_access
-
     referentiel_mapping_displayable_for_usager.filter_map do |jsonpath, _mapping|
       value = format(jsonpath, safe_value_json.dig(jsonpath))
       [libelle(jsonpath), value] if !value.nil?
