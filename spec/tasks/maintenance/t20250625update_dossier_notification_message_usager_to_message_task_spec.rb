ce19b0cb75476a68538a3bf4d1d4d860233acf05
benoitqueyron <72251526+Benoit-MINT@users.noreply.github.com>
2025-06-25 14:50:36 +0200

    db(task): update les notifications :message_usager en :message

---
+++ spec/tasks/maintenance/t20250625update_dossier_notification_message_usager_to_message_task_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+module Maintenance
+  RSpec.describe T20250625updateDossierNotificationMessageUsagerToMessageTask do
+    let(:dossier) { create(:dossier) }
+    let(:instructeur) { create(:instructeur) }
+    let!(:notification_message_usager) { create(:dossier_notification, :for_instructeur, dossier:, instructeur:, notification_type: :message_usager) }
+
+    describe "#collection" do
+      subject(:collection) { described_class.collection }
+
+      let!(:other_notification) { create(:dossier_notification, :for_instructeur, dossier:, instructeur:, notification_type: :dossier_modifie) }
+
+      it "includes only :message_usager notification" do
+        expect(collection).to include(notification_message_usager)
+        expect(collection).not_to include(other_notification)
+      end
+    end
+
+    describe "#process" do
+      subject(:process) { described_class.process(notification_message_usager) }
+
+      it "updates notification_type :message_usager to :message" do
+        subject
+        expect(notification_message_usager.notification_type).to eq('message')
+      end
+    end
+  end
+end
