44f79956c9fa7ddbe4bb58dc0b33bdacead1fb5c
benoitqueyron <72251526+Benoit-MINT@users.noreply.github.com>
2025-06-26 12:15:47 +0200

    db(task): update procedure_presentation with notication_type filter

---
+++ app/tasks/maintenance/t20250625update_procedure_presentation_filters_with_message_usager_task.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+module Maintenance
+  class T20250625updateProcedurePresentationFiltersWithMessageUsagerTask < MaintenanceTasks::Task
+    # Documentation: cette tâche modifie les données pour…
+
+    include RunnableOnDeployConcern
+    include StatementsHelpersConcern
+
+    # Uncomment only if this task MUST run imperatively on its first deployment.
+    # If possible, leave commented for manual execution later.
+    run_on_first_deploy
+
+    STATE_FILTERS = [
+      :tous_filters,
+      :suivis_filters,
+      :traites_filters,
+      :a_suivre_filters,
+      :archives_filters,
+      :expirant_filters,
+      :supprimes_filters,
+      :supprimes_recemment_filters
+    ].freeze
+
+    def collection
+      ProcedurePresentation.includes(assign_to: :procedure)
+    end
+
+    def process(pp)
+      Rails.logger.debug { "Task T20250625updateProcedurePresentationFiltersWithMessageUsagerTask process procedure_presentation_id=##{pp.id}" }
+      changed_pp = false
+
+      STATE_FILTERS.each do |state_filter|
+        current_state_filters = pp.send(state_filter)
+        next if current_state_filters.empty?
+        next unless current_state_filters.any? { |f| f.column.table == 'dossier_notifications' }
+        changed_pp = true
+
+        new_state_filters = current_state_filters.map do |filter|
+          if filter.column.table == "dossier_notifications" && filter.filter == "message_usager"
+            FilteredColumn.new(
+              column: pp.procedure.dossier_notifications_column,
+              filter: "message"
+            )
+          else
+            filter
+          end
+        end
+
+        pp.send("#{state_filter}=", new_state_filters)
+      # a column can be not found for various reasons (deleted tdc, changed type, etc)
+      # in this case we just ignore the error and continue
+      rescue ActiveRecord::RecordNotFound
+      end
+
+      pp.save! if changed_pp
+    end
+  end
+end
---
+++ spec/tasks/maintenance/t20250625update_procedure_presentation_filters_with_message_usager_task_spec.rb
@@ -0,0 +1,85 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+module Maintenance
+  RSpec.describe T20250625updateProcedurePresentationFiltersWithMessageUsagerTask do
+    let(:procedure) { create(:procedure) }
+    let(:instructeur) { create(:instructeur) }
+    let(:assign_to) { create(:assign_to, procedure: procedure, instructeur: instructeur) }
+    let!(:procedure_presentation) { create(:procedure_presentation, assign_to: assign_to) }
+
+    describe "#process" do
+      subject(:process) { described_class.process(procedure_presentation) }
+
+      context "when procedure_presentation has not filter" do
+        it "does not change procedure_presentation" do
+          expect { process }.not_to change { procedure_presentation }
+        end
+      end
+
+      context "when procedure_presentation has filters but none on notification_type" do
+        before do
+          procedure_presentation.update!(
+            suivis_filters: [
+              FilteredColumn.new(
+                column: procedure.dossier_id_column,
+                filter: "1"
+              )
+            ]
+          )
+        end
+
+        it "does not change procedure_presentation filters" do
+          expect { process }.not_to change { procedure_presentation }
+        end
+      end
+
+      context "when procedure_presentation has notification_type filter" do
+        context "which is not 'message_usager'" do
+          before do
+            procedure_presentation.update!(
+              suivis_filters: [
+                FilteredColumn.new(
+                  column: procedure.dossier_notifications_column,
+                  filter: "dossier_modifie"
+                )
+              ]
+            )
+          end
+
+          it "refesh DossierColumn to obtain correct options_for_select" do
+            expect { process }.to change { procedure_presentation.suivis_filters }
+          end
+
+          it "does not change notification_type filter" do
+            expect { process }.not_to change { procedure_presentation.suivis_filters.map(&:to_json) }
+          end
+        end
+
+        context "which is 'message_usager'" do
+          before do
+            procedure_presentation.update!(
+              suivis_filters: [
+                FilteredColumn.new(
+                  column: procedure.dossier_notifications_column,
+                  filter: "message_usager"
+                )
+              ]
+            )
+          end
+
+          it "refesh DossierColumn to obtain correct options_for_select" do
+            expect { process }.to change { procedure_presentation.suivis_filters }
+          end
+
+          it "updates filter to 'message'" do
+            subject
+            expect(procedure_presentation.suivis_filters.first.column).to eq(procedure.dossier_notifications_column)
+            expect(procedure_presentation.suivis_filters.first.filter).to eq('message')
+          end
+        end
+      end
+    end
+  end
+end
