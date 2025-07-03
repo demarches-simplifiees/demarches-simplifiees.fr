90b15779b2cf7489ddd0289d002a06891ddc4a2a
Mathieu Magnin <mathieu@magnin.sh>
2025-07-03 16:06:17 +0200

    [#11159] delete pro connect session cookie after default sign in

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -18,6 +18,7 @@ class Users::SessionsController < Devise::SessionsController
 
     if user&.valid_password?(params[:user][:password])
       delete_france_connect_cookies
+      delete_pro_connect_session_info_cookie
       user.update(loged_in_with_france_connect: nil)
       user.update_preferred_domain(Current.host) if helpers.switch_domain_enabled?(request)
     end
