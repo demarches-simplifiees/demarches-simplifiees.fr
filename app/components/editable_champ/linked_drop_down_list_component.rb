4314701566ca741c9bdd5ade15d163455bdc2ce2
benoitqueyron <72251526+Benoit-MINT@users.noreply.github.com>
2025-07-16 11:24:46 +0200

    style(component): ajuste le rendu de linked_drop_down_list

--- app/components/editable_champ/linked_drop_down_list_component.rb
+++ app/components/editable_champ/linked_drop_down_list_component.rb
@@ -12,18 +12,4 @@ class EditableChamp::LinkedDropDownListComponent < EditableChamp::EditableChampB
   def select_options
     @champ.mandatory? ? { prompt: t('views.components.select_list') } : { include_blank: t('views.components.select_list') }
   end
-
-  private
-
-  def secondary_label
-    secondary_label_text + secondary_label_mandatory
-  end
-
-  def secondary_label_text
-    @champ.drop_down_secondary_libelle.presence || "Valeur secondaire dépendant de la première"
-  end
-
-  def secondary_label_mandatory
-    @champ.mandatory? ? tag.span(' *', class: 'mandatory') : ''
-  end
 end
