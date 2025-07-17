a6043f5b3bc2aacc24adbd15c17c0330e6b44d85
benoitqueyron <72251526+Benoit-MINT@users.noreply.github.com>
2025-07-17 17:27:38 +0200

    refactor: deplace les méthodes liees à InstructeursProcedure

--- app/models/instructeurs_procedure.rb
+++ app/models/instructeurs_procedure.rb
@@ -3,4 +3,13 @@
 class InstructeursProcedure < ApplicationRecord
   belongs_to :instructeur
   belongs_to :procedure
+
+  def self.update_instructeur_procedures_positions(instructeur, ordered_procedure_ids)
+    procedure_id_position = ordered_procedure_ids.reverse.each.with_index.to_h
+    InstructeursProcedure.transaction do
+      procedure_id_position.each do |procedure_id, position|
+        InstructeursProcedure.where(procedure_id:, instructeur:).update(position:)
+      end
+    end
+  end
 end
