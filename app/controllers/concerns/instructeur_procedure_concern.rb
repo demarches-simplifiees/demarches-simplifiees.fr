aa36819df9ece2825b28b63f55f18aff884468a8
benoitqueyron <72251526+Benoit-MINT@users.noreply.github.com>
2025-07-18 14:11:44 +0200

    fix(InstructeursProcedure): initialise tous les attributs

--- app/controllers/concerns/instructeur_procedure_concern.rb
+++ app/controllers/concerns/instructeur_procedure_concern.rb
@@ -8,19 +8,28 @@ module InstructeurProcedureConcern
 
     def ensure_instructeur_procedures_for(procedures)
       current_instructeur_procedures = current_instructeur.instructeurs_procedures.where(procedure_id: procedures.map(&:id))
-      top_position = current_instructeur_procedures.map(&:position).max || 0
-      missing_instructeur_procedures = procedures.sort_by(&:published_at).map(&:id).filter_map do |procedure_id|
-        if !procedure_id.in?(current_instructeur_procedures.map(&:procedure_id))
-          { instructeur_id: current_instructeur.id, procedure_id:, position: top_position += 1 }
+      top_position = current_instructeur_procedures.map(&:position).max.to_i
+      missing_instructeur_procedures = procedures.sort_by(&:published_at).filter_map do |procedure|
+        if !procedure.id.in?(current_instructeur_procedures.map(&:procedure_id))
+          { instructeur_id: current_instructeur.id, procedure_id: procedure.id, position: top_position += 1, last_revision_seen_id: procedure.published_revision_id }
         end
       end
       InstructeursProcedure.insert_all(missing_instructeur_procedures) if missing_instructeur_procedures.size.positive?
     end
 
     def find_or_create_instructeur_procedure(procedure)
-      InstructeursProcedure
-        .create_with(last_revision_seen_id: procedure.published_revision_id)
-        .find_or_create_by(instructeur: current_instructeur, procedure: procedure)
+      instructeur_procedure = InstructeursProcedure.find_or_initialize_by(
+        instructeur: current_instructeur,
+        procedure: procedure
+      )
+
+      if instructeur_procedure.new_record?
+        instructeur_procedure.last_revision_seen_id = procedure.published_revision_id
+        instructeur_procedure.position = current_instructeur.instructeurs_procedures.map(&:position).max.to_i + 1
+        instructeur_procedure.save!
+      end
+
+      instructeur_procedure
     end
   end
 end
