ea97a5a62d3960a1c09cad31885c47d825912347
benoitqueyron <72251526+Benoit-MINT@users.noreply.github.com>
2025-07-18 13:43:03 +0200

    db(migration): supprime la position par défaut dans InstructeursProcedure

---
+++ db/migrate/20250718084353_remove_default_from_position_in_instructeurs_procedures.rb
@@ -0,0 +1,5 @@
+class RemoveDefaultFromPositionInInstructeursProcedures < ActiveRecord::Migration[7.1]
+  def change
+    change_column_default :instructeurs_procedures, :position, from: 99, to: nil
+  end
+end
