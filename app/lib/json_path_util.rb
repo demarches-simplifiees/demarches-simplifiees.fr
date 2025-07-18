0d3238650307ea2362bb34fcbbae76a7741e7e04
mfo <mfo@users.noreply.github.com>
2025-07-18 17:05:49 +0200

    feat(JSONPathUtil): array_paths_with_examples: identify array of possible suggestion for autocomplete

--- app/lib/json_path_util.rb
+++ app/lib/json_path_util.rb
@@ -2,6 +2,22 @@
 
 # Extension de la classe JsonPath de la gem
 class JSONPathUtil
+  def self.array_paths_with_examples(hash, parent_path = '$')
+    hash.each_with_object({}) do |(key, value), result|
+      current_path = "#{parent_path}.#{key}"
+
+      case value
+      in [Hash => first, *]
+        result.merge!(current_path => first)
+      in Hash
+        nested = array_paths_with_examples(value, current_path)
+        result.merge!(nested)
+      else
+        result
+      end
+    end
+  end
+
   def self.hash_to_jsonpath(hash, parent_path = '$')
     hash.each_with_object({}) do |(key, value), result|
       current_path = "#{parent_path}.#{key}"
