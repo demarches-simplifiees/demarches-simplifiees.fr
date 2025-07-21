7d088c79e60eb73d15a59c0f4986d696e4abf12e
Kara Diaby <kdiaby.pro@gmail.com>
2025-07-21 11:37:13 +0000

    Utilise une modale plus large et fixe les espaces

--- app/helpers/sanitize_with_link_helper.rb
+++ app/helpers/sanitize_with_link_helper.rb
@@ -2,7 +2,14 @@
 
 module SanitizeWithLinkHelper
   def sanitize_with_link(value)
-    tags = Rails.configuration.action_view.sanitized_allowed_tags + ['a']
-    sanitize(value, tags:)
+    allowed_tags = Rails.configuration.action_view.sanitized_allowed_tags || []
+    tags = allowed_tags + ['a']
+
+    allowed_attributes = Rails.configuration.action_view.sanitized_allowed_attributes || []
+    attributes = allowed_attributes + [
+      'aria-controls', 'data-fr-opened', 'data-fr-js-modal-button', 'href', 'class'
+    ]
+
+    sanitize(value, tags:, attributes:)
   end
 end
