326f6334bfb26372cc0ef222ace1d6c574e221f1
Paul Chavard <pro@paul.chavard.net>
2025-07-25 17:33:42 +0200

    use dossier tree in sections summary

--- spec/views/instructeur/dossiers/annotations_privee.html.haml_spec.rb
+++ spec/views/instructeur/dossiers/annotations_privee.html.haml_spec.rb
@@ -24,7 +24,8 @@ describe 'instructeurs/dossiers/annotations_privees', type: :view do
     let(:procedure) { create(:procedure, types_de_champ_private:) }
     let(:types_de_champ_private) do
       [
-        { type: :header_section, level: 1, libelle: 'l1' }
+        { type: :header_section, level: 1, libelle: 'l1' },
+        {}
       ]
     end
     let(:dossier) { create(:dossier, :en_construction, procedure:) }
