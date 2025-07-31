830797868026b39208e1833bf4ef0f539021de7b
benoitqueyron <72251526+Benoit-MINT@users.noreply.github.com>
2025-07-31 10:11:35 +0200

    db(migration): ajoute une column de preference pour chaque type de notification dans instructeurs_procedure

--- app/models/instructeurs_procedure.rb
+++ app/models/instructeurs_procedure.rb
@@ -1,8 +1,18 @@
 # frozen_string_literal: true
 
 class InstructeursProcedure < ApplicationRecord
+  NOTIFICATION_PREFERENCES = ['all', 'followed', 'none'].freeze
+
   belongs_to :instructeur
   belongs_to :procedure
+
+  validates :display_dossier_depose_notifications, inclusion: { in: NOTIFICATION_PREFERENCES - ['followed'] }
+  validates :display_dossier_modifie_notifications, inclusion: { in: NOTIFICATION_PREFERENCES }
+  validates :display_message_notifications, inclusion: { in: NOTIFICATION_PREFERENCES }
+  validates :display_annotation_instructeur_notifications, inclusion: { in: NOTIFICATION_PREFERENCES }
+  validates :display_avis_externe_notifications, inclusion: { in: NOTIFICATION_PREFERENCES }
+  validates :display_attente_correction_notifications, inclusion: { in: NOTIFICATION_PREFERENCES }
+  validates :display_attente_avis_notifications, inclusion: { in: NOTIFICATION_PREFERENCES }
 
   def self.update_instructeur_procedures_positions(instructeur, ordered_procedure_ids)
     procedure_id_position = ordered_procedure_ids.reverse.each.with_index.to_h
