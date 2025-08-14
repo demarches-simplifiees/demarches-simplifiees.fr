faebd3381532d210f298e8362af7de0c5d4b8e27
simon lehericey <mail@simon.lehericey.net>
2025-08-14 12:28:39 +0200

    spec: clearer, more case

--- spec/models/champs/datetime_champ_spec.rb
+++ spec/models/champs/datetime_champ_spec.rb
@@ -55,22 +55,38 @@ describe Champs::DatetimeChamp do
   end
 
   context 'when the value is not in the past' do
-    let(:champ) { dossier.champs.first.tap { _1.update(value:) } }
-    subject { champ.validate(:champs_public_value) }
+    subject { datetime_champ.validate(:champs_public_value) }
 
     context 'all dates are accepted' do
-      let(:value) { DateTime.now }
+      before { datetime_champ.update(value: DateTime.now.iso8601) }
 
       it { is_expected.to be_truthy }
     end
 
     context 'dates not in past are not accepted' do
-      before { champ.type_de_champ.update(options: { date_in_past: '1' }) }
-      let(:value) { DateTime.now }
+      let(:now) { DateTime.parse('2023-10-01') }
 
-      it 'is not valid and contains errors' do
-        is_expected.to be_falsey
-        expect(champ.errors[:value]).to eq(["doit être une date dans le passé"])
+      before do
+        travel_to(now)
+        datetime_champ.type_de_champ.update(options: { date_in_past: '1' })
+        datetime_champ.update(value:)
+      end
+
+      context 'when the value is in the future or today' do
+        let(:value) { now.iso8601 }
+
+        it 'is not valid and contains errors' do
+          is_expected.to be_falsey
+          expect(datetime_champ.errors[:value]).to eq(["doit être une date dans le passé"])
+        end
+      end
+
+      context 'when the value one day before' do
+        let(:value) { (now - 1.day).iso8601 }
+
+        it 'is valid' do
+          is_expected.to be_truthy
+        end
       end
     end
   end
