1c79f388d7b5dfacd4d4a23a26708dbaec74ff75
simon lehericey <mail@simon.lehericey.net>
2025-08-15 10:00:23 +0200

    rename: record -> champ

--- app/validators/date_limit_validator.rb
+++ app/validators/date_limit_validator.rb
@@ -1,44 +1,44 @@
 # frozen_string_literal: true
 
 class DateLimitValidator < ActiveModel::Validator
-  def validate(record)
-    return if record.value.blank?
-    validate_in_past(record) if record.type_de_champ.date_in_past?
-    validate_in_range(record) if record.type_de_champ.range_date?
+  def validate(champ)
+    return if champ.value.blank?
+    validate_in_past(champ) if champ.type_de_champ.date_in_past?
+    validate_in_range(champ) if champ.type_de_champ.range_date?
   end
 
   private
 
-  def validate_in_past(record)
+  def validate_in_past(champ)
     # value has a timezone, it has to be converted to a date before the comparison
-    date = date_or_datetime(record, 'value').to_date
+    date = date_or_datetime(champ, 'value').to_date
 
     if date >= Date.today
       # i18n-tasks-use t('errors.messages.date_in_past')
-      record.errors.add(:value, :date_in_past)
+      champ.errors.add(:value, :date_in_past)
     end
   end
 
-  def validate_in_range(record)
-    value = date_or_datetime(record, 'value')
-    start_date = date_or_datetime(record, 'start_date')
-    end_date = date_or_datetime(record, 'end_date')
+  def validate_in_range(champ)
+    value = date_or_datetime(champ, 'value')
+    start_date = date_or_datetime(champ, 'start_date')
+    end_date = date_or_datetime(champ, 'end_date')
 
     if start_date.present? && end_date.present? && not_in_range(start_date, end_date, value)
       # i18n-tasks-use t('errors.messages.not_in_range_date')
-      record.errors.add(:value, :not_in_range_date, start_date: I18n.l(start_date, format: '%d %B %Y'), end_date: I18n.l(end_date, format: '%d %B %Y'))
+      champ.errors.add(:value, :not_in_range_date, start_date: I18n.l(start_date, format: '%d %B %Y'), end_date: I18n.l(end_date, format: '%d %B %Y'))
     elsif start_date.present? && value < start_date
       # i18n-tasks-use t('errors.messages.limit_start_date')
-      record.errors.add(:value, :limit_start_date, start_date: I18n.l(start_date, format: '%d %B %Y'))
+      champ.errors.add(:value, :limit_start_date, start_date: I18n.l(start_date, format: '%d %B %Y'))
     elsif end_date.present? && value > end_date
       # i18n-tasks-use t('errors.messages.limit_end_date')
-      record.errors.add(:value, :limit_end_date, end_date: I18n.l(end_date, format: '%d %B %Y'))
+      champ.errors.add(:value, :limit_end_date, end_date: I18n.l(end_date, format: '%d %B %Y'))
     end
   end
 
-  def date_or_datetime(record, attribute)
-    return '' if record.method(attribute).call.blank?
-    record.date? ? record.method(attribute).call.to_date : record.method(attribute).call.to_datetime
+  def date_or_datetime(champ, attribute)
+    return '' if champ.method(attribute).call.blank?
+    champ.date? ? champ.method(attribute).call.to_date : champ.method(attribute).call.to_datetime
   rescue Date::Error
     nil
   end
