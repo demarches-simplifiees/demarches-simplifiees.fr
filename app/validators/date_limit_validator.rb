86e8001ef311b5012418e4710c1e1c32e5311ea2
simon lehericey <mail@simon.lehericey.net>
2025-08-15 10:45:24 +0200

    early check date

--- app/validators/date_limit_validator.rb
+++ app/validators/date_limit_validator.rb
@@ -2,33 +2,32 @@
 
 class DateLimitValidator < ActiveModel::Validator
   def validate(champ)
-    return if champ.value.blank?
-    validate_in_past(champ) if champ.type_de_champ.date_in_past?
-    validate_in_range(champ) if champ.type_de_champ.range_date?
+    date = safe_date(champ.value)
+    return if date.nil?
+
+    validate_in_past(champ, date) if champ.type_de_champ.date_in_past?
+    validate_in_range(champ, date) if champ.type_de_champ.range_date?
   end
 
   private
 
-  def validate_in_past(champ)
-    # value has a timezone, it has to be converted to a date before the comparison
-    date = safe_date(champ.value)
-
+  def validate_in_past(champ, date)
     if date >= Date.today
       # i18n-tasks-use t('errors.messages.date_in_past')
       champ.errors.add(:value, :date_in_past)
     end
   end
 
-  def validate_in_range(champ)
-    value, start_date, end_date = [champ.value, champ.start_date, champ.end_date].map { safe_date(it) }
+  def validate_in_range(champ, date)
+    start_date, end_date = [champ.start_date, champ.end_date].map { safe_date(it) }
 
-    if start_date.present? && end_date.present? && not_in_range(start_date, end_date, value)
+    if start_date.present? && end_date.present? && not_in_range(start_date, end_date, date)
       # i18n-tasks-use t('errors.messages.not_in_range_date')
       champ.errors.add(:value, :not_in_range_date, start_date: I18n.l(start_date, format: '%d %B %Y'), end_date: I18n.l(end_date, format: '%d %B %Y'))
-    elsif start_date.present? && value < start_date
+    elsif start_date.present? && date < start_date
       # i18n-tasks-use t('errors.messages.limit_start_date')
       champ.errors.add(:value, :limit_start_date, start_date: I18n.l(start_date, format: '%d %B %Y'))
-    elsif end_date.present? && value > end_date
+    elsif end_date.present? && date > end_date
       # i18n-tasks-use t('errors.messages.limit_end_date')
       champ.errors.add(:value, :limit_end_date, end_date: I18n.l(end_date, format: '%d %B %Y'))
     end
