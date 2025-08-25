dd154952b249316f893ec198ce74fa43dab66cd9
mfo <mfo@users.noreply.github.com>
2025-08-25 15:54:26 +0200

    fix(IntegerColumnType): use BigInt in background, indeed Int are limited to 2^31 - 1 (±2 billions).

--- app/graphql/types/columns/integer_column_type.rb
+++ app/graphql/types/columns/integer_column_type.rb
@@ -4,7 +4,7 @@ module Types::Columns
   class IntegerColumnType < Types::BaseObject
     implements Types::ColumnType
 
-    field :value, Int, null: true, extras: [:parent]
+    field :value, GraphQL::Types::BigInt, null: true, extras: [:parent]
 
     def value(parent:)
       object.value(parent)
