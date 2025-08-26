d46c8efa0cb10e328113fb17ff5f0a44c9a25b76
simon lehericey <mail@simon.lehericey.net>
2025-08-26 16:59:35 +0200

    refactor: remove now useless set_api_entreprise_token_expires_at

--- app/models/concerns/api_entreprise_token_concern.rb
+++ app/models/concerns/api_entreprise_token_concern.rb
@@ -6,8 +6,6 @@ module APIEntrepriseTokenConcern
   included do
     validates_associated :api_entreprise_token
 
-    before_save :set_api_entreprise_token_expires_at, if: :will_save_change_to_api_entreprise_token?
-
     def api_entreprise_token
       t = self[:api_entreprise_token].presence || ENV['API_ENTREPRISE_KEY']
 
@@ -16,10 +14,6 @@ module APIEntrepriseTokenConcern
 
     def has_api_entreprise_token?
       self[:api_entreprise_token].present?
-    end
-
-    def set_api_entreprise_token_expires_at
-      self.api_entreprise_token_expires_at = api_entreprise_token.expires_at
     end
   end
 end
