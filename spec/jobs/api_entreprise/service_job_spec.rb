dee84e53958a1a2b895b8fa58aae68ddc2412310
simon lehericey <mail@simon.lehericey.net>
2025-08-27 10:21:23 +0200

    fix spec by injecting default token in env

--- spec/jobs/api_entreprise/service_job_spec.rb
+++ spec/jobs/api_entreprise/service_job_spec.rb
@@ -6,18 +6,21 @@ RSpec.describe APIEntreprise::ServiceJob, type: :job do
   let(:entreprise_body) { File.read('spec/fixtures/files/api_entreprise/etablissements.json') }
   let(:geocoder_body) { File.read('spec/fixtures/files/api_address/address.json') }
   let(:status) { 200 }
+  let(:api_entreprise_token) { JWT.encode({ exp: 2.months.from_now.to_i }, nil, 'none') }
 
   let (:adresse) { "DIRECTION INTERMINISTERIELLE DU NUMERIQUE\r\nJEAN MARIE DURAND\r\nZAE SAINT GUENAULT\r\n51 BIS RUE DE LA PAIX\r\nCS 72809\r\n75256 PARIX CEDEX 12\r\nFRANCE" }
 
   before do
     stub_request(:get, %r{https://entreprise.api.gouv.fr/v3\/insee\/sirene\/etablissements\/#{siret}})
       .to_return(body: entreprise_body, status: status)
-    allow_any_instance_of(APIEntrepriseToken).to receive(:expired?).and_return(false)
 
     Geocoder.configure(lookup: :ban_data_gouv_fr, use_https: true)
 
     stub_request(:get, "https://api-adresse.data.gouv.fr/search/?citycode=75112&limit=1&q=#{adresse}")
       .to_return(body: geocoder_body, status: status)
+
+    allow(ENV).to receive(:[]).and_call_original
+    allow(ENV).to receive(:[]).with('API_ENTREPRISE_KEY').and_return(api_entreprise_token)
   end
 
   after do
