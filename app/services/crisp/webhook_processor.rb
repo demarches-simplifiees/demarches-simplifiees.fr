6860c37f4e3863a79f9c4a17b125fd20f38c283c
Colin Darie <colin@darie.eu>
2025-09-01 10:50:19 +0200

    fix(crisp): sync metadata when webhook payload does not include directly user email

--- app/services/crisp/webhook_processor.rb
+++ app/services/crisp/webhook_processor.rb
@@ -2,13 +2,18 @@
 
 module Crisp
   class WebhookProcessor
+    include Dry::Monads[:result]
+
     def initialize(params)
       @params = params
-      @email = extract_email
+      @email = extract_email_from_params
     end
 
     def process
       return unless processable_event?
+
+      @email = fetch_email_from_session if email.blank?
+
       return if user.blank?
 
       CrispUpdatePeopleDataJob.perform_later(user)
@@ -19,15 +24,29 @@ module Crisp
     attr_reader :params, :email
 
     def processable_event?
-      params[:event] == "message:send"
+      params[:event] == "message:send" && params.dig(:data, :from) == "user"
     end
 
-    def extract_email
-      params.dig(:data, :user, :user_id)
+    def extract_email_from_params
+      # conversations from chat does not use an email as user id
+      maybe_email = params.dig(:data, :user, :user_id)
+      maybe_email&.include?("@") ? maybe_email : nil
     end
 
     def user
       @user ||= User.find_by(email:)
+    end
+
+    def fetch_email_from_session
+      session_id = params.dig(:data, :session_id)
+
+      result = Crisp::APIService.new.get_conversation_meta(session_id:)
+      case result
+      in Success(data: {email:})
+        email
+      in Failure(reason:)
+        fail reason
+      end
     end
   end
 end
--- spec/services/crisp/webhook_processor_spec.rb
+++ spec/services/crisp/webhook_processor_spec.rb
@@ -6,10 +6,13 @@ RSpec.describe Crisp::WebhookProcessor do
   let!(:user) { users(:default_user) }
   let(:event) { "message:send" }
   let(:email) { user.email }
+  let(:session_id) { "session_d57cb2d9-4607-42fe-a6be-000001112222" }
   let(:params) do
     {
       event:,
       data: {
+        session_id:,
+        from: "user",
         user: {
           user_id: email
         }
@@ -32,6 +35,20 @@ RSpec.describe Crisp::WebhookProcessor do
 
       it 'does not handle webhook' do
         expect { subject }.not_to have_enqueued_job
+      end
+    end
+
+    context 'with a session id instead of email' do
+      let(:email) { session_id } # simulate real payload !
+      before do
+        stub_request(:get, %r{^https://api.crisp.chat/v1/website/.+/conversation/.+/meta$})
+          .and_return(
+            body: { "error" => false, "reason" => "resolved", "data" => { "email" => user.email } }.to_json
+          )
+      end
+
+      it 'fetch email from API' do
+        expect { subject }.to have_enqueued_job.with(user)
       end
     end
 
