546299c0500d2ec49c80c898a80bd08d11a11c46
simon lehericey <mail@simon.lehericey.net>
2025-09-02 17:53:03 +0200

    fix: override external_error_present? in RIB cause it's not using external_id

--- spec/models/champs/piece_justificative_champ_spec.rb
+++ spec/models/champs/piece_justificative_champ_spec.rb
@@ -110,6 +110,14 @@ describe Champs::PieceJustificativeChamp do
       end
     end
 
+    describe "#external_error_present?" do
+      context 'an error is present' do
+        before { champ.update(fetch_external_data_exceptions: [ExternalDataException.new(reason: 'oops', code: 123)]) }
+
+        it { expect(champ.external_error_present?).to be_truthy }
+      end
+    end
+
     describe "cleaning up" do
       let(:dossier) { create(:dossier, procedure:) }
 
