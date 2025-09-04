318b18ec89e3d7866833f7bb9d50072535e0ed64
benoitqueyron <72251526+Benoit-MINT@users.noreply.github.com>
2025-09-04 17:06:58 +0200

    feat(badge notification): quand un dossier change de groupe instructeur, destroy les notifs des instructeurs non présents dans le nouveau groupe

--- app/models/dossier.rb
+++ app/models/dossier.rb
@@ -717,7 +717,7 @@ class Dossier < ApplicationRecord
 
     if !brouillon?
       unfollow_stale_instructeurs
-      update_notifications(self, previous_groupe_instructeur, groupe_instructeur) if previous_groupe_instructeur.present?
+      update_notifications(previous_groupe_instructeur, groupe_instructeur) if previous_groupe_instructeur.present?
 
       if author.present?
         log_dossier_operation(author, :changer_groupe_instructeur, self)
@@ -1238,8 +1238,13 @@ class Dossier < ApplicationRecord
     )
   end
 
-  def update_notifications(dossier, previous_groupe_instructeur, new_groupe_instructeur)
-    DossierNotification.destroy_notifications_by_dossier_and_type(dossier, :dossier_depose)
+  def update_notifications(previous_groupe_instructeur, new_groupe_instructeur)
     DossierNotification.create_notification(dossier, :dossier_depose) if dossier.en_construction? && dossier.follows.empty?
+
+    previous_groupe_instructeur.instructeurs.each do |instructeur|
+      if instructeur.groupe_instructeurs.exclude?(new_groupe_instructeur)
+        DossierNotification.destroy_notifications_instructeur_of_dossier(instructeur, self)
+      end
+    end
   end
 end
