ba0597c070bd18bcae56ae14897acbc3e95e6365
Colin Darie <colin@darie.eu>
2025-09-08 10:23:12 +0200

    refactor(secrets): staging auth as env var

--- app/services/staging_auth_service.rb
+++ app/services/staging_auth_service.rb
@@ -3,13 +3,11 @@
 class StagingAuthService
   def self.authenticate(username, password)
     if enabled?
-      username == Rails.application.secrets.basic_auth[:username] && password == Rails.application.secrets.basic_auth[:password]
+      username == ENV.fetch("BASIC_AUTH_USERNAME") && password == ENV.fetch("BASIC_AUTH_PASSWORD")
     else
       true
     end
   end
 
-  def self.enabled?
-    ENV['BASIC_AUTH_ENABLED'] == 'enabled'
-  end
+  def self.enabled? = ENV.enabled?("BASIC_AUTH")
 end
