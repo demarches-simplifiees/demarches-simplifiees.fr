68c5c86649ab23e761e89dea8a783038b421641b
Colin Darie <colin@darie.eu>
2025-09-08 14:50:18 +0200

    refactor(secrets): mailtrap as env var

--- config/initializers/mailtrap.rb
+++ config/initializers/mailtrap.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-if ENV.fetch('MAILTRAP_ENABLED') == 'enabled'
+if ENV.enabled?("MAILTRAP")
   ActiveSupport.on_load(:action_mailer) do
     module Mailtrap
       class SMTP < ::Mail::SMTP; end
@@ -8,8 +8,8 @@ if ENV.fetch('MAILTRAP_ENABLED') == 'enabled'
 
     ActionMailer::Base.add_delivery_method :mailtrap, Mailtrap::SMTP
     ActionMailer::Base.mailtrap_settings = {
-      user_name: Rails.application.secrets.mailtrap[:username],
-      password: Rails.application.secrets.mailtrap[:password],
+      user_name: ENV.fetch("MAILTRAP_USERNAME"),
+      password: ENV.fetch("MAILTRAP_PASSWORD"),
       address: 'sandbox.smtp.mailtrap.io',
       domain: 'sandbox.smtp.mailtrap.io',
       port: '2525',
