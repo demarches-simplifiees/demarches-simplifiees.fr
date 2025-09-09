efdef9c249635de2f1cf2a36941ac1afea3986dd
mfo <mfo@users.noreply.github.com>
2025-09-09 17:03:42 +0200

    feat(llm_enqueue_nightly_improve_procedure_job): make it idempotent

--- app/jobs/cron/llm_enqueue_nightly_improve_procedure_job.rb
+++ app/jobs/cron/llm_enqueue_nightly_improve_procedure_job.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'digest'
+
 class Cron::LLMEnqueueNightlyImproveProcedureJob < Cron::CronJob
   self.schedule_expression = "every day at 02:30"
 
@@ -9,7 +11,15 @@ class Cron::LLMEnqueueNightlyImproveProcedureJob < Cron::CronJob
       .find_each(batch_size: 200) do |procedure|
         next unless Flipper.enabled?(:llm_nightly_improve_procedure, procedure)
 
-        LLM::GenerateImproveLabelJob.perform_later(procedure.id)
+        procedure_revision = procedure.published_revision
+        schema = procedure_revision.schema_to_llm.to_json
+        schema_hash = Digest::SHA256.hexdigest(schema)
+
+        if !LLMRuleSuggestion.exists?(procedure_revision_id: procedure_revision.id, schema_hash:)
+          suggestion = LLMRuleSuggestion.create!(procedure_revision:, schema_hash:, state: :queued, rule: LLM::LabelImprover::TOOL_NAME)
+
+          LLM::GenerateImproveLabelJob.perform_later(suggestion.id)
+        end
       end
   end
 end
