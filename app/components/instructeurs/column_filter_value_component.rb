c740a0888a0dba9a495cb769873120aaade70f9c
Mathieu Magnin <mathieu@magnin.sh>
2025-09-09 17:51:25 +0200

    Editable filters: For each filter render a form to edit it

--- app/components/instructeurs/column_filter_value_component.rb
+++ app/components/instructeurs/column_filter_value_component.rb
@@ -3,9 +3,15 @@
 class Instructeurs::ColumnFilterValueComponent < ApplicationComponent
   attr_reader :filtered_column, :form
 
-  def initialize(filtered_column:, form:)
+  def initialize(filtered_column:, form:, edit_mode: false)
     @filtered_column = filtered_column
     @form = form
+    @edit_mode = edit_mode
+  end
+
+  def id
+    # temp id to avoid turbo-frame reload
+    @edit_mode ? "#{filtered_column.id.parameterize}_column_filter_value_component" : "column_filter_value_component"
   end
 
   def operator_hidden_field
@@ -16,6 +22,14 @@ class Instructeurs::ColumnFilterValueComponent < ApplicationComponent
 
   def column
     filtered_column&.column
+  end
+
+  def label
+    @edit_mode ? filtered_column&.label : t('.value')
+  end
+
+  def value
+    filtered_column&.filter_value
   end
 
   def column_filter_options
@@ -67,7 +81,8 @@ class Instructeurs::ColumnFilterValueComponent < ApplicationComponent
       class: 'fr-mt-1w',
       name: 'filter[filter][value][]',
       items: column_filter_options,
-      value_separator: false
+      value_separator: false,
+      selected_keys: filtered_column&.filter_value
     }
   end
 
---
+++ app/components/instructeurs/editable_filters_component.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class Instructeurs::EditableFiltersComponent < ApplicationComponent
+  attr_reader :procedure_presentation, :statut
+
+  def initialize(procedure_presentation:, statut:)
+    @procedure_presentation = procedure_presentation
+    @statut = statut
+  end
+
+  def filters
+    @procedure_presentation.filters_for(statut)
+  end
+end
