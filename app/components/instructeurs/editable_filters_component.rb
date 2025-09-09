7aa5aad54a1c4bcfa69095fd6c96f1fc3a31937c
Mathieu Magnin <mathieu@magnin.sh>
2025-09-09 17:51:25 +0200

    Editable filters: For each filter render a form to edit it

---
+++ app/components/instructeurs/editable_filters_component.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class Instructeurs::EditableFiltersComponent < ApplicationComponent
+  attr_reader :procedure_presentation, :statut, :instructeur_procedure
+
+  def initialize(procedure_presentation:, instructeur_procedure:, statut:)
+    @procedure_presentation = procedure_presentation
+    @instructeur_procedure = instructeur_procedure
+    @statut = statut
+  end
+
+  def filters
+    @procedure_presentation.filters_for(statut)
+  end
+end
