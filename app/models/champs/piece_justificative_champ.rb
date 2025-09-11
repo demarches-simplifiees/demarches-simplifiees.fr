4b1db8e6ba9052cfc202547efaa7a29c58ad5db2
Kara Diaby <kdiaby.pro@gmail.com>
2025-09-11 20:58:23 +0000

    Validations : règles dynamiques des PJ

--- app/models/champs/piece_justificative_champ.rb
+++ app/models/champs/piece_justificative_champ.rb
@@ -13,6 +13,9 @@ class Champs::PieceJustificativeChamp < Champ
     content_type: AUTHORIZED_CONTENT_TYPES,
     if: -> { can_validate? && !type_de_champ.skip_content_type_pj_validation }
 
+  validate :validate_dynamic_piece_justificative_rules,
+    if: -> { can_validate? && piece_justificative_file.attached? }
+
   def main_value_name
     :piece_justificative_file
   end
@@ -44,5 +47,31 @@ class Champs::PieceJustificativeChamp < Champ
   def fetch_external_data
     blob = piece_justificative_file.blobs.first
     OCRService.analyze(blob)
+  end
+
+  def validate_dynamic_piece_justificative_rules
+    allowed_types = nil
+    max_size = nil
+
+    if type_de_champ.titre_identite_nature?
+      allowed_types = type_de_champ.allowed_content_types
+      max_size = type_de_champ.max_file_size_bytes
+    elsif type_de_champ.rib_nature?
+      allowed_types = type_de_champ.allowed_content_types
+    elsif type_de_champ.pj_limit_formats? && type_de_champ.pj_format_families.present?
+      allowed_types = type_de_champ.allowed_content_types
+    end
+
+    return if allowed_types.nil? && max_size.nil?
+
+    piece_justificative_file.blobs.each do |blob|
+      if allowed_types.present? && !allowed_types.include?(blob.content_type)
+        errors.add(:piece_justificative_file, :content_type_invalid)
+      end
+
+      if max_size.present? && blob.byte_size > max_size
+        errors.add(:piece_justificative_file, :file_size_out_of_range, max_size: ActiveSupport::NumberHelper.number_to_human_size(max_size))
+      end
+    end
   end
 end
