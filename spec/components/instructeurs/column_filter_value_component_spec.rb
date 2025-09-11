be9966eca721614d9ec4bc169f0aed405ee6fc97
benoitqueyron <72251526+Benoit-MINT@users.noreply.github.com>
2025-09-11 14:55:44 +0200

    feat(badge notification): ajuste le choix de filtres selon les préférences de notification de l'instructeur

--- spec/components/instructeurs/column_filter_value_component_spec.rb
+++ spec/components/instructeurs/column_filter_value_component_spec.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 describe Instructeurs::ColumnFilterValueComponent, type: :component do
+  let!(:instructeur_procedure) { create(:instructeurs_procedure, display_message_notifications: 'none') }
+
   before do
     component = nil
 
     ActionView::Base.empty.form_with(url: "/") do |form|
-      component = described_class.new(filtered_column:, form:)
+      component = described_class.new(filtered_column:, form:, instructeur_procedure:)
     end
 
     render_inline(component)
@@ -16,7 +18,7 @@ describe Instructeurs::ColumnFilterValueComponent, type: :component do
 
   describe 'the select case' do
     let!(:column) do
-      column = double("Column", type: :enum, tdc_type: "drop_down_list", options_for_select:, mandatory: true)
+      column = double("Column", column: :value, type: :enum, tdc_type: "drop_down_list", options_for_select:, mandatory: true)
       allow(column).to receive(:is_a?).with(Columns::ChampColumn).and_return(true)
       column
     end
@@ -33,7 +35,7 @@ describe Instructeurs::ColumnFilterValueComponent, type: :component do
   end
 
   describe 'the input case' do
-    let(:column) { double("Column", type: :datetime, mandatory: true) }
+    let(:column) { double("Column", column: :value, type: :datetime, mandatory: true) }
 
     it { puts "page: #{page.text}" }
     it { expect(page).to have_selector('input[name="filter[filter][value][]"][type="date"]', count: 1) }
@@ -46,7 +48,7 @@ describe Instructeurs::ColumnFilterValueComponent, type: :component do
   end
 
   describe 'the yes no case' do
-    let(:column) { double("Column", type: :boolean, tdc_type: "yes_no", options_for_select: Champs::YesNoChamp.options, mandatory:) }
+    let(:column) { double("Column", column: :value, type: :boolean, tdc_type: "yes_no", options_for_select: Champs::YesNoChamp.options, mandatory:) }
 
     context 'when the column is mandatory' do
       let(:mandatory) { true }
@@ -67,7 +69,7 @@ describe Instructeurs::ColumnFilterValueComponent, type: :component do
   end
 
   describe 'the checkbox case' do
-    let(:column) { double("Column", type: :boolean, tdc_type: "checkbox", options_for_select: Champs::CheckboxChamp.options, mandatory:) }
+    let(:column) { double("Column", column: :value, type: :boolean, tdc_type: "checkbox", options_for_select: Champs::CheckboxChamp.options, mandatory:) }
 
     context 'when the column is mandatory' do
       let(:mandatory) { true }
@@ -85,6 +87,22 @@ describe Instructeurs::ColumnFilterValueComponent, type: :component do
       it { expect(page).to have_selector('input[name="filter[filter][value][]"][type="radio"]', count: 2) }
       it { expect(page).to have_selector('label[for="filter[filter][value][]_true"]', text: 'coché') }
       it { expect(page).to have_selector('label[for="filter[filter][value][]_false"]', text: 'non coché') }
+    end
+  end
+
+  describe 'the notification_type case' do
+    let(:column) { double("Column", column: 'notification_type', type: :enum, options_for_select:) }
+    let(:options_for_select) { I18n.t('instructeurs.dossiers.filterable_notification').map(&:to_a).map(&:reverse) }
+
+    context 'when the instructeur has chosen not to have certain notifications' do
+      let(:react_component) { page.find('react-component') }
+      let(:react_props_items) { JSON.parse(react_component['props']) }
+
+      it {
+        expect(react_props_items["items"].map(&:first)).to eq([
+          "Déposé depuis longtemps", "Dossier modifié", "Annotation privée", "Avis externe", "En attente de correction", "En attente d'avis externe"
+        ])
+      }
     end
   end
 end
