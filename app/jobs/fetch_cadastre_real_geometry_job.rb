d1ce02217c6d518816a227b999473dee40e27740
Colin Darie <colin@darie.eu>
2025-09-15 20:32:32 +0200

    fix: don't retry forever when geoarea was destroyed before job

--- app/jobs/fetch_cadastre_real_geometry_job.rb
+++ app/jobs/fetch_cadastre_real_geometry_job.rb
@@ -2,6 +2,7 @@
 
 class FetchCadastreRealGeometryJob < ApplicationJob
   MAX_ATTEMPT = 10
+  discard_on ActiveRecord::RecordNotFound
   retry_on StandardError, attempts: MAX_ATTEMPT, wait: :polynomially_longer
 
   def perform(geo_area)
