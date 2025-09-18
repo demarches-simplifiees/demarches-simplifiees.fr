9919117c2d16f481a10c948d24cc8d53c1c3598d
mfo <mfo@users.noreply.github.com>
2025-09-18 09:07:23 +0200

    refactor(Referentiels::ReferentielPrefillComponent): simplier/cleaner logic

--- app/components/referentiels/referentiel_prefill_component.rb
+++ app/components/referentiels/referentiel_prefill_component.rb
@@ -15,38 +15,92 @@ class Referentiels::ReferentielPrefillComponent < Referentiels::MappingFormBase
     Referentiels::MappingFormComponent::TYPES[:array] => %w[multiple_drop_down_list]
   }.freeze
 
+  PUBLIC_FIELDS_GROUP = "Champs"
+  PRIVATE_ANNOTATIONS_GROUP = "Annotations privées"
+
   def source_tdcs
-    if type_de_champ.public?
-      @source_tdcs ||= procedure.draft_revision.types_de_champ_for
-    else
-      @source_tdcs ||= procedure.draft_revision.types_de_champ_for(scope: :private)
+    @source_tdcs ||= begin
+      if type_de_champ.public?
+        procedure.draft_revision.types_de_champ_for
+      else
+        procedure.draft_revision.types_de_champ_for(scope: :private)
+      end
     end
   end
 
   def prefill_stable_id_tag(jsonpath, mapping_opts)
-    tdcs = tdc_targets(mapping_opts)
-    selected = lookup_existing_value(jsonpath, "prefill_stable_id")
-    options = type_de_champ.public? ? grouped_options_for_select(tdcs, selected) : options_for_select(tdcs, selected)
+    target_tdcs = tdc_targets(mapping_opts)
+    selected_value = lookup_existing_value(jsonpath, "prefill_stable_id")
+
     select_tag(
       attribute_name(jsonpath, "prefill_stable_id"),
-      options,
+      build_select_options(target_tdcs, selected_value),
       class: "fr-select"
     )
   end
 
   private
 
-  def tdc_targets(referentiel_mapping_element)
-    mapping_type = referentiel_mapping_element[:type]
-    allowed_types = MAPPING_TYPE_TO_TYPE_DE_CHAMP[mapping_type.to_sym] || []
-    source_tdcs
-      .each_with_object({ "Champs" => [], "Annotations privées" => [] }) do |tdc, grouped_tdcs|
-        next if tdc.stable_id == @type_de_champ.stable_id
-        next unless allowed_types.include?(tdc.type_champ)
+  def build_select_options(target_tdcs, selected_value)
+    if type_de_champ.public?
+      grouped_options_for_select(target_tdcs, selected_value)
+    else
+      options_for_select(target_tdcs, selected_value)
+    end
+  end
 
-        grouped_tdcs[tdc.public? ? "Champs" : "Annotations privées"] << [tdc.libelle_with_parent(@procedure.draft_revision), tdc.stable_id]
-      end
-      .then { type_de_champ.public? ? it : it["Annotations privées"] }
+  def tdc_targets(mapping_element)
+    allowed_types_for_mapping(mapping_element[:type])
+      .then { |allowed_types| filter_incompatible_tdcs(allowed_types) }
+      .then { |filtered_tdcs| group_tdcs_by_visibility(filtered_tdcs) }
+      .then { |grouped_tdcs| select_grouped_tdcs(grouped_tdcs) }
+  end
+
+  def allowed_types_for_mapping(mapping_type)
+    MAPPING_TYPE_TO_TYPE_DE_CHAMP[mapping_type.to_sym] || []
+  end
+
+  def filter_incompatible_tdcs(allowed_types)
+    source_tdcs.reject { current_field?(it) || incompatible_type?(it, allowed_types) }
+  end
+
+  def current_field?(tdc)
+    tdc.stable_id == type_de_champ.stable_id
+  end
+
+  def incompatible_type?(tdc, allowed_types)
+    allowed_types.exclude?(tdc.type_champ)
+  end
+
+  def group_tdcs_by_visibility(tdcs)
+    tdcs.each_with_object(empty_groups) do |tdc, grouped_tdcs|
+      group = visibility_group_for(tdc)
+      grouped_tdcs[group] << tdc_option_for(tdc)
+    end
+  end
+
+  def empty_groups
+    { PUBLIC_FIELDS_GROUP => [], PRIVATE_ANNOTATIONS_GROUP => [] }
+  end
+
+  def visibility_group_for(tdc)
+    if tdc.public?
+      PUBLIC_FIELDS_GROUP
+    else
+      PRIVATE_ANNOTATIONS_GROUP
+    end
+  end
+
+  def tdc_option_for(tdc)
+    [tdc.libelle_with_parent(@procedure.draft_revision), tdc.stable_id]
+  end
+
+  def select_grouped_tdcs(grouped_tdcs)
+    if type_de_champ.public?
+      grouped_tdcs
+    else
+      grouped_tdcs[PRIVATE_ANNOTATIONS_GROUP]
+    end
   end
 
   def render?
