52e386c3d1f3504f60d0e8f8b468e450ca3253cc
Colin Darie <colin@darie.eu>
2025-10-01 17:07:52 +0200

    chore(dev): reactionview debug only on demand with env var

--- config/initializers/reactionview.rb
+++ config/initializers/reactionview.rb
@@ -4,6 +4,6 @@ ReActionView.configure do |config|
   # Intercept .html.erb templates and process them with `Herb::Engine` for enhanced features
   # config.intercept_erb = true
 
-  # Enable debug mode in development (adds debug attributes to HTML)
-  config.debug_mode = Rails.env.development?
+  # Enable debug mode in development (adds debug elements and attributes to HTML, may break design).
+  config.debug_mode = Rails.env.development? && ENV["REACTIONVIEW_DEBUG"].present?
 end
