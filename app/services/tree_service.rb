b9d8f4036937d49e11f96d62c582f07b20a18aa7
simon lehericey <mail@simon.lehericey.net>
2025-10-01 16:30:10 +0200

    add tree_service (cannot do conditionnal yet)

---
+++ app/services/tree_service.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+class TreeService
+  def initialize(dossier)
+    @dossier = dossier
+  end
+
+  def tree
+    coordinates = @dossier.revision.revision_types_de_champ.filter(&:public?).filter(&:root?)
+    tree_it(coordinates)
+  end
+
+  private
+
+  def tree_it(coordinates, row_id: nil)
+    return [] if coordinates.blank?
+
+    head, *tail = coordinates
+
+    if head.header_section?
+      children, rest = tail.slice_before { same_level?(head, it) }.to_a
+      tree_children = tree_it(children, row_id:)
+
+      [to_champ(head, children: tree_children, row_id:)] + tree_it(rest, row_id:)
+
+    elsif head.repetition?
+      repetition = @dossier.project_champ(head.type_de_champ)
+      rows = repetition.row_ids.map do |row_id|
+        tree_children = tree_it(head.revision_types_de_champ, row_id:)
+        Row.new(children: tree_children)
+      end
+
+      [to_champ(head, rows:)] + tree_it(tail)
+
+    else
+      [to_champ(head, row_id:)] + tree_it(tail, row_id:)
+    end
+  end
+
+  def same_level?(header, el)
+    el.header_section? &&
+      header.type_de_champ.header_section_level_value == el.type_de_champ.header_section_level_value
+  end
+
+  def to_champ(coord, row_id: nil, children: nil, rows: nil)
+    champ = @dossier.project_champ(coord.type_de_champ, row_id:)
+    champ.children = children if children
+    champ.new_rows = rows if rows
+    champ
+  end
+
+  Row = Data.define(:children)
+end
---
+++ spec/services/tree_service_spec.rb
@@ -0,0 +1,80 @@
+# frozen_string_literal: true
+
+describe TreeService do
+  describe '.tree' do
+    let(:procedure) { create(:procedure, types_de_champ_public:) }
+    let(:dossier) { create(:dossier, :en_construction, procedure:) }
+
+    subject { described_class.new(dossier).tree }
+
+    context 'with a champ' do
+      let(:types_de_champ_public) { [{ type: :text, libelle: 'text' }] }
+
+      it { expect(subject.map(&:libelle)).to eq(["text"]) }
+    end
+
+    context 'with a empty header section' do
+      let(:types_de_champ_public) { [{ type: :header_section, libelle: 'h1' }] }
+
+      it { expect(subject.map(&:libelle)).to eq(["h1"]) }
+    end
+
+    context 'with a header section and a champ' do
+      let(:types_de_champ_public) do
+        [
+          { type: :header_section, libelle: 'h1' },
+          { type: :text, libelle: 'text' }
+        ]
+      end
+      let(:header_section) { subject.first }
+
+      it do
+        expect(subject.map(&:libelle)).to eq(["h1"])
+        expect(header_section.children.map(&:libelle)).to eq(["text"])
+      end
+    end
+
+    context 'with a header section and a nested header section and a champ' do
+      let(:types_de_champ_public) do
+        [
+          { type: :header_section, libelle: 'h1' },
+          { type: :header_section, libelle: 'h2', level: 2 },
+          { type: :text, libelle: 'text' }
+        ]
+      end
+      let(:header_section) { subject.first }
+      let(:nested_header_section) { header_section.children.first }
+
+      it do
+        expect(subject.map(&:libelle)).to eq(["h1"])
+        expect(header_section.children.map(&:libelle)).to eq(["h2"])
+        expect(nested_header_section.children.map(&:libelle)).to eq(["text"])
+      end
+    end
+
+    context 'with nested section and repetition' do
+      let(:types_de_champ_public) do
+        [
+          { type: :header_section, level: 1, libelle: 'h1' },
+          {
+            type: :repetition,
+            libelle: 'repetition',
+            children: [
+              { type: :header_section, level: 1, libelle: 'nested h1' },
+              { type: :header_section, level: 2, libelle: 'nested h2' },
+              { type: :header_section, level: 1, libelle: 'another nested h1' }
+            ]
+          }
+        ]
+      end
+      let(:header_section) { subject.first }
+      let(:repetition) { header_section.children.first }
+
+      it do
+        expect(subject.map(&:libelle)).to eq(["h1"])
+        expect(header_section.children.map(&:libelle)).to eq(["repetition"])
+        expect(repetition.new_rows.first.children.map(&:libelle)).to eq(["nested h1", "another nested h1"])
+      end
+    end
+  end
+end
