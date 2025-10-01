f2079bf0197dba3101b42e3ffb23bde4b4c112dc
mfo <mfo@users.noreply.github.com>
2025-10-01 06:42:00 +0200

    feat(Cron::LLMEnqueueNightlyImproveProcedureJob): split in two jobs so we can run one job on demand per procedure

---
+++ app/jobs/llm/improve_procedure_job.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'digest'
+
+module LLM
+  class ImproveProcedureJob < ApplicationJob
+    queue_as :default
+
+    def perform(procedure)
+      return unless Flipper.enabled?(:llm_nightly_improve_procedure, procedure)
+      procedure_revision = procedure.draft_revision
+      schema_hash = Digest::SHA256.hexdigest(procedure_revision.schema_to_llm.to_json)
+      return if LLMRuleSuggestion.exists?(procedure_revision: procedure_revision, schema_hash:)
+      available_rules.each do |rule|
+        suggestion = LLMRuleSuggestion.create!(procedure_revision: procedure_revision, schema_hash:, state: :queued, rule:)
+        LLM::GenerateRuleSuggestionJob.perform_later(suggestion)
+      end
+    end
+
+    private
+    def available_rules
+      [
+        LLM::LabelImprover::TOOL_NAME,
+        LLM::StructureImprover::TOOL_NAME
+      ]
+    end
+  end
+end
--- spec/jobs/cron/llm_enqueue_nightly_improve_procedure_job_spec.rb
+++ spec/jobs/cron/llm_enqueue_nightly_improve_procedure_job_spec.rb
@@ -1,23 +1,17 @@
 # frozen_string_literal: true
 
 describe Cron::LLMEnqueueNightlyImproveProcedureJob, type: :job do
-  subject { described_class.perform_now }
+  subject(:perform) { described_class.perform_now }
 
   let!(:p1) { create(:procedure, :published) }
   let!(:p2) { create(:procedure, :published) }
 
   before { Flipper.enable_actor(:llm_nightly_improve_procedure, p1) }
 
-  describe 'perform' do
-    it 'enqueues jobs only for procedures with feature enabled' do
-      expect { subject }.to have_enqueued_job(LLM::GenerateRuleSuggestionJob).with(an_instance_of(LLMRuleSuggestion)).twice
-    end
+  it 'enqueues the dedicated job only for procedures with feature enabled' do
+    perform
 
-    context 'idempotence' do
-      it 'create once' do
-        expect { subject }.to change { LLMRuleSuggestion.count }.by(2)
-        expect { subject }.not_to change { LLMRuleSuggestion.count }
-      end
-    end
+    expect(LLM::ImproveProcedureJob).to have_been_enqueued.with(p1)
+    expect(LLM::ImproveProcedureJob).not_to have_been_enqueued.with(p2)
   end
 end
