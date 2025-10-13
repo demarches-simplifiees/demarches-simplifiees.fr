aa864654c01e95c69e93482ac2702abf8961eb15
benoitqueyron <72251526+Benoit-MINT@users.noreply.github.com>
2025-10-13 17:56:43 +0200

    feat(badge notification): lorsqu'un admin modifie le duree de conservation (à la hausse), on supprime les notifs :dossier_expirants et :dossier_suppression (s'il a été supprimé par l'expiration seulement)

--- spec/jobs/reset_expiring_dossiers_job_spec.rb
+++ spec/jobs/reset_expiring_dossiers_job_spec.rb
@@ -2,25 +2,54 @@
 
 describe ResetExpiringDossiersJob do
   subject { described_class.new(procedure).perform_now }
+
   let(:duree_conservation_dossiers_dans_ds) { 2 }
   let(:procedure) { create(:procedure, duree_conservation_dossiers_dans_ds:) }
 
+  let!(:expiring_dossier_brouillon) { create(:dossier, :brouillon, procedure: procedure, brouillon_close_to_expiration_notice_sent_at: duree_conservation_dossiers_dans_ds.months.ago) }
+  let!(:expiring_dossier_en_construction) { create(:dossier, :en_construction, procedure: procedure, en_construction_close_to_expiration_notice_sent_at: duree_conservation_dossiers_dans_ds.months.ago) }
+  let!(:expiring_dossier_termine) { create(:dossier, :accepte, procedure: procedure, termine_close_to_expiration_notice_sent_at: duree_conservation_dossiers_dans_ds.months.ago) }
+  let!(:automatic_expiring_dossier) { create(:dossier, :accepte, procedure:, termine_close_to_expiration_notice_sent_at: 3.weeks.ago, hidden_by_expired_at: 1.week.ago) }
+
   describe '.perform_now' do
     it 'resets flags' do
-      expiring_dossier_brouillon = create(:dossier, :brouillon, procedure: procedure, brouillon_close_to_expiration_notice_sent_at: duree_conservation_dossiers_dans_ds.months.ago)
-      expiring_dossier_en_construction = create(:dossier, :en_construction, procedure: procedure, en_construction_close_to_expiration_notice_sent_at: duree_conservation_dossiers_dans_ds.months.ago)
-      expiring_dossier_en_termine = create(:dossier, :accepte, procedure: procedure, termine_close_to_expiration_notice_sent_at: duree_conservation_dossiers_dans_ds.months.ago)
-      automatic_expiring_dossier = create(:dossier, :accepte, procedure:, termine_close_to_expiration_notice_sent_at: 3.weeks.ago, hidden_by_expired_at: 1.week.ago)
-
       subject
 
       expect(expiring_dossier_brouillon.reload.brouillon_close_to_expiration_notice_sent_at).to eq(nil)
       expect(expiring_dossier_en_construction.reload.en_construction_close_to_expiration_notice_sent_at).to eq(nil)
-      expect(expiring_dossier_en_termine.reload.termine_close_to_expiration_notice_sent_at).to eq(nil)
+      expect(expiring_dossier_termine.reload.termine_close_to_expiration_notice_sent_at).to eq(nil)
       expect(expiring_dossier_brouillon.expired_at).to be_within(1.hour).of(2.months.from_now)
       expect(expiring_dossier_en_construction.expired_at).to be_within(1.hour).of(2.months.from_now)
-      expect(expiring_dossier_en_termine.expired_at).to be_within(1.hour).of(2.months.from_now)
+      expect(expiring_dossier_termine.expired_at).to be_within(1.hour).of(2.months.from_now)
       expect(automatic_expiring_dossier.reload.hidden_by_expired_at).to eq(nil)
+    end
+
+    it 'destroys dossier_expirant notification' do
+      notif_expirant_dossier_en_construction = create(:dossier_notification, dossier: expiring_dossier_en_construction, notification_type: :dossier_expirant)
+      notif_expirant_dossier_termine = create(:dossier_notification, dossier: expiring_dossier_termine, notification_type: :dossier_expirant)
+
+      subject
+
+      expect(DossierNotification.count).to eq(0)
+    end
+
+    context "when the dossier is hidden" do
+      let!(:hidden_dossier) { create(:dossier, :accepte, procedure:, termine_close_to_expiration_notice_sent_at: 3.weeks.ago) }
+      let!(:notification_suppression) { create(:dossier_notification, dossier: hidden_dossier, notification_type: :dossier_suppression) }
+
+      it "destroys dossier_suppression if the dossier is already hidden by expired" do
+        hidden_dossier.update(hidden_by_expired_at: 1.week.ago)
+        subject
+
+        expect(DossierNotification.count).to eq(0)
+      end
+
+      it "does not destroy dossier_suppression notification if the dossier is hidden by administraiton" do
+        hidden_dossier.update(hidden_by_administration_at: 2.weeks.ago)
+        subject
+
+        expect(DossierNotification.all).to include(notification_suppression)
+      end
     end
   end
 end
