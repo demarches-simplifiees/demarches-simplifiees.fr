1069279e9fcde226f33a8a997609375a9e9ff5a5
Mathieu Magnin <mathieu@magnin.sh>
2025-10-14 12:25:26 +0200

    When api geo fails, adressecontroller should not return a 200

--- app/controllers/data_sources/adresse_controller.rb
+++ app/controllers/data_sources/adresse_controller.rb
@@ -9,6 +9,12 @@ class DataSources::AdresseController < ApplicationController
         results = JSON.parse(response.body, symbolize_names: true)
 
         return render json: APIGeoService.format_address_response(results)
+      elsif response.timed_out?
+        return head :gateway_timeout
+      else
+        Sentry.set_extras(body: response.body, code: response.code)
+        Sentry.capture_message("Adresse API failure: #{response.return_message}")
+        return head :bad_gateway
       end
     end
 
@@ -17,7 +23,7 @@ class DataSources::AdresseController < ApplicationController
   rescue JSON::ParserError => e
     Sentry.set_extras(body: response.body, code: response.code)
     Sentry.capture_exception(e)
-    render json: []
+    return head :server_error
   end
 
   private
