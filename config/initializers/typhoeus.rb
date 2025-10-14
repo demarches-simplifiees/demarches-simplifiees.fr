f83ede4c711bfd89f277630ecfdec558cfe9c31e
simon lehericey <mail@simon.lehericey.net>
2025-10-14 15:03:21 +0200

    feat: always use cache in Typhoeus

--- config/initializers/typhoeus.rb
+++ config/initializers/typhoeus.rb
@@ -1,3 +1,7 @@
 # frozen_string_literal: true
 
 Typhoeus::Config.user_agent = APPLICATION_NAME
+
+Rails.application.config.after_initialize do
+  Typhoeus::Config.cache = Typhoeus::Cache::SuccessfulRequestsRailsCache.new
+end
