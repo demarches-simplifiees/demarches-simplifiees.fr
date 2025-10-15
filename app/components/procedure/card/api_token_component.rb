6cf993fa1733f878ec19e091ee5b257ea3785658
Lisa Durand <lisa.c.durand@gmail.com>
2025-10-15 15:05:50 +0200

    feat: improve admin API token card with correct badges

--- app/components/procedure/card/api_token_component.rb
+++ app/components/procedure/card/api_token_component.rb
@@ -1,7 +1,19 @@
 # frozen_string_literal: true
 
 class Procedure::Card::APITokenComponent < ApplicationComponent
+  API_TOKENS_AVAILABLE_COUNT = 2
   def initialize(procedure:)
     @procedure = procedure
+  end
+
+  def api_tokens_count_for_badge
+    "#{api_tokens_count} / #{API_TOKENS_AVAILABLE_COUNT}"
+  end
+
+  def api_tokens_count
+    [
+      @procedure.specific_api_entreprise_token? || nil,
+      @procedure.api_particulier_token.presence,
+    ].compact.size
   end
 end
