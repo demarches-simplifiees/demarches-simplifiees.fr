cf6ee740666035057984d2608aa91f4db4c8beba
Colin Darie <colin@darie.eu>
2025-10-15 21:40:51 +0200

    fix(pj): bunch of n+1 when adding attachment to piece justificative champ

--- app/controllers/champs/piece_justificative_controller.rb
+++ app/controllers/champs/piece_justificative_controller.rb
@@ -35,6 +35,10 @@ class Champs::PieceJustificativeController < Champs::ChampController
       @champ.fetch_later! if @champ.uses_external_data?
 
       @champ.update_timestamps
+
+      dossier = DossierPreloader.load_one(@champ.dossier, pj_template: true)
+      # because preloader reassigns new champ instances champs, we have to reassign it
+      @champ = dossier.champs.find { it.id == @champ.id }
     end
 
     save_succeed
