84ea4dddafdf18fca1d63064e1f76bc558ef95a3
simon lehericey <mail@simon.lehericey.net>
2025-11-01 18:06:39 +0100

    feat: use the simplified system
    - use original error if possible
    - use StandardError otherwise
    - token error is not retryable

--- app/models/champs/siret_champ.rb
+++ app/models/champs/siret_champ.rb
@@ -29,21 +29,21 @@ class Champs::SiretChamp < Champ
   def fetch_external_data
     etablissement = APIEntrepriseService.create_etablissement(self, external_id.delete(" "), dossier.user&.id)
     if etablissement.blank?
-      Failure(retryable: false, reason: Excon::Error::NotFound.new('NotFound'), code: 404)
+      Failure(retryable: false, reason: StandardError.new('NotFound'), code: 404)
     else
       Success(etablissement:)
     end
-  rescue APIEntrepriseToken::TokenError
-    Failure(retryable: true, reason: Excon::Error::Unauthorized.new('wrong token'), code: 401)
+  rescue APIEntrepriseToken::TokenError => error
+    Failure(retryable: false, reason: error, code: 401)
   rescue APIEntreprise::API::Error => error
     if APIEntrepriseService.service_unavailable_error?(error, target: :insee)
       update!(
         etablissement: APIEntrepriseService.create_etablissement_as_degraded_mode(self, external_id, dossier.user&.id)
       )
-      Failure(retryable: true, reason: Excon::Error::ServiceUnavailable.new('APIDown'), code: 503)
+      Failure(retryable: true, reason: error, code: 503)
     else
       Sentry.capture_exception(error, extra: { dossier_id:, siret: external_id })
-      Failure(retryable: false, reason: Excon::Error::InternalServerError.new('API crashed'), code: 500)
+      Failure(retryable: false, reason: error, code: 500)
     end
   end
 
