9abc601bf9aba75349b72d69f594fe83ceec7749
mfo <mfo@users.noreply.github.com>
2025-11-05 09:37:15 +0100

    refactor(stepper): extract basic stepper class for simpliscore funnel

--- app/components/referentiels/stepper_component.rb
+++ app/components/referentiels/stepper_component.rb
@@ -1,41 +1,32 @@
 # frozen_string_literal: true
 
-class Referentiels::StepperComponent < ViewComponent::Base
-  attr_reader :referentiel, :type_de_champ, :procedure, :step_component
+class Referentiels::StepperComponent < StepperBaseComponent
+  delegate :referentiel, :type_de_champ, :procedure, to: :step_component
 
-  def initialize(referentiel:, type_de_champ:, procedure:, step_component:)
-    @referentiel = referentiel
-    @type_de_champ = type_de_champ
-    @procedure = procedure
-    @step_component = step_component
+  def initialize(step_component:)
+    super(step_component:)
   end
 
   def back_link
-    opts = { class: 'fr-link fr-icon-arrow-left-line fr-link--icon--left fr-icon--sm' }
-
-    if type_de_champ.public?
-      link_to "Champs du formulaire", champs_admin_procedure_path(procedure), opts
-    else
-      link_to "Annotations privées", annotations_admin_procedure_path(procedure), opts
-    end
+    helpers.link_to(back_link_label, back_path, class: 'fr-link fr-icon-arrow-left-line fr-link--icon--left fr-icon--sm')
   end
 
   def title
-    "Configuration #{type_de_champ.public? ? 'du champ' : 'de l\'annotation privée'} « #{type_de_champ.libelle} »"
-  end
-
-  def step_state
-    "Étape #{current_step} sur #{step_count}"
+    if type_de_champ.public?
+      "Configuration du champ « #{type_de_champ.libelle} »"
+    else
+      "Configuration de l'annotation privée « #{type_de_champ.libelle} »"
+    end
   end
 
   def step_title
-    if step_component == Referentiels::NewFormComponent || (step_component == Referentiels::ConfigurationErrorComponent && referentiel.exact_match?)
+    if step_component == Referentiels::NewFormComponent || step_component == Referentiels::ConfigurationErrorComponent && referentiel.exact_match?
       "Requête"
     elsif step_component == Referentiels::MappingFormComponent
       "Réponse et mapping"
     elsif step_component == Referentiels::PrefillAndDisplayComponent
       "Pré remplissage des champs et/ou affichage des données récupérées"
-    elsif step_component == Referentiels::AutocompleteConfigurationComponent || (step_component == Referentiels::ConfigurationErrorComponent && referentiel.autocomplete?)
+    elsif step_component == Referentiels::AutocompleteConfigurationComponent || step_component == Referentiels::ConfigurationErrorComponent && referentiel.autocomplete?
       "Configuration de l'autocomplétion"
     end
   end
@@ -69,5 +60,19 @@ class Referentiels::StepperComponent < ViewComponent::Base
 
   def step_count
     referentiel.mode == 'exact_match' ? 3 : 4
+  end
+
+  private
+
+  def back_link_label
+    type_de_champ.public? ? 'Champs du formulaire' : 'Annotations privées'
+  end
+
+  def back_path
+    if type_de_champ.public?
+      helpers.champs_admin_procedure_path(procedure)
+    else
+      helpers.annotations_admin_procedure_path(procedure)
+    end
   end
 end
