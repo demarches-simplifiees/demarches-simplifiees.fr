2d914fced2509e9bb05c93f75fb1339fb04d312d
Kara Diaby <kdiaby.pro@gmail.com>
2025-11-10 09:09:52 +0000

    Fix apres retours UX / UI

--- spec/controllers/instructeurs/dossiers_controller_spec.rb
+++ spec/controllers/instructeurs/dossiers_controller_spec.rb
@@ -811,7 +811,7 @@ describe Instructeurs::DossiersController, type: :controller do
           procedure_id: procedure.id,
           dossier_id: dossier.id,
           commentaire: { body: body },
-          mark_as_pending_response: 'true'
+          mark_as_pending_response: 'true',
         }
       end
 
@@ -844,7 +844,7 @@ describe Instructeurs::DossiersController, type: :controller do
           procedure_id: procedure.id,
           dossier_id: dossier.id,
           commentaire: { body: body },
-          mark_as_pending_response: 'false'
+          mark_as_pending_response: 'false',
         }
       end
 
@@ -904,7 +904,7 @@ describe Instructeurs::DossiersController, type: :controller do
           file: file,
         },
         mark_as_pending_response: mark_as_pending_response,
-        statut: 'a-suivre'
+        statut: 'a-suivre',
       }
     }
 
