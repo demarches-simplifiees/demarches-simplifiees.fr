3eb39a7d48e90270fad2487afacf40d84b30c2a1
mfo <mfo@users.noreply.github.com>
2025-11-17 11:21:55 +0100

    refactor(stepper): extract basic stepper class for simpliscore funnel

---
+++ app/components/stepper_base_component.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+class StepperBaseComponent < ViewComponent::Base
+  attr_reader :step_component
+
+  def initialize(step_component:)
+    @step_component = step_component
+  end
+
+  def back_link
+    raise NotImplementedError
+  end
+
+  def title
+    raise NotImplementedError
+  end
+
+  def step_title
+    raise NotImplementedError
+  end
+
+  def next_step_title
+    raise NotImplementedError
+  end
+
+  def current_step
+    raise NotImplementedError
+  end
+
+  def step_count
+    raise NotImplementedError
+  end
+
+  def step_state
+    "Étape #{current_step} sur #{step_count}"
+  end
+end
