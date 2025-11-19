f017aa8ab774105b74bf31a31324ea09e81dfa0c
mfo <mfo@users.noreply.github.com>
2025-11-19 14:39:17 +0100

    fix(backfill.siret): try to go with raw sql to avoid timeout

--- spec/tasks/maintenance/t20251112backfill_champ_siret_external_state_with_etablissement_but_no_value_or_external_id_task_spec.rb
+++ spec/tasks/maintenance/t20251112backfill_champ_siret_external_state_with_etablissement_but_no_value_or_external_id_task_spec.rb
@@ -6,20 +6,22 @@ module Maintenance
   RSpec.describe T20251112backfillChampSiretExternalStateWithEtablissementButNoValueOrExternalIdTask do
     describe "#process" do
       let(:procedure) { create(:procedure, types_de_champ_public: [{ type: :siret }]) }
-      let(:etablissement) { create(:etablissement, siret: "44011762001530") }
-      let(:dossier) { create(:dossier, :with_populated_champs, procedure:, etablissement:) }
-      let(:champ) { dossier.project_champs_public.first }
+      let(:dossier1) { create(:dossier, procedure:) }
+      let(:dossier2) { create(:dossier, procedure:) }
+      let(:champ1) { dossier1.project_champs_public.first }
+      let(:champ2) { dossier2.project_champs_public.first }
+      let(:etablissement1)  { create(:etablissement, siret: "12345678901234") }
+      let(:etablissement2)  { create(:etablissement, siret: "01232123456789") }
 
       subject(:process) { described_class.process(Champs::SiretChamp.all) }
-
       before do
-        champ.update_columns(etablissement_id: etablissement.id, value: nil, external_id: nil, external_state: :fetched)
+        champ1.update(etablissement: etablissement1, external_state: 'fetched', external_id: nil, value: nil)
+        champ2.update(etablissement: etablissement2, external_state: 'fetched', external_id: nil, value: nil)
       end
-
-      context 'when champ has an etablissement' do
-        it 'backfills the value and external_id with etablissement siret' do
-          expect { subject }.to change { champ.reload.value }.from(nil).to(etablissement.siret)
-          expect(champ.external_id).to eq(etablissement.siret)
+      context 'when champs was filled with SiretController' do
+        it 'works' do
+          expect { subject }.to change { champ1.reload.external_id }.to(etablissement1.siret)
+          expect(champ2.reload.external_id).to eq(etablissement2.siret)
         end
       end
     end
