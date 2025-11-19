4eebc1cde7fea296bd894487532c21b3d9e70158
Kara Diaby <kdiaby.pro@gmail.com>
2025-11-19 17:20:23 +0000

    Permet de supprimer un message en attente de réponse et de supprimer le badge

--- app/models/dossier_notification.rb
+++ app/models/dossier_notification.rb
@@ -13,8 +13,9 @@ class DossierNotification < ApplicationRecord
     annotation_instructeur: 'annotation_instructeur',
     avis_externe: 'avis_externe',
     attente_correction: 'attente_correction',
-    attente_avis: 'attente_avis',
     attente_reponse: 'attente_reponse',
+    attente_avis: 'attente_avis',
+
   }
 
   belongs_to :instructeur
