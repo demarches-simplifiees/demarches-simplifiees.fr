ee743cc3e1410071a92b9207868343ac728bc7cd
Mathieu Magnin <mathieu@magnin.sh>
2025-11-28 10:57:35 +0100

    fix(api-entreprise-jeton): when jeton is invalid the view should not crash

--- spec/controllers/administrateurs/procedures_controller_spec.rb
+++ spec/controllers/administrateurs/procedures_controller_spec.rb
@@ -1302,6 +1302,8 @@ describe Administrateurs::ProceduresController, type: :controller do
   end
 
   describe 'PATCH #jeton' do
+    render_views
+
     let(:procedure) { create(:procedure, administrateur: admin) }
     let(:token) { "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c" }
     let(:api_response_body) { nil }
