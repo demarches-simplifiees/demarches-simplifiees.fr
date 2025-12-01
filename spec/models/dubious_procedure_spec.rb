078260dc659e576f9ddfd1417000ca1bd6363a6e
Corinne Durrmeyer <corinne@inseo.fr>
2025-12-01 12:48:03 +0100

    fix(typo): Replace t' with t’

--- spec/models/dubious_procedure_spec.rb
+++ spec/models/dubious_procedure_spec.rb
@@ -10,7 +10,7 @@ describe DubiousProcedure, type: :model do
       let(:forbidden_tdcs) do
         [
           { libelle: 'num de securite sociale, stp' },
-          { libelle: "t'aurais une carte bancaire ?" },
+          { libelle: "t’aurais une carte bancaire ?" },
         ]
       end
 
@@ -20,7 +20,7 @@ describe DubiousProcedure, type: :model do
         expect(subject.first.id).to eq(procedure.id)
         expect(subject.first.libelle).to eq(procedure.libelle)
         expect(subject.first.dubious_champs).to include("num de securite sociale, stp")
-        expect(subject.first.dubious_champs).to include("t'aurais une carte bancaire ?")
+        expect(subject.first.dubious_champs).to include("t’aurais une carte bancaire ?")
       end
 
       context 'and a whitelisted procedure' do
