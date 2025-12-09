f2bf22efbff28e9f2a098e7040a37517382782be
simon lehericey <mail@simon.lehericey.net>
2025-12-09 11:47:03 +0100

    spec: fix flaky spec by using find which (should) wait for the element and retry

--- spec/system/users/en_construction_spec.rb
+++ spec/system/users/en_construction_spec.rb
@@ -47,7 +47,7 @@ describe "Dossier en_construction", js: true do
       visit_dossier(dossier)
 
       expect(page).to have_button("Supprimer le fichier toto.png")
-      click_on "Supprimer le fichier toto.png"
+      find('button', text: 'Supprimer le fichier toto.png').click
       expect(page).to have_text("La pièce jointe (toto.png) a bien été supprimée. Vous pouvez en ajouter une autre.")
 
       input_selector = "##{champ.focusable_input_id}"
