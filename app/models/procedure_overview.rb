3fca73876914a1779f341db6cafc565047b95ea5
benoitqueyron <72251526+Benoit-MINT@users.noreply.github.com>
2025-12-11 10:30:25 +0100

    perf(weekly email summary): supprime les N+1 sur les dossiers

--- app/models/procedure_overview.rb
+++ app/models/procedure_overview.rb
@@ -8,25 +8,24 @@ class ProcedureOverview
     :dossiers_en_construction_count,
     :old_dossiers_en_construction
 
-  def initialize(procedure, groups)
+  def initialize(procedure, dossiers)
     @start_date = 1.week.ago.beginning_of_week
     @procedure = procedure
 
-    dossiers = procedure.dossiers.where(groupe_instructeur: groups).visible_by_administration
+    @dossiers_en_instruction = dossiers.filter(&:en_instruction?)
+    @dossiers_en_construction = dossiers.filter(&:en_construction?)
 
-    @dossiers_en_instruction_count = dossiers.state_en_instruction.count
-    @old_dossiers_en_instruction = dossiers
-      .state_en_instruction
-      .where(en_instruction_at: ...1.week.ago)
+    @dossiers_en_instruction_count = @dossiers_en_instruction.count
+    @dossiers_en_construction_count = @dossiers_en_construction.count
 
-    @dossiers_en_construction_count = dossiers.state_en_construction.count
-    @old_dossiers_en_construction = dossiers
-      .state_en_construction
-      .where(depose_at: ...1.week.ago)
+    @old_dossiers_en_instruction =
+      @dossiers_en_instruction.filter { |d| d.en_instruction_at < 1.week.ago }
 
-    @created_dossiers_count = dossiers
-      .where(created_at: @start_date..Time.zone.now)
-      .count
+    @old_dossiers_en_construction =
+      @dossiers_en_construction.filter { |d| d.depose_at < 1.week.ago }
+
+    @created_dossiers_count =
+      dossiers.count { |d| d.created_at >= @start_date }
   end
 
   def had_some_activities?
