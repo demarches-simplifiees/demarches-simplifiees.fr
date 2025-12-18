27a92cc218c72856149d25561f672ccf21c699d7
Eric Leroy-Terquem <eric.leroyterquem@beta.gouv.fr>
2025-12-18 12:20:08 +0100

    fix(instructeurs import): send only one email notification if instructeur added to many groups

--- spec/mailers/previews/groupe_instructeur_mailer_preview.rb
+++ spec/mailers/previews/groupe_instructeur_mailer_preview.rb
@@ -16,4 +16,21 @@ class GroupeInstructeurMailerPreview < ActionMailer::Preview
     instructeurs = Instructeur.limit(2)
     GroupeInstructeurMailer.notify_added_instructeurs(groupe, instructeurs, current_instructeur_email)
   end
+
+  def notify_added_instructeur_from_groupes_import
+    procedure = Procedure.new(id: 1, libelle: 'une superbe procedure')
+    groups = [GroupeInstructeur.new(id: 1, label: 'Val-De-Marne', procedure:), GroupeInstructeur.new(id: 2, label: 'Seine-Saint-Denis', procedure:)]
+    current_instructeur_email = 'admin@dgfip.com'
+    instructeur = Instructeur.last
+    GroupeInstructeurMailer.notify_added_instructeur_from_groupes_import(instructeur, groups, current_instructeur_email)
+  end
+
+  def confirm_and_notify_added_instructeur_from_groupes_import
+    procedure = Procedure.new(id: 1, libelle: 'une superbe procedure')
+    groups = [GroupeInstructeur.new(id: 1, label: 'Val-De-Marne', procedure:), GroupeInstructeur.new(id: 2, label: 'Seine-Saint-Denis', procedure:)]
+    current_instructeur_email = 'admin@dgfip.com'
+    instructeur = Instructeur.last
+    @reset_password_token = instructeur.user.send(:set_reset_password_token)
+    GroupeInstructeurMailer.confirm_and_notify_added_instructeur_from_groupes_import(instructeur, groups, current_instructeur_email)
+  end
 end
