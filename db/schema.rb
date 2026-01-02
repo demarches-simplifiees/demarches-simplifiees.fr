ffae8283bbde4fe38ab263f8c646d54f273ba632
benoitqueyron <72251526+Benoit-MINT@users.noreply.github.com>
2026-01-02 09:41:50 +0100

    db(migration): ajoute birthcountry à la table france_connect_informations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_11_21_091000) do
+ActiveRecord::Schema[7.2].define(version: 2026_01_02_083858) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_buffercache"
   enable_extension "pg_stat_statements"
@@ -753,6 +753,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_11_21_091000) do
   end
 
   create_table "france_connect_informations", id: :serial, force: :cascade do |t|
+    t.string "birthcountry"
     t.date "birthdate"
     t.string "birthplace"
     t.datetime "created_at", precision: nil, null: false
