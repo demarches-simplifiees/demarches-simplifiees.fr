ab39f77c4db96680a19c47399f894e1ccc9c51bc
benoitqueyron <72251526+Benoit-MINT@users.noreply.github.com>
2026-01-08 15:15:20 +0100

    feat(quotient familial): conditionne le champ PJ de substitution en cas de user non fc, données incorrectes ou erreur api

--- app/models/types_de_champ/quotient_familial_type_de_champ.rb
+++ app/models/types_de_champ/quotient_familial_type_de_champ.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class TypesDeChamp::QuotientFamilialTypeDeChamp < TypesDeChamp::TypeDeChampBase
+  include Logic
+
   def create_substitution_champ
     draft = @type_de_champ.revisions.first.procedure.draft_revision
 
@@ -11,6 +13,7 @@ class TypesDeChamp::QuotientFamilialTypeDeChamp < TypesDeChamp::TypeDeChampBase
         libelle: 'Justificatif de quotient familial',
         type_champ: TypeDeChamp.type_champs.fetch(:piece_justificative),
         parent_stable_id: @type_de_champ.stable_id,
+        condition: ds_not_eq(champ_value(@type_de_champ.stable_id), constant('correct')),
       }
     )
   end
